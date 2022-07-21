66c52429331ce5167aed85c54e250ab36b5dff99
Config Connector Team <no-reply@google.com>
2022-07-21 16:10:44 +0000

    Automated Config Connector import.
      - 9a4008c656daa539aa7615a03824821e8a4f6b48 Implement unit tests for resourcewatcher by Config Connector Team <no-reply@google.com>
    
    GitOrigin-RevId: 9a4008c656daa539aa7615a03824821e8a4f6b48

--- pkg/controller/resourcewatcher/resourcewatcher.go
+++ pkg/controller/resourcewatcher/resourcewatcher.go
@@ -53,6 +53,59 @@ func NewWithClient(dynamicClient dynamic.Interface, logger logr.Logger) *Resourc
 	}
 }
 
+// WaitForResourceToBeReady waits for the resource identified by the given GVK
+// and NamespacedName. It blocks until the resource is ready, an error occurs, or a context
+// cancellation occurs. Note that a nil return value signifies that the resource is ready and
+// no errors have occurred.
+func (r *ResourceWatcher) WaitForResourceToBeReady(ctx context.Context, nn types.NamespacedName, gvk schema.GroupVersionKind) error {
+	logger := r.logger.WithValues("resource", nn, "resourceGVK", gvk)
+	watch, err := r.WatchResource(ctx, nn, gvk)
+	if err != nil {
+		return err
+	}
+	defer watch.Stop()
+	logger.Info("successfully created watch on resource")
+	return WaitForResourceToBeReadyViaWatch(ctx, watch, logger)
+}
+
+// WatchResource creates a watch on a resource identified by the given GVK and NamespacedName.
+func (r *ResourceWatcher) WatchResource(ctx context.Context, nn types.NamespacedName, gvk schema.GroupVersionKind) (watch.Interface, error) {
+	client := r.dynamicClient.Resource(k8s.ToGVR(gvk)).Namespace(nn.Namespace)
+	nameSelector := fields.OneTermEqualSelector("metadata.name", nn.Name).String()
+	watch, err := client.Watch(ctx, metav1.ListOptions{FieldSelector: nameSelector})
+	if err != nil {
+		return nil, fmt.Errorf("error creating watch on resource: %w", err)
+	}
+	return watch, nil
+}
+
+// WaitForResourceToBeReadyViaWatch monitors a given 'Watch' for any
+// updates to the resource that the given 'Watch' is targeting. Note that
+// an error is returned to signify a failure during the 'Watch' process,
+// while nil is returned to signify the watched resource is ready.
+func WaitForResourceToBeReadyViaWatch(ctx context.Context, watch watch.Interface, logger logr.Logger) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("context was cancelled: %w", ctx.Err())
+		case event, ok := <-watch.ResultChan():
+			if !ok {
+				return fmt.Errorf("watch channel was closed")
+			}
+			ok, reason, err := isResourceReady(event)
+			if err != nil {
+				return fmt.Errorf("error checking if resource is ready: %w", err)
+			}
+			if !ok {
+				logger.Info("resource not ready", "reason", reason)
+				continue
+			}
+			logger.Info("resource is ready")
+			return nil
+		}
+	}
+}
+
 // isResourceReady returns whether a resource identified by the given GVK
 // and NamespacedName is ready. Note that a 'reason' for failure is returned only
 // when the resource is not ready and no fatal error has occurred.
@@ -72,40 +125,4 @@ func isResourceReady(event watch.Event) (ok bool, reason string, err error) {
 		return false, "resource not ready", nil
 	}
 	return true, "", nil
-}
-
-// WaitForResourceToBeReady waits for the resource identified by the given GVK
-// and NamespacedName. It blocks until the resource is ready, an error occurs, or a context
-// cancellation occurs. Note that a nil return value signifies that the resource is ready and
-// no errors have occurred.
-func (r *ResourceWatcher) WaitForResourceToBeReady(ctx context.Context, nn types.NamespacedName, gvk schema.GroupVersionKind) error {
-	logger := r.logger.WithValues("resource", nn, "resourceGVK", gvk)
-	client := r.dynamicClient.Resource(k8s.ToGVR(gvk)).Namespace(nn.Namespace)
-	nameSelector := fields.OneTermEqualSelector("metadata.name", nn.Name).String()
-	watch, err := client.Watch(ctx, metav1.ListOptions{FieldSelector: nameSelector})
-	if err != nil {
-		return fmt.Errorf("error creating watch on resource: %w", err)
-	}
-	logger.Info("successfully created watch on resource")
-	defer watch.Stop()
-	for {
-		select {
-		case <-ctx.Done():
-			return fmt.Errorf("context was cancelled: %v", ctx.Err())
-		case event, ok := <-watch.ResultChan():
-			if !ok {
-				return fmt.Errorf("watch channel was closed")
-			}
-			ok, reason, err := isResourceReady(event)
-			if err != nil {
-				return fmt.Errorf("error checking if resource is ready: %w", err)
-			}
-			if !ok {
-				logger.Info("resource not ready", "reason", reason)
-				continue
-			}
-			logger.Info("resource is ready")
-			return nil
-		}
-	}
 }
---
+++ pkg/controller/resourcewatcher/resourcewatcher_test.go
@@ -0,0 +1,115 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package resourcewatcher_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"testing"
+	"time"
+
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/resourcewatcher"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/k8s"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	dynamicfake "k8s.io/client-go/dynamic/fake"
+)
+
+func TestWatchResourceTimeout(t *testing.T) {
+	unreadyResourceUnstructured := newResourceUnstructured(newStatus(corev1.ConditionFalse))
+	gvk, nn, err := getResourceInformation(unreadyResourceUnstructured)
+	if err != nil {
+		t.Fatalf("got unexpected error: %v", err)
+	}
+	fake := dynamicfake.NewSimpleDynamicClient(runtime.NewScheme())
+	logger := log.Log.WithName("resourcewatcher-test-timeout")
+	watch, err := resourcewatcher.NewWithClient(fake, logger).WatchResource(context.TODO(), nn, gvk)
+	if err != nil {
+		t.Fatalf("got unexpected error: %v", err)
+	}
+	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*10)
+	defer cancel()
+	if err := resourcewatcher.WaitForResourceToBeReadyViaWatch(ctx, watch, logger); !errors.Is(err, context.DeadlineExceeded) {
+		t.Fatalf("got error '%v', expected '%v'", err, context.DeadlineExceeded)
+	}
+}
+
+func TestWatchResourceSuccess(t *testing.T) {
+	readyResourceUnstructured := newResourceUnstructured(newStatus(corev1.ConditionTrue))
+	gvk, nn, err := getResourceInformation(readyResourceUnstructured)
+	if err != nil {
+		t.Fatalf("got unexpected error: %v", err)
+	}
+	fake := dynamicfake.NewSimpleDynamicClient(runtime.NewScheme())
+	logger := log.Log.WithName("resourcewatcher-test-success")
+	watch, err := resourcewatcher.NewWithClient(fake, logger).WatchResource(context.TODO(), nn, gvk)
+	if err != nil {
+		t.Fatalf("got unexpected error: %v", err)
+	}
+	fake.Resource(k8s.ToGVR(gvk)).
+		Namespace(nn.Namespace).
+		Create(context.TODO(), readyResourceUnstructured, metav1.CreateOptions{})
+	ctx, cancel := context.WithTimeout(context.TODO(), time.Minute)
+	defer cancel()
+	if err := resourcewatcher.WaitForResourceToBeReadyViaWatch(ctx, watch, logger); err != nil {
+		t.Fatalf("got unexpected error: %v", err)
+	}
+}
+
+func newResourceUnstructured(status map[string]interface{}) *unstructured.Unstructured {
+	u := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "pubsub.cnrm.cloud.google.com/v1beta1",
+			"kind":       "PubSubTopic",
+			"metadata": map[string]interface{}{
+				"name":      "test_topic",
+				"namespace": "test_namespace",
+			},
+			"spec": map[string]interface{}{
+				"resourceID": "pubsubtopic-sample",
+			},
+		},
+	}
+	if status != nil {
+		u.Object["status"] = status
+	}
+	return u
+}
+
+func newStatus(readyStatus corev1.ConditionStatus) map[string]interface{} {
+	return map[string]interface{}{
+		"conditions": []interface{}{
+			map[string]interface{}{
+				"type":   "Ready",
+				"status": string(readyStatus),
+			},
+		},
+	}
+}
+
+func getResourceInformation(u *unstructured.Unstructured) (schema.GroupVersionKind, types.NamespacedName, error) {
+	resource, err := k8s.NewResource(u)
+	if err != nil {
+		return schema.GroupVersionKind{}, types.NamespacedName{}, fmt.Errorf("error creating k8s resource from unstructured: %w", err)
+	}
+	return resource.GroupVersionKind(), resource.GetNamespacedName(), nil
+}
