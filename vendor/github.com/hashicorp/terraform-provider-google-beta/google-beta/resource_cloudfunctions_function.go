828e31e50850f8ab8a04b7d954670eea56f8a588
Config Connector Team <no-reply@google.com>
2023-08-08 22:37:53 +0800

    Automated Config Connector import.
      - 9f6790a5c5cd59d0e0c1464ad0cb981465d9a4bc remove vendor directory by Config Connector Team <no-reply@google.com>
    
    GitOrigin-RevId: 9f6790a5c5cd59d0e0c1464ad0cb981465d9a4bc

--- vendor/github.com/hashicorp/terraform-provider-google-beta/google-beta/resource_cloudfunctions_function.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-package google
-
-import (
-	"github.com/hashicorp/terraform-provider-google-beta/google-beta/services/cloudfunctions"
-)
-
-type cloudFunctionId = cloudfunctions.CloudFunctionId
