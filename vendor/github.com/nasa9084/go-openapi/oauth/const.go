8973aab6798ad137cf157a1e7b6c2f55da6b11a1
Config Connector Team <no-reply@google.com>
2023-08-08 14:27:31 -0400

    Automated Config Connector import.
      - 346b59e60ea92d066cf0a1c410e78fee7a9f8a71 Remove vendor directory by Config Connector Team <no-reply@google.com>
    
    GitOrigin-RevId: 346b59e60ea92d066cf0a1c410e78fee7a9f8a71

--- vendor/github.com/nasa9084/go-openapi/oauth/const.go
+++
@@ -1,9 +0,0 @@
-package oauth
-
-// Constants relates OAuth flow
-const (
-	ImplicitFlow          = "implicit"
-	PasswordFlow          = "password"
-	ClientCredentialsFlow = "clientCredentials"
-	AuthorizationCodeFlow = "authorizationCode"
-)
