6e8ae2fd67461baa1b4e4d898eb862a9692d223c
Config Connector Team <no-reply@google.com>
2023-08-25 00:59:35 +0000

    Automated Config Connector import.
      - f349298f42ef74550d65a39a48fea00d298f454c Ensure resources in the allowlists are ordered alphabetic... by Config Connector Team <no-reply@google.com>
    
    GitOrigin-RevId: f349298f42ef74550d65a39a48fea00d298f454c

--- scripts/unit-test.sh
+++ scripts/unit-test.sh
@@ -18,5 +18,5 @@ set -o pipefail
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 cd "${REPO_ROOT}"
 make -C operator test
-UNIT_TEST_PACKAGES=$(go list ./pkg/... ./cmd/... ./config/tests/... ./scripts/generate-go-crd-clients/... | grep -v mocktests) 
+UNIT_TEST_PACKAGES=$(go list ./pkg/... ./cmd/... ./config/tests/... ./scripts/generate-go-crd-clients/... ./scripts/resource-autogen/... | grep -v mocktests)
 go test -v ${UNIT_TEST_PACKAGES} -coverprofile cover.out -count=1
