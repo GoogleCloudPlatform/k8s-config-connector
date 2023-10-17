d484a1a8853de41959123f73fc84c2df6fa35484
justinsb <justinsb@google.com>
2023-10-16 22:09:30 -0400

    mockgcp: implement attachedCluster update

--- mockgcp/mockgkemulticloud/attachedcluster.go
+++ mockgcp/mockgkemulticloud/attachedcluster.go
@@ -70,6 +70,53 @@ func (s *GKEMulticloudV1) CreateAttachedCluster(ctx context.Context, req *pb.Cre
 	return s.operations.NewLRO(ctx)
 }
 
+func (s *GKEMulticloudV1) UpdateAttachedCluster(ctx context.Context, req *pb.UpdateAttachedClusterRequest) (*longrunning.Operation, error) {
+	name, err := s.parseAttachedClustersName(req.GetAttachedCluster().GetName())
+	if err != nil {
+		return nil, err
+	}
+
+	fqn := name.String()
+	obj := &pb.AttachedCluster{}
+	if err := s.storage.Get(ctx, fqn, obj); err != nil {
+		if apierrors.IsNotFound(err) {
+			return nil, status.Errorf(codes.NotFound, "attachedCluster %q not found", fqn)
+		}
+		return nil, status.Errorf(codes.Internal, "error reading attachedCluster: %v", err)
+	}
+	// Mask of fields to update. At least one path must be supplied in
+	// this field. The elements of the repeated paths field can only include these
+	// fields from
+	// [AttachedCluster][mockgcp.cloud.gkemulticloud.v1.AttachedCluster]:
+	//
+	//   - `description`.
+	//   - `annotations`.
+	//   - `platform_version`.
+	//   - `authorization.admin_users`.
+	//   - `logging_config.component_config.enable_components`.
+	//   - `monitoring_config.managed_prometheus_config.enabled`.
+
+	paths := req.GetUpdateMask().GetPaths()
+	if len(paths) == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "must specify updateMask")
+	}
+
+	// TODO: Some sort of helper for fieldmask?
+	for _, path := range paths {
+		switch path {
+		case "description":
+			obj.Description = req.GetAttachedCluster().GetDescription()
+		default:
+			return nil, status.Errorf(codes.InvalidArgument, "update_mask path %q not valid", path)
+		}
+	}
+
+	if err := s.storage.Update(ctx, fqn, obj); err != nil {
+		return nil, status.Errorf(codes.Internal, "error updating attachedCluster: %v", err)
+	}
+	return s.operations.NewLRO(ctx)
+}
+
 func (s *GKEMulticloudV1) DeleteAttachedCluster(ctx context.Context, req *pb.DeleteAttachedClusterRequest) (*longrunning.Operation, error) {
 	name, err := s.parseAttachedClustersName(req.Name)
 	if err != nil {
