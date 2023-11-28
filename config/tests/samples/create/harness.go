d7fccbe944e76490585f5fe0b0a11634df8a2a0f
justinsb <justinsb@google.com>
2023-11-28 17:22:59 -0500

    cp:mockgcp: Implement ComputeSSLCertificate
    Integrates c0b4f8e from #936
    
    Signed-off-by: Alex Pana <8968914+acpana@users.noreply.github.com>

--- config/tests/samples/create/harness.go
+++ config/tests/samples/create/harness.go
@@ -652,6 +652,7 @@ func MaybeSkip(t *testing.T, name string, resources []*unstructured.Unstructured
 			case schema.GroupKind{Group: "compute.cnrm.cloud.google.com", Kind: "ComputeNetwork"}:
 			case schema.GroupKind{Group: "compute.cnrm.cloud.google.com", Kind: "ComputeNodeGroup"}:
 			case schema.GroupKind{Group: "compute.cnrm.cloud.google.com", Kind: "ComputeNodeTemplate"}:
+			case schema.GroupKind{Group: "compute.cnrm.cloud.google.com", Kind: "ComputeManagedSSLCertificate"}:
 			case schema.GroupKind{Group: "compute.cnrm.cloud.google.com", Kind: "ComputeSubnetwork"}:
 				// ok
 
