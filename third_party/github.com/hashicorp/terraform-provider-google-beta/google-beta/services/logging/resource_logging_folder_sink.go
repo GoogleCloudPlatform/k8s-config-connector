432d7191e202455550167dd495788485ec234f46
Shuxian Cai <shuxiancai@google.com>
2023-11-29 00:08:39 +0000

    combined_log_sink.patch
    Add a combined logging log sink resource to the provider, in order to
    avoid needing to implement more generic resource merging in KCC.

--- third_party/github.com/hashicorp/terraform-provider-google-beta/google-beta/services/logging/resource_logging_folder_sink.go
+++ third_party/github.com/hashicorp/terraform-provider-google-beta/google-beta/services/logging/resource_logging_folder_sink.go
@@ -85,6 +85,10 @@ func resourceLoggingFolderSinkRead(d *schema.ResourceData, meta interface{}) err
 		return fmt.Errorf("Error setting include_children: %s", err)
 	}
 
+	if err := d.Set("unique_writer_identity", false); err != nil {
+		return fmt.Errorf("Error setting unique_writer_identity: %s", err)
+	}
+
 	return nil
 }
 
