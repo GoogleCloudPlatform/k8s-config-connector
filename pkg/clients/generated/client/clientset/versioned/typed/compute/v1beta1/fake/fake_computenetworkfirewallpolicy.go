e49621a17772ef11e86c23bf03746cf495cdbfdd
Joyce Ma <maqiuyu@google.com>
2024-01-04 22:50:20 +0000

    Rerun 'make ready-pr'

--- pkg/clients/generated/client/clientset/versioned/typed/compute/v1beta1/fake/fake_computenetworkfirewallpolicy.go
+++ pkg/clients/generated/client/clientset/versioned/typed/compute/v1beta1/fake/fake_computenetworkfirewallpolicy.go
@@ -27,7 +27,6 @@ import (
 	v1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/clients/generated/apis/compute/v1beta1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	labels "k8s.io/apimachinery/pkg/labels"
-	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
 	testing "k8s.io/client-go/testing"
@@ -39,9 +38,9 @@ type FakeComputeNetworkFirewallPolicies struct {
 	ns   string
 }
 
-var computenetworkfirewallpoliciesResource = schema.GroupVersionResource{Group: "compute.cnrm.cloud.google.com", Version: "v1beta1", Resource: "computenetworkfirewallpolicies"}
+var computenetworkfirewallpoliciesResource = v1beta1.SchemeGroupVersion.WithResource("computenetworkfirewallpolicies")
 
-var computenetworkfirewallpoliciesKind = schema.GroupVersionKind{Group: "compute.cnrm.cloud.google.com", Version: "v1beta1", Kind: "ComputeNetworkFirewallPolicy"}
+var computenetworkfirewallpoliciesKind = v1beta1.SchemeGroupVersion.WithKind("ComputeNetworkFirewallPolicy")
 
 // Get takes name of the computeNetworkFirewallPolicy, and returns the corresponding computeNetworkFirewallPolicy object, and an error if there is any.
 func (c *FakeComputeNetworkFirewallPolicies) Get(ctx context.Context, name string, options v1.GetOptions) (result *v1beta1.ComputeNetworkFirewallPolicy, err error) {
