9839d9922a886a7794c2acade8da6f75315c43b5
justinsb <justinsb@google.com>
2024-02-12 21:17:30 -0500

    mockgcp: send 204 instead of 200 status code
    It's hard to do this with grpc-gateway, so instead we use an http
    filter.
    
    Likely clients don't actually care about 200 vs 204, but helps with
    our golden testing.

--- mockgcp/mockbigquery/service.go
+++ mockgcp/mockbigquery/service.go
@@ -15,6 +15,7 @@
 package mockbigquery
 
 import (
+	"bytes"
 	"context"
 	"net/http"
 
@@ -62,5 +63,63 @@ func (s *MockService) NewHTTPMux(ctx context.Context, conn *grpc.ClientConn) (ht
 	if err != nil {
 		return nil, err
 	}
-	return mux, nil
+
+	// To be compatible with the real BigQuery API, we need to serve a 204 on DELETE
+	// Add a filter to do that, it's hard to do with grpc-gateway
+	filter := func(w http.ResponseWriter, r *http.Request) {
+		brw := &bufferedResponseWriter{
+			header: make(http.Header),
+		}
+		mux.ServeHTTP(brw, r)
+		// Send a 204 on DELETE instead of a 200 with an empty response.
+		if brw.statusCode == 200 && r.Method == http.MethodDelete && brw.body.String() == "{}" {
+			brw.statusCode = 204
+			brw.body.Reset()
+			brw.header.Set("Content-Length", "0")
+			brw.header.Del("Cache-Control")
+		}
+		brw.WriteTo(w)
+	}
+
+	return http.HandlerFunc(filter), nil
+}
+
+// bufferedResponseWriter implements http.ResponseWriter and stores the response.
+type bufferedResponseWriter struct {
+	statusCode int
+	body       bytes.Buffer
+	header     http.Header
+}
+
+var _ http.ResponseWriter = &bufferedResponseWriter{}
+
+// Header implements http.ResponseWriter
+func (w *bufferedResponseWriter) Header() http.Header {
+	return w.header
+}
+
+// Write implements http.ResponseWriter
+func (w *bufferedResponseWriter) Write(b []byte) (int, error) {
+	if w.statusCode == 0 {
+		w.statusCode = 200
+	}
+	return w.body.Write(b)
+}
+
+// WriteHeader implements http.ResponseWriter
+func (w *bufferedResponseWriter) WriteHeader(statusCode int) {
+	w.statusCode = statusCode
+}
+
+// WriteTo writes the buffered response to a different http.ResponseWriter.
+func (w *bufferedResponseWriter) WriteTo(out http.ResponseWriter) {
+	for k, values := range w.header {
+		out.Header()[k] = values
+	}
+	statusCode := w.statusCode
+	if statusCode == 0 {
+		statusCode = 200
+	}
+	out.WriteHeader(statusCode)
+	out.Write(w.body.Bytes())
 }
