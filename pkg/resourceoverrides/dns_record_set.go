4d46d3b57c5d9f3dec7e43a1ced0ed7bf64b20c1
Alex Pana <8968914+acpana@users.noreply.github.com>
2024-02-16 00:34:26 +0000

    run make fmt
    Signed-off-by: Alex Pana <8968914+acpana@users.noreply.github.com>

--- pkg/resourceoverrides/dns_record_set.go
+++ pkg/resourceoverrides/dns_record_set.go
@@ -103,7 +103,7 @@ func enforceMutuallyExclusiveRrdatasAndRoutingPolicy() ResourceOverride {
 		}); err != nil {
 			return err
 		}
-	
+
 		return crdutil.SetSchemaForFieldUnderObjectOrArray("spec", schema, &spec)
 	}
 	return o
