98838c51e221ae44c2ceef0c6d74782440575c73
justinsb <justinsb@google.com>
2024-03-03 14:32:07 +0000

    Update generated code

--- pkg/clients/generated/apis/containeranalysis/v1beta1/doc.go
+++ pkg/clients/generated/apis/containeranalysis/v1beta1/doc.go
@@ -35,7 +35,4 @@
 // +k8s:defaulter-gen=TypeMeta
 // +groupName=containeranalysis.cnrm.cloud.google.com
 
-// Generate deepcopy object for containeranalysis/v1beta1 API group
-//
-//go:generate go run ../../../../../../scripts/deepcopy-gen/main.go -O zz_generated.deepcopy -i . -h ../../../../../../hack/boilerplate_client_alpha.go.txt
 package v1beta1
