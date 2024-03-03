fe3e2d493edc5af7dea604f13b577800e1dfcce9
justinsb <justinsb@google.com>
2024-03-03 14:32:07 +0000

    Update generated code

--- pkg/clients/generated/apis/accesscontextmanager/v1alpha1/doc.go
+++ pkg/clients/generated/apis/accesscontextmanager/v1alpha1/doc.go
@@ -35,7 +35,4 @@
 // +k8s:defaulter-gen=TypeMeta
 // +groupName=accesscontextmanager.cnrm.cloud.google.com
 
-// Generate deepcopy object for accesscontextmanager/v1alpha1 API group
-//
-//go:generate go run ../../../../../../scripts/deepcopy-gen/main.go -O zz_generated.deepcopy -i . -h ../../../../../../hack/boilerplate_client_alpha.go.txt
 package v1alpha1
