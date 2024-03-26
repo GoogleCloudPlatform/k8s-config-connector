143dc8fb09fd93a18583a72337628d248d836767
justinsb <justinsb@google.com>
2024-03-26 13:11:09 +0000

    tests: support strictly ordered creation in golden tests

--- tests/e2e/unified_test.go
+++ tests/e2e/unified_test.go
@@ -147,7 +147,6 @@ func testFixturesInSeries(ctx context.Context, t *testing.T, testPause bool, can
 					primaryResource := bytesToUnstructured(t, fixture.Create, uniqueID, project)
 
 					opt := create.CreateDeleteTestOptions{CleanupResources: true}
-					opt.Create = append(opt.Create, primaryResource)
 
 					if fixture.Dependencies != nil {
 						dependencyYamls := testyaml.SplitYAML(t, fixture.Dependencies)
@@ -156,6 +155,8 @@ func testFixturesInSeries(ctx context.Context, t *testing.T, testPause bool, can
 							opt.Create = append(opt.Create, depUnstruct)
 						}
 					}
+
+					opt.Create = append(opt.Create, primaryResource)
 
 					if fixture.Update != nil {
 						u := bytesToUnstructured(t, fixture.Update, uniqueID, project)
@@ -307,6 +308,11 @@ func testFixturesInSeries(ctx context.Context, t *testing.T, testPause bool, can
 				opt.CleanupResources = false // We delete explicitly below
 				if testPause {
 					opt.SkipWaitForReady = true // Paused resources don't send out an event yet.
+				}
+				if os.Getenv("GOLDEN_REQUEST_CHECKS") != "" {
+					// If we're doing golden request checks, delete synchronously so that it is reproducible.
+					// Note that this does introduce a dependency that objects are ordered correctly for deletion.
+					opt.CreateInOrder = true
 				}
 				create.RunCreateDeleteTest(h, opt)
 
