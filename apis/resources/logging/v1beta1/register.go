181c00cbe80bea9e79ceb04086a36e89ad42e20c
Alex Pana <8968914+acpana@users.noreply.github.com>
2024-04-23 17:17:35 +0000

    move groupversion to types
    Signed-off-by: Alex Pana <8968914+acpana@users.noreply.github.com>

--- apis/resources/logging/v1beta1/register.go
+++ apis/resources/logging/v1beta1/register.go
@@ -14,26 +14,9 @@
 
 package v1beta1
 
-import (
-	"reflect"
-
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"sigs.k8s.io/controller-runtime/pkg/scheme"
-)
+import "k8s.io/apimachinery/pkg/runtime/schema"
 
 var (
 	// SchemeGroupVersion is the group version used to register these objects.
 	SchemeGroupVersion = schema.GroupVersion{Group: "logging.cnrm.cloud.google.com", Version: "v1beta1"}
-
-	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
-	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
-
-	// AddToScheme is a global function that registers this API group & version to a scheme
-	AddToScheme = SchemeBuilder.AddToScheme
-
-	LoggingLogMetricGVK = schema.GroupVersionKind{
-		Group:   SchemeGroupVersion.Group,
-		Version: SchemeGroupVersion.Version,
-		Kind:    reflect.TypeOf(LoggingLogMetric{}).Name(),
-	}
 )
