989f4de426c613d9c25667fef4193194ea08e3ea
xiaoweim <xiaoweim@google.com>
2024-05-02 19:04:04 +0000

    feat:add an expander image version CRD

---
+++ experiments/compositions/composition/api/v1alpha1/expanderversion_types.go
@@ -0,0 +1,57 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// ExpanderVersion is the Schema for the compositions API
+type ExpanderVersion struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ExpanderVersionSpec   `json:"spec,omitempty"`
+	Status ExpanderVersionStatus `json:"status,omitempty"`
+}
+
+// ExpanderVersionSpec defines the desired state of ExpanderVersion
+type ExpanderVersionSpec struct {
+	// Defines the image registry for the expander specified by metadata.name
+	ImageRegistry string   `json:"imageRegistry,omitempty"`
+	ValidVersions []string `json:"validVersions"`
+}
+
+// ExpanderVersionStatus defines the observed state of ExpanderVersion
+type ExpanderVersionStatus struct {
+}
+
+//+kubebuilder:object:root=true
+
+// ExpanderVersionList contains a list of Composition
+type ExpanderVersionList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ExpanderVersion `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ExpanderVersion{}, &ExpanderVersionList{})
+}
