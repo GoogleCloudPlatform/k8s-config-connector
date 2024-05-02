82a614d95453a8ed1e7debbeeef0ab6f3e8077e2
xiaoweim <xiaoweim@google.com>
2024-05-02 22:20:45 +0000

    feat:add an expander image crd.

--- experiments/compositions/composition/cmd/main.go
+++ experiments/compositions/composition/cmd/main.go
@@ -127,6 +127,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Facade")
 		os.Exit(1)
 	}
+	if err = (&controller.ExpanderVersionReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ExpanderVersion")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
