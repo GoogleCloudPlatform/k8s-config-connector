b2b0ad1f3c8ee0cb6e49dfa0e803884c1ece1dce
Barni S <barni@google.com>
2024-05-16 18:40:58 -0400

    Add ExpanderVersion reconciler.
    - Add Validation for the ExpanderVersion
    - Process ExpanderVersion and populate the status with a map of image/service

--- experiments/compositions/composition/api/v1alpha1/expanderversion_types.go
+++ experiments/compositions/composition/api/v1alpha1/expanderversion_types.go
@@ -17,11 +17,24 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	semver "github.com/Masterminds/semver/v3"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+type ExpanderType string
+
+const (
+	// ExpanderTypeJob run expander in a job
+	ExpanderTypeJob ExpanderType = "job"
+	// ExpanderTypeGRPC expect expander service to be present
+	ExpanderTypeGRPC ExpanderType = "grpc"
+)
 
 // ExpanderVersionSpec defines the desired state of ExpanderVersion
 type ExpanderVersionSpec struct {
@@ -39,13 +52,13 @@ type ExpanderVersionSpec struct {
 	//   grpc - grpc service expander. persistent
 	// +kubebuilder:validation:Enum=job;grpc
 	// +kubebuilder:default=job
-	Type string `json:"type"`
+	Type ExpanderType `json:"type"`
 }
 
 // ExpanderVersionStatus defines the observed state of ExpanderVersion
 type ExpanderVersionStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	VersionMap map[string]string  `json:"versionMap,omitempty"`
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
 //+kubebuilder:object:root=true
@@ -67,6 +80,51 @@ type ExpanderVersionList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []ExpanderVersion `json:"items"`
+}
+
+// Status helpers
+func (s *ExpanderVersionStatus) ClearCondition(condition ConditionType) {
+	meta.RemoveStatusCondition(&s.Conditions, string(condition))
+}
+
+// Validation
+func (ev *ExpanderVersion) Validate() bool {
+	// Several of these validations should eventually be CEL rules on the composition CRD
+	ev.Status.ClearCondition(ValidationFailed)
+	// Validate
+	message := ""
+	if ev.Spec.ImageRegistry == "" && ev.Spec.Type == ExpanderTypeJob {
+		message += "spec.imageRegistry required for type=job; "
+	}
+	if len(ev.Spec.ValidVersions) == 0 {
+		message += "spec.validVersions required; "
+	}
+
+	invalidVersions := []string{}
+	invalidVersionMessage := ""
+	for _, r := range ev.Spec.ValidVersions {
+		_, err := semver.NewVersion(r)
+		if err != nil {
+			invalidVersions = append(invalidVersions, r)
+			invalidVersionMessage += fmt.Sprintf("%s: %v\n", r, err)
+		}
+	}
+	if invalidVersionMessage != "" {
+		message += fmt.Sprintf("invalid versions: %v\n", invalidVersions)
+		message += invalidVersionMessage
+	}
+
+	if message != "" {
+		ev.Status.Conditions = append(ev.Status.Conditions, metav1.Condition{
+			LastTransitionTime: metav1.Now(),
+			Message:            message,
+			Reason:             "ValidationFailed",
+			Type:               string(ValidationFailed),
+			Status:             metav1.ConditionTrue,
+		})
+		return false
+	}
+	return true
 }
 
 func init() {
