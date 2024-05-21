e4bc6c9a341786f8930a9b5251a52f17e6298acf
Ziyue Yan <ziyue@google.com>
2024-05-21 22:17:47 +0000

    Find the hubfeaturemembership resource when doing the Delete

--- pkg/controller/direct/gkehub/featuremembership_controller.go
+++ pkg/controller/direct/gkehub/featuremembership_controller.go
@@ -121,6 +121,14 @@ func (a *gkeHubAdapter) Find(ctx context.Context) (bool, error) {
 
 // Delete implements the Adapter interface.
 func (a *gkeHubAdapter) Delete(ctx context.Context) (bool, error) {
+	exist, err := a.Find(ctx)
+	if err != nil {
+		return false, fmt.Errorf("finding feature for %s:%w", a.featureID, err)
+	}
+	if !exist {
+		// return (false, nil) if the object was not found but should be presumed deleted.
+		return false, nil
+	}
 	// emptying the membershipspec is sufficient
 	a.desired = &featureapi.MembershipFeatureSpec{}
 	if _, err := a.patchMembershipSpec(ctx); err != nil {
