f5ccc75100100a48efe59dca9c4999d7fb2daef7
justinsb <justinsb@google.com>
2024-05-21 21:15:32 -0400

    autogen: generate direct-actuation CRDs

--- pkg/clients/generated/apis/monitoring/v1beta1/monitoringdashboard_types.go
+++ pkg/clients/generated/apis/monitoring/v1beta1/monitoringdashboard_types.go
@@ -72,20 +72,7 @@ type DashboardAggregation struct {
 	// +optional
 	CrossSeriesReducer *string `json:"crossSeriesReducer,omitempty"`
 
-	/* The set of fields to preserve when `cross_series_reducer` is
-	specified. The `group_by_fields` determine how the time series are
-	partitioned into subsets prior to applying the aggregation
-	operation. Each subset contains time series that have the same
-	value for each of the grouping fields. Each individual time
-	series is a member of exactly one subset. The
-	`cross_series_reducer` is applied to each subset of time series.
-	It is not possible to reduce across different resource types, so
-	this field implicitly contains `resource.type`.  Fields not
-	specified in `group_by_fields` are aggregated away.  If
-	`group_by_fields` is not specified and all the time series have
-	the same resource type, then the time series are aggregated into
-	a single output time series. If `cross_series_reducer` is not
-	defined, this field is ignored. */
+	/* The set of fields to preserve when `cross_series_reducer` is specified. The `group_by_fields` determine how the time series are partitioned into subsets prior to applying the aggregation operation. Each subset contains time series that have the same value for each of the grouping fields. Each individual time series is a member of exactly one subset. The `cross_series_reducer` is applied to each subset of time series. It is not possible to reduce across different resource types, so this field implicitly contains `resource.type`.  Fields not specified in `group_by_fields` are aggregated away.  If `group_by_fields` is not specified and all the time series have the same resource type, then the time series are aggregated into a single output time series. If `cross_series_reducer` is not defined, this field is ignored. */
 	// +optional
 	GroupByFields []string `json:"groupByFields,omitempty"`
 
@@ -127,10 +114,7 @@ type DashboardColumnLayout struct {
 }
 
 type DashboardColumns struct {
-	/* The relative weight of this column. The column weight is used to adjust
-	the width of columns on the screen (relative to peers).
-	Greater the weight, greater the width of the column on the screen.
-	If omitted, a value of 1 is used while rendering. */
+	/* The relative weight of this column. The column weight is used to adjust the width of columns on the screen (relative to peers). Greater the weight, greater the width of the column on the screen. If omitted, a value of 1 is used while rendering. */
 	// +optional
 	Weight *int64 `json:"weight,omitempty"`
 
@@ -140,17 +124,11 @@ type DashboardColumns struct {
 }
 
 type DashboardDataSets struct {
-	/* A template string for naming `TimeSeries` in the resulting data set.
-	This should be a string with interpolations of the form `${label_name}`,
-	which will resolve to the label's value. */
+	/* A template string for naming `TimeSeries` in the resulting data set. This should be a string with interpolations of the form `${label_name}`, which will resolve to the label's value. */
 	// +optional
 	LegendTemplate *string `json:"legendTemplate,omitempty"`
 
-	/* Optional. The lower bound on data point frequency for this data set,
-	implemented by specifying the minimum alignment period to use in a time
-	series query For example, if the data is published once every 10 minutes,
-	the `min_alignment_period` should be at least 10 minutes. It would not
-	make sense to fetch and align data at one minute intervals. */
+	/* Optional. The lower bound on data point frequency for this data set, implemented by specifying the minimum alignment period to use in a time series query For example, if the data is published once every 10 minutes, the `min_alignment_period` should be at least 10 minutes. It would not make sense to fetch and align data at one minute intervals. */
 	// +optional
 	MinAlignmentPeriod *string `json:"minAlignmentPeriod,omitempty"`
 
@@ -158,39 +136,31 @@ type DashboardDataSets struct {
 	// +optional
 	PlotType *string `json:"plotType,omitempty"`
 
-	/* Required. Fields for querying time series data from the
-	Stackdriver metrics API. */
+	/* Required. Fields for querying time series data from the Stackdriver metrics API. */
 	TimeSeriesQuery DashboardTimeSeriesQuery `json:"timeSeriesQuery"`
 }
 
 type DashboardDenominator struct {
-	/* By default, the raw time series data is returned.
-	Use this field to combine multiple time series for different views of the
-	data. */
+	/* By default, the raw time series data is returned. Use this field to combine multiple time series for different views of the data. */
 	// +optional
 	Aggregation *DashboardAggregation `json:"aggregation,omitempty"`
 
-	/* Required. The [monitoring
-	filter](https://cloud.google.com/monitoring/api/v3/filters) that
-	identifies the metric types, resources, and projects to query. */
+	/* Required. The [monitoring filter](https://cloud.google.com/monitoring/api/v3/filters) that identifies the metric types, resources, and projects to query. */
 	Filter string `json:"filter"`
 }
 
 type DashboardGaugeView struct {
-	/* The lower bound for this gauge chart. The value of the chart should
-	always be greater than or equal to this. */
+	/* The lower bound for this gauge chart. The value of the chart should always be greater than or equal to this. */
 	// +optional
 	LowerBound *float64 `json:"lowerBound,omitempty"`
 
-	/* The upper bound for this gauge chart. The value of the chart should
-	always be less than or equal to this. */
+	/* The upper bound for this gauge chart. The value of the chart should always be less than or equal to this. */
 	// +optional
 	UpperBound *float64 `json:"upperBound,omitempty"`
 }
 
 type DashboardGridLayout struct {
-	/* The number of columns into which the view's width is divided. If omitted
-	or set to zero, a system default will be used while rendering. */
+	/* The number of columns into which the view's width is divided. If omitted or set to zero, a system default will be used while rendering. */
 	// +optional
 	Columns *DashboardColumns `json:"columns,omitempty"`
 
@@ -200,22 +170,17 @@ type DashboardGridLayout struct {
 }
 
 type DashboardLogsPanel struct {
-	/* A filter that chooses which log entries to return.  See [Advanced Logs
-	Queries](https://cloud.google.com/logging/docs/view/advanced-queries).
-	Only log entries that match the filter are returned.  An empty filter
-	matches all log entries. */
+	/* A filter that chooses which log entries to return.  See [Advanced Logs Queries](https://cloud.google.com/logging/docs/view/advanced-queries). Only log entries that match the filter are returned.  An empty filter matches all log entries. */
 	// +optional
 	Filter *string `json:"filter,omitempty"`
 
-	/* The names of logging resources to collect logs for. Currently only projects
-	are supported. If empty, the widget will default to the host project. */
+	/* The names of logging resources to collect logs for. Currently only projects are supported. If empty, the widget will default to the host project. */
 	// +optional
 	ResourceNames []DashboardResourceNames `json:"resourceNames,omitempty"`
 }
 
 type DashboardMosaicLayout struct {
-	/* The number of columns in the mosaic grid. The number of columns must be
-	between 1 and 12, inclusive. */
+	/* The number of columns in the mosaic grid. The number of columns must be between 1 and 12, inclusive. */
 	// +optional
 	Columns *DashboardColumns `json:"columns,omitempty"`
 
@@ -225,15 +190,11 @@ type DashboardMosaicLayout struct {
 }
 
 type DashboardNumerator struct {
-	/* By default, the raw time series data is returned.
-	Use this field to combine multiple time series for different views of the
-	data. */
+	/* By default, the raw time series data is returned. Use this field to combine multiple time series for different views of the data. */
 	// +optional
 	Aggregation *DashboardAggregation `json:"aggregation,omitempty"`
 
-	/* Required. The [monitoring
-	filter](https://cloud.google.com/monitoring/api/v3/filters) that
-	identifies the metric types, resources, and projects to query. */
+	/* Required. The [monitoring filter](https://cloud.google.com/monitoring/api/v3/filters) that identifies the metric types, resources, and projects to query. */
 	Filter string `json:"filter"`
 }
 
@@ -246,9 +207,7 @@ type DashboardPickTimeSeriesFilter struct {
 	// +optional
 	NumTimeSeries *int32 `json:"numTimeSeries,omitempty"`
 
-	/* `ranking_method` is applied to each time series independently to produce
-	the value which will be used to compare the time series to other time
-	series. */
+	/* `ranking_method` is applied to each time series independently to produce the value which will be used to compare the time series to other time series. */
 	// +optional
 	RankingMethod *string `json:"rankingMethod,omitempty"`
 }
@@ -278,10 +237,7 @@ type DashboardRowLayout struct {
 }
 
 type DashboardRows struct {
-	/* The relative weight of this row. The row weight is used to adjust the
-	height of rows on the screen (relative to peers). Greater the weight,
-	greater the height of the row on the screen. If omitted, a value
-	of 1 is used while rendering. */
+	/* The relative weight of this row. The row weight is used to adjust the height of rows on the screen (relative to peers). Greater the weight, greater the height of the row on the screen. If omitted, a value of 1 is used while rendering. */
 	// +optional
 	Weight *int64 `json:"weight,omitempty"`
 
@@ -343,8 +299,7 @@ type DashboardScorecard struct {
 	// +optional
 	Thresholds []DashboardThresholds `json:"thresholds,omitempty"`
 
-	/* Required. Fields for querying time series data from the
-	Stackdriver metrics API. */
+	/* Required. Fields for querying time series data from the Stackdriver metrics API. */
 	TimeSeriesQuery DashboardTimeSeriesQuery `json:"timeSeriesQuery"`
 }
 
@@ -385,20 +340,7 @@ type DashboardSecondaryAggregation struct {
 	// +optional
 	CrossSeriesReducer *string `json:"crossSeriesReducer,omitempty"`
 
-	/* The set of fields to preserve when `cross_series_reducer` is
-	specified. The `group_by_fields` determine how the time series are
-	partitioned into subsets prior to applying the aggregation
-	operation. Each subset contains time series that have the same
-	value for each of the grouping fields. Each individual time
-	series is a member of exactly one subset. The
-	`cross_series_reducer` is applied to each subset of time series.
-	It is not possible to reduce across different resource types, so
-	this field implicitly contains `resource.type`.  Fields not
-	specified in `group_by_fields` are aggregated away.  If
-	`group_by_fields` is not specified and all the time series have
-	the same resource type, then the time series are aggregated into
-	a single output time series. If `cross_series_reducer` is not
-	defined, this field is ignored. */
+	/* The set of fields to preserve when `cross_series_reducer` is specified. The `group_by_fields` determine how the time series are partitioned into subsets prior to applying the aggregation operation. Each subset contains time series that have the same value for each of the grouping fields. Each individual time series is a member of exactly one subset. The `cross_series_reducer` is applied to each subset of time series. It is not possible to reduce across different resource types, so this field implicitly contains `resource.type`.  Fields not specified in `group_by_fields` are aggregated away.  If `group_by_fields` is not specified and all the time series have the same resource type, then the time series are aggregated into a single output time series. If `cross_series_reducer` is not defined, this field is ignored. */
 	// +optional
 	GroupByFields []string `json:"groupByFields,omitempty"`
 
@@ -425,11 +367,7 @@ type DashboardSecondaryAggregation struct {
 }
 
 type DashboardSparkChartView struct {
-	/* The lower bound on data point frequency in the chart implemented by
-	specifying the minimum alignment period to use in a time series query.
-	For example, if the data is published once every 10 minutes it would not
-	make sense to fetch and align data at one minute intervals. This field is
-	optional and exists only as a hint. */
+	/* The lower bound on data point frequency in the chart implemented by specifying the minimum alignment period to use in a time series query. For example, if the data is published once every 10 minutes it would not make sense to fetch and align data at one minute intervals. This field is optional and exists only as a hint. */
 	// +optional
 	MinAlignmentPeriod *string `json:"minAlignmentPeriod,omitempty"`
 
@@ -452,8 +390,7 @@ type DashboardThresholds struct {
 	// +optional
 	Color *string `json:"color,omitempty"`
 
-	/* The direction for the current threshold. Direction is not allowed in a
-	XyChart. */
+	/* The direction for the current threshold. Direction is not allowed in a XyChart. */
 	// +optional
 	Direction *string `json:"direction,omitempty"`
 
@@ -461,15 +398,13 @@ type DashboardThresholds struct {
 	// +optional
 	Label *string `json:"label,omitempty"`
 
-	/* The value of the threshold. The value should be defined in the native scale
-	of the metric. */
+	/* The value of the threshold. The value should be defined in the native scale of the metric. */
 	// +optional
 	Value *float64 `json:"value,omitempty"`
 }
 
 type DashboardTiles struct {
-	/* The height of the tile, measured in grid blocks. Tiles must have a
-	minimum height of 1. */
+	/* The height of the tile, measured in grid blocks. Tiles must have a minimum height of 1. */
 	// +optional
 	Height *int32 `json:"height,omitempty"`
 
@@ -477,33 +412,25 @@ type DashboardTiles struct {
 	// +optional
 	Widget *DashboardWidget `json:"widget,omitempty"`
 
-	/* The width of the tile, measured in grid blocks. Tiles must have a
-	minimum width of 1. */
+	/* The width of the tile, measured in grid blocks. Tiles must have a minimum width of 1. */
 	// +optional
 	Width *int32 `json:"width,omitempty"`
 
-	/* The zero-indexed position of the tile in grid blocks relative to the
-	left edge of the grid. Tiles must be contained within the specified
-	number of columns. `x_pos` cannot be negative. */
+	/* The zero-indexed position of the tile in grid blocks relative to the left edge of the grid. Tiles must be contained within the specified number of columns. `x_pos` cannot be negative. */
 	// +optional
 	XPos *int32 `json:"xPos,omitempty"`
 
-	/* The zero-indexed position of the tile in grid blocks relative to the
-	top edge of the grid. `y_pos` cannot be negative. */
+	/* The zero-indexed position of the tile in grid blocks relative to the top edge of the grid. `y_pos` cannot be negative. */
 	// +optional
 	YPos *int32 `json:"yPos,omitempty"`
 }
 
 type DashboardTimeSeriesFilter struct {
-	/* By default, the raw time series data is returned.
-	Use this field to combine multiple time series for different views of the
-	data. */
+	/* By default, the raw time series data is returned. Use this field to combine multiple time series for different views of the data. */
 	// +optional
 	Aggregation *DashboardAggregation `json:"aggregation,omitempty"`
 
-	/* Required. The [monitoring
-	filter](https://cloud.google.com/monitoring/api/v3/filters) that identifies
-	the metric types, resources, and projects to query. */
+	/* Required. The [monitoring filter](https://cloud.google.com/monitoring/api/v3/filters) that identifies the metric types, resources, and projects to query. */
 	Filter string `json:"filter"`
 
 	/* Ranking based time series filter. */
@@ -546,11 +473,7 @@ type DashboardTimeSeriesQuery struct {
 	// +optional
 	TimeSeriesQueryLanguage *string `json:"timeSeriesQueryLanguage,omitempty"`
 
-	/* The unit of data contained in fetched time series. If non-empty, this
-	unit will override any unit that accompanies fetched data. The format is
-	the same as the
-	[`unit`](https://cloud.google.com/monitoring/api/ref_v3/rest/v3/projects.metricDescriptors)
-	field in `MetricDescriptor`. */
+	/* The unit of data contained in fetched time series. If non-empty, this unit will override any unit that accompanies fetched data. The format is the same as the [`unit`](https://cloud.google.com/monitoring/api/ref_v3/rest/v3/projects.metricDescriptors) field in `MetricDescriptor`. */
 	// +optional
 	UnitOverride *string `json:"unitOverride,omitempty"`
 }
@@ -629,11 +552,7 @@ type DashboardXyChart struct {
 	// +optional
 	Thresholds []DashboardThresholds `json:"thresholds,omitempty"`
 
-	/* The duration used to display a comparison chart. A comparison chart
-	simultaneously shows values from two similar-length time periods
-	(e.g., week-over-week metrics).
-	The duration must be positive, and it can only be applied to charts with
-	data sets of LINE plot type. */
+	/* The duration used to display a comparison chart. A comparison chart simultaneously shows values from two similar-length time periods (e.g., week-over-week metrics). The duration must be positive, and it can only be applied to charts with data sets of LINE plot type. */
 	// +optional
 	TimeshiftDuration *string `json:"timeshiftDuration,omitempty"`
 
@@ -657,21 +576,18 @@ type DashboardYAxis struct {
 }
 
 type MonitoringDashboardSpec struct {
-	/* The content is divided into equally spaced columns and the widgets are
-	arranged vertically. */
+	/* The content is divided into equally spaced columns and the widgets are arranged vertically. */
 	// +optional
 	ColumnLayout *DashboardColumnLayout `json:"columnLayout,omitempty"`
 
 	/* Required. The mutable, human-readable name. */
 	DisplayName string `json:"displayName"`
 
-	/* Content is arranged with a basic layout that re-flows a simple list of
-	informational elements like widgets or tiles. */
+	/* Content is arranged with a basic layout that re-flows a simple list of informational elements like widgets or tiles. */
 	// +optional
 	GridLayout *DashboardGridLayout `json:"gridLayout,omitempty"`
 
-	/* The content is arranged as a grid of tiles, with each content widget
-	occupying one or more grid blocks. */
+	/* The content is arranged as a grid of tiles, with each content widget occupying one or more grid blocks. */
 	// +optional
 	MosaicLayout *DashboardMosaicLayout `json:"mosaicLayout,omitempty"`
 
@@ -682,8 +598,7 @@ type MonitoringDashboardSpec struct {
 	// +optional
 	ResourceID *string `json:"resourceID,omitempty"`
 
-	/* The content is divided into equally spaced rows and the widgets are
-	arranged horizontally. */
+	/* The content is divided into equally spaced rows and the widgets are arranged horizontally. */
 	// +optional
 	RowLayout *DashboardRowLayout `json:"rowLayout,omitempty"`
 }
