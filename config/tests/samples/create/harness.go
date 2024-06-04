3c72078dcd7ec4c999e5c1186b18c8287059fd23
Alex Pana <8968914+acpana@users.noreply.github.com>
2024-06-04 23:14:50 +0000

    tests: turn on mock for idsendpoint
    Signed-off-by: Alex Pana <8968914+acpana@users.noreply.github.com>

--- config/tests/samples/create/harness.go
+++ config/tests/samples/create/harness.go
@@ -594,6 +594,7 @@ func MaybeSkip(t *testing.T, name string, resources []*unstructured.Unstructured
 			case schema.GroupKind{Group: "gkehub.cnrm.cloud.google.com", Kind: "GKEHubFeature"}:
 
 			case schema.GroupKind{Group: "cloudfunctions.cnrm.cloud.google.com", Kind: "CloudFunctionsFunction"}:
+			case schema.GroupKind{Group: "cloudids.cnrm.cloud.google.com", Kind: "CloudIDSEndpoint"}:
 
 			case schema.GroupKind{Group: "containerattached.cnrm.cloud.google.com", Kind: "ContainerAttachedCluster"}:
 
