3f449a21d58eac40e8d152c4d9ee1463ab8f8378
Jingyi Hu <jingyih@google.com>
2024-06-12 22:44:17 +0000

    code refactor

--- operator/pkg/controllers/configconnectorcontext/configconnectorcontext_controller.go
+++ operator/pkg/controllers/configconnectorcontext/configconnectorcontext_controller.go
@@ -404,36 +404,45 @@ func (r *Reconciler) verifyCNRMSystemNamespaceIsActive(ctx context.Context) erro
 // applyNamespacedCustomizations fetches and applies all namespace-scoped customization CRDs.
 func (r *Reconciler) applyNamespacedCustomizations() declarative.ObjectTransform {
 	return func(ctx context.Context, o declarative.DeclarativeObject, m *manifest.Objects) error {
-		ccc, ok := o.(*corev1beta1.ConfigConnectorContext)
-		if !ok {
-			return fmt.Errorf("expected the resource to be a ConfigConnectorContext, but it was not. Object: %v", o)
-		}
-		// List all the customization CRs in the same namespace as ConfigConnectorContext object.
-		crs, err := controllers.ListNamespacedControllerResources(ctx, r.client, ccc.Namespace)
-		if err != nil {
-			return err
-		}
-		// Apply all the customization CRs in the same namespace as ConfigConnectorContext object.
-		for _, cr := range crs {
-			if cr.Namespace != ccc.Namespace {
-				// this shouldn't happen!
-				r.log.Error(fmt.Errorf("unexpected namespace for NamespacedControllerResource object"), "expected namespace", ccc.Namespace, "got namespace", cr.Namespace)
-			}
-			r.log.Info("applying namespace-scoped controller resource customization", "Namespace", cr.Namespace, "Name", cr.Name)
-			if err := r.applyNamespacedControllerResourceCustomization(ctx, &cr, m); err != nil {
-				return err
-			}
+		if err := r.fetchAndApplyAllNamespacedControllerResources(ctx, o, m); err != nil {
+			r.log.Error(err, "error applying all namespaced controller resource customization CRs")
+			// Don't fail entire reconciliation if we cannot apply customization CRs.
+			// return err
 		}
 		return nil
 	}
 }
 
-// applyNamespacedControllerResourceCustomization applies customizations specified in NamespacedControllerResource CR.
-func (r *Reconciler) applyNamespacedControllerResourceCustomization(ctx context.Context, cr *customizev1beta1.NamespacedControllerResource, m *manifest.Objects) error {
+func (r *Reconciler) fetchAndApplyAllNamespacedControllerResources(ctx context.Context, o declarative.DeclarativeObject, m *manifest.Objects) error {
+	ccc, ok := o.(*corev1beta1.ConfigConnectorContext)
+	if !ok {
+		return fmt.Errorf("expected the resource to be a ConfigConnectorContext, but it was not. Object: %v", o)
+	}
+	// List all the NamespacedControllerResource CRs in the same namespace as ConfigConnectorContext object.
+	crs, err := controllers.ListNamespacedControllerResources(ctx, r.client, ccc.Namespace)
+	if err != nil {
+		return err
+	}
+	// Apply all the NamespacedControllerResource CRs in the same namespace as ConfigConnectorContext object.
+	for _, cr := range crs {
+		if cr.Namespace != ccc.Namespace {
+			// this shouldn't happen!
+			r.log.Error(fmt.Errorf("unexpected namespace for NamespacedControllerResource object"), "expected namespace", ccc.Namespace, "got namespace", cr.Namespace)
+		}
+		r.log.Info("applying namespace-scoped controller resource customization", "Namespace", cr.Namespace, "Name", cr.Name)
+		if err := r.applyNamespacedControllerResource(ctx, &cr, m); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// applyNamespacedControllerResource applies customizations specified in NamespacedControllerResource CR.
+func (r *Reconciler) applyNamespacedControllerResource(ctx context.Context, cr *customizev1beta1.NamespacedControllerResource, m *manifest.Objects) error {
 	if cr.Name != "cnrm-controller-manager" {
 		msg := fmt.Sprintf("resource customization for controller %s is not supported", cr.Name)
 		r.log.Info(msg)
-		return r.handleApplyCustomizationFailed(ctx, cr.Namespace, cr.Name, msg)
+		return r.handleApplyNamespacedControllerResourceFailed(ctx, cr.Namespace, cr.Name, msg)
 	}
 	controllerGVK := schema.GroupVersionKind{
 		Group:   appsv1.SchemeGroupVersion.Group,
@@ -442,12 +451,12 @@ func (r *Reconciler) applyNamespacedControllerResourceCustomization(ctx context.
 	}
 	if err := controllers.ApplyContainerResourceCustomization(true, m, cr.Name, controllerGVK, cr.Spec.Containers, nil); err != nil {
 		r.log.Error(err, "failed to apply customization", "Namespace", cr.Namespace, "Name", cr.Name)
-		return r.handleApplyCustomizationFailed(ctx, cr.Namespace, cr.Name, fmt.Sprintf("failed to apply customization %s: %v", cr.Name, err))
+		return r.handleApplyNamespacedControllerResourceFailed(ctx, cr.Namespace, cr.Name, fmt.Sprintf("failed to apply customization %s: %v", cr.Name, err))
 	}
-	return r.handleApplyCustomizationSucceeded(ctx, cr.Namespace, cr.Name)
+	return r.handleApplyNamespacedControllerResourceSucceeded(ctx, cr.Namespace, cr.Name)
 }
 
-func (r *Reconciler) handleApplyCustomizationFailed(ctx context.Context, namespace, name string, msg string) error {
+func (r *Reconciler) handleApplyNamespacedControllerResourceFailed(ctx context.Context, namespace, name string, msg string) error {
 	cr, err := controllers.GetNamespacedControllerResource(ctx, r.client, namespace, name)
 	if err != nil {
 		if apierrors.IsNotFound(err) {
@@ -466,7 +475,7 @@ func (r *Reconciler) handleApplyCustomizationFailed(ctx context.Context, namespa
 	return r.updateNamespacedControllerResourceStatus(ctx, cr)
 }
 
-func (r *Reconciler) handleApplyCustomizationSucceeded(ctx context.Context, namespace, name string) error {
+func (r *Reconciler) handleApplyNamespacedControllerResourceSucceeded(ctx context.Context, namespace, name string) error {
 	cr, err := controllers.GetNamespacedControllerResource(ctx, r.client, namespace, name)
 	if err != nil {
 		if apierrors.IsNotFound(err) {
