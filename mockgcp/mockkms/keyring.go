1ae4b4c839751d35d5ede7fe22825e0635ad7330
justinsb <justinsb@google.com>
2024-06-29 14:07:02 -0400

    mockgcp: support for KMSCryptoKey
    Implement CryptoKey and CryptoKeyVersion proto resources.

--- mockgcp/mockkms/keyring.go
+++ mockgcp/mockkms/keyring.go
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +mockgcp-support
+// apiVersion: kms.cnrm.cloud.google.com/v1beta1
+// kind: KMSKeyRing
+// service: google.cloud.kms.v1.KeyManagementService
+// resource: KeyRing
+
 package mockkms
 
 import (
