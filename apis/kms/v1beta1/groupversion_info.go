b64151123956d5a550fc19007dd9c3a52926eff3
justinsb <justinsb@google.com>
2024-06-29 17:38:01 -0400

    chore: generate CRD for KMSCryptoKey

--- apis/kms/v1beta1/groupversion_info.go
+++ apis/kms/v1beta1/groupversion_info.go
@@ -12,5 +12,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +kcc:proto=google.cloud.kms.v1
+// +kubebuilder:object:generate=true
+// +groupName=kms.cnrm.cloud.google.com
 package v1beta1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/scheme"
+)
+
+var (
+	// GroupVersion is group version used to register these objects
+	GroupVersion = schema.GroupVersion{Group: "kms.cnrm.cloud.google.com", Version: "v1beta1"}
+
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+
+	// AddToScheme adds the types in this group-version to the given scheme.
+	AddToScheme = SchemeBuilder.AddToScheme
+)
