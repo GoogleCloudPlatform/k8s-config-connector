2cf1758177aa7ab7804d63f0136e6e8f2ddc1905
justinsb <justinsb@google.com>
2024-08-12 13:14:38 -0400

    chore: add test to RepresentativeCRUDTestsForAllServices

--- pkg/test/constants/presubmitconstants.go
+++ pkg/test/constants/presubmitconstants.go
@@ -77,6 +77,7 @@ var (
 		"resourcemanager":      {"resourcemanagerlien"},
 		"run":                  {"runservicebasic"},
 		"secretmanager":        {"secretmanagersecretversion"},
+		"securesourcemanager":  {"securesourcemanagerinstancebasic"},
 		"servicedirectory":     {"servicedirectorynamespace"},
 		"servicenetworking":    {"servicenetworkingconnection"},
 		"serviceusage":         {"service"},
