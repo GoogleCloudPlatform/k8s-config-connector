946efca1de8d2cdecd2e53489d74b998914cf973
Jingyi Hu <jingyih@google.com>
2024-08-30 00:02:50 +0000

    feat: manually edit types for BigQueryDataTransferConfig

--- apis/bigquerydatatransfer/v1alpha1/bigquerydatatransferconfig_types.go
+++ apis/bigquerydatatransfer/v1alpha1/bigquerydatatransferconfig_types.go
@@ -15,21 +15,98 @@
 package v1alpha1
 
 import (
+	refv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/apis/k8s/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var BigQueryDataTransferConfigGVK = GroupVersion.WithKind("BigQueryDataTransferConfig")
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // BigQueryDataTransferConfigSpec defines the desired state of BigQueryDataTransferConfig
 // +kcc:proto=google.cloud.bigquery.datatransfer.v1.TransferConfig
 type BigQueryDataTransferConfigSpec struct {
+	// The number of days to look back to automatically refresh the data.
+	//  For example, if `data_refresh_window_days = 10`, then every day
+	//  BigQuery reingests data for [today-10, today-1], rather than ingesting data
+	//  for just [today-1].
+	//  Only valid if the data source supports the feature. Set the value to 0
+	//  to use the default value.
+	DataRefreshWindowDays *int32 `json:"dataRefreshWindowDays,omitempty"`
+
+	// Data source ID. This cannot be changed once data transfer is created. The
+	//  full list of available data source IDs can be returned through an API call:
+	//  https://cloud.google.com/bigquery-transfer/docs/reference/datatransfer/rest/v1/projects.locations.dataSources/list
+	DataSourceID *string `json:"dataSourceID,omitempty"`
+
+	// The BigQuery target dataset id.
+	DestinationDatasetID *string `json:"destinationDatasetID,omitempty"`
+
+	// Is this config disabled. When set to true, no runs will be scheduled for
+	//  this transfer config.
+	Disabled *bool `json:"disabled,omitempty"`
+
+	// User specified display name for the data transfer.
+	DisplayName *string `json:"displayName,omitempty"`
+
+	// Email notifications will be sent according to these preferences
+	//  to the email address of the user who owns this transfer config.
+	EmailPreferences *EmailPreferences `json:"emailPreferences,omitempty"`
+
+	// The encryption configuration part. Currently, it is only used for the
+	//  optional KMS key name. The BigQuery service account of your project must be
+	//  granted permissions to use the key. Read methods will return the key name
+	//  applied in effect. Write methods will apply the key if it is present, or
+	//  otherwise try to apply project default keys if it is absent.
+	EncryptionConfiguration *EncryptionConfiguration `json:"encryptionConfiguration,omitempty"`
+
+	// Pub/Sub topic where notifications will be sent after transfer runs
+	//  associated with this transfer config finish.
+	//
+	//  The format for specifying a pubsub topic is:
+	//  `projects/{project_id}/topics/{topic_id}`
+	NotificationPubsubTopic *string `json:"notificationPubsubTopic,omitempty"`
+
+	// Parameters specific to each data source. For more information see the
+	//  bq tab in the 'Setting up a data transfer' section for each data source.
+	//  For example the parameters for Cloud Storage transfers are listed here:
+	//  https://cloud.google.com/bigquery-transfer/docs/cloud-storage-transfer#bq
+	Params map[string]string `json:"params,omitempty"`
+
+	// +required
+	Parent `json:",inline"`
+
 	// The BigQueryDataTransferConfig name. If not given, the metadata.name will be used.
 	// + optional
 	ResourceID *string `json:"resourceID,omitempty"`
+
+	// Data transfer schedule.
+	//  If the data source does not support a custom schedule, this should be
+	//  empty. If it is empty, the default value for the data source will be used.
+	//  The specified times are in UTC.
+	//  Examples of valid format:
+	//  `1st,3rd monday of month 15:30`,
+	//  `every wed,fri of jan,jun 13:15`, and
+	//  `first sunday of quarter 00:00`.
+	//  See more explanation about the format here:
+	//  https://cloud.google.com/appengine/docs/flexible/python/scheduling-jobs-with-cron-yaml#the_schedule_format
+	//
+	//  NOTE: The minimum interval time between recurring transfers depends on the
+	//  data source; refer to the documentation for your data source.
+	Schedule *string `json:"schedule,omitempty"`
+
+	// Options customizing the data transfer schedule.
+	ScheduleOptions *ScheduleOptions `json:"scheduleOptions,omitempty"`
+
+	// Deprecated. Unique ID of the user on whose behalf transfer is done.
+	UserID *int64 `json:"userID,omitempty"`
+}
+
+type Parent struct {
+	// +required
+	ProjectRef *refv1beta1.ProjectRef `json:"projectRef"`
+
+	// +required
+	Location string `json:"location"`
 }
 
 // BigQueryDataTransferConfigStatus defines the config connector machine state of BigQueryDataTransferConfig
@@ -54,6 +131,31 @@ type BigQueryDataTransferConfigStatus struct {
 // BigQueryDataTransferConfigSpec defines the desired state of BigQueryDataTransferConfig
 // +kcc:proto=google.cloud.bigquery.datatransfer.v1.TransferConfig
 type BigQueryDataTransferConfigObservedState struct {
+	// Output only. Region in which BigQuery dataset is located.
+	DatasetRegion *string `json:"datasetRegion,omitempty"`
+
+	// Identifier. The resource name of the transfer config.
+	//  Transfer config names have the form either
+	//  `projects/{project_id}/locations/{region}/transferConfigs/{config_id}` or
+	//  `projects/{project_id}/transferConfigs/{config_id}`,
+	//  where `config_id` is usually a UUID, even though it is not
+	//  guaranteed or required. The name is ignored when creating a transfer
+	//  config.
+	Name *string `json:"name,omitempty"`
+
+	// Output only. Next time when data transfer will run.
+	NextRunTime *string `json:"nextRunTime,omitempty"`
+
+	// Output only. Information about the user whose credentials are used to
+	//  transfer data. Populated only for `transferConfigs.get` requests. In case
+	//  the user information is not available, this field will not be populated.
+	OwnerInfo *UserInfo `json:"ownerInfo,omitempty"`
+
+	// Output only. State of the most recently updated transfer run.
+	State *string `json:"state,omitempty"`
+
+	// Output only. Data transfer modification time. Ignored by server on input.
+	UpdateTime *string `json:"updateTime,omitempty"`
 }
 
 // +genclient
