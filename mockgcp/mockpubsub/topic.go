eb740bbf7ae805dd51299b79f713f966f468fe4b
Yuwen Ma <yuwenma@google.com>
2024-09-06 17:02:01 +0000

    fix: improve MockPusub fidelity on AllowedPersistenceRegions

--- mockgcp/mockpubsub/topic.go
+++ mockgcp/mockpubsub/topic.go
@@ -53,35 +53,32 @@ func (s *publisherService) CreateTopic(ctx context.Context, req *pb.Topic) (*pb.
 }
 
 func (s *publisherService) populateDefaultsForTopic(obj *pb.Topic) {
-	// TODO: When _is_ this populated?
-	populateMessageStoragePolicy := false
-	if populateMessageStoragePolicy {
-		obj.MessageStoragePolicy = &pb.MessageStoragePolicy{
-			AllowedPersistenceRegions: []string{
-				"asia-east1",
-				"asia-northeast1",
-				"asia-southeast1",
-				"australia-southeast1",
-				"europe-north1",
-				"europe-west1",
-				"europe-west2",
-				"europe-west3",
-				"europe-west4",
-				"southamerica-west1",
-				"us-central1",
-				"us-central2",
-				"us-east1",
-				"us-east4",
-				"us-east5",
-				"us-east7",
-				"us-south1",
-				"us-west1",
-				"us-west2",
-				"us-west3",
-				"us-west4",
-				"us-west8",
-			},
-		}
+	// create and update methods populate the AllowedPersistenceRegions
+	obj.MessageStoragePolicy = &pb.MessageStoragePolicy{
+		AllowedPersistenceRegions: []string{
+			"asia-east1",
+			"asia-northeast1",
+			"asia-southeast1",
+			"australia-southeast1",
+			"europe-north1",
+			"europe-west1",
+			"europe-west2",
+			"europe-west3",
+			"europe-west4",
+			"southamerica-west1",
+			"us-central1",
+			"us-central2",
+			"us-east1",
+			"us-east4",
+			"us-east5",
+			"us-east7",
+			"us-south1",
+			"us-west1",
+			"us-west2",
+			"us-west3",
+			"us-west4",
+			"us-west8",
+		},
 	}
 }
 
@@ -116,6 +113,7 @@ func (s *publisherService) UpdateTopic(ctx context.Context, req *pb.UpdateTopicR
 			return nil, status.Errorf(codes.InvalidArgument, "update_mask path %q not valid", path)
 		}
 	}
+	s.populateDefaultsForTopic(updated)
 
 	if err := s.storage.Update(ctx, fqn, updated); err != nil {
 		return nil, err
