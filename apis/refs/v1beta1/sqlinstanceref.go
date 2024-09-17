fd9aa51d1aa83988623568bf13da9edc313e8343
Yuwen Ma <yuwenma@google.com>
2024-09-17 14:03:30 +0000

    example: use SQLInstance Reference

--- apis/refs/v1beta1/sqlinstanceref.go
+++ apis/refs/v1beta1/sqlinstanceref.go
@@ -14,6 +14,18 @@
 
 package v1beta1
 
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
 type SQLInstanceRef struct {
 	/* The SQLInstance selfLink, when not managed by Config Connector. */
 	External string `json:"external,omitempty"`
@@ -22,3 +34,83 @@ type SQLInstanceRef struct {
 	/* The `namespace` field of a `SQLInstance` resource. */
 	Namespace string `json:"namespace,omitempty"`
 }
+
+type SQLInstance struct {
+	ProjectID       string
+	SQLInstanceName string
+}
+
+func (s *SQLInstance) String() string {
+	return "projects/" + s.ProjectID + "/instances/" + s.SQLInstanceName
+}
+
+func (r *SQLInstanceRef) ResolveRef(ctx context.Context, reader client.Reader, src client.Object) (*SQLInstance, error) {
+	if r == nil {
+		return nil, nil
+	}
+
+	if r.External != "" && r.Name != "" {
+		return nil, fmt.Errorf("cannot specify both name and external on project reference")
+	}
+
+	tokens := strings.Split(r.External, "/")
+	if len(tokens) != 4 && tokens[0] != "projects" && tokens[2] != "instances" {
+		return nil, fmt.Errorf("format of sqlinstance external=%q was not known (use projects/<projectId>/instances/<instanceName>)", r.External)
+	}
+
+	key := types.NamespacedName{
+		Namespace: r.Namespace,
+		Name:      r.Name,
+	}
+	if key.Namespace == "" {
+		key.Namespace = src.GetNamespace()
+	}
+
+	sqlinstance := &unstructured.Unstructured{}
+	sqlinstance.SetGroupVersionKind(schema.GroupVersionKind{
+		Group:   "sqlinstance.cnrm.cloud.google.com",
+		Version: "v1beta1",
+		Kind:    "SQLInstance",
+	})
+	if err := reader.Get(ctx, key, sqlinstance); err != nil {
+		if apierrors.IsNotFound(err) {
+			return nil, fmt.Errorf("referenced SQLInstance %v not found", key)
+		}
+		return nil, fmt.Errorf("error reading referenced SQLInstance %v: %w", key, err)
+	}
+
+	resourceID, _, err := unstructured.NestedString(sqlinstance.Object, "spec", "resourceID")
+	if err != nil {
+		return nil, fmt.Errorf("reading spec.resourceID from SQLInstance %s/%s: %w", sqlinstance.GetNamespace(), sqlinstance.GetName(), err)
+	}
+	if resourceID == "" {
+		resourceID = sqlinstance.GetName()
+	}
+
+	projectID, err := ResolveProjectID(ctx, reader, sqlinstance)
+	if err != nil {
+		return nil, err
+	}
+	return &SQLInstance{
+		ProjectID:       projectID,
+		SQLInstanceName: resourceID,
+	}, nil
+}
+
+// Example
+/*
+
+func CloudSqlPropertiesSpec_ToProto(mapCtx *direct.MapContext, in *krm.CloudSqlPropertiesSpec) *pb.CloudSqlProperties {
+    if in == nil {
+        return nil
+    }
+    out := &pb.CloudSqlProperties{}
+    // out.InstanceId = direct.ValueOf(in.InstanceID)
+    out.Database = direct.ValueOf(in.Database)
+    out.Type = direct.Enum_ToProto[pb.CloudSqlProperties_DatabaseType](mapCtx, in.Type)
+    out.Credential = CloudSqlCredential_ToProto(mapCtx, in.Credential)
+    if in.InstanceRef != nil {
+		sqlinstance, err := in.InstanceRef.ResolveRef(ctx, reader, obj)
+        out.InstanceId = sqlinstance.String()
+    }
+*/
