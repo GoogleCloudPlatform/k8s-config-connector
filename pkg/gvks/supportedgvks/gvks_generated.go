648f421b9c7a818ac58c3388e521706918bb26a2
Nikhil Bhoyar <nikhilbhoyar@google.com>
2024-10-14 08:38:22 +0000

    Make ready-pr

--- pkg/gvks/supportedgvks/gvks_generated.go
+++ pkg/gvks/supportedgvks/gvks_generated.go
@@ -3050,6 +3050,16 @@ var SupportedGVKs = map[schema.GroupVersionKind]GVKMetadata{
 	},
 	{
 		Group:   "kms.cnrm.cloud.google.com",
+		Version: "v1alpha1",
+		Kind:    "KMSAutokeyConfig",
+	}: {
+		Labels: map[string]string{
+			"cnrm.cloud.google.com/managed-by-kcc": "true",
+			"cnrm.cloud.google.com/system":         "true",
+		},
+	},
+	{
+		Group:   "kms.cnrm.cloud.google.com",
 		Version: "v1beta1",
 		Kind:    "KMSCryptoKey",
 	}: {
