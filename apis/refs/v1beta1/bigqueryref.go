b17ebe26ca38c494404da81e131bc05ac310f8da
Alex Pana <8968914+acpana@users.noreply.github.com>
2024-10-23 20:53:12 +0000

    api:refs: table ref
    Signed-off-by: Alex Pana <8968914+acpana@users.noreply.github.com>

--- apis/refs/v1beta1/bigqueryref.go
+++ apis/refs/v1beta1/bigqueryref.go
@@ -107,3 +107,121 @@ func (d *BigQueryDataset) String() string {
 func (d *BigQueryDataset) GetDatasetID() string {
 	return d.datasetID
 }
+
+type BigQueryTableRef struct {
+	// If provided must be in the format `projects/{projectId}/datasets/{datasetId}/tables/{tableId}`.
+	External string `json:"external,omitempty"`
+	// The `metadata.name` field of a `BigQueryTable` resource.
+	Name string `json:"name,omitempty"`
+	// The `metadata.namespace` field of a `BigQueryTable` resource.
+	Namespace string `json:"namespace,omitempty"`
+}
+
+type BigQueryTable struct {
+	projectID string
+	datasetID string
+	tableID   string
+}
+
+func ResolveBigQueryTable(ctx context.Context, reader client.Reader, src client.Object, ref *BigQueryTableRef) (*BigQueryTable, error) {
+	if ref == nil {
+		return nil, nil
+	}
+
+	if ref.Name == "" && ref.External == "" {
+		return nil, fmt.Errorf("must specify either name or external on BigQueryTableRef")
+	}
+	if ref.Name != "" && ref.External != "" {
+		return nil, fmt.Errorf("cannot specify both name and external on BigQueryTableRef")
+	}
+
+	// External is provided.
+	if ref.External != "" {
+		// External should be in the `projects/{projectId}/datasets/{datasetId}/tables/{tableId}` format.
+		tokens := strings.Split(ref.External, "/")
+		if len(tokens) == 4 && tokens[0] == "projects" && tokens[2] == "datasets" && tokens[4] == "tables" {
+			return &BigQueryTable{
+				projectID: tokens[1],
+				datasetID: tokens[3],
+				tableID:   tokens[5],
+			}, nil
+		}
+		return nil, fmt.Errorf("format of BigQueryTableRef external=%q was not known (use projects/{projectId}/datasets/{datasetId}/tables/{tableId})", ref.External)
+
+	}
+
+	// Fetch BigQueryDataset object to construct the external form.
+	table := &unstructured.Unstructured{}
+	table.SetGroupVersionKind(schema.GroupVersionKind{
+		Group:   "bigquery.cnrm.cloud.google.com",
+		Version: "v1beta1",
+		Kind:    "BigQueryTable",
+	})
+	nn := types.NamespacedName{
+		Namespace: ref.Namespace,
+		Name:      ref.Name,
+	}
+	if nn.Namespace == "" {
+		nn.Namespace = src.GetNamespace()
+	}
+	if err := reader.Get(ctx, nn, table); err != nil {
+		if apierrors.IsNotFound(err) {
+			return nil, fmt.Errorf("referenced BigQueryTable %v not found", nn)
+		}
+		return nil, fmt.Errorf("error reading referenced BigQueryTable %v: %w", nn, err)
+	}
+	projectID, err := ResolveProjectID(ctx, reader, table)
+	if err != nil {
+		return nil, err
+	}
+	dataset, err := ResolveDatasetForObject(ctx, reader, table)
+	if err != nil {
+		return nil, err
+	}
+	tableID, err := GetResourceID(table)
+	if err != nil {
+		return nil, err
+	}
+	return &BigQueryTable{
+		projectID: projectID,
+		datasetID: dataset.datasetID,
+		tableID:   tableID,
+	}, nil
+}
+
+func (d *BigQueryTable) String() string {
+	return fmt.Sprintf("projects/%s/datasets/%s/tables/%s", d.projectID, d.datasetID, d.tableID)
+}
+
+func ResolveDatasetForObject(ctx context.Context, reader client.Reader, obj *unstructured.Unstructured) (*BigQueryDataset, error) {
+	datasetRefExternal, _, err := unstructured.NestedString(obj.Object, "spec", "datasetRef", "external")
+	if err != nil {
+		return nil, fmt.Errorf("error fetching datasetRef.external %w", err)
+	}
+	if datasetRefExternal != "" {
+		return ResolveBigQueryDataset(ctx, reader, obj, &BigQueryDatasetRef{External: datasetRefExternal})
+	}
+
+	datasetRefName, _, err := unstructured.NestedString(obj.Object, "spec", "datasetRef", "name")
+	if err != nil {
+		return nil, fmt.Errorf("error fetching datasetRef.name %w", err)
+	}
+	if datasetRefName != "" {
+		datasetRefNs, _, err := unstructured.NestedString(obj.Object, "spec", "datasetRef", "namespace")
+		if err != nil {
+			return nil, fmt.Errorf("error fetching datasetRef.namespace %w", err)
+		}
+
+		datasetRef := BigQueryDatasetRef{
+			Name:      datasetRefName,
+			Namespace: datasetRefNs,
+		}
+		if datasetRef.Namespace == "" {
+			datasetRef.Namespace = obj.GetNamespace()
+		}
+
+		return ResolveBigQueryDataset(ctx, reader, obj, &datasetRef)
+	}
+
+	return nil, fmt.Errorf("cannot find datasetRef for %v %v/%v", obj.GetKind(), obj.GetNamespace(), obj.GetName())
+}
