aa3019e133796b2eefd0ba33a7c88ff85cba01c8
Nikhil Bhoyar <nikhilbhoyar@google.com>
2024-10-25 10:53:50 +0530

    Removing todos

--- apis/kms/v1alpha1/kmskeyhandle_types.go
+++ apis/kms/v1alpha1/kmskeyhandle_types.go
@@ -68,7 +68,6 @@ type KMSKeyHandleObservedState struct {
 
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-// TODO(user): make sure the pluralizaiton below is correct
 // +kubebuilder:resource:categories=gcp,shortName=gcpkmskeyhandle;gcpkmskeyhandles
 // +kubebuilder:subresource:status
 // +kubebuilder:metadata:labels="cnrm.cloud.google.com/managed-by-kcc=true";"cnrm.cloud.google.com/system=true"
