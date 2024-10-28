c4b04b9755d5a27030ca19ecdbc6793cbd3a985b
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-10-28 18:11:55 -0400

    spelling: into
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/cli/outputsink/sink_test.go
+++ pkg/cli/outputsink/sink_test.go
@@ -50,7 +50,7 @@ func TestDirectorySink(t *testing.T) {
 	testCases := []struct {
 		name            string
 		sinkConstructor func(*schema.Provider, string) outputsink.OutputSink
-		// the file that will be read, with results passed in to the sink for testing
+		// the file that will be read, with results passed into the sink for testing
 		testCaseFile string
 		// the yaml file to use to pull unstructured schemas
 		unstructuredFile   string
