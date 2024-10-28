a24f87bc48cb865d89893c0e7807eaedd2fe3853
Nikhil Bhoyar <nikhilbhoyar@google.com>
2024-10-28 10:28:53 +0530

    part 4

--- pkg/snippet/snippetgeneration/snippetgeneration.go
+++ pkg/snippet/snippetgeneration/snippetgeneration.go
@@ -111,7 +111,7 @@ var preferredSampleForResource = map[string]string{
 	"vpcaccessconnector":                "cidr-connector",
 	"vertexaidataset":                   "vertexai-dataset-encryptionkey",
 	"vertexaiendpoint":                  "vertexai-endpoint-network",
-	"kmsautokeyconfig": "kmsautokeyconfig",
+	"kmsautokeyconfig":                  "kmsautokeyconfig",
 }
 
 type Snippet struct {
