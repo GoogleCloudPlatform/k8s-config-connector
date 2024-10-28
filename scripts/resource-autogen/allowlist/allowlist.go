eda0b66624c7e0a26388c22dcb3692f8fb9ecd4e
Andy Lu <luyul@google.com>
2024-10-28 12:36:31 +0100

    turn on doc auto-generation for BigQueryConnection

--- scripts/resource-autogen/allowlist/allowlist.go
+++ scripts/resource-autogen/allowlist/allowlist.go
@@ -57,7 +57,6 @@ var (
 		"bigquery/google_bigquery_dataset_access",
 		"bigquery_analytics_hub/google_bigquery_analytics_hub_data_exchange",
 		"bigquery_analytics_hub/google_bigquery_analytics_hub_listing",
-		"bigquery_connection/google_bigquery_connection",
 		"bigquery_datapolicy/google_bigquery_datapolicy_data_policy",
 		"bigquery_reservation/google_bigquery_capacity_commitment",
 		"bigquery_reservation/google_bigquery_reservation",
