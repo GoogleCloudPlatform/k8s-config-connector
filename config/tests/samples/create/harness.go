dc5355bf2db3d432f205ad221500064ca4e83f02
Gemma Hou <yuhou@google.com>
2024-10-28 17:02:47 +0000

    fix dataproccluster dynamic test

--- config/tests/samples/create/harness.go
+++ config/tests/samples/create/harness.go
@@ -423,7 +423,7 @@ func NewHarness(ctx context.Context, t *testing.T, opts ...HarnessOption) *Harne
 
 		h.Project = testgcp.GCPProject{
 			ProjectID:     "example-project",
-			ProjectNumber: 12345678,
+			ProjectNumber: 123456789,
 		}
 		testgcp.TestDependentOrgProjectID.Set("example-project-01")
 		testgcp.TestDependentFolderProjectID.Set("example-project-02")
