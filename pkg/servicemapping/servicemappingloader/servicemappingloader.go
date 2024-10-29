9bc279b69ec44382a977bfa45fe340255299668b
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-10-29 19:20:43 -0400

    spelling: unmarshalling
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/servicemapping/servicemappingloader/servicemappingloader.go
+++ pkg/servicemapping/servicemappingloader/servicemappingloader.go
@@ -251,7 +251,7 @@ func fileToServiceMapping(key string) (*v1alpha1.ServiceMapping, error) {
 func parseServiceMapping(b []byte) (*v1alpha1.ServiceMapping, error) {
 	var sm v1alpha1.ServiceMapping
 	if err := yaml.UnmarshalStrict(b, &sm); err != nil {
-		return nil, fmt.Errorf("error unmarshaling byte to service mapping: %w", err)
+		return nil, fmt.Errorf("error unmarshalling byte to service mapping: %w", err)
 	}
 	return &sm, nil
 }
