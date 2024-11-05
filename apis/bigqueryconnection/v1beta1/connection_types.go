87625012c2ab8ae8ed72b79403aee762595fd91b
Andy Lu <luyul@google.com>
2024-11-05 16:16:42 +0100

    re-generate CRD for bigqueryconnection API v1beta1

--- apis/bigqueryconnection/v1beta1/connection_types.go
+++ apis/bigqueryconnection/v1beta1/connection_types.go
@@ -319,6 +319,7 @@ type CloudSqlCredential struct {
 // +kubebuilder:printcolumn:name="Ready",JSONPath=".status.conditions[?(@.type=='Ready')].status",type="string",description="When 'True', the most recent reconcile of the resource succeeded"
 // +kubebuilder:printcolumn:name="Status",JSONPath=".status.conditions[?(@.type=='Ready')].reason",type="string",description="The reason for the value in 'Ready'"
 // +kubebuilder:printcolumn:name="Status Age",JSONPath=".status.conditions[?(@.type=='Ready')].lastTransitionTime",type="date",description="The last transition time for the value in 'Status'"
+// +kubebuilder:storageversion
 
 // BigQueryConnectionConnection is the Schema for the BigQueryConnectionConnection API
 // +k8s:openapi-gen=true
