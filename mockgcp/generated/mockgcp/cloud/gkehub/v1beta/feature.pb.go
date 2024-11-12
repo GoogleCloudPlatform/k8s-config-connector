8b468853e3534b3e65c5149b39746cf4c4510d13
Joyce Ma <maqiuyu@google.com>
2024-11-12 23:10:49 +0000

    Drop the change in feature.pb.go

--- mockgcp/generated/mockgcp/cloud/gkehub/v1beta/feature.pb.go
+++ mockgcp/generated/mockgcp/cloud/gkehub/v1beta/feature.pb.go
@@ -21,11 +21,11 @@
 package gkehubpb
 
 import (
-	configmanagementpb "cloud.google.com/go/gkehub/configmanagement/apiv1beta/configmanagementpb"
-	meteringpb "cloud.google.com/go/gkehub/metering/apiv1beta/meteringpb"
-	multiclusteringresspb "cloud.google.com/go/gkehub/multiclusteringress/apiv1beta/multiclusteringresspb"
-	policycontrollerpb "cloud.google.com/go/gkehub/policycontroller/apiv1beta/policycontrollerpb"
-	servicemeshpb "cloud.google.com/go/gkehub/servicemesh/apiv1beta/servicemeshpb"
+	configmanagementpb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/gkehub/v1beta/configmanagement"
+	meteringpb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/gkehub/v1beta/metering"
+	multiclusteringresspb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/gkehub/v1beta/multiclusteringress"
+	servicemeshpb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/gkehub/servicemesh/v1beta"
+	policycontrollerpb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/gkehub/policycontroller/v1beta"
 	timestamp "github.com/golang/protobuf/ptypes/timestamp"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
