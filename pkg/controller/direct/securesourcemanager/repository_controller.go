5c84414802583cd3d3538bd033a434f650933ced
Eric Pang <epang@google.com>
2024-11-12 18:22:29 +0000

    Fix instance ref in repo controller

--- pkg/controller/direct/securesourcemanager/repository_controller.go
+++ pkg/controller/direct/securesourcemanager/repository_controller.go
@@ -73,15 +73,12 @@ func (m *modelSecureSourceManagerRepository) AdapterForObject(ctx context.Contex
 	}
 
 	// Resolve SecureSourceManagerInstanceRef
-	if obj.Spec.InstanceRef != nil {
-		if obj.Spec.InstanceRef != nil {
-			instance, err := krm.ResolveSecureSourceManagerInstanceRef(ctx, reader, obj, obj.Spec.InstanceRef)
-			if err != nil {
-				return nil, err
-			}
-			obj.Spec.InstanceRef.External = instance.String()
-		}
+	instanceRef := obj.Spec.InstanceRef
+	normalizedExternal, err := instanceRef.NormalizedExternal(ctx, reader, instanceRef.Namespace)
+	if err != nil {
+		return nil, err
 	}
+	obj.Spec.InstanceRef.External = normalizedExternal
 
 	id, err := krm.NewSecureSourceManagerRepositoryRef(ctx, reader, obj)
 	if err != nil {
