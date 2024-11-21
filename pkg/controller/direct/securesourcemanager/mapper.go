06e7892bf009afe0f3d7d18b125c974e790b1b47
Eric Pang <epang@google.com>
2024-11-21 22:48:29 +0000

    Remove pscallowedprojects and add a capoolref func

--- pkg/controller/direct/securesourcemanager/mapper.go
+++ pkg/controller/direct/securesourcemanager/mapper.go
@@ -88,3 +88,9 @@ func SecureSourceManagerRepositorySpec_InstanceRef_FromProto(mapCtx *direct.MapC
 	}
 	return &krm.SecureSourceManagerInstanceRef{External: in}
 }
+func SecureSourceManagerInstanceSpec_CaPoolRef_FromProto(mapCtx *direct.MapContext, in string) *krm.PrivateCACAPoolRef {
+	if in == "" {
+		return nil
+	}
+	return &krm.PrivateCACAPoolRef{External: in}
+}
