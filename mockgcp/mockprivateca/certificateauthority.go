27d03b8d947e324480063178e0a2c8a8d4278c64
Eric Pang <epang@google.com>
2024-11-28 15:29:17 +0000

    mock ca skeleton

--- mockgcp/mockprivateca/certificateauthority.go
+++ mockgcp/mockprivateca/certificateauthority.go
@@ -17,7 +17,6 @@ package mockprivateca
 import (
 	"context"
 	"fmt"
-	"strings"
 	"time"
 
 	"google.golang.org/genproto/googleapis/longrunning"
@@ -27,16 +26,10 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
-	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/projects"
 	pb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/security/privateca/v1"
 )
 
-type PrivateCAV1 struct {
-	*MockService
-	pb.UnimplementedCertificateAuthorityServiceServer
-}
-
-func (s *PrivateCAV1) GetCaPool(ctx context.Context, req *pb.GetCaPoolRequest) (*pb.CaPool, error) {
+func (s *PrivateCAV1) GetCertificateAuthority(context.Context, *pb.GetCertificateAuthorityRequest) (*pb.CertificateAuthority, error) {
 	name, err := s.parseCAPoolName(req.Name)
 	if err != nil {
 		return nil, err
@@ -55,7 +48,7 @@ func (s *PrivateCAV1) GetCaPool(ctx context.Context, req *pb.GetCaPoolRequest) (
 	return obj, nil
 }
 
-func (s *PrivateCAV1) CreateCaPool(ctx context.Context, req *pb.CreateCaPoolRequest) (*longrunning.Operation, error) {
+func (s *PrivateCAV1) CreateCertificateAuthority(context.Context, *pb.CreateCertificateAuthorityRequest) (*longrunning.Operation, error) {
 	reqName := req.Parent + "/caPools/" + req.CaPoolId
 	name, err := s.parseCAPoolName(reqName)
 	if err != nil {
@@ -102,58 +95,7 @@ func (s *PrivateCAV1) CreateCaPool(ctx context.Context, req *pb.CreateCaPoolRequ
 	})
 }
 
-func (s *PrivateCAV1) UpdateCaPool(ctx context.Context, req *pb.UpdateCaPoolRequest) (*longrunning.Operation, error) {
-	reqName := req.GetCaPool().GetName()
-
-	name, err := s.parseCAPoolName(reqName)
-	if err != nil {
-		return nil, err
-	}
-
-	now := time.Now()
-
-	fqn := name.String()
-	obj := &pb.CaPool{}
-	if err := s.storage.Get(ctx, fqn, obj); err != nil {
-		return nil, err
-	}
-
-	// Required. A list of fields to be updated in this request.
-	paths := req.GetUpdateMask().GetPaths()
-
-	// TODO: Some sort of helper for fieldmask?
-	for _, path := range paths {
-		switch path {
-		case "issuancePolicy":
-			obj.IssuancePolicy = req.GetCaPool().GetIssuancePolicy()
-		case "publishingOptions":
-			obj.PublishingOptions = req.GetCaPool().GetPublishingOptions()
-		case "labels":
-			obj.Labels = req.GetCaPool().GetLabels()
-		default:
-			return nil, status.Errorf(codes.InvalidArgument, "update_mask path %q not valid", path)
-		}
-	}
-
-	if err := s.storage.Update(ctx, fqn, obj); err != nil {
-		return nil, err
-	}
-
-	opMetadata := &pb.OperationMetadata{
-		ApiVersion:            "v1",
-		CreateTime:            timestamppb.New(now),
-		Verb:                  "update",
-		RequestedCancellation: false,
-		Target:                fqn,
-	}
-	opPrefix := fmt.Sprintf("projects/%s/locations/%s", name.Project.ID, name.Location)
-	return s.operations.StartLRO(ctx, opPrefix, opMetadata, func() (proto.Message, error) {
-		opMetadata.EndTime = timestamppb.Now()
-		return obj, nil
-	})
-}
-
-func (s *PrivateCAV1) DeleteCaPool(ctx context.Context, req *pb.DeleteCaPoolRequest) (*longrunning.Operation, error) {
+func (s *PrivateCAV1) DeleteCertificateAuthority(context.Context, *pb.DeleteCertificateAuthorityRequest) (*longrunning.Operation, error) {
 	name, err := s.parseCAPoolName(req.Name)
 	if err != nil {
 		return nil, err
@@ -180,37 +122,4 @@ func (s *PrivateCAV1) DeleteCaPool(ctx context.Context, req *pb.DeleteCaPoolRequ
 		opMetadata.EndTime = timestamppb.Now()
 		return &emptypb.Empty{}, nil
 	})
-}
-
-type caPoolName struct {
-	Project    *projects.ProjectData
-	Location   string
-	CAPoolName string
-}
-
-func (n *caPoolName) String() string {
-	return "projects/" + n.Project.ID + "/locations/" + n.Location + "/caPools/" + n.CAPoolName
-}
-
-// parseCAPoolName parses a string into a caPoolName.
-// The expected form is projects/<projectID>/locations/<region>/caPools/<capoolName>
-func (s *MockService) parseCAPoolName(name string) (*caPoolName, error) {
-	tokens := strings.Split(name, "/")
-
-	if len(tokens) == 6 && tokens[0] == "projects" && tokens[2] == "locations" && tokens[4] == "caPools" {
-		project, err := s.Projects.GetProjectByID(tokens[1])
-		if err != nil {
-			return nil, err
-		}
-
-		name := &caPoolName{
-			Project:    project,
-			Location:   tokens[3],
-			CAPoolName: tokens[5],
-		}
-
-		return name, nil
-	} else {
-		return nil, status.Errorf(codes.InvalidArgument, "name %q is not valid", name)
-	}
 }
