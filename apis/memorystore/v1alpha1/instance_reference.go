217c0aedf7ea7791148fa40f11479783f01a6e34
Alex Pana <8968914+acpana@users.noreply.github.com>
2024-12-04 00:16:04 +0000

    fix: update to use new ResolveProject func
    Signed-off-by: Alex Pana <8968914+acpana@users.noreply.github.com>

--- apis/memorystore/v1alpha1/instance_reference.go
+++ apis/memorystore/v1alpha1/instance_reference.go
@@ -90,7 +90,7 @@ func NewMemorystoreInstanceRef(ctx context.Context, reader client.Reader, obj *M
 	id := &MemorystoreInstanceRef{}
 
 	// Get Parent
-	projectRef, err := refsv1beta1.ResolveProject(ctx, reader, obj, obj.Spec.ProjectRef)
+	projectRef, err := refsv1beta1.ResolveProject(ctx, reader, obj.GetNamespace(), obj.Spec.ProjectRef)
 	if err != nil {
 		return nil, err
 	}
