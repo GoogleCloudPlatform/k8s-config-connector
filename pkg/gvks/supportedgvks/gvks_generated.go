f393adb445b4a7b10a3332de1ed201058192f358
justinsb <justinsb@google.com>
2024-12-18 12:39:41 -0500

    DiscoveryEngineTargetSite: generated apimachinery

--- pkg/gvks/supportedgvks/gvks_generated.go
+++ pkg/gvks/supportedgvks/gvks_generated.go
@@ -2354,6 +2354,16 @@ var SupportedGVKs = map[schema.GroupVersionKind]GVKMetadata{
 	{
 		Group:   "discoveryengine.cnrm.cloud.google.com",
 		Version: "v1alpha1",
+		Kind:    "DiscoveryEngineDataStoreTargetSite",
+	}: {
+		Labels: map[string]string{
+			"cnrm.cloud.google.com/managed-by-kcc": "true",
+			"cnrm.cloud.google.com/system":         "true",
+		},
+	},
+	{
+		Group:   "discoveryengine.cnrm.cloud.google.com",
+		Version: "v1alpha1",
 		Kind:    "DiscoveryEngineEngine",
 	}: {
 		Labels: map[string]string{
