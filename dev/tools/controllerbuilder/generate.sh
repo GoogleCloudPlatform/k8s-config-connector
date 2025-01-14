39e066169c54cc13974b37c28897dd7ce221dcde
Jingyi Hu <jingyih@google.com>
2025-01-14 09:02:18 +0000

    feat: add mapping functions for IAPSettings

--- dev/tools/controllerbuilder/generate.sh
+++ dev/tools/controllerbuilder/generate.sh
@@ -184,5 +184,9 @@ go run . generate-types \
     --api-version iap.cnrm.cloud.google.com/v1alpha1 \
     --resource IAPSettings:IapSettings
 
+go run . generate-mapper \
+    --service google.cloud.iap.v1 \
+    --api-version iap.cnrm.cloud.google.com/v1alpha1
+
 # Fix up formatting
 ${REPO_ROOT}/dev/tasks/fix-gofmt
