a7614c1d89c9de357f47ae010eef7c7b1f2c1458
justinsb <justinsb@google.com>
2025-01-23 10:53:07 -0500

    mockgcp: support for filestore instance

--- mockgcp/mockfilestore/instance.go
+++ mockgcp/mockfilestore/instance.go
@@ -21,6 +21,7 @@ package mockfilestore
 import (
 	"context"
 	"fmt"
+	"reflect"
 	"strings"
 	"time"
 
@@ -33,6 +34,7 @@ import (
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/projects"
 	commonpb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/common"
 	pb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/filestore/v1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/pkg/storage"
 	longrunningpb "google.golang.org/genproto/googleapis/longrunning"
 )
 
@@ -95,17 +97,52 @@ func (s *FilestoreV1) CreateInstance(ctx context.Context, req *pb.CreateInstance
 		return nil, err
 	}
 
-	lroPrefix := fmt.Sprintf("projects/%d/locations/%s", name.Project.Number, name.Location)
-	metadata := &commonpb.OperationMetadata{
+	lroPrefix := fmt.Sprintf("projects/%s/locations/%s", name.Project.ID, name.Location)
+	lroMetadata := &commonpb.OperationMetadata{
 		ApiVersion: "v1",
+		CreateTime: timestamppb.New(now),
+		Target:     obj.Name,
+		Verb:       "create",
 	}
-	return s.operations.StartLRO(ctx, lroPrefix, metadata, func() (proto.Message, error) {
-		return obj, nil
+	return s.operations.StartLRO(ctx, lroPrefix, lroMetadata, func() (proto.Message, error) {
+		lroMetadata.EndTime = timestamppb.Now()
+
+		return mutateObject(ctx, s.storage, fqn, func(obj *pb.Instance) error {
+			obj.State = pb.Instance_READY
+			for _, network := range obj.Networks {
+				network.ReservedIpRange = "10.1.2.0/29"
+				network.IpAddresses = []string{"10.1.2.1"}
+			}
+			return nil
+		})
 	})
 }
 
-func (s *FilestoreV1) populateDefaultsForInstance(obj *pb.Instance) {
+// mutateObject updates the object; it gets the object by fqn, calls mutator, then updates the object
+func mutateObject[T proto.Message](ctx context.Context, storage storage.Storage, fqn string, mutator func(obj T) error) (T, error) {
+	var nilT T
 
+	typeT := reflect.TypeOf(nilT)
+	obj := reflect.New(typeT.Elem()).Interface().(T)
+	if err := storage.Get(ctx, fqn, obj); err != nil {
+		return nilT, err
+	}
+
+	if err := mutator(obj); err != nil {
+		return nilT, err
+	}
+
+	if err := storage.Update(ctx, fqn, obj); err != nil {
+		return nilT, err
+	}
+
+	return obj, nil
+}
+
+func (s *FilestoreV1) populateDefaultsForInstance(obj *pb.Instance) {
+	if obj.Tier == pb.Instance_TIER_UNSPECIFIED {
+		obj.Tier = pb.Instance_STANDARD
+	}
 }
 
 func (s *FilestoreV1) UpdateInstance(ctx context.Context, req *pb.UpdateInstanceRequest) (*longrunningpb.Operation, error) {
@@ -166,9 +203,18 @@ func (s *FilestoreV1) DeleteInstance(ctx context.Context, req *pb.DeleteInstance
 	if err := s.storage.Delete(ctx, fqn, deleted); err != nil {
 		return nil, err
 	}
-	prefix := fmt.Sprintf("projects/%d/locations/%s", name.Project.Number, name.Location)
-	return s.operations.DoneLRO(ctx, prefix, nil, &emptypb.Empty{})
 
+	lroPrefix := fmt.Sprintf("projects/%s/locations/%s", name.Project.ID, name.Location)
+	lroMetadata := &commonpb.OperationMetadata{
+		ApiVersion: "v1",
+		CreateTime: timestamppb.Now(),
+		Target:     fqn,
+		Verb:       "delete",
+	}
+	return s.operations.StartLRO(ctx, lroPrefix, lroMetadata, func() (proto.Message, error) {
+		lroMetadata.EndTime = timestamppb.Now()
+		return &emptypb.Empty{}, nil
+	})
 }
 
 type instanceName struct {
@@ -178,7 +224,7 @@ type instanceName struct {
 }
 
 func (n *instanceName) String() string {
-	return fmt.Sprintf("projects/%d/locations/%s/instances/%s", n.Project.Number, n.Location, n.Instance)
+	return fmt.Sprintf("projects/%s/locations/%s/instances/%s", n.Project.ID, n.Location, n.Instance)
 }
 
 // parseInstanceName parses a string into an InstanceName.
