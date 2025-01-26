99e55f3125a62a2e736917c21aca389ea3dda1e0
justinsb <justinsb@google.com>
2025-01-26 10:02:53 -0500

    filestore: autogenerate types and CRDs

--- pkg/controller/direct/filestore/mapper.generated.go
+++ pkg/controller/direct/filestore/mapper.generated.go
@@ -18,8 +18,8 @@ import (
 	pb "cloud.google.com/go/filestore/apiv1/filestorepb"
 	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/filestore/v1alpha1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
-	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
 )
+
 func FileShareConfig_FromProto(mapCtx *direct.MapContext, in *pb.FileShareConfig) *krm.FileShareConfig {
 	if in == nil {
 		return nil
@@ -27,7 +27,7 @@ func FileShareConfig_FromProto(mapCtx *direct.MapContext, in *pb.FileShareConfig
 	out := &krm.FileShareConfig{}
 	out.Name = direct.LazyPtr(in.GetName())
 	out.CapacityGB = direct.LazyPtr(in.GetCapacityGb())
-	out.SourceBackup = direct.LazyPtr(in.GetSourceBackup())
+	// MISSING: SourceBackup
 	out.NfsExportOptions = direct.Slice_FromProto(mapCtx, in.NfsExportOptions, NfsExportOptions_FromProto)
 	return out
 }
@@ -38,9 +38,7 @@ func FileShareConfig_ToProto(mapCtx *direct.MapContext, in *krm.FileShareConfig)
 	out := &pb.FileShareConfig{}
 	out.Name = direct.ValueOf(in.Name)
 	out.CapacityGb = direct.ValueOf(in.CapacityGB)
-	if oneof := FileShareConfig_SourceBackup_ToProto(mapCtx, in.SourceBackup); oneof != nil {
-		out.Source = oneof
-	}
+	// MISSING: SourceBackup
 	out.NfsExportOptions = direct.Slice_ToProto(mapCtx, in.NfsExportOptions, NfsExportOptions_ToProto)
 	return out
 }
@@ -49,20 +47,16 @@ func FilestoreInstanceObservedState_FromProto(mapCtx *direct.MapContext, in *pb.
 		return nil
 	}
 	out := &krm.FilestoreInstanceObservedState{}
-	// MISSING: Name
-	// MISSING: Description
-	// MISSING: State
-	// MISSING: StatusMessage
-	// MISSING: CreateTime
-	// MISSING: Tier
+	out.Name = direct.LazyPtr(in.GetName())
+	out.State = direct.Enum_FromProto(mapCtx, in.GetState())
+	out.StatusMessage = direct.LazyPtr(in.GetStatusMessage())
+	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
 	// MISSING: Labels
-	// MISSING: FileShares
-	// MISSING: Networks
+	out.Networks = direct.Slice_FromProto(mapCtx, in.Networks, NetworkConfigObservedState_FromProto)
 	// MISSING: Etag
-	// MISSING: SatisfiesPzs
-	// MISSING: SatisfiesPzi
-	// MISSING: KMSKeyName
-	// MISSING: SuspensionReasons
+	out.SatisfiesPzs = direct.BoolValue_FromProto(mapCtx, in.GetSatisfiesPzs())
+	out.SatisfiesPzi = direct.LazyPtr(in.GetSatisfiesPzi())
+	out.SuspensionReasons = direct.EnumSlice_FromProto(mapCtx, in.SuspensionReasons)
 	return out
 }
 func FilestoreInstanceObservedState_ToProto(mapCtx *direct.MapContext, in *krm.FilestoreInstanceObservedState) *pb.Instance {
@@ -70,20 +64,16 @@ func FilestoreInstanceObservedState_ToProto(mapCtx *direct.MapContext, in *krm.F
 		return nil
 	}
 	out := &pb.Instance{}
-	// MISSING: Name
-	// MISSING: Description
-	// MISSING: State
-	// MISSING: StatusMessage
-	// MISSING: CreateTime
-	// MISSING: Tier
+	out.Name = direct.ValueOf(in.Name)
+	out.State = direct.Enum_ToProto[pb.Instance_State](mapCtx, in.State)
+	out.StatusMessage = direct.ValueOf(in.StatusMessage)
+	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
 	// MISSING: Labels
-	// MISSING: FileShares
-	// MISSING: Networks
+	out.Networks = direct.Slice_ToProto(mapCtx, in.Networks, NetworkConfigObservedState_ToProto)
 	// MISSING: Etag
-	// MISSING: SatisfiesPzs
-	// MISSING: SatisfiesPzi
-	// MISSING: KMSKeyName
-	// MISSING: SuspensionReasons
+	out.SatisfiesPzs = direct.BoolValue_ToProto(mapCtx, in.SatisfiesPzs)
+	out.SatisfiesPzi = direct.ValueOf(in.SatisfiesPzi)
+	out.SuspensionReasons = direct.EnumSlice_ToProto[pb.Instance_SuspensionReason](mapCtx, in.SuspensionReasons)
 	return out
 }
 func FilestoreInstanceSpec_FromProto(mapCtx *direct.MapContext, in *pb.Instance) *krm.FilestoreInstanceSpec {
@@ -91,20 +81,13 @@ func FilestoreInstanceSpec_FromProto(mapCtx *direct.MapContext, in *pb.Instance)
 		return nil
 	}
 	out := &krm.FilestoreInstanceSpec{}
-	// MISSING: Name
-	// MISSING: Description
-	// MISSING: State
-	// MISSING: StatusMessage
-	// MISSING: CreateTime
-	// MISSING: Tier
+	out.Description = direct.LazyPtr(in.GetDescription())
+	out.Tier = direct.Enum_FromProto(mapCtx, in.GetTier())
 	// MISSING: Labels
-	// MISSING: FileShares
-	// MISSING: Networks
+	out.FileShares = direct.Slice_FromProto(mapCtx, in.FileShares, FileShareConfig_FromProto)
+	out.Networks = direct.Slice_FromProto(mapCtx, in.Networks, NetworkConfig_FromProto)
 	// MISSING: Etag
-	// MISSING: SatisfiesPzs
-	// MISSING: SatisfiesPzi
-	// MISSING: KMSKeyName
-	// MISSING: SuspensionReasons
+	out.KMSKeyName = direct.LazyPtr(in.GetKmsKeyName())
 	return out
 }
 func FilestoreInstanceSpec_ToProto(mapCtx *direct.MapContext, in *krm.FilestoreInstanceSpec) *pb.Instance {
@@ -112,104 +95,13 @@ func FilestoreInstanceSpec_ToProto(mapCtx *direct.MapContext, in *krm.FilestoreI
 		return nil
 	}
 	out := &pb.Instance{}
-	// MISSING: Name
-	// MISSING: Description
-	// MISSING: State
-	// MISSING: StatusMessage
-	// MISSING: CreateTime
-	// MISSING: Tier
-	// MISSING: Labels
-	// MISSING: FileShares
-	// MISSING: Networks
-	// MISSING: Etag
-	// MISSING: SatisfiesPzs
-	// MISSING: SatisfiesPzi
-	// MISSING: KMSKeyName
-	// MISSING: SuspensionReasons
-	return out
-}
-func Instance_FromProto(mapCtx *direct.MapContext, in *pb.Instance) *krm.Instance {
-	if in == nil {
-		return nil
-	}
-	out := &krm.Instance{}
-	// MISSING: Name
-	out.Description = direct.LazyPtr(in.GetDescription())
-	// MISSING: State
-	// MISSING: StatusMessage
-	// MISSING: CreateTime
-	out.Tier = direct.Enum_FromProto(mapCtx, in.GetTier())
-	out.Labels = in.Labels
-	out.FileShares = direct.Slice_FromProto(mapCtx, in.FileShares, FileShareConfig_FromProto)
-	out.Networks = direct.Slice_FromProto(mapCtx, in.Networks, NetworkConfig_FromProto)
-	out.Etag = direct.LazyPtr(in.GetEtag())
-	// MISSING: SatisfiesPzs
-	// MISSING: SatisfiesPzi
-	out.KMSKeyName = direct.LazyPtr(in.GetKmsKeyName())
-	// MISSING: SuspensionReasons
-	return out
-}
-func Instance_ToProto(mapCtx *direct.MapContext, in *krm.Instance) *pb.Instance {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Instance{}
-	// MISSING: Name
 	out.Description = direct.ValueOf(in.Description)
-	// MISSING: State
-	// MISSING: StatusMessage
-	// MISSING: CreateTime
 	out.Tier = direct.Enum_ToProto[pb.Instance_Tier](mapCtx, in.Tier)
-	out.Labels = in.Labels
+	// MISSING: Labels
 	out.FileShares = direct.Slice_ToProto(mapCtx, in.FileShares, FileShareConfig_ToProto)
 	out.Networks = direct.Slice_ToProto(mapCtx, in.Networks, NetworkConfig_ToProto)
-	out.Etag = direct.ValueOf(in.Etag)
-	// MISSING: SatisfiesPzs
-	// MISSING: SatisfiesPzi
+	// MISSING: Etag
 	out.KmsKeyName = direct.ValueOf(in.KMSKeyName)
-	// MISSING: SuspensionReasons
-	return out
-}
-func InstanceObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Instance) *krm.InstanceObservedState {
-	if in == nil {
-		return nil
-	}
-	out := &krm.InstanceObservedState{}
-	out.Name = direct.LazyPtr(in.GetName())
-	// MISSING: Description
-	out.State = direct.Enum_FromProto(mapCtx, in.GetState())
-	out.StatusMessage = direct.LazyPtr(in.GetStatusMessage())
-	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
-	// MISSING: Tier
-	// MISSING: Labels
-	// MISSING: FileShares
-	out.Networks = direct.Slice_FromProto(mapCtx, in.Networks, NetworkConfigObservedState_FromProto)
-	// MISSING: Etag
-	out.SatisfiesPzs = direct.BoolValue_FromProto(mapCtx, in.GetSatisfiesPzs())
-	out.SatisfiesPzi = direct.LazyPtr(in.GetSatisfiesPzi())
-	// MISSING: KMSKeyName
-	out.SuspensionReasons = direct.EnumSlice_FromProto(mapCtx, in.SuspensionReasons)
-	return out
-}
-func InstanceObservedState_ToProto(mapCtx *direct.MapContext, in *krm.InstanceObservedState) *pb.Instance {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Instance{}
-	out.Name = direct.ValueOf(in.Name)
-	// MISSING: Description
-	out.State = direct.Enum_ToProto[pb.Instance_State](mapCtx, in.State)
-	out.StatusMessage = direct.ValueOf(in.StatusMessage)
-	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
-	// MISSING: Tier
-	// MISSING: Labels
-	// MISSING: FileShares
-	out.Networks = direct.Slice_ToProto(mapCtx, in.Networks, NetworkConfigObservedState_ToProto)
-	// MISSING: Etag
-	out.SatisfiesPzs = direct.BoolValue_ToProto(mapCtx, in.SatisfiesPzs)
-	out.SatisfiesPzi = direct.ValueOf(in.SatisfiesPzi)
-	// MISSING: KMSKeyName
-	out.SuspensionReasons = direct.EnumSlice_ToProto[pb.Instance_SuspensionReason](mapCtx, in.SuspensionReasons)
 	return out
 }
 func NetworkConfig_FromProto(mapCtx *direct.MapContext, in *pb.NetworkConfig) *krm.NetworkConfig {
@@ -244,7 +136,7 @@ func NetworkConfigObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Netw
 	// MISSING: Network
 	// MISSING: Modes
 	// MISSING: ReservedIPRange
-	out.IPAddresses = in.IPAddresses
+	out.IPAddresses = in.IpAddresses
 	// MISSING: ConnectMode
 	return out
 }
@@ -265,7 +157,7 @@ func NfsExportOptions_FromProto(mapCtx *direct.MapContext, in *pb.NfsExportOptio
 		return nil
 	}
 	out := &krm.NfsExportOptions{}
-	out.IPRanges = in.IPRanges
+	out.IPRanges = in.IpRanges
 	out.AccessMode = direct.Enum_FromProto(mapCtx, in.GetAccessMode())
 	out.SquashMode = direct.Enum_FromProto(mapCtx, in.GetSquashMode())
 	out.AnonUid = direct.LazyPtr(in.GetAnonUid())
