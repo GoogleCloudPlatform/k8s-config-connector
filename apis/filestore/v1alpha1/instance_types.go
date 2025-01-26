5fcec6ffd85d5f68d5a159032615de405d7412fe
justinsb <justinsb@google.com>
2025-01-26 07:49:55 -0500

    FilestoreInstance: update spec & status definitions

--- apis/filestore/v1alpha1/instance_types.go
+++ apis/filestore/v1alpha1/instance_types.go
@@ -22,8 +22,29 @@ import (
 var FilestoreInstanceGVK = GroupVersion.WithKind("FilestoreInstance")
 
 // FilestoreInstanceSpec defines the desired state of FilestoreInstance
-// +kcc:proto=google.cloud.filestore.v1.Instance
+// +xkcc:proto=google.cloud.filestore.v1.Instance
 type FilestoreInstanceSpec struct {
+	// The description of the instance (2048 characters or less).
+	Description *string `json:"description,omitempty"`
+
+	// The service tier of the instance.
+	Tier *string `json:"tier,omitempty"`
+
+	// NOTYET
+	// Resource labels to represent user provided metadata.
+	// Labels []InstanceLabel `json:"labels,omitempty"`
+
+	// File system shares on the instance.
+	//  For this version, only a single file share is supported.
+	FileShares []FileShareConfig `json:"fileShares,omitempty"`
+
+	// VPC networks to which the instance is connected.
+	//  For this version, only a single network is supported.
+	Networks []NetworkConfig `json:"networks,omitempty"`
+
+	// The KMS key name used for data encryption.
+	KMSKeyName *string `json:"kmsKeyName,omitempty"`
+
 	// The FilestoreInstance name. If not given, the metadata.name will be used.
 	ResourceID *string `json:"resourceID,omitempty"`
 }
@@ -44,10 +65,35 @@ type FilestoreInstanceStatus struct {
 	ObservedState *FilestoreInstanceObservedState `json:"observedState,omitempty"`
 }
 
-// FilestoreInstanceSpec defines the desired state of FilestoreInstance
-// +kcc:proto=google.cloud.filestore.v1.Instance
 // FilestoreInstanceObservedState is the state of the FilestoreInstance resource as most recently observed in GCP.
+// +xkcc:proto=google.cloud.filestore.v1.Instance
 type FilestoreInstanceObservedState struct {
+	// Output only. The resource name of the instance, in the format
+	//  `projects/{project}/locations/{location}/instances/{instance}`.
+	Name *string `json:"name,omitempty"`
+
+	// Output only. The instance state.
+	State *string `json:"state,omitempty"`
+
+	// Output only. Additional information about the instance state, if available.
+	StatusMessage *string `json:"statusMessage,omitempty"`
+
+	// Output only. The time when the instance was created.
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// VPC networks to which the instance is connected.
+	//  For this version, only a single network is supported.
+	Networks []NetworkConfigObservedState `json:"networks,omitempty"`
+
+	// Output only. Reserved for future use.
+	SatisfiesPzs *bool `json:"satisfiesPzs,omitempty"`
+
+	// Output only. Reserved for future use.
+	SatisfiesPzi *bool `json:"satisfiesPzi,omitempty"`
+
+	// Output only. Field indicates all the reasons the instance is in "SUSPENDED"
+	//  state.
+	SuspensionReasons []string `json:"suspensionReasons,omitempty"`
 }
 
 // +genclient
