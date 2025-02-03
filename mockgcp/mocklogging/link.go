b88659bfbebee810ac2475261ff6bb854dc8ac5e
Tyler Reid <tylerreid@google.com>
2025-02-03 21:18:52 +0000

    cleanup some comments, rename file

--- mockgcp/mocklogging/link.go
+++ mockgcp/mocklogging/link.go
@@ -13,21 +13,19 @@
 // limitations under the License.
 
 // +tool:mockgcp-support
-// krm.apiVersion: logging.cnrm.cloud.google.com/v1beta1
-// krm.kind: LoggingLoggingLink
+// krm.apiVersion: logging.cnrm.cloud.google.com/v1alpha1
+// krm.kind: LoggingLink
 // proto.service: google.logging.v2.ConfigServiceV2
-// proto.resource: LoggingLink
+// proto.resource: Link
 
 package mocklogging
 
 import (
 	"context"
-	// "fmt"
 	"fmt"
 	"strings"
-	"time"
 
-	"github.com/golang/protobuf/ptypes/empty"
+	"cloud.google.com/go/longrunning/autogen/longrunningpb"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/proto"
@@ -37,50 +35,14 @@ import (
 	pb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/logging/v2"
 )
 
-type linkService struct {
-	*MockService
-	pb.UnimplementedConfigServiceV2Server
-}
-
-/*
-
-// createLinkDefaultObjects will ensure that the default log bucket is created for the folder/project/org
-// The input to this is probably linkName not bucketNmae
-func (s *linkService) createLinkDefaultObjects(ctx context.Context, name *loggingLinkName) error {
-	// Create the default bucket
-	{
-		bucket := &pb.LogBucket{
-			Description:    "Default bucket",
-			LifecycleState: pb.LifecycleState_ACTIVE,
-			RetentionDays:  30,
-		}
-		if name.folder != "" {
-			bucket.Name = fmt.Sprintf("folders/%s/locations/global/buckets/_Default", name.folder)
-		}
-
-		// This function exists in LogBucket, assuming I can call it here
-		if err := s.createBucketIfNotExists(ctx, bucket); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-*/
-
-func (s *linkService) GetLink(ctx context.Context, req *pb.GetLinkRequest) (*pb.Link, error) {
+func (s *configService) GetLink(ctx context.Context, req *pb.GetLinkRequest) (*pb.Link, error) {
 	name, err := s.parseLoggingLinkName(req.Name)
 	fmt.Printf("MOCK LOGGING GET LINK")
 
 	if err != nil {
 		return nil, err
 	}
-	/*
-		if err := s.createLinkDefaultObjects(ctx, name); err != nil {
-			return nil, err
-		}
-	*/
+
 	fqn := name.String()
 	obj := &pb.Link{}
 	if err := s.storage.Get(ctx, fqn, obj); err != nil {
@@ -92,52 +54,57 @@ func (s *linkService) GetLink(ctx context.Context, req *pb.GetLinkRequest) (*pb.
 	return obj, nil
 }
 
-func (s *linkService) CreateLink(ctx context.Context, req *pb.CreateLinkRequest) (*pb.Link, error) {
+func (s *configService) CreateLink(ctx context.Context, req *pb.CreateLinkRequest) (*longrunningpb.Operation, error) {
 	fmt.Printf("MOCK LOGGING CREATE LINK")
 	reqName := req.Parent + "/links/" + req.GetLinkId()
 	name, err := s.parseLoggingLinkName(reqName)
 	if err != nil {
 		return nil, err
 	}
-	/*
-		if err := s.createLinkDefaultObjects(ctx, name); err != nil {
-			return nil, err
-		}
-	*/
+
 	fqn := name.String()
-	now := time.Now()
+	now := timestamppb.Now()
 	obj := proto.Clone(req.GetLink()).(*pb.Link)
 	obj.Name = fqn
-	obj.CreateTime = timestamppb.New(now)
-	// s.populateDefaultsForLogBucket(obj)
+	obj.CreateTime = now
+
 	if err := s.storage.Create(ctx, fqn, obj); err != nil {
 		return nil, err
 	}
-	return obj, nil
+
+	metadata := &pb.LinkMetadata{
+		StartTime: now,
+	}
+
+	return s.operations.StartLRO(ctx, fqn, metadata, func() (proto.Message, error) {
+		return obj, nil
+	})
 }
 
-func (s *linkService) populateDefaultsForLoggingLink(obj *pb.Link) {
+func (s *configService) populateDefaultsForLoggingLink(obj *pb.Link) {
 	if obj.LifecycleState == pb.LifecycleState_LIFECYCLE_STATE_UNSPECIFIED {
 		obj.LifecycleState = pb.LifecycleState_ACTIVE
 	}
 }
 
-func (s *linkService) DeleteLink(ctx context.Context, req *pb.DeleteLinkRequest) (*empty.Empty, error) {
+func (s *configService) DeleteLink(ctx context.Context, req *pb.DeleteLinkRequest) (*longrunningpb.Operation, error) {
 	name, err := s.parseLoggingLinkName(req.Name)
 	if err != nil {
 		return nil, err
 	}
-	/*
-		if err := s.createLinkDefaultObjects(ctx, name); err != nil {
-			return nil, err
-		}
-	*/
+
 	fqn := name.String()
 	deletedObj := &pb.Link{}
 	if err := s.storage.Delete(ctx, fqn, deletedObj); err != nil {
 		return nil, err
 	}
-	return &empty.Empty{}, nil
+	now := timestamppb.Now()
+	metadata := &pb.LinkMetadata{
+		StartTime: now,
+	}
+
+	return s.operations.DoneLRO(ctx, fqn, metadata, &pb.Link{})
+
 }
 
 type loggingLinkName struct {
