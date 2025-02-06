bbbfde6d5c86e63a8dc598ff47f14bd33b4f690f
Jingyi Hu <jingyih@google.com>
2025-02-06 02:48:05 +0000

    chore: manually fix the fuzzer for ManagedKafkaTopic
    The resource has no ObservedState struct.

--- pkg/controller/direct/managedkafka/topic_fuzzer.go
+++ pkg/controller/direct/managedkafka/topic_fuzzer.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -23,20 +23,15 @@ import (
 )
 
 func init() {
-	fuzztesting.RegisterKRMFuzzer(managedKafkaTopicFuzzer())
+	fuzztesting.RegisterKRMSpecFuzzer(managedKafkaTopicFuzzer())
 }
 
 func managedKafkaTopicFuzzer() fuzztesting.KRMFuzzer {
-	f := fuzztesting.NewKRMTypedFuzzer(&pb.Topic{},
+	f := fuzztesting.NewKRMTypedSpecFuzzer(&pb.Topic{},
 		ManagedKafkaTopicSpec_FromProto, ManagedKafkaTopicSpec_ToProto,
-		nil, nil,
 	)
 
 	f.UnimplementedFields.Insert(".name") // Special Field
-
-	f.SpecFields.Insert(".partition_count")
-	f.SpecFields.Insert(".replication_factor")
-	f.SpecFields.Insert(".configs")
 
 	return f
 }
