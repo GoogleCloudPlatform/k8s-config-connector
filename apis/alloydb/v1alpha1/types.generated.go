cbd3bbdc928632cfa12b474712e1de03169dc07a
Your Name <yuwenma@google.com>
2025-02-07 09:59:20 +0000

    feat: add resource AlloydbBackup

--- apis/alloydb/v1alpha1/types.generated.go
+++ apis/alloydb/v1alpha1/types.generated.go
@@ -15,30 +15,174 @@
 package v1alpha1
 
 
-// +kcc:proto=google.cloud.alloydb.v1.ConnectionInfo
-type ConnectionInfo struct {
-	// The name of the ConnectionInfo singleton resource, e.g.:
-	//  projects/{project}/locations/{location}/clusters/*/instances/*/connectionInfo
-	//  This field currently has no semantic meaning.
-	// +kcc:proto:field=google.cloud.alloydb.v1.ConnectionInfo.name
-	Name *string `json:"name,omitempty"`
+// +kcc:proto=google.cloud.alloydb.v1.Backup
+type Backup struct {
+
+	// User-settable and human-readable display name for the Backup.
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.display_name
+	DisplayName *string `json:"displayName,omitempty"`
+
+	// Labels as key value pairs
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.labels
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// The backup type, which suggests the trigger for the backup.
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.type
+	Type *string `json:"type,omitempty"`
+
+	// User-provided description of the backup.
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.description
+	Description *string `json:"description,omitempty"`
+
+	// Required. The full resource name of the backup source cluster
+	//  (e.g., projects/{project}/locations/{region}/clusters/{cluster_id}).
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.cluster_name
+	ClusterName *string `json:"clusterName,omitempty"`
+
+	// Optional. The encryption config can be specified to encrypt the
+	//  backup with a customer-managed encryption key (CMEK). When this field is
+	//  not specified, the backup will then use default encryption scheme to
+	//  protect the user data.
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.encryption_config
+	EncryptionConfig *EncryptionConfig `json:"encryptionConfig,omitempty"`
+
+	// For Resource freshness validation (https://google.aip.dev/154)
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.etag
+	Etag *string `json:"etag,omitempty"`
+
+	// Annotations to allow client tools to store small amount of arbitrary data.
+	//  This is distinct from labels.
+	//  https://google.aip.dev/128
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.annotations
+	Annotations map[string]string `json:"annotations,omitempty"`
+
+	// Optional. Input only. Immutable. Tag keys/values directly bound to this
+	//  resource. For example:
+	//  ```
+	//  "123/environment": "production",
+	//  "123/costCenter": "marketing"
+	//  ```
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.tags
+	Tags map[string]string `json:"tags,omitempty"`
 }
 
-// +kcc:proto=google.cloud.alloydb.v1.ConnectionInfo
-type ConnectionInfoObservedState struct {
-	// Output only. The private network IP address for the Instance. This is the
-	//  default IP for the instance and is always created (even if enable_public_ip
-	//  is set). This is the connection endpoint for an end-user application.
-	// +kcc:proto:field=google.cloud.alloydb.v1.ConnectionInfo.ip_address
-	IPAddress *string `json:"ipAddress,omitempty"`
+// +kcc:proto=google.cloud.alloydb.v1.Backup.QuantityBasedExpiry
+type Backup_QuantityBasedExpiry struct {
+}
 
-	// Output only. The public IP addresses for the Instance. This is available
-	//  ONLY when enable_public_ip is set. This is the connection endpoint for an
-	//  end-user application.
-	// +kcc:proto:field=google.cloud.alloydb.v1.ConnectionInfo.public_ip_address
-	PublicIPAddress *string `json:"publicIPAddress,omitempty"`
+// +kcc:proto=google.cloud.alloydb.v1.EncryptionConfig
+type EncryptionConfig struct {
+	// The fully-qualified resource name of the KMS key.
+	//  Each Cloud KMS key is regionalized and has the following format:
+	//  projects/[PROJECT]/locations/[REGION]/keyRings/[RING]/cryptoKeys/[KEY_NAME]
+	// +kcc:proto:field=google.cloud.alloydb.v1.EncryptionConfig.kms_key_name
+	KMSKeyName *string `json:"kmsKeyName,omitempty"`
+}
 
-	// Output only. The unique ID of the Instance.
-	// +kcc:proto:field=google.cloud.alloydb.v1.ConnectionInfo.instance_uid
-	InstanceUid *string `json:"instanceUid,omitempty"`
+// +kcc:proto=google.cloud.alloydb.v1.EncryptionInfo
+type EncryptionInfo struct {
+}
+
+// +kcc:proto=google.cloud.alloydb.v1.Backup
+type BackupObservedState struct {
+	// Output only. The name of the backup resource with the format:
+	//   * projects/{project}/locations/{region}/backups/{backup_id}
+	//  where the cluster and backup ID segments should satisfy the regex
+	//  expression `[a-z]([a-z0-9-]{0,61}[a-z0-9])?`, e.g. 1-63 characters of
+	//  lowercase letters, numbers, and dashes, starting with a letter, and ending
+	//  with a letter or number. For more details see https://google.aip.dev/122.
+	//  The prefix of the backup resource name is the name of the parent
+	//  resource:
+	//   * projects/{project}/locations/{region}
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.name
+	Name *string `json:"name,omitempty"`
+
+	// Output only. The system-generated UID of the resource. The UID is assigned
+	//  when the resource is created, and it is retained until it is deleted.
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.uid
+	Uid *string `json:"uid,omitempty"`
+
+	// Output only. Create time stamp
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.create_time
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// Output only. Update time stamp
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
+
+	// Output only. Delete time stamp
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.delete_time
+	DeleteTime *string `json:"deleteTime,omitempty"`
+
+	// Output only. The current state of the backup.
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.state
+	State *string `json:"state,omitempty"`
+
+	// Output only. The system-generated UID of the cluster which was used to
+	//  create this resource.
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.cluster_uid
+	ClusterUid *string `json:"clusterUid,omitempty"`
+
+	// Output only. Reconciling (https://google.aip.dev/128#reconciliation), if
+	//  true, indicates that the service is actively updating the resource. This
+	//  can happen due to user-triggered updates or system actions like failover or
+	//  maintenance.
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.reconciling
+	Reconciling *bool `json:"reconciling,omitempty"`
+
+	// Output only. The encryption information for the backup.
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.encryption_info
+	EncryptionInfo *EncryptionInfo `json:"encryptionInfo,omitempty"`
+
+	// Output only. The size of the backup in bytes.
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.size_bytes
+	SizeBytes *int64 `json:"sizeBytes,omitempty"`
+
+	// Output only. The time at which after the backup is eligible to be garbage
+	//  collected. It is the duration specified by the backup's retention policy,
+	//  added to the backup's create_time.
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.expiry_time
+	ExpiryTime *string `json:"expiryTime,omitempty"`
+
+	// Output only. The QuantityBasedExpiry of the backup, specified by the
+	//  backup's retention policy. Once the expiry quantity is over retention, the
+	//  backup is eligible to be garbage collected.
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.expiry_quantity
+	ExpiryQuantity *Backup_QuantityBasedExpiry `json:"expiryQuantity,omitempty"`
+
+	// Output only. Reserved for future use.
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.satisfies_pzs
+	SatisfiesPzs *bool `json:"satisfiesPzs,omitempty"`
+
+	// Output only. The database engine major version of the cluster this backup
+	//  was created from. Any restored cluster created from this backup will have
+	//  the same database version.
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.database_version
+	DatabaseVersion *string `json:"databaseVersion,omitempty"`
+}
+
+// +kcc:proto=google.cloud.alloydb.v1.Backup.QuantityBasedExpiry
+type Backup_QuantityBasedExpiryObservedState struct {
+	// Output only. The backup's position among its backups with the same source
+	//  cluster and type, by descending chronological order create time(i.e.
+	//  newest first).
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.QuantityBasedExpiry.retention_count
+	RetentionCount *int32 `json:"retentionCount,omitempty"`
+
+	// Output only. The length of the quantity-based queue, specified by the
+	//  backup's retention policy.
+	// +kcc:proto:field=google.cloud.alloydb.v1.Backup.QuantityBasedExpiry.total_retention_count
+	TotalRetentionCount *int32 `json:"totalRetentionCount,omitempty"`
+}
+
+// +kcc:proto=google.cloud.alloydb.v1.EncryptionInfo
+type EncryptionInfoObservedState struct {
+	// Output only. Type of encryption.
+	// +kcc:proto:field=google.cloud.alloydb.v1.EncryptionInfo.encryption_type
+	EncryptionType *string `json:"encryptionType,omitempty"`
+
+	// Output only. Cloud KMS key versions that are being used to protect the
+	//  database or the backup.
+	// +kcc:proto:field=google.cloud.alloydb.v1.EncryptionInfo.kms_key_versions
+	KMSKeyVersions []string `json:"kmsKeyVersions,omitempty"`
 }
--- pkg/controller/direct/alloydb/mapper.generated.go
+++ pkg/controller/direct/alloydb/mapper.generated.go
@@ -15,11 +15,131 @@
 package alloydb
 
 import (
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
 	pb "cloud.google.com/go/alloydb/apiv1/alloydbpb"
 	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/alloydb/v1alpha1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
-	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
 )
+func AlloydbBackupObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Backup) *krm.AlloydbBackupObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.AlloydbBackupObservedState{}
+	// MISSING: Name
+	// MISSING: DisplayName
+	// MISSING: Uid
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: DeleteTime
+	// MISSING: Labels
+	// MISSING: State
+	// MISSING: Type
+	// MISSING: Description
+	// MISSING: ClusterUid
+	// MISSING: ClusterName
+	// MISSING: Reconciling
+	// MISSING: EncryptionConfig
+	// MISSING: EncryptionInfo
+	// MISSING: Etag
+	// MISSING: Annotations
+	// MISSING: SizeBytes
+	// MISSING: ExpiryTime
+	// MISSING: ExpiryQuantity
+	// MISSING: SatisfiesPzs
+	// MISSING: DatabaseVersion
+	// MISSING: Tags
+	return out
+}
+func AlloydbBackupObservedState_ToProto(mapCtx *direct.MapContext, in *krm.AlloydbBackupObservedState) *pb.Backup {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Backup{}
+	// MISSING: Name
+	// MISSING: DisplayName
+	// MISSING: Uid
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: DeleteTime
+	// MISSING: Labels
+	// MISSING: State
+	// MISSING: Type
+	// MISSING: Description
+	// MISSING: ClusterUid
+	// MISSING: ClusterName
+	// MISSING: Reconciling
+	// MISSING: EncryptionConfig
+	// MISSING: EncryptionInfo
+	// MISSING: Etag
+	// MISSING: Annotations
+	// MISSING: SizeBytes
+	// MISSING: ExpiryTime
+	// MISSING: ExpiryQuantity
+	// MISSING: SatisfiesPzs
+	// MISSING: DatabaseVersion
+	// MISSING: Tags
+	return out
+}
+func AlloydbBackupSpec_FromProto(mapCtx *direct.MapContext, in *pb.Backup) *krm.AlloydbBackupSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.AlloydbBackupSpec{}
+	// MISSING: Name
+	// MISSING: DisplayName
+	// MISSING: Uid
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: DeleteTime
+	// MISSING: Labels
+	// MISSING: State
+	// MISSING: Type
+	// MISSING: Description
+	// MISSING: ClusterUid
+	// MISSING: ClusterName
+	// MISSING: Reconciling
+	// MISSING: EncryptionConfig
+	// MISSING: EncryptionInfo
+	// MISSING: Etag
+	// MISSING: Annotations
+	// MISSING: SizeBytes
+	// MISSING: ExpiryTime
+	// MISSING: ExpiryQuantity
+	// MISSING: SatisfiesPzs
+	// MISSING: DatabaseVersion
+	// MISSING: Tags
+	return out
+}
+func AlloydbBackupSpec_ToProto(mapCtx *direct.MapContext, in *krm.AlloydbBackupSpec) *pb.Backup {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Backup{}
+	// MISSING: Name
+	// MISSING: DisplayName
+	// MISSING: Uid
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: DeleteTime
+	// MISSING: Labels
+	// MISSING: State
+	// MISSING: Type
+	// MISSING: Description
+	// MISSING: ClusterUid
+	// MISSING: ClusterName
+	// MISSING: Reconciling
+	// MISSING: EncryptionConfig
+	// MISSING: EncryptionInfo
+	// MISSING: Etag
+	// MISSING: Annotations
+	// MISSING: SizeBytes
+	// MISSING: ExpiryTime
+	// MISSING: ExpiryQuantity
+	// MISSING: SatisfiesPzs
+	// MISSING: DatabaseVersion
+	// MISSING: Tags
+	return out
+}
 func AlloydbClusterObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Cluster) *krm.AlloydbClusterObservedState {
 	if in == nil {
 		return nil
@@ -224,47 +344,211 @@ func AlloydbConnectionInfoSpec_ToProto(mapCtx *direct.MapContext, in *krm.Alloyd
 	// MISSING: InstanceUid
 	return out
 }
-func ConnectionInfo_FromProto(mapCtx *direct.MapContext, in *pb.ConnectionInfo) *krm.ConnectionInfo {
+func Backup_FromProto(mapCtx *direct.MapContext, in *pb.Backup) *krm.Backup {
 	if in == nil {
 		return nil
 	}
-	out := &krm.ConnectionInfo{}
+	out := &krm.Backup{}
+	// MISSING: Name
+	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
+	// MISSING: Uid
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: DeleteTime
+	out.Labels = in.Labels
+	// MISSING: State
+	out.Type = direct.Enum_FromProto(mapCtx, in.GetType())
+	out.Description = direct.LazyPtr(in.GetDescription())
+	// MISSING: ClusterUid
+	out.ClusterName = direct.LazyPtr(in.GetClusterName())
+	// MISSING: Reconciling
+	out.EncryptionConfig = EncryptionConfig_FromProto(mapCtx, in.GetEncryptionConfig())
+	// MISSING: EncryptionInfo
+	out.Etag = direct.LazyPtr(in.GetEtag())
+	out.Annotations = in.Annotations
+	// MISSING: SizeBytes
+	// MISSING: ExpiryTime
+	// MISSING: ExpiryQuantity
+	// MISSING: SatisfiesPzs
+	// MISSING: DatabaseVersion
+	out.Tags = in.Tags
+	return out
+}
+func Backup_ToProto(mapCtx *direct.MapContext, in *krm.Backup) *pb.Backup {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Backup{}
+	// MISSING: Name
+	out.DisplayName = direct.ValueOf(in.DisplayName)
+	// MISSING: Uid
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: DeleteTime
+	out.Labels = in.Labels
+	// MISSING: State
+	out.Type = direct.Enum_ToProto[pb.Backup_Type](mapCtx, in.Type)
+	out.Description = direct.ValueOf(in.Description)
+	// MISSING: ClusterUid
+	out.ClusterName = direct.ValueOf(in.ClusterName)
+	// MISSING: Reconciling
+	out.EncryptionConfig = EncryptionConfig_ToProto(mapCtx, in.EncryptionConfig)
+	// MISSING: EncryptionInfo
+	out.Etag = direct.ValueOf(in.Etag)
+	out.Annotations = in.Annotations
+	// MISSING: SizeBytes
+	// MISSING: ExpiryTime
+	// MISSING: ExpiryQuantity
+	// MISSING: SatisfiesPzs
+	// MISSING: DatabaseVersion
+	out.Tags = in.Tags
+	return out
+}
+func BackupObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Backup) *krm.BackupObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.BackupObservedState{}
 	out.Name = direct.LazyPtr(in.GetName())
-	// MISSING: IPAddress
-	// MISSING: PublicIPAddress
-	// MISSING: InstanceUid
+	// MISSING: DisplayName
+	out.Uid = direct.LazyPtr(in.GetUid())
+	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
+	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
+	out.DeleteTime = direct.StringTimestamp_FromProto(mapCtx, in.GetDeleteTime())
+	// MISSING: Labels
+	out.State = direct.Enum_FromProto(mapCtx, in.GetState())
+	// MISSING: Type
+	// MISSING: Description
+	out.ClusterUid = direct.LazyPtr(in.GetClusterUid())
+	// MISSING: ClusterName
+	out.Reconciling = direct.LazyPtr(in.GetReconciling())
+	// MISSING: EncryptionConfig
+	out.EncryptionInfo = EncryptionInfo_FromProto(mapCtx, in.GetEncryptionInfo())
+	// MISSING: Etag
+	// MISSING: Annotations
+	out.SizeBytes = direct.LazyPtr(in.GetSizeBytes())
+	out.ExpiryTime = direct.StringTimestamp_FromProto(mapCtx, in.GetExpiryTime())
+	out.ExpiryQuantity = Backup_QuantityBasedExpiry_FromProto(mapCtx, in.GetExpiryQuantity())
+	out.SatisfiesPzs = direct.LazyPtr(in.GetSatisfiesPzs())
+	out.DatabaseVersion = direct.Enum_FromProto(mapCtx, in.GetDatabaseVersion())
+	// MISSING: Tags
 	return out
 }
-func ConnectionInfo_ToProto(mapCtx *direct.MapContext, in *krm.ConnectionInfo) *pb.ConnectionInfo {
+func BackupObservedState_ToProto(mapCtx *direct.MapContext, in *krm.BackupObservedState) *pb.Backup {
 	if in == nil {
 		return nil
 	}
-	out := &pb.ConnectionInfo{}
+	out := &pb.Backup{}
 	out.Name = direct.ValueOf(in.Name)
-	// MISSING: IPAddress
-	// MISSING: PublicIPAddress
-	// MISSING: InstanceUid
+	// MISSING: DisplayName
+	out.Uid = direct.ValueOf(in.Uid)
+	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
+	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
+	out.DeleteTime = direct.StringTimestamp_ToProto(mapCtx, in.DeleteTime)
+	// MISSING: Labels
+	out.State = direct.Enum_ToProto[pb.Backup_State](mapCtx, in.State)
+	// MISSING: Type
+	// MISSING: Description
+	out.ClusterUid = direct.ValueOf(in.ClusterUid)
+	// MISSING: ClusterName
+	out.Reconciling = direct.ValueOf(in.Reconciling)
+	// MISSING: EncryptionConfig
+	out.EncryptionInfo = EncryptionInfo_ToProto(mapCtx, in.EncryptionInfo)
+	// MISSING: Etag
+	// MISSING: Annotations
+	out.SizeBytes = direct.ValueOf(in.SizeBytes)
+	out.ExpiryTime = direct.StringTimestamp_ToProto(mapCtx, in.ExpiryTime)
+	out.ExpiryQuantity = Backup_QuantityBasedExpiry_ToProto(mapCtx, in.ExpiryQuantity)
+	out.SatisfiesPzs = direct.ValueOf(in.SatisfiesPzs)
+	out.DatabaseVersion = direct.Enum_ToProto[pb.DatabaseVersion](mapCtx, in.DatabaseVersion)
+	// MISSING: Tags
 	return out
 }
-func ConnectionInfoObservedState_FromProto(mapCtx *direct.MapContext, in *pb.ConnectionInfo) *krm.ConnectionInfoObservedState {
+func Backup_QuantityBasedExpiry_FromProto(mapCtx *direct.MapContext, in *pb.Backup_QuantityBasedExpiry) *krm.Backup_QuantityBasedExpiry {
 	if in == nil {
 		return nil
 	}
-	out := &krm.ConnectionInfoObservedState{}
-	// MISSING: Name
-	out.IPAddress = direct.LazyPtr(in.GetIpAddress())
-	out.PublicIPAddress = direct.LazyPtr(in.GetPublicIpAddress())
-	out.InstanceUid = direct.LazyPtr(in.GetInstanceUid())
+	out := &krm.Backup_QuantityBasedExpiry{}
+	// MISSING: RetentionCount
+	// MISSING: TotalRetentionCount
 	return out
 }
-func ConnectionInfoObservedState_ToProto(mapCtx *direct.MapContext, in *krm.ConnectionInfoObservedState) *pb.ConnectionInfo {
+func Backup_QuantityBasedExpiry_ToProto(mapCtx *direct.MapContext, in *krm.Backup_QuantityBasedExpiry) *pb.Backup_QuantityBasedExpiry {
 	if in == nil {
 		return nil
 	}
-	out := &pb.ConnectionInfo{}
-	// MISSING: Name
-	out.IpAddress = direct.ValueOf(in.IPAddress)
-	out.PublicIpAddress = direct.ValueOf(in.PublicIPAddress)
-	out.InstanceUid = direct.ValueOf(in.InstanceUid)
+	out := &pb.Backup_QuantityBasedExpiry{}
+	// MISSING: RetentionCount
+	// MISSING: TotalRetentionCount
+	return out
+}
+func Backup_QuantityBasedExpiryObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Backup_QuantityBasedExpiry) *krm.Backup_QuantityBasedExpiryObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Backup_QuantityBasedExpiryObservedState{}
+	out.RetentionCount = direct.LazyPtr(in.GetRetentionCount())
+	out.TotalRetentionCount = direct.LazyPtr(in.GetTotalRetentionCount())
+	return out
+}
+func Backup_QuantityBasedExpiryObservedState_ToProto(mapCtx *direct.MapContext, in *krm.Backup_QuantityBasedExpiryObservedState) *pb.Backup_QuantityBasedExpiry {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Backup_QuantityBasedExpiry{}
+	out.RetentionCount = direct.ValueOf(in.RetentionCount)
+	out.TotalRetentionCount = direct.ValueOf(in.TotalRetentionCount)
+	return out
+}
+func EncryptionConfig_FromProto(mapCtx *direct.MapContext, in *pb.EncryptionConfig) *krm.EncryptionConfig {
+	if in == nil {
+		return nil
+	}
+	out := &krm.EncryptionConfig{}
+	out.KMSKeyName = direct.LazyPtr(in.GetKmsKeyName())
+	return out
+}
+func EncryptionConfig_ToProto(mapCtx *direct.MapContext, in *krm.EncryptionConfig) *pb.EncryptionConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.EncryptionConfig{}
+	out.KmsKeyName = direct.ValueOf(in.KMSKeyName)
+	return out
+}
+func EncryptionInfo_FromProto(mapCtx *direct.MapContext, in *pb.EncryptionInfo) *krm.EncryptionInfo {
+	if in == nil {
+		return nil
+	}
+	out := &krm.EncryptionInfo{}
+	// MISSING: EncryptionType
+	// MISSING: KMSKeyVersions
+	return out
+}
+func EncryptionInfo_ToProto(mapCtx *direct.MapContext, in *krm.EncryptionInfo) *pb.EncryptionInfo {
+	if in == nil {
+		return nil
+	}
+	out := &pb.EncryptionInfo{}
+	// MISSING: EncryptionType
+	// MISSING: KMSKeyVersions
+	return out
+}
+func EncryptionInfoObservedState_FromProto(mapCtx *direct.MapContext, in *pb.EncryptionInfo) *krm.EncryptionInfoObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.EncryptionInfoObservedState{}
+	out.EncryptionType = direct.Enum_FromProto(mapCtx, in.GetEncryptionType())
+	out.KMSKeyVersions = in.KmsKeyVersions
+	return out
+}
+func EncryptionInfoObservedState_ToProto(mapCtx *direct.MapContext, in *krm.EncryptionInfoObservedState) *pb.EncryptionInfo {
+	if in == nil {
+		return nil
+	}
+	out := &pb.EncryptionInfo{}
+	out.EncryptionType = direct.Enum_ToProto[pb.EncryptionInfo_Type](mapCtx, in.EncryptionType)
+	out.KmsKeyVersions = in.KMSKeyVersions
 	return out
 }
