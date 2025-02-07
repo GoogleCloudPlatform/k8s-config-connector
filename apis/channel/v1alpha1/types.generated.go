6aa6d0a7b57552ed76eb675b0de54973ac5e5208
Your Name <yuwenma@google.com>
2025-02-07 11:56:43 +0000

    feat: add resource ChannelChannelPartnerLink

--- apis/channel/v1alpha1/types.generated.go
+++ apis/channel/v1alpha1/types.generated.go
@@ -15,30 +15,100 @@
 package v1alpha1
 
 
-// +kcc:proto=google.cloud.channel.v1.BillingAccount
-type BillingAccount struct {
+// +kcc:proto=google.cloud.channel.v1.ChannelPartnerLink
+type ChannelPartnerLink struct {
 
-	// Display name of the billing account.
-	// +kcc:proto:field=google.cloud.channel.v1.BillingAccount.display_name
-	DisplayName *string `json:"displayName,omitempty"`
+	// Required. Cloud Identity ID of the linked reseller.
+	// +kcc:proto:field=google.cloud.channel.v1.ChannelPartnerLink.reseller_cloud_identity_id
+	ResellerCloudIdentityID *string `json:"resellerCloudIdentityID,omitempty"`
+
+	// Required. State of the channel partner link.
+	// +kcc:proto:field=google.cloud.channel.v1.ChannelPartnerLink.link_state
+	LinkState *string `json:"linkState,omitempty"`
 }
 
-// +kcc:proto=google.cloud.channel.v1.BillingAccount
-type BillingAccountObservedState struct {
-	// Output only. Resource name of the billing account.
-	//  Format: accounts/{account_id}/billingAccounts/{billing_account_id}.
-	// +kcc:proto:field=google.cloud.channel.v1.BillingAccount.name
+// +kcc:proto=google.cloud.channel.v1.CloudIdentityInfo
+type CloudIdentityInfo struct {
+	// CustomerType indicates verification type needed for using services.
+	// +kcc:proto:field=google.cloud.channel.v1.CloudIdentityInfo.customer_type
+	CustomerType *string `json:"customerType,omitempty"`
+
+	// The alternate email.
+	// +kcc:proto:field=google.cloud.channel.v1.CloudIdentityInfo.alternate_email
+	AlternateEmail *string `json:"alternateEmail,omitempty"`
+
+	// Phone number associated with the Cloud Identity.
+	// +kcc:proto:field=google.cloud.channel.v1.CloudIdentityInfo.phone_number
+	PhoneNumber *string `json:"phoneNumber,omitempty"`
+
+	// Language code.
+	// +kcc:proto:field=google.cloud.channel.v1.CloudIdentityInfo.language_code
+	LanguageCode *string `json:"languageCode,omitempty"`
+
+	// Edu information about the customer.
+	// +kcc:proto:field=google.cloud.channel.v1.CloudIdentityInfo.edu_data
+	EduData *EduData `json:"eduData,omitempty"`
+}
+
+// +kcc:proto=google.cloud.channel.v1.EduData
+type EduData struct {
+	// Designated institute type of customer.
+	// +kcc:proto:field=google.cloud.channel.v1.EduData.institute_type
+	InstituteType *string `json:"instituteType,omitempty"`
+
+	// Size of the institute.
+	// +kcc:proto:field=google.cloud.channel.v1.EduData.institute_size
+	InstituteSize *string `json:"instituteSize,omitempty"`
+
+	// Web address for the edu customer's institution.
+	// +kcc:proto:field=google.cloud.channel.v1.EduData.website
+	Website *string `json:"website,omitempty"`
+}
+
+// +kcc:proto=google.cloud.channel.v1.ChannelPartnerLink
+type ChannelPartnerLinkObservedState struct {
+	// Output only. Resource name for the channel partner link, in the format
+	//  accounts/{account_id}/channelPartnerLinks/{id}.
+	// +kcc:proto:field=google.cloud.channel.v1.ChannelPartnerLink.name
 	Name *string `json:"name,omitempty"`
 
-	// Output only. The time when this billing account was created.
-	// +kcc:proto:field=google.cloud.channel.v1.BillingAccount.create_time
+	// Output only. URI of the web page where partner accepts the link invitation.
+	// +kcc:proto:field=google.cloud.channel.v1.ChannelPartnerLink.invite_link_uri
+	InviteLinkURI *string `json:"inviteLinkURI,omitempty"`
+
+	// Output only. Timestamp of when the channel partner link is created.
+	// +kcc:proto:field=google.cloud.channel.v1.ChannelPartnerLink.create_time
 	CreateTime *string `json:"createTime,omitempty"`
 
-	// Output only. The 3-letter currency code defined in ISO 4217.
-	// +kcc:proto:field=google.cloud.channel.v1.BillingAccount.currency_code
-	CurrencyCode *string `json:"currencyCode,omitempty"`
+	// Output only. Timestamp of when the channel partner link is updated.
+	// +kcc:proto:field=google.cloud.channel.v1.ChannelPartnerLink.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
 
-	// Output only. The CLDR region code.
-	// +kcc:proto:field=google.cloud.channel.v1.BillingAccount.region_code
-	RegionCode *string `json:"regionCode,omitempty"`
+	// Output only. Public identifier that a customer must use to generate a
+	//  transfer token to move to this distributor-reseller combination.
+	// +kcc:proto:field=google.cloud.channel.v1.ChannelPartnerLink.public_id
+	PublicID *string `json:"publicID,omitempty"`
+
+	// Output only. Cloud Identity info of the channel partner (IR).
+	// +kcc:proto:field=google.cloud.channel.v1.ChannelPartnerLink.channel_partner_cloud_identity_info
+	ChannelPartnerCloudIdentityInfo *CloudIdentityInfo `json:"channelPartnerCloudIdentityInfo,omitempty"`
+}
+
+// +kcc:proto=google.cloud.channel.v1.CloudIdentityInfo
+type CloudIdentityInfoObservedState struct {
+	// Output only. The primary domain name.
+	// +kcc:proto:field=google.cloud.channel.v1.CloudIdentityInfo.primary_domain
+	PrimaryDomain *string `json:"primaryDomain,omitempty"`
+
+	// Output only. Whether the domain is verified.
+	//  This field is not returned for a Customer's cloud_identity_info resource.
+	//  Partners can use the domains.get() method of the Workspace SDK's
+	//  Directory API, or listen to the PRIMARY_DOMAIN_VERIFIED Pub/Sub event in
+	//  to track domain verification of their resolve Workspace customers.
+	// +kcc:proto:field=google.cloud.channel.v1.CloudIdentityInfo.is_domain_verified
+	IsDomainVerified *bool `json:"isDomainVerified,omitempty"`
+
+	// Output only. URI of Customer's Admin console dashboard.
+	// +kcc:proto:field=google.cloud.channel.v1.CloudIdentityInfo.admin_console_uri
+	AdminConsoleURI *string `json:"adminConsoleURI,omitempty"`
 }
--- pkg/controller/direct/channel/mapper.generated.go
+++ pkg/controller/direct/channel/mapper.generated.go
@@ -15,59 +15,11 @@
 package channel
 
 import (
-	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/channel/v1alpha1"
-	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
 	pb "cloud.google.com/go/channel/apiv1/channelpb"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/channel/v1alpha1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 )
-func BillingAccount_FromProto(mapCtx *direct.MapContext, in *pb.BillingAccount) *krm.BillingAccount {
-	if in == nil {
-		return nil
-	}
-	out := &krm.BillingAccount{}
-	// MISSING: Name
-	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
-	// MISSING: CreateTime
-	// MISSING: CurrencyCode
-	// MISSING: RegionCode
-	return out
-}
-func BillingAccount_ToProto(mapCtx *direct.MapContext, in *krm.BillingAccount) *pb.BillingAccount {
-	if in == nil {
-		return nil
-	}
-	out := &pb.BillingAccount{}
-	// MISSING: Name
-	out.DisplayName = direct.ValueOf(in.DisplayName)
-	// MISSING: CreateTime
-	// MISSING: CurrencyCode
-	// MISSING: RegionCode
-	return out
-}
-func BillingAccountObservedState_FromProto(mapCtx *direct.MapContext, in *pb.BillingAccount) *krm.BillingAccountObservedState {
-	if in == nil {
-		return nil
-	}
-	out := &krm.BillingAccountObservedState{}
-	out.Name = direct.LazyPtr(in.GetName())
-	// MISSING: DisplayName
-	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
-	out.CurrencyCode = direct.LazyPtr(in.GetCurrencyCode())
-	out.RegionCode = direct.LazyPtr(in.GetRegionCode())
-	return out
-}
-func BillingAccountObservedState_ToProto(mapCtx *direct.MapContext, in *krm.BillingAccountObservedState) *pb.BillingAccount {
-	if in == nil {
-		return nil
-	}
-	out := &pb.BillingAccount{}
-	out.Name = direct.ValueOf(in.Name)
-	// MISSING: DisplayName
-	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
-	out.CurrencyCode = direct.ValueOf(in.CurrencyCode)
-	out.RegionCode = direct.ValueOf(in.RegionCode)
-	return out
-}
 func ChannelBillingAccountObservedState_FromProto(mapCtx *direct.MapContext, in *pb.BillingAccount) *krm.ChannelBillingAccountObservedState {
 	if in == nil {
 		return nil
@@ -114,5 +66,205 @@ func ChannelBillingAccountSpec_ToProto(mapCtx *direct.MapContext, in *krm.Channe
 	// MISSING: CreateTime
 	// MISSING: CurrencyCode
 	// MISSING: RegionCode
+	return out
+}
+func ChannelChannelPartnerLinkObservedState_FromProto(mapCtx *direct.MapContext, in *pb.ChannelPartnerLink) *krm.ChannelChannelPartnerLinkObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ChannelChannelPartnerLinkObservedState{}
+	// MISSING: Name
+	// MISSING: ResellerCloudIdentityID
+	// MISSING: LinkState
+	// MISSING: InviteLinkURI
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: PublicID
+	// MISSING: ChannelPartnerCloudIdentityInfo
+	return out
+}
+func ChannelChannelPartnerLinkObservedState_ToProto(mapCtx *direct.MapContext, in *krm.ChannelChannelPartnerLinkObservedState) *pb.ChannelPartnerLink {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ChannelPartnerLink{}
+	// MISSING: Name
+	// MISSING: ResellerCloudIdentityID
+	// MISSING: LinkState
+	// MISSING: InviteLinkURI
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: PublicID
+	// MISSING: ChannelPartnerCloudIdentityInfo
+	return out
+}
+func ChannelChannelPartnerLinkSpec_FromProto(mapCtx *direct.MapContext, in *pb.ChannelPartnerLink) *krm.ChannelChannelPartnerLinkSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ChannelChannelPartnerLinkSpec{}
+	// MISSING: Name
+	// MISSING: ResellerCloudIdentityID
+	// MISSING: LinkState
+	// MISSING: InviteLinkURI
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: PublicID
+	// MISSING: ChannelPartnerCloudIdentityInfo
+	return out
+}
+func ChannelChannelPartnerLinkSpec_ToProto(mapCtx *direct.MapContext, in *krm.ChannelChannelPartnerLinkSpec) *pb.ChannelPartnerLink {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ChannelPartnerLink{}
+	// MISSING: Name
+	// MISSING: ResellerCloudIdentityID
+	// MISSING: LinkState
+	// MISSING: InviteLinkURI
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: PublicID
+	// MISSING: ChannelPartnerCloudIdentityInfo
+	return out
+}
+func ChannelPartnerLink_FromProto(mapCtx *direct.MapContext, in *pb.ChannelPartnerLink) *krm.ChannelPartnerLink {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ChannelPartnerLink{}
+	// MISSING: Name
+	out.ResellerCloudIdentityID = direct.LazyPtr(in.GetResellerCloudIdentityId())
+	out.LinkState = direct.Enum_FromProto(mapCtx, in.GetLinkState())
+	// MISSING: InviteLinkURI
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: PublicID
+	// MISSING: ChannelPartnerCloudIdentityInfo
+	return out
+}
+func ChannelPartnerLink_ToProto(mapCtx *direct.MapContext, in *krm.ChannelPartnerLink) *pb.ChannelPartnerLink {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ChannelPartnerLink{}
+	// MISSING: Name
+	out.ResellerCloudIdentityId = direct.ValueOf(in.ResellerCloudIdentityID)
+	out.LinkState = direct.Enum_ToProto[pb.ChannelPartnerLinkState](mapCtx, in.LinkState)
+	// MISSING: InviteLinkURI
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: PublicID
+	// MISSING: ChannelPartnerCloudIdentityInfo
+	return out
+}
+func ChannelPartnerLinkObservedState_FromProto(mapCtx *direct.MapContext, in *pb.ChannelPartnerLink) *krm.ChannelPartnerLinkObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ChannelPartnerLinkObservedState{}
+	out.Name = direct.LazyPtr(in.GetName())
+	// MISSING: ResellerCloudIdentityID
+	// MISSING: LinkState
+	out.InviteLinkURI = direct.LazyPtr(in.GetInviteLinkUri())
+	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
+	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
+	out.PublicID = direct.LazyPtr(in.GetPublicId())
+	out.ChannelPartnerCloudIdentityInfo = CloudIdentityInfo_FromProto(mapCtx, in.GetChannelPartnerCloudIdentityInfo())
+	return out
+}
+func ChannelPartnerLinkObservedState_ToProto(mapCtx *direct.MapContext, in *krm.ChannelPartnerLinkObservedState) *pb.ChannelPartnerLink {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ChannelPartnerLink{}
+	out.Name = direct.ValueOf(in.Name)
+	// MISSING: ResellerCloudIdentityID
+	// MISSING: LinkState
+	out.InviteLinkUri = direct.ValueOf(in.InviteLinkURI)
+	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
+	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
+	out.PublicId = direct.ValueOf(in.PublicID)
+	out.ChannelPartnerCloudIdentityInfo = CloudIdentityInfo_ToProto(mapCtx, in.ChannelPartnerCloudIdentityInfo)
+	return out
+}
+func CloudIdentityInfo_FromProto(mapCtx *direct.MapContext, in *pb.CloudIdentityInfo) *krm.CloudIdentityInfo {
+	if in == nil {
+		return nil
+	}
+	out := &krm.CloudIdentityInfo{}
+	out.CustomerType = direct.Enum_FromProto(mapCtx, in.GetCustomerType())
+	// MISSING: PrimaryDomain
+	// MISSING: IsDomainVerified
+	out.AlternateEmail = direct.LazyPtr(in.GetAlternateEmail())
+	out.PhoneNumber = direct.LazyPtr(in.GetPhoneNumber())
+	out.LanguageCode = direct.LazyPtr(in.GetLanguageCode())
+	// MISSING: AdminConsoleURI
+	out.EduData = EduData_FromProto(mapCtx, in.GetEduData())
+	return out
+}
+func CloudIdentityInfo_ToProto(mapCtx *direct.MapContext, in *krm.CloudIdentityInfo) *pb.CloudIdentityInfo {
+	if in == nil {
+		return nil
+	}
+	out := &pb.CloudIdentityInfo{}
+	out.CustomerType = direct.Enum_ToProto[pb.CloudIdentityInfo_CustomerType](mapCtx, in.CustomerType)
+	// MISSING: PrimaryDomain
+	// MISSING: IsDomainVerified
+	out.AlternateEmail = direct.ValueOf(in.AlternateEmail)
+	out.PhoneNumber = direct.ValueOf(in.PhoneNumber)
+	out.LanguageCode = direct.ValueOf(in.LanguageCode)
+	// MISSING: AdminConsoleURI
+	out.EduData = EduData_ToProto(mapCtx, in.EduData)
+	return out
+}
+func CloudIdentityInfoObservedState_FromProto(mapCtx *direct.MapContext, in *pb.CloudIdentityInfo) *krm.CloudIdentityInfoObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.CloudIdentityInfoObservedState{}
+	// MISSING: CustomerType
+	out.PrimaryDomain = direct.LazyPtr(in.GetPrimaryDomain())
+	out.IsDomainVerified = direct.LazyPtr(in.GetIsDomainVerified())
+	// MISSING: AlternateEmail
+	// MISSING: PhoneNumber
+	// MISSING: LanguageCode
+	out.AdminConsoleURI = direct.LazyPtr(in.GetAdminConsoleUri())
+	// MISSING: EduData
+	return out
+}
+func CloudIdentityInfoObservedState_ToProto(mapCtx *direct.MapContext, in *krm.CloudIdentityInfoObservedState) *pb.CloudIdentityInfo {
+	if in == nil {
+		return nil
+	}
+	out := &pb.CloudIdentityInfo{}
+	// MISSING: CustomerType
+	out.PrimaryDomain = direct.ValueOf(in.PrimaryDomain)
+	out.IsDomainVerified = direct.ValueOf(in.IsDomainVerified)
+	// MISSING: AlternateEmail
+	// MISSING: PhoneNumber
+	// MISSING: LanguageCode
+	out.AdminConsoleUri = direct.ValueOf(in.AdminConsoleURI)
+	// MISSING: EduData
+	return out
+}
+func EduData_FromProto(mapCtx *direct.MapContext, in *pb.EduData) *krm.EduData {
+	if in == nil {
+		return nil
+	}
+	out := &krm.EduData{}
+	out.InstituteType = direct.Enum_FromProto(mapCtx, in.GetInstituteType())
+	out.InstituteSize = direct.Enum_FromProto(mapCtx, in.GetInstituteSize())
+	out.Website = direct.LazyPtr(in.GetWebsite())
+	return out
+}
+func EduData_ToProto(mapCtx *direct.MapContext, in *krm.EduData) *pb.EduData {
+	if in == nil {
+		return nil
+	}
+	out := &pb.EduData{}
+	out.InstituteType = direct.Enum_ToProto[pb.EduData_InstituteType](mapCtx, in.InstituteType)
+	out.InstituteSize = direct.Enum_ToProto[pb.EduData_InstituteSize](mapCtx, in.InstituteSize)
+	out.Website = direct.ValueOf(in.Website)
 	return out
 }
