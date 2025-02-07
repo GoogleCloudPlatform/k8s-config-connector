296e278817d31f0c80f32780011b7d4b5b898a69
Your Name <yuwenma@google.com>
2025-02-07 11:34:49 +0000

    feat: add resource BigqueryReadStream

--- pkg/controller/direct/bigquery/mapper.generated.go
+++ pkg/controller/direct/bigquery/mapper.generated.go
@@ -20,70 +20,6 @@ import (
 	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/bigquery/v1alpha1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 )
-func ArrowSchema_FromProto(mapCtx *direct.MapContext, in *pb.ArrowSchema) *krm.ArrowSchema {
-	if in == nil {
-		return nil
-	}
-	out := &krm.ArrowSchema{}
-	out.SerializedSchema = in.GetSerializedSchema()
-	return out
-}
-func ArrowSchema_ToProto(mapCtx *direct.MapContext, in *krm.ArrowSchema) *pb.ArrowSchema {
-	if in == nil {
-		return nil
-	}
-	out := &pb.ArrowSchema{}
-	out.SerializedSchema = in.SerializedSchema
-	return out
-}
-func ArrowSerializationOptions_FromProto(mapCtx *direct.MapContext, in *pb.ArrowSerializationOptions) *krm.ArrowSerializationOptions {
-	if in == nil {
-		return nil
-	}
-	out := &krm.ArrowSerializationOptions{}
-	out.BufferCompression = direct.Enum_FromProto(mapCtx, in.GetBufferCompression())
-	return out
-}
-func ArrowSerializationOptions_ToProto(mapCtx *direct.MapContext, in *krm.ArrowSerializationOptions) *pb.ArrowSerializationOptions {
-	if in == nil {
-		return nil
-	}
-	out := &pb.ArrowSerializationOptions{}
-	out.BufferCompression = direct.Enum_ToProto[pb.ArrowSerializationOptions_CompressionCodec](mapCtx, in.BufferCompression)
-	return out
-}
-func AvroSchema_FromProto(mapCtx *direct.MapContext, in *pb.AvroSchema) *krm.AvroSchema {
-	if in == nil {
-		return nil
-	}
-	out := &krm.AvroSchema{}
-	out.Schema = direct.LazyPtr(in.GetSchema())
-	return out
-}
-func AvroSchema_ToProto(mapCtx *direct.MapContext, in *krm.AvroSchema) *pb.AvroSchema {
-	if in == nil {
-		return nil
-	}
-	out := &pb.AvroSchema{}
-	out.Schema = direct.ValueOf(in.Schema)
-	return out
-}
-func AvroSerializationOptions_FromProto(mapCtx *direct.MapContext, in *pb.AvroSerializationOptions) *krm.AvroSerializationOptions {
-	if in == nil {
-		return nil
-	}
-	out := &krm.AvroSerializationOptions{}
-	out.EnableDisplayNameAttribute = direct.LazyPtr(in.GetEnableDisplayNameAttribute())
-	return out
-}
-func AvroSerializationOptions_ToProto(mapCtx *direct.MapContext, in *krm.AvroSerializationOptions) *pb.AvroSerializationOptions {
-	if in == nil {
-		return nil
-	}
-	out := &pb.AvroSerializationOptions{}
-	out.EnableDisplayNameAttribute = direct.ValueOf(in.EnableDisplayNameAttribute)
-	return out
-}
 func BigqueryReadSessionObservedState_FromProto(mapCtx *direct.MapContext, in *pb.ReadSession) *krm.BigqueryReadSessionObservedState {
 	if in == nil {
 		return nil
@@ -164,136 +100,36 @@ func BigqueryReadSessionSpec_ToProto(mapCtx *direct.MapContext, in *krm.Bigquery
 	// MISSING: TraceID
 	return out
 }
-func ReadSession_FromProto(mapCtx *direct.MapContext, in *pb.ReadSession) *krm.ReadSession {
+func BigqueryReadStreamObservedState_FromProto(mapCtx *direct.MapContext, in *pb.ReadStream) *krm.BigqueryReadStreamObservedState {
 	if in == nil {
 		return nil
 	}
-	out := &krm.ReadSession{}
+	out := &krm.BigqueryReadStreamObservedState{}
 	// MISSING: Name
-	// MISSING: ExpireTime
-	out.DataFormat = direct.Enum_FromProto(mapCtx, in.GetDataFormat())
-	// MISSING: AvroSchema
-	// MISSING: ArrowSchema
-	out.Table = direct.LazyPtr(in.GetTable())
-	out.TableModifiers = ReadSession_TableModifiers_FromProto(mapCtx, in.GetTableModifiers())
-	out.ReadOptions = ReadSession_TableReadOptions_FromProto(mapCtx, in.GetReadOptions())
-	// MISSING: Streams
-	// MISSING: EstimatedTotalBytesScanned
-	// MISSING: EstimatedTotalPhysicalFileSize
-	// MISSING: EstimatedRowCount
-	out.TraceID = direct.LazyPtr(in.GetTraceId())
 	return out
 }
-func ReadSession_ToProto(mapCtx *direct.MapContext, in *krm.ReadSession) *pb.ReadSession {
+func BigqueryReadStreamObservedState_ToProto(mapCtx *direct.MapContext, in *krm.BigqueryReadStreamObservedState) *pb.ReadStream {
 	if in == nil {
 		return nil
 	}
-	out := &pb.ReadSession{}
+	out := &pb.ReadStream{}
 	// MISSING: Name
-	// MISSING: ExpireTime
-	out.DataFormat = direct.Enum_ToProto[pb.DataFormat](mapCtx, in.DataFormat)
-	// MISSING: AvroSchema
-	// MISSING: ArrowSchema
-	out.Table = direct.ValueOf(in.Table)
-	out.TableModifiers = ReadSession_TableModifiers_ToProto(mapCtx, in.TableModifiers)
-	out.ReadOptions = ReadSession_TableReadOptions_ToProto(mapCtx, in.ReadOptions)
-	// MISSING: Streams
-	// MISSING: EstimatedTotalBytesScanned
-	// MISSING: EstimatedTotalPhysicalFileSize
-	// MISSING: EstimatedRowCount
-	out.TraceId = direct.ValueOf(in.TraceID)
 	return out
 }
-func ReadSessionObservedState_FromProto(mapCtx *direct.MapContext, in *pb.ReadSession) *krm.ReadSessionObservedState {
+func BigqueryReadStreamSpec_FromProto(mapCtx *direct.MapContext, in *pb.ReadStream) *krm.BigqueryReadStreamSpec {
 	if in == nil {
 		return nil
 	}
-	out := &krm.ReadSessionObservedState{}
-	out.Name = direct.LazyPtr(in.GetName())
-	out.ExpireTime = direct.StringTimestamp_FromProto(mapCtx, in.GetExpireTime())
-	// MISSING: DataFormat
-	out.AvroSchema = AvroSchema_FromProto(mapCtx, in.GetAvroSchema())
-	out.ArrowSchema = ArrowSchema_FromProto(mapCtx, in.GetArrowSchema())
-	// MISSING: Table
-	// MISSING: TableModifiers
-	// MISSING: ReadOptions
-	out.Streams = direct.Slice_FromProto(mapCtx, in.Streams, ReadStream_FromProto)
-	out.EstimatedTotalBytesScanned = direct.LazyPtr(in.GetEstimatedTotalBytesScanned())
-	out.EstimatedTotalPhysicalFileSize = direct.LazyPtr(in.GetEstimatedTotalPhysicalFileSize())
-	out.EstimatedRowCount = direct.LazyPtr(in.GetEstimatedRowCount())
-	// MISSING: TraceID
+	out := &krm.BigqueryReadStreamSpec{}
+	// MISSING: Name
 	return out
 }
-func ReadSessionObservedState_ToProto(mapCtx *direct.MapContext, in *krm.ReadSessionObservedState) *pb.ReadSession {
+func BigqueryReadStreamSpec_ToProto(mapCtx *direct.MapContext, in *krm.BigqueryReadStreamSpec) *pb.ReadStream {
 	if in == nil {
 		return nil
 	}
-	out := &pb.ReadSession{}
-	out.Name = direct.ValueOf(in.Name)
-	out.ExpireTime = direct.StringTimestamp_ToProto(mapCtx, in.ExpireTime)
-	// MISSING: DataFormat
-	if oneof := AvroSchema_ToProto(mapCtx, in.AvroSchema); oneof != nil {
-		out.Schema = &pb.ReadSession_AvroSchema{AvroSchema: oneof}
-	}
-	if oneof := ArrowSchema_ToProto(mapCtx, in.ArrowSchema); oneof != nil {
-		out.Schema = &pb.ReadSession_ArrowSchema{ArrowSchema: oneof}
-	}
-	// MISSING: Table
-	// MISSING: TableModifiers
-	// MISSING: ReadOptions
-	out.Streams = direct.Slice_ToProto(mapCtx, in.Streams, ReadStream_ToProto)
-	out.EstimatedTotalBytesScanned = direct.ValueOf(in.EstimatedTotalBytesScanned)
-	out.EstimatedTotalPhysicalFileSize = direct.ValueOf(in.EstimatedTotalPhysicalFileSize)
-	out.EstimatedRowCount = direct.ValueOf(in.EstimatedRowCount)
-	// MISSING: TraceID
-	return out
-}
-func ReadSession_TableModifiers_FromProto(mapCtx *direct.MapContext, in *pb.ReadSession_TableModifiers) *krm.ReadSession_TableModifiers {
-	if in == nil {
-		return nil
-	}
-	out := &krm.ReadSession_TableModifiers{}
-	out.SnapshotTime = direct.StringTimestamp_FromProto(mapCtx, in.GetSnapshotTime())
-	return out
-}
-func ReadSession_TableModifiers_ToProto(mapCtx *direct.MapContext, in *krm.ReadSession_TableModifiers) *pb.ReadSession_TableModifiers {
-	if in == nil {
-		return nil
-	}
-	out := &pb.ReadSession_TableModifiers{}
-	out.SnapshotTime = direct.StringTimestamp_ToProto(mapCtx, in.SnapshotTime)
-	return out
-}
-func ReadSession_TableReadOptions_FromProto(mapCtx *direct.MapContext, in *pb.ReadSession_TableReadOptions) *krm.ReadSession_TableReadOptions {
-	if in == nil {
-		return nil
-	}
-	out := &krm.ReadSession_TableReadOptions{}
-	out.SelectedFields = in.SelectedFields
-	out.RowRestriction = direct.LazyPtr(in.GetRowRestriction())
-	out.ArrowSerializationOptions = ArrowSerializationOptions_FromProto(mapCtx, in.GetArrowSerializationOptions())
-	out.AvroSerializationOptions = AvroSerializationOptions_FromProto(mapCtx, in.GetAvroSerializationOptions())
-	out.SamplePercentage = in.SamplePercentage
-	out.ResponseCompressionCodec = direct.Enum_FromProto(mapCtx, in.GetResponseCompressionCodec())
-	return out
-}
-func ReadSession_TableReadOptions_ToProto(mapCtx *direct.MapContext, in *krm.ReadSession_TableReadOptions) *pb.ReadSession_TableReadOptions {
-	if in == nil {
-		return nil
-	}
-	out := &pb.ReadSession_TableReadOptions{}
-	out.SelectedFields = in.SelectedFields
-	out.RowRestriction = direct.ValueOf(in.RowRestriction)
-	if oneof := ArrowSerializationOptions_ToProto(mapCtx, in.ArrowSerializationOptions); oneof != nil {
-		out.OutputFormatSerializationOptions = &pb.ReadSession_TableReadOptions_ArrowSerializationOptions{ArrowSerializationOptions: oneof}
-	}
-	if oneof := AvroSerializationOptions_ToProto(mapCtx, in.AvroSerializationOptions); oneof != nil {
-		out.OutputFormatSerializationOptions = &pb.ReadSession_TableReadOptions_AvroSerializationOptions{AvroSerializationOptions: oneof}
-	}
-	out.SamplePercentage = in.SamplePercentage
-	if oneof := ReadSession_TableReadOptions_ResponseCompressionCodec_ToProto(mapCtx, in.ResponseCompressionCodec); oneof != nil {
-		out.ResponseCompressionCodec = oneof
-	}
+	out := &pb.ReadStream{}
+	// MISSING: Name
 	return out
 }
 func ReadStream_FromProto(mapCtx *direct.MapContext, in *pb.ReadStream) *krm.ReadStream {
---
+++ pkg/controller/direct/bigquery/readstream_controller.go
@@ -0,0 +1,276 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bigquery
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/bigquery/v1alpha1"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/config"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/common"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/directbase"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/registry"
+
+	// TODO(contributor): Update the import with the google cloud client
+	gcp "cloud.google.com/go/bigquery/apiv1"
+
+	// TODO(contributor): Update the import with the google cloud client api protobuf
+	bigquerypb "cloud.google.com/go/bigquery/v1/bigquerypb"
+	"google.golang.org/api/option"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func init() {
+	registry.RegisterModel(krm.BigqueryReadStreamGVK, NewReadStreamModel)
+}
+
+func NewReadStreamModel(ctx context.Context, config *config.ControllerConfig) (directbase.Model, error) {
+	return &modelReadStream{config: *config}, nil
+}
+
+var _ directbase.Model = &modelReadStream{}
+
+type modelReadStream struct {
+	config config.ControllerConfig
+}
+
+func (m *modelReadStream) client(ctx context.Context) (*gcp.Client, error) {
+	var opts []option.ClientOption
+	opts, err := m.config.RESTClientOptions()
+	if err != nil {
+		return nil, err
+	}
+	gcpClient, err := gcp.NewRESTClient(ctx, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("building ReadStream client: %w", err)
+	}
+	return gcpClient, err
+}
+
+func (m *modelReadStream) AdapterForObject(ctx context.Context, reader client.Reader, u *unstructured.Unstructured) (directbase.Adapter, error) {
+	obj := &krm.BigqueryReadStream{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, &obj); err != nil {
+		return nil, fmt.Errorf("error converting to %T: %w", obj, err)
+	}
+
+	id, err := krm.NewReadStreamIdentity(ctx, reader, obj)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get bigquery GCP client
+	gcpClient, err := m.client(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &ReadStreamAdapter{
+		id:        id,
+		gcpClient: gcpClient,
+		desired:   obj,
+	}, nil
+}
+
+func (m *modelReadStream) AdapterForURL(ctx context.Context, url string) (directbase.Adapter, error) {
+	// TODO: Support URLs
+	return nil, nil
+}
+
+type ReadStreamAdapter struct {
+	id        *krm.ReadStreamIdentity
+	gcpClient *gcp.Client
+	desired   *krm.BigqueryReadStream
+	actual    *bigquerypb.ReadStream
+}
+
+var _ directbase.Adapter = &ReadStreamAdapter{}
+
+// Find retrieves the GCP resource.
+// Return true means the object is found. This triggers Adapter `Update` call.
+// Return false means the object is not found. This triggers Adapter `Create` call.
+// Return a non-nil error requeues the requests.
+func (a *ReadStreamAdapter) Find(ctx context.Context) (bool, error) {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("getting ReadStream", "name", a.id)
+
+	req := &bigquerypb.GetReadStreamRequest{Name: a.id}
+	readstreampb, err := a.gcpClient.GetReadStream(ctx, req)
+	if err != nil {
+		if direct.IsNotFound(err) {
+			return false, nil
+		}
+		return false, fmt.Errorf("getting ReadStream %q: %w", a.id, err)
+	}
+
+	a.actual = readstreampb
+	return true, nil
+}
+
+// Create creates the resource in GCP based on `spec` and update the Config Connector object `status` based on the GCP response.
+func (a *ReadStreamAdapter) Create(ctx context.Context, createOp *directbase.CreateOperation) error {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("creating ReadStream", "name", a.id)
+	mapCtx := &direct.MapContext{}
+
+	desired := a.desired.DeepCopy()
+	resource := BigqueryReadStreamSpec_ToProto(mapCtx, &desired.Spec)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+
+	// TODO(contributor): Complete the gcp "CREATE" or "INSERT" request.
+	req := &bigquerypb.CreateReadStreamRequest{
+		Parent:     a.id.Parent().String(),
+		ReadStream: resource,
+	}
+	op, err := a.gcpClient.CreateReadStream(ctx, req)
+	if err != nil {
+		return fmt.Errorf("creating ReadStream %s: %w", a.id, err)
+	}
+	created, err := op.Wait(ctx)
+	if err != nil {
+		return fmt.Errorf("ReadStream %s waiting creation: %w", a.id, err)
+	}
+	log.V(2).Info("successfully created ReadStream", "name", a.id)
+
+	status := &krm.BigqueryReadStreamStatus{}
+	status.ObservedState = BigqueryReadStreamObservedState_FromProto(mapCtx, created)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+	status.ExternalRef = &a.id.External
+	return createOp.UpdateStatus(ctx, status, nil)
+}
+
+// Update updates the resource in GCP based on `spec` and update the Config Connector object `status` based on the GCP response.
+func (a *ReadStreamAdapter) Update(ctx context.Context, updateOp *directbase.UpdateOperation) error {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("updating ReadStream", "name", a.id)
+	mapCtx := &direct.MapContext{}
+
+	desiredPb := BigqueryReadStreamSpec_ToProto(mapCtx, &a.desired.DeepCopy().Spec)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+
+	paths := []string{}
+	// Option 1: This option is good for proto that has `field_mask` for output-only, immutable, required/optional.
+	// TODO(contributor): If choosing this option, remove the "Option 2" code.
+	{
+		var err error
+		paths, err = common.CompareProtoMessage(desiredPb, a.actual, common.BasicDiff)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Option 2: manually add all mutable fields.
+	// TODO(contributor): If choosing this option, remove the "Option 1" code.
+	{
+		if !reflect.DeepEqual(a.desired.Spec.DisplayName, a.actual.DisplayName) {
+			paths = append(paths, "display_name")
+		}
+	}
+
+	if len(paths) == 0 {
+		log.V(2).Info("no field needs update", "name", a.id.External)
+		status := &krm.BigqueryReadStreamStatus{}
+		status.ObservedState = BigqueryReadStreamObservedState_FromProto(mapCtx, a.actual)
+		if mapCtx.Err() != nil {
+			return mapCtx.Err()
+		}
+		return updateOp.UpdateStatus(ctx, status, nil)
+	}
+	updateMask := &fieldmaskpb.FieldMask{
+		Paths: sets.List(paths)}
+
+	// TODO(contributor): Complete the gcp "UPDATE" or "PATCH" request.
+	req := &bigquerypb.UpdateReadStreamRequest{
+		Name:       a.id.External,
+		UpdateMask: updateMask,
+		ReadStream: desiredPb,
+	}
+	op, err := a.gcpClient.UpdateReadStream(ctx, req)
+	if err != nil {
+		return fmt.Errorf("updating ReadStream %s: %w", a.id.External, err)
+	}
+	updated, err := op.Wait(ctx)
+	if err != nil {
+		return fmt.Errorf("ReadStream %s waiting update: %w", a.id.External, err)
+	}
+	log.V(2).Info("successfully updated ReadStream", "name", a.id.External)
+
+	status := &krm.BigqueryReadStreamStatus{}
+	status.ObservedState = BigqueryReadStreamObservedState_FromProto(mapCtx, updated)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+	return updateOp.UpdateStatus(ctx, status, nil)
+}
+
+// Export maps the GCP object to a Config Connector resource `spec`.
+func (a *ReadStreamAdapter) Export(ctx context.Context) (*unstructured.Unstructured, error) {
+	if a.actual == nil {
+		return nil, fmt.Errorf("Find() not called")
+	}
+	u := &unstructured.Unstructured{}
+
+	obj := &krm.BigqueryReadStream{}
+	mapCtx := &direct.MapContext{}
+	obj.Spec = direct.ValueOf(BigqueryReadStreamSpec_FromProto(mapCtx, a.actual))
+	if mapCtx.Err() != nil {
+		return nil, mapCtx.Err()
+	}
+	obj.Spec.ProjectRef = &refs.ProjectRef{External: a.id.Parent().ProjectID}
+	obj.Spec.Location = a.id.Parent().Location
+	uObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+	if err != nil {
+		return nil, err
+	}
+
+	u.SetName(a.actual.Id)
+	u.SetGroupVersionKind(krm.BigqueryReadStreamGVK)
+
+	u.Object = uObj
+	return u, nil
+}
+
+// Delete the resource from GCP service when the corresponding Config Connector resource is deleted.
+func (a *ReadStreamAdapter) Delete(ctx context.Context, deleteOp *directbase.DeleteOperation) (bool, error) {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("deleting ReadStream", "name", a.id)
+
+	req := &bigquerypb.DeleteReadStreamRequest{Name: a.id.String()}
+	op, err := a.gcpClient.DeleteReadStream(ctx, req)
+	if err != nil {
+		return false, fmt.Errorf("deleting ReadStream %s: %w", a.id, err)
+	}
+	log.V(2).Info("successfully deleted ReadStream", "name", a.id)
+
+	err = op.Wait(ctx)
+	if err != nil {
+		return false, fmt.Errorf("waiting delete ReadStream %s: %w", a.id, err)
+	}
+	return true, nil
+}
