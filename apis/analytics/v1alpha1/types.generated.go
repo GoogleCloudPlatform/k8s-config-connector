a90cb001d1dc22f301f1e79163b6349b7d1e217d
Your Name <yuwenma@google.com>
2025-02-07 08:04:43 +0000

    feat: add resource AnalyticsProperty

--- apis/analytics/v1alpha1/types.generated.go
+++ apis/analytics/v1alpha1/types.generated.go
@@ -15,42 +15,90 @@
 package v1alpha1
 
 
-// +kcc:proto=google.analytics.admin.v1beta.Account
-type Account struct {
+// +kcc:proto=google.analytics.admin.v1beta.Property
+type Property struct {
 
-	// Required. Human-readable display name for this account.
-	// +kcc:proto:field=google.analytics.admin.v1beta.Account.display_name
+	// Immutable. The property type for this Property resource. When creating a
+	//  property, if the type is "PROPERTY_TYPE_UNSPECIFIED", then
+	//  "ORDINARY_PROPERTY" will be implied.
+	// +kcc:proto:field=google.analytics.admin.v1beta.Property.property_type
+	PropertyType *string `json:"propertyType,omitempty"`
+
+	// Immutable. Resource name of this property's logical parent.
+	//
+	//  Note: The Property-Moving UI can be used to change the parent.
+	//  Format: accounts/{account}, properties/{property}
+	//  Example: "accounts/100", "properties/101"
+	// +kcc:proto:field=google.analytics.admin.v1beta.Property.parent
+	Parent *string `json:"parent,omitempty"`
+
+	// Required. Human-readable display name for this property.
+	//
+	//  The max allowed display name length is 100 UTF-16 code units.
+	// +kcc:proto:field=google.analytics.admin.v1beta.Property.display_name
 	DisplayName *string `json:"displayName,omitempty"`
 
-	// Country of business. Must be a Unicode CLDR region code.
-	// +kcc:proto:field=google.analytics.admin.v1beta.Account.region_code
-	RegionCode *string `json:"regionCode,omitempty"`
+	// Industry associated with this property
+	//  Example: AUTOMOTIVE, FOOD_AND_DRINK
+	// +kcc:proto:field=google.analytics.admin.v1beta.Property.industry_category
+	IndustryCategory *string `json:"industryCategory,omitempty"`
+
+	// Required. Reporting Time Zone, used as the day boundary for reports,
+	//  regardless of where the data originates. If the time zone honors DST,
+	//  Analytics will automatically adjust for the changes.
+	//
+	//  NOTE: Changing the time zone only affects data going forward, and is not
+	//  applied retroactively.
+	//
+	//  Format: https://www.iana.org/time-zones
+	//  Example: "America/Los_Angeles"
+	// +kcc:proto:field=google.analytics.admin.v1beta.Property.time_zone
+	TimeZone *string `json:"timeZone,omitempty"`
+
+	// The currency type used in reports involving monetary values.
+	//
+	//
+	//  Format: https://en.wikipedia.org/wiki/ISO_4217
+	//  Examples: "USD", "EUR", "JPY"
+	// +kcc:proto:field=google.analytics.admin.v1beta.Property.currency_code
+	CurrencyCode *string `json:"currencyCode,omitempty"`
+
+	// Immutable. The resource name of the parent account
+	//  Format: accounts/{account_id}
+	//  Example: "accounts/123"
+	// +kcc:proto:field=google.analytics.admin.v1beta.Property.account
+	Account *string `json:"account,omitempty"`
 }
 
-// +kcc:proto=google.analytics.admin.v1beta.Account
-type AccountObservedState struct {
-	// Output only. Resource name of this account.
-	//  Format: accounts/{account}
-	//  Example: "accounts/100"
-	// +kcc:proto:field=google.analytics.admin.v1beta.Account.name
+// +kcc:proto=google.analytics.admin.v1beta.Property
+type PropertyObservedState struct {
+	// Output only. Resource name of this property.
+	//  Format: properties/{property_id}
+	//  Example: "properties/1000"
+	// +kcc:proto:field=google.analytics.admin.v1beta.Property.name
 	Name *string `json:"name,omitempty"`
 
-	// Output only. Time when this account was originally created.
-	// +kcc:proto:field=google.analytics.admin.v1beta.Account.create_time
+	// Output only. Time when the entity was originally created.
+	// +kcc:proto:field=google.analytics.admin.v1beta.Property.create_time
 	CreateTime *string `json:"createTime,omitempty"`
 
-	// Output only. Time when account payload fields were last updated.
-	// +kcc:proto:field=google.analytics.admin.v1beta.Account.update_time
+	// Output only. Time when entity payload fields were last updated.
+	// +kcc:proto:field=google.analytics.admin.v1beta.Property.update_time
 	UpdateTime *string `json:"updateTime,omitempty"`
 
-	// Output only. Indicates whether this Account is soft-deleted or not. Deleted
-	//  accounts are excluded from List results unless specifically requested.
-	// +kcc:proto:field=google.analytics.admin.v1beta.Account.deleted
-	Deleted *bool `json:"deleted,omitempty"`
+	// Output only. The Google Analytics service level that applies to this
+	//  property.
+	// +kcc:proto:field=google.analytics.admin.v1beta.Property.service_level
+	ServiceLevel *string `json:"serviceLevel,omitempty"`
 
-	// Output only. The URI for a Google Marketing Platform organization resource.
-	//  Only set when this account is connected to a GMP organization.
-	//  Format: marketingplatformadmin.googleapis.com/organizations/{org_id}
-	// +kcc:proto:field=google.analytics.admin.v1beta.Account.gmp_organization
-	GmpOrganization *string `json:"gmpOrganization,omitempty"`
+	// Output only. If set, the time at which this property was trashed. If not
+	//  set, then this property is not currently in the trash can.
+	// +kcc:proto:field=google.analytics.admin.v1beta.Property.delete_time
+	DeleteTime *string `json:"deleteTime,omitempty"`
+
+	// Output only. If set, the time at which this trashed property will be
+	//  permanently deleted. If not set, then this property is not currently in the
+	//  trash can and is not slated to be deleted.
+	// +kcc:proto:field=google.analytics.admin.v1beta.Property.expire_time
+	ExpireTime *string `json:"expireTime,omitempty"`
 }
--- pkg/controller/direct/analytics/mapper.generated.go
+++ pkg/controller/direct/analytics/mapper.generated.go
@@ -20,62 +20,6 @@ import (
 	pb "cloud.google.com/go/analytics/admin/apiv1beta/adminpb"
 	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/analytics/v1alpha1"
 )
-func Account_FromProto(mapCtx *direct.MapContext, in *pb.Account) *krm.Account {
-	if in == nil {
-		return nil
-	}
-	out := &krm.Account{}
-	// MISSING: Name
-	// MISSING: CreateTime
-	// MISSING: UpdateTime
-	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
-	out.RegionCode = direct.LazyPtr(in.GetRegionCode())
-	// MISSING: Deleted
-	// MISSING: GmpOrganization
-	return out
-}
-func Account_ToProto(mapCtx *direct.MapContext, in *krm.Account) *pb.Account {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Account{}
-	// MISSING: Name
-	// MISSING: CreateTime
-	// MISSING: UpdateTime
-	out.DisplayName = direct.ValueOf(in.DisplayName)
-	out.RegionCode = direct.ValueOf(in.RegionCode)
-	// MISSING: Deleted
-	// MISSING: GmpOrganization
-	return out
-}
-func AccountObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Account) *krm.AccountObservedState {
-	if in == nil {
-		return nil
-	}
-	out := &krm.AccountObservedState{}
-	out.Name = direct.LazyPtr(in.GetName())
-	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
-	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
-	// MISSING: DisplayName
-	// MISSING: RegionCode
-	out.Deleted = direct.LazyPtr(in.GetDeleted())
-	out.GmpOrganization = direct.LazyPtr(in.GetGmpOrganization())
-	return out
-}
-func AccountObservedState_ToProto(mapCtx *direct.MapContext, in *krm.AccountObservedState) *pb.Account {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Account{}
-	out.Name = direct.ValueOf(in.Name)
-	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
-	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
-	// MISSING: DisplayName
-	// MISSING: RegionCode
-	out.Deleted = direct.ValueOf(in.Deleted)
-	out.GmpOrganization = direct.ValueOf(in.GmpOrganization)
-	return out
-}
 func AnalyticsAccountObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Account) *krm.AnalyticsAccountObservedState {
 	if in == nil {
 		return nil
@@ -130,5 +74,165 @@ func AnalyticsAccountSpec_ToProto(mapCtx *direct.MapContext, in *krm.AnalyticsAc
 	// MISSING: RegionCode
 	// MISSING: Deleted
 	// MISSING: GmpOrganization
+	return out
+}
+func AnalyticsPropertyObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Property) *krm.AnalyticsPropertyObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.AnalyticsPropertyObservedState{}
+	// MISSING: Name
+	// MISSING: PropertyType
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Parent
+	// MISSING: DisplayName
+	// MISSING: IndustryCategory
+	// MISSING: TimeZone
+	// MISSING: CurrencyCode
+	// MISSING: ServiceLevel
+	// MISSING: DeleteTime
+	// MISSING: ExpireTime
+	// MISSING: Account
+	return out
+}
+func AnalyticsPropertyObservedState_ToProto(mapCtx *direct.MapContext, in *krm.AnalyticsPropertyObservedState) *pb.Property {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Property{}
+	// MISSING: Name
+	// MISSING: PropertyType
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Parent
+	// MISSING: DisplayName
+	// MISSING: IndustryCategory
+	// MISSING: TimeZone
+	// MISSING: CurrencyCode
+	// MISSING: ServiceLevel
+	// MISSING: DeleteTime
+	// MISSING: ExpireTime
+	// MISSING: Account
+	return out
+}
+func AnalyticsPropertySpec_FromProto(mapCtx *direct.MapContext, in *pb.Property) *krm.AnalyticsPropertySpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.AnalyticsPropertySpec{}
+	// MISSING: Name
+	// MISSING: PropertyType
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Parent
+	// MISSING: DisplayName
+	// MISSING: IndustryCategory
+	// MISSING: TimeZone
+	// MISSING: CurrencyCode
+	// MISSING: ServiceLevel
+	// MISSING: DeleteTime
+	// MISSING: ExpireTime
+	// MISSING: Account
+	return out
+}
+func AnalyticsPropertySpec_ToProto(mapCtx *direct.MapContext, in *krm.AnalyticsPropertySpec) *pb.Property {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Property{}
+	// MISSING: Name
+	// MISSING: PropertyType
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Parent
+	// MISSING: DisplayName
+	// MISSING: IndustryCategory
+	// MISSING: TimeZone
+	// MISSING: CurrencyCode
+	// MISSING: ServiceLevel
+	// MISSING: DeleteTime
+	// MISSING: ExpireTime
+	// MISSING: Account
+	return out
+}
+func Property_FromProto(mapCtx *direct.MapContext, in *pb.Property) *krm.Property {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Property{}
+	// MISSING: Name
+	out.PropertyType = direct.Enum_FromProto(mapCtx, in.GetPropertyType())
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	out.Parent = direct.LazyPtr(in.GetParent())
+	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
+	out.IndustryCategory = direct.Enum_FromProto(mapCtx, in.GetIndustryCategory())
+	out.TimeZone = direct.LazyPtr(in.GetTimeZone())
+	out.CurrencyCode = direct.LazyPtr(in.GetCurrencyCode())
+	// MISSING: ServiceLevel
+	// MISSING: DeleteTime
+	// MISSING: ExpireTime
+	out.Account = direct.LazyPtr(in.GetAccount())
+	return out
+}
+func Property_ToProto(mapCtx *direct.MapContext, in *krm.Property) *pb.Property {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Property{}
+	// MISSING: Name
+	out.PropertyType = direct.Enum_ToProto[pb.PropertyType](mapCtx, in.PropertyType)
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	out.Parent = direct.ValueOf(in.Parent)
+	out.DisplayName = direct.ValueOf(in.DisplayName)
+	out.IndustryCategory = direct.Enum_ToProto[pb.IndustryCategory](mapCtx, in.IndustryCategory)
+	out.TimeZone = direct.ValueOf(in.TimeZone)
+	out.CurrencyCode = direct.ValueOf(in.CurrencyCode)
+	// MISSING: ServiceLevel
+	// MISSING: DeleteTime
+	// MISSING: ExpireTime
+	out.Account = direct.ValueOf(in.Account)
+	return out
+}
+func PropertyObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Property) *krm.PropertyObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.PropertyObservedState{}
+	out.Name = direct.LazyPtr(in.GetName())
+	// MISSING: PropertyType
+	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
+	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
+	// MISSING: Parent
+	// MISSING: DisplayName
+	// MISSING: IndustryCategory
+	// MISSING: TimeZone
+	// MISSING: CurrencyCode
+	out.ServiceLevel = direct.Enum_FromProto(mapCtx, in.GetServiceLevel())
+	out.DeleteTime = direct.StringTimestamp_FromProto(mapCtx, in.GetDeleteTime())
+	out.ExpireTime = direct.StringTimestamp_FromProto(mapCtx, in.GetExpireTime())
+	// MISSING: Account
+	return out
+}
+func PropertyObservedState_ToProto(mapCtx *direct.MapContext, in *krm.PropertyObservedState) *pb.Property {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Property{}
+	out.Name = direct.ValueOf(in.Name)
+	// MISSING: PropertyType
+	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
+	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
+	// MISSING: Parent
+	// MISSING: DisplayName
+	// MISSING: IndustryCategory
+	// MISSING: TimeZone
+	// MISSING: CurrencyCode
+	out.ServiceLevel = direct.Enum_ToProto[pb.ServiceLevel](mapCtx, in.ServiceLevel)
+	out.DeleteTime = direct.StringTimestamp_ToProto(mapCtx, in.DeleteTime)
+	out.ExpireTime = direct.StringTimestamp_ToProto(mapCtx, in.ExpireTime)
+	// MISSING: Account
 	return out
 }
