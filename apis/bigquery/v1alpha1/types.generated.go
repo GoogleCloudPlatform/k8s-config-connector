026d095ba0bd084aff929b9b7bef59e2d11d9f56
Your Name <yuwenma@google.com>
2025-02-07 11:38:26 +0000

    feat: add resource BigqueryReadSession

--- apis/bigquery/v1alpha1/types.generated.go
+++ apis/bigquery/v1alpha1/types.generated.go
@@ -15,84 +15,122 @@
 package v1alpha1
 
 
-// +kcc:proto=google.cloud.bigquery.storage.v1beta1.ArrowSchema
+// +kcc:proto=google.cloud.bigquery.storage.v1beta2.ArrowSchema
 type ArrowSchema struct {
 	// IPC serialized Arrow schema.
-	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta1.ArrowSchema.serialized_schema
+	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta2.ArrowSchema.serialized_schema
 	SerializedSchema []byte `json:"serializedSchema,omitempty"`
 }
 
-// +kcc:proto=google.cloud.bigquery.storage.v1beta1.AvroSchema
+// +kcc:proto=google.cloud.bigquery.storage.v1beta2.ArrowSerializationOptions
+type ArrowSerializationOptions struct {
+	// The Arrow IPC format to use.
+	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta2.ArrowSerializationOptions.format
+	Format *string `json:"format,omitempty"`
+}
+
+// +kcc:proto=google.cloud.bigquery.storage.v1beta2.AvroSchema
 type AvroSchema struct {
 	// Json serialized schema, as described at
-	//  https://avro.apache.org/docs/1.8.1/spec.html
-	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta1.AvroSchema.schema
+	//  https://avro.apache.org/docs/1.8.1/spec.html.
+	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta2.AvroSchema.schema
 	Schema *string `json:"schema,omitempty"`
 }
 
-// +kcc:proto=google.cloud.bigquery.storage.v1beta1.ReadSession
+// +kcc:proto=google.cloud.bigquery.storage.v1beta2.ReadSession
 type ReadSession struct {
-	// Unique identifier for the session, in the form
-	//  `projects/{project_id}/locations/{location}/sessions/{session_id}`.
-	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta1.ReadSession.name
-	Name *string `json:"name,omitempty"`
 
-	// Time at which the session becomes invalid. After this time, subsequent
-	//  requests to read this Session will return errors.
-	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta1.ReadSession.expire_time
-	ExpireTime *string `json:"expireTime,omitempty"`
+	// Immutable. Data format of the output data.
+	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta2.ReadSession.data_format
+	DataFormat *string `json:"dataFormat,omitempty"`
 
-	// Avro schema.
-	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta1.ReadSession.avro_schema
-	AvroSchema *AvroSchema `json:"avroSchema,omitempty"`
+	// Immutable. Table that this ReadSession is reading from, in the form
+	//  `projects/{project_id}/datasets/{dataset_id}/tables/{table_id}
+	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta2.ReadSession.table
+	Table *string `json:"table,omitempty"`
 
-	// Arrow schema.
-	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta1.ReadSession.arrow_schema
-	ArrowSchema *ArrowSchema `json:"arrowSchema,omitempty"`
+	// Optional. Any modifiers which are applied when reading from the specified table.
+	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta2.ReadSession.table_modifiers
+	TableModifiers *ReadSession_TableModifiers `json:"tableModifiers,omitempty"`
 
-	// Streams associated with this session.
-	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta1.ReadSession.streams
-	Streams []Stream `json:"streams,omitempty"`
-
-	// Table that this ReadSession is reading from.
-	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta1.ReadSession.table_reference
-	TableReference *TableReference `json:"tableReference,omitempty"`
-
-	// Any modifiers which are applied when reading from the specified table.
-	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta1.ReadSession.table_modifiers
-	TableModifiers *TableModifiers `json:"tableModifiers,omitempty"`
-
-	// The strategy to use for distributing data among the streams.
-	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta1.ReadSession.sharding_strategy
-	ShardingStrategy *string `json:"shardingStrategy,omitempty"`
+	// Optional. Read options for this session (e.g. column selection, filters).
+	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta2.ReadSession.read_options
+	ReadOptions *ReadSession_TableReadOptions `json:"readOptions,omitempty"`
 }
 
-// +kcc:proto=google.cloud.bigquery.storage.v1beta1.Stream
-type Stream struct {
-	// Name of the stream, in the form
-	//  `projects/{project_id}/locations/{location}/streams/{stream_id}`.
-	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta1.Stream.name
-	Name *string `json:"name,omitempty"`
-}
-
-// +kcc:proto=google.cloud.bigquery.storage.v1beta1.TableModifiers
-type TableModifiers struct {
+// +kcc:proto=google.cloud.bigquery.storage.v1beta2.ReadSession.TableModifiers
+type ReadSession_TableModifiers struct {
 	// The snapshot time of the table. If not set, interpreted as now.
-	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta1.TableModifiers.snapshot_time
+	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta2.ReadSession.TableModifiers.snapshot_time
 	SnapshotTime *string `json:"snapshotTime,omitempty"`
 }
 
-// +kcc:proto=google.cloud.bigquery.storage.v1beta1.TableReference
-type TableReference struct {
-	// The assigned project ID of the project.
-	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta1.TableReference.project_id
-	ProjectID *string `json:"projectID,omitempty"`
+// +kcc:proto=google.cloud.bigquery.storage.v1beta2.ReadSession.TableReadOptions
+type ReadSession_TableReadOptions struct {
+	// Names of the fields in the table that should be read. If empty, all
+	//  fields will be read. If the specified field is a nested field, all
+	//  the sub-fields in the field will be selected. The output field order is
+	//  unrelated to the order of fields in selected_fields.
+	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta2.ReadSession.TableReadOptions.selected_fields
+	SelectedFields []string `json:"selectedFields,omitempty"`
 
-	// The ID of the dataset in the above project.
-	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta1.TableReference.dataset_id
-	DatasetID *string `json:"datasetID,omitempty"`
+	// SQL text filtering statement, similar to a WHERE clause in a query.
+	//  Aggregates are not supported.
+	//
+	//  Examples: "int_field > 5"
+	//            "date_field = CAST('2014-9-27' as DATE)"
+	//            "nullable_field is not NULL"
+	//            "st_equals(geo_field, st_geofromtext("POINT(2, 2)"))"
+	//            "numeric_field BETWEEN 1.0 AND 5.0"
+	//
+	//  Restricted to a maximum length for 1 MB.
+	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta2.ReadSession.TableReadOptions.row_restriction
+	RowRestriction *string `json:"rowRestriction,omitempty"`
 
-	// The ID of the table in the above dataset.
-	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta1.TableReference.table_id
-	TableID *string `json:"tableID,omitempty"`
+	// Optional. Options specific to the Apache Arrow output format.
+	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta2.ReadSession.TableReadOptions.arrow_serialization_options
+	ArrowSerializationOptions *ArrowSerializationOptions `json:"arrowSerializationOptions,omitempty"`
+}
+
+// +kcc:proto=google.cloud.bigquery.storage.v1beta2.ReadStream
+type ReadStream struct {
+}
+
+// +kcc:proto=google.cloud.bigquery.storage.v1beta2.ReadSession
+type ReadSessionObservedState struct {
+	// Output only. Unique identifier for the session, in the form
+	//  `projects/{project_id}/locations/{location}/sessions/{session_id}`.
+	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta2.ReadSession.name
+	Name *string `json:"name,omitempty"`
+
+	// Output only. Time at which the session becomes invalid. After this time, subsequent
+	//  requests to read this Session will return errors. The expire_time is
+	//  automatically assigned and currently cannot be specified or updated.
+	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta2.ReadSession.expire_time
+	ExpireTime *string `json:"expireTime,omitempty"`
+
+	// Output only. Avro schema.
+	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta2.ReadSession.avro_schema
+	AvroSchema *AvroSchema `json:"avroSchema,omitempty"`
+
+	// Output only. Arrow schema.
+	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta2.ReadSession.arrow_schema
+	ArrowSchema *ArrowSchema `json:"arrowSchema,omitempty"`
+
+	// Output only. A list of streams created with the session.
+	//
+	//  At least one stream is created with the session. In the future, larger
+	//  request_stream_count values *may* result in this list being unpopulated,
+	//  in that case, the user will need to use a List method to get the streams
+	//  instead, which is not yet available.
+	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta2.ReadSession.streams
+	Streams []ReadStream `json:"streams,omitempty"`
+}
+
+// +kcc:proto=google.cloud.bigquery.storage.v1beta2.ReadStream
+type ReadStreamObservedState struct {
+	// Output only. Name of the stream, in the form
+	//  `projects/{project_id}/locations/{location}/sessions/{session_id}/streams/{stream_id}`.
+	// +kcc:proto:field=google.cloud.bigquery.storage.v1beta2.ReadStream.name
+	Name *string `json:"name,omitempty"`
 }
--- pkg/controller/direct/bigquery/mapper.generated.go
+++ pkg/controller/direct/bigquery/mapper.generated.go
@@ -16,7 +16,7 @@ package bigquery
 
 import (
 	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
-	pb "cloud.google.com/go/bigquery/storage/apiv1beta1/storagepb"
+	pb "cloud.google.com/go/bigquery/storage/apiv1beta2/storagepb"
 	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/bigquery/v1alpha1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 )
@@ -34,6 +34,22 @@ func ArrowSchema_ToProto(mapCtx *direct.MapContext, in *krm.ArrowSchema) *pb.Arr
 	}
 	out := &pb.ArrowSchema{}
 	out.SerializedSchema = in.SerializedSchema
+	return out
+}
+func ArrowSerializationOptions_FromProto(mapCtx *direct.MapContext, in *pb.ArrowSerializationOptions) *krm.ArrowSerializationOptions {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ArrowSerializationOptions{}
+	out.Format = direct.Enum_FromProto(mapCtx, in.GetFormat())
+	return out
+}
+func ArrowSerializationOptions_ToProto(mapCtx *direct.MapContext, in *krm.ArrowSerializationOptions) *pb.ArrowSerializationOptions {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ArrowSerializationOptions{}
+	out.Format = direct.Enum_ToProto[pb.ArrowSerializationOptions_Format](mapCtx, in.Format)
 	return out
 }
 func AvroSchema_FromProto(mapCtx *direct.MapContext, in *pb.AvroSchema) *krm.AvroSchema {
@@ -57,14 +73,15 @@ func ReadSession_FromProto(mapCtx *direct.MapContext, in *pb.ReadSession) *krm.R
 		return nil
 	}
 	out := &krm.ReadSession{}
-	out.Name = direct.LazyPtr(in.GetName())
-	out.ExpireTime = direct.StringTimestamp_FromProto(mapCtx, in.GetExpireTime())
-	out.AvroSchema = AvroSchema_FromProto(mapCtx, in.GetAvroSchema())
-	out.ArrowSchema = ArrowSchema_FromProto(mapCtx, in.GetArrowSchema())
-	out.Streams = direct.Slice_FromProto(mapCtx, in.Streams, Stream_FromProto)
-	out.TableReference = TableReference_FromProto(mapCtx, in.GetTableReference())
-	out.TableModifiers = TableModifiers_FromProto(mapCtx, in.GetTableModifiers())
-	out.ShardingStrategy = direct.Enum_FromProto(mapCtx, in.GetShardingStrategy())
+	// MISSING: Name
+	// MISSING: ExpireTime
+	out.DataFormat = direct.Enum_FromProto(mapCtx, in.GetDataFormat())
+	// MISSING: AvroSchema
+	// MISSING: ArrowSchema
+	out.Table = direct.LazyPtr(in.GetTable())
+	out.TableModifiers = ReadSession_TableModifiers_FromProto(mapCtx, in.GetTableModifiers())
+	out.ReadOptions = ReadSession_TableReadOptions_FromProto(mapCtx, in.GetReadOptions())
+	// MISSING: Streams
 	return out
 }
 func ReadSession_ToProto(mapCtx *direct.MapContext, in *krm.ReadSession) *pb.ReadSession {
@@ -72,69 +89,118 @@ func ReadSession_ToProto(mapCtx *direct.MapContext, in *krm.ReadSession) *pb.Rea
 		return nil
 	}
 	out := &pb.ReadSession{}
+	// MISSING: Name
+	// MISSING: ExpireTime
+	out.DataFormat = direct.Enum_ToProto[pb.DataFormat](mapCtx, in.DataFormat)
+	// MISSING: AvroSchema
+	// MISSING: ArrowSchema
+	out.Table = direct.ValueOf(in.Table)
+	out.TableModifiers = ReadSession_TableModifiers_ToProto(mapCtx, in.TableModifiers)
+	out.ReadOptions = ReadSession_TableReadOptions_ToProto(mapCtx, in.ReadOptions)
+	// MISSING: Streams
+	return out
+}
+func ReadSessionObservedState_FromProto(mapCtx *direct.MapContext, in *pb.ReadSession) *krm.ReadSessionObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ReadSessionObservedState{}
+	out.Name = direct.LazyPtr(in.GetName())
+	out.ExpireTime = direct.StringTimestamp_FromProto(mapCtx, in.GetExpireTime())
+	// MISSING: DataFormat
+	out.AvroSchema = AvroSchema_FromProto(mapCtx, in.GetAvroSchema())
+	out.ArrowSchema = ArrowSchema_FromProto(mapCtx, in.GetArrowSchema())
+	// MISSING: Table
+	// MISSING: TableModifiers
+	// MISSING: ReadOptions
+	out.Streams = direct.Slice_FromProto(mapCtx, in.Streams, ReadStream_FromProto)
+	return out
+}
+func ReadSessionObservedState_ToProto(mapCtx *direct.MapContext, in *krm.ReadSessionObservedState) *pb.ReadSession {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ReadSession{}
 	out.Name = direct.ValueOf(in.Name)
 	out.ExpireTime = direct.StringTimestamp_ToProto(mapCtx, in.ExpireTime)
+	// MISSING: DataFormat
 	if oneof := AvroSchema_ToProto(mapCtx, in.AvroSchema); oneof != nil {
 		out.Schema = &pb.ReadSession_AvroSchema{AvroSchema: oneof}
 	}
 	if oneof := ArrowSchema_ToProto(mapCtx, in.ArrowSchema); oneof != nil {
 		out.Schema = &pb.ReadSession_ArrowSchema{ArrowSchema: oneof}
 	}
-	out.Streams = direct.Slice_ToProto(mapCtx, in.Streams, Stream_ToProto)
-	out.TableReference = TableReference_ToProto(mapCtx, in.TableReference)
-	out.TableModifiers = TableModifiers_ToProto(mapCtx, in.TableModifiers)
-	out.ShardingStrategy = direct.Enum_ToProto[pb.ShardingStrategy](mapCtx, in.ShardingStrategy)
+	// MISSING: Table
+	// MISSING: TableModifiers
+	// MISSING: ReadOptions
+	out.Streams = direct.Slice_ToProto(mapCtx, in.Streams, ReadStream_ToProto)
 	return out
 }
-func Stream_FromProto(mapCtx *direct.MapContext, in *pb.Stream) *krm.Stream {
+func ReadSession_TableModifiers_FromProto(mapCtx *direct.MapContext, in *pb.ReadSession_TableModifiers) *krm.ReadSession_TableModifiers {
 	if in == nil {
 		return nil
 	}
-	out := &krm.Stream{}
-	out.Name = direct.LazyPtr(in.GetName())
-	return out
-}
-func Stream_ToProto(mapCtx *direct.MapContext, in *krm.Stream) *pb.Stream {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Stream{}
-	out.Name = direct.ValueOf(in.Name)
-	return out
-}
-func TableModifiers_FromProto(mapCtx *direct.MapContext, in *pb.TableModifiers) *krm.TableModifiers {
-	if in == nil {
-		return nil
-	}
-	out := &krm.TableModifiers{}
+	out := &krm.ReadSession_TableModifiers{}
 	out.SnapshotTime = direct.StringTimestamp_FromProto(mapCtx, in.GetSnapshotTime())
 	return out
 }
-func TableModifiers_ToProto(mapCtx *direct.MapContext, in *krm.TableModifiers) *pb.TableModifiers {
+func ReadSession_TableModifiers_ToProto(mapCtx *direct.MapContext, in *krm.ReadSession_TableModifiers) *pb.ReadSession_TableModifiers {
 	if in == nil {
 		return nil
 	}
-	out := &pb.TableModifiers{}
+	out := &pb.ReadSession_TableModifiers{}
 	out.SnapshotTime = direct.StringTimestamp_ToProto(mapCtx, in.SnapshotTime)
 	return out
 }
-func TableReference_FromProto(mapCtx *direct.MapContext, in *pb.TableReference) *krm.TableReference {
+func ReadSession_TableReadOptions_FromProto(mapCtx *direct.MapContext, in *pb.ReadSession_TableReadOptions) *krm.ReadSession_TableReadOptions {
 	if in == nil {
 		return nil
 	}
-	out := &krm.TableReference{}
-	out.ProjectID = direct.LazyPtr(in.GetProjectId())
-	out.DatasetID = direct.LazyPtr(in.GetDatasetId())
-	out.TableID = direct.LazyPtr(in.GetTableId())
+	out := &krm.ReadSession_TableReadOptions{}
+	out.SelectedFields = in.SelectedFields
+	out.RowRestriction = direct.LazyPtr(in.GetRowRestriction())
+	out.ArrowSerializationOptions = ArrowSerializationOptions_FromProto(mapCtx, in.GetArrowSerializationOptions())
 	return out
 }
-func TableReference_ToProto(mapCtx *direct.MapContext, in *krm.TableReference) *pb.TableReference {
+func ReadSession_TableReadOptions_ToProto(mapCtx *direct.MapContext, in *krm.ReadSession_TableReadOptions) *pb.ReadSession_TableReadOptions {
 	if in == nil {
 		return nil
 	}
-	out := &pb.TableReference{}
-	out.ProjectId = direct.ValueOf(in.ProjectID)
-	out.DatasetId = direct.ValueOf(in.DatasetID)
-	out.TableId = direct.ValueOf(in.TableID)
+	out := &pb.ReadSession_TableReadOptions{}
+	out.SelectedFields = in.SelectedFields
+	out.RowRestriction = direct.ValueOf(in.RowRestriction)
+	out.ArrowSerializationOptions = ArrowSerializationOptions_ToProto(mapCtx, in.ArrowSerializationOptions)
+	return out
+}
+func ReadStream_FromProto(mapCtx *direct.MapContext, in *pb.ReadStream) *krm.ReadStream {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ReadStream{}
+	// MISSING: Name
+	return out
+}
+func ReadStream_ToProto(mapCtx *direct.MapContext, in *krm.ReadStream) *pb.ReadStream {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ReadStream{}
+	// MISSING: Name
+	return out
+}
+func ReadStreamObservedState_FromProto(mapCtx *direct.MapContext, in *pb.ReadStream) *krm.ReadStreamObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ReadStreamObservedState{}
+	out.Name = direct.LazyPtr(in.GetName())
+	return out
+}
+func ReadStreamObservedState_ToProto(mapCtx *direct.MapContext, in *krm.ReadStreamObservedState) *pb.ReadStream {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ReadStream{}
+	out.Name = direct.ValueOf(in.Name)
 	return out
 }
