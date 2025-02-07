7d7284d0333bd0252c622acb7d8f907a954b85d1
Your Name <yuwenma@google.com>
2025-02-07 08:05:34 +0000

    feat: add resource AnalyticsDataStream

--- apis/analytics/v1alpha1/types.generated.go
+++ apis/analytics/v1alpha1/types.generated.go
@@ -15,90 +15,121 @@
 package v1alpha1
 
 
-// +kcc:proto=google.analytics.admin.v1beta.Property
-type Property struct {
+// +kcc:proto=google.analytics.admin.v1beta.DataStream
+type DataStream struct {
+	// Data specific to web streams. Must be populated if type is
+	//  WEB_DATA_STREAM.
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.web_stream_data
+	WebStreamData *DataStream_WebStreamData `json:"webStreamData,omitempty"`
 
-	// Immutable. The property type for this Property resource. When creating a
-	//  property, if the type is "PROPERTY_TYPE_UNSPECIFIED", then
-	//  "ORDINARY_PROPERTY" will be implied.
-	// +kcc:proto:field=google.analytics.admin.v1beta.Property.property_type
-	PropertyType *string `json:"propertyType,omitempty"`
+	// Data specific to Android app streams. Must be populated if type is
+	//  ANDROID_APP_DATA_STREAM.
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.android_app_stream_data
+	AndroidAppStreamData *DataStream_AndroidAppStreamData `json:"androidAppStreamData,omitempty"`
 
-	// Immutable. Resource name of this property's logical parent.
+	// Data specific to iOS app streams. Must be populated if type is
+	//  IOS_APP_DATA_STREAM.
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.ios_app_stream_data
+	IosAppStreamData *DataStream_IosAppStreamData `json:"iosAppStreamData,omitempty"`
+
+	// Required. Immutable. The type of this DataStream resource.
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.type
+	Type *string `json:"type,omitempty"`
+
+	// Human-readable display name for the Data Stream.
 	//
-	//  Note: The Property-Moving UI can be used to change the parent.
-	//  Format: accounts/{account}, properties/{property}
-	//  Example: "accounts/100", "properties/101"
-	// +kcc:proto:field=google.analytics.admin.v1beta.Property.parent
-	Parent *string `json:"parent,omitempty"`
-
-	// Required. Human-readable display name for this property.
+	//  Required for web data streams.
 	//
-	//  The max allowed display name length is 100 UTF-16 code units.
-	// +kcc:proto:field=google.analytics.admin.v1beta.Property.display_name
+	//  The max allowed display name length is 255 UTF-16 code units.
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.display_name
 	DisplayName *string `json:"displayName,omitempty"`
-
-	// Industry associated with this property
-	//  Example: AUTOMOTIVE, FOOD_AND_DRINK
-	// +kcc:proto:field=google.analytics.admin.v1beta.Property.industry_category
-	IndustryCategory *string `json:"industryCategory,omitempty"`
-
-	// Required. Reporting Time Zone, used as the day boundary for reports,
-	//  regardless of where the data originates. If the time zone honors DST,
-	//  Analytics will automatically adjust for the changes.
-	//
-	//  NOTE: Changing the time zone only affects data going forward, and is not
-	//  applied retroactively.
-	//
-	//  Format: https://www.iana.org/time-zones
-	//  Example: "America/Los_Angeles"
-	// +kcc:proto:field=google.analytics.admin.v1beta.Property.time_zone
-	TimeZone *string `json:"timeZone,omitempty"`
-
-	// The currency type used in reports involving monetary values.
-	//
-	//
-	//  Format: https://en.wikipedia.org/wiki/ISO_4217
-	//  Examples: "USD", "EUR", "JPY"
-	// +kcc:proto:field=google.analytics.admin.v1beta.Property.currency_code
-	CurrencyCode *string `json:"currencyCode,omitempty"`
-
-	// Immutable. The resource name of the parent account
-	//  Format: accounts/{account_id}
-	//  Example: "accounts/123"
-	// +kcc:proto:field=google.analytics.admin.v1beta.Property.account
-	Account *string `json:"account,omitempty"`
 }
 
-// +kcc:proto=google.analytics.admin.v1beta.Property
-type PropertyObservedState struct {
-	// Output only. Resource name of this property.
-	//  Format: properties/{property_id}
-	//  Example: "properties/1000"
-	// +kcc:proto:field=google.analytics.admin.v1beta.Property.name
+// +kcc:proto=google.analytics.admin.v1beta.DataStream.AndroidAppStreamData
+type DataStream_AndroidAppStreamData struct {
+
+	// Immutable. The package name for the app being measured.
+	//  Example: "com.example.myandroidapp"
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.AndroidAppStreamData.package_name
+	PackageName *string `json:"packageName,omitempty"`
+}
+
+// +kcc:proto=google.analytics.admin.v1beta.DataStream.IosAppStreamData
+type DataStream_IosAppStreamData struct {
+
+	// Required. Immutable. The Apple App Store Bundle ID for the app
+	//  Example: "com.example.myiosapp"
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.IosAppStreamData.bundle_id
+	BundleID *string `json:"bundleID,omitempty"`
+}
+
+// +kcc:proto=google.analytics.admin.v1beta.DataStream.WebStreamData
+type DataStream_WebStreamData struct {
+
+	// Domain name of the web app being measured, or empty.
+	//  Example: "http://www.google.com", "https://www.google.com"
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.WebStreamData.default_uri
+	DefaultURI *string `json:"defaultURI,omitempty"`
+}
+
+// +kcc:proto=google.analytics.admin.v1beta.DataStream
+type DataStreamObservedState struct {
+	// Data specific to web streams. Must be populated if type is
+	//  WEB_DATA_STREAM.
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.web_stream_data
+	WebStreamData *DataStream_WebStreamDataObservedState `json:"webStreamData,omitempty"`
+
+	// Data specific to Android app streams. Must be populated if type is
+	//  ANDROID_APP_DATA_STREAM.
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.android_app_stream_data
+	AndroidAppStreamData *DataStream_AndroidAppStreamDataObservedState `json:"androidAppStreamData,omitempty"`
+
+	// Data specific to iOS app streams. Must be populated if type is
+	//  IOS_APP_DATA_STREAM.
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.ios_app_stream_data
+	IosAppStreamData *DataStream_IosAppStreamDataObservedState `json:"iosAppStreamData,omitempty"`
+
+	// Output only. Resource name of this Data Stream.
+	//  Format: properties/{property_id}/dataStreams/{stream_id}
+	//  Example: "properties/1000/dataStreams/2000"
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.name
 	Name *string `json:"name,omitempty"`
 
-	// Output only. Time when the entity was originally created.
-	// +kcc:proto:field=google.analytics.admin.v1beta.Property.create_time
+	// Output only. Time when this stream was originally created.
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.create_time
 	CreateTime *string `json:"createTime,omitempty"`
 
-	// Output only. Time when entity payload fields were last updated.
-	// +kcc:proto:field=google.analytics.admin.v1beta.Property.update_time
+	// Output only. Time when stream payload fields were last updated.
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.update_time
 	UpdateTime *string `json:"updateTime,omitempty"`
+}
 
-	// Output only. The Google Analytics service level that applies to this
-	//  property.
-	// +kcc:proto:field=google.analytics.admin.v1beta.Property.service_level
-	ServiceLevel *string `json:"serviceLevel,omitempty"`
+// +kcc:proto=google.analytics.admin.v1beta.DataStream.AndroidAppStreamData
+type DataStream_AndroidAppStreamDataObservedState struct {
+	// Output only. ID of the corresponding Android app in Firebase, if any.
+	//  This ID can change if the Android app is deleted and recreated.
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.AndroidAppStreamData.firebase_app_id
+	FirebaseAppID *string `json:"firebaseAppID,omitempty"`
+}
 
-	// Output only. If set, the time at which this property was trashed. If not
-	//  set, then this property is not currently in the trash can.
-	// +kcc:proto:field=google.analytics.admin.v1beta.Property.delete_time
-	DeleteTime *string `json:"deleteTime,omitempty"`
+// +kcc:proto=google.analytics.admin.v1beta.DataStream.IosAppStreamData
+type DataStream_IosAppStreamDataObservedState struct {
+	// Output only. ID of the corresponding iOS app in Firebase, if any.
+	//  This ID can change if the iOS app is deleted and recreated.
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.IosAppStreamData.firebase_app_id
+	FirebaseAppID *string `json:"firebaseAppID,omitempty"`
+}
 
-	// Output only. If set, the time at which this trashed property will be
-	//  permanently deleted. If not set, then this property is not currently in the
-	//  trash can and is not slated to be deleted.
-	// +kcc:proto:field=google.analytics.admin.v1beta.Property.expire_time
-	ExpireTime *string `json:"expireTime,omitempty"`
+// +kcc:proto=google.analytics.admin.v1beta.DataStream.WebStreamData
+type DataStream_WebStreamDataObservedState struct {
+	// Output only. Analytics Measurement ID.
+	//
+	//  Example: "G-1A2BCD345E"
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.WebStreamData.measurement_id
+	MeasurementID *string `json:"measurementID,omitempty"`
+
+	// Output only. ID of the corresponding web app in Firebase, if any.
+	//  This ID can change if the web app is deleted and recreated.
+	// +kcc:proto:field=google.analytics.admin.v1beta.DataStream.WebStreamData.firebase_app_id
+	FirebaseAppID *string `json:"firebaseAppID,omitempty"`
 }
