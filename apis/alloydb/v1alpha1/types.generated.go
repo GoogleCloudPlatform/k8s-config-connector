4c9eea72a4b67c49698b693699c12c75006fa33e
Your Name <yuwenma@google.com>
2025-02-07 10:06:35 +0000

    feat: add resource AlloydbUser

--- apis/alloydb/v1alpha1/types.generated.go
+++ apis/alloydb/v1alpha1/types.generated.go
@@ -15,32 +15,32 @@
 package v1alpha1
 
 
-// +kcc:proto=google.cloud.alloydb.v1.User
+// +kcc:proto=google.cloud.alloydb.v1beta.User
 type User struct {
 
 	// Input only. Password for the user.
-	// +kcc:proto:field=google.cloud.alloydb.v1.User.password
+	// +kcc:proto:field=google.cloud.alloydb.v1beta.User.password
 	Password *string `json:"password,omitempty"`
 
 	// Optional. List of database roles this user has.
 	//  The database role strings are subject to the PostgreSQL naming conventions.
-	// +kcc:proto:field=google.cloud.alloydb.v1.User.database_roles
+	// +kcc:proto:field=google.cloud.alloydb.v1beta.User.database_roles
 	DatabaseRoles []string `json:"databaseRoles,omitempty"`
 
 	// Optional. Type of this user.
-	// +kcc:proto:field=google.cloud.alloydb.v1.User.user_type
+	// +kcc:proto:field=google.cloud.alloydb.v1beta.User.user_type
 	UserType *string `json:"userType,omitempty"`
 
 	// Input only. If the user already exists and it has additional roles, keep
 	//  them granted.
-	// +kcc:proto:field=google.cloud.alloydb.v1.User.keep_extra_roles
+	// +kcc:proto:field=google.cloud.alloydb.v1beta.User.keep_extra_roles
 	KeepExtraRoles *bool `json:"keepExtraRoles,omitempty"`
 }
 
-// +kcc:proto=google.cloud.alloydb.v1.User
+// +kcc:proto=google.cloud.alloydb.v1beta.User
 type UserObservedState struct {
 	// Output only. Name of the resource in the form of
 	//  projects/{project}/locations/{location}/cluster/{cluster}/users/{user}.
-	// +kcc:proto:field=google.cloud.alloydb.v1.User.name
+	// +kcc:proto:field=google.cloud.alloydb.v1beta.User.name
 	Name *string `json:"name,omitempty"`
 }
