de7880c5fca5020766be4cc2fa1eb63f608d788c
Your Name <yuwenma@google.com>
2025-02-07 10:45:41 +0000

    feat: add resource AutomlColumnSpec

--- apis/automl/v1alpha1/types.generated.go
+++ apis/automl/v1alpha1/types.generated.go
@@ -15,22 +15,251 @@
 package v1alpha1
 
 
-// +kcc:proto=google.cloud.automl.v1beta1.AnnotationSpec
-type AnnotationSpec struct {
-	// Output only. Resource name of the annotation spec.
+// +kcc:proto=google.cloud.automl.v1beta1.ArrayStats
+type ArrayStats struct {
+	// Stats of all the values of all arrays, as if they were a single long
+	//  series of data. The type depends on the element type of the array.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.ArrayStats.member_stats
+	MemberStats *DataStats `json:"memberStats,omitempty"`
+}
+
+// +kcc:proto=google.cloud.automl.v1beta1.CategoryStats
+type CategoryStats struct {
+	// The statistics of the top 20 CATEGORY values, ordered by
+	//
+	//  [count][google.cloud.automl.v1beta1.CategoryStats.SingleCategoryStats.count].
+	// +kcc:proto:field=google.cloud.automl.v1beta1.CategoryStats.top_category_stats
+	TopCategoryStats []CategoryStats_SingleCategoryStats `json:"topCategoryStats,omitempty"`
+}
+
+// +kcc:proto=google.cloud.automl.v1beta1.CategoryStats.SingleCategoryStats
+type CategoryStats_SingleCategoryStats struct {
+	// The CATEGORY value.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.CategoryStats.SingleCategoryStats.value
+	Value *string `json:"value,omitempty"`
+
+	// The number of occurrences of this value in the series.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.CategoryStats.SingleCategoryStats.count
+	Count *int64 `json:"count,omitempty"`
+}
+
+// +kcc:proto=google.cloud.automl.v1beta1.ColumnSpec
+type ColumnSpec struct {
+	// Output only. The resource name of the column specs.
 	//  Form:
 	//
-	//  'projects/{project_id}/locations/{location_id}/datasets/{dataset_id}/annotationSpecs/{annotation_spec_id}'
-	// +kcc:proto:field=google.cloud.automl.v1beta1.AnnotationSpec.name
+	//  `projects/{project_id}/locations/{location_id}/datasets/{dataset_id}/tableSpecs/{table_spec_id}/columnSpecs/{column_spec_id}`
+	// +kcc:proto:field=google.cloud.automl.v1beta1.ColumnSpec.name
 	Name *string `json:"name,omitempty"`
 
-	// Required. The name of the annotation spec to show in the interface. The name can be
-	//  up to 32 characters long and must match the regexp `[a-zA-Z0-9_]+`.
-	// +kcc:proto:field=google.cloud.automl.v1beta1.AnnotationSpec.display_name
+	// The data type of elements stored in the column.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.ColumnSpec.data_type
+	DataType *DataType `json:"dataType,omitempty"`
+
+	// Output only. The name of the column to show in the interface. The name can
+	//  be up to 100 characters long and can consist only of ASCII Latin letters
+	//  A-Z and a-z, ASCII digits 0-9, underscores(_), and forward slashes(/), and
+	//  must start with a letter or a digit.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.ColumnSpec.display_name
 	DisplayName *string `json:"displayName,omitempty"`
 
-	// Output only. The number of examples in the parent dataset
-	//  labeled by the annotation spec.
-	// +kcc:proto:field=google.cloud.automl.v1beta1.AnnotationSpec.example_count
-	ExampleCount *int32 `json:"exampleCount,omitempty"`
+	// Output only. Stats of the series of values in the column.
+	//  This field may be stale, see the ancestor's
+	//  Dataset.tables_dataset_metadata.stats_update_time field
+	//  for the timestamp at which these stats were last updated.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.ColumnSpec.data_stats
+	DataStats *DataStats `json:"dataStats,omitempty"`
+
+	// Deprecated.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.ColumnSpec.top_correlated_columns
+	TopCorrelatedColumns []ColumnSpec_CorrelatedColumn `json:"topCorrelatedColumns,omitempty"`
+
+	// Used to perform consistent read-modify-write updates. If not set, a blind
+	//  "overwrite" update happens.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.ColumnSpec.etag
+	Etag *string `json:"etag,omitempty"`
+}
+
+// +kcc:proto=google.cloud.automl.v1beta1.ColumnSpec.CorrelatedColumn
+type ColumnSpec_CorrelatedColumn struct {
+	// The column_spec_id of the correlated column, which belongs to the same
+	//  table as the in-context column.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.ColumnSpec.CorrelatedColumn.column_spec_id
+	ColumnSpecID *string `json:"columnSpecID,omitempty"`
+
+	// Correlation between this and the in-context column.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.ColumnSpec.CorrelatedColumn.correlation_stats
+	CorrelationStats *CorrelationStats `json:"correlationStats,omitempty"`
+}
+
+// +kcc:proto=google.cloud.automl.v1beta1.CorrelationStats
+type CorrelationStats struct {
+	// The correlation value using the Cramer's V measure.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.CorrelationStats.cramers_v
+	CramersV *float64 `json:"cramersV,omitempty"`
+}
+
+// +kcc:proto=google.cloud.automl.v1beta1.DataStats
+type DataStats struct {
+	// The statistics for FLOAT64 DataType.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.DataStats.float64_stats
+	Float64Stats *Float64Stats `json:"float64Stats,omitempty"`
+
+	// The statistics for STRING DataType.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.DataStats.string_stats
+	StringStats *StringStats `json:"stringStats,omitempty"`
+
+	// The statistics for TIMESTAMP DataType.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.DataStats.timestamp_stats
+	TimestampStats *TimestampStats `json:"timestampStats,omitempty"`
+
+	// The statistics for ARRAY DataType.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.DataStats.array_stats
+	ArrayStats *ArrayStats `json:"arrayStats,omitempty"`
+
+	// The statistics for STRUCT DataType.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.DataStats.struct_stats
+	StructStats *StructStats `json:"structStats,omitempty"`
+
+	// The statistics for CATEGORY DataType.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.DataStats.category_stats
+	CategoryStats *CategoryStats `json:"categoryStats,omitempty"`
+
+	// The number of distinct values.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.DataStats.distinct_value_count
+	DistinctValueCount *int64 `json:"distinctValueCount,omitempty"`
+
+	// The number of values that are null.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.DataStats.null_value_count
+	NullValueCount *int64 `json:"nullValueCount,omitempty"`
+
+	// The number of values that are valid.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.DataStats.valid_value_count
+	ValidValueCount *int64 `json:"validValueCount,omitempty"`
+}
+
+// +kcc:proto=google.cloud.automl.v1beta1.DataType
+type DataType struct {
+	// If [type_code][google.cloud.automl.v1beta1.DataType.type_code] == [ARRAY][google.cloud.automl.v1beta1.TypeCode.ARRAY],
+	//  then `list_element_type` is the type of the elements.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.DataType.list_element_type
+	ListElementType *DataType `json:"listElementType,omitempty"`
+
+	// If [type_code][google.cloud.automl.v1beta1.DataType.type_code] == [STRUCT][google.cloud.automl.v1beta1.TypeCode.STRUCT], then `struct_type`
+	//  provides type information for the struct's fields.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.DataType.struct_type
+	StructType *StructType `json:"structType,omitempty"`
+
+	// If [type_code][google.cloud.automl.v1beta1.DataType.type_code] == [TIMESTAMP][google.cloud.automl.v1beta1.TypeCode.TIMESTAMP]
+	//  then `time_format` provides the format in which that time field is
+	//  expressed. The time_format must either be one of:
+	//  * `UNIX_SECONDS`
+	//  * `UNIX_MILLISECONDS`
+	//  * `UNIX_MICROSECONDS`
+	//  * `UNIX_NANOSECONDS`
+	//  (for respectively number of seconds, milliseconds, microseconds and
+	//  nanoseconds since start of the Unix epoch);
+	//  or be written in `strftime` syntax. If time_format is not set, then the
+	//  default format as described on the type_code is used.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.DataType.time_format
+	TimeFormat *string `json:"timeFormat,omitempty"`
+
+	// Required. The [TypeCode][google.cloud.automl.v1beta1.TypeCode] for this type.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.DataType.type_code
+	TypeCode *string `json:"typeCode,omitempty"`
+
+	// If true, this DataType can also be `NULL`. In .CSV files `NULL` value is
+	//  expressed as an empty string.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.DataType.nullable
+	Nullable *bool `json:"nullable,omitempty"`
+}
+
+// +kcc:proto=google.cloud.automl.v1beta1.Float64Stats
+type Float64Stats struct {
+	// The mean of the series.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.Float64Stats.mean
+	Mean *float64 `json:"mean,omitempty"`
+
+	// The standard deviation of the series.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.Float64Stats.standard_deviation
+	StandardDeviation *float64 `json:"standardDeviation,omitempty"`
+
+	// Ordered from 0 to k k-quantile values of the data series of n values.
+	//  The value at index i is, approximately, the i*n/k-th smallest value in the
+	//  series; for i = 0 and i = k these are, respectively, the min and max
+	//  values.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.Float64Stats.quantiles
+	Quantiles []float64 `json:"quantiles,omitempty"`
+
+	// Histogram buckets of the data series. Sorted by the min value of the
+	//  bucket, ascendingly, and the number of the buckets is dynamically
+	//  generated. The buckets are non-overlapping and completely cover whole
+	//  FLOAT64 range with min of first bucket being `"-Infinity"`, and max of
+	//  the last one being `"Infinity"`.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.Float64Stats.histogram_buckets
+	HistogramBuckets []Float64Stats_HistogramBucket `json:"histogramBuckets,omitempty"`
+}
+
+// +kcc:proto=google.cloud.automl.v1beta1.Float64Stats.HistogramBucket
+type Float64Stats_HistogramBucket struct {
+	// The minimum value of the bucket, inclusive.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.Float64Stats.HistogramBucket.min
+	Min *float64 `json:"min,omitempty"`
+
+	// The maximum value of the bucket, exclusive unless max = `"Infinity"`, in
+	//  which case it's inclusive.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.Float64Stats.HistogramBucket.max
+	Max *float64 `json:"max,omitempty"`
+
+	// The number of data values that are in the bucket, i.e. are between
+	//  min and max values.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.Float64Stats.HistogramBucket.count
+	Count *int64 `json:"count,omitempty"`
+}
+
+// +kcc:proto=google.cloud.automl.v1beta1.StringStats
+type StringStats struct {
+	// The statistics of the top 20 unigrams, ordered by
+	//  [count][google.cloud.automl.v1beta1.StringStats.UnigramStats.count].
+	// +kcc:proto:field=google.cloud.automl.v1beta1.StringStats.top_unigram_stats
+	TopUnigramStats []StringStats_UnigramStats `json:"topUnigramStats,omitempty"`
+}
+
+// +kcc:proto=google.cloud.automl.v1beta1.StringStats.UnigramStats
+type StringStats_UnigramStats struct {
+	// The unigram.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.StringStats.UnigramStats.value
+	Value *string `json:"value,omitempty"`
+
+	// The number of occurrences of this unigram in the series.
+	// +kcc:proto:field=google.cloud.automl.v1beta1.StringStats.UnigramStats.count
+	Count *int64 `json:"count,omitempty"`
+}
+
+// +kcc:proto=google.cloud.automl.v1beta1.StructStats
+type StructStats struct {
+
+	// TODO: unsupported map type with key string and value message
+
+}
+
+// +kcc:proto=google.cloud.automl.v1beta1.StructType
+type StructType struct {
+
+	// TODO: unsupported map type with key string and value message
+
+}
+
+// +kcc:proto=google.cloud.automl.v1beta1.TimestampStats
+type TimestampStats struct {
+
+	// TODO: unsupported map type with key string and value message
+
+}
+
+// +kcc:proto=google.cloud.automl.v1beta1.TimestampStats.GranularStats
+type TimestampStats_GranularStats struct {
+
+	// TODO: unsupported map type with key int32 and value int64
+
 }
--- pkg/controller/direct/automl/mapper.generated.go
+++ pkg/controller/direct/automl/mapper.generated.go
@@ -20,23 +20,379 @@ import (
 	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/automl/v1alpha1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 )
-func AnnotationSpec_FromProto(mapCtx *direct.MapContext, in *pb.AnnotationSpec) *krm.AnnotationSpec {
+func ArrayStats_FromProto(mapCtx *direct.MapContext, in *pb.ArrayStats) *krm.ArrayStats {
 	if in == nil {
 		return nil
 	}
-	out := &krm.AnnotationSpec{}
-	out.Name = direct.LazyPtr(in.GetName())
-	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
-	out.ExampleCount = direct.LazyPtr(in.GetExampleCount())
+	out := &krm.ArrayStats{}
+	out.MemberStats = DataStats_FromProto(mapCtx, in.GetMemberStats())
 	return out
 }
-func AnnotationSpec_ToProto(mapCtx *direct.MapContext, in *krm.AnnotationSpec) *pb.AnnotationSpec {
+func ArrayStats_ToProto(mapCtx *direct.MapContext, in *krm.ArrayStats) *pb.ArrayStats {
 	if in == nil {
 		return nil
 	}
-	out := &pb.AnnotationSpec{}
+	out := &pb.ArrayStats{}
+	out.MemberStats = DataStats_ToProto(mapCtx, in.MemberStats)
+	return out
+}
+func AutomlColumnSpecObservedState_FromProto(mapCtx *direct.MapContext, in *pb.ColumnSpec) *krm.AutomlColumnSpecObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.AutomlColumnSpecObservedState{}
+	// MISSING: Name
+	// MISSING: DataType
+	// MISSING: DisplayName
+	// MISSING: DataStats
+	// MISSING: TopCorrelatedColumns
+	// MISSING: Etag
+	return out
+}
+func AutomlColumnSpecObservedState_ToProto(mapCtx *direct.MapContext, in *krm.AutomlColumnSpecObservedState) *pb.ColumnSpec {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ColumnSpec{}
+	// MISSING: Name
+	// MISSING: DataType
+	// MISSING: DisplayName
+	// MISSING: DataStats
+	// MISSING: TopCorrelatedColumns
+	// MISSING: Etag
+	return out
+}
+func AutomlColumnSpecSpec_FromProto(mapCtx *direct.MapContext, in *pb.ColumnSpec) *krm.AutomlColumnSpecSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.AutomlColumnSpecSpec{}
+	// MISSING: Name
+	// MISSING: DataType
+	// MISSING: DisplayName
+	// MISSING: DataStats
+	// MISSING: TopCorrelatedColumns
+	// MISSING: Etag
+	return out
+}
+func AutomlColumnSpecSpec_ToProto(mapCtx *direct.MapContext, in *krm.AutomlColumnSpecSpec) *pb.ColumnSpec {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ColumnSpec{}
+	// MISSING: Name
+	// MISSING: DataType
+	// MISSING: DisplayName
+	// MISSING: DataStats
+	// MISSING: TopCorrelatedColumns
+	// MISSING: Etag
+	return out
+}
+func CategoryStats_FromProto(mapCtx *direct.MapContext, in *pb.CategoryStats) *krm.CategoryStats {
+	if in == nil {
+		return nil
+	}
+	out := &krm.CategoryStats{}
+	out.TopCategoryStats = direct.Slice_FromProto(mapCtx, in.TopCategoryStats, CategoryStats_SingleCategoryStats_FromProto)
+	return out
+}
+func CategoryStats_ToProto(mapCtx *direct.MapContext, in *krm.CategoryStats) *pb.CategoryStats {
+	if in == nil {
+		return nil
+	}
+	out := &pb.CategoryStats{}
+	out.TopCategoryStats = direct.Slice_ToProto(mapCtx, in.TopCategoryStats, CategoryStats_SingleCategoryStats_ToProto)
+	return out
+}
+func CategoryStats_SingleCategoryStats_FromProto(mapCtx *direct.MapContext, in *pb.CategoryStats_SingleCategoryStats) *krm.CategoryStats_SingleCategoryStats {
+	if in == nil {
+		return nil
+	}
+	out := &krm.CategoryStats_SingleCategoryStats{}
+	out.Value = direct.LazyPtr(in.GetValue())
+	out.Count = direct.LazyPtr(in.GetCount())
+	return out
+}
+func CategoryStats_SingleCategoryStats_ToProto(mapCtx *direct.MapContext, in *krm.CategoryStats_SingleCategoryStats) *pb.CategoryStats_SingleCategoryStats {
+	if in == nil {
+		return nil
+	}
+	out := &pb.CategoryStats_SingleCategoryStats{}
+	out.Value = direct.ValueOf(in.Value)
+	out.Count = direct.ValueOf(in.Count)
+	return out
+}
+func ColumnSpec_FromProto(mapCtx *direct.MapContext, in *pb.ColumnSpec) *krm.ColumnSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ColumnSpec{}
+	out.Name = direct.LazyPtr(in.GetName())
+	out.DataType = DataType_FromProto(mapCtx, in.GetDataType())
+	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
+	out.DataStats = DataStats_FromProto(mapCtx, in.GetDataStats())
+	out.TopCorrelatedColumns = direct.Slice_FromProto(mapCtx, in.TopCorrelatedColumns, ColumnSpec_CorrelatedColumn_FromProto)
+	out.Etag = direct.LazyPtr(in.GetEtag())
+	return out
+}
+func ColumnSpec_ToProto(mapCtx *direct.MapContext, in *krm.ColumnSpec) *pb.ColumnSpec {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ColumnSpec{}
 	out.Name = direct.ValueOf(in.Name)
+	out.DataType = DataType_ToProto(mapCtx, in.DataType)
 	out.DisplayName = direct.ValueOf(in.DisplayName)
-	out.ExampleCount = direct.ValueOf(in.ExampleCount)
+	out.DataStats = DataStats_ToProto(mapCtx, in.DataStats)
+	out.TopCorrelatedColumns = direct.Slice_ToProto(mapCtx, in.TopCorrelatedColumns, ColumnSpec_CorrelatedColumn_ToProto)
+	out.Etag = direct.ValueOf(in.Etag)
+	return out
+}
+func ColumnSpec_CorrelatedColumn_FromProto(mapCtx *direct.MapContext, in *pb.ColumnSpec_CorrelatedColumn) *krm.ColumnSpec_CorrelatedColumn {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ColumnSpec_CorrelatedColumn{}
+	out.ColumnSpecID = direct.LazyPtr(in.GetColumnSpecId())
+	out.CorrelationStats = CorrelationStats_FromProto(mapCtx, in.GetCorrelationStats())
+	return out
+}
+func ColumnSpec_CorrelatedColumn_ToProto(mapCtx *direct.MapContext, in *krm.ColumnSpec_CorrelatedColumn) *pb.ColumnSpec_CorrelatedColumn {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ColumnSpec_CorrelatedColumn{}
+	out.ColumnSpecId = direct.ValueOf(in.ColumnSpecID)
+	out.CorrelationStats = CorrelationStats_ToProto(mapCtx, in.CorrelationStats)
+	return out
+}
+func CorrelationStats_FromProto(mapCtx *direct.MapContext, in *pb.CorrelationStats) *krm.CorrelationStats {
+	if in == nil {
+		return nil
+	}
+	out := &krm.CorrelationStats{}
+	out.CramersV = direct.LazyPtr(in.GetCramersV())
+	return out
+}
+func CorrelationStats_ToProto(mapCtx *direct.MapContext, in *krm.CorrelationStats) *pb.CorrelationStats {
+	if in == nil {
+		return nil
+	}
+	out := &pb.CorrelationStats{}
+	out.CramersV = direct.ValueOf(in.CramersV)
+	return out
+}
+func DataStats_FromProto(mapCtx *direct.MapContext, in *pb.DataStats) *krm.DataStats {
+	if in == nil {
+		return nil
+	}
+	out := &krm.DataStats{}
+	out.Float64Stats = Float64Stats_FromProto(mapCtx, in.GetFloat64Stats())
+	out.StringStats = StringStats_FromProto(mapCtx, in.GetStringStats())
+	out.TimestampStats = TimestampStats_FromProto(mapCtx, in.GetTimestampStats())
+	out.ArrayStats = ArrayStats_FromProto(mapCtx, in.GetArrayStats())
+	out.StructStats = StructStats_FromProto(mapCtx, in.GetStructStats())
+	out.CategoryStats = CategoryStats_FromProto(mapCtx, in.GetCategoryStats())
+	out.DistinctValueCount = direct.LazyPtr(in.GetDistinctValueCount())
+	out.NullValueCount = direct.LazyPtr(in.GetNullValueCount())
+	out.ValidValueCount = direct.LazyPtr(in.GetValidValueCount())
+	return out
+}
+func DataStats_ToProto(mapCtx *direct.MapContext, in *krm.DataStats) *pb.DataStats {
+	if in == nil {
+		return nil
+	}
+	out := &pb.DataStats{}
+	if oneof := Float64Stats_ToProto(mapCtx, in.Float64Stats); oneof != nil {
+		out.Stats = &pb.DataStats_Float64Stats{Float64Stats: oneof}
+	}
+	if oneof := StringStats_ToProto(mapCtx, in.StringStats); oneof != nil {
+		out.Stats = &pb.DataStats_StringStats{StringStats: oneof}
+	}
+	if oneof := TimestampStats_ToProto(mapCtx, in.TimestampStats); oneof != nil {
+		out.Stats = &pb.DataStats_TimestampStats{TimestampStats: oneof}
+	}
+	if oneof := ArrayStats_ToProto(mapCtx, in.ArrayStats); oneof != nil {
+		out.Stats = &pb.DataStats_ArrayStats{ArrayStats: oneof}
+	}
+	if oneof := StructStats_ToProto(mapCtx, in.StructStats); oneof != nil {
+		out.Stats = &pb.DataStats_StructStats{StructStats: oneof}
+	}
+	if oneof := CategoryStats_ToProto(mapCtx, in.CategoryStats); oneof != nil {
+		out.Stats = &pb.DataStats_CategoryStats{CategoryStats: oneof}
+	}
+	out.DistinctValueCount = direct.ValueOf(in.DistinctValueCount)
+	out.NullValueCount = direct.ValueOf(in.NullValueCount)
+	out.ValidValueCount = direct.ValueOf(in.ValidValueCount)
+	return out
+}
+func DataType_FromProto(mapCtx *direct.MapContext, in *pb.DataType) *krm.DataType {
+	if in == nil {
+		return nil
+	}
+	out := &krm.DataType{}
+	out.ListElementType = DataType_FromProto(mapCtx, in.GetListElementType())
+	out.StructType = StructType_FromProto(mapCtx, in.GetStructType())
+	out.TimeFormat = direct.LazyPtr(in.GetTimeFormat())
+	out.TypeCode = direct.Enum_FromProto(mapCtx, in.GetTypeCode())
+	out.Nullable = direct.LazyPtr(in.GetNullable())
+	return out
+}
+func DataType_ToProto(mapCtx *direct.MapContext, in *krm.DataType) *pb.DataType {
+	if in == nil {
+		return nil
+	}
+	out := &pb.DataType{}
+	if oneof := DataType_ToProto(mapCtx, in.ListElementType); oneof != nil {
+		out.Details = &pb.DataType_ListElementType{ListElementType: oneof}
+	}
+	if oneof := StructType_ToProto(mapCtx, in.StructType); oneof != nil {
+		out.Details = &pb.DataType_StructType{StructType: oneof}
+	}
+	if oneof := DataType_TimeFormat_ToProto(mapCtx, in.TimeFormat); oneof != nil {
+		out.Details = oneof
+	}
+	out.TypeCode = direct.Enum_ToProto[pb.TypeCode](mapCtx, in.TypeCode)
+	out.Nullable = direct.ValueOf(in.Nullable)
+	return out
+}
+func Float64Stats_FromProto(mapCtx *direct.MapContext, in *pb.Float64Stats) *krm.Float64Stats {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Float64Stats{}
+	out.Mean = direct.LazyPtr(in.GetMean())
+	out.StandardDeviation = direct.LazyPtr(in.GetStandardDeviation())
+	out.Quantiles = in.Quantiles
+	out.HistogramBuckets = direct.Slice_FromProto(mapCtx, in.HistogramBuckets, Float64Stats_HistogramBucket_FromProto)
+	return out
+}
+func Float64Stats_ToProto(mapCtx *direct.MapContext, in *krm.Float64Stats) *pb.Float64Stats {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Float64Stats{}
+	out.Mean = direct.ValueOf(in.Mean)
+	out.StandardDeviation = direct.ValueOf(in.StandardDeviation)
+	out.Quantiles = in.Quantiles
+	out.HistogramBuckets = direct.Slice_ToProto(mapCtx, in.HistogramBuckets, Float64Stats_HistogramBucket_ToProto)
+	return out
+}
+func Float64Stats_HistogramBucket_FromProto(mapCtx *direct.MapContext, in *pb.Float64Stats_HistogramBucket) *krm.Float64Stats_HistogramBucket {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Float64Stats_HistogramBucket{}
+	out.Min = direct.LazyPtr(in.GetMin())
+	out.Max = direct.LazyPtr(in.GetMax())
+	out.Count = direct.LazyPtr(in.GetCount())
+	return out
+}
+func Float64Stats_HistogramBucket_ToProto(mapCtx *direct.MapContext, in *krm.Float64Stats_HistogramBucket) *pb.Float64Stats_HistogramBucket {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Float64Stats_HistogramBucket{}
+	out.Min = direct.ValueOf(in.Min)
+	out.Max = direct.ValueOf(in.Max)
+	out.Count = direct.ValueOf(in.Count)
+	return out
+}
+func StringStats_FromProto(mapCtx *direct.MapContext, in *pb.StringStats) *krm.StringStats {
+	if in == nil {
+		return nil
+	}
+	out := &krm.StringStats{}
+	out.TopUnigramStats = direct.Slice_FromProto(mapCtx, in.TopUnigramStats, StringStats_UnigramStats_FromProto)
+	return out
+}
+func StringStats_ToProto(mapCtx *direct.MapContext, in *krm.StringStats) *pb.StringStats {
+	if in == nil {
+		return nil
+	}
+	out := &pb.StringStats{}
+	out.TopUnigramStats = direct.Slice_ToProto(mapCtx, in.TopUnigramStats, StringStats_UnigramStats_ToProto)
+	return out
+}
+func StringStats_UnigramStats_FromProto(mapCtx *direct.MapContext, in *pb.StringStats_UnigramStats) *krm.StringStats_UnigramStats {
+	if in == nil {
+		return nil
+	}
+	out := &krm.StringStats_UnigramStats{}
+	out.Value = direct.LazyPtr(in.GetValue())
+	out.Count = direct.LazyPtr(in.GetCount())
+	return out
+}
+func StringStats_UnigramStats_ToProto(mapCtx *direct.MapContext, in *krm.StringStats_UnigramStats) *pb.StringStats_UnigramStats {
+	if in == nil {
+		return nil
+	}
+	out := &pb.StringStats_UnigramStats{}
+	out.Value = direct.ValueOf(in.Value)
+	out.Count = direct.ValueOf(in.Count)
+	return out
+}
+func StructStats_FromProto(mapCtx *direct.MapContext, in *pb.StructStats) *krm.StructStats {
+	if in == nil {
+		return nil
+	}
+	out := &krm.StructStats{}
+	// MISSING: FieldStats
+	return out
+}
+func StructStats_ToProto(mapCtx *direct.MapContext, in *krm.StructStats) *pb.StructStats {
+	if in == nil {
+		return nil
+	}
+	out := &pb.StructStats{}
+	// MISSING: FieldStats
+	return out
+}
+func StructType_FromProto(mapCtx *direct.MapContext, in *pb.StructType) *krm.StructType {
+	if in == nil {
+		return nil
+	}
+	out := &krm.StructType{}
+	// MISSING: Fields
+	return out
+}
+func StructType_ToProto(mapCtx *direct.MapContext, in *krm.StructType) *pb.StructType {
+	if in == nil {
+		return nil
+	}
+	out := &pb.StructType{}
+	// MISSING: Fields
+	return out
+}
+func TimestampStats_FromProto(mapCtx *direct.MapContext, in *pb.TimestampStats) *krm.TimestampStats {
+	if in == nil {
+		return nil
+	}
+	out := &krm.TimestampStats{}
+	// MISSING: GranularStats
+	return out
+}
+func TimestampStats_ToProto(mapCtx *direct.MapContext, in *krm.TimestampStats) *pb.TimestampStats {
+	if in == nil {
+		return nil
+	}
+	out := &pb.TimestampStats{}
+	// MISSING: GranularStats
+	return out
+}
+func TimestampStats_GranularStats_FromProto(mapCtx *direct.MapContext, in *pb.TimestampStats_GranularStats) *krm.TimestampStats_GranularStats {
+	if in == nil {
+		return nil
+	}
+	out := &krm.TimestampStats_GranularStats{}
+	// MISSING: Buckets
+	return out
+}
+func TimestampStats_GranularStats_ToProto(mapCtx *direct.MapContext, in *krm.TimestampStats_GranularStats) *pb.TimestampStats_GranularStats {
+	if in == nil {
+		return nil
+	}
+	out := &pb.TimestampStats_GranularStats{}
+	// MISSING: Buckets
 	return out
 }
