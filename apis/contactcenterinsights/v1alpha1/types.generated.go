35714c8e633f9f134286c58f282cd8d50b44df26
Your Name <yuwenma@google.com>
2025-02-08 01:52:02 +0000

    feat: add resource ContactcenterinsightsEncryptionSpec

---
+++ apis/contactcenterinsights/v1alpha1/types.generated.go
@@ -0,0 +1,33 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+
+// +kcc:proto=google.cloud.contactcenterinsights.v1.EncryptionSpec
+type EncryptionSpec struct {
+	// Immutable. The resource name of the encryption key specification resource.
+	//  Format:
+	//  projects/{project}/locations/{location}/encryptionSpec
+	// +kcc:proto:field=google.cloud.contactcenterinsights.v1.EncryptionSpec.name
+	Name *string `json:"name,omitempty"`
+
+	// Required. The name of customer-managed encryption key that is used to
+	//  secure a resource and its sub-resources. If empty, the resource is secured
+	//  by our default encryption key. Only the key in the same location as this
+	//  resource is allowed to be used for encryption. Format:
+	//  `projects/{project}/locations/{location}/keyRings/{keyRing}/cryptoKeys/{key}`
+	// +kcc:proto:field=google.cloud.contactcenterinsights.v1.EncryptionSpec.kms_key
+	KMSKey *string `json:"kmsKey,omitempty"`
+}
---
+++ pkg/controller/direct/contactcenterinsights/mapper.generated.go
@@ -0,0 +1,76 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package contactcenterinsights
+
+import (
+	pb "cloud.google.com/go/contactcenterinsights/apiv1/contactcenterinsightspb"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/contactcenterinsights/v1alpha1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+)
+func ContactcenterinsightsEncryptionSpecObservedState_FromProto(mapCtx *direct.MapContext, in *pb.EncryptionSpec) *krm.ContactcenterinsightsEncryptionSpecObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ContactcenterinsightsEncryptionSpecObservedState{}
+	// MISSING: Name
+	// MISSING: KMSKey
+	return out
+}
+func ContactcenterinsightsEncryptionSpecObservedState_ToProto(mapCtx *direct.MapContext, in *krm.ContactcenterinsightsEncryptionSpecObservedState) *pb.EncryptionSpec {
+	if in == nil {
+		return nil
+	}
+	out := &pb.EncryptionSpec{}
+	// MISSING: Name
+	// MISSING: KMSKey
+	return out
+}
+func ContactcenterinsightsEncryptionSpecSpec_FromProto(mapCtx *direct.MapContext, in *pb.EncryptionSpec) *krm.ContactcenterinsightsEncryptionSpecSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ContactcenterinsightsEncryptionSpecSpec{}
+	// MISSING: Name
+	// MISSING: KMSKey
+	return out
+}
+func ContactcenterinsightsEncryptionSpecSpec_ToProto(mapCtx *direct.MapContext, in *krm.ContactcenterinsightsEncryptionSpecSpec) *pb.EncryptionSpec {
+	if in == nil {
+		return nil
+	}
+	out := &pb.EncryptionSpec{}
+	// MISSING: Name
+	// MISSING: KMSKey
+	return out
+}
+func EncryptionSpec_FromProto(mapCtx *direct.MapContext, in *pb.EncryptionSpec) *krm.EncryptionSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.EncryptionSpec{}
+	out.Name = direct.LazyPtr(in.GetName())
+	out.KMSKey = direct.LazyPtr(in.GetKmsKey())
+	return out
+}
+func EncryptionSpec_ToProto(mapCtx *direct.MapContext, in *krm.EncryptionSpec) *pb.EncryptionSpec {
+	if in == nil {
+		return nil
+	}
+	out := &pb.EncryptionSpec{}
+	out.Name = direct.ValueOf(in.Name)
+	out.KmsKey = direct.ValueOf(in.KMSKey)
+	return out
+}
