aa4c8b08679e3de34134c08db15b819649ece7d6
Your Name <yuwenma@google.com>
2025-02-08 13:43:43 +0000

    feat: add resource FirestoreField

--- apis/firestore/v1alpha1/types.generated.go
+++ apis/firestore/v1alpha1/types.generated.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,80 +14,177 @@
 
 package v1alpha1
 
-/*
-// +kcc:proto=google.firestore.admin.v1.Database
-type Database struct {
-	// The resource name of the Database.
-	//  Format: `projects/{project}/databases/{database}`
+
+// +kcc:proto=google.firestore.admin.v1.Field
+type Field struct {
+	// Required. A field name of the form:
+	//  `projects/{project_id}/databases/{database_id}/collectionGroups/{collection_id}/fields/{field_path}`
+	//
+	//  A field path can be a simple field name, e.g. `address` or a path to fields
+	//  within `map_value` , e.g. `address.city`,
+	//  or a special field path. The only valid special field is `*`, which
+	//  represents any field.
+	//
+	//  Field paths can be quoted using `` ` `` (backtick). The only character that
+	//  must be escaped within a quoted field path is the backtick character
+	//  itself, escaped using a backslash. Special characters in field paths that
+	//  must be quoted include: `*`, `.`,
+	//  `` ` `` (backtick), `[`, `]`, as well as any ascii symbolic characters.
+	//
+	//  Examples:
+	//  `` `address.city` `` represents a field named `address.city`, not the map
+	//  key `city` in the field `address`. `` `*` `` represents a field named `*`,
+	//  not any field.
+	//
+	//  A special `Field` contains the default indexing settings for all fields.
+	//  This field's resource name is:
+	//  `projects/{project_id}/databases/{database_id}/collectionGroups/__default__/fields/*`
+	//  Indexes defined on this `Field` will be applied to all fields which do not
+	//  have their own `Field` index configuration.
+	// +kcc:proto:field=google.firestore.admin.v1.Field.name
 	Name *string `json:"name,omitempty"`
 
-	// Output only. The system-generated UUID4 for this Database.
-	Uid *string `json:"uid,omitempty"`
+	// The index configuration for this field. If unset, field indexing will
+	//  revert to the configuration defined by the `ancestor_field`. To
+	//  explicitly remove all indexes for this field, specify an index config
+	//  with an empty list of indexes.
+	// +kcc:proto:field=google.firestore.admin.v1.Field.index_config
+	IndexConfig *Field_IndexConfig `json:"indexConfig,omitempty"`
 
-	// Output only. The timestamp at which this database was created. Databases
-	//  created before 2016 do not populate create_time.
-	CreateTime *string `json:"createTime,omitempty"`
-
-	// Output only. The timestamp at which this database was most recently
-	//  updated. Note this only includes updates to the database resource and not
-	//  data contained by the database.
-	UpdateTime *string `json:"updateTime,omitempty"`
-
-	// The location of the database. Available locations are listed at
-	//  https://cloud.google.com/firestore/docs/locations.
-	LocationID *string `json:"locationID,omitempty"`
-
-	// The type of the database.
-	//  See https://cloud.google.com/datastore/docs/firestore-or-datastore for
-	//  information about how to choose.
-	Type *string `json:"type,omitempty"`
-
-	// The concurrency control mode to use for this database.
-	ConcurrencyMode *string `json:"concurrencyMode,omitempty"`
-
-	// Output only. The period during which past versions of data are retained in
-	//  the database.
-	//
-	//  Any [read][google.firestore.v1.GetDocumentRequest.read_time]
-	//  or [query][google.firestore.v1.ListDocumentsRequest.read_time] can specify
-	//  a `read_time` within this window, and will read the state of the database
-	//  at that time.
-	//
-	//  If the PITR feature is enabled, the retention period is 7 days. Otherwise,
-	//  the retention period is 1 hour.
-	VersionRetentionPeriod *string `json:"versionRetentionPeriod,omitempty"`
-
-	// Output only. The earliest timestamp at which older versions of the data can
-	//  be read from the database. See [version_retention_period] above; this field
-	//  is populated with `now - version_retention_period`.
-	//
-	//  This value is continuously updated, and becomes stale the moment it is
-	//  queried. If you are using this value to recover data, make sure to account
-	//  for the time from the moment when the value is queried to the moment when
-	//  you initiate the recovery.
-	EarliestVersionTime *string `json:"earliestVersionTime,omitempty"`
-
-	// Whether to enable the PITR feature on this database.
-	PointInTimeRecoveryEnablement *string `json:"pointInTimeRecoveryEnablement,omitempty"`
-
-	// The App Engine integration mode to use for this database.
-	AppEngineIntegrationMode *string `json:"appEngineIntegrationMode,omitempty"`
-
-	// Output only. The key_prefix for this database. This key_prefix is used, in
-	//  combination with the project id ("<key prefix>~<project id>") to construct
-	//  the application id that is returned from the Cloud Datastore APIs in Google
-	//  App Engine first generation runtimes.
-	//
-	//  This value may be empty in which case the appid to use for URL-encoded keys
-	//  is the project_id (eg: foo instead of v~foo).
-	KeyPrefix *string `json:"keyPrefix,omitempty"`
-
-	// State of delete protection for the database.
-	DeleteProtectionState *string `json:"deleteProtectionState,omitempty"`
-
-	// This checksum is computed by the server based on the value of other
-	//  fields, and may be sent on update and delete requests to ensure the
-	//  client has an up-to-date value before proceeding.
-	Etag *string `json:"etag,omitempty"`
+	// The TTL configuration for this `Field`.
+	//  Setting or unsetting this will enable or disable the TTL for
+	//  documents that have this `Field`.
+	// +kcc:proto:field=google.firestore.admin.v1.Field.ttl_config
+	TtlConfig *Field_TtlConfig `json:"ttlConfig,omitempty"`
 }
-*/
+
+// +kcc:proto=google.firestore.admin.v1.Field.IndexConfig
+type Field_IndexConfig struct {
+	// The indexes supported for this field.
+	// +kcc:proto:field=google.firestore.admin.v1.Field.IndexConfig.indexes
+	Indexes []Index `json:"indexes,omitempty"`
+
+	// Output only. When true, the `Field`'s index configuration is set from the
+	//  configuration specified by the `ancestor_field`.
+	//  When false, the `Field`'s index configuration is defined explicitly.
+	// +kcc:proto:field=google.firestore.admin.v1.Field.IndexConfig.uses_ancestor_config
+	UsesAncestorConfig *bool `json:"usesAncestorConfig,omitempty"`
+
+	// Output only. Specifies the resource name of the `Field` from which this
+	//  field's index configuration is set (when `uses_ancestor_config` is true),
+	//  or from which it *would* be set if this field had no index configuration
+	//  (when `uses_ancestor_config` is false).
+	// +kcc:proto:field=google.firestore.admin.v1.Field.IndexConfig.ancestor_field
+	AncestorField *string `json:"ancestorField,omitempty"`
+
+	// Output only
+	//  When true, the `Field`'s index configuration is in the process of being
+	//  reverted. Once complete, the index config will transition to the same
+	//  state as the field specified by `ancestor_field`, at which point
+	//  `uses_ancestor_config` will be `true` and `reverting` will be `false`.
+	// +kcc:proto:field=google.firestore.admin.v1.Field.IndexConfig.reverting
+	Reverting *bool `json:"reverting,omitempty"`
+}
+
+// +kcc:proto=google.firestore.admin.v1.Field.TtlConfig
+type Field_TtlConfig struct {
+}
+
+// +kcc:proto=google.firestore.admin.v1.Index
+type Index struct {
+	// Output only. A server defined name for this index.
+	//  The form of this name for composite indexes will be:
+	//  `projects/{project_id}/databases/{database_id}/collectionGroups/{collection_id}/indexes/{composite_index_id}`
+	//  For single field indexes, this field will be empty.
+	// +kcc:proto:field=google.firestore.admin.v1.Index.name
+	Name *string `json:"name,omitempty"`
+
+	// Indexes with a collection query scope specified allow queries
+	//  against a collection that is the child of a specific document, specified at
+	//  query time, and that has the same collection ID.
+	//
+	//  Indexes with a collection group query scope specified allow queries against
+	//  all collections descended from a specific document, specified at query
+	//  time, and that have the same collection ID as this index.
+	// +kcc:proto:field=google.firestore.admin.v1.Index.query_scope
+	QueryScope *string `json:"queryScope,omitempty"`
+
+	// The API scope supported by this index.
+	// +kcc:proto:field=google.firestore.admin.v1.Index.api_scope
+	ApiScope *string `json:"apiScope,omitempty"`
+
+	// The fields supported by this index.
+	//
+	//  For composite indexes, this requires a minimum of 2 and a maximum of 100
+	//  fields. The last field entry is always for the field path `__name__`. If,
+	//  on creation, `__name__` was not specified as the last field, it will be
+	//  added automatically with the same direction as that of the last field
+	//  defined. If the final field in a composite index is not directional, the
+	//  `__name__` will be ordered ASCENDING (unless explicitly specified).
+	//
+	//  For single field indexes, this will always be exactly one entry with a
+	//  field path equal to the field path of the associated field.
+	// +kcc:proto:field=google.firestore.admin.v1.Index.fields
+	Fields []Index_IndexField `json:"fields,omitempty"`
+
+	// Output only. The serving state of the index.
+	// +kcc:proto:field=google.firestore.admin.v1.Index.state
+	State *string `json:"state,omitempty"`
+}
+
+// +kcc:proto=google.firestore.admin.v1.Index.IndexField
+type Index_IndexField struct {
+	// Can be __name__.
+	//  For single field indexes, this must match the name of the field or may
+	//  be omitted.
+	// +kcc:proto:field=google.firestore.admin.v1.Index.IndexField.field_path
+	FieldPath *string `json:"fieldPath,omitempty"`
+
+	// Indicates that this field supports ordering by the specified order or
+	//  comparing using =, !=, <, <=, >, >=.
+	// +kcc:proto:field=google.firestore.admin.v1.Index.IndexField.order
+	Order *string `json:"order,omitempty"`
+
+	// Indicates that this field supports operations on `array_value`s.
+	// +kcc:proto:field=google.firestore.admin.v1.Index.IndexField.array_config
+	ArrayConfig *string `json:"arrayConfig,omitempty"`
+
+	// Indicates that this field supports nearest neighbor and distance
+	//  operations on vector.
+	// +kcc:proto:field=google.firestore.admin.v1.Index.IndexField.vector_config
+	VectorConfig *Index_IndexField_VectorConfig `json:"vectorConfig,omitempty"`
+}
+
+// +kcc:proto=google.firestore.admin.v1.Index.IndexField.VectorConfig
+type Index_IndexField_VectorConfig struct {
+	// Required. The vector dimension this configuration applies to.
+	//
+	//  The resulting index will only include vectors of this dimension, and
+	//  can be used for vector search with the same dimension.
+	// +kcc:proto:field=google.firestore.admin.v1.Index.IndexField.VectorConfig.dimension
+	Dimension *int32 `json:"dimension,omitempty"`
+
+	// Indicates the vector index is a flat index.
+	// +kcc:proto:field=google.firestore.admin.v1.Index.IndexField.VectorConfig.flat
+	Flat *Index_IndexField_VectorConfig_FlatIndex `json:"flat,omitempty"`
+}
+
+// +kcc:proto=google.firestore.admin.v1.Index.IndexField.VectorConfig.FlatIndex
+type Index_IndexField_VectorConfig_FlatIndex struct {
+}
+
+// +kcc:proto=google.firestore.admin.v1.Field
+type FieldObservedState struct {
+	// The TTL configuration for this `Field`.
+	//  Setting or unsetting this will enable or disable the TTL for
+	//  documents that have this `Field`.
+	// +kcc:proto:field=google.firestore.admin.v1.Field.ttl_config
+	TtlConfig *Field_TtlConfigObservedState `json:"ttlConfig,omitempty"`
+}
+
+// +kcc:proto=google.firestore.admin.v1.Field.TtlConfig
+type Field_TtlConfigObservedState struct {
+	// Output only. The state of the TTL configuration.
+	// +kcc:proto:field=google.firestore.admin.v1.Field.TtlConfig.state
+	State *string `json:"state,omitempty"`
+}
--- pkg/controller/direct/firestore/mapper.generated.go
+++ pkg/controller/direct/firestore/mapper.generated.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,31 +14,230 @@
 
 package firestore
 
-/*
 import (
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
 	pb "cloud.google.com/go/firestore/apiv1/admin/adminpb"
 	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/firestore/v1alpha1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/firestore/v1beta1"
 )
-func FirestoreDatabaseObservedState_ToProto(mapCtx *direct.MapContext, in *krm.FirestoreDatabaseObservedState) *pb.Database {
+func Field_FromProto(mapCtx *direct.MapContext, in *pb.Field) *krm.Field {
 	if in == nil {
 		return nil
 	}
-	out := &pb.Database{}
-	// MISSING: Name
-	out.Uid = direct.ValueOf(in.Uid)
-	out.CreateTime = Database_CreateTime_ToProto(mapCtx, in.CreateTime)
-	out.UpdateTime = Database_UpdateTime_ToProto(mapCtx, in.UpdateTime)
-	// MISSING: LocationID
-	// MISSING: Type
-	// MISSING: ConcurrencyMode
-	out.VersionRetentionPeriod = Database_VersionRetentionPeriod_ToProto(mapCtx, in.VersionRetentionPeriod)
-	out.EarliestVersionTime = Database_EarliestVersionTime_ToProto(mapCtx, in.EarliestVersionTime)
-	// MISSING: PointInTimeRecoveryEnablement
-	// MISSING: AppEngineIntegrationMode
-	out.KeyPrefix = direct.ValueOf(in.KeyPrefix)
-	// MISSING: DeleteProtectionState
-	out.Etag = direct.ValueOf(in.Etag)
+	out := &krm.Field{}
+	out.Name = direct.LazyPtr(in.GetName())
+	out.IndexConfig = Field_IndexConfig_FromProto(mapCtx, in.GetIndexConfig())
+	out.TtlConfig = Field_TtlConfig_FromProto(mapCtx, in.GetTtlConfig())
 	return out
 }
-*/
+func Field_ToProto(mapCtx *direct.MapContext, in *krm.Field) *pb.Field {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Field{}
+	out.Name = direct.ValueOf(in.Name)
+	out.IndexConfig = Field_IndexConfig_ToProto(mapCtx, in.IndexConfig)
+	out.TtlConfig = Field_TtlConfig_ToProto(mapCtx, in.TtlConfig)
+	return out
+}
+func FieldObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Field) *krm.FieldObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.FieldObservedState{}
+	// MISSING: Name
+	// MISSING: IndexConfig
+	out.TtlConfig = Field_TtlConfigObservedState_FromProto(mapCtx, in.GetTtlConfig())
+	return out
+}
+func FieldObservedState_ToProto(mapCtx *direct.MapContext, in *krm.FieldObservedState) *pb.Field {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Field{}
+	// MISSING: Name
+	// MISSING: IndexConfig
+	out.TtlConfig = Field_TtlConfigObservedState_ToProto(mapCtx, in.TtlConfig)
+	return out
+}
+func Field_IndexConfig_FromProto(mapCtx *direct.MapContext, in *pb.Field_IndexConfig) *krm.Field_IndexConfig {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Field_IndexConfig{}
+	out.Indexes = direct.Slice_FromProto(mapCtx, in.Indexes, Index_FromProto)
+	out.UsesAncestorConfig = direct.LazyPtr(in.GetUsesAncestorConfig())
+	out.AncestorField = direct.LazyPtr(in.GetAncestorField())
+	out.Reverting = direct.LazyPtr(in.GetReverting())
+	return out
+}
+func Field_IndexConfig_ToProto(mapCtx *direct.MapContext, in *krm.Field_IndexConfig) *pb.Field_IndexConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Field_IndexConfig{}
+	out.Indexes = direct.Slice_ToProto(mapCtx, in.Indexes, Index_ToProto)
+	out.UsesAncestorConfig = direct.ValueOf(in.UsesAncestorConfig)
+	out.AncestorField = direct.ValueOf(in.AncestorField)
+	out.Reverting = direct.ValueOf(in.Reverting)
+	return out
+}
+func Field_TtlConfig_FromProto(mapCtx *direct.MapContext, in *pb.Field_TtlConfig) *krm.Field_TtlConfig {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Field_TtlConfig{}
+	// MISSING: State
+	return out
+}
+func Field_TtlConfig_ToProto(mapCtx *direct.MapContext, in *krm.Field_TtlConfig) *pb.Field_TtlConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Field_TtlConfig{}
+	// MISSING: State
+	return out
+}
+func Field_TtlConfigObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Field_TtlConfig) *krm.Field_TtlConfigObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Field_TtlConfigObservedState{}
+	out.State = direct.Enum_FromProto(mapCtx, in.GetState())
+	return out
+}
+func Field_TtlConfigObservedState_ToProto(mapCtx *direct.MapContext, in *krm.Field_TtlConfigObservedState) *pb.Field_TtlConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Field_TtlConfig{}
+	out.State = direct.Enum_ToProto[pb.Field_TtlConfig_State](mapCtx, in.State)
+	return out
+}
+func FirestoreFieldObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Field) *krm.FirestoreFieldObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.FirestoreFieldObservedState{}
+	// MISSING: Name
+	// MISSING: IndexConfig
+	// MISSING: TtlConfig
+	return out
+}
+func FirestoreFieldObservedState_ToProto(mapCtx *direct.MapContext, in *krm.FirestoreFieldObservedState) *pb.Field {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Field{}
+	// MISSING: Name
+	// MISSING: IndexConfig
+	// MISSING: TtlConfig
+	return out
+}
+func FirestoreFieldSpec_FromProto(mapCtx *direct.MapContext, in *pb.Field) *krm.FirestoreFieldSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.FirestoreFieldSpec{}
+	// MISSING: Name
+	// MISSING: IndexConfig
+	// MISSING: TtlConfig
+	return out
+}
+func FirestoreFieldSpec_ToProto(mapCtx *direct.MapContext, in *krm.FirestoreFieldSpec) *pb.Field {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Field{}
+	// MISSING: Name
+	// MISSING: IndexConfig
+	// MISSING: TtlConfig
+	return out
+}
+func Index_FromProto(mapCtx *direct.MapContext, in *pb.Index) *krm.Index {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Index{}
+	out.Name = direct.LazyPtr(in.GetName())
+	out.QueryScope = direct.Enum_FromProto(mapCtx, in.GetQueryScope())
+	out.ApiScope = direct.Enum_FromProto(mapCtx, in.GetApiScope())
+	out.Fields = direct.Slice_FromProto(mapCtx, in.Fields, Index_IndexField_FromProto)
+	out.State = direct.Enum_FromProto(mapCtx, in.GetState())
+	return out
+}
+func Index_ToProto(mapCtx *direct.MapContext, in *krm.Index) *pb.Index {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Index{}
+	out.Name = direct.ValueOf(in.Name)
+	out.QueryScope = direct.Enum_ToProto[pb.Index_QueryScope](mapCtx, in.QueryScope)
+	out.ApiScope = direct.Enum_ToProto[pb.Index_ApiScope](mapCtx, in.ApiScope)
+	out.Fields = direct.Slice_ToProto(mapCtx, in.Fields, Index_IndexField_ToProto)
+	out.State = direct.Enum_ToProto[pb.Index_State](mapCtx, in.State)
+	return out
+}
+func Index_IndexField_FromProto(mapCtx *direct.MapContext, in *pb.Index_IndexField) *krm.Index_IndexField {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Index_IndexField{}
+	out.FieldPath = direct.LazyPtr(in.GetFieldPath())
+	out.Order = direct.Enum_FromProto(mapCtx, in.GetOrder())
+	out.ArrayConfig = direct.Enum_FromProto(mapCtx, in.GetArrayConfig())
+	out.VectorConfig = Index_IndexField_VectorConfig_FromProto(mapCtx, in.GetVectorConfig())
+	return out
+}
+func Index_IndexField_ToProto(mapCtx *direct.MapContext, in *krm.Index_IndexField) *pb.Index_IndexField {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Index_IndexField{}
+	out.FieldPath = direct.ValueOf(in.FieldPath)
+	if oneof := Index_IndexField_Order_ToProto(mapCtx, in.Order); oneof != nil {
+		out.ValueMode = oneof
+	}
+	if oneof := Index_IndexField_ArrayConfig_ToProto(mapCtx, in.ArrayConfig); oneof != nil {
+		out.ValueMode = oneof
+	}
+	if oneof := Index_IndexField_VectorConfig_ToProto(mapCtx, in.VectorConfig); oneof != nil {
+		out.ValueMode = &pb.Index_IndexField_VectorConfig_{VectorConfig: oneof}
+	}
+	return out
+}
+func Index_IndexField_VectorConfig_FromProto(mapCtx *direct.MapContext, in *pb.Index_IndexField_VectorConfig) *krm.Index_IndexField_VectorConfig {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Index_IndexField_VectorConfig{}
+	out.Dimension = direct.LazyPtr(in.GetDimension())
+	out.Flat = Index_IndexField_VectorConfig_FlatIndex_FromProto(mapCtx, in.GetFlat())
+	return out
+}
+func Index_IndexField_VectorConfig_ToProto(mapCtx *direct.MapContext, in *krm.Index_IndexField_VectorConfig) *pb.Index_IndexField_VectorConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Index_IndexField_VectorConfig{}
+	out.Dimension = direct.ValueOf(in.Dimension)
+	if oneof := Index_IndexField_VectorConfig_FlatIndex_ToProto(mapCtx, in.Flat); oneof != nil {
+		out.Type = &pb.Index_IndexField_VectorConfig_Flat{Flat: oneof}
+	}
+	return out
+}
+func Index_IndexField_VectorConfig_FlatIndex_FromProto(mapCtx *direct.MapContext, in *pb.Index_IndexField_VectorConfig_FlatIndex) *krm.Index_IndexField_VectorConfig_FlatIndex {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Index_IndexField_VectorConfig_FlatIndex{}
+	return out
+}
+func Index_IndexField_VectorConfig_FlatIndex_ToProto(mapCtx *direct.MapContext, in *krm.Index_IndexField_VectorConfig_FlatIndex) *pb.Index_IndexField_VectorConfig_FlatIndex {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Index_IndexField_VectorConfig_FlatIndex{}
+	return out
+}
