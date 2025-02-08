e0c519689113986913092cc3998b5836a97d1cf8
Your Name <yuwenma@google.com>
2025-02-08 09:40:49 +0000

    feat: add resource RetailAttributesConfig

---
+++ apis/retail/v1alpha1/types.generated.go
@@ -0,0 +1,239 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+
+// +kcc:proto=google.cloud.retail.v2.AttributesConfig
+type AttributesConfig struct {
+	// Required. Immutable. The fully qualified resource name of the attribute
+	//  config. Format: `projects/*/locations/*/catalogs/*/attributesConfig`
+	// +kcc:proto:field=google.cloud.retail.v2.AttributesConfig.name
+	Name *string `json:"name,omitempty"`
+
+	// TODO: unsupported map type with key string and value message
+
+}
+
+// +kcc:proto=google.cloud.retail.v2.CatalogAttribute
+type CatalogAttribute struct {
+	// Required. Attribute name.
+	//  For example: `color`, `brands`, `attributes.custom_attribute`, such as
+	//  `attributes.xyz`.
+	//  To be indexable, the attribute name can contain only alpha-numeric
+	//  characters and underscores. For example, an attribute named
+	//  `attributes.abc_xyz` can be indexed, but an attribute named
+	//  `attributes.abc-xyz` cannot be indexed.
+	//
+	//  If the attribute key starts with `attributes.`, then the attribute is a
+	//  custom attribute. Attributes such as `brands`, `patterns`, and `title` are
+	//  built-in and called system attributes.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.key
+	Key *string `json:"key,omitempty"`
+
+	// When
+	//  [AttributesConfig.attribute_config_level][google.cloud.retail.v2.AttributesConfig.attribute_config_level]
+	//  is CATALOG_LEVEL_ATTRIBUTE_CONFIG, if INDEXABLE_ENABLED attribute values
+	//  are indexed so that it can be filtered, faceted, or boosted in
+	//  [SearchService.Search][google.cloud.retail.v2.SearchService.Search].
+	//
+	//  Must be specified when
+	//  [AttributesConfig.attribute_config_level][google.cloud.retail.v2.AttributesConfig.attribute_config_level]
+	//  is CATALOG_LEVEL_ATTRIBUTE_CONFIG, otherwise throws INVALID_FORMAT error.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.indexable_option
+	IndexableOption *string `json:"indexableOption,omitempty"`
+
+	// If DYNAMIC_FACETABLE_ENABLED, attribute values are available for dynamic
+	//  facet. Could only be DYNAMIC_FACETABLE_DISABLED if
+	//  [CatalogAttribute.indexable_option][google.cloud.retail.v2.CatalogAttribute.indexable_option]
+	//  is INDEXABLE_DISABLED. Otherwise, an INVALID_ARGUMENT error is returned.
+	//
+	//  Must be specified, otherwise throws INVALID_FORMAT error.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.dynamic_facetable_option
+	DynamicFacetableOption *string `json:"dynamicFacetableOption,omitempty"`
+
+	// When
+	//  [AttributesConfig.attribute_config_level][google.cloud.retail.v2.AttributesConfig.attribute_config_level]
+	//  is CATALOG_LEVEL_ATTRIBUTE_CONFIG, if SEARCHABLE_ENABLED, attribute values
+	//  are searchable by text queries in
+	//  [SearchService.Search][google.cloud.retail.v2.SearchService.Search].
+	//
+	//  If SEARCHABLE_ENABLED but attribute type is numerical, attribute values
+	//  will not be searchable by text queries in
+	//  [SearchService.Search][google.cloud.retail.v2.SearchService.Search], as
+	//  there are no text values associated to numerical attributes.
+	//
+	//  Must be specified, when
+	//  [AttributesConfig.attribute_config_level][google.cloud.retail.v2.AttributesConfig.attribute_config_level]
+	//  is CATALOG_LEVEL_ATTRIBUTE_CONFIG, otherwise throws INVALID_FORMAT error.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.searchable_option
+	SearchableOption *string `json:"searchableOption,omitempty"`
+
+	// If EXACT_SEARCHABLE_ENABLED, attribute values will be exact searchable.
+	//  This property only applies to textual custom attributes and requires
+	//  indexable set to enabled to enable exact-searchable. If unset, the server
+	//  behavior defaults to
+	//  [EXACT_SEARCHABLE_DISABLED][google.cloud.retail.v2.CatalogAttribute.ExactSearchableOption.EXACT_SEARCHABLE_DISABLED].
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.exact_searchable_option
+	ExactSearchableOption *string `json:"exactSearchableOption,omitempty"`
+
+	// If RETRIEVABLE_ENABLED, attribute values are retrievable in the search
+	//  results. If unset, the server behavior defaults to
+	//  [RETRIEVABLE_DISABLED][google.cloud.retail.v2.CatalogAttribute.RetrievableOption.RETRIEVABLE_DISABLED].
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.retrievable_option
+	RetrievableOption *string `json:"retrievableOption,omitempty"`
+
+	// Contains facet options.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.facet_config
+	FacetConfig *CatalogAttribute_FacetConfig `json:"facetConfig,omitempty"`
+}
+
+// +kcc:proto=google.cloud.retail.v2.CatalogAttribute.FacetConfig
+type CatalogAttribute_FacetConfig struct {
+	// If you don't set the facet
+	//  [SearchRequest.FacetSpec.FacetKey.intervals][google.cloud.retail.v2.SearchRequest.FacetSpec.FacetKey.intervals]
+	//  in the request to a numerical attribute, then we use the computed
+	//  intervals with rounded bounds obtained from all its product numerical
+	//  attribute values. The computed intervals might not be ideal for some
+	//  attributes. Therefore, we give you the option to overwrite them with the
+	//  facet_intervals field. The maximum of facet intervals per
+	//  [CatalogAttribute][google.cloud.retail.v2.CatalogAttribute] is 40. Each
+	//  interval must have a lower bound or an upper bound. If both bounds are
+	//  provided, then the lower bound must be smaller or equal than the upper
+	//  bound.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.FacetConfig.facet_intervals
+	FacetIntervals []Interval `json:"facetIntervals,omitempty"`
+
+	// Each instance represents a list of attribute values to ignore as facet
+	//  values for a specific time range. The maximum number of instances per
+	//  [CatalogAttribute][google.cloud.retail.v2.CatalogAttribute] is 25.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.FacetConfig.ignored_facet_values
+	IgnoredFacetValues []CatalogAttribute_FacetConfig_IgnoredFacetValues `json:"ignoredFacetValues,omitempty"`
+
+	// Each instance replaces a list of facet values by a merged facet
+	//  value. If a facet value is not in any list, then it will stay the same.
+	//  To avoid conflicts, only paths of length 1 are accepted. In other words,
+	//  if "dark_blue" merged into "BLUE", then the latter can't merge into
+	//  "blues" because this would create a path of length 2. The maximum number
+	//  of instances of MergedFacetValue per
+	//  [CatalogAttribute][google.cloud.retail.v2.CatalogAttribute] is 100. This
+	//  feature is available only for textual custom attributes.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.FacetConfig.merged_facet_values
+	MergedFacetValues []CatalogAttribute_FacetConfig_MergedFacetValue `json:"mergedFacetValues,omitempty"`
+
+	// Use this field only if you want to merge a facet key into another facet
+	//  key.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.FacetConfig.merged_facet
+	MergedFacet *CatalogAttribute_FacetConfig_MergedFacet `json:"mergedFacet,omitempty"`
+
+	// Set this field only if you want to rerank based on facet values engaged
+	//  by the user for the current key. This option is only possible for custom
+	//  facetable textual keys.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.FacetConfig.rerank_config
+	RerankConfig *CatalogAttribute_FacetConfig_RerankConfig `json:"rerankConfig,omitempty"`
+}
+
+// +kcc:proto=google.cloud.retail.v2.CatalogAttribute.FacetConfig.IgnoredFacetValues
+type CatalogAttribute_FacetConfig_IgnoredFacetValues struct {
+	// List of facet values to ignore for the following time range. The facet
+	//  values are the same as the attribute values. There is a limit of 10
+	//  values per instance of IgnoredFacetValues. Each value can have at most
+	//  128 characters.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.FacetConfig.IgnoredFacetValues.values
+	Values []string `json:"values,omitempty"`
+
+	// Time range for the current list of facet values to ignore.
+	//  If multiple time ranges are specified for an facet value for the
+	//  current attribute, consider all of them. If both are empty, ignore
+	//  always. If start time and end time are set, then start time
+	//  must be before end time.
+	//  If start time is not empty and end time is empty, then will ignore
+	//  these facet values after the start time.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.FacetConfig.IgnoredFacetValues.start_time
+	StartTime *string `json:"startTime,omitempty"`
+
+	// If start time is empty and end time is not empty, then ignore these
+	//  facet values before end time.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.FacetConfig.IgnoredFacetValues.end_time
+	EndTime *string `json:"endTime,omitempty"`
+}
+
+// +kcc:proto=google.cloud.retail.v2.CatalogAttribute.FacetConfig.MergedFacet
+type CatalogAttribute_FacetConfig_MergedFacet struct {
+	// The merged facet key should be a valid facet key that is different than
+	//  the facet key of the current catalog attribute. We refer this is
+	//  merged facet key as the child of the current catalog attribute. This
+	//  merged facet key can't be a parent of another facet key (i.e. no
+	//  directed path of length 2). This merged facet key needs to be either a
+	//  textual custom attribute or a numerical custom attribute.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.FacetConfig.MergedFacet.merged_facet_key
+	MergedFacetKey *string `json:"mergedFacetKey,omitempty"`
+}
+
+// +kcc:proto=google.cloud.retail.v2.CatalogAttribute.FacetConfig.MergedFacetValue
+type CatalogAttribute_FacetConfig_MergedFacetValue struct {
+	// All the facet values that are replaces by the same
+	//  [merged_value][google.cloud.retail.v2.CatalogAttribute.FacetConfig.MergedFacetValue.merged_value]
+	//  that follows. The maximum number of values per MergedFacetValue is 25.
+	//  Each value can have up to 128 characters.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.FacetConfig.MergedFacetValue.values
+	Values []string `json:"values,omitempty"`
+
+	// All the previous values are replaced by this merged facet value.
+	//  This merged_value must be non-empty and can have up to 128 characters.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.FacetConfig.MergedFacetValue.merged_value
+	MergedValue *string `json:"mergedValue,omitempty"`
+}
+
+// +kcc:proto=google.cloud.retail.v2.CatalogAttribute.FacetConfig.RerankConfig
+type CatalogAttribute_FacetConfig_RerankConfig struct {
+	// If set to true, then we also rerank the dynamic facets based on the
+	//  facet values engaged by the user for the current attribute key during
+	//  serving.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.FacetConfig.RerankConfig.rerank_facet
+	RerankFacet *bool `json:"rerankFacet,omitempty"`
+
+	// If empty, rerank on all facet values for the current key. Otherwise,
+	//  will rerank on the facet values from this list only.
+	// +kcc:proto:field=google.cloud.retail.v2.CatalogAttribute.FacetConfig.RerankConfig.facet_values
+	FacetValues []string `json:"facetValues,omitempty"`
+}
+
+// +kcc:proto=google.cloud.retail.v2.Interval
+type Interval struct {
+	// Inclusive lower bound.
+	// +kcc:proto:field=google.cloud.retail.v2.Interval.minimum
+	Minimum *float64 `json:"minimum,omitempty"`
+
+	// Exclusive lower bound.
+	// +kcc:proto:field=google.cloud.retail.v2.Interval.exclusive_minimum
+	ExclusiveMinimum *float64 `json:"exclusiveMinimum,omitempty"`
+
+	// Inclusive upper bound.
+	// +kcc:proto:field=google.cloud.retail.v2.Interval.maximum
+	Maximum *float64 `json:"maximum,omitempty"`
+
+	// Exclusive upper bound.
+	// +kcc:proto:field=google.cloud.retail.v2.Interval.exclusive_maximum
+	ExclusiveMaximum *float64 `json:"exclusiveMaximum,omitempty"`
+}
+
+// +kcc:proto=google.cloud.retail.v2.AttributesConfig
+type AttributesConfigObservedState struct {
+	// Output only. The
+	//  [AttributeConfigLevel][google.cloud.retail.v2.AttributeConfigLevel] used
+	//  for this catalog.
+	// +kcc:proto:field=google.cloud.retail.v2.AttributesConfig.attribute_config_level
+	AttributeConfigLevel *string `json:"attributeConfigLevel,omitempty"`
+}
---
+++ pkg/controller/direct/retail/mapper.generated.go
@@ -0,0 +1,260 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package retail
+
+import (
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/retail/v1alpha1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	pb "cloud.google.com/go/retail/apiv2/retailpb"
+)
+func AttributesConfig_FromProto(mapCtx *direct.MapContext, in *pb.AttributesConfig) *krm.AttributesConfig {
+	if in == nil {
+		return nil
+	}
+	out := &krm.AttributesConfig{}
+	out.Name = direct.LazyPtr(in.GetName())
+	// MISSING: CatalogAttributes
+	// MISSING: AttributeConfigLevel
+	return out
+}
+func AttributesConfig_ToProto(mapCtx *direct.MapContext, in *krm.AttributesConfig) *pb.AttributesConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.AttributesConfig{}
+	out.Name = direct.ValueOf(in.Name)
+	// MISSING: CatalogAttributes
+	// MISSING: AttributeConfigLevel
+	return out
+}
+func AttributesConfigObservedState_FromProto(mapCtx *direct.MapContext, in *pb.AttributesConfig) *krm.AttributesConfigObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.AttributesConfigObservedState{}
+	// MISSING: Name
+	// MISSING: CatalogAttributes
+	out.AttributeConfigLevel = direct.Enum_FromProto(mapCtx, in.GetAttributeConfigLevel())
+	return out
+}
+func AttributesConfigObservedState_ToProto(mapCtx *direct.MapContext, in *krm.AttributesConfigObservedState) *pb.AttributesConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.AttributesConfig{}
+	// MISSING: Name
+	// MISSING: CatalogAttributes
+	out.AttributeConfigLevel = direct.Enum_ToProto[pb.AttributeConfigLevel](mapCtx, in.AttributeConfigLevel)
+	return out
+}
+func CatalogAttribute_FromProto(mapCtx *direct.MapContext, in *pb.CatalogAttribute) *krm.CatalogAttribute {
+	if in == nil {
+		return nil
+	}
+	out := &krm.CatalogAttribute{}
+	out.Key = direct.LazyPtr(in.GetKey())
+	// MISSING: InUse
+	// MISSING: Type
+	out.IndexableOption = direct.Enum_FromProto(mapCtx, in.GetIndexableOption())
+	out.DynamicFacetableOption = direct.Enum_FromProto(mapCtx, in.GetDynamicFacetableOption())
+	out.SearchableOption = direct.Enum_FromProto(mapCtx, in.GetSearchableOption())
+	out.ExactSearchableOption = direct.Enum_FromProto(mapCtx, in.GetExactSearchableOption())
+	out.RetrievableOption = direct.Enum_FromProto(mapCtx, in.GetRetrievableOption())
+	out.FacetConfig = CatalogAttribute_FacetConfig_FromProto(mapCtx, in.GetFacetConfig())
+	return out
+}
+func CatalogAttribute_ToProto(mapCtx *direct.MapContext, in *krm.CatalogAttribute) *pb.CatalogAttribute {
+	if in == nil {
+		return nil
+	}
+	out := &pb.CatalogAttribute{}
+	out.Key = direct.ValueOf(in.Key)
+	// MISSING: InUse
+	// MISSING: Type
+	out.IndexableOption = direct.Enum_ToProto[pb.CatalogAttribute_IndexableOption](mapCtx, in.IndexableOption)
+	out.DynamicFacetableOption = direct.Enum_ToProto[pb.CatalogAttribute_DynamicFacetableOption](mapCtx, in.DynamicFacetableOption)
+	out.SearchableOption = direct.Enum_ToProto[pb.CatalogAttribute_SearchableOption](mapCtx, in.SearchableOption)
+	out.ExactSearchableOption = direct.Enum_ToProto[pb.CatalogAttribute_ExactSearchableOption](mapCtx, in.ExactSearchableOption)
+	out.RetrievableOption = direct.Enum_ToProto[pb.CatalogAttribute_RetrievableOption](mapCtx, in.RetrievableOption)
+	out.FacetConfig = CatalogAttribute_FacetConfig_ToProto(mapCtx, in.FacetConfig)
+	return out
+}
+func CatalogAttribute_FacetConfig_FromProto(mapCtx *direct.MapContext, in *pb.CatalogAttribute_FacetConfig) *krm.CatalogAttribute_FacetConfig {
+	if in == nil {
+		return nil
+	}
+	out := &krm.CatalogAttribute_FacetConfig{}
+	out.FacetIntervals = direct.Slice_FromProto(mapCtx, in.FacetIntervals, Interval_FromProto)
+	out.IgnoredFacetValues = direct.Slice_FromProto(mapCtx, in.IgnoredFacetValues, CatalogAttribute_FacetConfig_IgnoredFacetValues_FromProto)
+	out.MergedFacetValues = direct.Slice_FromProto(mapCtx, in.MergedFacetValues, CatalogAttribute_FacetConfig_MergedFacetValue_FromProto)
+	out.MergedFacet = CatalogAttribute_FacetConfig_MergedFacet_FromProto(mapCtx, in.GetMergedFacet())
+	out.RerankConfig = CatalogAttribute_FacetConfig_RerankConfig_FromProto(mapCtx, in.GetRerankConfig())
+	return out
+}
+func CatalogAttribute_FacetConfig_ToProto(mapCtx *direct.MapContext, in *krm.CatalogAttribute_FacetConfig) *pb.CatalogAttribute_FacetConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.CatalogAttribute_FacetConfig{}
+	out.FacetIntervals = direct.Slice_ToProto(mapCtx, in.FacetIntervals, Interval_ToProto)
+	out.IgnoredFacetValues = direct.Slice_ToProto(mapCtx, in.IgnoredFacetValues, CatalogAttribute_FacetConfig_IgnoredFacetValues_ToProto)
+	out.MergedFacetValues = direct.Slice_ToProto(mapCtx, in.MergedFacetValues, CatalogAttribute_FacetConfig_MergedFacetValue_ToProto)
+	out.MergedFacet = CatalogAttribute_FacetConfig_MergedFacet_ToProto(mapCtx, in.MergedFacet)
+	out.RerankConfig = CatalogAttribute_FacetConfig_RerankConfig_ToProto(mapCtx, in.RerankConfig)
+	return out
+}
+func CatalogAttribute_FacetConfig_IgnoredFacetValues_FromProto(mapCtx *direct.MapContext, in *pb.CatalogAttribute_FacetConfig_IgnoredFacetValues) *krm.CatalogAttribute_FacetConfig_IgnoredFacetValues {
+	if in == nil {
+		return nil
+	}
+	out := &krm.CatalogAttribute_FacetConfig_IgnoredFacetValues{}
+	out.Values = in.Values
+	out.StartTime = direct.StringTimestamp_FromProto(mapCtx, in.GetStartTime())
+	out.EndTime = direct.StringTimestamp_FromProto(mapCtx, in.GetEndTime())
+	return out
+}
+func CatalogAttribute_FacetConfig_IgnoredFacetValues_ToProto(mapCtx *direct.MapContext, in *krm.CatalogAttribute_FacetConfig_IgnoredFacetValues) *pb.CatalogAttribute_FacetConfig_IgnoredFacetValues {
+	if in == nil {
+		return nil
+	}
+	out := &pb.CatalogAttribute_FacetConfig_IgnoredFacetValues{}
+	out.Values = in.Values
+	out.StartTime = direct.StringTimestamp_ToProto(mapCtx, in.StartTime)
+	out.EndTime = direct.StringTimestamp_ToProto(mapCtx, in.EndTime)
+	return out
+}
+func CatalogAttribute_FacetConfig_MergedFacet_FromProto(mapCtx *direct.MapContext, in *pb.CatalogAttribute_FacetConfig_MergedFacet) *krm.CatalogAttribute_FacetConfig_MergedFacet {
+	if in == nil {
+		return nil
+	}
+	out := &krm.CatalogAttribute_FacetConfig_MergedFacet{}
+	out.MergedFacetKey = direct.LazyPtr(in.GetMergedFacetKey())
+	return out
+}
+func CatalogAttribute_FacetConfig_MergedFacet_ToProto(mapCtx *direct.MapContext, in *krm.CatalogAttribute_FacetConfig_MergedFacet) *pb.CatalogAttribute_FacetConfig_MergedFacet {
+	if in == nil {
+		return nil
+	}
+	out := &pb.CatalogAttribute_FacetConfig_MergedFacet{}
+	out.MergedFacetKey = direct.ValueOf(in.MergedFacetKey)
+	return out
+}
+func CatalogAttribute_FacetConfig_MergedFacetValue_FromProto(mapCtx *direct.MapContext, in *pb.CatalogAttribute_FacetConfig_MergedFacetValue) *krm.CatalogAttribute_FacetConfig_MergedFacetValue {
+	if in == nil {
+		return nil
+	}
+	out := &krm.CatalogAttribute_FacetConfig_MergedFacetValue{}
+	out.Values = in.Values
+	out.MergedValue = direct.LazyPtr(in.GetMergedValue())
+	return out
+}
+func CatalogAttribute_FacetConfig_MergedFacetValue_ToProto(mapCtx *direct.MapContext, in *krm.CatalogAttribute_FacetConfig_MergedFacetValue) *pb.CatalogAttribute_FacetConfig_MergedFacetValue {
+	if in == nil {
+		return nil
+	}
+	out := &pb.CatalogAttribute_FacetConfig_MergedFacetValue{}
+	out.Values = in.Values
+	out.MergedValue = direct.ValueOf(in.MergedValue)
+	return out
+}
+func CatalogAttribute_FacetConfig_RerankConfig_FromProto(mapCtx *direct.MapContext, in *pb.CatalogAttribute_FacetConfig_RerankConfig) *krm.CatalogAttribute_FacetConfig_RerankConfig {
+	if in == nil {
+		return nil
+	}
+	out := &krm.CatalogAttribute_FacetConfig_RerankConfig{}
+	out.RerankFacet = direct.LazyPtr(in.GetRerankFacet())
+	out.FacetValues = in.FacetValues
+	return out
+}
+func CatalogAttribute_FacetConfig_RerankConfig_ToProto(mapCtx *direct.MapContext, in *krm.CatalogAttribute_FacetConfig_RerankConfig) *pb.CatalogAttribute_FacetConfig_RerankConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.CatalogAttribute_FacetConfig_RerankConfig{}
+	out.RerankFacet = direct.ValueOf(in.RerankFacet)
+	out.FacetValues = in.FacetValues
+	return out
+}
+func Interval_FromProto(mapCtx *direct.MapContext, in *pb.Interval) *krm.Interval {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Interval{}
+	out.Minimum = direct.LazyPtr(in.GetMinimum())
+	out.ExclusiveMinimum = direct.LazyPtr(in.GetExclusiveMinimum())
+	out.Maximum = direct.LazyPtr(in.GetMaximum())
+	out.ExclusiveMaximum = direct.LazyPtr(in.GetExclusiveMaximum())
+	return out
+}
+func Interval_ToProto(mapCtx *direct.MapContext, in *krm.Interval) *pb.Interval {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Interval{}
+	if oneof := Interval_Minimum_ToProto(mapCtx, in.Minimum); oneof != nil {
+		out.Min = oneof
+	}
+	if oneof := Interval_ExclusiveMinimum_ToProto(mapCtx, in.ExclusiveMinimum); oneof != nil {
+		out.Min = oneof
+	}
+	if oneof := Interval_Maximum_ToProto(mapCtx, in.Maximum); oneof != nil {
+		out.Max = oneof
+	}
+	if oneof := Interval_ExclusiveMaximum_ToProto(mapCtx, in.ExclusiveMaximum); oneof != nil {
+		out.Max = oneof
+	}
+	return out
+}
+func RetailAttributesConfigObservedState_FromProto(mapCtx *direct.MapContext, in *pb.AttributesConfig) *krm.RetailAttributesConfigObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.RetailAttributesConfigObservedState{}
+	// MISSING: Name
+	// MISSING: CatalogAttributes
+	// MISSING: AttributeConfigLevel
+	return out
+}
+func RetailAttributesConfigObservedState_ToProto(mapCtx *direct.MapContext, in *krm.RetailAttributesConfigObservedState) *pb.AttributesConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.AttributesConfig{}
+	// MISSING: Name
+	// MISSING: CatalogAttributes
+	// MISSING: AttributeConfigLevel
+	return out
+}
+func RetailAttributesConfigSpec_FromProto(mapCtx *direct.MapContext, in *pb.AttributesConfig) *krm.RetailAttributesConfigSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.RetailAttributesConfigSpec{}
+	// MISSING: Name
+	// MISSING: CatalogAttributes
+	// MISSING: AttributeConfigLevel
+	return out
+}
+func RetailAttributesConfigSpec_ToProto(mapCtx *direct.MapContext, in *krm.RetailAttributesConfigSpec) *pb.AttributesConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.AttributesConfig{}
+	// MISSING: Name
+	// MISSING: CatalogAttributes
+	// MISSING: AttributeConfigLevel
+	return out
+}
