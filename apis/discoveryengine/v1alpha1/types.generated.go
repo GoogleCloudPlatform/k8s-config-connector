25109e479014afdbdd45eb9ca9e437487adc92e6
Your Name <yuwenma@google.com>
2025-02-08 04:50:31 +0000

    feat: add resource DiscoveryengineChunk

--- apis/discoveryengine/v1alpha1/types.generated.go
+++ apis/discoveryengine/v1alpha1/types.generated.go
@@ -15,7 +15,7 @@
 package v1alpha1
 
 
-// +kcc:proto=google.cloud.discoveryengine.v1.Chunk
+// +kcc:proto=google.cloud.discoveryengine.v1beta.Chunk
 type Chunk struct {
 	// The full resource name of the chunk.
 	//  Format:
@@ -23,116 +23,116 @@ type Chunk struct {
 	//
 	//  This field must be a UTF-8 encoded string with a length limit of 1024
 	//  characters.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.name
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.name
 	Name *string `json:"name,omitempty"`
 
 	// Unique chunk ID of the current chunk.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.id
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.id
 	ID *string `json:"id,omitempty"`
 
 	// Content is a string from a document (parsed content).
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.content
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.content
 	Content *string `json:"content,omitempty"`
 
 	// Metadata of the document from the current chunk.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.document_metadata
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.document_metadata
 	DocumentMetadata *Chunk_DocumentMetadata `json:"documentMetadata,omitempty"`
 
 	// Page span of the chunk.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.page_span
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.page_span
 	PageSpan *Chunk_PageSpan `json:"pageSpan,omitempty"`
 }
 
-// +kcc:proto=google.cloud.discoveryengine.v1.Chunk.ChunkMetadata
+// +kcc:proto=google.cloud.discoveryengine.v1beta.Chunk.ChunkMetadata
 type Chunk_ChunkMetadata struct {
 	// The previous chunks of the current chunk. The number is controlled by
-	//  [SearchRequest.ContentSearchSpec.ChunkSpec.num_previous_chunks][google.cloud.discoveryengine.v1.SearchRequest.ContentSearchSpec.ChunkSpec.num_previous_chunks].
+	//  [SearchRequest.ContentSearchSpec.ChunkSpec.num_previous_chunks][google.cloud.discoveryengine.v1beta.SearchRequest.ContentSearchSpec.ChunkSpec.num_previous_chunks].
 	//  This field is only populated on
-	//  [SearchService.Search][google.cloud.discoveryengine.v1.SearchService.Search]
+	//  [SearchService.Search][google.cloud.discoveryengine.v1beta.SearchService.Search]
 	//  API.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.ChunkMetadata.previous_chunks
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.ChunkMetadata.previous_chunks
 	PreviousChunks []Chunk `json:"previousChunks,omitempty"`
 
 	// The next chunks of the current chunk. The number is controlled by
-	//  [SearchRequest.ContentSearchSpec.ChunkSpec.num_next_chunks][google.cloud.discoveryengine.v1.SearchRequest.ContentSearchSpec.ChunkSpec.num_next_chunks].
+	//  [SearchRequest.ContentSearchSpec.ChunkSpec.num_next_chunks][google.cloud.discoveryengine.v1beta.SearchRequest.ContentSearchSpec.ChunkSpec.num_next_chunks].
 	//  This field is only populated on
-	//  [SearchService.Search][google.cloud.discoveryengine.v1.SearchService.Search]
+	//  [SearchService.Search][google.cloud.discoveryengine.v1beta.SearchService.Search]
 	//  API.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.ChunkMetadata.next_chunks
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.ChunkMetadata.next_chunks
 	NextChunks []Chunk `json:"nextChunks,omitempty"`
 }
 
-// +kcc:proto=google.cloud.discoveryengine.v1.Chunk.DocumentMetadata
+// +kcc:proto=google.cloud.discoveryengine.v1beta.Chunk.DocumentMetadata
 type Chunk_DocumentMetadata struct {
 	// Uri of the document.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.DocumentMetadata.uri
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.DocumentMetadata.uri
 	URI *string `json:"uri,omitempty"`
 
 	// Title of the document.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.DocumentMetadata.title
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.DocumentMetadata.title
 	Title *string `json:"title,omitempty"`
 
 	// Data representation.
 	//  The structured JSON data for the document. It should conform to the
-	//  registered [Schema][google.cloud.discoveryengine.v1.Schema] or an
+	//  registered [Schema][google.cloud.discoveryengine.v1beta.Schema] or an
 	//  `INVALID_ARGUMENT` error is thrown.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.DocumentMetadata.struct_data
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.DocumentMetadata.struct_data
 	StructData map[string]string `json:"structData,omitempty"`
 }
 
-// +kcc:proto=google.cloud.discoveryengine.v1.Chunk.PageSpan
+// +kcc:proto=google.cloud.discoveryengine.v1beta.Chunk.PageSpan
 type Chunk_PageSpan struct {
 	// The start page of the chunk.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.PageSpan.page_start
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.PageSpan.page_start
 	PageStart *int32 `json:"pageStart,omitempty"`
 
 	// The end page of the chunk.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.PageSpan.page_end
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.PageSpan.page_end
 	PageEnd *int32 `json:"pageEnd,omitempty"`
 }
 
-// +kcc:proto=google.cloud.discoveryengine.v1.Chunk
+// +kcc:proto=google.cloud.discoveryengine.v1beta.Chunk
 type ChunkObservedState struct {
 	// Output only. Represents the relevance score based on similarity.
 	//  Higher score indicates higher chunk relevance.
 	//  The score is in range [-1.0, 1.0].
 	//  Only populated on [SearchService.SearchResponse][].
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.relevance_score
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.relevance_score
 	RelevanceScore *float64 `json:"relevanceScore,omitempty"`
 
 	// Output only. This field is OUTPUT_ONLY.
 	//  It contains derived data that are not in the original input document.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.derived_struct_data
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.derived_struct_data
 	DerivedStructData map[string]string `json:"derivedStructData,omitempty"`
 
 	// Output only. Represents the relevance score based on similarity.
 	//  Higher score indicates higher chunk relevance.
 	//  The score is in range [-1.0, 1.0].
 	//  Only populated on [SearchService.SearchResponse][].
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.relevance_score
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.relevance_score
 	RelevanceScore *float64 `json:"relevanceScore,omitempty"`
 
 	// Output only. This field is OUTPUT_ONLY.
 	//  It contains derived data that are not in the original input document.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.derived_struct_data
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.derived_struct_data
 	DerivedStructData map[string]string `json:"derivedStructData,omitempty"`
 
 	// Output only. Metadata of the current chunk.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.chunk_metadata
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.chunk_metadata
 	ChunkMetadata *Chunk_ChunkMetadata `json:"chunkMetadata,omitempty"`
 
 	// Output only. Metadata of the current chunk.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.chunk_metadata
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.chunk_metadata
 	ChunkMetadata *Chunk_ChunkMetadata `json:"chunkMetadata,omitempty"`
 }
 
-// +kcc:proto=google.cloud.discoveryengine.v1.Chunk.ChunkMetadata
+// +kcc:proto=google.cloud.discoveryengine.v1beta.Chunk.ChunkMetadata
 type Chunk_ChunkMetadataObservedState struct {
 	// The previous chunks of the current chunk. The number is controlled by
-	//  [SearchRequest.ContentSearchSpec.ChunkSpec.num_previous_chunks][google.cloud.discoveryengine.v1.SearchRequest.ContentSearchSpec.ChunkSpec.num_previous_chunks].
+	//  [SearchRequest.ContentSearchSpec.ChunkSpec.num_previous_chunks][google.cloud.discoveryengine.v1beta.SearchRequest.ContentSearchSpec.ChunkSpec.num_previous_chunks].
 	//  This field is only populated on
-	//  [SearchService.Search][google.cloud.discoveryengine.v1.SearchService.Search]
+	//  [SearchService.Search][google.cloud.discoveryengine.v1beta.SearchService.Search]
 	//  API.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Chunk.ChunkMetadata.previous_chunks
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Chunk.ChunkMetadata.previous_chunks
 	PreviousChunks []ChunkObservedState `json:"previousChunks,omitempty"`
 }
