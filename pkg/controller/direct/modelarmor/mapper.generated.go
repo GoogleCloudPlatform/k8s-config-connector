885bbe6fb0780c15cef07171db5eac557b5ae4b3
Your Name <yuwenma@google.com>
2025-02-08 06:54:11 +0000

    feat: add resource ModelarmorFloorSetting

---
+++ pkg/controller/direct/modelarmor/mapper.generated.go
@@ -0,0 +1,264 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package modelarmor
+
+import (
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	pb "cloud.google.com/go/modelarmor/apiv1/modelarmorpb"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/modelarmor/v1alpha1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+)
+func FilterConfig_FromProto(mapCtx *direct.MapContext, in *pb.FilterConfig) *krm.FilterConfig {
+	if in == nil {
+		return nil
+	}
+	out := &krm.FilterConfig{}
+	out.RaiSettings = RaiFilterSettings_FromProto(mapCtx, in.GetRaiSettings())
+	out.SdpSettings = SdpFilterSettings_FromProto(mapCtx, in.GetSdpSettings())
+	out.PiAndJailbreakFilterSettings = PiAndJailbreakFilterSettings_FromProto(mapCtx, in.GetPiAndJailbreakFilterSettings())
+	out.MaliciousURIFilterSettings = MaliciousUriFilterSettings_FromProto(mapCtx, in.GetMaliciousUriFilterSettings())
+	return out
+}
+func FilterConfig_ToProto(mapCtx *direct.MapContext, in *krm.FilterConfig) *pb.FilterConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.FilterConfig{}
+	out.RaiSettings = RaiFilterSettings_ToProto(mapCtx, in.RaiSettings)
+	out.SdpSettings = SdpFilterSettings_ToProto(mapCtx, in.SdpSettings)
+	out.PiAndJailbreakFilterSettings = PiAndJailbreakFilterSettings_ToProto(mapCtx, in.PiAndJailbreakFilterSettings)
+	out.MaliciousUriFilterSettings = MaliciousUriFilterSettings_ToProto(mapCtx, in.MaliciousURIFilterSettings)
+	return out
+}
+func FloorSetting_FromProto(mapCtx *direct.MapContext, in *pb.FloorSetting) *krm.FloorSetting {
+	if in == nil {
+		return nil
+	}
+	out := &krm.FloorSetting{}
+	out.Name = direct.LazyPtr(in.GetName())
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	out.FilterConfig = FilterConfig_FromProto(mapCtx, in.GetFilterConfig())
+	out.EnableFloorSettingEnforcement = direct.LazyPtr(in.GetEnableFloorSettingEnforcement())
+	return out
+}
+func FloorSetting_ToProto(mapCtx *direct.MapContext, in *krm.FloorSetting) *pb.FloorSetting {
+	if in == nil {
+		return nil
+	}
+	out := &pb.FloorSetting{}
+	out.Name = direct.ValueOf(in.Name)
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	out.FilterConfig = FilterConfig_ToProto(mapCtx, in.FilterConfig)
+	out.EnableFloorSettingEnforcement = direct.ValueOf(in.EnableFloorSettingEnforcement)
+	return out
+}
+func FloorSettingObservedState_FromProto(mapCtx *direct.MapContext, in *pb.FloorSetting) *krm.FloorSettingObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.FloorSettingObservedState{}
+	// MISSING: Name
+	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
+	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
+	// MISSING: FilterConfig
+	// MISSING: EnableFloorSettingEnforcement
+	return out
+}
+func FloorSettingObservedState_ToProto(mapCtx *direct.MapContext, in *krm.FloorSettingObservedState) *pb.FloorSetting {
+	if in == nil {
+		return nil
+	}
+	out := &pb.FloorSetting{}
+	// MISSING: Name
+	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
+	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
+	// MISSING: FilterConfig
+	// MISSING: EnableFloorSettingEnforcement
+	return out
+}
+func MaliciousUriFilterSettings_FromProto(mapCtx *direct.MapContext, in *pb.MaliciousUriFilterSettings) *krm.MaliciousUriFilterSettings {
+	if in == nil {
+		return nil
+	}
+	out := &krm.MaliciousUriFilterSettings{}
+	out.FilterEnforcement = direct.Enum_FromProto(mapCtx, in.GetFilterEnforcement())
+	return out
+}
+func MaliciousUriFilterSettings_ToProto(mapCtx *direct.MapContext, in *krm.MaliciousUriFilterSettings) *pb.MaliciousUriFilterSettings {
+	if in == nil {
+		return nil
+	}
+	out := &pb.MaliciousUriFilterSettings{}
+	out.FilterEnforcement = direct.Enum_ToProto[pb.MaliciousUriFilterSettings_MaliciousUriFilterEnforcement](mapCtx, in.FilterEnforcement)
+	return out
+}
+func ModelarmorFloorSettingObservedState_FromProto(mapCtx *direct.MapContext, in *pb.FloorSetting) *krm.ModelarmorFloorSettingObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ModelarmorFloorSettingObservedState{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: FilterConfig
+	// MISSING: EnableFloorSettingEnforcement
+	return out
+}
+func ModelarmorFloorSettingObservedState_ToProto(mapCtx *direct.MapContext, in *krm.ModelarmorFloorSettingObservedState) *pb.FloorSetting {
+	if in == nil {
+		return nil
+	}
+	out := &pb.FloorSetting{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: FilterConfig
+	// MISSING: EnableFloorSettingEnforcement
+	return out
+}
+func ModelarmorFloorSettingSpec_FromProto(mapCtx *direct.MapContext, in *pb.FloorSetting) *krm.ModelarmorFloorSettingSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ModelarmorFloorSettingSpec{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: FilterConfig
+	// MISSING: EnableFloorSettingEnforcement
+	return out
+}
+func ModelarmorFloorSettingSpec_ToProto(mapCtx *direct.MapContext, in *krm.ModelarmorFloorSettingSpec) *pb.FloorSetting {
+	if in == nil {
+		return nil
+	}
+	out := &pb.FloorSetting{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: FilterConfig
+	// MISSING: EnableFloorSettingEnforcement
+	return out
+}
+func PiAndJailbreakFilterSettings_FromProto(mapCtx *direct.MapContext, in *pb.PiAndJailbreakFilterSettings) *krm.PiAndJailbreakFilterSettings {
+	if in == nil {
+		return nil
+	}
+	out := &krm.PiAndJailbreakFilterSettings{}
+	out.FilterEnforcement = direct.Enum_FromProto(mapCtx, in.GetFilterEnforcement())
+	out.ConfidenceLevel = direct.Enum_FromProto(mapCtx, in.GetConfidenceLevel())
+	return out
+}
+func PiAndJailbreakFilterSettings_ToProto(mapCtx *direct.MapContext, in *krm.PiAndJailbreakFilterSettings) *pb.PiAndJailbreakFilterSettings {
+	if in == nil {
+		return nil
+	}
+	out := &pb.PiAndJailbreakFilterSettings{}
+	out.FilterEnforcement = direct.Enum_ToProto[pb.PiAndJailbreakFilterSettings_PiAndJailbreakFilterEnforcement](mapCtx, in.FilterEnforcement)
+	out.ConfidenceLevel = direct.Enum_ToProto[pb.DetectionConfidenceLevel](mapCtx, in.ConfidenceLevel)
+	return out
+}
+func RaiFilterSettings_FromProto(mapCtx *direct.MapContext, in *pb.RaiFilterSettings) *krm.RaiFilterSettings {
+	if in == nil {
+		return nil
+	}
+	out := &krm.RaiFilterSettings{}
+	out.RaiFilters = direct.Slice_FromProto(mapCtx, in.RaiFilters, RaiFilterSettings_RaiFilter_FromProto)
+	return out
+}
+func RaiFilterSettings_ToProto(mapCtx *direct.MapContext, in *krm.RaiFilterSettings) *pb.RaiFilterSettings {
+	if in == nil {
+		return nil
+	}
+	out := &pb.RaiFilterSettings{}
+	out.RaiFilters = direct.Slice_ToProto(mapCtx, in.RaiFilters, RaiFilterSettings_RaiFilter_ToProto)
+	return out
+}
+func RaiFilterSettings_RaiFilter_FromProto(mapCtx *direct.MapContext, in *pb.RaiFilterSettings_RaiFilter) *krm.RaiFilterSettings_RaiFilter {
+	if in == nil {
+		return nil
+	}
+	out := &krm.RaiFilterSettings_RaiFilter{}
+	out.FilterType = direct.Enum_FromProto(mapCtx, in.GetFilterType())
+	out.ConfidenceLevel = direct.Enum_FromProto(mapCtx, in.GetConfidenceLevel())
+	return out
+}
+func RaiFilterSettings_RaiFilter_ToProto(mapCtx *direct.MapContext, in *krm.RaiFilterSettings_RaiFilter) *pb.RaiFilterSettings_RaiFilter {
+	if in == nil {
+		return nil
+	}
+	out := &pb.RaiFilterSettings_RaiFilter{}
+	out.FilterType = direct.Enum_ToProto[pb.RaiFilterType](mapCtx, in.FilterType)
+	out.ConfidenceLevel = direct.Enum_ToProto[pb.DetectionConfidenceLevel](mapCtx, in.ConfidenceLevel)
+	return out
+}
+func SdpAdvancedConfig_FromProto(mapCtx *direct.MapContext, in *pb.SdpAdvancedConfig) *krm.SdpAdvancedConfig {
+	if in == nil {
+		return nil
+	}
+	out := &krm.SdpAdvancedConfig{}
+	out.InspectTemplate = direct.LazyPtr(in.GetInspectTemplate())
+	out.DeidentifyTemplate = direct.LazyPtr(in.GetDeidentifyTemplate())
+	return out
+}
+func SdpAdvancedConfig_ToProto(mapCtx *direct.MapContext, in *krm.SdpAdvancedConfig) *pb.SdpAdvancedConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.SdpAdvancedConfig{}
+	out.InspectTemplate = direct.ValueOf(in.InspectTemplate)
+	out.DeidentifyTemplate = direct.ValueOf(in.DeidentifyTemplate)
+	return out
+}
+func SdpBasicConfig_FromProto(mapCtx *direct.MapContext, in *pb.SdpBasicConfig) *krm.SdpBasicConfig {
+	if in == nil {
+		return nil
+	}
+	out := &krm.SdpBasicConfig{}
+	out.FilterEnforcement = direct.Enum_FromProto(mapCtx, in.GetFilterEnforcement())
+	return out
+}
+func SdpBasicConfig_ToProto(mapCtx *direct.MapContext, in *krm.SdpBasicConfig) *pb.SdpBasicConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.SdpBasicConfig{}
+	out.FilterEnforcement = direct.Enum_ToProto[pb.SdpBasicConfig_SdpBasicConfigEnforcement](mapCtx, in.FilterEnforcement)
+	return out
+}
+func SdpFilterSettings_FromProto(mapCtx *direct.MapContext, in *pb.SdpFilterSettings) *krm.SdpFilterSettings {
+	if in == nil {
+		return nil
+	}
+	out := &krm.SdpFilterSettings{}
+	out.BasicConfig = SdpBasicConfig_FromProto(mapCtx, in.GetBasicConfig())
+	out.AdvancedConfig = SdpAdvancedConfig_FromProto(mapCtx, in.GetAdvancedConfig())
+	return out
+}
+func SdpFilterSettings_ToProto(mapCtx *direct.MapContext, in *krm.SdpFilterSettings) *pb.SdpFilterSettings {
+	if in == nil {
+		return nil
+	}
+	out := &pb.SdpFilterSettings{}
+	if oneof := SdpBasicConfig_ToProto(mapCtx, in.BasicConfig); oneof != nil {
+		out.SdpConfiguration = &pb.SdpFilterSettings_BasicConfig{BasicConfig: oneof}
+	}
+	if oneof := SdpAdvancedConfig_ToProto(mapCtx, in.AdvancedConfig); oneof != nil {
+		out.SdpConfiguration = &pb.SdpFilterSettings_AdvancedConfig{AdvancedConfig: oneof}
+	}
+	return out
+}
