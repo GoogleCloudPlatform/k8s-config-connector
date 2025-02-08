2f73f8c5f82e713e57d81cf6f4e0f529a34a41c3
Your Name <yuwenma@google.com>
2025-02-08 14:23:29 +0000

    feat: add resource SpannerInstanceConfig

--- pkg/controller/direct/spanner/mapper.generated.go
+++ pkg/controller/direct/spanner/mapper.generated.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,11 +15,12 @@
 package spanner
 
 import (
-	pb "cloud.google.com/go/spanner/admin/instance/apiv1/instancepb"
 	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/spanner/v1beta1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/spanner/v1alpha1"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	pb "cloud.google.com/go/spanner/admin/instance/apiv1/instancepb"
 )
-
 func AutoscalingConfig_FromProto(mapCtx *direct.MapContext, in *pb.AutoscalingConfig) *krm.AutoscalingConfig {
 	if in == nil {
 		return nil
@@ -27,6 +28,7 @@ func AutoscalingConfig_FromProto(mapCtx *direct.MapContext, in *pb.AutoscalingCo
 	out := &krm.AutoscalingConfig{}
 	out.AutoscalingLimits = AutoscalingConfig_AutoscalingLimits_FromProto(mapCtx, in.GetAutoscalingLimits())
 	out.AutoscalingTargets = AutoscalingConfig_AutoscalingTargets_FromProto(mapCtx, in.GetAutoscalingTargets())
+	// MISSING: AsymmetricAutoscalingOptions
 	return out
 }
 func AutoscalingConfig_ToProto(mapCtx *direct.MapContext, in *krm.AutoscalingConfig) *pb.AutoscalingConfig {
@@ -36,6 +38,7 @@ func AutoscalingConfig_ToProto(mapCtx *direct.MapContext, in *krm.AutoscalingCon
 	out := &pb.AutoscalingConfig{}
 	out.AutoscalingLimits = AutoscalingConfig_AutoscalingLimits_ToProto(mapCtx, in.AutoscalingLimits)
 	out.AutoscalingTargets = AutoscalingConfig_AutoscalingTargets_ToProto(mapCtx, in.AutoscalingTargets)
+	// MISSING: AsymmetricAutoscalingOptions
 	return out
 }
 func AutoscalingConfig_AsymmetricAutoscalingOption_FromProto(mapCtx *direct.MapContext, in *pb.AutoscalingConfig_AsymmetricAutoscalingOption) *krm.AutoscalingConfig_AsymmetricAutoscalingOption {
@@ -122,7 +125,7 @@ func AutoscalingConfig_AutoscalingTargets_ToProto(mapCtx *direct.MapContext, in 
 	out.StorageUtilizationPercent = direct.ValueOf(in.StorageUtilizationPercent)
 	return out
 }
-func Instance_FromProto(mapCtx *direct.MapContext, in *pb.Instance, parent *string) *krm.Instance {
+func Instance_FromProto(mapCtx *direct.MapContext, in *pb.Instance) *krm.Instance {
 	if in == nil {
 		return nil
 	}
@@ -134,12 +137,15 @@ func Instance_FromProto(mapCtx *direct.MapContext, in *pb.Instance, parent *stri
 	out.ProcessingUnits = direct.LazyPtr(in.GetProcessingUnits())
 	out.ReplicaComputeCapacity = direct.Slice_FromProto(mapCtx, in.ReplicaComputeCapacity, ReplicaComputeCapacity_FromProto)
 	out.AutoscalingConfig = AutoscalingConfig_FromProto(mapCtx, in.GetAutoscalingConfig())
-	out.State = State_FromProto(mapCtx, in)
+	out.State = direct.Enum_FromProto(mapCtx, in.GetState())
 	out.Labels = in.Labels
+	// MISSING: InstanceType
 	out.EndpointUris = in.EndpointUris
 	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
 	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
+	// MISSING: FreeInstanceMetadata
 	out.Edition = direct.Enum_FromProto(mapCtx, in.GetEdition())
+	out.DefaultBackupScheduleType = direct.Enum_FromProto(mapCtx, in.GetDefaultBackupScheduleType())
 	return out
 }
 func Instance_ToProto(mapCtx *direct.MapContext, in *krm.Instance) *pb.Instance {
@@ -156,10 +162,97 @@ func Instance_ToProto(mapCtx *direct.MapContext, in *krm.Instance) *pb.Instance 
 	out.AutoscalingConfig = AutoscalingConfig_ToProto(mapCtx, in.AutoscalingConfig)
 	out.State = direct.Enum_ToProto[pb.Instance_State](mapCtx, in.State)
 	out.Labels = in.Labels
+	// MISSING: InstanceType
 	out.EndpointUris = in.EndpointUris
 	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
 	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
+	// MISSING: FreeInstanceMetadata
 	out.Edition = direct.Enum_ToProto[pb.Instance_Edition](mapCtx, in.Edition)
+	out.DefaultBackupScheduleType = direct.Enum_ToProto[pb.Instance_DefaultBackupScheduleType](mapCtx, in.DefaultBackupScheduleType)
+	return out
+}
+func InstanceConfig_FromProto(mapCtx *direct.MapContext, in *pb.InstanceConfig) *krm.InstanceConfig {
+	if in == nil {
+		return nil
+	}
+	out := &krm.InstanceConfig{}
+	out.Name = direct.LazyPtr(in.GetName())
+	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
+	// MISSING: ConfigType
+	out.Replicas = direct.Slice_FromProto(mapCtx, in.Replicas, ReplicaInfo_FromProto)
+	// MISSING: OptionalReplicas
+	out.BaseConfig = direct.LazyPtr(in.GetBaseConfig())
+	out.Labels = in.Labels
+	out.Etag = direct.LazyPtr(in.GetEtag())
+	out.LeaderOptions = in.LeaderOptions
+	// MISSING: Reconciling
+	// MISSING: State
+	// MISSING: FreeInstanceAvailability
+	// MISSING: QuorumType
+	// MISSING: StorageLimitPerProcessingUnit
+	return out
+}
+func InstanceConfig_ToProto(mapCtx *direct.MapContext, in *krm.InstanceConfig) *pb.InstanceConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.InstanceConfig{}
+	out.Name = direct.ValueOf(in.Name)
+	out.DisplayName = direct.ValueOf(in.DisplayName)
+	// MISSING: ConfigType
+	out.Replicas = direct.Slice_ToProto(mapCtx, in.Replicas, ReplicaInfo_ToProto)
+	// MISSING: OptionalReplicas
+	out.BaseConfig = direct.ValueOf(in.BaseConfig)
+	out.Labels = in.Labels
+	out.Etag = direct.ValueOf(in.Etag)
+	out.LeaderOptions = in.LeaderOptions
+	// MISSING: Reconciling
+	// MISSING: State
+	// MISSING: FreeInstanceAvailability
+	// MISSING: QuorumType
+	// MISSING: StorageLimitPerProcessingUnit
+	return out
+}
+func InstanceConfigObservedState_FromProto(mapCtx *direct.MapContext, in *pb.InstanceConfig) *krm.InstanceConfigObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.InstanceConfigObservedState{}
+	// MISSING: Name
+	// MISSING: DisplayName
+	out.ConfigType = direct.Enum_FromProto(mapCtx, in.GetConfigType())
+	// MISSING: Replicas
+	out.OptionalReplicas = direct.Slice_FromProto(mapCtx, in.OptionalReplicas, ReplicaInfo_FromProto)
+	// MISSING: BaseConfig
+	// MISSING: Labels
+	// MISSING: Etag
+	// MISSING: LeaderOptions
+	out.Reconciling = direct.LazyPtr(in.GetReconciling())
+	out.State = direct.Enum_FromProto(mapCtx, in.GetState())
+	out.FreeInstanceAvailability = direct.Enum_FromProto(mapCtx, in.GetFreeInstanceAvailability())
+	out.QuorumType = direct.Enum_FromProto(mapCtx, in.GetQuorumType())
+	out.StorageLimitPerProcessingUnit = direct.LazyPtr(in.GetStorageLimitPerProcessingUnit())
+	return out
+}
+func InstanceConfigObservedState_ToProto(mapCtx *direct.MapContext, in *krm.InstanceConfigObservedState) *pb.InstanceConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.InstanceConfig{}
+	// MISSING: Name
+	// MISSING: DisplayName
+	out.ConfigType = direct.Enum_ToProto[pb.InstanceConfig_Type](mapCtx, in.ConfigType)
+	// MISSING: Replicas
+	out.OptionalReplicas = direct.Slice_ToProto(mapCtx, in.OptionalReplicas, ReplicaInfo_ToProto)
+	// MISSING: BaseConfig
+	// MISSING: Labels
+	// MISSING: Etag
+	// MISSING: LeaderOptions
+	out.Reconciling = direct.ValueOf(in.Reconciling)
+	out.State = direct.Enum_ToProto[pb.InstanceConfig_State](mapCtx, in.State)
+	out.FreeInstanceAvailability = direct.Enum_ToProto[pb.InstanceConfig_FreeInstanceAvailability](mapCtx, in.FreeInstanceAvailability)
+	out.QuorumType = direct.Enum_ToProto[pb.InstanceConfig_QuorumType](mapCtx, in.QuorumType)
+	out.StorageLimitPerProcessingUnit = direct.ValueOf(in.StorageLimitPerProcessingUnit)
 	return out
 }
 func ReplicaComputeCapacity_FromProto(mapCtx *direct.MapContext, in *pb.ReplicaComputeCapacity) *krm.ReplicaComputeCapacity {
@@ -186,6 +279,26 @@ func ReplicaComputeCapacity_ToProto(mapCtx *direct.MapContext, in *krm.ReplicaCo
 	}
 	return out
 }
+func ReplicaInfo_FromProto(mapCtx *direct.MapContext, in *pb.ReplicaInfo) *krm.ReplicaInfo {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ReplicaInfo{}
+	out.Location = direct.LazyPtr(in.GetLocation())
+	out.Type = direct.Enum_FromProto(mapCtx, in.GetType())
+	out.DefaultLeaderLocation = direct.LazyPtr(in.GetDefaultLeaderLocation())
+	return out
+}
+func ReplicaInfo_ToProto(mapCtx *direct.MapContext, in *krm.ReplicaInfo) *pb.ReplicaInfo {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ReplicaInfo{}
+	out.Location = direct.ValueOf(in.Location)
+	out.Type = direct.Enum_ToProto[pb.ReplicaInfo_ReplicaType](mapCtx, in.Type)
+	out.DefaultLeaderLocation = direct.ValueOf(in.DefaultLeaderLocation)
+	return out
+}
 func ReplicaSelection_FromProto(mapCtx *direct.MapContext, in *pb.ReplicaSelection) *krm.ReplicaSelection {
 	if in == nil {
 		return nil
@@ -200,5 +313,89 @@ func ReplicaSelection_ToProto(mapCtx *direct.MapContext, in *krm.ReplicaSelectio
 	}
 	out := &pb.ReplicaSelection{}
 	out.Location = direct.ValueOf(in.Location)
+	return out
+}
+func SpannerInstanceConfigObservedState_FromProto(mapCtx *direct.MapContext, in *pb.InstanceConfig) *krm.SpannerInstanceConfigObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.SpannerInstanceConfigObservedState{}
+	// MISSING: Name
+	// MISSING: DisplayName
+	// MISSING: ConfigType
+	// MISSING: Replicas
+	// MISSING: OptionalReplicas
+	// MISSING: BaseConfig
+	// MISSING: Labels
+	// MISSING: Etag
+	// MISSING: LeaderOptions
+	// MISSING: Reconciling
+	// MISSING: State
+	// MISSING: FreeInstanceAvailability
+	// MISSING: QuorumType
+	// MISSING: StorageLimitPerProcessingUnit
+	return out
+}
+func SpannerInstanceConfigObservedState_ToProto(mapCtx *direct.MapContext, in *krm.SpannerInstanceConfigObservedState) *pb.InstanceConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.InstanceConfig{}
+	// MISSING: Name
+	// MISSING: DisplayName
+	// MISSING: ConfigType
+	// MISSING: Replicas
+	// MISSING: OptionalReplicas
+	// MISSING: BaseConfig
+	// MISSING: Labels
+	// MISSING: Etag
+	// MISSING: LeaderOptions
+	// MISSING: Reconciling
+	// MISSING: State
+	// MISSING: FreeInstanceAvailability
+	// MISSING: QuorumType
+	// MISSING: StorageLimitPerProcessingUnit
+	return out
+}
+func SpannerInstanceConfigSpec_FromProto(mapCtx *direct.MapContext, in *pb.InstanceConfig) *krm.SpannerInstanceConfigSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.SpannerInstanceConfigSpec{}
+	// MISSING: Name
+	// MISSING: DisplayName
+	// MISSING: ConfigType
+	// MISSING: Replicas
+	// MISSING: OptionalReplicas
+	// MISSING: BaseConfig
+	// MISSING: Labels
+	// MISSING: Etag
+	// MISSING: LeaderOptions
+	// MISSING: Reconciling
+	// MISSING: State
+	// MISSING: FreeInstanceAvailability
+	// MISSING: QuorumType
+	// MISSING: StorageLimitPerProcessingUnit
+	return out
+}
+func SpannerInstanceConfigSpec_ToProto(mapCtx *direct.MapContext, in *krm.SpannerInstanceConfigSpec) *pb.InstanceConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.InstanceConfig{}
+	// MISSING: Name
+	// MISSING: DisplayName
+	// MISSING: ConfigType
+	// MISSING: Replicas
+	// MISSING: OptionalReplicas
+	// MISSING: BaseConfig
+	// MISSING: Labels
+	// MISSING: Etag
+	// MISSING: LeaderOptions
+	// MISSING: Reconciling
+	// MISSING: State
+	// MISSING: FreeInstanceAvailability
+	// MISSING: QuorumType
+	// MISSING: StorageLimitPerProcessingUnit
 	return out
 }
