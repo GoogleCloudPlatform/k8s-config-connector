e554cf8fb8a7626f009946d206cb27a0d13a788f
Your Name <yuwenma@google.com>
2025-02-08 10:22:59 +0000

    feat: add resource SecuritycenterAttackPath

---
+++ apis/securitycenter/v1alpha1/types.generated.go
@@ -0,0 +1,113 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+
+// +kcc:proto=google.cloud.securitycenter.v1.AttackPath
+type AttackPath struct {
+	// The attack path name, for example,
+	//   `organizations/12/simulation/34/valuedResources/56/attackPaths/78`
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.name
+	Name *string `json:"name,omitempty"`
+
+	// A list of nodes that exist in this attack path.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.path_nodes
+	PathNodes []AttackPath_AttackPathNode `json:"pathNodes,omitempty"`
+
+	// A list of the edges between nodes in this attack path.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.edges
+	Edges []AttackPath_AttackPathEdge `json:"edges,omitempty"`
+}
+
+// +kcc:proto=google.cloud.securitycenter.v1.AttackPath.AttackPathEdge
+type AttackPath_AttackPathEdge struct {
+	// The attack node uuid of the source node.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.AttackPathEdge.source
+	Source *string `json:"source,omitempty"`
+
+	// The attack node uuid of the destination node.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.AttackPathEdge.destination
+	Destination *string `json:"destination,omitempty"`
+}
+
+// +kcc:proto=google.cloud.securitycenter.v1.AttackPath.AttackPathNode
+type AttackPath_AttackPathNode struct {
+	// The name of the resource at this point in the attack path.
+	//  The format of the name follows the Cloud Asset Inventory [resource
+	//  name
+	//  format](https://cloud.google.com/asset-inventory/docs/resource-name-format)
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.AttackPathNode.resource
+	Resource *string `json:"resource,omitempty"`
+
+	// The [supported resource
+	//  type](https://cloud.google.com/asset-inventory/docs/supported-asset-types)
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.AttackPathNode.resource_type
+	ResourceType *string `json:"resourceType,omitempty"`
+
+	// Human-readable name of this resource.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.AttackPathNode.display_name
+	DisplayName *string `json:"displayName,omitempty"`
+
+	// The findings associated with this node in the attack path.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.AttackPathNode.associated_findings
+	AssociatedFindings []AttackPath_AttackPathNode_PathNodeAssociatedFinding `json:"associatedFindings,omitempty"`
+
+	// Unique id of the attack path node.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.AttackPathNode.uuid
+	Uuid *string `json:"uuid,omitempty"`
+
+	// A list of attack step nodes that exist in this attack path node.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.AttackPathNode.attack_steps
+	AttackSteps []AttackPath_AttackPathNode_AttackStepNode `json:"attackSteps,omitempty"`
+}
+
+// +kcc:proto=google.cloud.securitycenter.v1.AttackPath.AttackPathNode.AttackStepNode
+type AttackPath_AttackPathNode_AttackStepNode struct {
+	// Unique ID for one Node
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.AttackPathNode.AttackStepNode.uuid
+	Uuid *string `json:"uuid,omitempty"`
+
+	// Attack step type. Can be either AND, OR or DEFENSE
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.AttackPathNode.AttackStepNode.type
+	Type *string `json:"type,omitempty"`
+
+	// User friendly name of the attack step
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.AttackPathNode.AttackStepNode.display_name
+	DisplayName *string `json:"displayName,omitempty"`
+
+	// Attack step labels for metadata
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.AttackPathNode.AttackStepNode.labels
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// Attack step description
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.AttackPathNode.AttackStepNode.description
+	Description *string `json:"description,omitempty"`
+}
+
+// +kcc:proto=google.cloud.securitycenter.v1.AttackPath.AttackPathNode.PathNodeAssociatedFinding
+type AttackPath_AttackPathNode_PathNodeAssociatedFinding struct {
+	// Canonical name of the associated findings. Example:
+	//  `organizations/123/sources/456/findings/789`
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.AttackPathNode.PathNodeAssociatedFinding.canonical_finding
+	CanonicalFinding *string `json:"canonicalFinding,omitempty"`
+
+	// The additional taxonomy group within findings from a given source.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.AttackPathNode.PathNodeAssociatedFinding.finding_category
+	FindingCategory *string `json:"findingCategory,omitempty"`
+
+	// Full resource name of the finding.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.AttackPath.AttackPathNode.PathNodeAssociatedFinding.name
+	Name *string `json:"name,omitempty"`
+}
---
+++ pkg/controller/direct/securitycenter/mapper.generated.go
@@ -0,0 +1,170 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package securitycenter
+
+import (
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	pb "cloud.google.com/go/securitycenter/apiv1/securitycenterpb"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/securitycenter/v1alpha1"
+)
+func AttackPath_FromProto(mapCtx *direct.MapContext, in *pb.AttackPath) *krm.AttackPath {
+	if in == nil {
+		return nil
+	}
+	out := &krm.AttackPath{}
+	out.Name = direct.LazyPtr(in.GetName())
+	out.PathNodes = direct.Slice_FromProto(mapCtx, in.PathNodes, AttackPath_AttackPathNode_FromProto)
+	out.Edges = direct.Slice_FromProto(mapCtx, in.Edges, AttackPath_AttackPathEdge_FromProto)
+	return out
+}
+func AttackPath_ToProto(mapCtx *direct.MapContext, in *krm.AttackPath) *pb.AttackPath {
+	if in == nil {
+		return nil
+	}
+	out := &pb.AttackPath{}
+	out.Name = direct.ValueOf(in.Name)
+	out.PathNodes = direct.Slice_ToProto(mapCtx, in.PathNodes, AttackPath_AttackPathNode_ToProto)
+	out.Edges = direct.Slice_ToProto(mapCtx, in.Edges, AttackPath_AttackPathEdge_ToProto)
+	return out
+}
+func AttackPath_AttackPathEdge_FromProto(mapCtx *direct.MapContext, in *pb.AttackPath_AttackPathEdge) *krm.AttackPath_AttackPathEdge {
+	if in == nil {
+		return nil
+	}
+	out := &krm.AttackPath_AttackPathEdge{}
+	out.Source = direct.LazyPtr(in.GetSource())
+	out.Destination = direct.LazyPtr(in.GetDestination())
+	return out
+}
+func AttackPath_AttackPathEdge_ToProto(mapCtx *direct.MapContext, in *krm.AttackPath_AttackPathEdge) *pb.AttackPath_AttackPathEdge {
+	if in == nil {
+		return nil
+	}
+	out := &pb.AttackPath_AttackPathEdge{}
+	out.Source = direct.ValueOf(in.Source)
+	out.Destination = direct.ValueOf(in.Destination)
+	return out
+}
+func AttackPath_AttackPathNode_FromProto(mapCtx *direct.MapContext, in *pb.AttackPath_AttackPathNode) *krm.AttackPath_AttackPathNode {
+	if in == nil {
+		return nil
+	}
+	out := &krm.AttackPath_AttackPathNode{}
+	out.Resource = direct.LazyPtr(in.GetResource())
+	out.ResourceType = direct.LazyPtr(in.GetResourceType())
+	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
+	out.AssociatedFindings = direct.Slice_FromProto(mapCtx, in.AssociatedFindings, AttackPath_AttackPathNode_PathNodeAssociatedFinding_FromProto)
+	out.Uuid = direct.LazyPtr(in.GetUuid())
+	out.AttackSteps = direct.Slice_FromProto(mapCtx, in.AttackSteps, AttackPath_AttackPathNode_AttackStepNode_FromProto)
+	return out
+}
+func AttackPath_AttackPathNode_ToProto(mapCtx *direct.MapContext, in *krm.AttackPath_AttackPathNode) *pb.AttackPath_AttackPathNode {
+	if in == nil {
+		return nil
+	}
+	out := &pb.AttackPath_AttackPathNode{}
+	out.Resource = direct.ValueOf(in.Resource)
+	out.ResourceType = direct.ValueOf(in.ResourceType)
+	out.DisplayName = direct.ValueOf(in.DisplayName)
+	out.AssociatedFindings = direct.Slice_ToProto(mapCtx, in.AssociatedFindings, AttackPath_AttackPathNode_PathNodeAssociatedFinding_ToProto)
+	out.Uuid = direct.ValueOf(in.Uuid)
+	out.AttackSteps = direct.Slice_ToProto(mapCtx, in.AttackSteps, AttackPath_AttackPathNode_AttackStepNode_ToProto)
+	return out
+}
+func AttackPath_AttackPathNode_AttackStepNode_FromProto(mapCtx *direct.MapContext, in *pb.AttackPath_AttackPathNode_AttackStepNode) *krm.AttackPath_AttackPathNode_AttackStepNode {
+	if in == nil {
+		return nil
+	}
+	out := &krm.AttackPath_AttackPathNode_AttackStepNode{}
+	out.Uuid = direct.LazyPtr(in.GetUuid())
+	out.Type = direct.Enum_FromProto(mapCtx, in.GetType())
+	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
+	out.Labels = in.Labels
+	out.Description = direct.LazyPtr(in.GetDescription())
+	return out
+}
+func AttackPath_AttackPathNode_AttackStepNode_ToProto(mapCtx *direct.MapContext, in *krm.AttackPath_AttackPathNode_AttackStepNode) *pb.AttackPath_AttackPathNode_AttackStepNode {
+	if in == nil {
+		return nil
+	}
+	out := &pb.AttackPath_AttackPathNode_AttackStepNode{}
+	out.Uuid = direct.ValueOf(in.Uuid)
+	out.Type = direct.Enum_ToProto[pb.AttackPath_AttackPathNode_NodeType](mapCtx, in.Type)
+	out.DisplayName = direct.ValueOf(in.DisplayName)
+	out.Labels = in.Labels
+	out.Description = direct.ValueOf(in.Description)
+	return out
+}
+func AttackPath_AttackPathNode_PathNodeAssociatedFinding_FromProto(mapCtx *direct.MapContext, in *pb.AttackPath_AttackPathNode_PathNodeAssociatedFinding) *krm.AttackPath_AttackPathNode_PathNodeAssociatedFinding {
+	if in == nil {
+		return nil
+	}
+	out := &krm.AttackPath_AttackPathNode_PathNodeAssociatedFinding{}
+	out.CanonicalFinding = direct.LazyPtr(in.GetCanonicalFinding())
+	out.FindingCategory = direct.LazyPtr(in.GetFindingCategory())
+	out.Name = direct.LazyPtr(in.GetName())
+	return out
+}
+func AttackPath_AttackPathNode_PathNodeAssociatedFinding_ToProto(mapCtx *direct.MapContext, in *krm.AttackPath_AttackPathNode_PathNodeAssociatedFinding) *pb.AttackPath_AttackPathNode_PathNodeAssociatedFinding {
+	if in == nil {
+		return nil
+	}
+	out := &pb.AttackPath_AttackPathNode_PathNodeAssociatedFinding{}
+	out.CanonicalFinding = direct.ValueOf(in.CanonicalFinding)
+	out.FindingCategory = direct.ValueOf(in.FindingCategory)
+	out.Name = direct.ValueOf(in.Name)
+	return out
+}
+func SecuritycenterAttackPathObservedState_FromProto(mapCtx *direct.MapContext, in *pb.AttackPath) *krm.SecuritycenterAttackPathObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.SecuritycenterAttackPathObservedState{}
+	// MISSING: Name
+	// MISSING: PathNodes
+	// MISSING: Edges
+	return out
+}
+func SecuritycenterAttackPathObservedState_ToProto(mapCtx *direct.MapContext, in *krm.SecuritycenterAttackPathObservedState) *pb.AttackPath {
+	if in == nil {
+		return nil
+	}
+	out := &pb.AttackPath{}
+	// MISSING: Name
+	// MISSING: PathNodes
+	// MISSING: Edges
+	return out
+}
+func SecuritycenterAttackPathSpec_FromProto(mapCtx *direct.MapContext, in *pb.AttackPath) *krm.SecuritycenterAttackPathSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.SecuritycenterAttackPathSpec{}
+	// MISSING: Name
+	// MISSING: PathNodes
+	// MISSING: Edges
+	return out
+}
+func SecuritycenterAttackPathSpec_ToProto(mapCtx *direct.MapContext, in *krm.SecuritycenterAttackPathSpec) *pb.AttackPath {
+	if in == nil {
+		return nil
+	}
+	out := &pb.AttackPath{}
+	// MISSING: Name
+	// MISSING: PathNodes
+	// MISSING: Edges
+	return out
+}
