7cbe4a8b8d5ae254915b4bf69ef619675e8b9121
Your Name <yuwenma@google.com>
2025-02-08 10:07:30 +0000

    feat: add resource SecuresourcemanagerBranchRule

--- pkg/controller/direct/securesourcemanager/mapper.generated.go
+++ pkg/controller/direct/securesourcemanager/mapper.generated.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,74 +15,113 @@
 package securesourcemanager
 
 import (
-	pb "cloud.google.com/go/securesourcemanager/apiv1/securesourcemanagerpb"
 	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	pb "cloud.google.com/go/securesourcemanager/apiv1/securesourcemanagerpb"
 	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/securesourcemanager/v1alpha1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 )
-
-func Instance_HostConfig_FromProto(mapCtx *direct.MapContext, in *pb.Instance_HostConfig) *krm.Instance_HostConfig {
+func BranchRule_FromProto(mapCtx *direct.MapContext, in *pb.BranchRule) *krm.BranchRule {
 	if in == nil {
 		return nil
 	}
-	out := &krm.Instance_HostConfig{}
-	out.HTML = direct.LazyPtr(in.GetHtml())
-	out.Api = direct.LazyPtr(in.GetApi())
-	out.GitHTTP = direct.LazyPtr(in.GetGitHttp())
-	out.GitSSH = direct.LazyPtr(in.GetGitSsh())
+	out := &krm.BranchRule{}
+	out.Name = direct.LazyPtr(in.GetName())
+	// MISSING: Uid
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	out.Annotations = in.Annotations
+	out.Etag = direct.LazyPtr(in.GetEtag())
+	out.IncludePattern = direct.LazyPtr(in.GetIncludePattern())
+	out.Disabled = direct.LazyPtr(in.GetDisabled())
+	out.RequirePullRequest = direct.LazyPtr(in.GetRequirePullRequest())
+	out.MinimumReviewsCount = direct.LazyPtr(in.GetMinimumReviewsCount())
+	out.MinimumApprovalsCount = direct.LazyPtr(in.GetMinimumApprovalsCount())
+	out.RequireCommentsResolved = direct.LazyPtr(in.GetRequireCommentsResolved())
+	out.AllowStaleReviews = direct.LazyPtr(in.GetAllowStaleReviews())
+	out.RequireLinearHistory = direct.LazyPtr(in.GetRequireLinearHistory())
+	out.RequiredStatusChecks = direct.Slice_FromProto(mapCtx, in.RequiredStatusChecks, BranchRule_Check_FromProto)
 	return out
 }
-func Instance_HostConfig_ToProto(mapCtx *direct.MapContext, in *krm.Instance_HostConfig) *pb.Instance_HostConfig {
+func BranchRule_ToProto(mapCtx *direct.MapContext, in *krm.BranchRule) *pb.BranchRule {
 	if in == nil {
 		return nil
 	}
-	out := &pb.Instance_HostConfig{}
-	out.Html = direct.ValueOf(in.HTML)
-	out.Api = direct.ValueOf(in.Api)
-	out.GitHttp = direct.ValueOf(in.GitHTTP)
-	out.GitSsh = direct.ValueOf(in.GitSSH)
+	out := &pb.BranchRule{}
+	out.Name = direct.ValueOf(in.Name)
+	// MISSING: Uid
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	out.Annotations = in.Annotations
+	out.Etag = direct.ValueOf(in.Etag)
+	out.IncludePattern = direct.ValueOf(in.IncludePattern)
+	out.Disabled = direct.ValueOf(in.Disabled)
+	out.RequirePullRequest = direct.ValueOf(in.RequirePullRequest)
+	out.MinimumReviewsCount = direct.ValueOf(in.MinimumReviewsCount)
+	out.MinimumApprovalsCount = direct.ValueOf(in.MinimumApprovalsCount)
+	out.RequireCommentsResolved = direct.ValueOf(in.RequireCommentsResolved)
+	out.AllowStaleReviews = direct.ValueOf(in.AllowStaleReviews)
+	out.RequireLinearHistory = direct.ValueOf(in.RequireLinearHistory)
+	out.RequiredStatusChecks = direct.Slice_ToProto(mapCtx, in.RequiredStatusChecks, BranchRule_Check_ToProto)
 	return out
 }
-func Repository_InitialConfig_FromProto(mapCtx *direct.MapContext, in *pb.Repository_InitialConfig) *krm.Repository_InitialConfig {
+func BranchRuleObservedState_FromProto(mapCtx *direct.MapContext, in *pb.BranchRule) *krm.BranchRuleObservedState {
 	if in == nil {
 		return nil
 	}
-	out := &krm.Repository_InitialConfig{}
-	out.DefaultBranch = direct.LazyPtr(in.GetDefaultBranch())
-	out.Gitignores = in.Gitignores
-	out.License = direct.LazyPtr(in.GetLicense())
-	out.Readme = direct.LazyPtr(in.GetReadme())
+	out := &krm.BranchRuleObservedState{}
+	// MISSING: Name
+	out.Uid = direct.LazyPtr(in.GetUid())
+	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
+	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
+	// MISSING: Annotations
+	// MISSING: Etag
+	// MISSING: IncludePattern
+	// MISSING: Disabled
+	// MISSING: RequirePullRequest
+	// MISSING: MinimumReviewsCount
+	// MISSING: MinimumApprovalsCount
+	// MISSING: RequireCommentsResolved
+	// MISSING: AllowStaleReviews
+	// MISSING: RequireLinearHistory
+	// MISSING: RequiredStatusChecks
 	return out
 }
-func Repository_InitialConfig_ToProto(mapCtx *direct.MapContext, in *krm.Repository_InitialConfig) *pb.Repository_InitialConfig {
+func BranchRuleObservedState_ToProto(mapCtx *direct.MapContext, in *krm.BranchRuleObservedState) *pb.BranchRule {
 	if in == nil {
 		return nil
 	}
-	out := &pb.Repository_InitialConfig{}
-	out.DefaultBranch = direct.ValueOf(in.DefaultBranch)
-	out.Gitignores = in.Gitignores
-	out.License = direct.ValueOf(in.License)
-	out.Readme = direct.ValueOf(in.Readme)
+	out := &pb.BranchRule{}
+	// MISSING: Name
+	out.Uid = direct.ValueOf(in.Uid)
+	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
+	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
+	// MISSING: Annotations
+	// MISSING: Etag
+	// MISSING: IncludePattern
+	// MISSING: Disabled
+	// MISSING: RequirePullRequest
+	// MISSING: MinimumReviewsCount
+	// MISSING: MinimumApprovalsCount
+	// MISSING: RequireCommentsResolved
+	// MISSING: AllowStaleReviews
+	// MISSING: RequireLinearHistory
+	// MISSING: RequiredStatusChecks
 	return out
 }
-func Repository_URIs_FromProto(mapCtx *direct.MapContext, in *pb.Repository_URIs) *krm.Repository_URIs {
+func BranchRule_Check_FromProto(mapCtx *direct.MapContext, in *pb.BranchRule_Check) *krm.BranchRule_Check {
 	if in == nil {
 		return nil
 	}
-	out := &krm.Repository_URIs{}
-	out.HTML = direct.LazyPtr(in.GetHtml())
-	out.GitHTTPS = direct.LazyPtr(in.GetGitHttps())
-	out.Api = direct.LazyPtr(in.GetApi())
+	out := &krm.BranchRule_Check{}
+	out.Context = direct.LazyPtr(in.GetContext())
 	return out
 }
-func Repository_URIs_ToProto(mapCtx *direct.MapContext, in *krm.Repository_URIs) *pb.Repository_URIs {
+func BranchRule_Check_ToProto(mapCtx *direct.MapContext, in *krm.BranchRule_Check) *pb.BranchRule_Check {
 	if in == nil {
 		return nil
 	}
-	out := &pb.Repository_URIs{}
-	out.Html = direct.ValueOf(in.HTML)
-	out.GitHttps = direct.ValueOf(in.GitHTTPS)
-	out.Api = direct.ValueOf(in.Api)
+	out := &pb.BranchRule_Check{}
+	out.Context = direct.ValueOf(in.Context)
 	return out
 }
 func SecureSourceManagerInstanceObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Instance) *krm.SecureSourceManagerInstanceObservedState {
@@ -96,6 +135,7 @@ func SecureSourceManagerInstanceObservedState_FromProto(mapCtx *direct.MapContex
 	// MISSING: Labels
 	out.State = direct.Enum_FromProto(mapCtx, in.GetState())
 	out.StateNote = direct.Enum_FromProto(mapCtx, in.GetStateNote())
+	// MISSING: KMSKey
 	out.HostConfig = Instance_HostConfig_FromProto(mapCtx, in.GetHostConfig())
 	return out
 }
@@ -110,6 +150,7 @@ func SecureSourceManagerInstanceObservedState_ToProto(mapCtx *direct.MapContext,
 	// MISSING: Labels
 	out.State = direct.Enum_ToProto[pb.Instance_State](mapCtx, in.State)
 	out.StateNote = direct.Enum_ToProto[pb.Instance_StateNote](mapCtx, in.StateNote)
+	// MISSING: KMSKey
 	out.HostConfig = Instance_HostConfig_ToProto(mapCtx, in.HostConfig)
 	return out
 }
@@ -123,9 +164,7 @@ func SecureSourceManagerInstanceSpec_FromProto(mapCtx *direct.MapContext, in *pb
 	// MISSING: UpdateTime
 	// MISSING: Labels
 	out.PrivateConfig = Instance_PrivateConfig_FromProto(mapCtx, in.GetPrivateConfig())
-	if in.GetKmsKey() != "" {
-		out.KmsKeyRef = &refs.KMSCryptoKeyRef{External: in.GetKmsKey()}
-	}
+	// MISSING: KMSKey
 	return out
 }
 func SecureSourceManagerInstanceSpec_ToProto(mapCtx *direct.MapContext, in *krm.SecureSourceManagerInstanceSpec) *pb.Instance {
@@ -138,9 +177,7 @@ func SecureSourceManagerInstanceSpec_ToProto(mapCtx *direct.MapContext, in *krm.
 	// MISSING: UpdateTime
 	// MISSING: Labels
 	out.PrivateConfig = Instance_PrivateConfig_ToProto(mapCtx, in.PrivateConfig)
-	if in.KmsKeyRef != nil {
-		out.KmsKey = in.KmsKeyRef.External
-	}
+	// MISSING: KMSKey
 	return out
 }
 func SecureSourceManagerRepositorySpec_FromProto(mapCtx *direct.MapContext, in *pb.Repository) *krm.SecureSourceManagerRepositorySpec {
@@ -151,7 +188,7 @@ func SecureSourceManagerRepositorySpec_FromProto(mapCtx *direct.MapContext, in *
 	// MISSING: Name
 	// MISSING: Description
 	if in.GetInstance() != "" {
-		out.InstanceRef = &krm.SecureSourceManagerInstanceRef{External: in.GetInstance()}
+		out.InstanceRef = &refs.*SecureSourceManagerInstanceRef{External: in.GetInstance()}
 	}
 	// MISSING: Uid
 	// MISSING: CreateTime
@@ -177,5 +214,93 @@ func SecureSourceManagerRepositorySpec_ToProto(mapCtx *direct.MapContext, in *kr
 	// MISSING: Etag
 	// MISSING: Uris
 	out.InitialConfig = Repository_InitialConfig_ToProto(mapCtx, in.InitialConfig)
+	return out
+}
+func SecuresourcemanagerBranchRuleObservedState_FromProto(mapCtx *direct.MapContext, in *pb.BranchRule) *krm.SecuresourcemanagerBranchRuleObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.SecuresourcemanagerBranchRuleObservedState{}
+	// MISSING: Name
+	// MISSING: Uid
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Annotations
+	// MISSING: Etag
+	// MISSING: IncludePattern
+	// MISSING: Disabled
+	// MISSING: RequirePullRequest
+	// MISSING: MinimumReviewsCount
+	// MISSING: MinimumApprovalsCount
+	// MISSING: RequireCommentsResolved
+	// MISSING: AllowStaleReviews
+	// MISSING: RequireLinearHistory
+	// MISSING: RequiredStatusChecks
+	return out
+}
+func SecuresourcemanagerBranchRuleObservedState_ToProto(mapCtx *direct.MapContext, in *krm.SecuresourcemanagerBranchRuleObservedState) *pb.BranchRule {
+	if in == nil {
+		return nil
+	}
+	out := &pb.BranchRule{}
+	// MISSING: Name
+	// MISSING: Uid
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Annotations
+	// MISSING: Etag
+	// MISSING: IncludePattern
+	// MISSING: Disabled
+	// MISSING: RequirePullRequest
+	// MISSING: MinimumReviewsCount
+	// MISSING: MinimumApprovalsCount
+	// MISSING: RequireCommentsResolved
+	// MISSING: AllowStaleReviews
+	// MISSING: RequireLinearHistory
+	// MISSING: RequiredStatusChecks
+	return out
+}
+func SecuresourcemanagerBranchRuleSpec_FromProto(mapCtx *direct.MapContext, in *pb.BranchRule) *krm.SecuresourcemanagerBranchRuleSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.SecuresourcemanagerBranchRuleSpec{}
+	// MISSING: Name
+	// MISSING: Uid
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Annotations
+	// MISSING: Etag
+	// MISSING: IncludePattern
+	// MISSING: Disabled
+	// MISSING: RequirePullRequest
+	// MISSING: MinimumReviewsCount
+	// MISSING: MinimumApprovalsCount
+	// MISSING: RequireCommentsResolved
+	// MISSING: AllowStaleReviews
+	// MISSING: RequireLinearHistory
+	// MISSING: RequiredStatusChecks
+	return out
+}
+func SecuresourcemanagerBranchRuleSpec_ToProto(mapCtx *direct.MapContext, in *krm.SecuresourcemanagerBranchRuleSpec) *pb.BranchRule {
+	if in == nil {
+		return nil
+	}
+	out := &pb.BranchRule{}
+	// MISSING: Name
+	// MISSING: Uid
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Annotations
+	// MISSING: Etag
+	// MISSING: IncludePattern
+	// MISSING: Disabled
+	// MISSING: RequirePullRequest
+	// MISSING: MinimumReviewsCount
+	// MISSING: MinimumApprovalsCount
+	// MISSING: RequireCommentsResolved
+	// MISSING: AllowStaleReviews
+	// MISSING: RequireLinearHistory
+	// MISSING: RequiredStatusChecks
 	return out
 }
