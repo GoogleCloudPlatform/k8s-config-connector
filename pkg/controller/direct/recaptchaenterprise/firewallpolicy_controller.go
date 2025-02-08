dc37c6261bdbd565a5db96de672fc45b11c91133
Your Name <yuwenma@google.com>
2025-02-08 09:12:10 +0000

    feat: add resource RecaptchaenterpriseFirewallPolicy

---
+++ pkg/controller/direct/recaptchaenterprise/firewallpolicy_controller.go
@@ -0,0 +1,276 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package recaptchaenterprise
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/recaptchaenterprise/v1alpha1"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/config"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/common"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/directbase"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/registry"
+
+	// TODO(contributor): Update the import with the google cloud client
+	gcp "cloud.google.com/go/recaptchaenterprise/apiv1"
+
+	// TODO(contributor): Update the import with the google cloud client api protobuf
+	recaptchaenterprisepb "cloud.google.com/go/recaptchaenterprise/v1/recaptchaenterprisepb"
+	"google.golang.org/api/option"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func init() {
+	registry.RegisterModel(krm.RecaptchaenterpriseFirewallPolicyGVK, NewFirewallPolicyModel)
+}
+
+func NewFirewallPolicyModel(ctx context.Context, config *config.ControllerConfig) (directbase.Model, error) {
+	return &modelFirewallPolicy{config: *config}, nil
+}
+
+var _ directbase.Model = &modelFirewallPolicy{}
+
+type modelFirewallPolicy struct {
+	config config.ControllerConfig
+}
+
+func (m *modelFirewallPolicy) client(ctx context.Context) (*gcp.Client, error) {
+	var opts []option.ClientOption
+	opts, err := m.config.RESTClientOptions()
+	if err != nil {
+		return nil, err
+	}
+	gcpClient, err := gcp.NewRESTClient(ctx, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("building FirewallPolicy client: %w", err)
+	}
+	return gcpClient, err
+}
+
+func (m *modelFirewallPolicy) AdapterForObject(ctx context.Context, reader client.Reader, u *unstructured.Unstructured) (directbase.Adapter, error) {
+	obj := &krm.RecaptchaenterpriseFirewallPolicy{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, &obj); err != nil {
+		return nil, fmt.Errorf("error converting to %T: %w", obj, err)
+	}
+
+	id, err := krm.NewFirewallPolicyIdentity(ctx, reader, obj)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get recaptchaenterprise GCP client
+	gcpClient, err := m.client(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &FirewallPolicyAdapter{
+		id:        id,
+		gcpClient: gcpClient,
+		desired:   obj,
+	}, nil
+}
+
+func (m *modelFirewallPolicy) AdapterForURL(ctx context.Context, url string) (directbase.Adapter, error) {
+	// TODO: Support URLs
+	return nil, nil
+}
+
+type FirewallPolicyAdapter struct {
+	id        *krm.FirewallPolicyIdentity
+	gcpClient *gcp.Client
+	desired   *krm.RecaptchaenterpriseFirewallPolicy
+	actual    *recaptchaenterprisepb.FirewallPolicy
+}
+
+var _ directbase.Adapter = &FirewallPolicyAdapter{}
+
+// Find retrieves the GCP resource.
+// Return true means the object is found. This triggers Adapter `Update` call.
+// Return false means the object is not found. This triggers Adapter `Create` call.
+// Return a non-nil error requeues the requests.
+func (a *FirewallPolicyAdapter) Find(ctx context.Context) (bool, error) {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("getting FirewallPolicy", "name", a.id)
+
+	req := &recaptchaenterprisepb.GetFirewallPolicyRequest{Name: a.id}
+	firewallpolicypb, err := a.gcpClient.GetFirewallPolicy(ctx, req)
+	if err != nil {
+		if direct.IsNotFound(err) {
+			return false, nil
+		}
+		return false, fmt.Errorf("getting FirewallPolicy %q: %w", a.id, err)
+	}
+
+	a.actual = firewallpolicypb
+	return true, nil
+}
+
+// Create creates the resource in GCP based on `spec` and update the Config Connector object `status` based on the GCP response.
+func (a *FirewallPolicyAdapter) Create(ctx context.Context, createOp *directbase.CreateOperation) error {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("creating FirewallPolicy", "name", a.id)
+	mapCtx := &direct.MapContext{}
+
+	desired := a.desired.DeepCopy()
+	resource := RecaptchaenterpriseFirewallPolicySpec_ToProto(mapCtx, &desired.Spec)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+
+	// TODO(contributor): Complete the gcp "CREATE" or "INSERT" request.
+	req := &recaptchaenterprisepb.CreateFirewallPolicyRequest{
+		Parent:         a.id.Parent().String(),
+		FirewallPolicy: resource,
+	}
+	op, err := a.gcpClient.CreateFirewallPolicy(ctx, req)
+	if err != nil {
+		return fmt.Errorf("creating FirewallPolicy %s: %w", a.id, err)
+	}
+	created, err := op.Wait(ctx)
+	if err != nil {
+		return fmt.Errorf("FirewallPolicy %s waiting creation: %w", a.id, err)
+	}
+	log.V(2).Info("successfully created FirewallPolicy", "name", a.id)
+
+	status := &krm.RecaptchaenterpriseFirewallPolicyStatus{}
+	status.ObservedState = RecaptchaenterpriseFirewallPolicyObservedState_FromProto(mapCtx, created)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+	status.ExternalRef = &a.id.External
+	return createOp.UpdateStatus(ctx, status, nil)
+}
+
+// Update updates the resource in GCP based on `spec` and update the Config Connector object `status` based on the GCP response.
+func (a *FirewallPolicyAdapter) Update(ctx context.Context, updateOp *directbase.UpdateOperation) error {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("updating FirewallPolicy", "name", a.id)
+	mapCtx := &direct.MapContext{}
+
+	desiredPb := RecaptchaenterpriseFirewallPolicySpec_ToProto(mapCtx, &a.desired.DeepCopy().Spec)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+
+	paths := []string{}
+	// Option 1: This option is good for proto that has `field_mask` for output-only, immutable, required/optional.
+	// TODO(contributor): If choosing this option, remove the "Option 2" code.
+	{
+		var err error
+		paths, err = common.CompareProtoMessage(desiredPb, a.actual, common.BasicDiff)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Option 2: manually add all mutable fields.
+	// TODO(contributor): If choosing this option, remove the "Option 1" code.
+	{
+		if !reflect.DeepEqual(a.desired.Spec.DisplayName, a.actual.DisplayName) {
+			paths = append(paths, "display_name")
+		}
+	}
+
+	if len(paths) == 0 {
+		log.V(2).Info("no field needs update", "name", a.id.External)
+		status := &krm.RecaptchaenterpriseFirewallPolicyStatus{}
+		status.ObservedState = RecaptchaenterpriseFirewallPolicyObservedState_FromProto(mapCtx, a.actual)
+		if mapCtx.Err() != nil {
+			return mapCtx.Err()
+		}
+		return updateOp.UpdateStatus(ctx, status, nil)
+	}
+	updateMask := &fieldmaskpb.FieldMask{
+		Paths: sets.List(paths)}
+
+	// TODO(contributor): Complete the gcp "UPDATE" or "PATCH" request.
+	req := &recaptchaenterprisepb.UpdateFirewallPolicyRequest{
+		Name:           a.id.External,
+		UpdateMask:     updateMask,
+		FirewallPolicy: desiredPb,
+	}
+	op, err := a.gcpClient.UpdateFirewallPolicy(ctx, req)
+	if err != nil {
+		return fmt.Errorf("updating FirewallPolicy %s: %w", a.id.External, err)
+	}
+	updated, err := op.Wait(ctx)
+	if err != nil {
+		return fmt.Errorf("FirewallPolicy %s waiting update: %w", a.id.External, err)
+	}
+	log.V(2).Info("successfully updated FirewallPolicy", "name", a.id.External)
+
+	status := &krm.RecaptchaenterpriseFirewallPolicyStatus{}
+	status.ObservedState = RecaptchaenterpriseFirewallPolicyObservedState_FromProto(mapCtx, updated)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+	return updateOp.UpdateStatus(ctx, status, nil)
+}
+
+// Export maps the GCP object to a Config Connector resource `spec`.
+func (a *FirewallPolicyAdapter) Export(ctx context.Context) (*unstructured.Unstructured, error) {
+	if a.actual == nil {
+		return nil, fmt.Errorf("Find() not called")
+	}
+	u := &unstructured.Unstructured{}
+
+	obj := &krm.RecaptchaenterpriseFirewallPolicy{}
+	mapCtx := &direct.MapContext{}
+	obj.Spec = direct.ValueOf(RecaptchaenterpriseFirewallPolicySpec_FromProto(mapCtx, a.actual))
+	if mapCtx.Err() != nil {
+		return nil, mapCtx.Err()
+	}
+	obj.Spec.ProjectRef = &refs.ProjectRef{External: a.id.Parent().ProjectID}
+	obj.Spec.Location = a.id.Parent().Location
+	uObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+	if err != nil {
+		return nil, err
+	}
+
+	u.SetName(a.actual.Id)
+	u.SetGroupVersionKind(krm.RecaptchaenterpriseFirewallPolicyGVK)
+
+	u.Object = uObj
+	return u, nil
+}
+
+// Delete the resource from GCP service when the corresponding Config Connector resource is deleted.
+func (a *FirewallPolicyAdapter) Delete(ctx context.Context, deleteOp *directbase.DeleteOperation) (bool, error) {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("deleting FirewallPolicy", "name", a.id)
+
+	req := &recaptchaenterprisepb.DeleteFirewallPolicyRequest{Name: a.id.String()}
+	op, err := a.gcpClient.DeleteFirewallPolicy(ctx, req)
+	if err != nil {
+		return false, fmt.Errorf("deleting FirewallPolicy %s: %w", a.id, err)
+	}
+	log.V(2).Info("successfully deleted FirewallPolicy", "name", a.id)
+
+	err = op.Wait(ctx)
+	if err != nil {
+		return false, fmt.Errorf("waiting delete FirewallPolicy %s: %w", a.id, err)
+	}
+	return true, nil
+}
