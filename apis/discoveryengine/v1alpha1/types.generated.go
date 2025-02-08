cb44780f5530c883843254aeeed8e0a95d1e1df4
Your Name <yuwenma@google.com>
2025-02-08 04:51:25 +0000

    feat: add resource DiscoveryengineControl

--- apis/discoveryengine/v1alpha1/types.generated.go
+++ apis/discoveryengine/v1alpha1/types.generated.go
@@ -15,33 +15,33 @@
 package v1alpha1
 
 
-// +kcc:proto=google.cloud.discoveryengine.v1.Condition
+// +kcc:proto=google.cloud.discoveryengine.v1beta.Condition
 type Condition struct {
 	// Search only
 	//  A list of terms to match the query on.
 	//  Cannot be set when
-	//  [Condition.query_regex][google.cloud.discoveryengine.v1.Condition.query_regex]
+	//  [Condition.query_regex][google.cloud.discoveryengine.v1beta.Condition.query_regex]
 	//  is set.
 	//
 	//  Maximum of 10 query terms.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Condition.query_terms
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Condition.query_terms
 	QueryTerms []Condition_QueryTerm `json:"queryTerms,omitempty"`
 
 	// Range of time(s) specifying when condition is active.
 	//
 	//  Maximum of 10 time ranges.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Condition.active_time_range
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Condition.active_time_range
 	ActiveTimeRange []Condition_TimeRange `json:"activeTimeRange,omitempty"`
 
 	// Optional. Query regex to match the whole search query.
 	//  Cannot be set when
-	//  [Condition.query_terms][google.cloud.discoveryengine.v1.Condition.query_terms]
+	//  [Condition.query_terms][google.cloud.discoveryengine.v1beta.Condition.query_terms]
 	//  is set. This is currently supporting promotion use case.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Condition.query_regex
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Condition.query_regex
 	QueryRegex *string `json:"queryRegex,omitempty"`
 }
 
-// +kcc:proto=google.cloud.discoveryengine.v1.Condition.QueryTerm
+// +kcc:proto=google.cloud.discoveryengine.v1beta.Condition.QueryTerm
 type Condition_QueryTerm struct {
 	// The specific query value to match against
 	//
@@ -49,76 +49,76 @@ type Condition_QueryTerm struct {
 	//  Can have at most 3 space separated terms if full_match is true.
 	//  Cannot be an empty string.
 	//  Maximum length of 5000 characters.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Condition.QueryTerm.value
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Condition.QueryTerm.value
 	Value *string `json:"value,omitempty"`
 
 	// Whether the search query needs to exactly match the query term.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Condition.QueryTerm.full_match
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Condition.QueryTerm.full_match
 	FullMatch *bool `json:"fullMatch,omitempty"`
 }
 
-// +kcc:proto=google.cloud.discoveryengine.v1.Condition.TimeRange
+// +kcc:proto=google.cloud.discoveryengine.v1beta.Condition.TimeRange
 type Condition_TimeRange struct {
 	// Start of time range.
 	//
 	//  Range is inclusive.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Condition.TimeRange.start_time
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Condition.TimeRange.start_time
 	StartTime *string `json:"startTime,omitempty"`
 
 	// End of time range.
 	//
 	//  Range is inclusive.
 	//  Must be in the future.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Condition.TimeRange.end_time
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Condition.TimeRange.end_time
 	EndTime *string `json:"endTime,omitempty"`
 }
 
-// +kcc:proto=google.cloud.discoveryengine.v1.Control
+// +kcc:proto=google.cloud.discoveryengine.v1beta.Control
 type Control struct {
 	// Defines a boost-type control
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Control.boost_action
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Control.boost_action
 	BoostAction *Control_BoostAction `json:"boostAction,omitempty"`
 
 	// Defines a filter-type control
 	//  Currently not supported by Recommendation
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Control.filter_action
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Control.filter_action
 	FilterAction *Control_FilterAction `json:"filterAction,omitempty"`
 
 	// Defines a redirect-type control.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Control.redirect_action
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Control.redirect_action
 	RedirectAction *Control_RedirectAction `json:"redirectAction,omitempty"`
 
 	// Treats a group of terms as synonyms of one another.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Control.synonyms_action
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Control.synonyms_action
 	SynonymsAction *Control_SynonymsAction `json:"synonymsAction,omitempty"`
 
 	// Immutable. Fully qualified name
 	//  `projects/*/locations/global/dataStore/*/controls/*`
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Control.name
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Control.name
 	Name *string `json:"name,omitempty"`
 
 	// Required. Human readable name. The identifier used in UI views.
 	//
 	//  Must be UTF-8 encoded string. Length limit is 128 characters.
 	//  Otherwise an INVALID ARGUMENT error is thrown.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Control.display_name
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Control.display_name
 	DisplayName *string `json:"displayName,omitempty"`
 
 	// Required. Immutable. What solution the control belongs to.
 	//
 	//  Must be compatible with vertical of resource.
 	//  Otherwise an INVALID ARGUMENT error is thrown.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Control.solution_type
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Control.solution_type
 	SolutionType *string `json:"solutionType,omitempty"`
 
 	// Specifies the use case for the control.
 	//  Affects what condition fields can be set.
 	//  Only applies to
-	//  [SOLUTION_TYPE_SEARCH][google.cloud.discoveryengine.v1.SolutionType.SOLUTION_TYPE_SEARCH].
+	//  [SOLUTION_TYPE_SEARCH][google.cloud.discoveryengine.v1beta.SolutionType.SOLUTION_TYPE_SEARCH].
 	//  Currently only allow one use case per control.
 	//  Must be set when solution_type is
-	//  [SolutionType.SOLUTION_TYPE_SEARCH][google.cloud.discoveryengine.v1.SolutionType.SOLUTION_TYPE_SEARCH].
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Control.use_cases
+	//  [SolutionType.SOLUTION_TYPE_SEARCH][google.cloud.discoveryengine.v1beta.SolutionType.SOLUTION_TYPE_SEARCH].
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Control.use_cases
 	UseCases []string `json:"useCases,omitempty"`
 
 	// Determines when the associated action will trigger.
@@ -126,15 +126,15 @@ type Control struct {
 	//  Omit to always apply the action.
 	//  Currently only a single condition may be specified.
 	//  Otherwise an INVALID ARGUMENT error is thrown.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Control.conditions
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Control.conditions
 	Conditions []Condition `json:"conditions,omitempty"`
 }
 
-// +kcc:proto=google.cloud.discoveryengine.v1.Control.BoostAction
+// +kcc:proto=google.cloud.discoveryengine.v1beta.Control.BoostAction
 type Control_BoostAction struct {
 	// Required. Strength of the boost, which should be in [-1, 1]. Negative
 	//  boost means demotion. Default is 0.0 (No-op).
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Control.BoostAction.boost
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Control.BoostAction.boost
 	Boost *float32 `json:"boost,omitempty"`
 
 	// Required. Specifies which products to apply the boost to.
@@ -144,17 +144,17 @@ type Control_BoostAction struct {
 	//  https://cloud.google.com/retail/docs/filter-and-order
 	//  Maximum length is 5000 characters.
 	//  Otherwise an INVALID ARGUMENT error is thrown.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Control.BoostAction.filter
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Control.BoostAction.filter
 	Filter *string `json:"filter,omitempty"`
 
 	// Required. Specifies which data store's documents can be boosted by this
 	//  control. Full data store name e.g.
 	//  projects/123/locations/global/collections/default_collection/dataStores/default_data_store
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Control.BoostAction.data_store
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Control.BoostAction.data_store
 	DataStore *string `json:"dataStore,omitempty"`
 }
 
-// +kcc:proto=google.cloud.discoveryengine.v1.Control.FilterAction
+// +kcc:proto=google.cloud.discoveryengine.v1beta.Control.FilterAction
 type Control_FilterAction struct {
 	// Required. A filter to apply on the matching condition results.
 	//
@@ -163,42 +163,42 @@ type Control_FilterAction struct {
 	//  https://cloud.google.com/retail/docs/filter-and-order
 	//  Maximum length is 5000 characters. Otherwise an INVALID
 	//  ARGUMENT error is thrown.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Control.FilterAction.filter
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Control.FilterAction.filter
 	Filter *string `json:"filter,omitempty"`
 
 	// Required. Specifies which data store's documents can be filtered by this
 	//  control. Full data store name e.g.
 	//  projects/123/locations/global/collections/default_collection/dataStores/default_data_store
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Control.FilterAction.data_store
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Control.FilterAction.data_store
 	DataStore *string `json:"dataStore,omitempty"`
 }
 
-// +kcc:proto=google.cloud.discoveryengine.v1.Control.RedirectAction
+// +kcc:proto=google.cloud.discoveryengine.v1beta.Control.RedirectAction
 type Control_RedirectAction struct {
 	// Required. The URI to which the shopper will be redirected.
 	//
 	//  Required.
 	//  URI must have length equal or less than 2000 characters.
 	//  Otherwise an INVALID ARGUMENT error is thrown.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Control.RedirectAction.redirect_uri
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Control.RedirectAction.redirect_uri
 	RedirectURI *string `json:"redirectURI,omitempty"`
 }
 
-// +kcc:proto=google.cloud.discoveryengine.v1.Control.SynonymsAction
+// +kcc:proto=google.cloud.discoveryengine.v1beta.Control.SynonymsAction
 type Control_SynonymsAction struct {
 	// Defines a set of synonyms.
 	//  Can specify up to 100 synonyms.
 	//  Must specify at least 2 synonyms. Otherwise an INVALID ARGUMENT error is
 	//  thrown.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Control.SynonymsAction.synonyms
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Control.SynonymsAction.synonyms
 	Synonyms []string `json:"synonyms,omitempty"`
 }
 
-// +kcc:proto=google.cloud.discoveryengine.v1.Control
+// +kcc:proto=google.cloud.discoveryengine.v1beta.Control
 type ControlObservedState struct {
 	// Output only. List of all
-	//  [ServingConfig][google.cloud.discoveryengine.v1.ServingConfig] IDs this
+	//  [ServingConfig][google.cloud.discoveryengine.v1beta.ServingConfig] IDs this
 	//  control is attached to. May take up to 10 minutes to update after changes.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.Control.associated_serving_config_ids
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.Control.associated_serving_config_ids
 	AssociatedServingConfigIds []string `json:"associatedServingConfigIds,omitempty"`
 }
