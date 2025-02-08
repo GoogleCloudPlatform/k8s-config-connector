76ba9fef8696612e5d24bd954e41148c4e4085d1
Your Name <yuwenma@google.com>
2025-02-08 04:31:39 +0000

    feat: add resource DialogflowMessage

--- apis/dialogflow/v1alpha1/types.generated.go
+++ apis/dialogflow/v1alpha1/types.generated.go
@@ -15,20 +15,20 @@
 package v1alpha1
 
 
-// +kcc:proto=google.cloud.dialogflow.v2.AnnotatedMessagePart
+// +kcc:proto=google.cloud.dialogflow.v2beta1.AnnotatedMessagePart
 type AnnotatedMessagePart struct {
-	// A part of a message possibly annotated with an entity.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.AnnotatedMessagePart.text
+	// Required. A part of a message possibly annotated with an entity.
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.AnnotatedMessagePart.text
 	Text *string `json:"text,omitempty"`
 
-	// The [Dialogflow system entity
+	// Optional. The [Dialogflow system entity
 	//  type](https://cloud.google.com/dialogflow/docs/reference/system-entities)
 	//  of this message part. If this is empty, Dialogflow could not annotate the
 	//  phrase part with a system entity.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.AnnotatedMessagePart.entity_type
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.AnnotatedMessagePart.entity_type
 	EntityType *string `json:"entityType,omitempty"`
 
-	// The [Dialogflow system entity formatted value
+	// Optional. The [Dialogflow system entity formatted value
 	//  ](https://cloud.google.com/dialogflow/docs/reference/system-entities) of
 	//  this message part. For example for a system entity of type
 	//  `@sys.unit-currency`, this may contain:
@@ -38,63 +38,155 @@ type AnnotatedMessagePart struct {
 	//    "currency": "USD"
 	//  }
 	//  </pre>
-	// +kcc:proto:field=google.cloud.dialogflow.v2.AnnotatedMessagePart.formatted_value
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.AnnotatedMessagePart.formatted_value
 	FormattedValue *Value `json:"formattedValue,omitempty"`
 }
 
-// +kcc:proto=google.cloud.dialogflow.v2.Message
+// +kcc:proto=google.cloud.dialogflow.v2beta1.Message
 type Message struct {
 	// Optional. The unique identifier of the message.
 	//  Format: `projects/<Project ID>/locations/<Location
 	//  ID>/conversations/<Conversation ID>/messages/<Message ID>`.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Message.name
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Message.name
 	Name *string `json:"name,omitempty"`
 
 	// Required. The message content.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Message.content
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Message.content
 	Content *string `json:"content,omitempty"`
+
+	// Optional. Automated agent responses.
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Message.response_messages
+	ResponseMessages []ResponseMessage `json:"responseMessages,omitempty"`
 
 	// Optional. The message language.
 	//  This should be a [BCP-47](https://www.rfc-editor.org/rfc/bcp/bcp47.txt)
 	//  language tag. Example: "en-US".
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Message.language_code
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Message.language_code
 	LanguageCode *string `json:"languageCode,omitempty"`
 
 	// Optional. The time when the message was sent.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Message.send_time
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Message.send_time
 	SendTime *string `json:"sendTime,omitempty"`
 }
 
-// +kcc:proto=google.cloud.dialogflow.v2.MessageAnnotation
+// +kcc:proto=google.cloud.dialogflow.v2beta1.MessageAnnotation
 type MessageAnnotation struct {
-	// The collection of annotated message parts ordered by their
+	// Optional. The collection of annotated message parts ordered by their
 	//  position in the message. You can recover the annotated message by
 	//  concatenating [AnnotatedMessagePart.text].
-	// +kcc:proto:field=google.cloud.dialogflow.v2.MessageAnnotation.parts
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.MessageAnnotation.parts
 	Parts []AnnotatedMessagePart `json:"parts,omitempty"`
 
-	// Indicates whether the text message contains entities.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.MessageAnnotation.contain_entities
+	// Required. Indicates whether the text message contains entities.
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.MessageAnnotation.contain_entities
 	ContainEntities *bool `json:"containEntities,omitempty"`
 }
 
-// +kcc:proto=google.cloud.dialogflow.v2.Sentiment
+// +kcc:proto=google.cloud.dialogflow.v2beta1.ResponseMessage
+type ResponseMessage struct {
+	// Returns a text response.
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.ResponseMessage.text
+	Text *ResponseMessage_Text `json:"text,omitempty"`
+
+	// Returns a response containing a custom, platform-specific payload.
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.ResponseMessage.payload
+	Payload map[string]string `json:"payload,omitempty"`
+
+	// Hands off conversation to a live agent.
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.ResponseMessage.live_agent_handoff
+	LiveAgentHandoff *ResponseMessage_LiveAgentHandoff `json:"liveAgentHandoff,omitempty"`
+
+	// A signal that indicates the interaction with the Dialogflow agent has
+	//  ended.
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.ResponseMessage.end_interaction
+	EndInteraction *ResponseMessage_EndInteraction `json:"endInteraction,omitempty"`
+
+	// An audio response message composed of both the synthesized Dialogflow
+	//  agent responses and the audios hosted in places known to the client.
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.ResponseMessage.mixed_audio
+	MixedAudio *ResponseMessage_MixedAudio `json:"mixedAudio,omitempty"`
+
+	// A signal that the client should transfer the phone call connected to
+	//  this agent to a third-party endpoint.
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.ResponseMessage.telephony_transfer_call
+	TelephonyTransferCall *ResponseMessage_TelephonyTransferCall `json:"telephonyTransferCall,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dialogflow.v2beta1.ResponseMessage.EndInteraction
+type ResponseMessage_EndInteraction struct {
+}
+
+// +kcc:proto=google.cloud.dialogflow.v2beta1.ResponseMessage.LiveAgentHandoff
+type ResponseMessage_LiveAgentHandoff struct {
+	// Custom metadata for your handoff procedure. Dialogflow doesn't impose
+	//  any structure on this.
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.ResponseMessage.LiveAgentHandoff.metadata
+	Metadata map[string]string `json:"metadata,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dialogflow.v2beta1.ResponseMessage.MixedAudio
+type ResponseMessage_MixedAudio struct {
+	// Segments this audio response is composed of.
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.ResponseMessage.MixedAudio.segments
+	Segments []ResponseMessage_MixedAudio_Segment `json:"segments,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dialogflow.v2beta1.ResponseMessage.MixedAudio.Segment
+type ResponseMessage_MixedAudio_Segment struct {
+	// Raw audio synthesized from the Dialogflow agent's response using
+	//  the output config specified in the request.
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.ResponseMessage.MixedAudio.Segment.audio
+	Audio []byte `json:"audio,omitempty"`
+
+	// Client-specific URI that points to an audio clip accessible to the
+	//  client.
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.ResponseMessage.MixedAudio.Segment.uri
+	URI *string `json:"uri,omitempty"`
+
+	// Whether the playback of this segment can be interrupted by the end
+	//  user's speech and the client should then start the next Dialogflow
+	//  request.
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.ResponseMessage.MixedAudio.Segment.allow_playback_interruption
+	AllowPlaybackInterruption *bool `json:"allowPlaybackInterruption,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dialogflow.v2beta1.ResponseMessage.TelephonyTransferCall
+type ResponseMessage_TelephonyTransferCall struct {
+	// Transfer the call to a phone number
+	//  in [E.164 format](https://en.wikipedia.org/wiki/E.164).
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.ResponseMessage.TelephonyTransferCall.phone_number
+	PhoneNumber *string `json:"phoneNumber,omitempty"`
+
+	// Transfer the call to a SIP endpoint.
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.ResponseMessage.TelephonyTransferCall.sip_uri
+	SipURI *string `json:"sipURI,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dialogflow.v2beta1.ResponseMessage.Text
+type ResponseMessage_Text struct {
+	// A collection of text response variants. If multiple variants are
+	//  defined, only one text response variant is returned at runtime.
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.ResponseMessage.Text.text
+	Text []string `json:"text,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dialogflow.v2beta1.Sentiment
 type Sentiment struct {
 	// Sentiment score between -1.0 (negative sentiment) and 1.0 (positive
-	//  sentiment).
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Sentiment.score
+	//   sentiment).
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Sentiment.score
 	Score *float32 `json:"score,omitempty"`
 
 	// A non-negative number in the [0, +inf) range, which represents the absolute
 	//  magnitude of sentiment, regardless of score (positive or negative).
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Sentiment.magnitude
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Sentiment.magnitude
 	Magnitude *float32 `json:"magnitude,omitempty"`
 }
 
-// +kcc:proto=google.cloud.dialogflow.v2.SentimentAnalysisResult
+// +kcc:proto=google.cloud.dialogflow.v2beta1.SentimentAnalysisResult
 type SentimentAnalysisResult struct {
 	// The sentiment analysis result for `query_text`.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.SentimentAnalysisResult.query_text_sentiment
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.SentimentAnalysisResult.query_text_sentiment
 	QueryTextSentiment *Sentiment `json:"queryTextSentiment,omitempty"`
 }
 
@@ -132,25 +224,25 @@ type Value struct {
 	ListValue *ListValue `json:"listValue,omitempty"`
 }
 
-// +kcc:proto=google.cloud.dialogflow.v2.Message
+// +kcc:proto=google.cloud.dialogflow.v2beta1.Message
 type MessageObservedState struct {
 	// Output only. The participant that sends this message.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Message.participant
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Message.participant
 	Participant *string `json:"participant,omitempty"`
 
 	// Output only. The role of the participant.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Message.participant_role
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Message.participant_role
 	ParticipantRole *string `json:"participantRole,omitempty"`
 
 	// Output only. The time when the message was created in Contact Center AI.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Message.create_time
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Message.create_time
 	CreateTime *string `json:"createTime,omitempty"`
 
 	// Output only. The annotation for the message.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Message.message_annotation
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Message.message_annotation
 	MessageAnnotation *MessageAnnotation `json:"messageAnnotation,omitempty"`
 
 	// Output only. The sentiment analysis result for the message.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Message.sentiment_analysis
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Message.sentiment_analysis
 	SentimentAnalysis *SentimentAnalysisResult `json:"sentimentAnalysis,omitempty"`
 }
--- pkg/controller/direct/dialogflow/mapper.generated.go
+++ pkg/controller/direct/dialogflow/mapper.generated.go
@@ -16,7 +16,7 @@ package dialogflow
 
 import (
 	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
-	pb "cloud.google.com/go/dialogflow/apiv2/dialogflowpb"
+	pb "cloud.google.com/go/dialogflow/apiv2beta1/dialogflowpb"
 	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/dialogflow/v1alpha1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 )
@@ -40,70 +40,6 @@ func AnnotatedMessagePart_ToProto(mapCtx *direct.MapContext, in *krm.AnnotatedMe
 	out.FormattedValue = Value_ToProto(mapCtx, in.FormattedValue)
 	return out
 }
-func DialogflowMessageObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Message) *krm.DialogflowMessageObservedState {
-	if in == nil {
-		return nil
-	}
-	out := &krm.DialogflowMessageObservedState{}
-	// MISSING: Name
-	// MISSING: Content
-	// MISSING: LanguageCode
-	// MISSING: Participant
-	// MISSING: ParticipantRole
-	// MISSING: CreateTime
-	// MISSING: SendTime
-	// MISSING: MessageAnnotation
-	// MISSING: SentimentAnalysis
-	return out
-}
-func DialogflowMessageObservedState_ToProto(mapCtx *direct.MapContext, in *krm.DialogflowMessageObservedState) *pb.Message {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Message{}
-	// MISSING: Name
-	// MISSING: Content
-	// MISSING: LanguageCode
-	// MISSING: Participant
-	// MISSING: ParticipantRole
-	// MISSING: CreateTime
-	// MISSING: SendTime
-	// MISSING: MessageAnnotation
-	// MISSING: SentimentAnalysis
-	return out
-}
-func DialogflowMessageSpec_FromProto(mapCtx *direct.MapContext, in *pb.Message) *krm.DialogflowMessageSpec {
-	if in == nil {
-		return nil
-	}
-	out := &krm.DialogflowMessageSpec{}
-	// MISSING: Name
-	// MISSING: Content
-	// MISSING: LanguageCode
-	// MISSING: Participant
-	// MISSING: ParticipantRole
-	// MISSING: CreateTime
-	// MISSING: SendTime
-	// MISSING: MessageAnnotation
-	// MISSING: SentimentAnalysis
-	return out
-}
-func DialogflowMessageSpec_ToProto(mapCtx *direct.MapContext, in *krm.DialogflowMessageSpec) *pb.Message {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Message{}
-	// MISSING: Name
-	// MISSING: Content
-	// MISSING: LanguageCode
-	// MISSING: Participant
-	// MISSING: ParticipantRole
-	// MISSING: CreateTime
-	// MISSING: SendTime
-	// MISSING: MessageAnnotation
-	// MISSING: SentimentAnalysis
-	return out
-}
 func Message_FromProto(mapCtx *direct.MapContext, in *pb.Message) *krm.Message {
 	if in == nil {
 		return nil
@@ -111,6 +47,7 @@ func Message_FromProto(mapCtx *direct.MapContext, in *pb.Message) *krm.Message {
 	out := &krm.Message{}
 	out.Name = direct.LazyPtr(in.GetName())
 	out.Content = direct.LazyPtr(in.GetContent())
+	out.ResponseMessages = direct.Slice_FromProto(mapCtx, in.ResponseMessages, ResponseMessage_FromProto)
 	out.LanguageCode = direct.LazyPtr(in.GetLanguageCode())
 	// MISSING: Participant
 	// MISSING: ParticipantRole
@@ -127,6 +64,7 @@ func Message_ToProto(mapCtx *direct.MapContext, in *krm.Message) *pb.Message {
 	out := &pb.Message{}
 	out.Name = direct.ValueOf(in.Name)
 	out.Content = direct.ValueOf(in.Content)
+	out.ResponseMessages = direct.Slice_ToProto(mapCtx, in.ResponseMessages, ResponseMessage_ToProto)
 	out.LanguageCode = direct.ValueOf(in.LanguageCode)
 	// MISSING: Participant
 	// MISSING: ParticipantRole
@@ -161,6 +99,7 @@ func MessageObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Message) *
 	out := &krm.MessageObservedState{}
 	// MISSING: Name
 	// MISSING: Content
+	// MISSING: ResponseMessages
 	// MISSING: LanguageCode
 	out.Participant = direct.LazyPtr(in.GetParticipant())
 	out.ParticipantRole = direct.Enum_FromProto(mapCtx, in.GetParticipantRole())
@@ -177,6 +116,7 @@ func MessageObservedState_ToProto(mapCtx *direct.MapContext, in *krm.MessageObse
 	out := &pb.Message{}
 	// MISSING: Name
 	// MISSING: Content
+	// MISSING: ResponseMessages
 	// MISSING: LanguageCode
 	out.Participant = direct.ValueOf(in.Participant)
 	out.ParticipantRole = direct.Enum_ToProto[pb.Participant_Role](mapCtx, in.ParticipantRole)
@@ -184,6 +124,152 @@ func MessageObservedState_ToProto(mapCtx *direct.MapContext, in *krm.MessageObse
 	// MISSING: SendTime
 	out.MessageAnnotation = MessageAnnotation_ToProto(mapCtx, in.MessageAnnotation)
 	out.SentimentAnalysis = SentimentAnalysisResult_ToProto(mapCtx, in.SentimentAnalysis)
+	return out
+}
+func ResponseMessage_FromProto(mapCtx *direct.MapContext, in *pb.ResponseMessage) *krm.ResponseMessage {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ResponseMessage{}
+	out.Text = ResponseMessage_Text_FromProto(mapCtx, in.GetText())
+	out.Payload = Payload_FromProto(mapCtx, in.GetPayload())
+	out.LiveAgentHandoff = ResponseMessage_LiveAgentHandoff_FromProto(mapCtx, in.GetLiveAgentHandoff())
+	out.EndInteraction = ResponseMessage_EndInteraction_FromProto(mapCtx, in.GetEndInteraction())
+	out.MixedAudio = ResponseMessage_MixedAudio_FromProto(mapCtx, in.GetMixedAudio())
+	out.TelephonyTransferCall = ResponseMessage_TelephonyTransferCall_FromProto(mapCtx, in.GetTelephonyTransferCall())
+	return out
+}
+func ResponseMessage_ToProto(mapCtx *direct.MapContext, in *krm.ResponseMessage) *pb.ResponseMessage {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ResponseMessage{}
+	if oneof := ResponseMessage_Text_ToProto(mapCtx, in.Text); oneof != nil {
+		out.Message = &pb.ResponseMessage_Text_{Text: oneof}
+	}
+	if oneof := Payload_ToProto(mapCtx, in.Payload); oneof != nil {
+		out.Message = &pb.ResponseMessage_Payload{Payload: oneof}
+	}
+	if oneof := ResponseMessage_LiveAgentHandoff_ToProto(mapCtx, in.LiveAgentHandoff); oneof != nil {
+		out.Message = &pb.ResponseMessage_LiveAgentHandoff_{LiveAgentHandoff: oneof}
+	}
+	if oneof := ResponseMessage_EndInteraction_ToProto(mapCtx, in.EndInteraction); oneof != nil {
+		out.Message = &pb.ResponseMessage_EndInteraction_{EndInteraction: oneof}
+	}
+	if oneof := ResponseMessage_MixedAudio_ToProto(mapCtx, in.MixedAudio); oneof != nil {
+		out.Message = &pb.ResponseMessage_MixedAudio_{MixedAudio: oneof}
+	}
+	if oneof := ResponseMessage_TelephonyTransferCall_ToProto(mapCtx, in.TelephonyTransferCall); oneof != nil {
+		out.Message = &pb.ResponseMessage_TelephonyTransferCall_{TelephonyTransferCall: oneof}
+	}
+	return out
+}
+func ResponseMessage_EndInteraction_FromProto(mapCtx *direct.MapContext, in *pb.ResponseMessage_EndInteraction) *krm.ResponseMessage_EndInteraction {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ResponseMessage_EndInteraction{}
+	return out
+}
+func ResponseMessage_EndInteraction_ToProto(mapCtx *direct.MapContext, in *krm.ResponseMessage_EndInteraction) *pb.ResponseMessage_EndInteraction {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ResponseMessage_EndInteraction{}
+	return out
+}
+func ResponseMessage_LiveAgentHandoff_FromProto(mapCtx *direct.MapContext, in *pb.ResponseMessage_LiveAgentHandoff) *krm.ResponseMessage_LiveAgentHandoff {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ResponseMessage_LiveAgentHandoff{}
+	out.Metadata = Metadata_FromProto(mapCtx, in.GetMetadata())
+	return out
+}
+func ResponseMessage_LiveAgentHandoff_ToProto(mapCtx *direct.MapContext, in *krm.ResponseMessage_LiveAgentHandoff) *pb.ResponseMessage_LiveAgentHandoff {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ResponseMessage_LiveAgentHandoff{}
+	out.Metadata = Metadata_ToProto(mapCtx, in.Metadata)
+	return out
+}
+func ResponseMessage_MixedAudio_FromProto(mapCtx *direct.MapContext, in *pb.ResponseMessage_MixedAudio) *krm.ResponseMessage_MixedAudio {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ResponseMessage_MixedAudio{}
+	out.Segments = direct.Slice_FromProto(mapCtx, in.Segments, ResponseMessage_MixedAudio_Segment_FromProto)
+	return out
+}
+func ResponseMessage_MixedAudio_ToProto(mapCtx *direct.MapContext, in *krm.ResponseMessage_MixedAudio) *pb.ResponseMessage_MixedAudio {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ResponseMessage_MixedAudio{}
+	out.Segments = direct.Slice_ToProto(mapCtx, in.Segments, ResponseMessage_MixedAudio_Segment_ToProto)
+	return out
+}
+func ResponseMessage_MixedAudio_Segment_FromProto(mapCtx *direct.MapContext, in *pb.ResponseMessage_MixedAudio_Segment) *krm.ResponseMessage_MixedAudio_Segment {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ResponseMessage_MixedAudio_Segment{}
+	out.Audio = in.GetAudio()
+	out.URI = direct.LazyPtr(in.GetUri())
+	out.AllowPlaybackInterruption = direct.LazyPtr(in.GetAllowPlaybackInterruption())
+	return out
+}
+func ResponseMessage_MixedAudio_Segment_ToProto(mapCtx *direct.MapContext, in *krm.ResponseMessage_MixedAudio_Segment) *pb.ResponseMessage_MixedAudio_Segment {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ResponseMessage_MixedAudio_Segment{}
+	if oneof := ResponseMessage_MixedAudio_Segment_Audio_ToProto(mapCtx, in.Audio); oneof != nil {
+		out.Content = oneof
+	}
+	if oneof := ResponseMessage_MixedAudio_Segment_Uri_ToProto(mapCtx, in.URI); oneof != nil {
+		out.Content = oneof
+	}
+	out.AllowPlaybackInterruption = direct.ValueOf(in.AllowPlaybackInterruption)
+	return out
+}
+func ResponseMessage_TelephonyTransferCall_FromProto(mapCtx *direct.MapContext, in *pb.ResponseMessage_TelephonyTransferCall) *krm.ResponseMessage_TelephonyTransferCall {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ResponseMessage_TelephonyTransferCall{}
+	out.PhoneNumber = direct.LazyPtr(in.GetPhoneNumber())
+	out.SipURI = direct.LazyPtr(in.GetSipUri())
+	return out
+}
+func ResponseMessage_TelephonyTransferCall_ToProto(mapCtx *direct.MapContext, in *krm.ResponseMessage_TelephonyTransferCall) *pb.ResponseMessage_TelephonyTransferCall {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ResponseMessage_TelephonyTransferCall{}
+	if oneof := ResponseMessage_TelephonyTransferCall_PhoneNumber_ToProto(mapCtx, in.PhoneNumber); oneof != nil {
+		out.Endpoint = oneof
+	}
+	if oneof := ResponseMessage_TelephonyTransferCall_SipUri_ToProto(mapCtx, in.SipURI); oneof != nil {
+		out.Endpoint = oneof
+	}
+	return out
+}
+func ResponseMessage_Text_FromProto(mapCtx *direct.MapContext, in *pb.ResponseMessage_Text) *krm.ResponseMessage_Text {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ResponseMessage_Text{}
+	out.Text = in.Text
+	return out
+}
+func ResponseMessage_Text_ToProto(mapCtx *direct.MapContext, in *krm.ResponseMessage_Text) *pb.ResponseMessage_Text {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ResponseMessage_Text{}
+	out.Text = in.Text
 	return out
 }
 func Sentiment_FromProto(mapCtx *direct.MapContext, in *pb.Sentiment) *krm.Sentiment {
