0e74bfa6a1235ce9419d693ab10c6b050d5799cf
Your Name <yuwenma@google.com>
2025-02-08 10:05:26 +0000

    feat: add resource SecuresourcemanagerInstance

--- pkg/controller/direct/securesourcemanager/mapper.generated.go
+++ pkg/controller/direct/securesourcemanager/mapper.generated.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,24 +15,45 @@
 package securesourcemanager
 
 import (
-	pb "cloud.google.com/go/securesourcemanager/apiv1/securesourcemanagerpb"
 	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	pb "cloud.google.com/go/securesourcemanager/apiv1/securesourcemanagerpb"
 	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/securesourcemanager/v1alpha1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 )
-
 func Instance_HostConfig_FromProto(mapCtx *direct.MapContext, in *pb.Instance_HostConfig) *krm.Instance_HostConfig {
 	if in == nil {
 		return nil
 	}
 	out := &krm.Instance_HostConfig{}
+	// MISSING: HTML
+	// MISSING: Api
+	// MISSING: GitHTTP
+	// MISSING: GitSSH
+	return out
+}
+func Instance_HostConfig_ToProto(mapCtx *direct.MapContext, in *krm.Instance_HostConfig) *pb.Instance_HostConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Instance_HostConfig{}
+	// MISSING: HTML
+	// MISSING: Api
+	// MISSING: GitHTTP
+	// MISSING: GitSSH
+	return out
+}
+func Instance_HostConfigObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Instance_HostConfig) *krm.Instance_HostConfigObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Instance_HostConfigObservedState{}
 	out.HTML = direct.LazyPtr(in.GetHtml())
 	out.Api = direct.LazyPtr(in.GetApi())
 	out.GitHTTP = direct.LazyPtr(in.GetGitHttp())
 	out.GitSSH = direct.LazyPtr(in.GetGitSsh())
 	return out
 }
-func Instance_HostConfig_ToProto(mapCtx *direct.MapContext, in *krm.Instance_HostConfig) *pb.Instance_HostConfig {
+func Instance_HostConfigObservedState_ToProto(mapCtx *direct.MapContext, in *krm.Instance_HostConfigObservedState) *pb.Instance_HostConfig {
 	if in == nil {
 		return nil
 	}
@@ -41,48 +62,6 @@ func Instance_HostConfig_ToProto(mapCtx *direct.MapContext, in *krm.Instance_Hos
 	out.Api = direct.ValueOf(in.Api)
 	out.GitHttp = direct.ValueOf(in.GitHTTP)
 	out.GitSsh = direct.ValueOf(in.GitSSH)
-	return out
-}
-func Repository_InitialConfig_FromProto(mapCtx *direct.MapContext, in *pb.Repository_InitialConfig) *krm.Repository_InitialConfig {
-	if in == nil {
-		return nil
-	}
-	out := &krm.Repository_InitialConfig{}
-	out.DefaultBranch = direct.LazyPtr(in.GetDefaultBranch())
-	out.Gitignores = in.Gitignores
-	out.License = direct.LazyPtr(in.GetLicense())
-	out.Readme = direct.LazyPtr(in.GetReadme())
-	return out
-}
-func Repository_InitialConfig_ToProto(mapCtx *direct.MapContext, in *krm.Repository_InitialConfig) *pb.Repository_InitialConfig {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Repository_InitialConfig{}
-	out.DefaultBranch = direct.ValueOf(in.DefaultBranch)
-	out.Gitignores = in.Gitignores
-	out.License = direct.ValueOf(in.License)
-	out.Readme = direct.ValueOf(in.Readme)
-	return out
-}
-func Repository_URIs_FromProto(mapCtx *direct.MapContext, in *pb.Repository_URIs) *krm.Repository_URIs {
-	if in == nil {
-		return nil
-	}
-	out := &krm.Repository_URIs{}
-	out.HTML = direct.LazyPtr(in.GetHtml())
-	out.GitHTTPS = direct.LazyPtr(in.GetGitHttps())
-	out.Api = direct.LazyPtr(in.GetApi())
-	return out
-}
-func Repository_URIs_ToProto(mapCtx *direct.MapContext, in *krm.Repository_URIs) *pb.Repository_URIs {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Repository_URIs{}
-	out.Html = direct.ValueOf(in.HTML)
-	out.GitHttps = direct.ValueOf(in.GitHTTPS)
-	out.Api = direct.ValueOf(in.Api)
 	return out
 }
 func SecureSourceManagerInstanceObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Instance) *krm.SecureSourceManagerInstanceObservedState {
@@ -96,6 +75,7 @@ func SecureSourceManagerInstanceObservedState_FromProto(mapCtx *direct.MapContex
 	// MISSING: Labels
 	out.State = direct.Enum_FromProto(mapCtx, in.GetState())
 	out.StateNote = direct.Enum_FromProto(mapCtx, in.GetStateNote())
+	// MISSING: KMSKey
 	out.HostConfig = Instance_HostConfig_FromProto(mapCtx, in.GetHostConfig())
 	return out
 }
@@ -110,6 +90,7 @@ func SecureSourceManagerInstanceObservedState_ToProto(mapCtx *direct.MapContext,
 	// MISSING: Labels
 	out.State = direct.Enum_ToProto[pb.Instance_State](mapCtx, in.State)
 	out.StateNote = direct.Enum_ToProto[pb.Instance_StateNote](mapCtx, in.StateNote)
+	// MISSING: KMSKey
 	out.HostConfig = Instance_HostConfig_ToProto(mapCtx, in.HostConfig)
 	return out
 }
@@ -123,9 +104,7 @@ func SecureSourceManagerInstanceSpec_FromProto(mapCtx *direct.MapContext, in *pb
 	// MISSING: UpdateTime
 	// MISSING: Labels
 	out.PrivateConfig = Instance_PrivateConfig_FromProto(mapCtx, in.GetPrivateConfig())
-	if in.GetKmsKey() != "" {
-		out.KmsKeyRef = &refs.KMSCryptoKeyRef{External: in.GetKmsKey()}
-	}
+	// MISSING: KMSKey
 	return out
 }
 func SecureSourceManagerInstanceSpec_ToProto(mapCtx *direct.MapContext, in *krm.SecureSourceManagerInstanceSpec) *pb.Instance {
@@ -138,9 +117,7 @@ func SecureSourceManagerInstanceSpec_ToProto(mapCtx *direct.MapContext, in *krm.
 	// MISSING: UpdateTime
 	// MISSING: Labels
 	out.PrivateConfig = Instance_PrivateConfig_ToProto(mapCtx, in.PrivateConfig)
-	if in.KmsKeyRef != nil {
-		out.KmsKey = in.KmsKeyRef.External
-	}
+	// MISSING: KMSKey
 	return out
 }
 func SecureSourceManagerRepositorySpec_FromProto(mapCtx *direct.MapContext, in *pb.Repository) *krm.SecureSourceManagerRepositorySpec {
@@ -151,7 +128,7 @@ func SecureSourceManagerRepositorySpec_FromProto(mapCtx *direct.MapContext, in *
 	// MISSING: Name
 	// MISSING: Description
 	if in.GetInstance() != "" {
-		out.InstanceRef = &krm.SecureSourceManagerInstanceRef{External: in.GetInstance()}
+		out.InstanceRef = &refs.*SecureSourceManagerInstanceRef{External: in.GetInstance()}
 	}
 	// MISSING: Uid
 	// MISSING: CreateTime
