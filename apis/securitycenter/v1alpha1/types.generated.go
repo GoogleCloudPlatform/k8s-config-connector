e493a312788d6ff87e2da6715a80d0784c737e0b
Your Name <yuwenma@google.com>
2025-02-08 10:21:58 +0000

    feat: add resource SecuritycenterAsset

---
+++ apis/securitycenter/v1alpha1/types.generated.go
@@ -0,0 +1,198 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+
+// +kcc:proto=google.cloud.securitycenter.v1.Asset
+type Asset struct {
+	// The relative resource name of this asset. See:
+	//  https://cloud.google.com/apis/design/resource_names#relative_resource_name
+	//  Example:
+	//  "organizations/{organization_id}/assets/{asset_id}".
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Asset.name
+	Name *string `json:"name,omitempty"`
+
+	// Security Command Center managed properties. These properties are managed by
+	//  Security Command Center and cannot be modified by the user.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Asset.security_center_properties
+	SecurityCenterProperties *Asset_SecurityCenterProperties `json:"securityCenterProperties,omitempty"`
+
+	// TODO: unsupported map type with key string and value message
+
+
+	// User specified security marks. These marks are entirely managed by the user
+	//  and come from the SecurityMarks resource that belongs to the asset.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Asset.security_marks
+	SecurityMarks *SecurityMarks `json:"securityMarks,omitempty"`
+
+	// The time at which the asset was created in Security Command Center.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Asset.create_time
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// The time at which the asset was last updated or added in Cloud SCC.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Asset.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
+
+	// Cloud IAM Policy information associated with the Google Cloud resource
+	//  described by the Security Command Center asset. This information is managed
+	//  and defined by the Google Cloud resource and cannot be modified by the
+	//  user.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Asset.iam_policy
+	IamPolicy *Asset_IamPolicy `json:"iamPolicy,omitempty"`
+
+	// The canonical name of the resource. It's either
+	//  "organizations/{organization_id}/assets/{asset_id}",
+	//  "folders/{folder_id}/assets/{asset_id}" or
+	//  "projects/{project_number}/assets/{asset_id}", depending on the closest CRM
+	//  ancestor of the resource.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Asset.canonical_name
+	CanonicalName *string `json:"canonicalName,omitempty"`
+}
+
+// +kcc:proto=google.cloud.securitycenter.v1.Asset.IamPolicy
+type Asset_IamPolicy struct {
+	// The JSON representation of the Policy associated with the asset.
+	//  See https://cloud.google.com/iam/reference/rest/v1/Policy for format
+	//  details.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Asset.IamPolicy.policy_blob
+	PolicyBlob *string `json:"policyBlob,omitempty"`
+}
+
+// +kcc:proto=google.cloud.securitycenter.v1.Asset.SecurityCenterProperties
+type Asset_SecurityCenterProperties struct {
+	// The full resource name of the Google Cloud resource this asset
+	//  represents. This field is immutable after create time. See:
+	//  https://cloud.google.com/apis/design/resource_names#full_resource_name
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Asset.SecurityCenterProperties.resource_name
+	ResourceName *string `json:"resourceName,omitempty"`
+
+	// The type of the Google Cloud resource. Examples include: APPLICATION,
+	//  PROJECT, and ORGANIZATION. This is a case insensitive field defined by
+	//  Security Command Center and/or the producer of the resource and is
+	//  immutable after create time.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Asset.SecurityCenterProperties.resource_type
+	ResourceType *string `json:"resourceType,omitempty"`
+
+	// The full resource name of the immediate parent of the resource. See:
+	//  https://cloud.google.com/apis/design/resource_names#full_resource_name
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Asset.SecurityCenterProperties.resource_parent
+	ResourceParent *string `json:"resourceParent,omitempty"`
+
+	// The full resource name of the project the resource belongs to. See:
+	//  https://cloud.google.com/apis/design/resource_names#full_resource_name
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Asset.SecurityCenterProperties.resource_project
+	ResourceProject *string `json:"resourceProject,omitempty"`
+
+	// Owners of the Google Cloud resource.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Asset.SecurityCenterProperties.resource_owners
+	ResourceOwners []string `json:"resourceOwners,omitempty"`
+
+	// The user defined display name for this resource.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Asset.SecurityCenterProperties.resource_display_name
+	ResourceDisplayName *string `json:"resourceDisplayName,omitempty"`
+
+	// The user defined display name for the parent of this resource.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Asset.SecurityCenterProperties.resource_parent_display_name
+	ResourceParentDisplayName *string `json:"resourceParentDisplayName,omitempty"`
+
+	// The user defined display name for the project of this resource.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Asset.SecurityCenterProperties.resource_project_display_name
+	ResourceProjectDisplayName *string `json:"resourceProjectDisplayName,omitempty"`
+
+	// Contains a Folder message for each folder in the assets ancestry.
+	//  The first folder is the deepest nested folder, and the last folder is the
+	//  folder directly under the Organization.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Asset.SecurityCenterProperties.folders
+	Folders []Folder `json:"folders,omitempty"`
+}
+
+// +kcc:proto=google.cloud.securitycenter.v1.Folder
+type Folder struct {
+	// Full resource name of this folder. See:
+	//  https://cloud.google.com/apis/design/resource_names#full_resource_name
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Folder.resource_folder
+	ResourceFolder *string `json:"resourceFolder,omitempty"`
+
+	// The user defined display name for this folder.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Folder.resource_folder_display_name
+	ResourceFolderDisplayName *string `json:"resourceFolderDisplayName,omitempty"`
+}
+
+// +kcc:proto=google.cloud.securitycenter.v1.SecurityMarks
+type SecurityMarks struct {
+	// The relative resource name of the SecurityMarks. See:
+	//  https://cloud.google.com/apis/design/resource_names#relative_resource_name
+	//  Examples:
+	//  "organizations/{organization_id}/assets/{asset_id}/securityMarks"
+	//  "organizations/{organization_id}/sources/{source_id}/findings/{finding_id}/securityMarks".
+	// +kcc:proto:field=google.cloud.securitycenter.v1.SecurityMarks.name
+	Name *string `json:"name,omitempty"`
+
+	// Mutable user specified security marks belonging to the parent resource.
+	//  Constraints are as follows:
+	//
+	//    * Keys and values are treated as case insensitive
+	//    * Keys must be between 1 - 256 characters (inclusive)
+	//    * Keys must be letters, numbers, underscores, or dashes
+	//    * Values have leading and trailing whitespace trimmed, remaining
+	//      characters must be between 1 - 4096 characters (inclusive)
+	// +kcc:proto:field=google.cloud.securitycenter.v1.SecurityMarks.marks
+	Marks map[string]string `json:"marks,omitempty"`
+
+	// The canonical name of the marks.
+	//  Examples:
+	//  "organizations/{organization_id}/assets/{asset_id}/securityMarks"
+	//  "folders/{folder_id}/assets/{asset_id}/securityMarks"
+	//  "projects/{project_number}/assets/{asset_id}/securityMarks"
+	//  "organizations/{organization_id}/sources/{source_id}/findings/{finding_id}/securityMarks"
+	//  "folders/{folder_id}/sources/{source_id}/findings/{finding_id}/securityMarks"
+	//  "projects/{project_number}/sources/{source_id}/findings/{finding_id}/securityMarks"
+	// +kcc:proto:field=google.cloud.securitycenter.v1.SecurityMarks.canonical_name
+	CanonicalName *string `json:"canonicalName,omitempty"`
+}
+
+// +kcc:proto=google.protobuf.ListValue
+type ListValue struct {
+	// Repeated field of dynamically typed values.
+	// +kcc:proto:field=google.protobuf.ListValue.values
+	Values []Value `json:"values,omitempty"`
+}
+
+// +kcc:proto=google.protobuf.Value
+type Value struct {
+	// Represents a null value.
+	// +kcc:proto:field=google.protobuf.Value.null_value
+	NullValue *string `json:"nullValue,omitempty"`
+
+	// Represents a double value.
+	// +kcc:proto:field=google.protobuf.Value.number_value
+	NumberValue *float64 `json:"numberValue,omitempty"`
+
+	// Represents a string value.
+	// +kcc:proto:field=google.protobuf.Value.string_value
+	StringValue *string `json:"stringValue,omitempty"`
+
+	// Represents a boolean value.
+	// +kcc:proto:field=google.protobuf.Value.bool_value
+	BoolValue *bool `json:"boolValue,omitempty"`
+
+	// Represents a structured value.
+	// +kcc:proto:field=google.protobuf.Value.struct_value
+	StructValue map[string]string `json:"structValue,omitempty"`
+
+	// Represents a repeated `Value`.
+	// +kcc:proto:field=google.protobuf.Value.list_value
+	ListValue *ListValue `json:"listValue,omitempty"`
+}
---
+++ pkg/controller/direct/securitycenter/mapper.generated.go
@@ -0,0 +1,198 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package securitycenter
+
+import (
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	pb "cloud.google.com/go/securitycenter/apiv1/securitycenterpb"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/securitycenter/v1alpha1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+)
+func Asset_FromProto(mapCtx *direct.MapContext, in *pb.Asset) *krm.Asset {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Asset{}
+	out.Name = direct.LazyPtr(in.GetName())
+	out.SecurityCenterProperties = Asset_SecurityCenterProperties_FromProto(mapCtx, in.GetSecurityCenterProperties())
+	// MISSING: ResourceProperties
+	out.SecurityMarks = SecurityMarks_FromProto(mapCtx, in.GetSecurityMarks())
+	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
+	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
+	out.IamPolicy = Asset_IamPolicy_FromProto(mapCtx, in.GetIamPolicy())
+	out.CanonicalName = direct.LazyPtr(in.GetCanonicalName())
+	return out
+}
+func Asset_ToProto(mapCtx *direct.MapContext, in *krm.Asset) *pb.Asset {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Asset{}
+	out.Name = direct.ValueOf(in.Name)
+	out.SecurityCenterProperties = Asset_SecurityCenterProperties_ToProto(mapCtx, in.SecurityCenterProperties)
+	// MISSING: ResourceProperties
+	out.SecurityMarks = SecurityMarks_ToProto(mapCtx, in.SecurityMarks)
+	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
+	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
+	out.IamPolicy = Asset_IamPolicy_ToProto(mapCtx, in.IamPolicy)
+	out.CanonicalName = direct.ValueOf(in.CanonicalName)
+	return out
+}
+func Asset_IamPolicy_FromProto(mapCtx *direct.MapContext, in *pb.Asset_IamPolicy) *krm.Asset_IamPolicy {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Asset_IamPolicy{}
+	out.PolicyBlob = direct.LazyPtr(in.GetPolicyBlob())
+	return out
+}
+func Asset_IamPolicy_ToProto(mapCtx *direct.MapContext, in *krm.Asset_IamPolicy) *pb.Asset_IamPolicy {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Asset_IamPolicy{}
+	out.PolicyBlob = direct.ValueOf(in.PolicyBlob)
+	return out
+}
+func Asset_SecurityCenterProperties_FromProto(mapCtx *direct.MapContext, in *pb.Asset_SecurityCenterProperties) *krm.Asset_SecurityCenterProperties {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Asset_SecurityCenterProperties{}
+	out.ResourceName = direct.LazyPtr(in.GetResourceName())
+	out.ResourceType = direct.LazyPtr(in.GetResourceType())
+	out.ResourceParent = direct.LazyPtr(in.GetResourceParent())
+	out.ResourceProject = direct.LazyPtr(in.GetResourceProject())
+	out.ResourceOwners = in.ResourceOwners
+	out.ResourceDisplayName = direct.LazyPtr(in.GetResourceDisplayName())
+	out.ResourceParentDisplayName = direct.LazyPtr(in.GetResourceParentDisplayName())
+	out.ResourceProjectDisplayName = direct.LazyPtr(in.GetResourceProjectDisplayName())
+	out.Folders = direct.Slice_FromProto(mapCtx, in.Folders, Folder_FromProto)
+	return out
+}
+func Asset_SecurityCenterProperties_ToProto(mapCtx *direct.MapContext, in *krm.Asset_SecurityCenterProperties) *pb.Asset_SecurityCenterProperties {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Asset_SecurityCenterProperties{}
+	out.ResourceName = direct.ValueOf(in.ResourceName)
+	out.ResourceType = direct.ValueOf(in.ResourceType)
+	out.ResourceParent = direct.ValueOf(in.ResourceParent)
+	out.ResourceProject = direct.ValueOf(in.ResourceProject)
+	out.ResourceOwners = in.ResourceOwners
+	out.ResourceDisplayName = direct.ValueOf(in.ResourceDisplayName)
+	out.ResourceParentDisplayName = direct.ValueOf(in.ResourceParentDisplayName)
+	out.ResourceProjectDisplayName = direct.ValueOf(in.ResourceProjectDisplayName)
+	out.Folders = direct.Slice_ToProto(mapCtx, in.Folders, Folder_ToProto)
+	return out
+}
+func Folder_FromProto(mapCtx *direct.MapContext, in *pb.Folder) *krm.Folder {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Folder{}
+	out.ResourceFolder = direct.LazyPtr(in.GetResourceFolder())
+	out.ResourceFolderDisplayName = direct.LazyPtr(in.GetResourceFolderDisplayName())
+	return out
+}
+func Folder_ToProto(mapCtx *direct.MapContext, in *krm.Folder) *pb.Folder {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Folder{}
+	out.ResourceFolder = direct.ValueOf(in.ResourceFolder)
+	out.ResourceFolderDisplayName = direct.ValueOf(in.ResourceFolderDisplayName)
+	return out
+}
+func SecurityMarks_FromProto(mapCtx *direct.MapContext, in *pb.SecurityMarks) *krm.SecurityMarks {
+	if in == nil {
+		return nil
+	}
+	out := &krm.SecurityMarks{}
+	out.Name = direct.LazyPtr(in.GetName())
+	out.Marks = in.Marks
+	out.CanonicalName = direct.LazyPtr(in.GetCanonicalName())
+	return out
+}
+func SecurityMarks_ToProto(mapCtx *direct.MapContext, in *krm.SecurityMarks) *pb.SecurityMarks {
+	if in == nil {
+		return nil
+	}
+	out := &pb.SecurityMarks{}
+	out.Name = direct.ValueOf(in.Name)
+	out.Marks = in.Marks
+	out.CanonicalName = direct.ValueOf(in.CanonicalName)
+	return out
+}
+func SecuritycenterAssetObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Asset) *krm.SecuritycenterAssetObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.SecuritycenterAssetObservedState{}
+	// MISSING: Name
+	// MISSING: SecurityCenterProperties
+	// MISSING: ResourceProperties
+	// MISSING: SecurityMarks
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: IamPolicy
+	// MISSING: CanonicalName
+	return out
+}
+func SecuritycenterAssetObservedState_ToProto(mapCtx *direct.MapContext, in *krm.SecuritycenterAssetObservedState) *pb.Asset {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Asset{}
+	// MISSING: Name
+	// MISSING: SecurityCenterProperties
+	// MISSING: ResourceProperties
+	// MISSING: SecurityMarks
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: IamPolicy
+	// MISSING: CanonicalName
+	return out
+}
+func SecuritycenterAssetSpec_FromProto(mapCtx *direct.MapContext, in *pb.Asset) *krm.SecuritycenterAssetSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.SecuritycenterAssetSpec{}
+	// MISSING: Name
+	// MISSING: SecurityCenterProperties
+	// MISSING: ResourceProperties
+	// MISSING: SecurityMarks
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: IamPolicy
+	// MISSING: CanonicalName
+	return out
+}
+func SecuritycenterAssetSpec_ToProto(mapCtx *direct.MapContext, in *krm.SecuritycenterAssetSpec) *pb.Asset {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Asset{}
+	// MISSING: Name
+	// MISSING: SecurityCenterProperties
+	// MISSING: ResourceProperties
+	// MISSING: SecurityMarks
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: IamPolicy
+	// MISSING: CanonicalName
+	return out
+}
