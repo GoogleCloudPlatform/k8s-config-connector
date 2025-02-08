06e1f4dee8d90ebc85fcef6a11f2a51317813e94
Your Name <yuwenma@google.com>
2025-02-08 05:43:02 +0000

    feat: add resource FilestoreBackup

---
+++ apis/filestore/v1alpha1/types.generated.go
@@ -0,0 +1,88 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+
+// +kcc:proto=google.cloud.filestore.v1.Backup
+type Backup struct {
+
+	// A description of the backup with 2048 characters or less.
+	//  Requests with longer descriptions will be rejected.
+	// +kcc:proto:field=google.cloud.filestore.v1.Backup.description
+	Description *string `json:"description,omitempty"`
+
+	// Resource labels to represent user provided metadata.
+	// +kcc:proto:field=google.cloud.filestore.v1.Backup.labels
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// The resource name of the source Filestore instance, in the format
+	//  `projects/{project_number}/locations/{location_id}/instances/{instance_id}`,
+	//  used to create this backup.
+	// +kcc:proto:field=google.cloud.filestore.v1.Backup.source_instance
+	SourceInstance *string `json:"sourceInstance,omitempty"`
+
+	// Name of the file share in the source Filestore instance that the
+	//  backup is created from.
+	// +kcc:proto:field=google.cloud.filestore.v1.Backup.source_file_share
+	SourceFileShare *string `json:"sourceFileShare,omitempty"`
+
+	// Immutable. KMS key name used for data encryption.
+	// +kcc:proto:field=google.cloud.filestore.v1.Backup.kms_key
+	KMSKey *string `json:"kmsKey,omitempty"`
+}
+
+// +kcc:proto=google.cloud.filestore.v1.Backup
+type BackupObservedState struct {
+	// Output only. The resource name of the backup, in the format
+	//  `projects/{project_number}/locations/{location_id}/backups/{backup_id}`.
+	// +kcc:proto:field=google.cloud.filestore.v1.Backup.name
+	Name *string `json:"name,omitempty"`
+
+	// Output only. The backup state.
+	// +kcc:proto:field=google.cloud.filestore.v1.Backup.state
+	State *string `json:"state,omitempty"`
+
+	// Output only. The time when the backup was created.
+	// +kcc:proto:field=google.cloud.filestore.v1.Backup.create_time
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// Output only. Capacity of the source file share when the backup was created.
+	// +kcc:proto:field=google.cloud.filestore.v1.Backup.capacity_gb
+	CapacityGB *int64 `json:"capacityGB,omitempty"`
+
+	// Output only. The size of the storage used by the backup. As backups share
+	//  storage, this number is expected to change with backup creation/deletion.
+	// +kcc:proto:field=google.cloud.filestore.v1.Backup.storage_bytes
+	StorageBytes *int64 `json:"storageBytes,omitempty"`
+
+	// Output only. The service tier of the source Filestore instance that this
+	//  backup is created from.
+	// +kcc:proto:field=google.cloud.filestore.v1.Backup.source_instance_tier
+	SourceInstanceTier *string `json:"sourceInstanceTier,omitempty"`
+
+	// Output only. Amount of bytes that will be downloaded if the backup is
+	//  restored. This may be different than storage bytes, since sequential
+	//  backups of the same disk will share storage.
+	// +kcc:proto:field=google.cloud.filestore.v1.Backup.download_bytes
+	DownloadBytes *int64 `json:"downloadBytes,omitempty"`
+
+	// Output only. Reserved for future use.
+	// +kcc:proto:field=google.cloud.filestore.v1.Backup.satisfies_pzs
+	SatisfiesPzs *bool `json:"satisfiesPzs,omitempty"`
+
+	// Output only. Reserved for future use.
+	// +kcc:proto:field=google.cloud.filestore.v1.Backup.satisfies_pzi
+	SatisfiesPzi *bool `json:"satisfiesPzi,omitempty"`
+}
---
+++ pkg/controller/direct/filestore/mapper.generated.go
@@ -0,0 +1,190 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package filestore
+
+import (
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	pb "cloud.google.com/go/filestore/apiv1/filestorepb"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/filestore/v1alpha1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+)
+func Backup_FromProto(mapCtx *direct.MapContext, in *pb.Backup) *krm.Backup {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Backup{}
+	// MISSING: Name
+	out.Description = direct.LazyPtr(in.GetDescription())
+	// MISSING: State
+	// MISSING: CreateTime
+	out.Labels = in.Labels
+	// MISSING: CapacityGB
+	// MISSING: StorageBytes
+	out.SourceInstance = direct.LazyPtr(in.GetSourceInstance())
+	out.SourceFileShare = direct.LazyPtr(in.GetSourceFileShare())
+	// MISSING: SourceInstanceTier
+	// MISSING: DownloadBytes
+	// MISSING: SatisfiesPzs
+	// MISSING: SatisfiesPzi
+	out.KMSKey = direct.LazyPtr(in.GetKmsKey())
+	return out
+}
+func Backup_ToProto(mapCtx *direct.MapContext, in *krm.Backup) *pb.Backup {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Backup{}
+	// MISSING: Name
+	out.Description = direct.ValueOf(in.Description)
+	// MISSING: State
+	// MISSING: CreateTime
+	out.Labels = in.Labels
+	// MISSING: CapacityGB
+	// MISSING: StorageBytes
+	out.SourceInstance = direct.ValueOf(in.SourceInstance)
+	out.SourceFileShare = direct.ValueOf(in.SourceFileShare)
+	// MISSING: SourceInstanceTier
+	// MISSING: DownloadBytes
+	// MISSING: SatisfiesPzs
+	// MISSING: SatisfiesPzi
+	out.KmsKey = direct.ValueOf(in.KMSKey)
+	return out
+}
+func BackupObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Backup) *krm.BackupObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.BackupObservedState{}
+	out.Name = direct.LazyPtr(in.GetName())
+	// MISSING: Description
+	out.State = direct.Enum_FromProto(mapCtx, in.GetState())
+	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
+	// MISSING: Labels
+	out.CapacityGB = direct.LazyPtr(in.GetCapacityGb())
+	out.StorageBytes = direct.LazyPtr(in.GetStorageBytes())
+	// MISSING: SourceInstance
+	// MISSING: SourceFileShare
+	out.SourceInstanceTier = direct.Enum_FromProto(mapCtx, in.GetSourceInstanceTier())
+	out.DownloadBytes = direct.LazyPtr(in.GetDownloadBytes())
+	out.SatisfiesPzs = direct.BoolValue_FromProto(mapCtx, in.GetSatisfiesPzs())
+	out.SatisfiesPzi = direct.LazyPtr(in.GetSatisfiesPzi())
+	// MISSING: KMSKey
+	return out
+}
+func BackupObservedState_ToProto(mapCtx *direct.MapContext, in *krm.BackupObservedState) *pb.Backup {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Backup{}
+	out.Name = direct.ValueOf(in.Name)
+	// MISSING: Description
+	out.State = direct.Enum_ToProto[pb.Backup_State](mapCtx, in.State)
+	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
+	// MISSING: Labels
+	out.CapacityGb = direct.ValueOf(in.CapacityGB)
+	out.StorageBytes = direct.ValueOf(in.StorageBytes)
+	// MISSING: SourceInstance
+	// MISSING: SourceFileShare
+	out.SourceInstanceTier = direct.Enum_ToProto[pb.Instance_Tier](mapCtx, in.SourceInstanceTier)
+	out.DownloadBytes = direct.ValueOf(in.DownloadBytes)
+	out.SatisfiesPzs = direct.BoolValue_ToProto(mapCtx, in.SatisfiesPzs)
+	out.SatisfiesPzi = direct.ValueOf(in.SatisfiesPzi)
+	// MISSING: KMSKey
+	return out
+}
+func FilestoreBackupObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Backup) *krm.FilestoreBackupObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.FilestoreBackupObservedState{}
+	// MISSING: Name
+	// MISSING: Description
+	// MISSING: State
+	// MISSING: CreateTime
+	// MISSING: Labels
+	// MISSING: CapacityGB
+	// MISSING: StorageBytes
+	// MISSING: SourceInstance
+	// MISSING: SourceFileShare
+	// MISSING: SourceInstanceTier
+	// MISSING: DownloadBytes
+	// MISSING: SatisfiesPzs
+	// MISSING: SatisfiesPzi
+	// MISSING: KMSKey
+	return out
+}
+func FilestoreBackupObservedState_ToProto(mapCtx *direct.MapContext, in *krm.FilestoreBackupObservedState) *pb.Backup {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Backup{}
+	// MISSING: Name
+	// MISSING: Description
+	// MISSING: State
+	// MISSING: CreateTime
+	// MISSING: Labels
+	// MISSING: CapacityGB
+	// MISSING: StorageBytes
+	// MISSING: SourceInstance
+	// MISSING: SourceFileShare
+	// MISSING: SourceInstanceTier
+	// MISSING: DownloadBytes
+	// MISSING: SatisfiesPzs
+	// MISSING: SatisfiesPzi
+	// MISSING: KMSKey
+	return out
+}
+func FilestoreBackupSpec_FromProto(mapCtx *direct.MapContext, in *pb.Backup) *krm.FilestoreBackupSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.FilestoreBackupSpec{}
+	// MISSING: Name
+	// MISSING: Description
+	// MISSING: State
+	// MISSING: CreateTime
+	// MISSING: Labels
+	// MISSING: CapacityGB
+	// MISSING: StorageBytes
+	// MISSING: SourceInstance
+	// MISSING: SourceFileShare
+	// MISSING: SourceInstanceTier
+	// MISSING: DownloadBytes
+	// MISSING: SatisfiesPzs
+	// MISSING: SatisfiesPzi
+	// MISSING: KMSKey
+	return out
+}
+func FilestoreBackupSpec_ToProto(mapCtx *direct.MapContext, in *krm.FilestoreBackupSpec) *pb.Backup {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Backup{}
+	// MISSING: Name
+	// MISSING: Description
+	// MISSING: State
+	// MISSING: CreateTime
+	// MISSING: Labels
+	// MISSING: CapacityGB
+	// MISSING: StorageBytes
+	// MISSING: SourceInstance
+	// MISSING: SourceFileShare
+	// MISSING: SourceInstanceTier
+	// MISSING: DownloadBytes
+	// MISSING: SatisfiesPzs
+	// MISSING: SatisfiesPzi
+	// MISSING: KMSKey
+	return out
+}
