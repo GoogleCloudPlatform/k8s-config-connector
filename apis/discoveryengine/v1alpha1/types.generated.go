c7a9ae1bd51538b07ef1d4ebfc7c332a31cf6613
Your Name <yuwenma@google.com>
2025-02-08 05:06:28 +0000

    feat: add resource DiscoveryengineTargetSite

--- apis/discoveryengine/v1alpha1/types.generated.go
+++ apis/discoveryengine/v1alpha1/types.generated.go
@@ -15,28 +15,84 @@
 package v1alpha1
 
 
-// +kcc:proto=google.cloud.discoveryengine.v1.SiteVerificationInfo
+// +kcc:proto=google.cloud.discoveryengine.v1beta.SiteVerificationInfo
 type SiteVerificationInfo struct {
 	// Site verification state indicating the ownership and validity.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.SiteVerificationInfo.site_verification_state
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.SiteVerificationInfo.site_verification_state
 	SiteVerificationState *string `json:"siteVerificationState,omitempty"`
 
 	// Latest site verification time.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.SiteVerificationInfo.verify_time
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.SiteVerificationInfo.verify_time
 	VerifyTime *string `json:"verifyTime,omitempty"`
 }
 
-// +kcc:proto=google.cloud.discoveryengine.v1.TargetSite.FailureReason
+// +kcc:proto=google.cloud.discoveryengine.v1beta.TargetSite
+type TargetSite struct {
+
+	// Required. Input only. The user provided URI pattern from which the
+	//  `generated_uri_pattern` is generated.
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.TargetSite.provided_uri_pattern
+	ProvidedURIPattern *string `json:"providedURIPattern,omitempty"`
+
+	// The type of the target site, e.g., whether the site is to be included or
+	//  excluded.
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.TargetSite.type
+	Type *string `json:"type,omitempty"`
+
+	// Input only. If set to false, a uri_pattern is generated to include all
+	//  pages whose address contains the provided_uri_pattern. If set to true, an
+	//  uri_pattern is generated to try to be an exact match of the
+	//  provided_uri_pattern or just the specific page if the provided_uri_pattern
+	//  is a specific one. provided_uri_pattern is always normalized to
+	//  generate the URI pattern to be used by the search engine.
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.TargetSite.exact_match
+	ExactMatch *bool `json:"exactMatch,omitempty"`
+}
+
+// +kcc:proto=google.cloud.discoveryengine.v1beta.TargetSite.FailureReason
 type TargetSite_FailureReason struct {
 	// Failed due to insufficient quota.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.TargetSite.FailureReason.quota_failure
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.TargetSite.FailureReason.quota_failure
 	QuotaFailure *TargetSite_FailureReason_QuotaFailure `json:"quotaFailure,omitempty"`
 }
 
-// +kcc:proto=google.cloud.discoveryengine.v1.TargetSite.FailureReason.QuotaFailure
+// +kcc:proto=google.cloud.discoveryengine.v1beta.TargetSite.FailureReason.QuotaFailure
 type TargetSite_FailureReason_QuotaFailure struct {
 	// This number is an estimation on how much total quota this project needs
 	//  to successfully complete indexing.
-	// +kcc:proto:field=google.cloud.discoveryengine.v1.TargetSite.FailureReason.QuotaFailure.total_required_quota
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.TargetSite.FailureReason.QuotaFailure.total_required_quota
 	TotalRequiredQuota *int64 `json:"totalRequiredQuota,omitempty"`
+}
+
+// +kcc:proto=google.cloud.discoveryengine.v1beta.TargetSite
+type TargetSiteObservedState struct {
+	// Output only. The fully qualified resource name of the target site.
+	//  `projects/{project}/locations/{location}/collections/{collection}/dataStores/{data_store}/siteSearchEngine/targetSites/{target_site}`
+	//  The `target_site_id` is system-generated.
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.TargetSite.name
+	Name *string `json:"name,omitempty"`
+
+	// Output only. This is system-generated based on the provided_uri_pattern.
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.TargetSite.generated_uri_pattern
+	GeneratedURIPattern *string `json:"generatedURIPattern,omitempty"`
+
+	// Output only. Root domain of the provided_uri_pattern.
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.TargetSite.root_domain_uri
+	RootDomainURI *string `json:"rootDomainURI,omitempty"`
+
+	// Output only. Site ownership and validity verification status.
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.TargetSite.site_verification_info
+	SiteVerificationInfo *SiteVerificationInfo `json:"siteVerificationInfo,omitempty"`
+
+	// Output only. Indexing status.
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.TargetSite.indexing_status
+	IndexingStatus *string `json:"indexingStatus,omitempty"`
+
+	// Output only. The target site's last updated time.
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.TargetSite.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
+
+	// Output only. Failure reason.
+	// +kcc:proto:field=google.cloud.discoveryengine.v1beta.TargetSite.failure_reason
+	FailureReason *TargetSite_FailureReason `json:"failureReason,omitempty"`
 }
--- pkg/controller/direct/discoveryengine/mapper.generated.go
+++ pkg/controller/direct/discoveryengine/mapper.generated.go
@@ -16,152 +16,10 @@ package discoveryengine
 
 import (
 	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
-	pb "cloud.google.com/go/discoveryengine/apiv1/discoveryenginepb"
+	pb "cloud.google.com/go/discoveryengine/apiv1beta/discoveryenginepb"
 	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/discoveryengine/v1alpha1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 )
-func DiscoveryEngineDataStoreObservedState_FromProto(mapCtx *direct.MapContext, in *pb.DataStore) *krm.DiscoveryEngineDataStoreObservedState {
-	if in == nil {
-		return nil
-	}
-	out := &krm.DiscoveryEngineDataStoreObservedState{}
-	// MISSING: Name
-	out.DefaultSchemaID = direct.LazyPtr(in.GetDefaultSchemaId())
-	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
-	out.BillingEstimation = DataStore_BillingEstimation_FromProto(mapCtx, in.GetBillingEstimation())
-	// MISSING: DocumentProcessingConfig
-	// MISSING: StartingSchema
-	return out
-}
-func DiscoveryEngineDataStoreObservedState_ToProto(mapCtx *direct.MapContext, in *krm.DiscoveryEngineDataStoreObservedState) *pb.DataStore {
-	if in == nil {
-		return nil
-	}
-	out := &pb.DataStore{}
-	// MISSING: Name
-	out.DefaultSchemaId = direct.ValueOf(in.DefaultSchemaID)
-	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
-	out.BillingEstimation = DataStore_BillingEstimation_ToProto(mapCtx, in.BillingEstimation)
-	// MISSING: DocumentProcessingConfig
-	// MISSING: StartingSchema
-	return out
-}
-func DiscoveryEngineDataStoreSpec_FromProto(mapCtx *direct.MapContext, in *pb.DataStore) *krm.DiscoveryEngineDataStoreSpec {
-	if in == nil {
-		return nil
-	}
-	out := &krm.DiscoveryEngineDataStoreSpec{}
-	// MISSING: Name
-	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
-	out.IndustryVertical = direct.Enum_FromProto(mapCtx, in.GetIndustryVertical())
-	out.SolutionTypes = direct.EnumSlice_FromProto(mapCtx, in.SolutionTypes)
-	out.ContentConfig = direct.Enum_FromProto(mapCtx, in.GetContentConfig())
-	out.WorkspaceConfig = WorkspaceConfig_FromProto(mapCtx, in.GetWorkspaceConfig())
-	// MISSING: DocumentProcessingConfig
-	// MISSING: StartingSchema
-	return out
-}
-func DiscoveryEngineDataStoreSpec_ToProto(mapCtx *direct.MapContext, in *krm.DiscoveryEngineDataStoreSpec) *pb.DataStore {
-	if in == nil {
-		return nil
-	}
-	out := &pb.DataStore{}
-	// MISSING: Name
-	out.DisplayName = direct.ValueOf(in.DisplayName)
-	out.IndustryVertical = direct.Enum_ToProto[pb.IndustryVertical](mapCtx, in.IndustryVertical)
-	out.SolutionTypes = direct.EnumSlice_ToProto[pb.SolutionType](mapCtx, in.SolutionTypes)
-	out.ContentConfig = direct.Enum_ToProto[pb.DataStore_ContentConfig](mapCtx, in.ContentConfig)
-	out.WorkspaceConfig = WorkspaceConfig_ToProto(mapCtx, in.WorkspaceConfig)
-	// MISSING: DocumentProcessingConfig
-	// MISSING: StartingSchema
-	return out
-}
-func DiscoveryEngineDataStoreTargetSiteObservedState_FromProto(mapCtx *direct.MapContext, in *pb.TargetSite) *krm.DiscoveryEngineDataStoreTargetSiteObservedState {
-	if in == nil {
-		return nil
-	}
-	out := &krm.DiscoveryEngineDataStoreTargetSiteObservedState{}
-	// MISSING: Name
-	// MISSING: ProvidedURIPattern
-	// MISSING: GeneratedURIPattern
-	// (near miss): "GeneratedURIPattern" vs "GeneratedUriPattern"
-	// MISSING: RootDomainURI
-	// (near miss): "RootDomainURI" vs "RootDomainUri"
-	out.SiteVerificationInfo = SiteVerificationInfo_FromProto(mapCtx, in.GetSiteVerificationInfo())
-	out.IndexingStatus = direct.Enum_FromProto(mapCtx, in.GetIndexingStatus())
-	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
-	out.FailureReason = TargetSite_FailureReason_FromProto(mapCtx, in.GetFailureReason())
-	return out
-}
-func DiscoveryEngineDataStoreTargetSiteObservedState_ToProto(mapCtx *direct.MapContext, in *krm.DiscoveryEngineDataStoreTargetSiteObservedState) *pb.TargetSite {
-	if in == nil {
-		return nil
-	}
-	out := &pb.TargetSite{}
-	// MISSING: Name
-	// MISSING: ProvidedURIPattern
-	// MISSING: GeneratedURIPattern
-	// (near miss): "GeneratedURIPattern" vs "GeneratedUriPattern"
-	// MISSING: RootDomainURI
-	// (near miss): "RootDomainURI" vs "RootDomainUri"
-	out.SiteVerificationInfo = SiteVerificationInfo_ToProto(mapCtx, in.SiteVerificationInfo)
-	out.IndexingStatus = direct.Enum_ToProto[pb.TargetSite_IndexingStatus](mapCtx, in.IndexingStatus)
-	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
-	out.FailureReason = TargetSite_FailureReason_ToProto(mapCtx, in.FailureReason)
-	return out
-}
-func DiscoveryEngineDataStoreTargetSiteSpec_FromProto(mapCtx *direct.MapContext, in *pb.TargetSite) *krm.DiscoveryEngineDataStoreTargetSiteSpec {
-	if in == nil {
-		return nil
-	}
-	out := &krm.DiscoveryEngineDataStoreTargetSiteSpec{}
-	// MISSING: Name
-	// MISSING: ProvidedURIPattern
-	// (near miss): "ProvidedURIPattern" vs "ProvidedUriPattern"
-	out.Type = direct.Enum_FromProto(mapCtx, in.GetType())
-	out.ExactMatch = direct.LazyPtr(in.GetExactMatch())
-	// MISSING: GeneratedURIPattern
-	// MISSING: RootDomainURI
-	return out
-}
-func DiscoveryEngineDataStoreTargetSiteSpec_ToProto(mapCtx *direct.MapContext, in *krm.DiscoveryEngineDataStoreTargetSiteSpec) *pb.TargetSite {
-	if in == nil {
-		return nil
-	}
-	out := &pb.TargetSite{}
-	// MISSING: Name
-	// MISSING: ProvidedURIPattern
-	// (near miss): "ProvidedURIPattern" vs "ProvidedUriPattern"
-	out.Type = direct.Enum_ToProto[pb.TargetSite_Type](mapCtx, in.Type)
-	out.ExactMatch = direct.ValueOf(in.ExactMatch)
-	// MISSING: GeneratedURIPattern
-	// MISSING: RootDomainURI
-	return out
-}
-func DiscoveryEngineEngineObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Engine) *krm.DiscoveryEngineEngineObservedState {
-	if in == nil {
-		return nil
-	}
-	out := &krm.DiscoveryEngineEngineObservedState{}
-	// MISSING: ChatEngineMetadata
-	// MISSING: Name
-	// MISSING: CreateTime
-	// MISSING: UpdateTime
-	// MISSING: DataStoreIds
-	return out
-}
-func DiscoveryEngineEngineObservedState_ToProto(mapCtx *direct.MapContext, in *krm.DiscoveryEngineEngineObservedState) *pb.Engine {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Engine{}
-	// MISSING: ChatEngineMetadata
-	// MISSING: Name
-	// MISSING: CreateTime
-	// MISSING: UpdateTime
-	// MISSING: DataStoreIds
-	return out
-}
 func SiteVerificationInfo_FromProto(mapCtx *direct.MapContext, in *pb.SiteVerificationInfo) *krm.SiteVerificationInfo {
 	if in == nil {
 		return nil
@@ -178,6 +36,74 @@ func SiteVerificationInfo_ToProto(mapCtx *direct.MapContext, in *krm.SiteVerific
 	out := &pb.SiteVerificationInfo{}
 	out.SiteVerificationState = direct.Enum_ToProto[pb.SiteVerificationInfo_SiteVerificationState](mapCtx, in.SiteVerificationState)
 	out.VerifyTime = direct.StringTimestamp_ToProto(mapCtx, in.VerifyTime)
+	return out
+}
+func TargetSite_FromProto(mapCtx *direct.MapContext, in *pb.TargetSite) *krm.TargetSite {
+	if in == nil {
+		return nil
+	}
+	out := &krm.TargetSite{}
+	// MISSING: Name
+	out.ProvidedURIPattern = direct.LazyPtr(in.GetProvidedUriPattern())
+	out.Type = direct.Enum_FromProto(mapCtx, in.GetType())
+	out.ExactMatch = direct.LazyPtr(in.GetExactMatch())
+	// MISSING: GeneratedURIPattern
+	// MISSING: RootDomainURI
+	// MISSING: SiteVerificationInfo
+	// MISSING: IndexingStatus
+	// MISSING: UpdateTime
+	// MISSING: FailureReason
+	return out
+}
+func TargetSite_ToProto(mapCtx *direct.MapContext, in *krm.TargetSite) *pb.TargetSite {
+	if in == nil {
+		return nil
+	}
+	out := &pb.TargetSite{}
+	// MISSING: Name
+	out.ProvidedUriPattern = direct.ValueOf(in.ProvidedURIPattern)
+	out.Type = direct.Enum_ToProto[pb.TargetSite_Type](mapCtx, in.Type)
+	out.ExactMatch = direct.ValueOf(in.ExactMatch)
+	// MISSING: GeneratedURIPattern
+	// MISSING: RootDomainURI
+	// MISSING: SiteVerificationInfo
+	// MISSING: IndexingStatus
+	// MISSING: UpdateTime
+	// MISSING: FailureReason
+	return out
+}
+func TargetSiteObservedState_FromProto(mapCtx *direct.MapContext, in *pb.TargetSite) *krm.TargetSiteObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.TargetSiteObservedState{}
+	out.Name = direct.LazyPtr(in.GetName())
+	// MISSING: ProvidedURIPattern
+	// MISSING: Type
+	// MISSING: ExactMatch
+	out.GeneratedURIPattern = direct.LazyPtr(in.GetGeneratedUriPattern())
+	out.RootDomainURI = direct.LazyPtr(in.GetRootDomainUri())
+	out.SiteVerificationInfo = SiteVerificationInfo_FromProto(mapCtx, in.GetSiteVerificationInfo())
+	out.IndexingStatus = direct.Enum_FromProto(mapCtx, in.GetIndexingStatus())
+	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
+	out.FailureReason = TargetSite_FailureReason_FromProto(mapCtx, in.GetFailureReason())
+	return out
+}
+func TargetSiteObservedState_ToProto(mapCtx *direct.MapContext, in *krm.TargetSiteObservedState) *pb.TargetSite {
+	if in == nil {
+		return nil
+	}
+	out := &pb.TargetSite{}
+	out.Name = direct.ValueOf(in.Name)
+	// MISSING: ProvidedURIPattern
+	// MISSING: Type
+	// MISSING: ExactMatch
+	out.GeneratedUriPattern = direct.ValueOf(in.GeneratedURIPattern)
+	out.RootDomainUri = direct.ValueOf(in.RootDomainURI)
+	out.SiteVerificationInfo = SiteVerificationInfo_ToProto(mapCtx, in.SiteVerificationInfo)
+	out.IndexingStatus = direct.Enum_ToProto[pb.TargetSite_IndexingStatus](mapCtx, in.IndexingStatus)
+	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
+	out.FailureReason = TargetSite_FailureReason_ToProto(mapCtx, in.FailureReason)
 	return out
 }
 func TargetSite_FailureReason_FromProto(mapCtx *direct.MapContext, in *pb.TargetSite_FailureReason) *krm.TargetSite_FailureReason {
