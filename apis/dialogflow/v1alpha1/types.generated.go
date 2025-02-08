84ede703f493e66f8fca7def5be3b8bc9491bd3e
Your Name <yuwenma@google.com>
2025-02-08 03:20:02 +0000

    feat: add resource DialogflowChangelog

---
+++ apis/dialogflow/v1alpha1/types.generated.go
@@ -0,0 +1,53 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+
+// +kcc:proto=google.cloud.dialogflow.cx.v3.Changelog
+type Changelog struct {
+	// The unique identifier of the changelog.
+	//  Format:
+	//  `projects/<ProjectID>/locations/<LocationID>/agents/<AgentID>/changelogs/<ChangelogID>`.
+	// +kcc:proto:field=google.cloud.dialogflow.cx.v3.Changelog.name
+	Name *string `json:"name,omitempty"`
+
+	// Email address of the authenticated user.
+	// +kcc:proto:field=google.cloud.dialogflow.cx.v3.Changelog.user_email
+	UserEmail *string `json:"userEmail,omitempty"`
+
+	// The affected resource display name of the change.
+	// +kcc:proto:field=google.cloud.dialogflow.cx.v3.Changelog.display_name
+	DisplayName *string `json:"displayName,omitempty"`
+
+	// The action of the change.
+	// +kcc:proto:field=google.cloud.dialogflow.cx.v3.Changelog.action
+	Action *string `json:"action,omitempty"`
+
+	// The affected resource type.
+	// +kcc:proto:field=google.cloud.dialogflow.cx.v3.Changelog.type
+	Type *string `json:"type,omitempty"`
+
+	// The affected resource name of the change.
+	// +kcc:proto:field=google.cloud.dialogflow.cx.v3.Changelog.resource
+	Resource *string `json:"resource,omitempty"`
+
+	// The timestamp of the change.
+	// +kcc:proto:field=google.cloud.dialogflow.cx.v3.Changelog.create_time
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// The affected language code of the change.
+	// +kcc:proto:field=google.cloud.dialogflow.cx.v3.Changelog.language_code
+	LanguageCode *string `json:"languageCode,omitempty"`
+}
---
+++ pkg/controller/direct/dialogflow/mapper.generated.go
@@ -0,0 +1,112 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dialogflow
+
+import (
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	pb "cloud.google.com/go/dialogflow/cx/apiv3/cxpb"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/dialogflow/v1alpha1"
+)
+func Changelog_FromProto(mapCtx *direct.MapContext, in *pb.Changelog) *krm.Changelog {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Changelog{}
+	out.Name = direct.LazyPtr(in.GetName())
+	out.UserEmail = direct.LazyPtr(in.GetUserEmail())
+	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
+	out.Action = direct.LazyPtr(in.GetAction())
+	out.Type = direct.LazyPtr(in.GetType())
+	out.Resource = direct.LazyPtr(in.GetResource())
+	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
+	out.LanguageCode = direct.LazyPtr(in.GetLanguageCode())
+	return out
+}
+func Changelog_ToProto(mapCtx *direct.MapContext, in *krm.Changelog) *pb.Changelog {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Changelog{}
+	out.Name = direct.ValueOf(in.Name)
+	out.UserEmail = direct.ValueOf(in.UserEmail)
+	out.DisplayName = direct.ValueOf(in.DisplayName)
+	out.Action = direct.ValueOf(in.Action)
+	out.Type = direct.ValueOf(in.Type)
+	out.Resource = direct.ValueOf(in.Resource)
+	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
+	out.LanguageCode = direct.ValueOf(in.LanguageCode)
+	return out
+}
+func DialogflowChangelogObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Changelog) *krm.DialogflowChangelogObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.DialogflowChangelogObservedState{}
+	// MISSING: Name
+	// MISSING: UserEmail
+	// MISSING: DisplayName
+	// MISSING: Action
+	// MISSING: Type
+	// MISSING: Resource
+	// MISSING: CreateTime
+	// MISSING: LanguageCode
+	return out
+}
+func DialogflowChangelogObservedState_ToProto(mapCtx *direct.MapContext, in *krm.DialogflowChangelogObservedState) *pb.Changelog {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Changelog{}
+	// MISSING: Name
+	// MISSING: UserEmail
+	// MISSING: DisplayName
+	// MISSING: Action
+	// MISSING: Type
+	// MISSING: Resource
+	// MISSING: CreateTime
+	// MISSING: LanguageCode
+	return out
+}
+func DialogflowChangelogSpec_FromProto(mapCtx *direct.MapContext, in *pb.Changelog) *krm.DialogflowChangelogSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.DialogflowChangelogSpec{}
+	// MISSING: Name
+	// MISSING: UserEmail
+	// MISSING: DisplayName
+	// MISSING: Action
+	// MISSING: Type
+	// MISSING: Resource
+	// MISSING: CreateTime
+	// MISSING: LanguageCode
+	return out
+}
+func DialogflowChangelogSpec_ToProto(mapCtx *direct.MapContext, in *krm.DialogflowChangelogSpec) *pb.Changelog {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Changelog{}
+	// MISSING: Name
+	// MISSING: UserEmail
+	// MISSING: DisplayName
+	// MISSING: Action
+	// MISSING: Type
+	// MISSING: Resource
+	// MISSING: CreateTime
+	// MISSING: LanguageCode
+	return out
+}
