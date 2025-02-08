c16f0b68a3f02bab0d6691e4712f26a96423afcf
Your Name <yuwenma@google.com>
2025-02-08 11:25:59 +0000

    feat: add resource SpeechPhraseSet

--- apis/speech/v1alpha1/types.generated.go
+++ apis/speech/v1alpha1/types.generated.go
@@ -15,44 +15,107 @@
 package v1alpha1
 
 
-// +kcc:proto=google.cloud.speech.v1p1beta1.PhraseSet
+// +kcc:proto=google.cloud.speech.v2.PhraseSet
 type PhraseSet struct {
-	// The resource name of the phrase set.
-	// +kcc:proto:field=google.cloud.speech.v1p1beta1.PhraseSet.name
-	Name *string `json:"name,omitempty"`
 
 	// A list of word and phrases.
-	// +kcc:proto:field=google.cloud.speech.v1p1beta1.PhraseSet.phrases
+	// +kcc:proto:field=google.cloud.speech.v2.PhraseSet.phrases
 	Phrases []PhraseSet_Phrase `json:"phrases,omitempty"`
 
 	// Hint Boost. Positive value will increase the probability that a specific
 	//  phrase will be recognized over other similar sounding phrases. The higher
 	//  the boost, the higher the chance of false positive recognition as well.
-	//  Negative boost values would correspond to anti-biasing. Anti-biasing is not
-	//  enabled, so negative boost will simply be ignored. Though `boost` can
-	//  accept a wide range of positive values, most use cases are best served with
-	//  values between 0 (exclusive) and 20. We recommend using a binary search
-	//  approach to finding the optimal value for your use case as well as adding
-	//  phrases both with and without boost to your requests.
-	// +kcc:proto:field=google.cloud.speech.v1p1beta1.PhraseSet.boost
+	//  Valid `boost` values are between 0 (exclusive) and 20. We recommend using a
+	//  binary search approach to finding the optimal value for your use case as
+	//  well as adding phrases both with and without boost to your requests.
+	// +kcc:proto:field=google.cloud.speech.v2.PhraseSet.boost
 	Boost *float32 `json:"boost,omitempty"`
+
+	// User-settable, human-readable name for the PhraseSet. Must be 63
+	//  characters or less.
+	// +kcc:proto:field=google.cloud.speech.v2.PhraseSet.display_name
+	DisplayName *string `json:"displayName,omitempty"`
+
+	// Allows users to store small amounts of arbitrary data.
+	//  Both the key and the value must be 63 characters or less each.
+	//  At most 100 annotations.
+	// +kcc:proto:field=google.cloud.speech.v2.PhraseSet.annotations
+	Annotations map[string]string `json:"annotations,omitempty"`
 }
 
-// +kcc:proto=google.cloud.speech.v1p1beta1.PhraseSet.Phrase
+// +kcc:proto=google.cloud.speech.v2.PhraseSet.Phrase
 type PhraseSet_Phrase struct {
 	// The phrase itself.
-	// +kcc:proto:field=google.cloud.speech.v1p1beta1.PhraseSet.Phrase.value
+	// +kcc:proto:field=google.cloud.speech.v2.PhraseSet.Phrase.value
 	Value *string `json:"value,omitempty"`
 
 	// Hint Boost. Overrides the boost set at the phrase set level.
 	//  Positive value will increase the probability that a specific phrase will
 	//  be recognized over other similar sounding phrases. The higher the boost,
 	//  the higher the chance of false positive recognition as well. Negative
-	//  boost will simply be ignored. Though `boost` can accept a wide range of
-	//  positive values, most use cases are best served
-	//  with values between 0 and 20. We recommend using a binary search approach
-	//  to finding the optimal value for your use case as well as adding
-	//  phrases both with and without boost to your requests.
-	// +kcc:proto:field=google.cloud.speech.v1p1beta1.PhraseSet.Phrase.boost
+	//  boost values would correspond to anti-biasing. Anti-biasing is not
+	//  enabled, so negative boost values will return an error. Boost values must
+	//  be between 0 and 20. Any values outside that range will return an error.
+	//  We recommend using a binary search approach to finding the optimal value
+	//  for your use case as well as adding phrases both with and without boost
+	//  to your requests.
+	// +kcc:proto:field=google.cloud.speech.v2.PhraseSet.Phrase.boost
 	Boost *float32 `json:"boost,omitempty"`
+}
+
+// +kcc:proto=google.cloud.speech.v2.PhraseSet
+type PhraseSetObservedState struct {
+	// Output only. Identifier. The resource name of the PhraseSet.
+	//  Format: `projects/{project}/locations/{location}/phraseSets/{phrase_set}`.
+	// +kcc:proto:field=google.cloud.speech.v2.PhraseSet.name
+	Name *string `json:"name,omitempty"`
+
+	// Output only. System-assigned unique identifier for the PhraseSet.
+	// +kcc:proto:field=google.cloud.speech.v2.PhraseSet.uid
+	Uid *string `json:"uid,omitempty"`
+
+	// Output only. The PhraseSet lifecycle state.
+	// +kcc:proto:field=google.cloud.speech.v2.PhraseSet.state
+	State *string `json:"state,omitempty"`
+
+	// Output only. Creation time.
+	// +kcc:proto:field=google.cloud.speech.v2.PhraseSet.create_time
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// Output only. The most recent time this resource was modified.
+	// +kcc:proto:field=google.cloud.speech.v2.PhraseSet.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
+
+	// Output only. The time at which this resource was requested for deletion.
+	// +kcc:proto:field=google.cloud.speech.v2.PhraseSet.delete_time
+	DeleteTime *string `json:"deleteTime,omitempty"`
+
+	// Output only. The time at which this resource will be purged.
+	// +kcc:proto:field=google.cloud.speech.v2.PhraseSet.expire_time
+	ExpireTime *string `json:"expireTime,omitempty"`
+
+	// Output only. This checksum is computed by the server based on the value of
+	//  other fields. This may be sent on update, undelete, and delete requests to
+	//  ensure the client has an up-to-date value before proceeding.
+	// +kcc:proto:field=google.cloud.speech.v2.PhraseSet.etag
+	Etag *string `json:"etag,omitempty"`
+
+	// Output only. Whether or not this PhraseSet is in the process of being
+	//  updated.
+	// +kcc:proto:field=google.cloud.speech.v2.PhraseSet.reconciling
+	Reconciling *bool `json:"reconciling,omitempty"`
+
+	// Output only. The [KMS key
+	//  name](https://cloud.google.com/kms/docs/resource-hierarchy#keys) with which
+	//  the PhraseSet is encrypted. The expected format is
+	//  `projects/{project}/locations/{location}/keyRings/{key_ring}/cryptoKeys/{crypto_key}`.
+	// +kcc:proto:field=google.cloud.speech.v2.PhraseSet.kms_key_name
+	KMSKeyName *string `json:"kmsKeyName,omitempty"`
+
+	// Output only. The [KMS key version
+	//  name](https://cloud.google.com/kms/docs/resource-hierarchy#key_versions)
+	//  with which the PhraseSet is encrypted. The expected format is
+	//  `projects/{project}/locations/{location}/keyRings/{key_ring}/cryptoKeys/{crypto_key}/cryptoKeyVersions/{crypto_key_version}`.
+	// +kcc:proto:field=google.cloud.speech.v2.PhraseSet.kms_key_version_name
+	KMSKeyVersionName *string `json:"kmsKeyVersionName,omitempty"`
 }
--- pkg/controller/direct/speech/mapper.generated.go
+++ pkg/controller/direct/speech/mapper.generated.go
@@ -15,19 +15,31 @@
 package speech
 
 import (
-	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
-	pb "cloud.google.com/go/speech/apiv1p1beta1/speechpb"
-	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/speech/v1alpha1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	pb "cloud.google.com/go/speech/apiv2/speechpb"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/speech/v1alpha1"
 )
 func PhraseSet_FromProto(mapCtx *direct.MapContext, in *pb.PhraseSet) *krm.PhraseSet {
 	if in == nil {
 		return nil
 	}
 	out := &krm.PhraseSet{}
-	out.Name = direct.LazyPtr(in.GetName())
+	// MISSING: Name
+	// MISSING: Uid
 	out.Phrases = direct.Slice_FromProto(mapCtx, in.Phrases, PhraseSet_Phrase_FromProto)
 	out.Boost = direct.LazyPtr(in.GetBoost())
+	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
+	// MISSING: State
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: DeleteTime
+	// MISSING: ExpireTime
+	out.Annotations = in.Annotations
+	// MISSING: Etag
+	// MISSING: Reconciling
+	// MISSING: KMSKeyName
+	// MISSING: KMSKeyVersionName
 	return out
 }
 func PhraseSet_ToProto(mapCtx *direct.MapContext, in *krm.PhraseSet) *pb.PhraseSet {
@@ -35,9 +47,65 @@ func PhraseSet_ToProto(mapCtx *direct.MapContext, in *krm.PhraseSet) *pb.PhraseS
 		return nil
 	}
 	out := &pb.PhraseSet{}
-	out.Name = direct.ValueOf(in.Name)
+	// MISSING: Name
+	// MISSING: Uid
 	out.Phrases = direct.Slice_ToProto(mapCtx, in.Phrases, PhraseSet_Phrase_ToProto)
 	out.Boost = direct.ValueOf(in.Boost)
+	out.DisplayName = direct.ValueOf(in.DisplayName)
+	// MISSING: State
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: DeleteTime
+	// MISSING: ExpireTime
+	out.Annotations = in.Annotations
+	// MISSING: Etag
+	// MISSING: Reconciling
+	// MISSING: KMSKeyName
+	// MISSING: KMSKeyVersionName
+	return out
+}
+func PhraseSetObservedState_FromProto(mapCtx *direct.MapContext, in *pb.PhraseSet) *krm.PhraseSetObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.PhraseSetObservedState{}
+	out.Name = direct.LazyPtr(in.GetName())
+	out.Uid = direct.LazyPtr(in.GetUid())
+	// MISSING: Phrases
+	// MISSING: Boost
+	// MISSING: DisplayName
+	out.State = direct.Enum_FromProto(mapCtx, in.GetState())
+	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
+	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
+	out.DeleteTime = direct.StringTimestamp_FromProto(mapCtx, in.GetDeleteTime())
+	out.ExpireTime = direct.StringTimestamp_FromProto(mapCtx, in.GetExpireTime())
+	// MISSING: Annotations
+	out.Etag = direct.LazyPtr(in.GetEtag())
+	out.Reconciling = direct.LazyPtr(in.GetReconciling())
+	out.KMSKeyName = direct.LazyPtr(in.GetKmsKeyName())
+	out.KMSKeyVersionName = direct.LazyPtr(in.GetKmsKeyVersionName())
+	return out
+}
+func PhraseSetObservedState_ToProto(mapCtx *direct.MapContext, in *krm.PhraseSetObservedState) *pb.PhraseSet {
+	if in == nil {
+		return nil
+	}
+	out := &pb.PhraseSet{}
+	out.Name = direct.ValueOf(in.Name)
+	out.Uid = direct.ValueOf(in.Uid)
+	// MISSING: Phrases
+	// MISSING: Boost
+	// MISSING: DisplayName
+	out.State = direct.Enum_ToProto[pb.PhraseSet_State](mapCtx, in.State)
+	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
+	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
+	out.DeleteTime = direct.StringTimestamp_ToProto(mapCtx, in.DeleteTime)
+	out.ExpireTime = direct.StringTimestamp_ToProto(mapCtx, in.ExpireTime)
+	// MISSING: Annotations
+	out.Etag = direct.ValueOf(in.Etag)
+	out.Reconciling = direct.ValueOf(in.Reconciling)
+	out.KmsKeyName = direct.ValueOf(in.KMSKeyName)
+	out.KmsKeyVersionName = direct.ValueOf(in.KMSKeyVersionName)
 	return out
 }
 func PhraseSet_Phrase_FromProto(mapCtx *direct.MapContext, in *pb.PhraseSet_Phrase) *krm.PhraseSet_Phrase {
