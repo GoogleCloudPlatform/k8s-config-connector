913c7e1c786c7f387c9ff116498086596ded6abc
Your Name <yuwenma@google.com>
2025-02-08 12:12:26 +0000

    feat: add resource VideoVodAdTagDetail

---
+++ apis/video/v1alpha1/types.generated.go
@@ -0,0 +1,77 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+
+// +kcc:proto=google.cloud.video.stitcher.v1.AdRequest
+type AdRequest struct {
+	// The ad tag URI processed with integrated macros.
+	// +kcc:proto:field=google.cloud.video.stitcher.v1.AdRequest.uri
+	URI *string `json:"uri,omitempty"`
+
+	// The request metadata used to make the ad request.
+	// +kcc:proto:field=google.cloud.video.stitcher.v1.AdRequest.request_metadata
+	RequestMetadata *RequestMetadata `json:"requestMetadata,omitempty"`
+
+	// The response metadata received from the ad request.
+	// +kcc:proto:field=google.cloud.video.stitcher.v1.AdRequest.response_metadata
+	ResponseMetadata *ResponseMetadata `json:"responseMetadata,omitempty"`
+}
+
+// +kcc:proto=google.cloud.video.stitcher.v1.RequestMetadata
+type RequestMetadata struct {
+	// The HTTP headers of the ad request.
+	// +kcc:proto:field=google.cloud.video.stitcher.v1.RequestMetadata.headers
+	Headers map[string]string `json:"headers,omitempty"`
+}
+
+// +kcc:proto=google.cloud.video.stitcher.v1.ResponseMetadata
+type ResponseMetadata struct {
+	// Error message received when making the ad request.
+	// +kcc:proto:field=google.cloud.video.stitcher.v1.ResponseMetadata.error
+	Error *string `json:"error,omitempty"`
+
+	// Headers from the response.
+	// +kcc:proto:field=google.cloud.video.stitcher.v1.ResponseMetadata.headers
+	Headers map[string]string `json:"headers,omitempty"`
+
+	// Status code for the response.
+	// +kcc:proto:field=google.cloud.video.stitcher.v1.ResponseMetadata.status_code
+	StatusCode *string `json:"statusCode,omitempty"`
+
+	// Size in bytes of the response.
+	// +kcc:proto:field=google.cloud.video.stitcher.v1.ResponseMetadata.size_bytes
+	SizeBytes *int32 `json:"sizeBytes,omitempty"`
+
+	// Total time elapsed for the response.
+	// +kcc:proto:field=google.cloud.video.stitcher.v1.ResponseMetadata.duration
+	Duration *string `json:"duration,omitempty"`
+
+	// The body of the response.
+	// +kcc:proto:field=google.cloud.video.stitcher.v1.ResponseMetadata.body
+	Body *string `json:"body,omitempty"`
+}
+
+// +kcc:proto=google.cloud.video.stitcher.v1.VodAdTagDetail
+type VodAdTagDetail struct {
+	// The name of the ad tag detail for the specified VOD session, in the form of
+	//  `projects/{project}/locations/{location}/vodSessions/{vod_session_id}/vodAdTagDetails/{id}`.
+	// +kcc:proto:field=google.cloud.video.stitcher.v1.VodAdTagDetail.name
+	Name *string `json:"name,omitempty"`
+
+	// A list of ad requests for one ad tag.
+	// +kcc:proto:field=google.cloud.video.stitcher.v1.VodAdTagDetail.ad_requests
+	AdRequests []AdRequest `json:"adRequests,omitempty"`
+}
---
+++ pkg/controller/direct/video/mapper.generated.go
@@ -0,0 +1,76 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package video
+
+import (
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	pb "cloud.google.com/go/video/stitcher/apiv1/stitcherpb"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/video/v1alpha1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+)
+func VideoVodAdTagDetailObservedState_FromProto(mapCtx *direct.MapContext, in *pb.VodAdTagDetail) *krm.VideoVodAdTagDetailObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.VideoVodAdTagDetailObservedState{}
+	// MISSING: Name
+	// MISSING: AdRequests
+	return out
+}
+func VideoVodAdTagDetailObservedState_ToProto(mapCtx *direct.MapContext, in *krm.VideoVodAdTagDetailObservedState) *pb.VodAdTagDetail {
+	if in == nil {
+		return nil
+	}
+	out := &pb.VodAdTagDetail{}
+	// MISSING: Name
+	// MISSING: AdRequests
+	return out
+}
+func VideoVodAdTagDetailSpec_FromProto(mapCtx *direct.MapContext, in *pb.VodAdTagDetail) *krm.VideoVodAdTagDetailSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.VideoVodAdTagDetailSpec{}
+	// MISSING: Name
+	// MISSING: AdRequests
+	return out
+}
+func VideoVodAdTagDetailSpec_ToProto(mapCtx *direct.MapContext, in *krm.VideoVodAdTagDetailSpec) *pb.VodAdTagDetail {
+	if in == nil {
+		return nil
+	}
+	out := &pb.VodAdTagDetail{}
+	// MISSING: Name
+	// MISSING: AdRequests
+	return out
+}
+func VodAdTagDetail_FromProto(mapCtx *direct.MapContext, in *pb.VodAdTagDetail) *krm.VodAdTagDetail {
+	if in == nil {
+		return nil
+	}
+	out := &krm.VodAdTagDetail{}
+	out.Name = direct.LazyPtr(in.GetName())
+	out.AdRequests = direct.Slice_FromProto(mapCtx, in.AdRequests, AdRequest_FromProto)
+	return out
+}
+func VodAdTagDetail_ToProto(mapCtx *direct.MapContext, in *krm.VodAdTagDetail) *pb.VodAdTagDetail {
+	if in == nil {
+		return nil
+	}
+	out := &pb.VodAdTagDetail{}
+	out.Name = direct.ValueOf(in.Name)
+	out.AdRequests = direct.Slice_ToProto(mapCtx, in.AdRequests, AdRequest_ToProto)
+	return out
+}
