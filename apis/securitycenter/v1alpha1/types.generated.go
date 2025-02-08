b5609107496f113401bbd10e8762c62e5ff69669
Your Name <yuwenma@google.com>
2025-02-08 10:38:09 +0000

    feat: add resource SecuritycenterSource

---
+++ apis/securitycenter/v1alpha1/types.generated.go
@@ -0,0 +1,53 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+
+// +kcc:proto=google.cloud.securitycenter.v1.Source
+type Source struct {
+	// The relative resource name of this source. See:
+	//  https://cloud.google.com/apis/design/resource_names#relative_resource_name
+	//  Example:
+	//  "organizations/{organization_id}/sources/{source_id}"
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Source.name
+	Name *string `json:"name,omitempty"`
+
+	// The source's display name.
+	//  A source's display name must be unique amongst its siblings, for example,
+	//  two sources with the same parent can't share the same display name.
+	//  The display name must have a length between 1 and 64 characters
+	//  (inclusive).
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Source.display_name
+	DisplayName *string `json:"displayName,omitempty"`
+
+	// The description of the source (max of 1024 characters).
+	//  Example:
+	//  "Web Security Scanner is a web security scanner for common
+	//  vulnerabilities in App Engine applications. It can automatically
+	//  scan and detect four common vulnerabilities, including cross-site-scripting
+	//  (XSS), Flash injection, mixed content (HTTP in HTTPS), and
+	//  outdated or insecure libraries."
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Source.description
+	Description *string `json:"description,omitempty"`
+
+	// The canonical name of the finding source. It's either
+	//  "organizations/{organization_id}/sources/{source_id}",
+	//  "folders/{folder_id}/sources/{source_id}", or
+	//  "projects/{project_number}/sources/{source_id}",
+	//  depending on the closest CRM ancestor of the resource associated with the
+	//  finding.
+	// +kcc:proto:field=google.cloud.securitycenter.v1.Source.canonical_name
+	CanonicalName *string `json:"canonicalName,omitempty"`
+}
---
+++ pkg/controller/direct/securitycenter/mapper.generated.go
@@ -0,0 +1,88 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package securitycenter
+
+import (
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	pb "cloud.google.com/go/securitycenter/apiv1/securitycenterpb"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/securitycenter/v1alpha1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+)
+func SecuritycenterSourceObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Source) *krm.SecuritycenterSourceObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.SecuritycenterSourceObservedState{}
+	// MISSING: Name
+	// MISSING: DisplayName
+	// MISSING: Description
+	// MISSING: CanonicalName
+	return out
+}
+func SecuritycenterSourceObservedState_ToProto(mapCtx *direct.MapContext, in *krm.SecuritycenterSourceObservedState) *pb.Source {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Source{}
+	// MISSING: Name
+	// MISSING: DisplayName
+	// MISSING: Description
+	// MISSING: CanonicalName
+	return out
+}
+func SecuritycenterSourceSpec_FromProto(mapCtx *direct.MapContext, in *pb.Source) *krm.SecuritycenterSourceSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.SecuritycenterSourceSpec{}
+	// MISSING: Name
+	// MISSING: DisplayName
+	// MISSING: Description
+	// MISSING: CanonicalName
+	return out
+}
+func SecuritycenterSourceSpec_ToProto(mapCtx *direct.MapContext, in *krm.SecuritycenterSourceSpec) *pb.Source {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Source{}
+	// MISSING: Name
+	// MISSING: DisplayName
+	// MISSING: Description
+	// MISSING: CanonicalName
+	return out
+}
+func Source_FromProto(mapCtx *direct.MapContext, in *pb.Source) *krm.Source {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Source{}
+	out.Name = direct.LazyPtr(in.GetName())
+	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
+	out.Description = direct.LazyPtr(in.GetDescription())
+	out.CanonicalName = direct.LazyPtr(in.GetCanonicalName())
+	return out
+}
+func Source_ToProto(mapCtx *direct.MapContext, in *krm.Source) *pb.Source {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Source{}
+	out.Name = direct.ValueOf(in.Name)
+	out.DisplayName = direct.ValueOf(in.DisplayName)
+	out.Description = direct.ValueOf(in.Description)
+	out.CanonicalName = direct.ValueOf(in.CanonicalName)
+	return out
+}
