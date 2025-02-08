29a2cfc3609adb68a67a1ebdd735c0fdb1a74c0a
Your Name <yuwenma@google.com>
2025-02-08 09:49:21 +0000

    feat: add resource RetailCatalog

--- apis/retail/v1alpha1/types.generated.go
+++ apis/retail/v1alpha1/types.generated.go
@@ -15,58 +15,139 @@
 package v1alpha1
 
 
-// +kcc:proto=google.cloud.retail.v2.Catalog
+// +kcc:proto=google.cloud.retail.v2beta.Catalog
 type Catalog struct {
 	// Required. Immutable. The fully qualified resource name of the catalog.
-	// +kcc:proto:field=google.cloud.retail.v2.Catalog.name
+	// +kcc:proto:field=google.cloud.retail.v2beta.Catalog.name
 	Name *string `json:"name,omitempty"`
 
 	// Required. Immutable. The catalog display name.
 	//
 	//  This field must be a UTF-8 encoded string with a length limit of 128
 	//  characters. Otherwise, an INVALID_ARGUMENT error is returned.
-	// +kcc:proto:field=google.cloud.retail.v2.Catalog.display_name
+	// +kcc:proto:field=google.cloud.retail.v2beta.Catalog.display_name
 	DisplayName *string `json:"displayName,omitempty"`
 
 	// Required. The product level configuration.
-	// +kcc:proto:field=google.cloud.retail.v2.Catalog.product_level_config
+	// +kcc:proto:field=google.cloud.retail.v2beta.Catalog.product_level_config
 	ProductLevelConfig *ProductLevelConfig `json:"productLevelConfig,omitempty"`
+
+	// The Merchant Center linking configuration.
+	//  After a link is added, the data stream from Merchant Center to Cloud Retail
+	//  will be enabled automatically. The requester must have access to the
+	//  Merchant Center account in order to make changes to this field.
+	// +kcc:proto:field=google.cloud.retail.v2beta.Catalog.merchant_center_linking_config
+	MerchantCenterLinkingConfig *MerchantCenterLinkingConfig `json:"merchantCenterLinkingConfig,omitempty"`
 }
 
-// +kcc:proto=google.cloud.retail.v2.ProductLevelConfig
+// +kcc:proto=google.cloud.retail.v2beta.MerchantCenterFeedFilter
+type MerchantCenterFeedFilter struct {
+	// Merchant Center primary feed ID.
+	// +kcc:proto:field=google.cloud.retail.v2beta.MerchantCenterFeedFilter.primary_feed_id
+	PrimaryFeedID *int64 `json:"primaryFeedID,omitempty"`
+
+	// Merchant Center primary feed name. The name is used for the display
+	//  purposes only.
+	// +kcc:proto:field=google.cloud.retail.v2beta.MerchantCenterFeedFilter.primary_feed_name
+	PrimaryFeedName *string `json:"primaryFeedName,omitempty"`
+}
+
+// +kcc:proto=google.cloud.retail.v2beta.MerchantCenterLink
+type MerchantCenterLink struct {
+	// Required. The linked [Merchant Center account
+	//  ID](https://developers.google.com/shopping-content/guides/accountstatuses).
+	//  The account must be a standalone account or a sub-account of a MCA.
+	// +kcc:proto:field=google.cloud.retail.v2beta.MerchantCenterLink.merchant_center_account_id
+	MerchantCenterAccountID *int64 `json:"merchantCenterAccountID,omitempty"`
+
+	// The branch ID (e.g. 0/1/2) within this catalog that products from
+	//  merchant_center_account_id are streamed to. When updating this field, an
+	//  empty value will use the currently configured default branch. However,
+	//  changing the default branch later on won't change the linked branch here.
+	//
+	//  A single branch ID can only have one linked Merchant Center account ID.
+	// +kcc:proto:field=google.cloud.retail.v2beta.MerchantCenterLink.branch_id
+	BranchID *string `json:"branchID,omitempty"`
+
+	// String representing the destination to import for, all if left empty.
+	//  List of possible values is given in [Included
+	//  destination](https://support.google.com/merchants/answer/7501026).
+	//  List of allowed string values:
+	//  "Shopping_ads", "Buy_on_google_listings", "Display_ads", "Local_inventory
+	//  _ads", "Free_listings", "Free_local_listings"
+	//  NOTE: The string values are case sensitive.
+	// +kcc:proto:field=google.cloud.retail.v2beta.MerchantCenterLink.destinations
+	Destinations []string `json:"destinations,omitempty"`
+
+	// Region code of offers to accept. 2-letter Uppercase ISO 3166-1 alpha-2
+	//  code. List of values can be found
+	//  [here](https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry)
+	//  under the `region` tag. If left blank no region filtering will be
+	//  performed.
+	//
+	//  Example value: `US`.
+	// +kcc:proto:field=google.cloud.retail.v2beta.MerchantCenterLink.region_code
+	RegionCode *string `json:"regionCode,omitempty"`
+
+	// Language of the title/description and other string attributes. Use language
+	//  tags defined by [BCP 47](https://www.rfc-editor.org/rfc/bcp/bcp47.txt).
+	//  ISO 639-1.
+	//
+	//  This specifies the language of offers in Merchant Center that will be
+	//  accepted. If  empty no language filtering will be performed.
+	//
+	//  Example value: `en`.
+	// +kcc:proto:field=google.cloud.retail.v2beta.MerchantCenterLink.language_code
+	LanguageCode *string `json:"languageCode,omitempty"`
+
+	// Criteria for the Merchant Center feeds to be ingested via the link.
+	//  All offers will be ingested if the list is empty.
+	//  Otherwise the offers will be ingested from selected feeds.
+	// +kcc:proto:field=google.cloud.retail.v2beta.MerchantCenterLink.feeds
+	Feeds []MerchantCenterFeedFilter `json:"feeds,omitempty"`
+}
+
+// +kcc:proto=google.cloud.retail.v2beta.MerchantCenterLinkingConfig
+type MerchantCenterLinkingConfig struct {
+	// Links between Merchant Center accounts and branches.
+	// +kcc:proto:field=google.cloud.retail.v2beta.MerchantCenterLinkingConfig.links
+	Links []MerchantCenterLink `json:"links,omitempty"`
+}
+
+// +kcc:proto=google.cloud.retail.v2beta.ProductLevelConfig
 type ProductLevelConfig struct {
-	// The type of [Product][google.cloud.retail.v2.Product]s allowed to be
+	// The type of [Product][google.cloud.retail.v2beta.Product]s allowed to be
 	//  ingested into the catalog. Acceptable values are:
 	//
 	//  * `primary` (default): You can ingest
-	//  [Product][google.cloud.retail.v2.Product]s of all types. When
-	//    ingesting a [Product][google.cloud.retail.v2.Product], its type will
+	//  [Product][google.cloud.retail.v2beta.Product]s of all types. When
+	//    ingesting a [Product][google.cloud.retail.v2beta.Product], its type will
 	//    default to
-	//    [Product.Type.PRIMARY][google.cloud.retail.v2.Product.Type.PRIMARY] if
-	//    unset.
+	//    [Product.Type.PRIMARY][google.cloud.retail.v2beta.Product.Type.PRIMARY]
+	//    if unset.
 	//  * `variant` (incompatible with Retail Search): You can only
 	//    ingest
-	//    [Product.Type.VARIANT][google.cloud.retail.v2.Product.Type.VARIANT]
-	//    [Product][google.cloud.retail.v2.Product]s. This means
-	//    [Product.primary_product_id][google.cloud.retail.v2.Product.primary_product_id]
+	//    [Product.Type.VARIANT][google.cloud.retail.v2beta.Product.Type.VARIANT]
+	//    [Product][google.cloud.retail.v2beta.Product]s. This means
+	//    [Product.primary_product_id][google.cloud.retail.v2beta.Product.primary_product_id]
 	//    cannot be empty.
 	//
 	//  If this field is set to an invalid value other than these, an
 	//  INVALID_ARGUMENT error is returned.
 	//
 	//  If this field is `variant` and
-	//  [merchant_center_product_id_field][google.cloud.retail.v2.ProductLevelConfig.merchant_center_product_id_field]
+	//  [merchant_center_product_id_field][google.cloud.retail.v2beta.ProductLevelConfig.merchant_center_product_id_field]
 	//  is `itemGroupId`, an INVALID_ARGUMENT error is returned.
 	//
 	//  See [Product
 	//  levels](https://cloud.google.com/retail/docs/catalog#product-levels)
 	//  for more details.
-	// +kcc:proto:field=google.cloud.retail.v2.ProductLevelConfig.ingestion_product_type
+	// +kcc:proto:field=google.cloud.retail.v2beta.ProductLevelConfig.ingestion_product_type
 	IngestionProductType *string `json:"ingestionProductType,omitempty"`
 
 	// Which field of [Merchant Center
 	//  Product](/bigquery-transfer/docs/merchant-center-products-schema) should be
-	//  imported as [Product.id][google.cloud.retail.v2.Product.id]. Acceptable
+	//  imported as [Product.id][google.cloud.retail.v2beta.Product.id]. Acceptable
 	//  values are:
 	//
 	//  * `offerId` (default): Import `offerId` as the product ID.
@@ -78,12 +159,12 @@ type ProductLevelConfig struct {
 	//  INVALID_ARGUMENT error is returned.
 	//
 	//  If this field is `itemGroupId` and
-	//  [ingestion_product_type][google.cloud.retail.v2.ProductLevelConfig.ingestion_product_type]
+	//  [ingestion_product_type][google.cloud.retail.v2beta.ProductLevelConfig.ingestion_product_type]
 	//  is `variant`, an INVALID_ARGUMENT error is returned.
 	//
 	//  See [Product
 	//  levels](https://cloud.google.com/retail/docs/catalog#product-levels)
 	//  for more details.
-	// +kcc:proto:field=google.cloud.retail.v2.ProductLevelConfig.merchant_center_product_id_field
+	// +kcc:proto:field=google.cloud.retail.v2beta.ProductLevelConfig.merchant_center_product_id_field
 	MerchantCenterProductIDField *string `json:"merchantCenterProductIDField,omitempty"`
 }
--- pkg/controller/direct/retail/mapper.generated.go
+++ pkg/controller/direct/retail/mapper.generated.go
@@ -15,10 +15,10 @@
 package retail
 
 import (
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/retail/v1alpha1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
-	pb "cloud.google.com/go/retail/apiv2/retailpb"
-	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/retail/v1alpha1"
+	pb "cloud.google.com/go/retail/apiv2beta/retailpb"
 )
 func Catalog_FromProto(mapCtx *direct.MapContext, in *pb.Catalog) *krm.Catalog {
 	if in == nil {
@@ -28,6 +28,7 @@ func Catalog_FromProto(mapCtx *direct.MapContext, in *pb.Catalog) *krm.Catalog {
 	out.Name = direct.LazyPtr(in.GetName())
 	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
 	out.ProductLevelConfig = ProductLevelConfig_FromProto(mapCtx, in.GetProductLevelConfig())
+	out.MerchantCenterLinkingConfig = MerchantCenterLinkingConfig_FromProto(mapCtx, in.GetMerchantCenterLinkingConfig())
 	return out
 }
 func Catalog_ToProto(mapCtx *direct.MapContext, in *krm.Catalog) *pb.Catalog {
@@ -38,6 +39,67 @@ func Catalog_ToProto(mapCtx *direct.MapContext, in *krm.Catalog) *pb.Catalog {
 	out.Name = direct.ValueOf(in.Name)
 	out.DisplayName = direct.ValueOf(in.DisplayName)
 	out.ProductLevelConfig = ProductLevelConfig_ToProto(mapCtx, in.ProductLevelConfig)
+	out.MerchantCenterLinkingConfig = MerchantCenterLinkingConfig_ToProto(mapCtx, in.MerchantCenterLinkingConfig)
+	return out
+}
+func MerchantCenterFeedFilter_FromProto(mapCtx *direct.MapContext, in *pb.MerchantCenterFeedFilter) *krm.MerchantCenterFeedFilter {
+	if in == nil {
+		return nil
+	}
+	out := &krm.MerchantCenterFeedFilter{}
+	out.PrimaryFeedID = direct.LazyPtr(in.GetPrimaryFeedId())
+	out.PrimaryFeedName = direct.LazyPtr(in.GetPrimaryFeedName())
+	return out
+}
+func MerchantCenterFeedFilter_ToProto(mapCtx *direct.MapContext, in *krm.MerchantCenterFeedFilter) *pb.MerchantCenterFeedFilter {
+	if in == nil {
+		return nil
+	}
+	out := &pb.MerchantCenterFeedFilter{}
+	out.PrimaryFeedId = direct.ValueOf(in.PrimaryFeedID)
+	out.PrimaryFeedName = direct.ValueOf(in.PrimaryFeedName)
+	return out
+}
+func MerchantCenterLink_FromProto(mapCtx *direct.MapContext, in *pb.MerchantCenterLink) *krm.MerchantCenterLink {
+	if in == nil {
+		return nil
+	}
+	out := &krm.MerchantCenterLink{}
+	out.MerchantCenterAccountID = direct.LazyPtr(in.GetMerchantCenterAccountId())
+	out.BranchID = direct.LazyPtr(in.GetBranchId())
+	out.Destinations = in.Destinations
+	out.RegionCode = direct.LazyPtr(in.GetRegionCode())
+	out.LanguageCode = direct.LazyPtr(in.GetLanguageCode())
+	out.Feeds = direct.Slice_FromProto(mapCtx, in.Feeds, MerchantCenterFeedFilter_FromProto)
+	return out
+}
+func MerchantCenterLink_ToProto(mapCtx *direct.MapContext, in *krm.MerchantCenterLink) *pb.MerchantCenterLink {
+	if in == nil {
+		return nil
+	}
+	out := &pb.MerchantCenterLink{}
+	out.MerchantCenterAccountId = direct.ValueOf(in.MerchantCenterAccountID)
+	out.BranchId = direct.ValueOf(in.BranchID)
+	out.Destinations = in.Destinations
+	out.RegionCode = direct.ValueOf(in.RegionCode)
+	out.LanguageCode = direct.ValueOf(in.LanguageCode)
+	out.Feeds = direct.Slice_ToProto(mapCtx, in.Feeds, MerchantCenterFeedFilter_ToProto)
+	return out
+}
+func MerchantCenterLinkingConfig_FromProto(mapCtx *direct.MapContext, in *pb.MerchantCenterLinkingConfig) *krm.MerchantCenterLinkingConfig {
+	if in == nil {
+		return nil
+	}
+	out := &krm.MerchantCenterLinkingConfig{}
+	out.Links = direct.Slice_FromProto(mapCtx, in.Links, MerchantCenterLink_FromProto)
+	return out
+}
+func MerchantCenterLinkingConfig_ToProto(mapCtx *direct.MapContext, in *krm.MerchantCenterLinkingConfig) *pb.MerchantCenterLinkingConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.MerchantCenterLinkingConfig{}
+	out.Links = direct.Slice_ToProto(mapCtx, in.Links, MerchantCenterLink_ToProto)
 	return out
 }
 func ProductLevelConfig_FromProto(mapCtx *direct.MapContext, in *pb.ProductLevelConfig) *krm.ProductLevelConfig {
@@ -56,45 +118,5 @@ func ProductLevelConfig_ToProto(mapCtx *direct.MapContext, in *krm.ProductLevelC
 	out := &pb.ProductLevelConfig{}
 	out.IngestionProductType = direct.ValueOf(in.IngestionProductType)
 	out.MerchantCenterProductIdField = direct.ValueOf(in.MerchantCenterProductIDField)
-	return out
-}
-func RetailCatalogObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Catalog) *krm.RetailCatalogObservedState {
-	if in == nil {
-		return nil
-	}
-	out := &krm.RetailCatalogObservedState{}
-	// MISSING: Name
-	// MISSING: DisplayName
-	// MISSING: ProductLevelConfig
-	return out
-}
-func RetailCatalogObservedState_ToProto(mapCtx *direct.MapContext, in *krm.RetailCatalogObservedState) *pb.Catalog {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Catalog{}
-	// MISSING: Name
-	// MISSING: DisplayName
-	// MISSING: ProductLevelConfig
-	return out
-}
-func RetailCatalogSpec_FromProto(mapCtx *direct.MapContext, in *pb.Catalog) *krm.RetailCatalogSpec {
-	if in == nil {
-		return nil
-	}
-	out := &krm.RetailCatalogSpec{}
-	// MISSING: Name
-	// MISSING: DisplayName
-	// MISSING: ProductLevelConfig
-	return out
-}
-func RetailCatalogSpec_ToProto(mapCtx *direct.MapContext, in *krm.RetailCatalogSpec) *pb.Catalog {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Catalog{}
-	// MISSING: Name
-	// MISSING: DisplayName
-	// MISSING: ProductLevelConfig
 	return out
 }
