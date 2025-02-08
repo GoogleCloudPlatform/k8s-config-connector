c401f5c1937080f6ff599d49a0a70c3080a401ad
Your Name <yuwenma@google.com>
2025-02-08 04:19:50 +0000

    feat: add resource DialogflowAgent

--- apis/dialogflow/v1alpha1/types.generated.go
+++ apis/dialogflow/v1alpha1/types.generated.go
@@ -15,15 +15,16 @@
 package v1alpha1
 
 
-// +kcc:proto=google.cloud.dialogflow.v2.Agent
+// +kcc:proto=google.cloud.dialogflow.v2beta1.Agent
 type Agent struct {
 	// Required. The project of this agent.
-	//  Format: `projects/<Project ID>`.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Agent.parent
+	//  Format: `projects/<Project ID>` or
+	//          `projects/<Project ID>/locations/<Location ID>`
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Agent.parent
 	Parent *string `json:"parent,omitempty"`
 
 	// Required. The name of this agent.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Agent.display_name
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Agent.display_name
 	DisplayName *string `json:"displayName,omitempty"`
 
 	// Required. The default language of the agent as a language tag. See
@@ -31,23 +32,23 @@ type Agent struct {
 	//  Support](https://cloud.google.com/dialogflow/docs/reference/language)
 	//  for a list of the currently supported language codes. This field cannot be
 	//  set by the `Update` method.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Agent.default_language_code
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Agent.default_language_code
 	DefaultLanguageCode *string `json:"defaultLanguageCode,omitempty"`
 
 	// Optional. The list of all languages supported by this agent (except for the
 	//  `default_language_code`).
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Agent.supported_language_codes
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Agent.supported_language_codes
 	SupportedLanguageCodes []string `json:"supportedLanguageCodes,omitempty"`
 
 	// Required. The time zone of this agent from the
 	//  [time zone database](https://www.iana.org/time-zones), e.g.,
 	//  America/New_York, Europe/Paris.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Agent.time_zone
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Agent.time_zone
 	TimeZone *string `json:"timeZone,omitempty"`
 
 	// Optional. The description of this agent.
 	//  The maximum length is 500 characters. If exceeded, the request is rejected.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Agent.description
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Agent.description
 	Description *string `json:"description,omitempty"`
 
 	// Optional. The URI of the agent's avatar.
@@ -55,15 +56,15 @@ type Agent struct {
 	//  [Web
 	//  Demo](https://cloud.google.com/dialogflow/docs/integrations/web-demo)
 	//  integration.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Agent.avatar_uri
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Agent.avatar_uri
 	AvatarURI *string `json:"avatarURI,omitempty"`
 
 	// Optional. Determines whether this agent should log conversation queries.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Agent.enable_logging
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Agent.enable_logging
 	EnableLogging *bool `json:"enableLogging,omitempty"`
 
 	// Optional. Determines how intents are detected from user queries.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Agent.match_mode
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Agent.match_mode
 	MatchMode *string `json:"matchMode,omitempty"`
 
 	// Optional. To filter out false positive results and still get variety in
@@ -73,17 +74,17 @@ type Agent struct {
 	//  are no fallback intents defined, no intent will be triggered. The score
 	//  values range from 0.0 (completely uncertain) to 1.0 (completely certain).
 	//  If set to 0.0, the default of 0.3 is used.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Agent.classification_threshold
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Agent.classification_threshold
 	ClassificationThreshold *float32 `json:"classificationThreshold,omitempty"`
 
 	// Optional. API version displayed in Dialogflow console. If not specified,
 	//  V2 API is assumed. Clients are free to query different service endpoints
 	//  for different API versions. However, bots connectors and webhook calls will
 	//  follow the specified API version.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Agent.api_version
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Agent.api_version
 	ApiVersion *string `json:"apiVersion,omitempty"`
 
 	// Optional. The agent tier. If not specified, TIER_STANDARD is assumed.
-	// +kcc:proto:field=google.cloud.dialogflow.v2.Agent.tier
+	// +kcc:proto:field=google.cloud.dialogflow.v2beta1.Agent.tier
 	Tier *string `json:"tier,omitempty"`
 }
--- pkg/controller/direct/dialogflow/mapper.generated.go
+++ pkg/controller/direct/dialogflow/mapper.generated.go
@@ -16,7 +16,7 @@ package dialogflow
 
 import (
 	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
-	pb "cloud.google.com/go/dialogflow/apiv2/dialogflowpb"
+	pb "cloud.google.com/go/dialogflow/apiv2beta1/dialogflowpb"
 	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/dialogflow/v1alpha1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 )
