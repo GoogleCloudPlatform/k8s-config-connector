61801c3021a3aef1a93ce1844c3344e8229d8e17
Your Name <yuwenma@google.com>
2025-02-08 11:20:31 +0000

    feat: add resource SpeechPhraseSet

---
+++ apis/speech/v1alpha1/types.generated.go
@@ -0,0 +1,58 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+
+// +kcc:proto=google.cloud.speech.v1.PhraseSet
+type PhraseSet struct {
+	// The resource name of the phrase set.
+	// +kcc:proto:field=google.cloud.speech.v1.PhraseSet.name
+	Name *string `json:"name,omitempty"`
+
+	// A list of word and phrases.
+	// +kcc:proto:field=google.cloud.speech.v1.PhraseSet.phrases
+	Phrases []PhraseSet_Phrase `json:"phrases,omitempty"`
+
+	// Hint Boost. Positive value will increase the probability that a specific
+	//  phrase will be recognized over other similar sounding phrases. The higher
+	//  the boost, the higher the chance of false positive recognition as well.
+	//  Negative boost values would correspond to anti-biasing. Anti-biasing is not
+	//  enabled, so negative boost will simply be ignored. Though `boost` can
+	//  accept a wide range of positive values, most use cases are best served with
+	//  values between 0 (exclusive) and 20. We recommend using a binary search
+	//  approach to finding the optimal value for your use case as well as adding
+	//  phrases both with and without boost to your requests.
+	// +kcc:proto:field=google.cloud.speech.v1.PhraseSet.boost
+	Boost *float32 `json:"boost,omitempty"`
+}
+
+// +kcc:proto=google.cloud.speech.v1.PhraseSet.Phrase
+type PhraseSet_Phrase struct {
+	// The phrase itself.
+	// +kcc:proto:field=google.cloud.speech.v1.PhraseSet.Phrase.value
+	Value *string `json:"value,omitempty"`
+
+	// Hint Boost. Overrides the boost set at the phrase set level.
+	//  Positive value will increase the probability that a specific phrase will
+	//  be recognized over other similar sounding phrases. The higher the boost,
+	//  the higher the chance of false positive recognition as well. Negative
+	//  boost will simply be ignored. Though `boost` can accept a wide range of
+	//  positive values, most use cases are best served
+	//  with values between 0 and 20. We recommend using a binary search approach
+	//  to finding the optimal value for your use case as well as adding
+	//  phrases both with and without boost to your requests.
+	// +kcc:proto:field=google.cloud.speech.v1.PhraseSet.Phrase.boost
+	Boost *float32 `json:"boost,omitempty"`
+}
---
+++ pkg/controller/direct/speech/mapper.generated.go
@@ -0,0 +1,100 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package speech
+
+import (
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	pb "cloud.google.com/go/speech/apiv1/speechpb"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/speech/v1alpha1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+)
+func PhraseSet_FromProto(mapCtx *direct.MapContext, in *pb.PhraseSet) *krm.PhraseSet {
+	if in == nil {
+		return nil
+	}
+	out := &krm.PhraseSet{}
+	out.Name = direct.LazyPtr(in.GetName())
+	out.Phrases = direct.Slice_FromProto(mapCtx, in.Phrases, PhraseSet_Phrase_FromProto)
+	out.Boost = direct.LazyPtr(in.GetBoost())
+	return out
+}
+func PhraseSet_ToProto(mapCtx *direct.MapContext, in *krm.PhraseSet) *pb.PhraseSet {
+	if in == nil {
+		return nil
+	}
+	out := &pb.PhraseSet{}
+	out.Name = direct.ValueOf(in.Name)
+	out.Phrases = direct.Slice_ToProto(mapCtx, in.Phrases, PhraseSet_Phrase_ToProto)
+	out.Boost = direct.ValueOf(in.Boost)
+	return out
+}
+func PhraseSet_Phrase_FromProto(mapCtx *direct.MapContext, in *pb.PhraseSet_Phrase) *krm.PhraseSet_Phrase {
+	if in == nil {
+		return nil
+	}
+	out := &krm.PhraseSet_Phrase{}
+	out.Value = direct.LazyPtr(in.GetValue())
+	out.Boost = direct.LazyPtr(in.GetBoost())
+	return out
+}
+func PhraseSet_Phrase_ToProto(mapCtx *direct.MapContext, in *krm.PhraseSet_Phrase) *pb.PhraseSet_Phrase {
+	if in == nil {
+		return nil
+	}
+	out := &pb.PhraseSet_Phrase{}
+	out.Value = direct.ValueOf(in.Value)
+	out.Boost = direct.ValueOf(in.Boost)
+	return out
+}
+func SpeechPhraseSetObservedState_FromProto(mapCtx *direct.MapContext, in *pb.PhraseSet) *krm.SpeechPhraseSetObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.SpeechPhraseSetObservedState{}
+	// MISSING: Name
+	// MISSING: Phrases
+	// MISSING: Boost
+	return out
+}
+func SpeechPhraseSetObservedState_ToProto(mapCtx *direct.MapContext, in *krm.SpeechPhraseSetObservedState) *pb.PhraseSet {
+	if in == nil {
+		return nil
+	}
+	out := &pb.PhraseSet{}
+	// MISSING: Name
+	// MISSING: Phrases
+	// MISSING: Boost
+	return out
+}
+func SpeechPhraseSetSpec_FromProto(mapCtx *direct.MapContext, in *pb.PhraseSet) *krm.SpeechPhraseSetSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.SpeechPhraseSetSpec{}
+	// MISSING: Name
+	// MISSING: Phrases
+	// MISSING: Boost
+	return out
+}
+func SpeechPhraseSetSpec_ToProto(mapCtx *direct.MapContext, in *krm.SpeechPhraseSetSpec) *pb.PhraseSet {
+	if in == nil {
+		return nil
+	}
+	out := &pb.PhraseSet{}
+	// MISSING: Name
+	// MISSING: Phrases
+	// MISSING: Boost
+	return out
+}
