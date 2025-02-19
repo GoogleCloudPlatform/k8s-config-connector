86d1f40ce729f9a9ad4d4d9f67f3715040013fcb
Eric Pang <epang@google.com>
2025-02-19 23:17:23 +0000

    Add gvks_generated

--- pkg/gvks/supportedgvks/gvks_generated.go
+++ pkg/gvks/supportedgvks/gvks_generated.go
@@ -4037,7 +4037,28 @@ var SupportedGVKs = map[schema.GroupVersionKind]GVKMetadata{
 	},
 	{
 		Group:   "securesourcemanager.cnrm.cloud.google.com",
+		Version: "v1beta1",
+		Kind:    "SecureSourceManagerInstance",
+	}: {
+		Labels: map[string]string{
+			"cnrm.cloud.google.com/managed-by-kcc":  "true",
+			"cnrm.cloud.google.com/stability-level": "alpha",
+			"cnrm.cloud.google.com/system":          "true",
+		},
+	},
+	{
+		Group:   "securesourcemanager.cnrm.cloud.google.com",
 		Version: "v1alpha1",
+		Kind:    "SecureSourceManagerRepository",
+	}: {
+		Labels: map[string]string{
+			"cnrm.cloud.google.com/managed-by-kcc": "true",
+			"cnrm.cloud.google.com/system":         "true",
+		},
+	},
+	{
+		Group:   "securesourcemanager.cnrm.cloud.google.com",
+		Version: "v1beta1",
 		Kind:    "SecureSourceManagerRepository",
 	}: {
 		Labels: map[string]string{
