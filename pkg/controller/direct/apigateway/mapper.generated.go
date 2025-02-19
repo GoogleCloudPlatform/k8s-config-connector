cbb24d594308dbe31ad04bae920a681301448f55
Your Name <yuwenma@google.com>
2025-02-19 22:00:39 +0000

    add basic APIGatewayAPI reconciler

---
+++ pkg/controller/direct/apigateway/mapper.generated.go
@@ -0,0 +1,134 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package apigateway
+
+import (
+	pb "cloud.google.com/go/apigateway/apiv1/apigatewaypb"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/apigateway/v1alpha1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+)
+
+func APIGatewayAPIObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Api) *krm.APIGatewayAPIObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.APIGatewayAPIObservedState{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
+	// MISSING: DisplayName
+	// MISSING: ManagedService
+	// MISSING: State
+	return out
+}
+func APIGatewayAPIObservedState_ToProto(mapCtx *direct.MapContext, in *krm.APIGatewayAPIObservedState) *pb.Api {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Api{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
+	// MISSING: DisplayName
+	// MISSING: ManagedService
+	// MISSING: State
+	return out
+}
+func APIGatewayAPISpec_FromProto(mapCtx *direct.MapContext, in *pb.Api) *krm.APIGatewayAPISpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.APIGatewayAPISpec{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
+	// MISSING: DisplayName
+	// MISSING: ManagedService
+	// MISSING: State
+	return out
+}
+func APIGatewayAPISpec_ToProto(mapCtx *direct.MapContext, in *krm.APIGatewayAPISpec) *pb.Api {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Api{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
+	// MISSING: DisplayName
+	// MISSING: ManagedService
+	// MISSING: State
+	return out
+}
+func Api_FromProto(mapCtx *direct.MapContext, in *pb.Api) *krm.Api {
+	if in == nil {
+		return nil
+	}
+	out := &krm.Api{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	out.Labels = in.Labels
+	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
+	out.ManagedService = direct.LazyPtr(in.GetManagedService())
+	// MISSING: State
+	return out
+}
+func Api_ToProto(mapCtx *direct.MapContext, in *krm.Api) *pb.Api {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Api{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	out.Labels = in.Labels
+	out.DisplayName = direct.ValueOf(in.DisplayName)
+	out.ManagedService = direct.ValueOf(in.ManagedService)
+	// MISSING: State
+	return out
+}
+func ApiObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Api) *krm.ApiObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ApiObservedState{}
+	out.Name = direct.LazyPtr(in.GetName())
+	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
+	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
+	// MISSING: Labels
+	// MISSING: DisplayName
+	// MISSING: ManagedService
+	out.State = direct.Enum_FromProto(mapCtx, in.GetState())
+	return out
+}
+func ApiObservedState_ToProto(mapCtx *direct.MapContext, in *krm.ApiObservedState) *pb.Api {
+	if in == nil {
+		return nil
+	}
+	out := &pb.Api{}
+	out.Name = direct.ValueOf(in.Name)
+	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
+	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
+	// MISSING: Labels
+	// MISSING: DisplayName
+	// MISSING: ManagedService
+	out.State = direct.Enum_ToProto[pb.Api_State](mapCtx, in.State)
+	return out
+}
