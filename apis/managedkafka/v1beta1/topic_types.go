7ea36884944ca4aa41af16ab3e1b447d5214a761
Jingyi Hu <jingyih@google.com>
2025-02-20 06:07:22 +0000

    feat: add v1beta1 API for ManagedKafkaTopic

--- apis/managedkafka/v1beta1/topic_types.go
+++ apis/managedkafka/v1beta1/topic_types.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package v1alpha1
+package v1beta1
 
 import (
 	commonv1alpha1 "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/apis/common/v1alpha1"
