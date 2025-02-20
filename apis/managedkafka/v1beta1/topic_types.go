5e34647dd2979a96d281fdcf41d027c29423bd95
Jingyi Hu <jingyih@google.com>
2025-02-20 06:10:57 +0000

    feat: set v1beta1 as storage version

--- apis/managedkafka/v1beta1/topic_types.go
+++ apis/managedkafka/v1beta1/topic_types.go
@@ -90,6 +90,7 @@ type ManagedKafkaTopicStatus struct {
 // +kubebuilder:printcolumn:name="Ready",JSONPath=".status.conditions[?(@.type=='Ready')].status",type="string",description="When 'True', the most recent reconcile of the resource succeeded"
 // +kubebuilder:printcolumn:name="Status",JSONPath=".status.conditions[?(@.type=='Ready')].reason",type="string",description="The reason for the value in 'Ready'"
 // +kubebuilder:printcolumn:name="Status Age",JSONPath=".status.conditions[?(@.type=='Ready')].lastTransitionTime",type="date",description="The last transition time for the value in 'Status'"
+// +kubebuilder:storageversion
 
 // ManagedKafkaTopic is the Schema for the ManagedKafkaTopic API
 // +k8s:openapi-gen=true
