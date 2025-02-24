f725bdc7a540661642ed1042545cec8886d5e304
justinsb <justinsb@google.com>
2025-02-24 10:34:31 -0500

    mockgcp: support for billingbudgets budget

--- mockgcp/mock_http_roundtrip.go
+++ mockgcp/mock_http_roundtrip.go
@@ -45,6 +45,7 @@ import (
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/mockbigqueryreservation"
 	_ "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/mockbigtable"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/mockbilling"
+	_ "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/mockbillingbudgets"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/mockcertificatemanager"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/mockcloudbuild"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/mockclouddeploy"
