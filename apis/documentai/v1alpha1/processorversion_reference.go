76623a4f8d2ff49183d512f24ed29b8a7667cc58
Gemma Hou <yuhou@google.com>
2025-02-25 18:01:18 +0000

    documentai: update types, mappers and CRDs(autogen+manual)
    # Conflicts:
    #	apis/documentai/v1alpha1/processor_identity.go
    #	apis/documentai/v1alpha1/processor_reference.go
    #	apis/documentai/v1alpha1/processor_types.go
    #	apis/documentai/v1alpha1/processorversion_types.go
    #	apis/documentai/v1alpha1/types.generated.go
    #	apis/documentai/v1alpha1/zz_generated.deepcopy.go
    #	config/crds/resources/apiextensions.k8s.io_v1_customresourcedefinition_documentaiprocessors.documentai.cnrm.cloud.google.com.yaml
    #	pkg/controller/direct/documentai/mapper.generated.go
    #	pkg/gvks/supportedgvks/gvks_generated.go

--- apis/documentai/v1alpha1/processorversion_reference.go
+++ apis/documentai/v1alpha1/processorversion_reference.go
@@ -47,7 +47,7 @@ type ProcessorVersionRef struct {
 // Otherwise, the "Name" and "Namespace" will be used to query the actual DocumentAI object from the cluster.
 func (r *ProcessorVersionRef) NormalizedExternal(ctx context.Context, reader client.Reader, otherNamespace string) (string, error) {
 	if r.External != "" && r.Name != "" {
-		return "", fmt.Errorf("cannot specify both name and external on %s reference", DocumentAIGVK.Kind)
+		return "", fmt.Errorf("cannot specify both name and external on %s reference", DocumentAIProcessorVersionGVK.Kind)
 	}
 	// From given External
 	if r.External != "" {
@@ -63,12 +63,12 @@ func (r *ProcessorVersionRef) NormalizedExternal(ctx context.Context, reader cli
 	}
 	key := types.NamespacedName{Name: r.Name, Namespace: r.Namespace}
 	u := &unstructured.Unstructured{}
-	u.SetGroupVersionKind(DocumentAIGVK)
+	u.SetGroupVersionKind(DocumentAIProcessorVersionGVK)
 	if err := reader.Get(ctx, key, u); err != nil {
 		if apierrors.IsNotFound(err) {
 			return "", k8s.NewReferenceNotFoundError(u.GroupVersionKind(), key)
 		}
-		return "", fmt.Errorf("reading referenced %s %s: %w", DocumentAIGVK, key, err)
+		return "", fmt.Errorf("reading referenced %s %s: %w", DocumentAIProcessorVersionGVK, key, err)
 	}
 	// Get external from status.externalRef. This is the most trustworthy place.
 	actualExternalRef, _, err := unstructured.NestedString(u.Object, "status", "externalRef")
