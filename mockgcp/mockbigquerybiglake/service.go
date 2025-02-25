63b84e7726c37a661c4e5d8758212f08b075b056
Alex Pana <8968914+acpana@users.noreply.github.com>
2025-02-25 21:11:29 +0000

    mockgcp: user edits
    Signed-off-by: Alex Pana <8968914+acpana@users.noreply.github.com>

--- mockgcp/mockbigquerybiglake/service.go
+++ mockgcp/mockbigquerybiglake/service.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -59,5 +59,3 @@ func (s *MockService) NewHTTPMux(ctx context.Context, conn *grpc.ClientConn) (ht
 	mux := http.NewServeMux()
 	return mux, nil
 }
-
-
