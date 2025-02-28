1f045f079ad59249dc05df0e8a46f3e48f697bd7
Jingyi Hu <jingyih@google.com>
2025-02-28 07:49:31 +0000

    feat: fix mappers for VertexAIFeaturestore

--- pkg/controller/direct/vertexai/mapper.generated.go
+++ pkg/controller/direct/vertexai/mapper.generated.go
@@ -20,94 +20,6 @@ import (
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 )
 
-func EncryptionSpec_FromProto(mapCtx *direct.MapContext, in *pb.EncryptionSpec) *krm.EncryptionSpec {
-	if in == nil {
-		return nil
-	}
-	out := &krm.EncryptionSpec{}
-	out.KMSKeyName = direct.LazyPtr(in.GetKmsKeyName())
-	return out
-}
-func EncryptionSpec_ToProto(mapCtx *direct.MapContext, in *krm.EncryptionSpec) *pb.EncryptionSpec {
-	if in == nil {
-		return nil
-	}
-	out := &pb.EncryptionSpec{}
-	out.KmsKeyName = direct.ValueOf(in.KMSKeyName)
-	return out
-}
-func Featurestore_FromProto(mapCtx *direct.MapContext, in *pb.Featurestore) *krm.Featurestore {
-	if in == nil {
-		return nil
-	}
-	out := &krm.Featurestore{}
-	// MISSING: Name
-	// MISSING: CreateTime
-	// MISSING: UpdateTime
-	out.Etag = direct.LazyPtr(in.GetEtag())
-	out.Labels = in.Labels
-	out.OnlineServingConfig = Featurestore_OnlineServingConfig_FromProto(mapCtx, in.GetOnlineServingConfig())
-	// MISSING: State
-	out.OnlineStorageTtlDays = direct.LazyPtr(in.GetOnlineStorageTtlDays())
-	out.EncryptionSpec = EncryptionSpec_FromProto(mapCtx, in.GetEncryptionSpec())
-	// MISSING: SatisfiesPzs
-	// MISSING: SatisfiesPzi
-	return out
-}
-func Featurestore_ToProto(mapCtx *direct.MapContext, in *krm.Featurestore) *pb.Featurestore {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Featurestore{}
-	// MISSING: Name
-	// MISSING: CreateTime
-	// MISSING: UpdateTime
-	out.Etag = direct.ValueOf(in.Etag)
-	out.Labels = in.Labels
-	out.OnlineServingConfig = Featurestore_OnlineServingConfig_ToProto(mapCtx, in.OnlineServingConfig)
-	// MISSING: State
-	out.OnlineStorageTtlDays = direct.ValueOf(in.OnlineStorageTtlDays)
-	out.EncryptionSpec = EncryptionSpec_ToProto(mapCtx, in.EncryptionSpec)
-	// MISSING: SatisfiesPzs
-	// MISSING: SatisfiesPzi
-	return out
-}
-func FeaturestoreObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Featurestore) *krm.FeaturestoreObservedState {
-	if in == nil {
-		return nil
-	}
-	out := &krm.FeaturestoreObservedState{}
-	out.Name = direct.LazyPtr(in.GetName())
-	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
-	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
-	// MISSING: Etag
-	// MISSING: Labels
-	// MISSING: OnlineServingConfig
-	out.State = direct.Enum_FromProto(mapCtx, in.GetState())
-	// MISSING: OnlineStorageTtlDays
-	// MISSING: EncryptionSpec
-	out.SatisfiesPzs = direct.LazyPtr(in.GetSatisfiesPzs())
-	out.SatisfiesPzi = direct.LazyPtr(in.GetSatisfiesPzi())
-	return out
-}
-func FeaturestoreObservedState_ToProto(mapCtx *direct.MapContext, in *krm.FeaturestoreObservedState) *pb.Featurestore {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Featurestore{}
-	out.Name = direct.ValueOf(in.Name)
-	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
-	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
-	// MISSING: Etag
-	// MISSING: Labels
-	// MISSING: OnlineServingConfig
-	out.State = direct.Enum_ToProto[pb.Featurestore_State](mapCtx, in.State)
-	// MISSING: OnlineStorageTtlDays
-	// MISSING: EncryptionSpec
-	out.SatisfiesPzs = direct.ValueOf(in.SatisfiesPzs)
-	out.SatisfiesPzi = direct.ValueOf(in.SatisfiesPzi)
-	return out
-}
 func Featurestore_OnlineServingConfig_FromProto(mapCtx *direct.MapContext, in *pb.Featurestore_OnlineServingConfig) *krm.Featurestore_OnlineServingConfig {
 	if in == nil {
 		return nil
@@ -152,14 +64,9 @@ func VertexAIFeaturestoreObservedState_FromProto(mapCtx *direct.MapContext, in *
 	}
 	out := &krm.VertexAIFeaturestoreObservedState{}
 	// MISSING: Name
-	// MISSING: CreateTime
-	// MISSING: UpdateTime
-	// MISSING: Etag
-	// MISSING: Labels
-	// MISSING: OnlineServingConfig
-	// MISSING: State
-	// MISSING: OnlineStorageTtlDays
-	// MISSING: EncryptionSpec
+	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
+	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
+	out.State = direct.Enum_FromProto(mapCtx, in.GetState())
 	// MISSING: SatisfiesPzs
 	// MISSING: SatisfiesPzi
 	return out
@@ -170,14 +77,9 @@ func VertexAIFeaturestoreObservedState_ToProto(mapCtx *direct.MapContext, in *kr
 	}
 	out := &pb.Featurestore{}
 	// MISSING: Name
-	// MISSING: CreateTime
-	// MISSING: UpdateTime
-	// MISSING: Etag
-	// MISSING: Labels
-	// MISSING: OnlineServingConfig
-	// MISSING: State
-	// MISSING: OnlineStorageTtlDays
-	// MISSING: EncryptionSpec
+	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
+	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
+	out.State = direct.Enum_ToProto[pb.Featurestore_State](mapCtx, in.State)
 	// MISSING: SatisfiesPzs
 	// MISSING: SatisfiesPzi
 	return out
@@ -188,14 +90,11 @@ func VertexAIFeaturestoreSpec_FromProto(mapCtx *direct.MapContext, in *pb.Featur
 	}
 	out := &krm.VertexAIFeaturestoreSpec{}
 	// MISSING: Name
-	// MISSING: CreateTime
-	// MISSING: UpdateTime
-	// MISSING: Etag
-	// MISSING: Labels
-	// MISSING: OnlineServingConfig
-	// MISSING: State
-	// MISSING: OnlineStorageTtlDays
-	// MISSING: EncryptionSpec
+	out.Etag = direct.LazyPtr(in.GetEtag())
+	out.Labels = in.Labels
+	out.OnlineServingConfig = Featurestore_OnlineServingConfig_FromProto(mapCtx, in.GetOnlineServingConfig())
+	out.OnlineStorageTtlDays = direct.LazyPtr(in.GetOnlineStorageTtlDays())
+	out.EncryptionSpec = EncryptionSpec_FromProto(mapCtx, in.GetEncryptionSpec())
 	// MISSING: SatisfiesPzs
 	// MISSING: SatisfiesPzi
 	return out
@@ -206,14 +105,11 @@ func VertexAIFeaturestoreSpec_ToProto(mapCtx *direct.MapContext, in *krm.VertexA
 	}
 	out := &pb.Featurestore{}
 	// MISSING: Name
-	// MISSING: CreateTime
-	// MISSING: UpdateTime
-	// MISSING: Etag
-	// MISSING: Labels
-	// MISSING: OnlineServingConfig
-	// MISSING: State
-	// MISSING: OnlineStorageTtlDays
-	// MISSING: EncryptionSpec
+	out.Etag = direct.ValueOf(in.Etag)
+	out.Labels = in.Labels
+	out.OnlineServingConfig = Featurestore_OnlineServingConfig_ToProto(mapCtx, in.OnlineServingConfig)
+	out.OnlineStorageTtlDays = direct.ValueOf(in.OnlineStorageTtlDays)
+	out.EncryptionSpec = EncryptionSpec_ToProto(mapCtx, in.EncryptionSpec)
 	// MISSING: SatisfiesPzs
 	// MISSING: SatisfiesPzi
 	return out
