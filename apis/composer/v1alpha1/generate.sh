5cec62580d46967c0c3775901aefd1814cf1b183
Joyce Ma <maqiuyu@google.com>
2025-02-28 07:39:15 +0000

    autogen: generated scripts for ComposerEnvironment

---
+++ apis/composer/v1alpha1/generate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+
+REPO_ROOT="/usr/local/google/home/maqiuyu/go/src/github.com/maqiuyujoyce/3-k8s-config-connector"
+cd /usr/local/google/home/maqiuyu/go/src/github.com/maqiuyujoyce/3-k8s-config-connector/dev/tools/controllerbuilder
+
+go run . generate-types     --service google.cloud.orchestration.airflow.service.v1     --api-version composer.cnrm.cloud.google.com/v1alpha1     --resource ComposerEnvironment:Environment
+
+go run . generate-mapper     --service google.cloud.orchestration.airflow.service.v1     --api-version composer.cnrm.cloud.google.com/v1alpha1
