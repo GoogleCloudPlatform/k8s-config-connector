4157e09370f2cf9bcb075365cfb903b24c5de31c
Jingyi Hu <jingyih@google.com>
2025-02-28 07:46:37 +0000

    feat: fix types for VertexAIFeaturestore

--- apis/vertexai/v1alpha1/featurestore_types.go
+++ apis/vertexai/v1alpha1/featurestore_types.go
@@ -15,17 +15,79 @@
 package v1alpha1
 
 import (
+	refsv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/apis/k8s/v1alpha1"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var VertexAIFeaturestoreGVK = GroupVersion.WithKind("VertexAIFeaturestore")
 
+// +kcc:proto=google.cloud.aiplatform.v1beta1.EncryptionSpec
+type EncryptionSpec struct {
+	// Required. The Cloud KMS resource identifier of the customer managed
+	//  encryption key used to protect a resource.
+	//  The key needs to be in the same region as where the compute resource is
+	//  created.
+	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.EncryptionSpec.kms_key_name
+	// +required
+	KMSKeyRef *refsv1beta1.KMSCryptoKeyRef `json:"kmsKeyRef,omitempty"`
+}
+
 // VertexAIFeaturestoreSpec defines the desired state of VertexAIFeaturestore
 // +kcc:proto=google.cloud.aiplatform.v1beta1.Featurestore
 type VertexAIFeaturestoreSpec struct {
+	// The Project that this resource belongs to.
+	// +required
+	ProjectRef *refsv1beta1.ProjectRef `json:"projectRef"`
+
+	// The location for the resource.
+	// +required
+	Location string `json:"location"`
+
 	// The VertexAIFeaturestore name. If not given, the metadata.name will be used.
 	ResourceID *string `json:"resourceID,omitempty"`
+
+	// Optional. Used to perform consistent read-modify-write updates. If not set,
+	//  a blind "overwrite" update happens.
+	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.etag
+	Etag *string `json:"etag,omitempty"`
+
+	// Optional. The labels with user-defined metadata to organize your
+	//  Featurestore.
+	//
+	//  Label keys and values can be no longer than 64 characters
+	//  (Unicode codepoints), can only contain lowercase letters, numeric
+	//  characters, underscores and dashes. International characters are allowed.
+	//
+	//  See https://goo.gl/xmQnxf for more information on and examples of labels.
+	//  No more than 64 user labels can be associated with one Featurestore(System
+	//  labels are excluded)."
+	//  System reserved label keys are prefixed with "aiplatform.googleapis.com/"
+	//  and are immutable.
+	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.labels
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// Optional. Config for online storage resources. The field should not
+	//  co-exist with the field of `OnlineStoreReplicationConfig`. If both of it
+	//  and OnlineStoreReplicationConfig are unset, the feature store will not have
+	//  an online store and cannot be used for online serving.
+	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.online_serving_config
+	OnlineServingConfig *Featurestore_OnlineServingConfig `json:"onlineServingConfig,omitempty"`
+
+	// Optional. TTL in days for feature values that will be stored in online
+	//  serving storage. The Feature Store online storage periodically removes
+	//  obsolete feature values older than `online_storage_ttl_days` since the
+	//  feature generation time. Note that `online_storage_ttl_days` should be less
+	//  than or equal to `offline_storage_ttl_days` for each EntityType under a
+	//  featurestore. If not set, default to 4000 days
+	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.online_storage_ttl_days
+	OnlineStorageTtlDays *int32 `json:"onlineStorageTtlDays,omitempty"`
+
+	// Optional. Customer-managed encryption key spec for data storage. If set,
+	//  both of the online and offline data storage will be secured by this key.
+	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.encryption_spec
+	EncryptionSpec *EncryptionSpec `json:"encryptionSpec,omitempty"`
 }
 
 // VertexAIFeaturestoreStatus defines the config connector machine state of VertexAIFeaturestore
@@ -47,11 +109,37 @@ type VertexAIFeaturestoreStatus struct {
 // VertexAIFeaturestoreObservedState is the state of the VertexAIFeaturestore resource as most recently observed in GCP.
 // +kcc:proto=google.cloud.aiplatform.v1beta1.Featurestore
 type VertexAIFeaturestoreObservedState struct {
+	// Output only. Name of the Featurestore. Format:
+	//  `projects/{project}/locations/{location}/featurestores/{featurestore}`
+	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.name
+	// NOTYET
+	// Name *string `json:"name,omitempty"`
+
+	// Output only. Timestamp when this Featurestore was created.
+	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.create_time
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// Output only. Timestamp when this Featurestore was last updated.
+	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
+
+	// Output only. State of the featurestore.
+	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.state
+	State *string `json:"state,omitempty"`
+
+	// Output only. Reserved for future use.
+	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.satisfies_pzs
+	// NOTYET
+	// SatisfiesPzs *bool `json:"satisfiesPzs,omitempty"`
+
+	// Output only. Reserved for future use.
+	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.satisfies_pzi
+	// NOTYET
+	// SatisfiesPzi *bool `json:"satisfiesPzi,omitempty"`
 }
 
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-// TODO(user): make sure the pluralizaiton below is correct
 // +kubebuilder:resource:categories=gcp,shortName=gcpvertexaifeaturestore;gcpvertexaifeaturestores
 // +kubebuilder:subresource:status
 // +kubebuilder:metadata:labels="cnrm.cloud.google.com/managed-by-kcc=true";"cnrm.cloud.google.com/system=true"
@@ -82,6 +170,7 @@ type VertexAIFeaturestoreList struct {
 func init() {
 	SchemeBuilder.Register(&VertexAIFeaturestore{}, &VertexAIFeaturestoreList{})
 }
+
 type Featurestore struct {
 	// Output only. Timestamp when this Featurestore was created.
 	CreateTime *string `json:"createTime,omitempty"`
@@ -105,6 +194,3 @@ type Featurestore struct {
 	// Output only. Timestamp when this Featurestore was last updated.
 	UpdateTime *string `json:"updateTime,omitempty"`
 }
-
-
-
--- apis/vertexai/v1alpha1/types.generated.go
+++ apis/vertexai/v1alpha1/types.generated.go
@@ -14,62 +14,6 @@
 
 package v1alpha1
 
-// +kcc:proto=google.cloud.aiplatform.v1beta1.EncryptionSpec
-type EncryptionSpec struct {
-	// Required. The Cloud KMS resource identifier of the customer managed
-	//  encryption key used to protect a resource. Has the form:
-	//  `projects/my-project/locations/my-region/keyRings/my-kr/cryptoKeys/my-key`.
-	//  The key needs to be in the same region as where the compute resource is
-	//  created.
-	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.EncryptionSpec.kms_key_name
-	KMSKeyName *string `json:"kmsKeyName,omitempty"`
-}
-
-// +kcc:proto=google.cloud.aiplatform.v1beta1.Featurestore
-type Featurestore struct {
-
-	// Optional. Used to perform consistent read-modify-write updates. If not set,
-	//  a blind "overwrite" update happens.
-	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.etag
-	Etag *string `json:"etag,omitempty"`
-
-	// Optional. The labels with user-defined metadata to organize your
-	//  Featurestore.
-	//
-	//  Label keys and values can be no longer than 64 characters
-	//  (Unicode codepoints), can only contain lowercase letters, numeric
-	//  characters, underscores and dashes. International characters are allowed.
-	//
-	//  See https://goo.gl/xmQnxf for more information on and examples of labels.
-	//  No more than 64 user labels can be associated with one Featurestore(System
-	//  labels are excluded)."
-	//  System reserved label keys are prefixed with "aiplatform.googleapis.com/"
-	//  and are immutable.
-	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.labels
-	Labels map[string]string `json:"labels,omitempty"`
-
-	// Optional. Config for online storage resources. The field should not
-	//  co-exist with the field of `OnlineStoreReplicationConfig`. If both of it
-	//  and OnlineStoreReplicationConfig are unset, the feature store will not have
-	//  an online store and cannot be used for online serving.
-	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.online_serving_config
-	OnlineServingConfig *Featurestore_OnlineServingConfig `json:"onlineServingConfig,omitempty"`
-
-	// Optional. TTL in days for feature values that will be stored in online
-	//  serving storage. The Feature Store online storage periodically removes
-	//  obsolete feature values older than `online_storage_ttl_days` since the
-	//  feature generation time. Note that `online_storage_ttl_days` should be less
-	//  than or equal to `offline_storage_ttl_days` for each EntityType under a
-	//  featurestore. If not set, default to 4000 days
-	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.online_storage_ttl_days
-	OnlineStorageTtlDays *int32 `json:"onlineStorageTtlDays,omitempty"`
-
-	// Optional. Customer-managed encryption key spec for data storage. If set,
-	//  both of the online and offline data storage will be secured by this key.
-	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.encryption_spec
-	EncryptionSpec *EncryptionSpec `json:"encryptionSpec,omitempty"`
-}
-
 // +kcc:proto=google.cloud.aiplatform.v1beta1.Featurestore.OnlineServingConfig
 type Featurestore_OnlineServingConfig struct {
 	// The number of nodes for the online store. The number of nodes doesn't
@@ -107,32 +51,4 @@ type Featurestore_OnlineServingConfig_Scaling struct {
 	//  or set to 0, default to 50.
 	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.OnlineServingConfig.Scaling.cpu_utilization_target
 	CPUUtilizationTarget *int32 `json:"cpuUtilizationTarget,omitempty"`
-}
-
-// +kcc:proto=google.cloud.aiplatform.v1beta1.Featurestore
-type FeaturestoreObservedState struct {
-	// Output only. Name of the Featurestore. Format:
-	//  `projects/{project}/locations/{location}/featurestores/{featurestore}`
-	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.name
-	Name *string `json:"name,omitempty"`
-
-	// Output only. Timestamp when this Featurestore was created.
-	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.create_time
-	CreateTime *string `json:"createTime,omitempty"`
-
-	// Output only. Timestamp when this Featurestore was last updated.
-	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.update_time
-	UpdateTime *string `json:"updateTime,omitempty"`
-
-	// Output only. State of the featurestore.
-	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.state
-	State *string `json:"state,omitempty"`
-
-	// Output only. Reserved for future use.
-	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.satisfies_pzs
-	SatisfiesPzs *bool `json:"satisfiesPzs,omitempty"`
-
-	// Output only. Reserved for future use.
-	// +kcc:proto:field=google.cloud.aiplatform.v1beta1.Featurestore.satisfies_pzi
-	SatisfiesPzi *bool `json:"satisfiesPzi,omitempty"`
 }
