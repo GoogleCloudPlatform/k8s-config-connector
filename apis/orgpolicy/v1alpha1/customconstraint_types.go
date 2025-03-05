9543542dc5830a8f2464792e26fa6cfe6d563ea8
Jiacong (JC) Xu <jiacong@google.com>
2025-03-05 03:00:49 +0000

    Refactor OrgPolicy integration and cleanup generated code
    - Update generated code for Custom OrgPolicy (organization-only)
    - Clean up unnecessary proto changes
    - Standardize spelling from Orgpolicy → OrgPolicy across CRDs and gvks_generated.go
    
    manually update generated code to support Custom OrgPolicy with parent=organization only
    
    remove the previous unnecessary changes(added OrgPolicy proto path) in generate-proto.sh
    
    update Orgpolicy to OrgPolicy
    
    change Orgpolicy to OrgPolicy for config/crds/resources/apiextensions.k8s.io_v1_customresourcedefinition_orgpolicycustomconstraints.orgpolicy.cnrm.cloud.google.com.yaml
    
    update pkg/gvks/supportedgvks/gvks_generated.go's OrgPolicy name
    
    add required fields to customconstraint_types.go
    
    add the changed g.mod and go.sum
    
    update deepcopy.go and yaml base on the updated OrgPolicy name
    
    remove name field from customconstraint_type.go
    
    remove name field from customconstraint_type.go
    
    remove the unneeded (yet) register.go

--- apis/orgpolicy/v1alpha1/customconstraint_types.go
+++ apis/orgpolicy/v1alpha1/customconstraint_types.go
@@ -15,21 +15,60 @@
 package v1alpha1
 
 import (
+	refsv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/apis/k8s/v1alpha1"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-var OrgpolicyCustomConstraintGVK = GroupVersion.WithKind("OrgpolicyCustomConstraint")
+var OrgPolicyCustomConstraintGVK = GroupVersion.WithKind("OrgPolicyCustomConstraint")
 
-// OrgpolicyCustomConstraintSpec defines the desired state of OrgpolicyCustomConstraint
+// OrgPolicyCustomConstraintSpec defines the desired state of OrgPolicyCustomConstraint
 // +kcc:proto=google.cloud.orgpolicy.v2.CustomConstraint
-type OrgpolicyCustomConstraintSpec struct {
-	// The OrgpolicyCustomConstraint name. If not given, the metadata.name will be used.
+type OrgPolicyCustomConstraintSpec struct {
+	// The Organization that this resource belongs to.
+	// +required
+	OrganizationRef *refsv1beta1.OrganizationRef `json:"organizationRef"`
+
+	// The OrgPolicyCustomConstraint name. If not given, the metadata.name will be used.
 	ResourceID *string `json:"resourceID,omitempty"`
+
+	// Immutable. The resource instance type on which this policy applies. Format
+	//  will be of the form : `<canonical service name>/<type>` Example:
+	//
+	//   * `compute.googleapis.com/Instance`.
+	// +kcc:proto:field=google.cloud.orgpolicy.v2.CustomConstraint.resource_types
+	ResourceTypes []string `json:"resourceTypes,omitempty"`
+
+	// All the operations being applied for this constraint.
+	// +kcc:proto:field=google.cloud.orgpolicy.v2.CustomConstraint.method_types
+	MethodTypes []string `json:"methodTypes,omitempty"`
+
+	// Org policy condition/expression. For example:
+	//  `resource.instanceName.matches("[production|test]_.*_(\d)+")` or,
+	//  `resource.management.auto_upgrade == true`
+	//
+	//  The max length of the condition is 1000 characters.
+	// +kcc:proto:field=google.cloud.orgpolicy.v2.CustomConstraint.condition
+	Condition *string `json:"condition,omitempty"`
+
+	// Allow or deny type.
+	// +kcc:proto:field=google.cloud.orgpolicy.v2.CustomConstraint.action_type
+	ActionType *string `json:"actionType,omitempty"`
+
+	// One line display name for the UI.
+	//  The max length of the display_name is 200 characters.
+	// +kcc:proto:field=google.cloud.orgpolicy.v2.CustomConstraint.display_name
+	DisplayName *string `json:"displayName,omitempty"`
+
+	// Detailed information about this custom policy constraint.
+	//  The max length of the description is 2000 characters.
+	// +kcc:proto:field=google.cloud.orgpolicy.v2.CustomConstraint.description
+	Description *string `json:"description,omitempty"`
 }
 
-// OrgpolicyCustomConstraintStatus defines the config connector machine state of OrgpolicyCustomConstraint
-type OrgpolicyCustomConstraintStatus struct {
+// OrgPolicyCustomConstraintStatus defines the config connector machine state of OrgPolicyCustomConstraint
+type OrgPolicyCustomConstraintStatus struct {
 	/* Conditions represent the latest available observations of the
 	   object's current state. */
 	Conditions []v1alpha1.Condition `json:"conditions,omitempty"`
@@ -37,16 +76,21 @@ type OrgpolicyCustomConstraintStatus struct {
 	// ObservedGeneration is the generation of the resource that was most recently observed by the Config Connector controller. If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource.
 	ObservedGeneration *int64 `json:"observedGeneration,omitempty"`
 
-	// A unique specifier for the OrgpolicyCustomConstraint resource in GCP.
+	// A unique specifier for the OrgPolicyCustomConstraint resource in GCP.
 	ExternalRef *string `json:"externalRef,omitempty"`
 
 	// ObservedState is the state of the resource as most recently observed in GCP.
-	ObservedState *OrgpolicyCustomConstraintObservedState `json:"observedState,omitempty"`
+	ObservedState *OrgPolicyCustomConstraintObservedState `json:"observedState,omitempty"`
 }
 
-// OrgpolicyCustomConstraintObservedState is the state of the OrgpolicyCustomConstraint resource as most recently observed in GCP.
+// OrgPolicyCustomConstraintObservedState is the state of the OrgPolicyCustomConstraint resource as most recently observed in GCP.
 // +kcc:proto=google.cloud.orgpolicy.v2.CustomConstraint
-type OrgpolicyCustomConstraintObservedState struct {
+type OrgPolicyCustomConstraintObservedState struct {
+	// Output only. The last time this custom constraint was updated. This
+	//  represents the last time that the `CreateCustomConstraint` or
+	//  `UpdateCustomConstraint` RPC was called
+	// +kcc:proto:field=google.cloud.orgpolicy.v2.CustomConstraint.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
 }
 
 // +genclient
@@ -60,25 +104,25 @@ type OrgpolicyCustomConstraintObservedState struct {
 // +kubebuilder:printcolumn:name="Status",JSONPath=".status.conditions[?(@.type=='Ready')].reason",type="string",description="The reason for the value in 'Ready'"
 // +kubebuilder:printcolumn:name="Status Age",JSONPath=".status.conditions[?(@.type=='Ready')].lastTransitionTime",type="date",description="The last transition time for the value in 'Status'"
 
-// OrgpolicyCustomConstraint is the Schema for the OrgpolicyCustomConstraint API
+// OrgPolicyCustomConstraint is the Schema for the OrgPolicyCustomConstraint API
 // +k8s:openapi-gen=true
-type OrgpolicyCustomConstraint struct {
+type OrgPolicyCustomConstraint struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
 	// +required
-	Spec   OrgpolicyCustomConstraintSpec   `json:"spec,omitempty"`
-	Status OrgpolicyCustomConstraintStatus `json:"status,omitempty"`
+	Spec   OrgPolicyCustomConstraintSpec   `json:"spec,omitempty"`
+	Status OrgPolicyCustomConstraintStatus `json:"status,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-// OrgpolicyCustomConstraintList contains a list of OrgpolicyCustomConstraint
-type OrgpolicyCustomConstraintList struct {
+// OrgPolicyCustomConstraintList contains a list of OrgPolicyCustomConstraint
+type OrgPolicyCustomConstraintList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []OrgpolicyCustomConstraint `json:"items"`
+	Items           []OrgPolicyCustomConstraint `json:"items"`
 }
 
 func init() {
-	SchemeBuilder.Register(&OrgpolicyCustomConstraint{}, &OrgpolicyCustomConstraintList{})
+	SchemeBuilder.Register(&OrgPolicyCustomConstraint{}, &OrgPolicyCustomConstraintList{})
 }
