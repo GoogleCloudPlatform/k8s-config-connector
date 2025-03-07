a9121d7aa151341caf0d1fff7788daf278c6e0f1
justinsb <justinsb@google.com>
2025-03-07 09:11:29 -0500

    chore: use generated gcp clients for tpu.v2

--- pkg/controller/direct/tpu/node_fuzzer.go
+++ pkg/controller/direct/tpu/node_fuzzer.go
@@ -19,8 +19,8 @@
 package tpu
 
 import (
+	pb "cloud.google.com/go/tpu/apiv2/tpupb"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/fuzztesting"
-	pb "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/gcpclients/generated/google/cloud/tpu/v2"
 )
 
 func init() {
