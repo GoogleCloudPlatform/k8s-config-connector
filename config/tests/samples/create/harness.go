5cb2606101a33b63b4dd0d77d3ba8bb31925ed2e
Jiacong (JC) Xu <jiacong@google.com>
2025-03-10 21:31:34 +0000

    add mockGCP for OrgPolicy by following k8s-config-connector/mockgcp/README.md

--- config/tests/samples/create/harness.go
+++ config/tests/samples/create/harness.go
@@ -938,6 +938,8 @@ func MaybeSkip(t *testing.T, name string, resources []*unstructured.Unstructured
 			case schema.GroupKind{Group: "iam.cnrm.cloud.google.com", Kind: "IAMPolicyMember"}:
 			case schema.GroupKind{Group: "iam.cnrm.cloud.google.com", Kind: "IAMServiceAccount"}:
 
+			case schema.GroupKind{Group: "orgpolicy.cnrm.cloud.google.com", Kind: "CustomConstraint"}:
+
 			case schema.GroupKind{Group: "edgecontainer.cnrm.cloud.google.com", Kind: "EdgeContainerCluster"}:
 			case schema.GroupKind{Group: "edgecontainer.cnrm.cloud.google.com", Kind: "EdgeContainerNodePool"}:
 
