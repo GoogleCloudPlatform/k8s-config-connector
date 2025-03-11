2bc62724341d8f587a7b04d66cec151b490ecae0
xiaoweim <xiaoweim@google.com>
2025-03-11 23:23:01 +0000

    conductor: "Generated types and mapper for EdgeContainerVpnConnection"

--- apis/edgecontainer/v1alpha1/types.generated.go
+++ apis/edgecontainer/v1alpha1/types.generated.go
@@ -13,3 +13,107 @@
 // limitations under the License.
 
 package v1alpha1
+
+// +kcc:proto=google.cloud.edgecontainer.v1.VpnConnection
+type VpnConnection struct {
+	// Required. The resource name of VPN connection
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.name
+	Name *string `json:"name,omitempty"`
+
+	// Labels associated with this resource.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.labels
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// NAT gateway IP, or WAN IP address. If a customer has multiple NAT IPs, the
+	//  customer needs to configure NAT such that only one external IP maps to the
+	//  GMEC Anthos cluster. This is empty if NAT is not used.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.nat_gateway_ip
+	NATGatewayIP *string `json:"natGatewayIP,omitempty"`
+
+	// Dynamic routing mode of the VPC network, `regional` or `global`.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.bgp_routing_mode
+	BGPRoutingMode *string `json:"bgpRoutingMode,omitempty"`
+
+	// The canonical Cluster name to connect to. It is in the form of
+	//  projects/{project}/locations/{location}/clusters/{cluster}.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.cluster
+	Cluster *string `json:"cluster,omitempty"`
+
+	// The network ID of VPC to connect to.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.vpc
+	Vpc *string `json:"vpc,omitempty"`
+
+	// Optional. Project detail of the VPC network. Required if VPC is in a
+	//  different project than the cluster project.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.vpc_project
+	VpcProject *VpnConnection_VpcProject `json:"vpcProject,omitempty"`
+
+	// Whether this VPN connection has HA enabled on cluster side. If enabled,
+	//  when creating VPN connection we will attempt to use 2 ANG floating IPs.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.enable_high_availability
+	EnableHighAvailability *bool `json:"enableHighAvailability,omitempty"`
+
+	// Optional. The VPN connection Cloud Router name.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.router
+	Router *string `json:"router,omitempty"`
+}
+
+// +kcc:proto=google.cloud.edgecontainer.v1.VpnConnection.Details
+type VpnConnection_Details struct {
+	// The state of this connection.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.Details.state
+	State *string `json:"state,omitempty"`
+
+	// The error message. This is only populated when state=ERROR.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.Details.error
+	Error *string `json:"error,omitempty"`
+
+	// The Cloud Router info.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.Details.cloud_router
+	CloudRouter *VpnConnection_Details_CloudRouter `json:"cloudRouter,omitempty"`
+
+	// Each connection has multiple Cloud VPN gateways.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.Details.cloud_vpns
+	CloudVpns []VpnConnection_Details_CloudVpn `json:"cloudVpns,omitempty"`
+}
+
+// +kcc:proto=google.cloud.edgecontainer.v1.VpnConnection.Details.CloudRouter
+type VpnConnection_Details_CloudRouter struct {
+	// The associated Cloud Router name.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.Details.CloudRouter.name
+	Name *string `json:"name,omitempty"`
+}
+
+// +kcc:proto=google.cloud.edgecontainer.v1.VpnConnection.Details.CloudVpn
+type VpnConnection_Details_CloudVpn struct {
+	// The created Cloud VPN gateway name.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.Details.CloudVpn.gateway
+	Gateway *string `json:"gateway,omitempty"`
+}
+
+// +kcc:proto=google.cloud.edgecontainer.v1.VpnConnection.VpcProject
+type VpnConnection_VpcProject struct {
+	// The project of the VPC to connect to. If not specified, it is the same as
+	//  the cluster project.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.VpcProject.project_id
+	ProjectID *string `json:"projectID,omitempty"`
+
+	// Optional. Deprecated: do not use.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.VpcProject.service_account
+	ServiceAccount *string `json:"serviceAccount,omitempty"`
+}
+
+// +kcc:proto=google.cloud.edgecontainer.v1.VpnConnection
+type VpnConnectionObservedState struct {
+	// Output only. The time when the VPN connection was created.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.create_time
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// Output only. The time when the VPN connection was last updated.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
+
+	// Output only. The created connection details.
+	// +kcc:proto:field=google.cloud.edgecontainer.v1.VpnConnection.details
+	Details *VpnConnection_Details `json:"details,omitempty"`
+}
--- pkg/controller/direct/edgecontainer/mapper.generated.go
+++ pkg/controller/direct/edgecontainer/mapper.generated.go
@@ -71,3 +71,227 @@ func EdgeContainerMachineSpec_ToProto(mapCtx *direct.MapContext, in *krm.EdgeCon
 	out.Zone = direct.ValueOf(in.Zone)
 	return out
 }
+func EdgeContainerVpnConnectionObservedState_FromProto(mapCtx *direct.MapContext, in *pb.VpnConnection) *krm.EdgeContainerVpnConnectionObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.EdgeContainerVpnConnectionObservedState{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
+	// MISSING: NATGatewayIP
+	// MISSING: BGPRoutingMode
+	// MISSING: Cluster
+	// MISSING: Vpc
+	// MISSING: VpcProject
+	// MISSING: EnableHighAvailability
+	// MISSING: Router
+	// MISSING: Details
+	return out
+}
+func EdgeContainerVpnConnectionObservedState_ToProto(mapCtx *direct.MapContext, in *krm.EdgeContainerVpnConnectionObservedState) *pb.VpnConnection {
+	if in == nil {
+		return nil
+	}
+	out := &pb.VpnConnection{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
+	// MISSING: NATGatewayIP
+	// MISSING: BGPRoutingMode
+	// MISSING: Cluster
+	// MISSING: Vpc
+	// MISSING: VpcProject
+	// MISSING: EnableHighAvailability
+	// MISSING: Router
+	// MISSING: Details
+	return out
+}
+func EdgeContainerVpnConnectionSpec_FromProto(mapCtx *direct.MapContext, in *pb.VpnConnection) *krm.EdgeContainerVpnConnectionSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.EdgeContainerVpnConnectionSpec{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
+	// MISSING: NATGatewayIP
+	// MISSING: BGPRoutingMode
+	// MISSING: Cluster
+	// MISSING: Vpc
+	// MISSING: VpcProject
+	// MISSING: EnableHighAvailability
+	// MISSING: Router
+	// MISSING: Details
+	return out
+}
+func EdgeContainerVpnConnectionSpec_ToProto(mapCtx *direct.MapContext, in *krm.EdgeContainerVpnConnectionSpec) *pb.VpnConnection {
+	if in == nil {
+		return nil
+	}
+	out := &pb.VpnConnection{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
+	// MISSING: NATGatewayIP
+	// MISSING: BGPRoutingMode
+	// MISSING: Cluster
+	// MISSING: Vpc
+	// MISSING: VpcProject
+	// MISSING: EnableHighAvailability
+	// MISSING: Router
+	// MISSING: Details
+	return out
+}
+func VpnConnection_FromProto(mapCtx *direct.MapContext, in *pb.VpnConnection) *krm.VpnConnection {
+	if in == nil {
+		return nil
+	}
+	out := &krm.VpnConnection{}
+	out.Name = direct.LazyPtr(in.GetName())
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	out.Labels = in.Labels
+	out.NATGatewayIP = direct.LazyPtr(in.GetNatGatewayIp())
+	out.BGPRoutingMode = direct.Enum_FromProto(mapCtx, in.GetBgpRoutingMode())
+	out.Cluster = direct.LazyPtr(in.GetCluster())
+	out.Vpc = direct.LazyPtr(in.GetVpc())
+	out.VpcProject = VpnConnection_VpcProject_FromProto(mapCtx, in.GetVpcProject())
+	out.EnableHighAvailability = direct.LazyPtr(in.GetEnableHighAvailability())
+	out.Router = direct.LazyPtr(in.GetRouter())
+	// MISSING: Details
+	return out
+}
+func VpnConnection_ToProto(mapCtx *direct.MapContext, in *krm.VpnConnection) *pb.VpnConnection {
+	if in == nil {
+		return nil
+	}
+	out := &pb.VpnConnection{}
+	out.Name = direct.ValueOf(in.Name)
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	out.Labels = in.Labels
+	out.NatGatewayIp = direct.ValueOf(in.NATGatewayIP)
+	out.BgpRoutingMode = direct.Enum_ToProto[pb.VpnConnection_BgpRoutingMode](mapCtx, in.BGPRoutingMode)
+	out.Cluster = direct.ValueOf(in.Cluster)
+	out.Vpc = direct.ValueOf(in.Vpc)
+	out.VpcProject = VpnConnection_VpcProject_ToProto(mapCtx, in.VpcProject)
+	out.EnableHighAvailability = direct.ValueOf(in.EnableHighAvailability)
+	out.Router = direct.ValueOf(in.Router)
+	// MISSING: Details
+	return out
+}
+func VpnConnectionObservedState_FromProto(mapCtx *direct.MapContext, in *pb.VpnConnection) *krm.VpnConnectionObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.VpnConnectionObservedState{}
+	// MISSING: Name
+	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
+	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
+	// MISSING: Labels
+	// MISSING: NATGatewayIP
+	// MISSING: BGPRoutingMode
+	// MISSING: Cluster
+	// MISSING: Vpc
+	// MISSING: VpcProject
+	// MISSING: EnableHighAvailability
+	// MISSING: Router
+	out.Details = VpnConnection_Details_FromProto(mapCtx, in.GetDetails())
+	return out
+}
+func VpnConnectionObservedState_ToProto(mapCtx *direct.MapContext, in *krm.VpnConnectionObservedState) *pb.VpnConnection {
+	if in == nil {
+		return nil
+	}
+	out := &pb.VpnConnection{}
+	// MISSING: Name
+	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
+	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
+	// MISSING: Labels
+	// MISSING: NATGatewayIP
+	// MISSING: BGPRoutingMode
+	// MISSING: Cluster
+	// MISSING: Vpc
+	// MISSING: VpcProject
+	// MISSING: EnableHighAvailability
+	// MISSING: Router
+	out.Details = VpnConnection_Details_ToProto(mapCtx, in.Details)
+	return out
+}
+func VpnConnection_Details_FromProto(mapCtx *direct.MapContext, in *pb.VpnConnection_Details) *krm.VpnConnection_Details {
+	if in == nil {
+		return nil
+	}
+	out := &krm.VpnConnection_Details{}
+	out.State = direct.Enum_FromProto(mapCtx, in.GetState())
+	out.Error = direct.LazyPtr(in.GetError())
+	out.CloudRouter = VpnConnection_Details_CloudRouter_FromProto(mapCtx, in.GetCloudRouter())
+	out.CloudVpns = direct.Slice_FromProto(mapCtx, in.CloudVpns, VpnConnection_Details_CloudVpn_FromProto)
+	return out
+}
+func VpnConnection_Details_ToProto(mapCtx *direct.MapContext, in *krm.VpnConnection_Details) *pb.VpnConnection_Details {
+	if in == nil {
+		return nil
+	}
+	out := &pb.VpnConnection_Details{}
+	out.State = direct.Enum_ToProto[pb.VpnConnection_Details_State](mapCtx, in.State)
+	out.Error = direct.ValueOf(in.Error)
+	out.CloudRouter = VpnConnection_Details_CloudRouter_ToProto(mapCtx, in.CloudRouter)
+	out.CloudVpns = direct.Slice_ToProto(mapCtx, in.CloudVpns, VpnConnection_Details_CloudVpn_ToProto)
+	return out
+}
+func VpnConnection_Details_CloudRouter_FromProto(mapCtx *direct.MapContext, in *pb.VpnConnection_Details_CloudRouter) *krm.VpnConnection_Details_CloudRouter {
+	if in == nil {
+		return nil
+	}
+	out := &krm.VpnConnection_Details_CloudRouter{}
+	out.Name = direct.LazyPtr(in.GetName())
+	return out
+}
+func VpnConnection_Details_CloudRouter_ToProto(mapCtx *direct.MapContext, in *krm.VpnConnection_Details_CloudRouter) *pb.VpnConnection_Details_CloudRouter {
+	if in == nil {
+		return nil
+	}
+	out := &pb.VpnConnection_Details_CloudRouter{}
+	out.Name = direct.ValueOf(in.Name)
+	return out
+}
+func VpnConnection_Details_CloudVpn_FromProto(mapCtx *direct.MapContext, in *pb.VpnConnection_Details_CloudVpn) *krm.VpnConnection_Details_CloudVpn {
+	if in == nil {
+		return nil
+	}
+	out := &krm.VpnConnection_Details_CloudVpn{}
+	out.Gateway = direct.LazyPtr(in.GetGateway())
+	return out
+}
+func VpnConnection_Details_CloudVpn_ToProto(mapCtx *direct.MapContext, in *krm.VpnConnection_Details_CloudVpn) *pb.VpnConnection_Details_CloudVpn {
+	if in == nil {
+		return nil
+	}
+	out := &pb.VpnConnection_Details_CloudVpn{}
+	out.Gateway = direct.ValueOf(in.Gateway)
+	return out
+}
+func VpnConnection_VpcProject_FromProto(mapCtx *direct.MapContext, in *pb.VpnConnection_VpcProject) *krm.VpnConnection_VpcProject {
+	if in == nil {
+		return nil
+	}
+	out := &krm.VpnConnection_VpcProject{}
+	out.ProjectID = direct.LazyPtr(in.GetProjectId())
+	out.ServiceAccount = direct.LazyPtr(in.GetServiceAccount())
+	return out
+}
+func VpnConnection_VpcProject_ToProto(mapCtx *direct.MapContext, in *krm.VpnConnection_VpcProject) *pb.VpnConnection_VpcProject {
+	if in == nil {
+		return nil
+	}
+	out := &pb.VpnConnection_VpcProject{}
+	out.ProjectId = direct.ValueOf(in.ProjectID)
+	out.ServiceAccount = direct.ValueOf(in.ServiceAccount)
+	return out
+}
