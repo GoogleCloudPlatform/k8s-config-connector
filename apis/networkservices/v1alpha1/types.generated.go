a210e6fb7e97109431e238eeafa06c8eca9f8f28
xiaoweim <xiaoweim@google.com>
2025-03-11 23:23:12 +0000

    conductor: "Generated types and mapper for NetworkServicesEndpointPolicy"

--- apis/networkservices/v1alpha1/types.generated.go
+++ apis/networkservices/v1alpha1/types.generated.go
@@ -13,3 +13,141 @@
 // limitations under the License.
 
 package v1alpha1
+
+// +kcc:proto=google.cloud.networkservices.v1.EndpointMatcher
+type EndpointMatcher struct {
+	// The matcher is based on node metadata presented by xDS clients.
+	// +kcc:proto:field=google.cloud.networkservices.v1.EndpointMatcher.metadata_label_matcher
+	MetadataLabelMatcher *EndpointMatcher_MetadataLabelMatcher `json:"metadataLabelMatcher,omitempty"`
+}
+
+// +kcc:proto=google.cloud.networkservices.v1.EndpointMatcher.MetadataLabelMatcher
+type EndpointMatcher_MetadataLabelMatcher struct {
+	// Specifies how matching should be done.
+	//
+	//  Supported values are:
+	//  MATCH_ANY: At least one of the Labels specified in the
+	//    matcher should match the metadata presented by xDS client.
+	//  MATCH_ALL: The metadata presented by the xDS client should
+	//    contain all of the labels specified here.
+	//
+	//  The selection is determined based on the best match. For
+	//  example, suppose there are three EndpointPolicy
+	//  resources P1, P2 and P3 and if P1 has a the matcher as
+	//  MATCH_ANY <A:1, B:1>, P2 has MATCH_ALL <A:1,B:1>, and P3 has
+	//  MATCH_ALL <A:1,B:1,C:1>.
+	//
+	//  If a client with label <A:1> connects, the config from P1
+	//  will be selected.
+	//
+	//  If a client with label <A:1,B:1> connects, the config from P2
+	//  will be selected.
+	//
+	//  If a client with label <A:1,B:1,C:1> connects, the config
+	//  from P3 will be selected.
+	//
+	//  If there is more than one best match, (for example, if a
+	//  config P4 with selector <A:1,D:1> exists and if a client with
+	//  label <A:1,B:1,D:1> connects), an error will be thrown.
+	// +kcc:proto:field=google.cloud.networkservices.v1.EndpointMatcher.MetadataLabelMatcher.metadata_label_match_criteria
+	MetadataLabelMatchCriteria *string `json:"metadataLabelMatchCriteria,omitempty"`
+
+	// The list of label value pairs that must match labels in the
+	//  provided metadata based on filterMatchCriteria This list can
+	//  have at most 64 entries. The list can be empty if the match
+	//  criteria is MATCH_ANY, to specify a wildcard match (i.e this
+	//  matches any client).
+	// +kcc:proto:field=google.cloud.networkservices.v1.EndpointMatcher.MetadataLabelMatcher.metadata_labels
+	MetadataLabels []EndpointMatcher_MetadataLabelMatcher_MetadataLabels `json:"metadataLabels,omitempty"`
+}
+
+// +kcc:proto=google.cloud.networkservices.v1.EndpointMatcher.MetadataLabelMatcher.MetadataLabels
+type EndpointMatcher_MetadataLabelMatcher_MetadataLabels struct {
+	// Required. Label name presented as key in xDS Node Metadata.
+	// +kcc:proto:field=google.cloud.networkservices.v1.EndpointMatcher.MetadataLabelMatcher.MetadataLabels.label_name
+	LabelName *string `json:"labelName,omitempty"`
+
+	// Required. Label value presented as value corresponding to the above
+	//  key, in xDS Node Metadata.
+	// +kcc:proto:field=google.cloud.networkservices.v1.EndpointMatcher.MetadataLabelMatcher.MetadataLabels.label_value
+	LabelValue *string `json:"labelValue,omitempty"`
+}
+
+// +kcc:proto=google.cloud.networkservices.v1.EndpointPolicy
+type EndpointPolicy struct {
+	// Required. Name of the EndpointPolicy resource. It matches pattern
+	//  `projects/{project}/locations/global/endpointPolicies/{endpoint_policy}`.
+	// +kcc:proto:field=google.cloud.networkservices.v1.EndpointPolicy.name
+	Name *string `json:"name,omitempty"`
+
+	// Optional. Set of label tags associated with the EndpointPolicy resource.
+	// +kcc:proto:field=google.cloud.networkservices.v1.EndpointPolicy.labels
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// Required. The type of endpoint policy. This is primarily used to validate
+	//  the configuration.
+	// +kcc:proto:field=google.cloud.networkservices.v1.EndpointPolicy.type
+	Type *string `json:"type,omitempty"`
+
+	// Optional. This field specifies the URL of AuthorizationPolicy resource that
+	//  applies authorization policies to the inbound traffic at the
+	//  matched endpoints. Refer to Authorization. If this field is not
+	//  specified, authorization is disabled(no authz checks) for this
+	//  endpoint.
+	// +kcc:proto:field=google.cloud.networkservices.v1.EndpointPolicy.authorization_policy
+	AuthorizationPolicy *string `json:"authorizationPolicy,omitempty"`
+
+	// Required. A matcher that selects endpoints to which the policies should be
+	//  applied.
+	// +kcc:proto:field=google.cloud.networkservices.v1.EndpointPolicy.endpoint_matcher
+	EndpointMatcher *EndpointMatcher `json:"endpointMatcher,omitempty"`
+
+	// Optional. Port selector for the (matched) endpoints. If no port selector is
+	//  provided, the matched config is applied to all ports.
+	// +kcc:proto:field=google.cloud.networkservices.v1.EndpointPolicy.traffic_port_selector
+	TrafficPortSelector *TrafficPortSelector `json:"trafficPortSelector,omitempty"`
+
+	// Optional. A free-text description of the resource. Max length 1024
+	//  characters.
+	// +kcc:proto:field=google.cloud.networkservices.v1.EndpointPolicy.description
+	Description *string `json:"description,omitempty"`
+
+	// Optional. A URL referring to ServerTlsPolicy resource. ServerTlsPolicy is
+	//  used to determine the authentication policy to be applied to terminate the
+	//  inbound traffic at the identified backends. If this field is not set,
+	//  authentication is disabled(open) for this endpoint.
+	// +kcc:proto:field=google.cloud.networkservices.v1.EndpointPolicy.server_tls_policy
+	ServerTLSPolicy *string `json:"serverTLSPolicy,omitempty"`
+
+	// Optional. A URL referring to a ClientTlsPolicy resource. ClientTlsPolicy
+	//  can be set to specify the authentication for traffic from the proxy to the
+	//  actual endpoints. More specifically, it is applied to the outgoing traffic
+	//  from the proxy to the endpoint. This is typically used for sidecar model
+	//  where the proxy identifies itself as endpoint to the control plane, with
+	//  the connection between sidecar and endpoint requiring authentication. If
+	//  this field is not set, authentication is disabled(open). Applicable only
+	//  when EndpointPolicyType is SIDECAR_PROXY.
+	// +kcc:proto:field=google.cloud.networkservices.v1.EndpointPolicy.client_tls_policy
+	ClientTLSPolicy *string `json:"clientTLSPolicy,omitempty"`
+}
+
+// +kcc:proto=google.cloud.networkservices.v1.TrafficPortSelector
+type TrafficPortSelector struct {
+	// Optional. A list of ports. Can be port numbers or port range
+	//  (example, [80-90] specifies all ports from 80 to 90, including
+	//  80 and 90) or named ports or * to specify all ports. If the
+	//  list is empty, all ports are selected.
+	// +kcc:proto:field=google.cloud.networkservices.v1.TrafficPortSelector.ports
+	Ports []string `json:"ports,omitempty"`
+}
+
+// +kcc:proto=google.cloud.networkservices.v1.EndpointPolicy
+type EndpointPolicyObservedState struct {
+	// Output only. The timestamp when the resource was created.
+	// +kcc:proto:field=google.cloud.networkservices.v1.EndpointPolicy.create_time
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// Output only. The timestamp when the resource was updated.
+	// +kcc:proto:field=google.cloud.networkservices.v1.EndpointPolicy.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
+}
--- pkg/controller/direct/networkservices/mapper.generated.go
+++ pkg/controller/direct/networkservices/mapper.generated.go
@@ -45,3 +45,243 @@ func NetworkServicesServiceBindingObservedState_ToProto(mapCtx *direct.MapContex
 	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
 	return out
 }
+<<<<<<< HEAD
+=======
+func NetworkServicesServiceBindingSpec_FromProto(mapCtx *direct.MapContext, in *pb.ServiceBinding) *krm.NetworkServicesServiceBindingSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.NetworkServicesServiceBindingSpec{}
+	// MISSING: Name
+	out.Description = direct.LazyPtr(in.GetDescription())
+	out.Service = direct.LazyPtr(in.GetService())
+	out.Labels = in.Labels
+	return out
+}
+func NetworkServicesServiceBindingSpec_ToProto(mapCtx *direct.MapContext, in *krm.NetworkServicesServiceBindingSpec) *pb.ServiceBinding {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ServiceBinding{}
+	// MISSING: Name
+	out.Description = direct.ValueOf(in.Description)
+	out.Service = direct.ValueOf(in.Service)
+	out.Labels = in.Labels
+	return out
+}
+
+func EndpointMatcher_FromProto(mapCtx *direct.MapContext, in *pb.EndpointMatcher) *krm.EndpointMatcher {
+	if in == nil {
+		return nil
+	}
+	out := &krm.EndpointMatcher{}
+	out.MetadataLabelMatcher = EndpointMatcher_MetadataLabelMatcher_FromProto(mapCtx, in.GetMetadataLabelMatcher())
+	return out
+}
+func EndpointMatcher_ToProto(mapCtx *direct.MapContext, in *krm.EndpointMatcher) *pb.EndpointMatcher {
+	if in == nil {
+		return nil
+	}
+	out := &pb.EndpointMatcher{}
+	if oneof := EndpointMatcher_MetadataLabelMatcher_ToProto(mapCtx, in.MetadataLabelMatcher); oneof != nil {
+		out.MatcherType = &pb.EndpointMatcher_MetadataLabelMatcher_{MetadataLabelMatcher: oneof}
+	}
+	return out
+}
+func EndpointMatcher_MetadataLabelMatcher_FromProto(mapCtx *direct.MapContext, in *pb.EndpointMatcher_MetadataLabelMatcher) *krm.EndpointMatcher_MetadataLabelMatcher {
+	if in == nil {
+		return nil
+	}
+	out := &krm.EndpointMatcher_MetadataLabelMatcher{}
+	out.MetadataLabelMatchCriteria = direct.Enum_FromProto(mapCtx, in.GetMetadataLabelMatchCriteria())
+	out.MetadataLabels = direct.Slice_FromProto(mapCtx, in.MetadataLabels, EndpointMatcher_MetadataLabelMatcher_MetadataLabels_FromProto)
+	return out
+}
+func EndpointMatcher_MetadataLabelMatcher_ToProto(mapCtx *direct.MapContext, in *krm.EndpointMatcher_MetadataLabelMatcher) *pb.EndpointMatcher_MetadataLabelMatcher {
+	if in == nil {
+		return nil
+	}
+	out := &pb.EndpointMatcher_MetadataLabelMatcher{}
+	out.MetadataLabelMatchCriteria = direct.Enum_ToProto[pb.EndpointMatcher_MetadataLabelMatcher_MetadataLabelMatchCriteria](mapCtx, in.MetadataLabelMatchCriteria)
+	out.MetadataLabels = direct.Slice_ToProto(mapCtx, in.MetadataLabels, EndpointMatcher_MetadataLabelMatcher_MetadataLabels_ToProto)
+	return out
+}
+func EndpointMatcher_MetadataLabelMatcher_MetadataLabels_FromProto(mapCtx *direct.MapContext, in *pb.EndpointMatcher_MetadataLabelMatcher_MetadataLabels) *krm.EndpointMatcher_MetadataLabelMatcher_MetadataLabels {
+	if in == nil {
+		return nil
+	}
+	out := &krm.EndpointMatcher_MetadataLabelMatcher_MetadataLabels{}
+	out.LabelName = direct.LazyPtr(in.GetLabelName())
+	out.LabelValue = direct.LazyPtr(in.GetLabelValue())
+	return out
+}
+func EndpointMatcher_MetadataLabelMatcher_MetadataLabels_ToProto(mapCtx *direct.MapContext, in *krm.EndpointMatcher_MetadataLabelMatcher_MetadataLabels) *pb.EndpointMatcher_MetadataLabelMatcher_MetadataLabels {
+	if in == nil {
+		return nil
+	}
+	out := &pb.EndpointMatcher_MetadataLabelMatcher_MetadataLabels{}
+	out.LabelName = direct.ValueOf(in.LabelName)
+	out.LabelValue = direct.ValueOf(in.LabelValue)
+	return out
+}
+func EndpointPolicy_FromProto(mapCtx *direct.MapContext, in *pb.EndpointPolicy) *krm.EndpointPolicy {
+	if in == nil {
+		return nil
+	}
+	out := &krm.EndpointPolicy{}
+	out.Name = direct.LazyPtr(in.GetName())
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	out.Labels = in.Labels
+	out.Type = direct.Enum_FromProto(mapCtx, in.GetType())
+	out.AuthorizationPolicy = direct.LazyPtr(in.GetAuthorizationPolicy())
+	out.EndpointMatcher = EndpointMatcher_FromProto(mapCtx, in.GetEndpointMatcher())
+	out.TrafficPortSelector = TrafficPortSelector_FromProto(mapCtx, in.GetTrafficPortSelector())
+	out.Description = direct.LazyPtr(in.GetDescription())
+	out.ServerTLSPolicy = direct.LazyPtr(in.GetServerTlsPolicy())
+	out.ClientTLSPolicy = direct.LazyPtr(in.GetClientTlsPolicy())
+	return out
+}
+func EndpointPolicy_ToProto(mapCtx *direct.MapContext, in *krm.EndpointPolicy) *pb.EndpointPolicy {
+	if in == nil {
+		return nil
+	}
+	out := &pb.EndpointPolicy{}
+	out.Name = direct.ValueOf(in.Name)
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	out.Labels = in.Labels
+	out.Type = direct.Enum_ToProto[pb.EndpointPolicy_EndpointPolicyType](mapCtx, in.Type)
+	out.AuthorizationPolicy = direct.ValueOf(in.AuthorizationPolicy)
+	out.EndpointMatcher = EndpointMatcher_ToProto(mapCtx, in.EndpointMatcher)
+	out.TrafficPortSelector = TrafficPortSelector_ToProto(mapCtx, in.TrafficPortSelector)
+	out.Description = direct.ValueOf(in.Description)
+	out.ServerTlsPolicy = direct.ValueOf(in.ServerTLSPolicy)
+	out.ClientTlsPolicy = direct.ValueOf(in.ClientTLSPolicy)
+	return out
+}
+func EndpointPolicyObservedState_FromProto(mapCtx *direct.MapContext, in *pb.EndpointPolicy) *krm.EndpointPolicyObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.EndpointPolicyObservedState{}
+	// MISSING: Name
+	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
+	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
+	// MISSING: Labels
+	// MISSING: Type
+	// MISSING: AuthorizationPolicy
+	// MISSING: EndpointMatcher
+	// MISSING: TrafficPortSelector
+	// MISSING: Description
+	// MISSING: ServerTLSPolicy
+	// MISSING: ClientTLSPolicy
+	return out
+}
+func EndpointPolicyObservedState_ToProto(mapCtx *direct.MapContext, in *krm.EndpointPolicyObservedState) *pb.EndpointPolicy {
+	if in == nil {
+		return nil
+	}
+	out := &pb.EndpointPolicy{}
+	// MISSING: Name
+	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
+	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
+	// MISSING: Labels
+	// MISSING: Type
+	// MISSING: AuthorizationPolicy
+	// MISSING: EndpointMatcher
+	// MISSING: TrafficPortSelector
+	// MISSING: Description
+	// MISSING: ServerTLSPolicy
+	// MISSING: ClientTLSPolicy
+	return out
+}
+func NetworkServicesEndpointPolicyObservedState_FromProto(mapCtx *direct.MapContext, in *pb.EndpointPolicy) *krm.NetworkServicesEndpointPolicyObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.NetworkServicesEndpointPolicyObservedState{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
+	// MISSING: Type
+	// MISSING: AuthorizationPolicy
+	// MISSING: EndpointMatcher
+	// MISSING: TrafficPortSelector
+	// MISSING: Description
+	// MISSING: ServerTLSPolicy
+	// MISSING: ClientTLSPolicy
+	return out
+}
+func NetworkServicesEndpointPolicyObservedState_ToProto(mapCtx *direct.MapContext, in *krm.NetworkServicesEndpointPolicyObservedState) *pb.EndpointPolicy {
+	if in == nil {
+		return nil
+	}
+	out := &pb.EndpointPolicy{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
+	// MISSING: Type
+	// MISSING: AuthorizationPolicy
+	// MISSING: EndpointMatcher
+	// MISSING: TrafficPortSelector
+	// MISSING: Description
+	// MISSING: ServerTLSPolicy
+	// MISSING: ClientTLSPolicy
+	return out
+}
+func NetworkServicesEndpointPolicySpec_FromProto(mapCtx *direct.MapContext, in *pb.EndpointPolicy) *krm.NetworkServicesEndpointPolicySpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.NetworkServicesEndpointPolicySpec{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
+	// MISSING: Type
+	// MISSING: AuthorizationPolicy
+	// MISSING: EndpointMatcher
+	// MISSING: TrafficPortSelector
+	// MISSING: Description
+	// MISSING: ServerTLSPolicy
+	// MISSING: ClientTLSPolicy
+	return out
+}
+func NetworkServicesEndpointPolicySpec_ToProto(mapCtx *direct.MapContext, in *krm.NetworkServicesEndpointPolicySpec) *pb.EndpointPolicy {
+	if in == nil {
+		return nil
+	}
+	out := &pb.EndpointPolicy{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
+	// MISSING: Type
+	// MISSING: AuthorizationPolicy
+	// MISSING: EndpointMatcher
+	// MISSING: TrafficPortSelector
+	// MISSING: Description
+	// MISSING: ServerTLSPolicy
+	// MISSING: ClientTLSPolicy
+	return out
+}
+func TrafficPortSelector_FromProto(mapCtx *direct.MapContext, in *pb.TrafficPortSelector) *krm.TrafficPortSelector {
+	if in == nil {
+		return nil
+	}
+	out := &krm.TrafficPortSelector{}
+	out.Ports = in.Ports
+	return out
+}
+func TrafficPortSelector_ToProto(mapCtx *direct.MapContext, in *krm.TrafficPortSelector) *pb.TrafficPortSelector {
+	if in == nil {
+		return nil
+	}
+	out := &pb.TrafficPortSelector{}
+	out.Ports = in.Ports
+	return out
+}
+>>>>>>> ac113f041 (conductor: "Generated types and mapper for NetworkServicesEndpointPolicy")
