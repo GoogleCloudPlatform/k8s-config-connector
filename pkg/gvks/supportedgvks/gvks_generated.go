a836d598af8d3074a5249c60233bd2635af19f34
Alex Pana <8968914+acpana@users.noreply.github.com>
2025-03-12 22:16:36 +0000

    chore: make ready-pr
    Signed-off-by: Alex Pana <8968914+acpana@users.noreply.github.com>

--- pkg/gvks/supportedgvks/gvks_generated.go
+++ pkg/gvks/supportedgvks/gvks_generated.go
@@ -2320,6 +2320,16 @@ var SupportedGVKs = map[schema.GroupVersionKind]GVKMetadata{
 		},
 	},
 	{
+		Group:   "clouddeploy.cnrm.cloud.google.com",
+		Version: "v1alpha1",
+		Kind:    "DeployDeliveryPipeline",
+	}: {
+		Labels: map[string]string{
+			"cnrm.cloud.google.com/managed-by-kcc": "true",
+			"cnrm.cloud.google.com/system":         "true",
+		},
+	},
+	{
 		Group:   "deploymentmanager.cnrm.cloud.google.com",
 		Version: "v1alpha1",
 		Kind:    "DeploymentManagerDeployment",
