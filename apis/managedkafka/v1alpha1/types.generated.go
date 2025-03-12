e974075583bbdfd52d2071562d0e60090c955c43
xiaoweim <xiaoweim@google.com>
2025-03-12 19:32:07 +0000

    conductor: "Generated types and mapper for ManagedKafkaConsumerGroup"

--- apis/managedkafka/v1alpha1/types.generated.go
+++ apis/managedkafka/v1alpha1/types.generated.go
@@ -21,10 +21,35 @@
 
 package v1alpha1
 
-// +kcc:proto=google.cloud.managedkafka.v1.RebalanceConfig
-type RebalanceConfig struct {
-	// Optional. The rebalance behavior for the cluster.
-	//  When not specified, defaults to `NO_REBALANCE`.
-	// +kcc:proto:field=google.cloud.managedkafka.v1.RebalanceConfig.mode
-	Mode *string `json:"mode,omitempty"`
+
+// +kcc:proto=google.cloud.managedkafka.v1.ConsumerGroup
+type ConsumerGroup struct {
+	// Identifier. The name of the consumer group. The `consumer_group` segment is
+	//  used when connecting directly to the cluster. Structured like:
+	//  projects/{project}/locations/{location}/clusters/{cluster}/consumerGroups/{consumer_group}
+	// +kcc:proto:field=google.cloud.managedkafka.v1.ConsumerGroup.name
+	Name *string `json:"name,omitempty"`
+
+	// TODO: unsupported map type with key string and value message
+
+}
+
+// +kcc:proto=google.cloud.managedkafka.v1.ConsumerPartitionMetadata
+type ConsumerPartitionMetadata struct {
+	// Required. The current offset for this partition, or 0 if no offset has been
+	//  committed.
+	// +kcc:proto:field=google.cloud.managedkafka.v1.ConsumerPartitionMetadata.offset
+	Offset *int64 `json:"offset,omitempty"`
+
+	// Optional. The associated metadata for this partition, or empty if it does
+	//  not exist.
+	// +kcc:proto:field=google.cloud.managedkafka.v1.ConsumerPartitionMetadata.metadata
+	Metadata *string `json:"metadata,omitempty"`
+}
+
+// +kcc:proto=google.cloud.managedkafka.v1.ConsumerTopicMetadata
+type ConsumerTopicMetadata struct {
+
+	// TODO: unsupported map type with key int32 and value message
+
 }
