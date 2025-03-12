6e231220fc1083ca2be4e22c78d6ec0dac24af25
justinsb <justinsb@google.com>
2025-03-12 16:40:07 -0400

    cleanup fuzzer

--- pkg/controller/direct/discoveryengine/servingconfig_fuzzer.go
+++ pkg/controller/direct/discoveryengine/servingconfig_fuzzer.go
@@ -20,7 +20,6 @@ package discoveryengine
 
 import (
 	pb "cloud.google.com/go/discoveryengine/apiv1beta/discoveryenginepb"
-	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/fuzztesting"
 )
 
@@ -59,77 +58,4 @@ func discoveryEngineServingConfigFuzzer() fuzztesting.KRMFuzzer {
 	f.UnimplementedFields.Insert(".update_time")
 
 	return f
-}
-
-func DiscoveryEngineServingConfigSpec_FromProto(mapCtx *direct.MapContext, in *pb.ServingConfig) *krm.DiscoveryEngineServingConfigSpec {
-	if in == nil {
-		return nil
-	}
-	out := &krm.DiscoveryEngineServingConfigSpec{}
-	out.MediaConfig = ServingConfig_MediaConfig_FromProto(mapCtx, in.GetMediaConfig())
-	out.GenericConfig = ServingConfig_GenericConfig_FromProto(mapCtx, in.GetGenericConfig())
-	out.Name = direct.LazyPtr(in.GetName())
-	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
-	out.SolutionType = direct.Enum_FromProto(mapCtx, in.GetSolutionType())
-	out.ModelID = direct.LazyPtr(in.GetModelId())
-	out.DiversityLevel = direct.LazyPtr(in.GetDiversityLevel())
-	out.EmbeddingConfig = EmbeddingConfig_FromProto(mapCtx, in.GetEmbeddingConfig())
-	out.RankingExpression = direct.LazyPtr(in.GetRankingExpression())
-	out.FilterControlIds = in.FilterControlIds
-	out.BoostControlIds = in.BoostControlIds
-	out.RedirectControlIds = in.RedirectControlIds
-	out.SynonymsControlIds = in.SynonymsControlIds
-	out.OnewaySynonymsControlIds = in.OnewaySynonymsControlIds
-	out.DissociateControlIds = in.DissociateControlIds
-	out.ReplacementControlIds = in.ReplacementControlIds
-	out.IgnoreControlIds = in.IgnoreControlIds
-	out.PersonalizationSpec = SearchRequest_PersonalizationSpec_FromProto(mapCtx, in.GetPersonalizationSpec())
-	return out
-}
-func DiscoveryEngineServingConfigSpec_ToProto(mapCtx *direct.MapContext, in *krm.DiscoveryEngineServingConfigSpec) *pb.ServingConfig {
-	if in == nil {
-		return nil
-	}
-	out := &pb.ServingConfig{}
-	if oneof := ServingConfig_MediaConfig_ToProto(mapCtx, in.MediaConfig); oneof != nil {
-		out.VerticalConfig = &pb.ServingConfig_MediaConfig{MediaConfig: oneof}
-	}
-	if oneof := ServingConfig_GenericConfig_ToProto(mapCtx, in.GenericConfig); oneof != nil {
-		out.VerticalConfig = &pb.ServingConfig_GenericConfig{GenericConfig: oneof}
-	}
-	out.Name = direct.ValueOf(in.Name)
-	out.DisplayName = direct.ValueOf(in.DisplayName)
-	out.SolutionType = direct.Enum_ToProto[pb.SolutionType](mapCtx, in.SolutionType)
-	out.ModelId = direct.ValueOf(in.ModelID)
-	out.DiversityLevel = direct.ValueOf(in.DiversityLevel)
-	out.EmbeddingConfig = EmbeddingConfig_ToProto(mapCtx, in.EmbeddingConfig)
-	out.RankingExpression = direct.ValueOf(in.RankingExpression)
-	out.FilterControlIds = in.FilterControlIds
-	out.BoostControlIds = in.BoostControlIds
-	out.RedirectControlIds = in.RedirectControlIds
-	out.SynonymsControlIds = in.SynonymsControlIds
-	out.OnewaySynonymsControlIds = in.OnewaySynonymsControlIds
-	out.DissociateControlIds = in.DissociateControlIds
-	out.ReplacementControlIds = in.ReplacementControlIds
-	out.IgnoreControlIds = in.IgnoreControlIds
-	out.PersonalizationSpec = SearchRequest_PersonalizationSpec_ToProto(mapCtx, in.PersonalizationSpec)
-	return out
-}
-func DiscoveryEngineServingConfigObservedState_FromProto(mapCtx *direct.MapContext, in *pb.ServingConfig) *krm.DiscoveryEngineServingConfigObservedState {
-	if in == nil {
-		return nil
-	}
-	out := &krm.DiscoveryEngineServingConfigObservedState{}
-	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
-	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
-	return out
-}
-func DiscoveryEngineServingConfigObservedState_ToProto(mapCtx *direct.MapContext, in *krm.DiscoveryEngineServingConfigObservedState) *pb.ServingConfig {
-	if in == nil {
-		return nil
-	}
-	out := &pb.ServingConfig{}
-	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
-	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
-	return out
 }
