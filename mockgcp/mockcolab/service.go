29f217b20f5102899cdab832f3d4bfeda8f41ce7
Joyce Ma <maqiuyu@google.com>
2025-03-12 23:27:11 +0000

    conductor: "Adding mock service and resource for colab-runtimes"

---
+++ mockgcp/mockcolab/service.go
@@ -0,0 +1,80 @@
+ // Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mockaiplatform
+
+// +tool:mockgcp-service
+// http.host: aiplatform.googleapis.com
+// proto.service: google.cloud.aiplatform.v1.NotebookService
+
+import (
+	"context"
+	"net/http"
+
+	"google.golang.org/grpc"
+
+	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/httpmux"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/operations"
+	pb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/aiplatform/v1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/pkg/storage"
+)
+
+// MockService represents a mocked aiplatform service.
+type MockService struct {
+	*common.MockEnvironment
+	storage storage.Storage
+
+	operations *operations.Operations
+
+	v1 *NotebookServiceV1
+}
+
+type NotebookServiceV1 struct {
+	*MockService
+	pb.UnimplementedNotebookServiceServer
+}
+
+// New creates a MockService.
+func New(env *common.MockEnvironment, storage storage.Storage) *MockService {
+	s := &MockService{
+		MockEnvironment: env,
+		storage:         storage,
+		operations:      operations.NewOperationsService(storage),
+	}
+	s.v1 = &NotebookServiceV1{MockService: s}
+	return s
+}
+
+func (s *MockService) ExpectedHosts() []string {
+	return []string{"aiplatform.googleapis.com"}
+}
+
+func (s *MockService) Register(grpcServer *grpc.Server) {
+	pb.RegisterNotebookServiceServer(grpcServer, s.v1)
+}
+
+func (s *MockService) NewHTTPMux(ctx context.Context, conn *grpc.ClientConn) (http.Handler, error) {
+	mux, err := httpmux.NewServeMux(ctx, conn, httpmux.Options{},
+		pb.RegisterNotebookServiceHandler,
+		s.operations.RegisterOperationsPath("/v1/{prefix=**}/operations/{name}"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return mux, nil
+}
+
+
