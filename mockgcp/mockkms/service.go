6f2c1cae317a56d5323429950c05741f51957a47
Joyce Ma <maqiuyu@google.com>
2025-03-12 23:24:21 +0000

    conductor: "Adding mock service and resource for kms-importjob"

--- mockgcp/mockkms/service.go
+++ mockgcp/mockkms/service.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+ // Copyright 2024 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,12 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package mockkms
+package mockcloudkms
+
+// +tool:mockgcp-service
+// http.host: cloudkms.googleapis.com
+// proto.service: google.cloud.kms.v1.KeyManagementService
 
 import (
 	"context"
 	"net/http"
-	"strings"
 
 	"google.golang.org/grpc"
 
@@ -25,16 +28,23 @@ import (
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/httpmux"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/operations"
 	pb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/kms/v1"
+
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/pkg/storage"
 )
 
 // MockService represents a mocked kms service.
 type MockService struct {
 	*common.MockEnvironment
-	storage              storage.Storage
-	operations           *operations.Operations
-	v1AutokeyAdminServer *autokeyAdminServer
-	v1AutokeyServer      *autokeyServer
+	storage storage.Storage
+
+	operations *operations.Operations
+
+	v1 *KMSV1
+}
+
+type KMSV1 struct {
+	*MockService
+	pb.UnimplementedKeyManagementServiceServer
 }
 
 // New creates a MockService.
@@ -44,8 +54,7 @@ func New(env *common.MockEnvironment, storage storage.Storage) *MockService {
 		storage:         storage,
 		operations:      operations.NewOperationsService(storage),
 	}
-	s.v1AutokeyAdminServer = &autokeyAdminServer{MockService: s}
-	s.v1AutokeyServer = &autokeyServer{MockService: s}
+	s.v1 = &KMSV1{MockService: s}
 	return s
 }
 
@@ -54,27 +63,19 @@ func (s *MockService) ExpectedHosts() []string {
 }
 
 func (s *MockService) Register(grpcServer *grpc.Server) {
-	pb.RegisterKeyManagementServiceServer(grpcServer, &kmsServer{MockService: s})
-	pb.RegisterAutokeyAdminServer(grpcServer, s.v1AutokeyAdminServer)
-	pb.RegisterAutokeyServer(grpcServer, s.v1AutokeyServer)
+	pb.RegisterKeyManagementServiceServer(grpcServer, s.v1)
 }
 
 func (s *MockService) NewHTTPMux(ctx context.Context, conn *grpc.ClientConn) (http.Handler, error) {
 	mux, err := httpmux.NewServeMux(ctx, conn, httpmux.Options{},
 		pb.RegisterKeyManagementServiceHandler,
-		pb.RegisterAutokeyAdminHandler,
-		pb.RegisterAutokeyHandler,
-		s.operations.RegisterOperationsPath("/v1/{prefix=**}/operations/{name}"),
-	)
+		s.operations.RegisterOperationsPath("/v1/{prefix=**}/operations/{name}"))
+
 	if err != nil {
 		return nil, err
 	}
 
-	// Returns slightly non-standard errors
-	mux.RewriteError = func(ctx context.Context, error *httpmux.ErrorResponse) {
-		if error.Code == 404 && (strings.Contains(error.Message, "KeyRing") || strings.Contains(error.Message, "CryptoKey")) {
-			error.Errors = nil
-		}
-	}
 	return mux, nil
 }
+
+
