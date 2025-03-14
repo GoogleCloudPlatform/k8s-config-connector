a0b33be62af0d02b0ecf4abeef794123827a1609
Alex Pana <8968914+acpana@users.noreply.github.com>
2025-03-14 19:35:02 +0000

    conductor: "Generated types and mapper for CertificateManagerTrustConfig"

--- apis/certificatemanager/v1alpha1/types.generated.go
+++ apis/certificatemanager/v1alpha1/types.generated.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,16 +14,81 @@
 
 package v1alpha1
 
-// +kcc:proto=google.cloud.certificatemanager.v1.DnsAuthorization.DnsResourceRecord
-type DnsAuthorization_DnsResourceRecord struct {
-	// Output only. Fully qualified name of the DNS Resource Record.
-	//  e.g. `_acme-challenge.example.com`
+
+// +kcc:proto=google.cloud.certificatemanager.v1.TrustConfig
+type TrustConfig struct {
+	// A user-defined name of the trust config. TrustConfig names must be
+	//  unique globally and match pattern
+	//  `projects/*/locations/*/trustConfigs/*`.
+	// +kcc:proto:field=google.cloud.certificatemanager.v1.TrustConfig.name
 	Name *string `json:"name,omitempty"`
 
-	// Output only. Type of the DNS Resource Record.
-	//  Currently always set to "CNAME".
-	Type *string `json:"type,omitempty"`
+	// Set of labels associated with a TrustConfig.
+	// +kcc:proto:field=google.cloud.certificatemanager.v1.TrustConfig.labels
+	Labels map[string]string `json:"labels,omitempty"`
 
-	// Output only. Data of the DNS Resource Record.
-	Data *string `json:"data,omitempty"`
+	// One or more paragraphs of text description of a TrustConfig.
+	// +kcc:proto:field=google.cloud.certificatemanager.v1.TrustConfig.description
+	Description *string `json:"description,omitempty"`
+
+	// This checksum is computed by the server based on the value of other
+	//  fields, and may be sent on update and delete requests to ensure the
+	//  client has an up-to-date value before proceeding.
+	// +kcc:proto:field=google.cloud.certificatemanager.v1.TrustConfig.etag
+	Etag *string `json:"etag,omitempty"`
+
+	// Set of trust stores to perform validation against.
+	//
+	//  This field is supported when TrustConfig is configured with Load Balancers,
+	//  currently not supported for SPIFFE certificate validation.
+	//
+	//  Only one TrustStore specified is currently allowed.
+	// +kcc:proto:field=google.cloud.certificatemanager.v1.TrustConfig.trust_stores
+	TrustStores []TrustConfig_TrustStore `json:"trustStores,omitempty"`
+}
+
+// +kcc:proto=google.cloud.certificatemanager.v1.TrustConfig.IntermediateCA
+type TrustConfig_IntermediateCA struct {
+	// PEM intermediate certificate used for building up paths
+	//  for validation.
+	//
+	//  Each certificate provided in PEM format may occupy up to 5kB.
+	// +kcc:proto:field=google.cloud.certificatemanager.v1.TrustConfig.IntermediateCA.pem_certificate
+	PemCertificate *string `json:"pemCertificate,omitempty"`
+}
+
+// +kcc:proto=google.cloud.certificatemanager.v1.TrustConfig.TrustAnchor
+type TrustConfig_TrustAnchor struct {
+	// PEM root certificate of the PKI used for validation.
+	//
+	//  Each certificate provided in PEM format may occupy up to 5kB.
+	// +kcc:proto:field=google.cloud.certificatemanager.v1.TrustConfig.TrustAnchor.pem_certificate
+	PemCertificate *string `json:"pemCertificate,omitempty"`
+}
+
+// +kcc:proto=google.cloud.certificatemanager.v1.TrustConfig.TrustStore
+type TrustConfig_TrustStore struct {
+	// List of Trust Anchors to be used while performing validation
+	//  against a given TrustStore.
+	// +kcc:proto:field=google.cloud.certificatemanager.v1.TrustConfig.TrustStore.trust_anchors
+	TrustAnchors []TrustConfig_TrustAnchor `json:"trustAnchors,omitempty"`
+
+	// Set of intermediate CA certificates used for the path building
+	//  phase of chain validation.
+	//
+	//  The field is currently not supported if TrustConfig is used for the
+	//  workload certificate feature.
+	// +kcc:proto:field=google.cloud.certificatemanager.v1.TrustConfig.TrustStore.intermediate_cas
+	IntermediateCas []TrustConfig_IntermediateCA `json:"intermediateCas,omitempty"`
+}
+
+// +kcc:proto=google.cloud.certificatemanager.v1.TrustConfig
+type TrustConfigObservedState struct {
+	// Output only. The creation timestamp of a TrustConfig.
+	// +kcc:proto:field=google.cloud.certificatemanager.v1.TrustConfig.create_time
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// Output only. The last update timestamp of a TrustConfig.
+	// +kcc:proto:field=google.cloud.certificatemanager.v1.TrustConfig.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
 }
--- pkg/controller/direct/certificatemanager/mapper.generated.go
+++ pkg/controller/direct/certificatemanager/mapper.generated.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@ package certificatemanager
 
 import (
 	pb "cloud.google.com/go/certificatemanager/apiv1/certificatemanagerpb"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/certificatemanager/v1alpha1"
 	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/certificatemanager/v1beta1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 )
@@ -25,8 +26,14 @@ func CertificateManagerDNSAuthorizationSpec_FromProto(mapCtx *direct.MapContext,
 		return nil
 	}
 	out := &krm.CertificateManagerDNSAuthorizationSpec{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
 	out.Description = direct.LazyPtr(in.GetDescription())
-	out.Domain = in.GetDomain()
+	out.Domain = direct.LazyPtr(in.GetDomain())
+	// MISSING: DNSResourceRecord
+	// MISSING: Type
 	return out
 }
 func CertificateManagerDNSAuthorizationSpec_ToProto(mapCtx *direct.MapContext, in *krm.CertificateManagerDNSAuthorizationSpec) *pb.DnsAuthorization {
@@ -34,8 +41,70 @@ func CertificateManagerDNSAuthorizationSpec_ToProto(mapCtx *direct.MapContext, i
 		return nil
 	}
 	out := &pb.DnsAuthorization{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
 	out.Description = direct.ValueOf(in.Description)
-	out.Domain = in.Domain
+	out.Domain = CertificateManagerDNSAuthorizationSpec_Domain_ToProto(mapCtx, in.Domain)
+	// MISSING: DNSResourceRecord
+	// MISSING: Type
+	return out
+}
+func CertificateManagerTrustConfigObservedState_FromProto(mapCtx *direct.MapContext, in *pb.TrustConfig) *krm.CertificateManagerTrustConfigObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.CertificateManagerTrustConfigObservedState{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
+	// MISSING: Description
+	// MISSING: Etag
+	// MISSING: TrustStores
+	return out
+}
+func CertificateManagerTrustConfigObservedState_ToProto(mapCtx *direct.MapContext, in *krm.CertificateManagerTrustConfigObservedState) *pb.TrustConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.TrustConfig{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
+	// MISSING: Description
+	// MISSING: Etag
+	// MISSING: TrustStores
+	return out
+}
+func CertificateManagerTrustConfigSpec_FromProto(mapCtx *direct.MapContext, in *pb.TrustConfig) *krm.CertificateManagerTrustConfigSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.CertificateManagerTrustConfigSpec{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
+	// MISSING: Description
+	// MISSING: Etag
+	// MISSING: TrustStores
+	return out
+}
+func CertificateManagerTrustConfigSpec_ToProto(mapCtx *direct.MapContext, in *krm.CertificateManagerTrustConfigSpec) *pb.TrustConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.TrustConfig{}
+	// MISSING: Name
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	// MISSING: Labels
+	// MISSING: Description
+	// MISSING: Etag
+	// MISSING: TrustStores
 	return out
 }
 func DnsAuthorization_DnsResourceRecord_FromProto(mapCtx *direct.MapContext, in *pb.DnsAuthorization_DnsResourceRecord) *krm.DnsAuthorization_DnsResourceRecord {
@@ -56,5 +125,115 @@ func DnsAuthorization_DnsResourceRecord_ToProto(mapCtx *direct.MapContext, in *k
 	out.Name = direct.ValueOf(in.Name)
 	out.Type = direct.ValueOf(in.Type)
 	out.Data = direct.ValueOf(in.Data)
+	return out
+}
+func TrustConfig_FromProto(mapCtx *direct.MapContext, in *pb.TrustConfig) *krm.TrustConfig {
+	if in == nil {
+		return nil
+	}
+	out := &krm.TrustConfig{}
+	out.Name = direct.LazyPtr(in.GetName())
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	out.Labels = in.Labels
+	out.Description = direct.LazyPtr(in.GetDescription())
+	out.Etag = direct.LazyPtr(in.GetEtag())
+	out.TrustStores = direct.Slice_FromProto(mapCtx, in.TrustStores, TrustConfig_TrustStore_FromProto)
+	return out
+}
+func TrustConfig_ToProto(mapCtx *direct.MapContext, in *krm.TrustConfig) *pb.TrustConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.TrustConfig{}
+	out.Name = direct.ValueOf(in.Name)
+	// MISSING: CreateTime
+	// MISSING: UpdateTime
+	out.Labels = in.Labels
+	out.Description = direct.ValueOf(in.Description)
+	out.Etag = direct.ValueOf(in.Etag)
+	out.TrustStores = direct.Slice_ToProto(mapCtx, in.TrustStores, TrustConfig_TrustStore_ToProto)
+	return out
+}
+func TrustConfigObservedState_FromProto(mapCtx *direct.MapContext, in *pb.TrustConfig) *krm.TrustConfigObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.TrustConfigObservedState{}
+	// MISSING: Name
+	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
+	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
+	// MISSING: Labels
+	// MISSING: Description
+	// MISSING: Etag
+	// MISSING: TrustStores
+	return out
+}
+func TrustConfigObservedState_ToProto(mapCtx *direct.MapContext, in *krm.TrustConfigObservedState) *pb.TrustConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.TrustConfig{}
+	// MISSING: Name
+	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
+	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
+	// MISSING: Labels
+	// MISSING: Description
+	// MISSING: Etag
+	// MISSING: TrustStores
+	return out
+}
+func TrustConfig_IntermediateCA_FromProto(mapCtx *direct.MapContext, in *pb.TrustConfig_IntermediateCA) *krm.TrustConfig_IntermediateCA {
+	if in == nil {
+		return nil
+	}
+	out := &krm.TrustConfig_IntermediateCA{}
+	out.PemCertificate = direct.LazyPtr(in.GetPemCertificate())
+	return out
+}
+func TrustConfig_IntermediateCA_ToProto(mapCtx *direct.MapContext, in *krm.TrustConfig_IntermediateCA) *pb.TrustConfig_IntermediateCA {
+	if in == nil {
+		return nil
+	}
+	out := &pb.TrustConfig_IntermediateCA{}
+	if oneof := TrustConfig_IntermediateCA_PemCertificate_ToProto(mapCtx, in.PemCertificate); oneof != nil {
+		out.Kind = oneof
+	}
+	return out
+}
+func TrustConfig_TrustAnchor_FromProto(mapCtx *direct.MapContext, in *pb.TrustConfig_TrustAnchor) *krm.TrustConfig_TrustAnchor {
+	if in == nil {
+		return nil
+	}
+	out := &krm.TrustConfig_TrustAnchor{}
+	out.PemCertificate = direct.LazyPtr(in.GetPemCertificate())
+	return out
+}
+func TrustConfig_TrustAnchor_ToProto(mapCtx *direct.MapContext, in *krm.TrustConfig_TrustAnchor) *pb.TrustConfig_TrustAnchor {
+	if in == nil {
+		return nil
+	}
+	out := &pb.TrustConfig_TrustAnchor{}
+	if oneof := TrustConfig_TrustAnchor_PemCertificate_ToProto(mapCtx, in.PemCertificate); oneof != nil {
+		out.Kind = oneof
+	}
+	return out
+}
+func TrustConfig_TrustStore_FromProto(mapCtx *direct.MapContext, in *pb.TrustConfig_TrustStore) *krm.TrustConfig_TrustStore {
+	if in == nil {
+		return nil
+	}
+	out := &krm.TrustConfig_TrustStore{}
+	out.TrustAnchors = direct.Slice_FromProto(mapCtx, in.TrustAnchors, TrustConfig_TrustAnchor_FromProto)
+	out.IntermediateCas = direct.Slice_FromProto(mapCtx, in.IntermediateCas, TrustConfig_IntermediateCA_FromProto)
+	return out
+}
+func TrustConfig_TrustStore_ToProto(mapCtx *direct.MapContext, in *krm.TrustConfig_TrustStore) *pb.TrustConfig_TrustStore {
+	if in == nil {
+		return nil
+	}
+	out := &pb.TrustConfig_TrustStore{}
+	out.TrustAnchors = direct.Slice_ToProto(mapCtx, in.TrustAnchors, TrustConfig_TrustAnchor_ToProto)
+	out.IntermediateCas = direct.Slice_ToProto(mapCtx, in.IntermediateCas, TrustConfig_IntermediateCA_ToProto)
 	return out
 }
