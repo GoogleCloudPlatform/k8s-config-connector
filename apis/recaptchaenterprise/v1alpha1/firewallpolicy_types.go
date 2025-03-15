49c85cfa6c5982a22867df615b1172e52f97ab9a
Joyce Ma <maqiuyu@google.com>
2025-03-15 10:34:26 +0000

    conductor: "Add parent to generated type. WARN: linting failed."

--- apis/recaptchaenterprise/v1alpha1/firewallpolicy_types.go
+++ apis/recaptchaenterprise/v1alpha1/firewallpolicy_types.go
@@ -15,15 +15,23 @@
 package v1alpha1
 
 import (
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/apis/k8s/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var ReCAPTCHAEnterpriseFirewallPolicyGVK = GroupVersion.WithKind("ReCAPTCHAEnterpriseFirewallPolicy")
 
+type Parent struct {
+	// +required
+	ProjectRef *refv1beta1.ProjectRef `json:"projectRef"`
+}
+
 // ReCAPTCHAEnterpriseFirewallPolicySpec defines the desired state of ReCAPTCHAEnterpriseFirewallPolicy
 // +kcc:proto=google.cloud.recaptchaenterprise.v1.FirewallPolicy
 type ReCAPTCHAEnterpriseFirewallPolicySpec struct {
+	Parent `json:",inline"`
+
 	// The ReCAPTCHAEnterpriseFirewallPolicy name. If not given, the metadata.name will be used.
 	ResourceID *string `json:"resourceID,omitempty"`
 
