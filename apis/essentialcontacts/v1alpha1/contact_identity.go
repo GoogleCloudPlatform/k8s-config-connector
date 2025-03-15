57cb5353592c76e4966e5023c4a6f7a123a2bc66
Barni S <barni@google.com>
2025-03-15 02:41:06 -0400

    conductor: "Adjust identity parent"

--- apis/essentialcontacts/v1alpha1/contact_identity.go
+++ apis/essentialcontacts/v1alpha1/contact_identity.go
@@ -43,28 +43,61 @@ func (i *ContactIdentity) Parent() *ContactParent {
 	return i.parent
 }
 
+// ContactParent defines the parent for the contact.
 type ContactParent struct {
-	ProjectID string
-	Location  string
+	ProjectID      string
+	OrganizationID string
+	FolderID       string
+	Location       string
 }
 
 func (p *ContactParent) String() string {
-	return "projects/" + p.ProjectID + "/locations/" + p.Location
+	if p.ProjectID != "" {
+		return "projects/" + p.ProjectID + "/locations/" + p.Location
+	} else if p.OrganizationID != "" {
+		return "organizations/" + p.OrganizationID + "/locations/" + p.Location
+	} else if p.FolderID != "" {
+		return "folders/" + p.FolderID + "/locations/" + p.Location
+	}
+	return ""
 }
 
 // New builds a ContactIdentity from the Config Connector Contact object.
 func NewContactIdentity(ctx context.Context, reader client.Reader, obj *EssentialContactsContact) (*ContactIdentity, error) {
 
 	// Get Parent
-	projectRef, err := refsv1beta1.ResolveProject(ctx, reader, obj.GetNamespace(), obj.Spec.ProjectRef)
-	if err != nil {
-		return nil, err
-	}
-	projectID := projectRef.ProjectID
-	if projectID == "" {
-		return nil, fmt.Errorf("cannot resolve project")
-	}
+	var projectID, organizationID, folderID string
 	location := obj.Spec.Location
+
+	if obj.Spec.ProjectRef != nil {
+		projectRef, err := refsv1beta1.ResolveProject(ctx, reader, obj.GetNamespace(), *obj.Spec.ProjectRef)
+		if err != nil {
+			return nil, err
+		}
+		projectID = projectRef.ProjectID
+		if projectID == "" {
+			return nil, fmt.Errorf("cannot resolve project")
+		}
+	} else if obj.Spec.OrganizationRef != nil {
+		organization, err := refsv1beta1.GetOrganization(ctx, obj.Spec.OrganizationRef.Name, obj.Spec.OrganizationRef.Namespace, reader)
+		if err != nil {
+			return nil, fmt.Errorf("error getting organization: %w", err)
+		}
+		organizationID = organization
+	} else if obj.Spec.FolderRef != nil {
+		if obj.Spec.FolderRef.External != "" {
+			folderID = obj.Spec.FolderRef.External
+		} else {
+			folder, err := refsv1beta1.GetFolderID(ctx, reader, obj.Spec.FolderRef.Name, obj.Namespace)
+			if err != nil {
+				return nil, err
+			}
+			folderID = folder
+		}
+		if folderID == "" {
+			return nil, fmt.Errorf("cannot resolve folder")
+		}
+	}
 
 	// Get desired ID
 	resourceID := common.ValueOf(obj.Spec.ResourceID)
@@ -83,8 +116,14 @@ func NewContactIdentity(ctx context.Context, reader client.Reader, obj *Essentia
 		if err != nil {
 			return nil, err
 		}
-		if actualParent.ProjectID != projectID {
-			return nil, fmt.Errorf("spec.projectRef changed, expect %s, got %s", actualParent.ProjectID, projectID)
+		if actualParent.ProjectID != projectID && actualParent.OrganizationID != organizationID && actualParent.FolderID != folderID {
+			if projectID != "" {
+				return nil, fmt.Errorf("spec.projectRef changed, expect %s, got %s", actualParent.ProjectID, projectID)
+			} else if organizationID != "" {
+				return nil, fmt.Errorf("spec.organizationRef changed, expect %s, got %s", actualParent.OrganizationID, organizationID)
+			} else if folderID != "" {
+				return nil, fmt.Errorf("spec.folderRef changed, expect %s, got %s", actualParent.FolderID, folderID)
+			}
 		}
 		if actualParent.Location != location {
 			return nil, fmt.Errorf("spec.location changed, expect %s, got %s", actualParent.Location, location)
@@ -96,8 +135,10 @@ func NewContactIdentity(ctx context.Context, reader client.Reader, obj *Essentia
 	}
 	return &ContactIdentity{
 		parent: &ContactParent{
-			ProjectID: projectID,
-			Location:  location,
+			ProjectID:      projectID,
+			OrganizationID: organizationID,
+			FolderID:       folderID,
+			Location:       location,
 		},
 		id: resourceID,
 	}, nil
@@ -105,13 +146,30 @@ func NewContactIdentity(ctx context.Context, reader client.Reader, obj *Essentia
 
 func ParseContactExternal(external string) (parent *ContactParent, resourceID string, err error) {
 	tokens := strings.Split(external, "/")
-	if len(tokens) != 6 || tokens[0] != "projects" || tokens[2] != "locations" || tokens[4] != "contacts" {
-		return nil, "", fmt.Errorf("format of EssentialContactsContact external=%q was not known (use projects/{{projectID}}/locations/{{location}}/contacts/{{contactID}})", external)
+	if len(tokens) == 6 && tokens[2] == "locations" && tokens[4] == "contacts" {
+		switch tokens[0] {
+		case "projects":
+			parent = &ContactParent{
+				ProjectID: tokens[1],
+				Location:  tokens[3],
+			}
+			resourceID = tokens[5]
+			return parent, resourceID, nil
+		case "organizations":
+			parent = &ContactParent{
+				OrganizationID: tokens[1],
+				Location:       tokens[3],
+			}
+			resourceID = tokens[5]
+			return parent, resourceID, nil
+		case "folders":
+			parent = &ContactParent{
+				FolderID: tokens[1],
+				Location: tokens[3],
+			}
+			resourceID = tokens[5]
+			return parent, resourceID, nil
+		}
 	}
-	parent = &ContactParent{
-		ProjectID: tokens[1],
-		Location:  tokens[3],
-	}
-	resourceID = tokens[5]
-	return parent, resourceID, nil
+	return nil, "", fmt.Errorf("format of EssentialContactsContact external=%q was not known (use projects/{{projectID}}/locations/{{location}}/contacts/{{contactID}} or organizations/{{organizationID}}/locations/{{location}}/contacts/{{contactID}} or folders/{{folderID}}/locations/{{location}}/contacts/{{contactID}})", external)
 }
