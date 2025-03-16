24f886b1b61da26b88517ef07c23af89429ef119
xiaoweim <xiaoweim@google.com>
2025-03-16 22:01:19 +0000

    resolve merge conflicts

--- apis/dataproc/v1alpha1/batch_identity.go
+++ apis/dataproc/v1alpha1/batch_identity.go
@@ -56,7 +56,7 @@ func (p *BatchParent) String() string {
 func NewBatchIdentity(ctx context.Context, reader client.Reader, obj *DataprocBatch) (*BatchIdentity, error) {
 
 	// Get Parent
-	projectRef, err := refsv1beta1.ResolveProject(ctx, reader, obj.GetNamespace(), obj.Spec.ProjectRef)
+	projectRef, err := refsv1beta1.ResolveProject(ctx, reader, obj.GetNamespace(), obj.Spec.Parent.ProjectRef)
 	if err != nil {
 		return nil, err
 	}
@@ -64,7 +64,7 @@ func NewBatchIdentity(ctx context.Context, reader client.Reader, obj *DataprocBa
 	if projectID == "" {
 		return nil, fmt.Errorf("cannot resolve project")
 	}
-	location := obj.Spec.Location
+	location := obj.Spec.Parent.Location
 
 	// Get desired ID
 	resourceID := common.ValueOf(obj.Spec.ResourceID)
