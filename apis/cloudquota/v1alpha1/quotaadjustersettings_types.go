a98bd8f82c389fc1824c4519be758b7735cba04a
Barni S <barni@google.com>
2025-03-16 12:40:20 -0400

    manual: fix import error

--- apis/cloudquota/v1alpha1/quotaadjustersettings_types.go
+++ apis/cloudquota/v1alpha1/quotaadjustersettings_types.go
@@ -15,9 +15,10 @@
 package v1alpha1
 
 import (
-	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
-	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/apis/k8s/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	refv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/apis/k8s/v1alpha1"
 )
 
 var APIQuotaAdjusterSettingsGVK = GroupVersion.WithKind("APIQuotaAdjusterSettings")
