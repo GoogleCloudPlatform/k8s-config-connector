0602e96df6334f4c9b8a3a5f0f522dc5ea4bc7d8
Barni S <barni@google.com>
2025-03-17 16:04:38 -0400

    conductor: "Add generated mock files"

---
+++ mockgcp/mockeventarc/googlechannelconfig.go
@@ -0,0 +1,123 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +tool:mockgcp-support
+// proto.service: google.cloud.eventarc.v1.Eventarc
+// proto.message: google.cloud.eventarc.v1.GoogleChannelConfig
+
+package mockeventarc
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/projects"
+	pb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/eventarc/v1"
+)
+
+func (s *EventarcV1) GetGoogleChannelConfig(ctx context.Context, req *pb.GetGoogleChannelConfigRequest) (*pb.GoogleChannelConfig, error) {
+	name, err := s.parseGoogleChannelConfigName(req.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	fqn := name.String()
+
+	obj := &pb.GoogleChannelConfig{}
+	if err := s.storage.Get(ctx, fqn, obj); err != nil {
+		if status.Code(err) == codes.NotFound {
+			return nil, status.Errorf(codes.NotFound, "GoogleChannelConfig %q not found", name)
+		}
+		return nil, err
+	}
+
+	return obj, nil
+}
+
+func (s *EventarcV1) UpdateGoogleChannelConfig(ctx context.Context, req *pb.UpdateGoogleChannelConfigRequest) (*pb.GoogleChannelConfig, error) {
+	reqName := req.GetGoogleChannelConfig().GetName()
+	name, err := s.parseGoogleChannelConfigName(reqName)
+	if err != nil {
+		return nil, err
+	}
+	fqn := name.String()
+
+	existing := &pb.GoogleChannelConfig{}
+	if err := s.storage.Get(ctx, fqn, existing); err != nil {
+		return nil, err
+	}
+
+	updated := proto.Clone(existing).(*pb.GoogleChannelConfig)
+
+	paths := req.GetUpdateMask().GetPaths()
+	if len(paths) == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "update_mask must be provided")
+	}
+
+	for _, path := range paths {
+		switch path {
+		case "cryptoKeyName":
+			updated.CryptoKeyName = req.GetGoogleChannelConfig().GetCryptoKeyName()
+		default:
+			return nil, status.Errorf(codes.InvalidArgument, "update_mask path %q not valid", path)
+		}
+	}
+	updated.UpdateTime = timestamppb.New(time.Now())
+
+	if err := s.storage.Update(ctx, fqn, updated); err != nil {
+		return nil, err
+	}
+	return updated, nil
+}
+
+type googleChannelConfigName struct {
+	Project  *projects.ProjectData
+	Location string
+}
+
+func (n *googleChannelConfigName) String() string {
+	return fmt.Sprintf("projects/%s/locations/%s/googleChannelConfig", n.Project.ID, n.Location)
+}
+
+// parseGoogleChannelConfigName parses a string into an googleChannelConfigName.
+// The expected form is `projects/*/locations/*/googleChannelConfig`.
+func (s *MockService) parseGoogleChannelConfigName(name string) (*googleChannelConfigName, error) {
+	tokens := strings.Split(name, "/")
+
+	if len(tokens) == 5 && tokens[0] == "projects" && tokens[2] == "locations" && tokens[4] == "googleChannelConfig" {
+		project, err := s.Projects.GetProjectByID(tokens[1])
+		if err != nil {
+			return nil, err
+		}
+
+		name := &googleChannelConfigName{
+			Project:  project,
+			Location: tokens[3],
+		}
+
+		return name, nil
+	}
+	return nil, status.Errorf(codes.InvalidArgument, "name %q is not valid", name)
+}
+```
+</out>
+
+
