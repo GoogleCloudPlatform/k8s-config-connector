7db4d506036f721359b137504b99723071c23280
Alex Pana <8968914+acpana@users.noreply.github.com>
2025-03-17 21:50:54 +0000

    "Adding service to mock_http_roundtrip.go for netapp-backupvault"

--- mockgcp/mock_http_roundtrip.go
+++ mockgcp/mock_http_roundtrip.go
@@ -106,6 +106,7 @@ import (
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/mockworkflows"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/mockworkstations"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/pkg/storage"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/mocknetapp"
 )
 
 type mockRoundTripper struct {
@@ -253,6 +254,7 @@ func NewMockRoundTripper(ctx context.Context, k8sClient client.Client, storage s
 	services = append(services, mockbackupdr.New(env, storage))
 	services = append(services, mockbatch.New(env, storage))
 	services = append(services, mockapigateway.New(env, storage))
+	services = append(services, mocknetapp.New(env, storage))
 
 	for _, service := range services {
 		service.Register(server)
