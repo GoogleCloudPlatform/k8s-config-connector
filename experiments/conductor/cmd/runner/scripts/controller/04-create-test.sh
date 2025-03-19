1cba58f54649a63be52375d1ebebfb39c4c6ab04
Jingyi Hu <jingyih@google.com>
2025-03-19 00:52:20 +0000

    conductor: minor adjustment to test generation

--- experiments/conductor/cmd/runner/scripts/controller/04-create-test.sh
+++ experiments/conductor/cmd/runner/scripts/controller/04-create-test.sh
@@ -43,6 +43,20 @@ TESTDIR=pkg/test/resourcefixture/testdata/basic/${SERVICE}/v1alpha1/${CRD_KIND,,
 mkdir -p ${TESTDIR}
 
 cat <<EOF > ${TESTDIR}/create.yaml
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 apiVersion: ${CRD_GROUP}/${CRD_VERSION}
 kind: ${CRD_KIND}
 metadata:
@@ -50,11 +64,25 @@ metadata:
 spec:
   projectRef:
     external: \${projectId}
-  locationID: us-west2
+  location: us-central1
   description: "Initial description"
 EOF
 
 cat <<EOF > ${TESTDIR}/update.yaml
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License a
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 apiVersion: ${CRD_GROUP}/${CRD_VERSION}
 kind: ${CRD_KIND}
 metadata:
@@ -62,7 +90,7 @@ metadata:
 spec:
   projectRef:
     external: \${projectId}
-  locationID: us-west2
+  location: us-central1
   description: "Updated description"
 EOF
 
