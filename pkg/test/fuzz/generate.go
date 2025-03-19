ee6fd4e10475b853e6f7a1142fbbc5841317a770
xiaoweim <xiaoweim@google.com>
2025-03-19 03:07:40 +0000

    Add required type handling to fix fuzz test

--- pkg/test/fuzz/generate.go
+++ pkg/test/fuzz/generate.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"math/rand"
 	"testing"
+	"time"
 
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
@@ -138,6 +139,14 @@ func fillWithRandom0(t *testing.T, randStream *rand.Rand, msg protoreflect.Messa
 						el := mapVal.Mutable(protoreflect.ValueOf(k).MapKey())
 						fillWithRandom0(t, randStream, el.Message())
 					}
+				}
+			case "string->int64":
+				mapVal := msg.Mutable(field).Map()
+				for j := 0; j < count; j++ {
+					k := randomString(randStream)
+					rand.Seed(time.Now().UnixNano())
+					v := rand.Int63()
+					mapVal.Set(protoreflect.ValueOf(k).MapKey(), protoreflect.ValueOf(v))
 				}
 
 			default:
