87e69c513cd114e74f893bbd7c48a508b284b677
Jingyi Hu <jingyih@google.com>
2025-03-19 01:24:19 +0000

    Revert "chore: remove duplicated resource"
    This reverts commit 7f9aa44f321e3c1245220a4c9fff8a39300d7a77.

--- pkg/gvks/supportedgvks/gvks_generated.go
+++ pkg/gvks/supportedgvks/gvks_generated.go
@@ -1125,6 +1125,18 @@ var SupportedGVKs = map[schema.GroupVersionKind]GVKMetadata{
 		},
 	},
 	{
+		Group:   "cloudtasks.cnrm.cloud.google.com",
+		Version: "v1alpha1",
+		Kind:    "CloudTasksQueue",
+	}: {
+		Labels: map[string]string{
+			"cnrm.cloud.google.com/managed-by-kcc":  "true",
+			"cnrm.cloud.google.com/stability-level": "alpha",
+			"cnrm.cloud.google.com/system":          "true",
+			"cnrm.cloud.google.com/tf2crd":          "true",
+		},
+	},
+	{
 		Group:   "colab.cnrm.cloud.google.com",
 		Version: "v1alpha1",
 		Kind:    "ColabRuntimeTemplate",
