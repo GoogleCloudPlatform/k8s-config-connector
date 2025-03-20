103716fb6b990afa382ca4a4e1993b6845f016fc
xiaoweim <xiaoweim@google.com>
2025-03-20 07:03:19 +0000

    make ready-pr

--- pkg/gvks/supportedgvks/gvks_generated.go
+++ pkg/gvks/supportedgvks/gvks_generated.go
@@ -468,6 +468,16 @@ var SupportedGVKs = map[schema.GroupVersionKind]GVKMetadata{
 	{
 		Group:   "apphub.cnrm.cloud.google.com",
 		Version: "v1alpha1",
+		Kind:    "AppHubDiscoveredService",
+	}: {
+		Labels: map[string]string{
+			"cnrm.cloud.google.com/managed-by-kcc": "true",
+			"cnrm.cloud.google.com/system":         "true",
+		},
+	},
+	{
+		Group:   "apphub.cnrm.cloud.google.com",
+		Version: "v1alpha1",
 		Kind:    "AppHubDiscoveredWorkload",
 	}: {
 		Labels: map[string]string{
