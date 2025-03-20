1be022e682568791aead7b8fe6d0296c057651f2
Alex Pana <8968914+acpana@users.noreply.github.com>
2025-03-20 03:09:58 +0000

    conductor: "Add spec and status to generated type"

--- apis/deploy/v1alpha1/target_types.go
+++ apis/deploy/v1alpha1/target_types.go
@@ -24,8 +24,89 @@ var DeployTargetGVK = GroupVersion.WithKind("DeployTarget")
 // DeployTargetSpec defines the desired state of DeployTarget
 // +kcc:proto=google.cloud.deploy.v1.Target
 type DeployTargetSpec struct {
+	//ProjectId:"!{{project}}"
+	ProjectRef *v1alpha1.ResourceRef `json:"projectRef"`
+
+	Location *string `json:"location"`
+
 	// The DeployTarget name. If not given, the metadata.name will be used.
 	ResourceID *string `json:"resourceID,omitempty"`
+	// Optional. Name of the `Target`. Format is
+	//  `projects/{project}/locations/{location}/targets/{target}`.
+	//  The `target` component must match `[a-z]([a-z0-9-]{0,61}[a-z0-9])?`
+	// +kcc:proto:field=google.cloud.deploy.v1.Target.name
+	Name *string `json:"name,omitempty"`
+
+	// Optional. Description of the `Target`. Max length is 255 characters.
+	// +kcc:proto:field=google.cloud.deploy.v1.Target.description
+	Description *string `json:"description,omitempty"`
+
+	// Optional. User annotations. These attributes can only be set and used by
+	//  the user, and not by Cloud Deploy. See
+	//  https://google.aip.dev/128#annotations for more details such as format and
+	//  size limitations.
+	// +kcc:proto:field=google.cloud.deploy.v1.Target.annotations
+	Annotations map[string]string `json:"annotations,omitempty"`
+
+	// Optional. Labels are attributes that can be set and used by both the
+	//  user and by Cloud Deploy. Labels must meet the following constraints:
+	//
+	//  * Keys and values can contain only lowercase letters, numeric characters,
+	//  underscores, and dashes.
+	//  * All characters must use UTF-8 encoding, and international characters are
+	//  allowed.
+	//  * Keys must start with a lowercase letter or international character.
+	//  * Each resource is limited to a maximum of 64 labels.
+	//
+	//  Both keys and values are additionally constrained to be <= 128 bytes.
+	// +kcc:proto:field=google.cloud.deploy.v1.Target.labels
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// Optional. Whether or not the `Target` requires approval.
+	// +kcc:proto:field=google.cloud.deploy.v1.Target.require_approval
+	RequireApproval *bool `json:"requireApproval,omitempty"`
+
+	// Optional. Information specifying a GKE Cluster.
+	// +kcc:proto:field=google.cloud.deploy.v1.Target.gke
+	Gke *GkeCluster `json:"gke,omitempty"`
+
+	// Optional. Information specifying an Anthos Cluster.
+	// +kcc:proto:field=google.cloud.deploy.v1.Target.anthos_cluster
+	AnthosCluster *AnthosCluster `json:"anthosCluster,omitempty"`
+
+	// Optional. Information specifying a Cloud Run deployment target.
+	// +kcc:proto:field=google.cloud.deploy.v1.Target.run
+	Run *CloudRunLocation `json:"run,omitempty"`
+
+	// Optional. Information specifying a multiTarget.
+	// +kcc:proto:field=google.cloud.deploy.v1.Target.multi_target
+	MultiTarget *MultiTarget `json:"multiTarget,omitempty"`
+
+	// Optional. Information specifying a Custom Target.
+	// +kcc:proto:field=google.cloud.deploy.v1.Target.custom_target
+	CustomTarget *CustomTarget `json:"customTarget,omitempty"`
+
+	// TODO: unsupported map type with key string and value message
+
+	// Optional. This checksum is computed by the server based on the value of
+	//  other fields, and may be sent on update and delete requests to ensure the
+	//  client has an up-to-date value before proceeding.
+	// +kcc:proto:field=google.cloud.deploy.v1.Target.etag
+	Etag *string `json:"etag,omitempty"`
+
+	// Configurations for all execution that relates to this `Target`.
+	//  Each `ExecutionEnvironmentUsage` value may only be used in a single
+	//  configuration; using the same value multiple times is an error.
+	//  When one or more configurations are specified, they must include the
+	//  `RENDER` and `DEPLOY` `ExecutionEnvironmentUsage` values.
+	//  When no configurations are specified, execution will use the default
+	//  specified in `DefaultPool`.
+	// +kcc:proto:field=google.cloud.deploy.v1.Target.execution_configs
+	ExecutionConfigs []ExecutionConfig `json:"executionConfigs,omitempty"`
+
+	// Optional. The deploy parameters to use for this target.
+	// +kcc:proto:field=google.cloud.deploy.v1.Target.deploy_parameters
+	DeployParameters map[string]string `json:"deployParameters,omitempty"`
 }
 
 // DeployTargetStatus defines the config connector machine state of DeployTarget
@@ -47,6 +128,21 @@ type DeployTargetStatus struct {
 // DeployTargetObservedState is the state of the DeployTarget resource as most recently observed in GCP.
 // +kcc:proto=google.cloud.deploy.v1.Target
 type DeployTargetObservedState struct {
+	// Output only. Resource id of the `Target`.
+	// +kcc:proto:field=google.cloud.deploy.v1.Target.target_id
+	TargetID *string `json:"targetID,omitempty"`
+
+	// Output only. Unique identifier of the `Target`.
+	// +kcc:proto:field=google.cloud.deploy.v1.Target.uid
+	Uid *string `json:"uid,omitempty"`
+
+	// Output only. Time at which the `Target` was created.
+	// +kcc:proto:field=google.cloud.deploy.v1.Target.create_time
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// Output only. Most recent time at which the `Target` was updated.
+	// +kcc:proto:field=google.cloud.deploy.v1.Target.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
 }
 
 // +genclient
