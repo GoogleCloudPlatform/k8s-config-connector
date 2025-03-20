467c15e2c3659f7fc9e5f8076c66eb619fc64a61
Alex Pana <8968914+acpana@users.noreply.github.com>
2025-03-20 03:27:12 +0000

    conductor: "Adjust identity parent. WARN: linting failed.. WARN: extra files found, committing them. "

--- apis/deploy/v1alpha1/target_types.go
+++ apis/deploy/v1alpha1/target_types.go
@@ -15,9 +15,226 @@
 package v1alpha1
 
 import (
-	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+        "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+        metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+
+
+// DeployTargetSpec defines the desired state of DeployTarget
+// +kcc:proto=google.cloud.deploy.v1.Target
+
+
+// DeployTargetSpec defines the desired state of DeployTarget
+// +kcc:proto=google.cloud.deploy.v1.Target
+type Parent struct {
+        // +required
+        ProjectRef *refv1beta1.ProjectRef `json:"projectRef"`
+        // +required
+        Location string `json:"location"`
+}
+
+import (
+        "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+)
+
+var DeployTargetGVK = GroupVersion.WithKind("DeployTarget")
+
+type Parent struct {
+        // +required
+        ProjectRef *refv1beta1.ProjectRef `json:"projectRef"`
+        // +required
+        Location string `json:"location"`
+}
+
+// DeployTargetSpec defines the desired state of DeployTarget
+// +kcc:proto=google.cloud.deploy.v1.Target
+type Parent struct {
+        // +required
+        Location string `json:"location"`
+        // +optional
+        ProjectRef *refv1beta1.ProjectRef `json:"projectRef,omitempty"`
+}
+
+// +k8s:openapi-gen=true
+type DeployTargetSpec struct {
+        Parent `json:",inline"`
+        // +required
+        Parent `json:",inline"`
+        // +required
+        Parent `json:",inline"`
+
+
+        //ProjectId:"!{{project}}"
+
+        // The DeployTarget name. If not given, the metadata.name will be used.
+        // +optional
+        ResourceID *string `json:"resourceID,omitempty"`
+        // Optional. Name of the `Target`. Format is
+        //  `projects/{project}/locations/{location}/targets/{target}`.
+        //  The `target` component must match `[a-z]([a-z0-9-]{0,61}[a-z0-9])?`
+        // +kcc:proto:field=google.cloud.deploy.v1.Target.name
+        Name *string `json:"name,omitempty"`
+
+        // Optional. Description of the `Target`. Max length is 255 characters.
+        // +kcc:proto:field=google.cloud.deploy.v1.Target.description
+        Description *string `json:",inline"`
+
+        // Optional. User annotations. These attributes can only be set and used by
+        //  the user, and not by Cloud Deploy. See
+        //  https://google.aip.dev/128#annotations for more details such as format and
+        //  size limitations.
+        // +kcc:proto:field=google.cloud.deploy.v1.Target.annotations
+        Annotations map[string]string `json:"annotations,omitempty"`
+
+        // Optional. Labels are attributes that can be set and used by both the
+        //  user and by Cloud Deploy. Labels must meet the following constraints:
+        //
+        //  * Keys and values can contain only lowercase letters, numeric characters,
+        //  underscores, and dashes.
+        //  * All characters must use UTF-8 encoding, and international characters are
+        //  allowed.
+        //  * Keys must start with a lowercase letter or international character.
+        //  * Each resource is limited to a maximum of 64 labels.
+        //
+        //  Both keys and values are additionally constrained to be <= 128 bytes.
+        // +kcc:proto:field=google.cloud.deploy.v1.Target.labels
+        Labels map[string]string `json:"labels,omitempty"`
+
+        // Optional. Whether or not the `Target` requires approval.
+        // +kcc:proto:field=google.cloud.deploy.v1.Target.require_approval
+        RequireApproval *bool `json:"requireApproval,omitempty"`
+
+        // Optional. Information specifying a GKE Cluster.
+        // +kcc:proto:field=google.cloud.deploy.v1.Target.gke
+        Gke *GkeCluster `json:"gke,omitempty"`
+
+        // Optional. Information specifying an Anthos Cluster.
+        // +kcc:proto:field=google.cloud.deploy.v1.Target.anthos_cluster
+        AnthosCluster *AnthosCluster `json:"anthosCluster,omitempty"`
+
+        // Optional. Information specifying a Cloud Run deployment target.
+        // +kcc:proto:field=google.cloud.deploy.v1.Target.run
+        Run *CloudRunLocation `json:",inline"`
+
+        // Optional. Information specifying a multiTarget.
+        // +kcc:proto:field=google.cloud.deploy.v1.Target.multi_target
+        MultiTarget *MultiTarget `json:"multiTarget,omitempty"`
+
+        // Optional. Information specifying a Custom Target.
+        // +kcc:proto:field=google.cloud.deploy.v1.Target.custom_target
+        CustomTarget *CustomTarget `json:"customTarget,omitempty"`
+
+        // TODO: unsupported map type with key string and value message
+
+        // Optional. This checksum is computed by the server based on the value of
+        //  other fields, and may be sent on update and delete requests to ensure the
+        //  client has an up-to-date value before proceeding.
+        // +kcc:proto:field=google.cloud.deploy.v1.Target.etag
+        Etag *string `json:",inline"`
+
+        // Configurations for all execution that relates to this `Target`.\n
+        //  Each `ExecutionEnvironmentUsage` value may only be used in a single
+        //  configuration; using the same value multiple times is an error.\n
+        //  When one or more configurations are specified, they must include the
+        //  `RENDER` and `DEPLOY` `ExecutionEnvironmentUsage` values.\n
+        //  When no configurations are specified, execution will use the default
+        //  specified in `DefaultPool`.\n
+        // +kcc:proto:field=google.cloud.deploy.v1.Target.execution_configs
+        ExecutionConfigs []ExecutionConfig `json:"executionConfigs,omitempty"`
+
+        // Optional. The deploy parameters to use for this target.
+        // +kcc:proto:field=google.cloud.deploy.v1.Target.deploy_parameters
+        DeployParameters map[string]string `json:"deployParameters,omitempty"`
+}
+
+// +k8s:openapi-gen=true
+type TargetStatus struct {
+        // Conditions represent the latest available observations of the
+        // Target's current state.
+        // +optional
+        Conditions []metav1.Condition `json:"conditions,omitempty"`
+type DeployTargetStatus struct {
+        /* Conditions represent the latest available observations of the
+           object's current state. */
+        Conditions []v1alpha1.Condition `json:"conditions,omitempty"`
+
+        // ObservedGeneration is the generation of the resource that was most recently observed by the Config Connector controller. If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource.
+        ObservedGeneration *int64 `json:"observedGeneration,omitempty"`
+
+        // A unique specifier for the DeployTarget resource in GCP.
+        ExternalRef *string `json:"externalRef,omitempty"`
+
+        // ObservedState is the state of the resource as most recently observed in GCP.
+        ObservedState *DeployTargetObservedState `json:"observedState,omitempty"`
+}
+
+// DeployTargetObservedState is the state of the DeployTarget resource as most recently observed in GCP.
+// +kcc:proto=google.cloud.deploy.v1.Target
+type DeployTargetObservedState struct {
+        // Output only. Resource id of the `Target`.
+        // +kcc:proto:field=google.cloud.deploy.v1.Target.target_id
+        TargetID *string `json:"targetID,omitempty"`
+
+        // Output only. Unique identifier of the `Target`.
+        // +kcc:proto:field=google.cloud.deploy.v1.Target.uid
+        Uid *string `json:"uid,omitempty"`
+
+        // Output only. Time at which the `Target` was created.
+        // +kcc:proto:field=google.cloud.deploy.v1.Target.create_time
+        CreateTime *string `json:"createTime,omitempty"`
+
+        // Output only. Most recent time at which the `Target` was updated.
+        // +kcc:proto:field=google.cloud.deploy.v1.Target.update_time
+        UpdateTime *string `json:"updateTime,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// TargetList contains a list of Target
+type TargetList struct {
+        metav1.TypeMeta `json:",inline"`
+        metav1.ListMeta `json:"metadata,omitempty"`
+        Items           []Target `json:"items"`
+}
+
+func init() {
+        SchemeBuilder.Register(&Target{}, &TargetList{})
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// TODO(user): make sure the pluralizaiton below is correct
+// +kubebuilder:resource:categories=gcp,shortName=gcpdeploytarget;gcpdeploytargets
+// +kubebuilder:subresource:status
+// +kubebuilder:metadata:labels="cnrm.cloud.google.com/managed-by-kcc=true";"cnrm.cloud.google.com/system=true"
+// +kubebuilder:printcolumn:name="Age",JSONPath=".metadata.creationTimestamp",type="date"
+// +kubebuilder:printcolumn:name="Ready",JSONPath=".status.conditions[?(@.type=='Ready')].status",type="string",description="When 'True', the most recent reconcile of the resource succeeded"
+// +kubebuilder:printcolumn:name="Status",JSONPath=".status.conditions[?(@.type=='Ready')].reason",type="string",description="The reason for the value in 'Ready'"
+// +kubebuilder:printcolumn:name="Status Age",JSONPath=".status.conditions[?(@.type=='Ready')].lastTransitionTime",type="date",description="The last transition time for the value in 'Status'"
+
+// DeployTarget is the Schema for the DeployTarget API
+// +k8s:openapi-gen=true
+type DeployTarget struct {
+        metav1.TypeMeta   `json:",inline"`
+        metav1.ObjectMeta `json:"metadata,omitempty"`
+
+        Spec   DeployTargetSpec   `json:",inline"`
+        Status TargetStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// DeployTargetList contains a list of DeployTarget
+type DeployTargetList struct {
+        metav1.TypeMeta `json:",inline"`
+        metav1.ListMeta `json:"metadata,omitempty"`
+        Items           []DeployTarget `json:"items"`
+}
+
+func init() {
+        SchemeBuilder.Register(&DeployTarget{}, &DeployTargetList{})
+}
+
 
 
 
