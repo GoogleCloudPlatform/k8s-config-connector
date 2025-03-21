be95a514947c3b0913c56fe8205b68e375867942
Jingyi Hu <jingyih@google.com>
2025-03-21 05:33:55 +0000

    DatastreamConnectionProfile: fix mappers from generated

---
+++ pkg/controller/direct/datastream/connectionprofile_mappers.go
@@ -0,0 +1,192 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package datastream
+
+import (
+	pb "cloud.google.com/go/datastream/apiv1/datastreampb"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/datastream/v1alpha1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+)
+
+func DatastreamConnectionProfileSpec_FromProto(mapCtx *direct.MapContext, in *pb.ConnectionProfile) *krm.DatastreamConnectionProfileSpec {
+	if in == nil {
+		return nil
+	}
+	out := &krm.DatastreamConnectionProfileSpec{}
+	// MISSING: Name
+	out.Labels = in.Labels
+	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
+	out.OracleProfile = OracleProfile_FromProto(mapCtx, in.GetOracleProfile())
+	out.GCSProfile = GCSProfile_FromProto(mapCtx, in.GetGcsProfile())
+	out.MySQLProfile = MysqlProfile_FromProto(mapCtx, in.GetMysqlProfile())
+	out.BigQueryProfile = BigQueryProfile_FromProto(mapCtx, in.GetBigqueryProfile())
+	out.SQLServerProfile = SQLServerProfile_FromProto(mapCtx, in.GetSqlServerProfile())
+	out.StaticServiceIPConnectivity = StaticServiceIPConnectivity_FromProto(mapCtx, in.GetStaticServiceIpConnectivity())
+	out.ForwardSSHConnectivity = ForwardSSHTunnelConnectivity_FromProto(mapCtx, in.GetForwardSshConnectivity())
+	out.PrivateConnectivity = PrivateConnectivity_FromProto(mapCtx, in.GetPrivateConnectivity())
+	return out
+}
+func DatastreamConnectionProfileSpec_ToProto(mapCtx *direct.MapContext, in *krm.DatastreamConnectionProfileSpec) *pb.ConnectionProfile {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ConnectionProfile{}
+	// MISSING: Name
+	out.Labels = in.Labels
+	out.DisplayName = direct.ValueOf(in.DisplayName)
+	if oneof := OracleProfile_ToProto(mapCtx, in.OracleProfile); oneof != nil {
+		out.Profile = &pb.ConnectionProfile_OracleProfile{OracleProfile: oneof}
+	}
+	if oneof := GCSProfile_ToProto(mapCtx, in.GCSProfile); oneof != nil {
+		out.Profile = &pb.ConnectionProfile_GcsProfile{GcsProfile: oneof}
+	}
+	if oneof := MysqlProfile_ToProto(mapCtx, in.MySQLProfile); oneof != nil {
+		out.Profile = &pb.ConnectionProfile_MysqlProfile{MysqlProfile: oneof}
+	}
+	if oneof := BigQueryProfile_ToProto(mapCtx, in.BigQueryProfile); oneof != nil {
+		out.Profile = &pb.ConnectionProfile_BigqueryProfile{BigqueryProfile: oneof}
+	}
+	if oneof := SQLServerProfile_ToProto(mapCtx, in.SQLServerProfile); oneof != nil {
+		out.Profile = &pb.ConnectionProfile_SqlServerProfile{SqlServerProfile: oneof}
+	}
+	if oneof := StaticServiceIPConnectivity_ToProto(mapCtx, in.StaticServiceIPConnectivity); oneof != nil {
+		out.Connectivity = &pb.ConnectionProfile_StaticServiceIpConnectivity{StaticServiceIpConnectivity: oneof}
+	}
+	if oneof := ForwardSSHTunnelConnectivity_ToProto(mapCtx, in.ForwardSSHConnectivity); oneof != nil {
+		out.Connectivity = &pb.ConnectionProfile_ForwardSshConnectivity{ForwardSshConnectivity: oneof}
+	}
+	if oneof := PrivateConnectivity_ToProto(mapCtx, in.PrivateConnectivity); oneof != nil {
+		out.Connectivity = &pb.ConnectionProfile_PrivateConnectivity{PrivateConnectivity: oneof}
+	}
+	return out
+}
+func DatastreamConnectionProfileObservedState_FromProto(mapCtx *direct.MapContext, in *pb.ConnectionProfile) *krm.DatastreamConnectionProfileObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.DatastreamConnectionProfileObservedState{}
+	// MISSING: Name
+	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
+	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
+	out.OracleProfile = OracleProfileObservedState_FromProto(mapCtx, in.GetOracleProfile())
+	out.MySQLProfile = MysqlProfileObservedState_FromProto(mapCtx, in.GetMysqlProfile())
+	// MISSING: BigqueryProfile
+	// MISSING: PostgresqlProfile
+	return out
+}
+func DatastreamConnectionProfileObservedState_ToProto(mapCtx *direct.MapContext, in *krm.DatastreamConnectionProfileObservedState) *pb.ConnectionProfile {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ConnectionProfile{}
+	// MISSING: Name
+	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
+	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
+	if oneof := OracleProfileObservedState_ToProto(mapCtx, in.OracleProfile); oneof != nil {
+		out.Profile = &pb.ConnectionProfile_OracleProfile{OracleProfile: oneof}
+	}
+	if oneof := MysqlProfileObservedState_ToProto(mapCtx, in.MySQLProfile); oneof != nil {
+		out.Profile = &pb.ConnectionProfile_MysqlProfile{MysqlProfile: oneof}
+	}
+	// MISSING: BigqueryProfile
+	// MISSING: PostgresqlProfile
+	return out
+}
+func OracleAsmConfig_FromProto(mapCtx *direct.MapContext, in *pb.OracleAsmConfig) *krm.OracleAsmConfig {
+	if in == nil {
+		return nil
+	}
+	out := &krm.OracleAsmConfig{}
+	out.Hostname = direct.LazyPtr(in.GetHostname())
+	out.Port = direct.LazyPtr(in.GetPort())
+	out.Username = direct.LazyPtr(in.GetUsername())
+	out.Password = direct.LazyPtr(in.GetPassword())
+	out.ASMService = direct.LazyPtr(in.GetAsmService())
+	out.ConnectionAttributes = in.ConnectionAttributes
+	out.OracleSSLConfig = OracleSSLConfig_FromProto(mapCtx, in.GetOracleSslConfig())
+	return out
+}
+func OracleAsmConfig_ToProto(mapCtx *direct.MapContext, in *krm.OracleAsmConfig) *pb.OracleAsmConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.OracleAsmConfig{}
+	out.Hostname = direct.ValueOf(in.Hostname)
+	out.Port = direct.ValueOf(in.Port)
+	out.Username = direct.ValueOf(in.Username)
+	out.Password = direct.ValueOf(in.Password)
+	out.AsmService = direct.ValueOf(in.ASMService)
+	out.ConnectionAttributes = in.ConnectionAttributes
+	out.OracleSslConfig = OracleSSLConfig_ToProto(mapCtx, in.OracleSSLConfig)
+	return out
+}
+func OracleProfile_FromProto(mapCtx *direct.MapContext, in *pb.OracleProfile) *krm.OracleProfile {
+	if in == nil {
+		return nil
+	}
+	out := &krm.OracleProfile{}
+	out.Hostname = direct.LazyPtr(in.GetHostname())
+	out.Port = direct.LazyPtr(in.GetPort())
+	out.Username = direct.LazyPtr(in.GetUsername())
+	out.Password = direct.LazyPtr(in.GetPassword())
+	out.DatabaseService = direct.LazyPtr(in.GetDatabaseService())
+	out.ConnectionAttributes = in.ConnectionAttributes
+	out.OracleSSLConfig = OracleSSLConfig_FromProto(mapCtx, in.GetOracleSslConfig())
+	out.OracleASMConfig = OracleAsmConfig_FromProto(mapCtx, in.GetOracleAsmConfig())
+	out.SecretManagerStoredPassword = direct.LazyPtr(in.GetSecretManagerStoredPassword())
+	return out
+}
+func OracleProfile_ToProto(mapCtx *direct.MapContext, in *krm.OracleProfile) *pb.OracleProfile {
+	if in == nil {
+		return nil
+	}
+	out := &pb.OracleProfile{}
+	out.Hostname = direct.ValueOf(in.Hostname)
+	out.Port = direct.ValueOf(in.Port)
+	out.Username = direct.ValueOf(in.Username)
+	out.Password = direct.ValueOf(in.Password)
+	out.DatabaseService = direct.ValueOf(in.DatabaseService)
+	out.ConnectionAttributes = in.ConnectionAttributes
+	out.OracleSslConfig = OracleSSLConfig_ToProto(mapCtx, in.OracleSSLConfig)
+	out.OracleAsmConfig = OracleAsmConfig_ToProto(mapCtx, in.OracleASMConfig)
+	out.SecretManagerStoredPassword = direct.ValueOf(in.SecretManagerStoredPassword)
+	return out
+}
+func PrivateConnectivity_FromProto(mapCtx *direct.MapContext, in *pb.PrivateConnectivity) *krm.PrivateConnectivity {
+	if in == nil {
+		return nil
+	}
+	out := &krm.PrivateConnectivity{}
+	if in.GetPrivateConnection() != "" {
+		out.PrivateConnectionRef = &krm.PrivateConnectionRef{External: in.GetPrivateConnection()}
+	}
+	return out
+}
+func ForwardSSHTunnelConnectivity_Password_ToProto(mapCtx *direct.MapContext, in *string) *pb.ForwardSshTunnelConnectivity_Password {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ForwardSshTunnelConnectivity_Password{}
+	out.Password = direct.ValueOf(in)
+	return out
+}
+func ForwardSSHTunnelConnectivity_PrivateKey_ToProto(mapCtx *direct.MapContext, in *string) *pb.ForwardSshTunnelConnectivity_PrivateKey {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ForwardSshTunnelConnectivity_PrivateKey{}
+	out.PrivateKey = direct.ValueOf(in)
+	return out
+}
--- pkg/controller/direct/datastream/mapper.generated.go
+++ pkg/controller/direct/datastream/mapper.generated.go
@@ -12,6 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +generated:mapper
+// krm.group: datastream.cnrm.cloud.google.com
+// krm.version: v1alpha1
+// proto.service: google.cloud.datastream.v1
+
 package datastream
 
 import (
@@ -20,6 +25,20 @@ import (
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 )
 
+func BigQueryProfile_FromProto(mapCtx *direct.MapContext, in *pb.BigQueryProfile) *krm.BigQueryProfile {
+	if in == nil {
+		return nil
+	}
+	out := &krm.BigQueryProfile{}
+	return out
+}
+func BigQueryProfile_ToProto(mapCtx *direct.MapContext, in *krm.BigQueryProfile) *pb.BigQueryProfile {
+	if in == nil {
+		return nil
+	}
+	out := &pb.BigQueryProfile{}
+	return out
+}
 func DatastreamPrivateConnectionObservedState_FromProto(mapCtx *direct.MapContext, in *pb.PrivateConnection) *krm.DatastreamPrivateConnectionObservedState {
 	if in == nil {
 		return nil
@@ -44,5 +63,291 @@ func DatastreamPrivateConnectionObservedState_ToProto(mapCtx *direct.MapContext,
 	out.State = direct.Enum_ToProto[pb.PrivateConnection_State](mapCtx, in.State)
 	out.Error = Error_ToProto(mapCtx, in.Error)
 	// MISSING: VpcPeeringConfig
+	return out
+}
+func ForwardSSHTunnelConnectivity_FromProto(mapCtx *direct.MapContext, in *pb.ForwardSshTunnelConnectivity) *krm.ForwardSSHTunnelConnectivity {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ForwardSSHTunnelConnectivity{}
+	out.Hostname = direct.LazyPtr(in.GetHostname())
+	out.Username = direct.LazyPtr(in.GetUsername())
+	out.Port = direct.LazyPtr(in.GetPort())
+	out.Password = direct.LazyPtr(in.GetPassword())
+	out.PrivateKey = direct.LazyPtr(in.GetPrivateKey())
+	return out
+}
+func ForwardSSHTunnelConnectivity_ToProto(mapCtx *direct.MapContext, in *krm.ForwardSSHTunnelConnectivity) *pb.ForwardSshTunnelConnectivity {
+	if in == nil {
+		return nil
+	}
+	out := &pb.ForwardSshTunnelConnectivity{}
+	out.Hostname = direct.ValueOf(in.Hostname)
+	out.Username = direct.ValueOf(in.Username)
+	out.Port = direct.ValueOf(in.Port)
+	if oneof := ForwardSSHTunnelConnectivity_Password_ToProto(mapCtx, in.Password); oneof != nil {
+		out.AuthenticationMethod = oneof
+	}
+	if oneof := ForwardSSHTunnelConnectivity_PrivateKey_ToProto(mapCtx, in.PrivateKey); oneof != nil {
+		out.AuthenticationMethod = oneof
+	}
+	return out
+}
+func GCSProfile_FromProto(mapCtx *direct.MapContext, in *pb.GcsProfile) *krm.GCSProfile {
+	if in == nil {
+		return nil
+	}
+	out := &krm.GCSProfile{}
+	out.Bucket = direct.LazyPtr(in.GetBucket())
+	out.RootPath = direct.LazyPtr(in.GetRootPath())
+	return out
+}
+func GCSProfile_ToProto(mapCtx *direct.MapContext, in *krm.GCSProfile) *pb.GcsProfile {
+	if in == nil {
+		return nil
+	}
+	out := &pb.GcsProfile{}
+	out.Bucket = direct.ValueOf(in.Bucket)
+	out.RootPath = direct.ValueOf(in.RootPath)
+	return out
+}
+func MysqlProfile_FromProto(mapCtx *direct.MapContext, in *pb.MysqlProfile) *krm.MysqlProfile {
+	if in == nil {
+		return nil
+	}
+	out := &krm.MysqlProfile{}
+	out.Hostname = direct.LazyPtr(in.GetHostname())
+	out.Port = direct.LazyPtr(in.GetPort())
+	out.Username = direct.LazyPtr(in.GetUsername())
+	out.Password = direct.LazyPtr(in.GetPassword())
+	out.SSLConfig = MysqlSSLConfig_FromProto(mapCtx, in.GetSslConfig())
+	return out
+}
+func MysqlProfile_ToProto(mapCtx *direct.MapContext, in *krm.MysqlProfile) *pb.MysqlProfile {
+	if in == nil {
+		return nil
+	}
+	out := &pb.MysqlProfile{}
+	out.Hostname = direct.ValueOf(in.Hostname)
+	out.Port = direct.ValueOf(in.Port)
+	out.Username = direct.ValueOf(in.Username)
+	out.Password = direct.ValueOf(in.Password)
+	out.SslConfig = MysqlSSLConfig_ToProto(mapCtx, in.SSLConfig)
+	return out
+}
+func MysqlProfileObservedState_FromProto(mapCtx *direct.MapContext, in *pb.MysqlProfile) *krm.MysqlProfileObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.MysqlProfileObservedState{}
+	// MISSING: Hostname
+	// MISSING: Port
+	// MISSING: Username
+	// MISSING: Password
+	out.SSLConfig = MysqlSSLConfigObservedState_FromProto(mapCtx, in.GetSslConfig())
+	return out
+}
+func MysqlProfileObservedState_ToProto(mapCtx *direct.MapContext, in *krm.MysqlProfileObservedState) *pb.MysqlProfile {
+	if in == nil {
+		return nil
+	}
+	out := &pb.MysqlProfile{}
+	// MISSING: Hostname
+	// MISSING: Port
+	// MISSING: Username
+	// MISSING: Password
+	out.SslConfig = MysqlSSLConfigObservedState_ToProto(mapCtx, in.SSLConfig)
+	return out
+}
+func MysqlSSLConfig_FromProto(mapCtx *direct.MapContext, in *pb.MysqlSslConfig) *krm.MysqlSSLConfig {
+	if in == nil {
+		return nil
+	}
+	out := &krm.MysqlSSLConfig{}
+	out.ClientKey = direct.LazyPtr(in.GetClientKey())
+	// MISSING: ClientKeySet
+	out.ClientCertificate = direct.LazyPtr(in.GetClientCertificate())
+	// MISSING: ClientCertificateSet
+	out.CACertificate = direct.LazyPtr(in.GetCaCertificate())
+	// MISSING: CACertificateSet
+	return out
+}
+func MysqlSSLConfig_ToProto(mapCtx *direct.MapContext, in *krm.MysqlSSLConfig) *pb.MysqlSslConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.MysqlSslConfig{}
+	out.ClientKey = direct.ValueOf(in.ClientKey)
+	// MISSING: ClientKeySet
+	out.ClientCertificate = direct.ValueOf(in.ClientCertificate)
+	// MISSING: ClientCertificateSet
+	out.CaCertificate = direct.ValueOf(in.CACertificate)
+	// MISSING: CACertificateSet
+	return out
+}
+func MysqlSSLConfigObservedState_FromProto(mapCtx *direct.MapContext, in *pb.MysqlSslConfig) *krm.MysqlSSLConfigObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.MysqlSSLConfigObservedState{}
+	// MISSING: ClientKey
+	out.ClientKeySet = direct.LazyPtr(in.GetClientKeySet())
+	// MISSING: ClientCertificate
+	out.ClientCertificateSet = direct.LazyPtr(in.GetClientCertificateSet())
+	// MISSING: CACertificate
+	out.CACertificateSet = direct.LazyPtr(in.GetCaCertificateSet())
+	return out
+}
+func MysqlSSLConfigObservedState_ToProto(mapCtx *direct.MapContext, in *krm.MysqlSSLConfigObservedState) *pb.MysqlSslConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.MysqlSslConfig{}
+	// MISSING: ClientKey
+	out.ClientKeySet = direct.ValueOf(in.ClientKeySet)
+	// MISSING: ClientCertificate
+	out.ClientCertificateSet = direct.ValueOf(in.ClientCertificateSet)
+	// MISSING: CACertificate
+	out.CaCertificateSet = direct.ValueOf(in.CACertificateSet)
+	return out
+}
+func OracleProfileObservedState_FromProto(mapCtx *direct.MapContext, in *pb.OracleProfile) *krm.OracleProfileObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.OracleProfileObservedState{}
+	// MISSING: Hostname
+	// MISSING: Port
+	// MISSING: Username
+	// MISSING: Password
+	// MISSING: DatabaseService
+	// MISSING: ConnectionAttributes
+	out.OracleSSLConfig = OracleSSLConfigObservedState_FromProto(mapCtx, in.GetOracleSslConfig())
+	// MISSING: OracleAsmConfig
+	// MISSING: SecretManagerStoredPassword
+	return out
+}
+func OracleProfileObservedState_ToProto(mapCtx *direct.MapContext, in *krm.OracleProfileObservedState) *pb.OracleProfile {
+	if in == nil {
+		return nil
+	}
+	out := &pb.OracleProfile{}
+	// MISSING: Hostname
+	// MISSING: Port
+	// MISSING: Username
+	// MISSING: Password
+	// MISSING: DatabaseService
+	// MISSING: ConnectionAttributes
+	out.OracleSslConfig = OracleSSLConfigObservedState_ToProto(mapCtx, in.OracleSSLConfig)
+	// MISSING: OracleAsmConfig
+	// MISSING: SecretManagerStoredPassword
+	return out
+}
+func OracleSSLConfig_FromProto(mapCtx *direct.MapContext, in *pb.OracleSslConfig) *krm.OracleSSLConfig {
+	if in == nil {
+		return nil
+	}
+	out := &krm.OracleSSLConfig{}
+	out.CACertificate = direct.LazyPtr(in.GetCaCertificate())
+	// MISSING: CACertificateSet
+	return out
+}
+func OracleSSLConfig_ToProto(mapCtx *direct.MapContext, in *krm.OracleSSLConfig) *pb.OracleSslConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.OracleSslConfig{}
+	out.CaCertificate = direct.ValueOf(in.CACertificate)
+	// MISSING: CACertificateSet
+	return out
+}
+func OracleSSLConfigObservedState_FromProto(mapCtx *direct.MapContext, in *pb.OracleSslConfig) *krm.OracleSSLConfigObservedState {
+	if in == nil {
+		return nil
+	}
+	out := &krm.OracleSSLConfigObservedState{}
+	// MISSING: CACertificate
+	out.CACertificateSet = direct.LazyPtr(in.GetCaCertificateSet())
+	return out
+}
+func OracleSSLConfigObservedState_ToProto(mapCtx *direct.MapContext, in *krm.OracleSSLConfigObservedState) *pb.OracleSslConfig {
+	if in == nil {
+		return nil
+	}
+	out := &pb.OracleSslConfig{}
+	// MISSING: CACertificate
+	out.CaCertificateSet = direct.ValueOf(in.CACertificateSet)
+	return out
+}
+func PostgresqlProfile_FromProto(mapCtx *direct.MapContext, in *pb.PostgresqlProfile) *krm.PostgresqlProfile {
+	if in == nil {
+		return nil
+	}
+	out := &krm.PostgresqlProfile{}
+	out.Hostname = direct.LazyPtr(in.GetHostname())
+	out.Port = direct.LazyPtr(in.GetPort())
+	out.Username = direct.LazyPtr(in.GetUsername())
+	out.Password = direct.LazyPtr(in.GetPassword())
+	out.Database = direct.LazyPtr(in.GetDatabase())
+	return out
+}
+func PostgresqlProfile_ToProto(mapCtx *direct.MapContext, in *krm.PostgresqlProfile) *pb.PostgresqlProfile {
+	if in == nil {
+		return nil
+	}
+	out := &pb.PostgresqlProfile{}
+	out.Hostname = direct.ValueOf(in.Hostname)
+	out.Port = direct.ValueOf(in.Port)
+	out.Username = direct.ValueOf(in.Username)
+	out.Password = direct.ValueOf(in.Password)
+	out.Database = direct.ValueOf(in.Database)
+	return out
+}
+func PrivateConnectivity_ToProto(mapCtx *direct.MapContext, in *krm.PrivateConnectivity) *pb.PrivateConnectivity {
+	if in == nil {
+		return nil
+	}
+	out := &pb.PrivateConnectivity{}
+	if in.PrivateConnectionRef != nil {
+		out.PrivateConnection = in.PrivateConnectionRef.External
+	}
+	return out
+}
+func SQLServerProfile_FromProto(mapCtx *direct.MapContext, in *pb.SqlServerProfile) *krm.SQLServerProfile {
+	if in == nil {
+		return nil
+	}
+	out := &krm.SQLServerProfile{}
+	out.Hostname = direct.LazyPtr(in.GetHostname())
+	out.Port = direct.LazyPtr(in.GetPort())
+	out.Username = direct.LazyPtr(in.GetUsername())
+	out.Password = direct.LazyPtr(in.GetPassword())
+	out.Database = direct.LazyPtr(in.GetDatabase())
+	return out
+}
+func SQLServerProfile_ToProto(mapCtx *direct.MapContext, in *krm.SQLServerProfile) *pb.SqlServerProfile {
+	if in == nil {
+		return nil
+	}
+	out := &pb.SqlServerProfile{}
+	out.Hostname = direct.ValueOf(in.Hostname)
+	out.Port = direct.ValueOf(in.Port)
+	out.Username = direct.ValueOf(in.Username)
+	out.Password = direct.ValueOf(in.Password)
+	out.Database = direct.ValueOf(in.Database)
+	return out
+}
+func StaticServiceIPConnectivity_FromProto(mapCtx *direct.MapContext, in *pb.StaticServiceIpConnectivity) *krm.StaticServiceIPConnectivity {
+	if in == nil {
+		return nil
+	}
+	out := &krm.StaticServiceIPConnectivity{}
+	return out
+}
+func StaticServiceIPConnectivity_ToProto(mapCtx *direct.MapContext, in *krm.StaticServiceIPConnectivity) *pb.StaticServiceIpConnectivity {
+	if in == nil {
+		return nil
+	}
+	out := &pb.StaticServiceIpConnectivity{}
 	return out
 }
