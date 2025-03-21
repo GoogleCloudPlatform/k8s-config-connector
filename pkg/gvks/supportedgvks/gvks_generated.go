5d51286044925463df370f266610679152e79e0e
Barni S <barni@google.com>
2025-03-21 16:52:56 -0400

    conductor: "make ready-pr. WARN: extra files found, committing them. "

--- pkg/gvks/supportedgvks/gvks_generated.go
+++ pkg/gvks/supportedgvks/gvks_generated.go
@@ -3560,6 +3560,16 @@ var SupportedGVKs = map[schema.GroupVersionKind]GVKMetadata{
 		},
 	},
 	{
+		Group:   "metastore.cnrm.cloud.google.com",
+		Version: "v1alpha1",
+		Kind:    "MetastoreFederation",
+	}: {
+		Labels: map[string]string{
+			"cnrm.cloud.google.com/managed-by-kcc": "true",
+			"cnrm.cloud.google.com/system":         "true",
+		},
+	},
+	{
 		Group:   "mlengine.cnrm.cloud.google.com",
 		Version: "v1alpha1",
 		Kind:    "MLEngineModel",
