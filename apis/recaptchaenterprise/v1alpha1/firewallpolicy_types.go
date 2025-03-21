811d08eee3ffe9ad431f02e7f8441e0ea3c5f106
Joyce Ma <maqiuyu@google.com>
2025-03-21 06:39:42 +0000

    Address presubmit failures

--- apis/recaptchaenterprise/v1alpha1/firewallpolicy_types.go
+++ apis/recaptchaenterprise/v1alpha1/firewallpolicy_types.go
@@ -94,7 +94,7 @@ type ReCAPTCHAEnterpriseFirewallPolicyObservedState struct {
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // TODO(user): make sure the pluralizaiton below is correct
-// +kubebuilder:resource:categories=gcp,shortName=gcprecaptchaenterprisefirewallpolicy;gcprecaptchaenterprisefirewallpolicys
+// +kubebuilder:resource:categories=gcp,shortName=gcprecaptchaenterprisefirewallpolicy;gcprecaptchaenterprisefirewallpolicies
 // +kubebuilder:subresource:status
 // +kubebuilder:metadata:labels="cnrm.cloud.google.com/managed-by-kcc=true";"cnrm.cloud.google.com/system=true"
 // +kubebuilder:printcolumn:name="Age",JSONPath=".metadata.creationTimestamp",type="date"
