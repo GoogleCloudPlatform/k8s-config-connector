d1ab2ac79cc7efd7f7f4e097d7aba6535cb3192e
justinsb <justinsb@google.com>
2025-03-22 08:32:43 -0400

    mockgcp: fixup monitoring alertPolicy

--- mockgcp/mockmonitoring/normalize.go
+++ mockgcp/mockmonitoring/normalize.go
@@ -15,6 +15,8 @@
 package mockmonitoring
 
 import (
+	"strings"
+
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/mockgcpregistry"
 )
 
@@ -28,18 +30,39 @@ const (
 func (s *MockService) ConfigureVisitor(url string, replacements mockgcpregistry.NormalizingVisitor) {
 	replacements.ReplacePath(".creationRecord.mutateTime", timePlaceholder)
 	replacements.ReplacePath(".notificationChannels[].creationRecord.mutateTime", timePlaceholder)
+	replacements.ReplacePath(".alertPolicies[].creationRecord.mutateTime", timePlaceholder)
 
 	replacements.ReplacePath(".mutationRecord.mutateTime", timePlaceholder)
 	replacements.ReplacePath(".notificationChannels[].mutationRecord.mutateTime", timePlaceholder)
+	replacements.ReplacePath(".alertPolicies[].mutationRecord.mutateTime", timePlaceholder)
 
 	replacements.ReplacePath(".mutationRecords[].mutateTime", timePlaceholder)
 	replacements.ReplacePath(".notificationChannels[].mutationRecords[].mutateTime", timePlaceholder)
+	replacements.ReplacePath(".alertPolicies[].mutationRecords[].mutateTime", timePlaceholder)
 
 	replacements.ReplacePath(".creationRecord.mutatedBy", emailPlaceholder)
 	replacements.ReplacePath(".mutationRecord.mutatedBy", emailPlaceholder)
 	replacements.ReplacePath(".mutationRecords[].mutatedBy", emailPlaceholder)
-
+	replacements.ReplacePath(".alertPolicies[].creationRecord.mutatedBy", emailPlaceholder)
+	replacements.ReplacePath(".alertPolicies[].mutationRecord.mutatedBy", emailPlaceholder)
+	replacements.ReplacePath(".alertPolicies[].mutationRecords[].mutatedBy", emailPlaceholder)
 }
 
 func (s *MockService) Previsit(event mockgcpregistry.Event, replacements mockgcpregistry.NormalizingVisitor) {
+	visitLink := func(name string) {
+		tokens := strings.Split(name, "/")
+		n := len(tokens)
+		if n > 2 && tokens[n-2] == "conditions" {
+			replacements.ReplaceStringValue(tokens[n-1], "${conditionID}")
+		}
+	}
+
+	event.VisitResponseStringValues(func(path string, value string) {
+		switch path {
+		case ".conditions[].name":
+			// In alertPolicy
+			//  e.g. "name": "projects/${projectId}/alertPolicies/${alertPolicyID}/conditions/5683099026496458999"
+			visitLink(value)
+		}
+	})
 }
