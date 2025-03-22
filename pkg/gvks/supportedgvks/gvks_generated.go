749406cd907799a9bdc47d4fd1a9869fb7eff4c5
Jingyi Hu <jingyih@google.com>
2025-03-22 02:12:22 +0000

    chore: make ready-pr

--- pkg/gvks/supportedgvks/gvks_generated.go
+++ pkg/gvks/supportedgvks/gvks_generated.go
@@ -4836,6 +4836,16 @@ var SupportedGVKs = map[schema.GroupVersionKind]GVKMetadata{
 		},
 	},
 	{
+		Group:   "vmwareengine.cnrm.cloud.google.com",
+		Version: "v1alpha1",
+		Kind:    "VMwareEnginePrivateCloud",
+	}: {
+		Labels: map[string]string{
+			"cnrm.cloud.google.com/managed-by-kcc": "true",
+			"cnrm.cloud.google.com/system":         "true",
+		},
+	},
+	{
 		Group:   "vpcaccess.cnrm.cloud.google.com",
 		Version: "v1beta1",
 		Kind:    "VPCAccessConnector",
