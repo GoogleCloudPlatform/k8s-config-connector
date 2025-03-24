b4f0ce6e1bc16a67950a6a0b8dbb40d96833a03f
Gemma Hou <yuhou@google.com>
2025-03-24 17:58:48 +0000

    run make ready-pr

--- pkg/gvks/supportedgvks/gvks_generated.go
+++ pkg/gvks/supportedgvks/gvks_generated.go
@@ -4520,6 +4520,16 @@ var SupportedGVKs = map[schema.GroupVersionKind]GVKMetadata{
 	},
 	{
 		Group:   "spanner.cnrm.cloud.google.com",
+		Version: "v1alpha1",
+		Kind:    "SpannerInstanceConfig",
+	}: {
+		Labels: map[string]string{
+			"cnrm.cloud.google.com/managed-by-kcc": "true",
+			"cnrm.cloud.google.com/system":         "true",
+		},
+	},
+	{
+		Group:   "spanner.cnrm.cloud.google.com",
 		Version: "v1beta1",
 		Kind:    "SpannerInstance",
 	}: {
