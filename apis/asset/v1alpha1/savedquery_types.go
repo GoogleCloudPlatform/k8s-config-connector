9455d6cf43c37337b978cf88ba662ec312ea9c1f
Barni S <barni@google.com>
2025-03-24 20:39:09 -0400

    Autofix attempt 1. AssetSavedQuery: Verify and Fix API checks

--- apis/asset/v1alpha1/savedquery_types.go
+++ apis/asset/v1alpha1/savedquery_types.go
@@ -166,7 +166,7 @@ type AssetSavedQueryObservedState struct {
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // TODO(user): make sure the pluralizaiton below is correct
-// +kubebuilder:resource:categories=gcp,shortName=gcpassetsavedquery;gcpassetsavedquerys
+// +kubebuilder:resource:categories=gcp,shortName=gcpassetsavedquery;gcpassetsavedqueries
 // +kubebuilder:subresource:status
 // +kubebuilder:metadata:labels="cnrm.cloud.google.com/managed-by-kcc=true";"cnrm.cloud.google.com/system=true"
 // +kubebuilder:printcolumn:name="Age",JSONPath=".metadata.creationTimestamp",type="date"
