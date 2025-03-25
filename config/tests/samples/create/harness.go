15ddc61b55975bfaabb81685ba59a8595c589c69
Gemma Hou <yuhou@google.com>
2025-03-25 23:45:00 +0000

    conductor: "Support for testing with mockgcp"

--- config/tests/samples/create/harness.go
+++ config/tests/samples/create/harness.go
@@ -830,6 +830,7 @@ func MaybeSkip(t *testing.T, name string, resources []*unstructured.Unstructured
 			case schema.GroupKind{Group: "compute.cnrm.cloud.google.com", Kind: "ComputeURLMap"}:
 				// ok
 
+			case schema.GroupKind{Group: "compute.cnrm.cloud.google.com", Kind: "ComputeInterconnect"}:
 			case schema.GroupKind{Group: "container.cnrm.cloud.google.com", Kind: "ContainerCluster"}:
 			case schema.GroupKind{Group: "container.cnrm.cloud.google.com", Kind: "ContainerNodePool"}:
 
