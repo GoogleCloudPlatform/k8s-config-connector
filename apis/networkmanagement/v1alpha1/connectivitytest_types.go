42725e411e662b1b95612e6a9409b16b8a60e1b4
Joyce Ma <maqiuyu@google.com>
2025-03-26 01:22:59 +0000

    NetworkManagementConnectivityTest: Add spec and status to generated type

--- apis/networkmanagement/v1alpha1/connectivitytest_types.go
+++ apis/networkmanagement/v1alpha1/connectivitytest_types.go
@@ -15,6 +15,7 @@
 package v1alpha1
 
 import (
+	refsv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/apis/k8s/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -24,8 +25,89 @@ var NetworkManagementConnectivityTestGVK = GroupVersion.WithKind("NetworkManagem
 // NetworkManagementConnectivityTestSpec defines the desired state of NetworkManagementConnectivityTest
 // +kcc:proto=google.cloud.networkmanagement.v1.ConnectivityTest
 type NetworkManagementConnectivityTestSpec struct {
+	// The project that this resource belongs to. If not provided, the provider project is used.
+	ProjectRef refsv1beta1.Reference `json:"projectRef"`
 	// The NetworkManagementConnectivityTest name. If not given, the metadata.name will be used.
 	ResourceID *string `json:"resourceID,omitempty"`
+
+	// Identifier. Unique name of the resource using the form:
+	//      `projects/{project_id}/locations/global/connectivityTests/{test_id}`
+	// +kcc:proto:field=google.cloud.networkmanagement.v1.ConnectivityTest.name
+	Name *string `json:"name,omitempty"`
+
+	// The user-supplied description of the Connectivity Test.
+	//  Maximum of 512 characters.
+	// +kcc:proto:field=google.cloud.networkmanagement.v1.ConnectivityTest.description
+	Description *string `json:"description,omitempty"`
+
+	// Required. Source specification of the Connectivity Test.
+	//
+	//  You can use a combination of source IP address, virtual machine
+	//  (VM) instance, or Compute Engine network to uniquely identify
+	//  the source location.
+	//
+	//  Examples:
+	//  If the source IP address is an internal IP address within a Google Cloud
+	//  Virtual Private Cloud (VPC) network, then you must also specify the VPC
+	//  network. Otherwise, specify the VM instance, which already contains its
+	//  internal IP address and VPC network information.
+	//
+	//  If the source of the test is within an on-premises network, then you must
+	//  provide the destination VPC network.
+	//
+	//  If the source endpoint is a Compute Engine VM instance with multiple
+	//  network interfaces, the instance itself is not sufficient to identify the
+	//  endpoint. So, you must also specify the source IP address or VPC network.
+	//
+	//  A reachability analysis proceeds even if the source location is
+	//  ambiguous. However, the test result may include endpoints that you don't
+	//  intend to test.
+	// +kcc:proto:field=google.cloud.networkmanagement.v1.ConnectivityTest.source
+	Source *Endpoint `json:"source,omitempty"`
+
+	// Required. Destination specification of the Connectivity Test.
+	//
+	//  You can use a combination of destination IP address, Compute Engine
+	//  VM instance, or VPC network to uniquely identify the destination
+	//  location.
+	//
+	//  Even if the destination IP address is not unique, the source IP
+	//  location is unique. Usually, the analysis can infer the destination
+	//  endpoint from route information.
+	//
+	//  If the destination you specify is a VM instance and the instance has
+	//  multiple network interfaces, then you must also specify either
+	//  a destination IP address  or VPC network to identify the destination
+	//  interface.
+	//
+	//  A reachability analysis proceeds even if the destination location is
+	//  ambiguous. However, the result can include endpoints that you don't
+	//  intend to test.
+	// +kcc:proto:field=google.cloud.networkmanagement.v1.ConnectivityTest.destination
+	Destination *Endpoint `json:"destination,omitempty"`
+
+	// IP Protocol of the test. When not provided, "TCP" is assumed.
+	// +kcc:proto:field=google.cloud.networkmanagement.v1.ConnectivityTest.protocol
+	Protocol *string `json:"protocol,omitempty"`
+
+	// Other projects that may be relevant for reachability analysis.
+	//  This is applicable to scenarios where a test can cross project boundaries.
+	// +kcc:proto:field=google.cloud.networkmanagement.v1.ConnectivityTest.related_projects
+	RelatedProjects []string `json:"relatedProjects,omitempty"`
+
+	// Resource labels to represent user-provided metadata.
+	// +kcc:proto:field=google.cloud.networkmanagement.v1.ConnectivityTest.labels
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// Whether run analysis for the return path from destination to source.
+	//  Default value is false.
+	// +kcc:proto:field=google.cloud.networkmanagement.v1.ConnectivityTest.round_trip
+	RoundTrip *bool `json:"roundTrip,omitempty"`
+
+	// Whether the test should skip firewall checking.
+	//  If not provided, we assume false.
+	// +kcc:proto:field=google.cloud.networkmanagement.v1.ConnectivityTest.bypass_firewall_checks
+	BypassFirewallChecks *bool `json:"bypassFirewallChecks,omitempty"`
 }
 
 // NetworkManagementConnectivityTestStatus defines the config connector machine state of NetworkManagementConnectivityTest
@@ -47,6 +129,62 @@ type NetworkManagementConnectivityTestStatus struct {
 // NetworkManagementConnectivityTestObservedState is the state of the NetworkManagementConnectivityTest resource as most recently observed in GCP.
 // +kcc:proto=google.cloud.networkmanagement.v1.ConnectivityTest
 type NetworkManagementConnectivityTestObservedState struct {
+	// Required. Source specification of the Connectivity Test.
+	//
+	//  You can use a combination of source IP address, virtual machine
+	//  (VM) instance, or Compute Engine network to uniquely identify
+	//  the source location.
+	//
+	//  Examples:
+	//  If the source IP address is an internal IP address within a Google Cloud
+	//  Virtual Private Cloud (VPC) network, then you must also specify the VPC
+	//  network. Otherwise, specify the VM instance, which already contains its
+	//  internal IP address and VPC network information.
+	//
+	//  If the source of the test is within an on-premises network, then you must
+	//  provide the destination VPC network.
+	//
+	//  If the source endpoint is a Compute Engine VM instance with multiple
+	//  network interfaces, the instance itself is not sufficient to identify the
+	//  endpoint. So, you must also specify the source IP address or VPC network.
+	//
+	//  A reachability analysis proceeds even if the source location is
+	//  ambiguous. However, the test result may include endpoints that you don't
+	//  intend to test.
+	// +kcc:proto:field=google.cloud.networkmanagement.v1.ConnectivityTest.source
+	Source *EndpointObservedState `json:"source,omitempty"`
+
+	// Output only. The display name of a Connectivity Test.
+	// +kcc:proto:field=google.cloud.networkmanagement.v1.ConnectivityTest.display_name
+	DisplayName *string `json:"displayName,omitempty"`
+
+	// Output only. The time the test was created.
+	// +kcc:proto:field=google.cloud.networkmanagement.v1.ConnectivityTest.create_time
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// Output only. The time the test's configuration was updated.
+	// +kcc:proto:field=google.cloud.networkmanagement.v1.ConnectivityTest.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
+
+	// Output only. The reachability details of this test from the latest run.
+	//  The details are updated when creating a new test, updating an
+	//  existing test, or triggering a one-time rerun of an existing test.
+	// +kcc:proto:field=google.cloud.networkmanagement.v1.ConnectivityTest.reachability_details
+	ReachabilityDetails *ReachabilityDetails `json:"reachabilityDetails,omitempty"`
+
+	// Output only. The probing details of this test from the latest run, present
+	//  for applicable tests only. The details are updated when creating a new
+	//  test, updating an existing test, or triggering a one-time rerun of an
+	//  existing test.
+	// +kcc:proto:field=google.cloud.networkmanagement.v1.ConnectivityTest.probing_details
+	ProbingDetails *ProbingDetails `json:"probingDetails,omitempty"`
+
+	// Output only. The reachability details of this test from the latest run for
+	//  the return path. The details are updated when creating a new test,
+	//  updating an existing test, or triggering a one-time rerun of an existing
+	//  test.
+	// +kcc:proto:field=google.cloud.networkmanagement.v1.ConnectivityTest.return_reachability_details
+	ReturnReachabilityDetails *ReachabilityDetails `json:"returnReachabilityDetails,omitempty"`
 }
 
 // +genclient
@@ -61,6 +199,7 @@ type NetworkManagementConnectivityTestObservedState struct {
 
 // NetworkManagementConnectivityTest is the Schema for the NetworkManagementConnectivityTest API
 // +k8s:openapi-gen=true
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type NetworkManagementConnectivityTest struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -72,6 +211,7 @@ type NetworkManagementConnectivityTest struct {
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // NetworkManagementConnectivityTestList contains a list of NetworkManagementConnectivityTest
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type NetworkManagementConnectivityTestList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
