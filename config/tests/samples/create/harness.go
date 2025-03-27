87d3cab731340cb4dcbe3e00276007c3681e1b2c
Jingyi Hu <jingyih@google.com>
2025-03-27 01:46:44 +0000

    GKEBackupRestorePlan: fix test from generated

--- config/tests/samples/create/harness.go
+++ config/tests/samples/create/harness.go
@@ -764,7 +764,6 @@ func MaybeSkip(t *testing.T, name string, resources []*unstructured.Unstructured
 			case schema.GroupKind{Group: "batch.cnrm.cloud.google.com", Kind: "BatchJob"}:
 
 			case schema.GroupKind{Group: "gkebackup.cnrm.cloud.google.com", Kind: "GKEBackupBackupPlan"}:
-
 			case schema.GroupKind{Group: "gkebackup.cnrm.cloud.google.com", Kind: "GKEBackupRestorePlan"}:
 
 			case schema.GroupKind{Group: "bigquery.cnrm.cloud.google.com", Kind: "BigQueryDataset"}:
