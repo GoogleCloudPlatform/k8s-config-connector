550e74bf13bf098d69c57a9209499cdb3e5d023d
Jingyi Hu <jingyih@google.com>
2025-03-28 09:56:01 +0000

    DatastreamConnectionProfile: fix mock from generated

--- mockgcp/mockdatastream/connectionprofile.go
+++ mockgcp/mockdatastream/connectionprofile.go
@@ -12,15 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build mockgcp
-// +build mockgcp
-
-// Package mockdatastream is a mock for the datastream service.
-//
-//	proto.service: google.cloud.datastream.v1.Datastream
-//
-// proto.message: google.cloud.datastream.v1.ConnectionProfile
 package mockdatastream
+
+// +tool:mockgcp-service
+// http.host: datastream.googleapis.com
+// proto.service: google.cloud.datastream.v1.Datastream
 
 import (
 	"context"
@@ -31,7 +27,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/proto"
-	"google.golang.org/protobuf/types/known/fieldmaskpb"
+	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/projects"
@@ -73,6 +69,9 @@ func (s *DatastreamV1) CreateConnectionProfile(ctx context.Context, req *pb.Crea
 	obj.Name = fqn
 	obj.CreateTime = timestamppb.New(now)
 	obj.UpdateTime = timestamppb.New(now)
+	if obj.Connectivity == nil {
+		obj.Connectivity = &pb.ConnectionProfile_StaticServiceIpConnectivity{}
+	}
 
 	if err := s.storage.Create(ctx, fqn, obj); err != nil {
 		return nil, err
@@ -103,32 +102,20 @@ func (s *DatastreamV1) UpdateConnectionProfile(ctx context.Context, req *pb.Upda
 	if err := s.storage.Get(ctx, fqn, obj); err != nil {
 		return nil, err
 	}
-
-	// We clone before, so we can check after, in case the update_mask is empty
-	original := proto.Clone(obj).(*pb.ConnectionProfile)
-
-	// Apply the update mask to the object, using a library function.
-	// Note: FieldMask.IsValid doesn't work yet.
-	updateMask := req.GetUpdateMask()
-	if updateMask == nil || len(updateMask.Paths) == 0 {
-		updateMask = &fieldmaskpb.FieldMask{}
-		for _, p := range allPaths(original) {
-			updateMask.Paths = append(updateMask.Paths, p)
-		}
-	}
-
-	if !updateMask.IsValid(obj) {
-		return nil, status.Errorf(codes.InvalidArgument, "invalid fieldmask")
-	}
-
-	// Actually do the update
-	if err := updateMask.Merge(req.GetConnectionProfile(), obj); err != nil {
-		return nil, fmt.Errorf("error applying fieldmask: %w", err)
-	}
-
-	// Some fields are "output only", so we preserve their values.
-	preserveOutputOnlyFields(original, obj)
 	obj.UpdateTime = timestamppb.New(time.Now())
+
+	paths := req.GetUpdateMask().GetPaths()
+	if len(paths) == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "update_mask must be provided")
+	}
+
+	for _, path := range paths {
+		switch path {
+		case "displayName":
+			obj.DisplayName = req.GetConnectionProfile().GetDisplayName()
+		}
+		// TODO: handle other fields
+	}
 
 	if err := s.storage.Update(ctx, fqn, obj); err != nil {
 		return nil, err
@@ -170,7 +157,7 @@ func (s *DatastreamV1) DeleteConnectionProfile(ctx context.Context, req *pb.Dele
 
 	return s.operations.StartLRO(ctx, lroPrefix, lroMetadata, func() (proto.Message, error) {
 		lroMetadata.EndTime = timestamppb.Now()
-		return nil, nil // Operation is 'done' immediately.
+		return &emptypb.Empty{}, nil
 	})
 }
 
@@ -204,29 +191,4 @@ func (s *MockService) parseConnectionProfileName(name string) (*connectionProfil
 	}
 
 	return nil, status.Errorf(codes.InvalidArgument, "name %q is not valid", name)
-}
-
-// preserveOutputOnlyFields copies known output-only fields from src to dest.
-// This function is hand-maintained.
-func preserveOutputOnlyFields(src *pb.ConnectionProfile, dest *pb.ConnectionProfile) {
-	dest.Name = src.Name // Field 1
-}
-
-// allPaths returns all the paths for a resource.
-// This function is hand-maintained; we can replace with protoreflect to
-// avoid manual maintenance, but adds a dependency.
-func allPaths(obj *pb.ConnectionProfile) []string {
-	return []string{
-		"display_name",
-		"oracle_profile",
-		"gcs_profile",
-		"mysql_profile",
-		"bigquery_profile",
-		"postgresql_profile",
-		"static_service_ip_connectivity",
-		"forward_ssh_connectivity",
-		"private_connectivity",
-		"labels",
-		"sql_server_profile",
-	}
 }
