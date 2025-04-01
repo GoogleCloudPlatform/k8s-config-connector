3ffb34785c97122265db6ed959d4b0b1d2a8fe61
xiaoweim <xiaoweim@google.com>
2025-04-01 03:56:48 +0000

    make ready-pr

--- pkg/gvks/supportedgvks/gvks_generated.go
+++ pkg/gvks/supportedgvks/gvks_generated.go
@@ -3774,6 +3774,16 @@ var SupportedGVKs = map[schema.GroupVersionKind]GVKMetadata{
 	{
 		Group:   "managedkafka.cnrm.cloud.google.com",
 		Version: "v1alpha1",
+		Kind:    "ManagedKafkaConsumerGroup",
+	}: {
+		Labels: map[string]string{
+			"cnrm.cloud.google.com/managed-by-kcc": "true",
+			"cnrm.cloud.google.com/system":         "true",
+		},
+	},
+	{
+		Group:   "managedkafka.cnrm.cloud.google.com",
+		Version: "v1alpha1",
 		Kind:    "ManagedKafkaTopic",
 	}: {
 		Labels: map[string]string{
