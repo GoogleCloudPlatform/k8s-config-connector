5e74e49159d14537c017d4e41f85e751c90fa262
Barni S <barni@google.com>
2025-04-01 22:41:34 -0400

    conductor: "MetastoreBackup: Regenerate types"

--- apis/metastore/v1alpha1/types.generated.go
+++ apis/metastore/v1alpha1/types.generated.go
@@ -36,19 +36,6 @@ type AuxiliaryVersionConfig struct {
 	ConfigOverrides map[string]string `json:"configOverrides,omitempty"`
 }
 
-// +kcc:proto=google.cloud.metastore.v1.Backup
-type Backup struct {
-	// Immutable. The relative resource name of the backup, in the following form:
-	//
-	//  `projects/{project_number}/locations/{location_id}/services/{service_id}/backups/{backup_id}`
-	// +kcc:proto:field=google.cloud.metastore.v1.Backup.name
-	Name *string `json:"name,omitempty"`
-
-	// The description of the backup.
-	// +kcc:proto:field=google.cloud.metastore.v1.Backup.description
-	Description *string `json:"description,omitempty"`
-}
-
 // +kcc:proto=google.cloud.metastore.v1.HiveMetastoreConfig
 type HiveMetastoreConfig struct {
 	// Immutable. The Hive metastore schema version.
@@ -156,29 +143,6 @@ type Int32Value struct {
 	// The int32 value.
 	// +kcc:proto:field=google.protobuf.Int32Value.value
 	Value *int32 `json:"value,omitempty"`
-}
-
-// +kcc:proto=google.cloud.metastore.v1.Backup
-type BackupObservedState struct {
-	// Output only. The time when the backup was started.
-	// +kcc:proto:field=google.cloud.metastore.v1.Backup.create_time
-	CreateTime *string `json:"createTime,omitempty"`
-
-	// Output only. The time when the backup finished creating.
-	// +kcc:proto:field=google.cloud.metastore.v1.Backup.end_time
-	EndTime *string `json:"endTime,omitempty"`
-
-	// Output only. The current state of the backup.
-	// +kcc:proto:field=google.cloud.metastore.v1.Backup.state
-	State *string `json:"state,omitempty"`
-
-	// Output only. The revision of the service at the time of backup.
-	// +kcc:proto:field=google.cloud.metastore.v1.Backup.service_revision
-	ServiceRevision *Service `json:"serviceRevision,omitempty"`
-
-	// Output only. Services that are restoring from the backup.
-	// +kcc:proto:field=google.cloud.metastore.v1.Backup.restoring_services
-	RestoringServices []string `json:"restoringServices,omitempty"`
 }
 
 // +kcc:proto=google.cloud.metastore.v1.MetadataExport
