a8c4cad07462c1a66622364687dff368791d5a21
Barni S <barni@google.com>
2025-04-02 13:36:49 -0400

    conductor: "EventarcGoogleChannelConfig: Add generated types"

---
+++ apis/eventarc/v1alpha1/googlechannelconfig_identity.go
@@ -0,0 +1,117 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common"
+	refsv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// GoogleChannelConfigIdentity defines the resource reference to EventarcGoogleChannelConfig, which "External" field
+// holds the GCP identifier for the KRM object.
+type GoogleChannelConfigIdentity struct {
+	parent *GoogleChannelConfigParent
+	id     string
+}
+
+func (i *GoogleChannelConfigIdentity) String() string {
+	return i.parent.String() + "/googlechannelconfigs/" + i.id
+}
+
+func (i *GoogleChannelConfigIdentity) ID() string {
+	return i.id
+}
+
+func (i *GoogleChannelConfigIdentity) Parent() *GoogleChannelConfigParent {
+	return i.parent
+}
+
+type GoogleChannelConfigParent struct {
+	ProjectID string
+	Location  string
+}
+
+func (p *GoogleChannelConfigParent) String() string {
+	return "projects/" + p.ProjectID + "/locations/" + p.Location
+}
+
+// New builds a GoogleChannelConfigIdentity from the Config Connector GoogleChannelConfig object.
+func NewGoogleChannelConfigIdentity(ctx context.Context, reader client.Reader, obj *EventarcGoogleChannelConfig) (*GoogleChannelConfigIdentity, error) {
+
+	// Get Parent
+	projectRef, err := refsv1beta1.ResolveProject(ctx, reader, obj.GetNamespace(), obj.Spec.ProjectRef)
+	if err != nil {
+		return nil, err
+	}
+	projectID := projectRef.ProjectID
+	if projectID == "" {
+		return nil, fmt.Errorf("cannot resolve project")
+	}
+	location := obj.Spec.Location
+
+	// Get desired ID
+	resourceID := common.ValueOf(obj.Spec.ResourceID)
+	if resourceID == "" {
+		resourceID = obj.GetName()
+	}
+	if resourceID == "" {
+		return nil, fmt.Errorf("cannot resolve resource ID")
+	}
+
+	// Use approved External
+	externalRef := common.ValueOf(obj.Status.ExternalRef)
+	if externalRef != "" {
+		// Validate desired with actual
+		actualParent, actualResourceID, err := ParseGoogleChannelConfigExternal(externalRef)
+		if err != nil {
+			return nil, err
+		}
+		if actualParent.ProjectID != projectID {
+			return nil, fmt.Errorf("spec.projectRef changed, expect %s, got %s", actualParent.ProjectID, projectID)
+		}
+		if actualParent.Location != location {
+			return nil, fmt.Errorf("spec.location changed, expect %s, got %s", actualParent.Location, location)
+		}
+		if actualResourceID != resourceID {
+			return nil, fmt.Errorf("cannot reset `metadata.name` or `spec.resourceID` to %s, since it has already assigned to %s",
+				resourceID, actualResourceID)
+		}
+	}
+	return &GoogleChannelConfigIdentity{
+		parent: &GoogleChannelConfigParent{
+			ProjectID: projectID,
+			Location:  location,
+		},
+		id: resourceID,
+	}, nil
+}
+
+func ParseGoogleChannelConfigExternal(external string) (parent *GoogleChannelConfigParent, resourceID string, err error) {
+	tokens := strings.Split(external, "/")
+	if len(tokens) != 6 || tokens[0] != "projects" || tokens[2] != "locations" || tokens[4] != "googlechannelconfigs" {
+		return nil, "", fmt.Errorf("format of EventarcGoogleChannelConfig external=%q was not known (use projects/{{projectID}}/locations/{{location}}/googlechannelconfigs/{{googlechannelconfigID}})", external)
+	}
+	parent = &GoogleChannelConfigParent{
+		ProjectID: tokens[1],
+		Location:  tokens[3],
+	}
+	resourceID = tokens[5]
+	return parent, resourceID, nil
+}
---
+++ apis/eventarc/v1alpha1/types.generated.go
@@ -0,0 +1,44 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +generated:types
+// krm.group: eventarc.cnrm.cloud.google.com
+// krm.version: v1alpha1
+// proto.service: google.cloud.eventarc.v1
+// resource: EventarcGoogleChannelConfig:GoogleChannelConfig
+
+package v1alpha1
+
+// +kcc:proto=google.cloud.eventarc.v1.GoogleChannelConfig
+type GoogleChannelConfig struct {
+	// Required. The resource name of the config. Must be in the format of,
+	//  `projects/{project}/locations/{location}/googleChannelConfig`.
+	// +kcc:proto:field=google.cloud.eventarc.v1.GoogleChannelConfig.name
+	Name *string `json:"name,omitempty"`
+
+	// Optional. Resource name of a KMS crypto key (managed by the user) used to
+	//  encrypt/decrypt their event data.
+	//
+	//  It must match the pattern
+	//  `projects/*/locations/*/keyRings/*/cryptoKeys/*`.
+	// +kcc:proto:field=google.cloud.eventarc.v1.GoogleChannelConfig.crypto_key_name
+	CryptoKeyName *string `json:"cryptoKeyName,omitempty"`
+}
+
+// +kcc:proto=google.cloud.eventarc.v1.GoogleChannelConfig
+type GoogleChannelConfigObservedState struct {
+	// Output only. The last-modified time.
+	// +kcc:proto:field=google.cloud.eventarc.v1.GoogleChannelConfig.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
+}
