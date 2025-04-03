3ab15cb6204d64a437f181fe0a4e35ab10f47803
xiaoweim <xiaoweim@google.com>
2025-04-03 20:32:24 +0000

    update fuzzer

--- config/tests/samples/create/harness.go
+++ config/tests/samples/create/harness.go
@@ -735,7 +735,7 @@ func MaybeSkip(t *testing.T, name string, resources []*unstructured.Unstructured
 				continue
 			}
 			if name == "dclbasedresourceserviceaccountref" {
-				continue
+				t.Skip()
 			}
 
 			switch gvk.Group {
@@ -854,6 +854,7 @@ func MaybeSkip(t *testing.T, name string, resources []*unstructured.Unstructured
 
 			case schema.GroupKind{Group: "dataproc.cnrm.cloud.google.com", Kind: "DataprocCluster"}:
 			case schema.GroupKind{Group: "dataproc.cnrm.cloud.google.com", Kind: "DataprocJob"}:
+			case schema.GroupKind{Group: "dataproc.cnrm.cloud.google.com", Kind: "DataprocBatch"}:
 
 			case schema.GroupKind{Group: "discoveryengine.cnrm.cloud.google.com", Kind: "DiscoveryEngineDataStore"}:
 
@@ -979,8 +980,6 @@ func MaybeSkip(t *testing.T, name string, resources []*unstructured.Unstructured
 			case schema.GroupKind{Group: "vpcaccess.cnrm.cloud.google.com", Kind: "VPCAccessConnector"}:
 
 			case schema.GroupKind{Group: "apphub.cnrm.cloud.google.com", Kind: "AppHubApplication"}:
-
-			case schema.GroupKind{Group: "dataproc.cnrm.cloud.google.com", Kind: "DataprocBatch"}:
 
 			default:
 				t.Skipf("gk %v not suppported by mock gcp %v; skipping", gvk.GroupKind(), name)
