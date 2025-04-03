b6496761a35dda455cb59fd72dffdffa0f0b6a3e
Barni S <barni@google.com>
2025-04-03 17:28:40 -0400

    conductor: "DataCatalogTag: Regenerate types"

--- apis/datacatalog/v1alpha1/types.generated.go
+++ apis/datacatalog/v1alpha1/types.generated.go
@@ -20,36 +20,6 @@
 
 package v1alpha1
 
-// +kcc:proto=google.cloud.datacatalog.v1.Tag
-type Tag struct {
-	// Identifier. The resource name of the tag in URL format where tag ID is a
-	//  system-generated identifier.
-	//
-	//  Note: The tag itself might not be stored in the location specified in its
-	//  name.
-	// +kcc:proto:field=google.cloud.datacatalog.v1.Tag.name
-	Name *string `json:"name,omitempty"`
-
-	// Required. The resource name of the tag template this tag uses. Example:
-	//
-	//  `projects/{PROJECT_ID}/locations/{LOCATION}/tagTemplates/{TAG_TEMPLATE_ID}`
-	//
-	//  This field cannot be modified after creation.
-	// +kcc:proto:field=google.cloud.datacatalog.v1.Tag.template
-	Template *string `json:"template,omitempty"`
-
-	// Resources like entry can have schemas associated with them. This scope
-	//  allows you to attach tags to an individual column based on that schema.
-	//
-	//  To attach a tag to a nested column, separate column names with a dot
-	//  (`.`). Example: `column.nested_column`.
-	// +kcc:proto:field=google.cloud.datacatalog.v1.Tag.column
-	Column *string `json:"column,omitempty"`
-
-	// TODO: unsupported map type with key string and value message
-
-}
-
 // +kcc:proto=google.cloud.datacatalog.v1.TagField
 type TagField struct {
 
@@ -91,15 +61,4 @@ type TagField_EnumValue struct {
 	// The display name of the enum value.
 	// +kcc:proto:field=google.cloud.datacatalog.v1.TagField.EnumValue.display_name
 	DisplayName *string `json:"displayName,omitempty"`
-}
-
-// +kcc:proto=google.cloud.datacatalog.v1.Tag
-type TagObservedState struct {
-	// Output only. The display name of the tag template.
-	// +kcc:proto:field=google.cloud.datacatalog.v1.Tag.template_display_name
-	TemplateDisplayName *string `json:"templateDisplayName,omitempty"`
-
-	// Output only. Denotes the transfer status of the Tag Template.
-	// +kcc:proto:field=google.cloud.datacatalog.v1.Tag.dataplex_transfer_status
-	DataplexTransferStatus *string `json:"dataplexTransferStatus,omitempty"`
 }
