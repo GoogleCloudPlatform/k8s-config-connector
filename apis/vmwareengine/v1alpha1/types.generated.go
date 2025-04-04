5dcfe0b9e4dd049d7d50be59596b14ebe8903c9c
Jingyi Hu <jingyih@google.com>
2025-04-04 22:41:57 +0000

    VMwareEngineExternalAddress: add generated types

--- apis/vmwareengine/v1alpha1/types.generated.go
+++ apis/vmwareengine/v1alpha1/types.generated.go
@@ -21,8 +21,21 @@
 // resource: VMwareEngineNetworkPolicy:NetworkPolicy
 // resource: VMwareEngineExternalAccessRule:ExternalAccessRule
 // resource: VMwareEnginePrivateCloud:PrivateCloud
+// resource: VMwareEngineExternalAddress:ExternalAddress
 
 package v1alpha1
+
+// +kcc:proto=google.cloud.vmwareengine.v1.ExternalAddress
+type ExternalAddress struct {
+
+	// The internal IP address of a workload VM.
+	// +kcc:proto:field=google.cloud.vmwareengine.v1.ExternalAddress.internal_ip
+	InternalIP *string `json:"internalIP,omitempty"`
+
+	// User-provided description for this resource.
+	// +kcc:proto:field=google.cloud.vmwareengine.v1.ExternalAddress.description
+	Description *string `json:"description,omitempty"`
+}
 
 // +kcc:proto=google.cloud.vmwareengine.v1.NetworkPolicy.NetworkService
 type NetworkPolicy_NetworkService struct {
@@ -33,6 +46,37 @@ type NetworkPolicy_NetworkService struct {
 
 // +kcc:proto=google.cloud.vmwareengine.v1.VmwareEngineNetwork.VpcNetwork
 type VmwareEngineNetwork_VpcNetwork struct {
+}
+
+// +kcc:proto=google.cloud.vmwareengine.v1.ExternalAddress
+type ExternalAddressObservedState struct {
+	// Output only. The resource name of this external IP address.
+	//  Resource names are schemeless URIs that follow the conventions in
+	//  https://cloud.google.com/apis/design/resource_names.
+	//  For example:
+	//  `projects/my-project/locations/us-central1-a/privateClouds/my-cloud/externalAddresses/my-address`
+	// +kcc:proto:field=google.cloud.vmwareengine.v1.ExternalAddress.name
+	Name *string `json:"name,omitempty"`
+
+	// Output only. Creation time of this resource.
+	// +kcc:proto:field=google.cloud.vmwareengine.v1.ExternalAddress.create_time
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// Output only. Last update time of this resource.
+	// +kcc:proto:field=google.cloud.vmwareengine.v1.ExternalAddress.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
+
+	// Output only. The external IP address of a workload VM.
+	// +kcc:proto:field=google.cloud.vmwareengine.v1.ExternalAddress.external_ip
+	ExternalIP *string `json:"externalIP,omitempty"`
+
+	// Output only. The state of the resource.
+	// +kcc:proto:field=google.cloud.vmwareengine.v1.ExternalAddress.state
+	State *string `json:"state,omitempty"`
+
+	// Output only. System-generated unique identifier for the resource.
+	// +kcc:proto:field=google.cloud.vmwareengine.v1.ExternalAddress.uid
+	Uid *string `json:"uid,omitempty"`
 }
 
 // +kcc:proto=google.cloud.vmwareengine.v1.NetworkPolicy.NetworkService
