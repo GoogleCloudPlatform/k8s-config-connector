3f2f74e11ac6267e8c3e2b500fa700bcfdf20bf8
Gemma Hou <yuhou@google.com>
2025-04-07 05:09:46 +0000

    conductor: "DataplexEntryGroup: Add spec and status to generated type"

--- apis/dataplex/v1alpha1/entrygroup_types.go
+++ apis/dataplex/v1alpha1/entrygroup_types.go
@@ -26,6 +26,24 @@ var DataplexEntryGroupGVK = GroupVersion.WithKind("DataplexEntryGroup")
 type DataplexEntryGroupSpec struct {
 	// The DataplexEntryGroup name. If not given, the metadata.name will be used.
 	ResourceID *string `json:"resourceID,omitempty"`
+
+	// Optional. Description of the EntryGroup.
+	// +kcc:proto:field=google.cloud.dataplex.v1.EntryGroup.description
+	Description *string `json:"description,omitempty"`
+
+	// Optional. User friendly display name.
+	// +kcc:proto:field=google.cloud.dataplex.v1.EntryGroup.display_name
+	DisplayName *string `json:"displayName,omitempty"`
+
+	// Optional. User-defined labels for the EntryGroup.
+	// +kcc:proto:field=google.cloud.dataplex.v1.EntryGroup.labels
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// This checksum is computed by the service, and might be sent on update and
+	//  delete requests to ensure the client has an up-to-date value before
+	//  proceeding.
+	// +kcc:proto:field=google.cloud.dataplex.v1.EntryGroup.etag
+	Etag *string `json:"etag,omitempty"`
 }
 
 // DataplexEntryGroupStatus defines the config connector machine state of DataplexEntryGroup
@@ -47,6 +65,29 @@ type DataplexEntryGroupStatus struct {
 // DataplexEntryGroupObservedState is the state of the DataplexEntryGroup resource as most recently observed in GCP.
 // +kcc:proto=google.cloud.dataplex.v1.EntryGroup
 type DataplexEntryGroupObservedState struct {
+	// Output only. The relative resource name of the EntryGroup, in the format
+	//  projects/{project_id_or_number}/locations/{location_id}/entryGroups/{entry_group_id}.
+	// +kcc:proto:field=google.cloud.dataplex.v1.EntryGroup.name
+	Name *string `json:"name,omitempty"`
+
+	// Output only. System generated globally unique ID for the EntryGroup. If you
+	//  delete and recreate the EntryGroup with the same name, this ID will be
+	//  different.
+	// +kcc:proto:field=google.cloud.dataplex.v1.EntryGroup.uid
+	Uid *string `json:"uid,omitempty"`
+
+	// Output only. The time when the EntryGroup was created.
+	// +kcc:proto:field=google.cloud.dataplex.v1.EntryGroup.create_time
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// Output only. The time when the EntryGroup was last updated.
+	// +kcc:proto:field=google.cloud.dataplex.v1.EntryGroup.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
+
+	// Output only. Denotes the transfer status of the Entry Group. It is
+	//  unspecified for Entry Group created from Dataplex API.
+	// +kcc:proto:field=google.cloud.dataplex.v1.EntryGroup.transfer_status
+	TransferStatus *string `json:"transferStatus,omitempty"`
 }
 
 // +genclient
