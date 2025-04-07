c6212848249791dbef2ad8fcde1ba46f9a0fbc7c
Gemma Hou <yuhou@google.com>
2025-04-07 05:34:17 +0000

    update types and generate crd

--- pkg/gvks/supportedgvks/gvks_generated.go
+++ pkg/gvks/supportedgvks/gvks_generated.go
@@ -2432,6 +2432,16 @@ var SupportedGVKs = map[schema.GroupVersionKind]GVKMetadata{
 	{
 		Group:   "dataplex.cnrm.cloud.google.com",
 		Version: "v1alpha1",
+		Kind:    "DataplexEntryGroup",
+	}: {
+		Labels: map[string]string{
+			"cnrm.cloud.google.com/managed-by-kcc": "true",
+			"cnrm.cloud.google.com/system":         "true",
+		},
+	},
+	{
+		Group:   "dataplex.cnrm.cloud.google.com",
+		Version: "v1alpha1",
 		Kind:    "DataplexEnvironment",
 	}: {
 		Labels: map[string]string{
