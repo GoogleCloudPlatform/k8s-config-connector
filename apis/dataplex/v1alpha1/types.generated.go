4fc1c951d460f3c6b94be88435e1916fc325b24f
Gemma Hou <yuhou@google.com>
2025-04-07 23:32:24 +0000

    update types and generate CRD

--- apis/dataplex/v1alpha1/types.generated.go
+++ apis/dataplex/v1alpha1/types.generated.go
@@ -12,13 +12,309 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +generated:types
-// krm.group: dataplex.cnrm.cloud.google.com
-// krm.version: v1alpha1
-// proto.service: google.cloud.dataplex.v1
-// resource: DataplexZone:Zone
-
 package v1alpha1
+
+// +kcc:proto=google.cloud.dataplex.v1.Lake
+type Lake struct {
+
+	// Optional. User friendly display name.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Lake.display_name
+	DisplayName *string `json:"displayName,omitempty"`
+
+	// Optional. User-defined labels for the lake.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Lake.labels
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// Optional. Description of the lake.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Lake.description
+	Description *string `json:"description,omitempty"`
+
+	// Optional. Settings to manage lake and Dataproc Metastore service instance
+	//  association.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Lake.metastore
+	Metastore *Lake_Metastore `json:"metastore,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dataplex.v1.Lake.MetastoreStatus
+type Lake_MetastoreStatus struct {
+	// Current state of association.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Lake.MetastoreStatus.state
+	State *string `json:"state,omitempty"`
+
+	// Additional information about the current status.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Lake.MetastoreStatus.message
+	Message *string `json:"message,omitempty"`
+
+	// Last update time of the metastore status of the lake.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Lake.MetastoreStatus.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
+
+	// The URI of the endpoint used to access the Metastore service.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Lake.MetastoreStatus.endpoint
+	Endpoint *string `json:"endpoint,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dataplex.v1.Lake
+type LakeObservedState struct {
+	// Output only. The relative resource name of the lake, of the form:
+	//  `projects/{project_number}/locations/{location_id}/lakes/{lake_id}`.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Lake.name
+	Name *string `json:"name,omitempty"`
+
+	// Output only. System generated globally unique ID for the lake. This ID will
+	//  be different if the lake is deleted and re-created with the same name.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Lake.uid
+	Uid *string `json:"uid,omitempty"`
+
+	// Output only. The time when the lake was created.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Lake.create_time
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// Output only. The time when the lake was last updated.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Lake.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
+
+	// Output only. Current state of the lake.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Lake.state
+	State *string `json:"state,omitempty"`
+
+	// Output only. Service account associated with this lake. This service
+	//  account must be authorized to access or operate on resources managed by the
+	//  lake.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Lake.service_account
+	ServiceAccount *string `json:"serviceAccount,omitempty"`
+
+	// Output only. Aggregated status of the underlying assets of the lake.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Lake.asset_status
+	AssetStatus *AssetStatus `json:"assetStatus,omitempty"`
+
+	// Output only. Metastore status of the lake.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Lake.metastore_status
+	MetastoreStatus *Lake_MetastoreStatus `json:"metastoreStatus,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dataplex.v1.Content
+type Content struct {
+
+	// Required. The path for the Content file, represented as directory
+	//  structure. Unique within a lake. Limited to alphanumerics, hyphens,
+	//  underscores, dots and slashes.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Content.path
+	Path *string `json:"path,omitempty"`
+
+	// Optional. User defined labels for the content.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Content.labels
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// Optional. Description of the content.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Content.description
+	Description *string `json:"description,omitempty"`
+
+	// Required. Content data in string format.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Content.data_text
+	DataText *string `json:"dataText,omitempty"`
+
+	// Sql Script related configurations.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Content.sql_script
+	SQLScript *Content_SQLScript `json:"sqlScript,omitempty"`
+
+	// Notebook related configurations.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Content.notebook
+	Notebook *Content_Notebook `json:"notebook,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dataplex.v1.Content.Notebook
+type Content_Notebook struct {
+	// Required. Kernel Type of the notebook.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Content.Notebook.kernel_type
+	KernelType *string `json:"kernelType,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dataplex.v1.Content.SqlScript
+type Content_SQLScript struct {
+	// Required. Query Engine to be used for the Sql Query.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Content.SqlScript.engine
+	Engine *string `json:"engine,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dataplex.v1.Content
+type ContentObservedState struct {
+	// Output only. The relative resource name of the content, of the form:
+	//  projects/{project_id}/locations/{location_id}/lakes/{lake_id}/content/{content_id}
+	// +kcc:proto:field=google.cloud.dataplex.v1.Content.name
+	Name *string `json:"name,omitempty"`
+
+	// Output only. System generated globally unique ID for the content. This ID
+	//  will be different if the content is deleted and re-created with the same
+	//  name.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Content.uid
+	Uid *string `json:"uid,omitempty"`
+
+	// Output only. Content creation time.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Content.create_time
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// Output only. The time when the content was last updated.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Content.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dataplex.v1.Environment
+type Environment struct {
+
+	// Optional. User friendly display name.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.display_name
+	DisplayName *string `json:"displayName,omitempty"`
+
+	// Optional. User defined labels for the environment.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.labels
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// Optional. Description of the environment.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.description
+	Description *string `json:"description,omitempty"`
+
+	// Required. Infrastructure specification for the Environment.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.infrastructure_spec
+	InfrastructureSpec *Environment_InfrastructureSpec `json:"infrastructureSpec,omitempty"`
+
+	// Optional. Configuration for sessions created for this environment.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.session_spec
+	SessionSpec *Environment_SessionSpec `json:"sessionSpec,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dataplex.v1.Environment.Endpoints
+type Environment_Endpoints struct {
+}
+
+// +kcc:proto=google.cloud.dataplex.v1.Environment.InfrastructureSpec
+type Environment_InfrastructureSpec struct {
+	// Optional. Compute resources needed for analyze interactive workloads.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.InfrastructureSpec.compute
+	Compute *Environment_InfrastructureSpec_ComputeResources `json:"compute,omitempty"`
+
+	// Required. Software Runtime Configuration for analyze interactive
+	//  workloads.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.InfrastructureSpec.os_image
+	OSImage *Environment_InfrastructureSpec_OSImageRuntime `json:"osImage,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dataplex.v1.Environment.InfrastructureSpec.ComputeResources
+type Environment_InfrastructureSpec_ComputeResources struct {
+	// Optional. Size in GB of the disk. Default is 100 GB.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.InfrastructureSpec.ComputeResources.disk_size_gb
+	DiskSizeGB *int32 `json:"diskSizeGB,omitempty"`
+
+	// Optional. Total number of nodes in the sessions created for this
+	//  environment.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.InfrastructureSpec.ComputeResources.node_count
+	NodeCount *int32 `json:"nodeCount,omitempty"`
+
+	// Optional. Max configurable nodes.
+	//  If max_node_count > node_count, then auto-scaling is enabled.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.InfrastructureSpec.ComputeResources.max_node_count
+	MaxNodeCount *int32 `json:"maxNodeCount,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dataplex.v1.Environment.InfrastructureSpec.OsImageRuntime
+type Environment_InfrastructureSpec_OSImageRuntime struct {
+	// Required. Dataplex Image version.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.InfrastructureSpec.OsImageRuntime.image_version
+	ImageVersion *string `json:"imageVersion,omitempty"`
+
+	// Optional. List of Java jars to be included in the runtime environment.
+	//  Valid input includes Cloud Storage URIs to Jar binaries.
+	//  For example, gs://bucket-name/my/path/to/file.jar
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.InfrastructureSpec.OsImageRuntime.java_libraries
+	JavaLibraries []string `json:"javaLibraries,omitempty"`
+
+	// Optional. A list of python packages to be installed.
+	//  Valid formats include Cloud Storage URI to a PIP installable library.
+	//  For example, gs://bucket-name/my/path/to/lib.tar.gz
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.InfrastructureSpec.OsImageRuntime.python_packages
+	PythonPackages []string `json:"pythonPackages,omitempty"`
+
+	// Optional. Spark properties to provide configuration for use in sessions
+	//  created for this environment. The properties to set on daemon config
+	//  files. Property keys are specified in `prefix:property` format. The
+	//  prefix must be "spark".
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.InfrastructureSpec.OsImageRuntime.properties
+	Properties map[string]string `json:"properties,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dataplex.v1.Environment.SessionSpec
+type Environment_SessionSpec struct {
+	// Optional. The idle time configuration of the session. The session will be
+	//  auto-terminated at the end of this period.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.SessionSpec.max_idle_duration
+	MaxIdleDuration *string `json:"maxIdleDuration,omitempty"`
+
+	// Optional. If True, this causes sessions to be pre-created and available
+	//  for faster startup to enable interactive exploration use-cases. This
+	//  defaults to False to avoid additional billed charges. These can only be
+	//  set to True for the environment with name set to "default", and with
+	//  default configuration.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.SessionSpec.enable_fast_startup
+	EnableFastStartup *bool `json:"enableFastStartup,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dataplex.v1.Environment.SessionStatus
+type Environment_SessionStatus struct {
+}
+
+// +kcc:proto=google.cloud.dataplex.v1.Environment
+type EnvironmentObservedState struct {
+	// Output only. The relative resource name of the environment, of the form:
+	//  projects/{project_id}/locations/{location_id}/lakes/{lake_id}/environment/{environment_id}
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.name
+	Name *string `json:"name,omitempty"`
+
+	// Output only. System generated globally unique ID for the environment. This
+	//  ID will be different if the environment is deleted and re-created with the
+	//  same name.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.uid
+	Uid *string `json:"uid,omitempty"`
+
+	// Output only. Environment creation time.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.create_time
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// Output only. The time when the environment was last updated.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.update_time
+	UpdateTime *string `json:"updateTime,omitempty"`
+
+	// Output only. Current state of the environment.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.state
+	State *string `json:"state,omitempty"`
+
+	// Output only. Status of sessions created for this environment.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.session_status
+	SessionStatus *Environment_SessionStatusObservedState `json:"sessionStatus,omitempty"`
+
+	// Output only. URI Endpoints to access sessions associated with the
+	//  Environment.
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.endpoints
+	Endpoints *Environment_EndpointsObservedState `json:"endpoints,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dataplex.v1.Environment.Endpoints
+type Environment_EndpointsObservedState struct {
+	// Output only. URI to serve notebook APIs
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.Endpoints.notebooks
+	Notebooks *string `json:"notebooks,omitempty"`
+
+	// Output only. URI to serve SQL APIs
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.Endpoints.sql
+	SQL *string `json:"sql,omitempty"`
+}
+
+// +kcc:proto=google.cloud.dataplex.v1.Environment.SessionStatus
+type Environment_SessionStatusObservedState struct {
+	// Output only. Queries over sessions to mark whether the environment is
+	//  currently active or not
+	// +kcc:proto:field=google.cloud.dataplex.v1.Environment.SessionStatus.active
+	Active *bool `json:"active,omitempty"`
+}
 
 // +kcc:proto=google.cloud.dataplex.v1.AssetStatus
 type AssetStatus struct {
