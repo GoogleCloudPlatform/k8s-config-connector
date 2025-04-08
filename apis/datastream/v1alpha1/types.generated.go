d33c056335104bed2f9c7da39bb4bb82ba588be0
Jingyi Hu <jingyih@google.com>
2025-04-08 08:48:21 +0000

    DatastreamStream: fix types from generated

--- apis/datastream/v1alpha1/types.generated.go
+++ apis/datastream/v1alpha1/types.generated.go
@@ -158,64 +158,6 @@ type JsonFileFormat struct {
 	Compression *string `json:"compression,omitempty"`
 }
 
-// +kcc:proto=google.cloud.datastream.v1.MysqlColumn
-type MysqlColumn struct {
-	// Column name.
-	// +kcc:proto:field=google.cloud.datastream.v1.MysqlColumn.column
-	Column *string `json:"column,omitempty"`
-
-	// The MySQL data type. Full data types list can be found here:
-	//  https://dev.mysql.com/doc/refman/8.0/en/data-types.html
-	// +kcc:proto:field=google.cloud.datastream.v1.MysqlColumn.data_type
-	DataType *string `json:"dataType,omitempty"`
-
-	// Column length.
-	// +kcc:proto:field=google.cloud.datastream.v1.MysqlColumn.length
-	Length *int32 `json:"length,omitempty"`
-
-	// Column collation.
-	// +kcc:proto:field=google.cloud.datastream.v1.MysqlColumn.collation
-	Collation *string `json:"collation,omitempty"`
-
-	// Whether or not the column represents a primary key.
-	// +kcc:proto:field=google.cloud.datastream.v1.MysqlColumn.primary_key
-	PrimaryKey *bool `json:"primaryKey,omitempty"`
-
-	// Whether or not the column can accept a null value.
-	// +kcc:proto:field=google.cloud.datastream.v1.MysqlColumn.nullable
-	Nullable *bool `json:"nullable,omitempty"`
-
-	// The ordinal position of the column in the table.
-	// +kcc:proto:field=google.cloud.datastream.v1.MysqlColumn.ordinal_position
-	OrdinalPosition *int32 `json:"ordinalPosition,omitempty"`
-
-	// Column precision.
-	// +kcc:proto:field=google.cloud.datastream.v1.MysqlColumn.precision
-	Precision *int32 `json:"precision,omitempty"`
-
-	// Column scale.
-	// +kcc:proto:field=google.cloud.datastream.v1.MysqlColumn.scale
-	Scale *int32 `json:"scale,omitempty"`
-}
-
-// +kcc:proto=google.cloud.datastream.v1.MysqlDatabase
-type MysqlDatabase struct {
-	// Database name.
-	// +kcc:proto:field=google.cloud.datastream.v1.MysqlDatabase.database
-	Database *string `json:"database,omitempty"`
-
-	// Tables in the database.
-	// +kcc:proto:field=google.cloud.datastream.v1.MysqlDatabase.mysql_tables
-	MysqlTables []MysqlTable `json:"mysqlTables,omitempty"`
-}
-
-// +kcc:proto=google.cloud.datastream.v1.MysqlRdbms
-type MysqlRdbms struct {
-	// Mysql databases on the server
-	// +kcc:proto:field=google.cloud.datastream.v1.MysqlRdbms.mysql_databases
-	MysqlDatabases []MysqlDatabase `json:"mysqlDatabases,omitempty"`
-}
-
 // +kcc:proto=google.cloud.datastream.v1.MysqlSourceConfig
 type MysqlSourceConfig struct {
 	// MySQL objects to retrieve from the source.
@@ -272,19 +214,6 @@ type MysqlSSLConfig struct {
 	//  database server's certificate.
 	// +kcc:proto:field=google.cloud.datastream.v1.MysqlSslConfig.ca_certificate
 	CACertificate *string `json:"caCertificate,omitempty"`
-}
-
-// +kcc:proto=google.cloud.datastream.v1.MysqlTable
-type MysqlTable struct {
-	// Table name.
-	// +kcc:proto:field=google.cloud.datastream.v1.MysqlTable.table
-	Table *string `json:"table,omitempty"`
-
-	// MySQL columns in the database.
-	//  When unspecified as part of include/exclude objects, includes/excludes
-	//  everything.
-	// +kcc:proto:field=google.cloud.datastream.v1.MysqlTable.mysql_columns
-	MysqlColumns []MysqlColumn `json:"mysqlColumns,omitempty"`
 }
 
 // +kcc:proto=google.cloud.datastream.v1.OracleColumn
@@ -440,59 +369,6 @@ type OracleTable struct {
 	OracleColumns []OracleColumn `json:"oracleColumns,omitempty"`
 }
 
-// +kcc:proto=google.cloud.datastream.v1.PostgresqlColumn
-type PostgresqlColumn struct {
-	// Column name.
-	// +kcc:proto:field=google.cloud.datastream.v1.PostgresqlColumn.column
-	Column *string `json:"column,omitempty"`
-
-	// The PostgreSQL data type.
-	// +kcc:proto:field=google.cloud.datastream.v1.PostgresqlColumn.data_type
-	DataType *string `json:"dataType,omitempty"`
-
-	// Column length.
-	// +kcc:proto:field=google.cloud.datastream.v1.PostgresqlColumn.length
-	Length *int32 `json:"length,omitempty"`
-
-	// Column precision.
-	// +kcc:proto:field=google.cloud.datastream.v1.PostgresqlColumn.precision
-	Precision *int32 `json:"precision,omitempty"`
-
-	// Column scale.
-	// +kcc:proto:field=google.cloud.datastream.v1.PostgresqlColumn.scale
-	Scale *int32 `json:"scale,omitempty"`
-
-	// Whether or not the column represents a primary key.
-	// +kcc:proto:field=google.cloud.datastream.v1.PostgresqlColumn.primary_key
-	PrimaryKey *bool `json:"primaryKey,omitempty"`
-
-	// Whether or not the column can accept a null value.
-	// +kcc:proto:field=google.cloud.datastream.v1.PostgresqlColumn.nullable
-	Nullable *bool `json:"nullable,omitempty"`
-
-	// The ordinal position of the column in the table.
-	// +kcc:proto:field=google.cloud.datastream.v1.PostgresqlColumn.ordinal_position
-	OrdinalPosition *int32 `json:"ordinalPosition,omitempty"`
-}
-
-// +kcc:proto=google.cloud.datastream.v1.PostgresqlRdbms
-type PostgresqlRdbms struct {
-	// PostgreSQL schemas in the database server.
-	// +kcc:proto:field=google.cloud.datastream.v1.PostgresqlRdbms.postgresql_schemas
-	PostgresqlSchemas []PostgresqlSchema `json:"postgresqlSchemas,omitempty"`
-}
-
-// +kcc:proto=google.cloud.datastream.v1.PostgresqlSchema
-type PostgresqlSchema struct {
-	// Schema name.
-	// +kcc:proto:field=google.cloud.datastream.v1.PostgresqlSchema.schema
-	Schema *string `json:"schema,omitempty"`
-
-	// Tables in the schema.
-	// +kcc:proto:field=google.cloud.datastream.v1.PostgresqlSchema.postgresql_tables
-	PostgresqlTables []PostgresqlTable `json:"postgresqlTables,omitempty"`
-}
-
 // +kcc:proto=google.cloud.datastream.v1.PostgresqlSourceConfig
 type PostgresqlSourceConfig struct {
 	// PostgreSQL objects to include in the stream.
@@ -518,43 +394,6 @@ type PostgresqlSourceConfig struct {
 	//  used.
 	// +kcc:proto:field=google.cloud.datastream.v1.PostgresqlSourceConfig.max_concurrent_backfill_tasks
 	MaxConcurrentBackfillTasks *int32 `json:"maxConcurrentBackfillTasks,omitempty"`
-}
-
-// +kcc:proto=google.cloud.datastream.v1.PostgresqlTable
-type PostgresqlTable struct {
-	// Table name.
-	// +kcc:proto:field=google.cloud.datastream.v1.PostgresqlTable.table
-	Table *string `json:"table,omitempty"`
-
-	// PostgreSQL columns in the schema.
-	//  When unspecified as part of include/exclude objects,
-	//  includes/excludes everything.
-	// +kcc:proto:field=google.cloud.datastream.v1.PostgresqlTable.postgresql_columns
-	PostgresqlColumns []PostgresqlColumn `json:"postgresqlColumns,omitempty"`
-}
-
-// +kcc:proto=google.cloud.datastream.v1.SourceConfig
-type SourceConfig struct {
-	// Required. Source connection profile resoource.
-	//  Format: `projects/{project}/locations/{location}/connectionProfiles/{name}`
-	// +kcc:proto:field=google.cloud.datastream.v1.SourceConfig.source_connection_profile
-	SourceConnectionProfile *string `json:"sourceConnectionProfile,omitempty"`
-
-	// Oracle data source configuration.
-	// +kcc:proto:field=google.cloud.datastream.v1.SourceConfig.oracle_source_config
-	OracleSourceConfig *OracleSourceConfig `json:"oracleSourceConfig,omitempty"`
-
-	// MySQL data source configuration.
-	// +kcc:proto:field=google.cloud.datastream.v1.SourceConfig.mysql_source_config
-	MysqlSourceConfig *MysqlSourceConfig `json:"mysqlSourceConfig,omitempty"`
-
-	// PostgreSQL data source configuration.
-	// +kcc:proto:field=google.cloud.datastream.v1.SourceConfig.postgresql_source_config
-	PostgresqlSourceConfig *PostgresqlSourceConfig `json:"postgresqlSourceConfig,omitempty"`
-
-	// SQLServer data source configuration.
-	// +kcc:proto:field=google.cloud.datastream.v1.SourceConfig.sql_server_source_config
-	SQLServerSourceConfig *SQLServerSourceConfig `json:"sqlServerSourceConfig,omitempty"`
 }
 
 // +kcc:proto=google.cloud.datastream.v1.SqlServerChangeTables
@@ -662,65 +501,6 @@ type SQLServerTransactionLogs struct {
 type StaticServiceIPConnectivity struct {
 }
 
-// +kcc:proto=google.cloud.datastream.v1.Stream
-type Stream struct {
-
-	// Labels.
-	// +kcc:proto:field=google.cloud.datastream.v1.Stream.labels
-	Labels map[string]string `json:"labels,omitempty"`
-
-	// Required. Display name.
-	// +kcc:proto:field=google.cloud.datastream.v1.Stream.display_name
-	DisplayName *string `json:"displayName,omitempty"`
-
-	// Required. Source connection profile configuration.
-	// +kcc:proto:field=google.cloud.datastream.v1.Stream.source_config
-	SourceConfig *SourceConfig `json:"sourceConfig,omitempty"`
-
-	// Required. Destination connection profile configuration.
-	// +kcc:proto:field=google.cloud.datastream.v1.Stream.destination_config
-	DestinationConfig *DestinationConfig `json:"destinationConfig,omitempty"`
-
-	// The state of the stream.
-	// +kcc:proto:field=google.cloud.datastream.v1.Stream.state
-	State *string `json:"state,omitempty"`
-
-	// Automatically backfill objects included in the stream source
-	//  configuration. Specific objects can be excluded.
-	// +kcc:proto:field=google.cloud.datastream.v1.Stream.backfill_all
-	BackfillAll *Stream_BackfillAllStrategy `json:"backfillAll,omitempty"`
-
-	// Do not automatically backfill any objects.
-	// +kcc:proto:field=google.cloud.datastream.v1.Stream.backfill_none
-	BackfillNone *Stream_BackfillNoneStrategy `json:"backfillNone,omitempty"`
-
-	// Immutable. A reference to a KMS encryption key.
-	//  If provided, it will be used to encrypt the data.
-	//  If left blank, data will be encrypted using an internal Stream-specific
-	//  encryption key provisioned through KMS.
-	// +kcc:proto:field=google.cloud.datastream.v1.Stream.customer_managed_encryption_key
-	CustomerManagedEncryptionKey *string `json:"customerManagedEncryptionKey,omitempty"`
-}
-
-// +kcc:proto=google.cloud.datastream.v1.Stream.BackfillAllStrategy
-type Stream_BackfillAllStrategy struct {
-	// Oracle data source objects to avoid backfilling.
-	// +kcc:proto:field=google.cloud.datastream.v1.Stream.BackfillAllStrategy.oracle_excluded_objects
-	OracleExcludedObjects *OracleRdbms `json:"oracleExcludedObjects,omitempty"`
-
-	// MySQL data source objects to avoid backfilling.
-	// +kcc:proto:field=google.cloud.datastream.v1.Stream.BackfillAllStrategy.mysql_excluded_objects
-	MysqlExcludedObjects *MysqlRdbms `json:"mysqlExcludedObjects,omitempty"`
-
-	// PostgreSQL data source objects to avoid backfilling.
-	// +kcc:proto:field=google.cloud.datastream.v1.Stream.BackfillAllStrategy.postgresql_excluded_objects
-	PostgresqlExcludedObjects *PostgresqlRdbms `json:"postgresqlExcludedObjects,omitempty"`
-
-	// SQLServer data source objects to avoid backfilling
-	// +kcc:proto:field=google.cloud.datastream.v1.Stream.BackfillAllStrategy.sql_server_excluded_objects
-	SQLServerExcludedObjects *SQLServerRdbms `json:"sqlServerExcludedObjects,omitempty"`
-}
-
 // +kcc:proto=google.cloud.datastream.v1.Stream.BackfillNoneStrategy
 type Stream_BackfillNoneStrategy struct {
 }
@@ -746,28 +526,4 @@ type OracleSSLConfigObservedState struct {
 	//  this Connection-Profile.
 	// +kcc:proto:field=google.cloud.datastream.v1.OracleSslConfig.ca_certificate_set
 	CACertificateSet *bool `json:"caCertificateSet,omitempty"`
-}
-
-// +kcc:proto=google.cloud.datastream.v1.Stream
-type StreamObservedState struct {
-	// Output only. The stream's name.
-	// +kcc:proto:field=google.cloud.datastream.v1.Stream.name
-	Name *string `json:"name,omitempty"`
-
-	// Output only. The creation time of the stream.
-	// +kcc:proto:field=google.cloud.datastream.v1.Stream.create_time
-	CreateTime *string `json:"createTime,omitempty"`
-
-	// Output only. The last update time of the stream.
-	// +kcc:proto:field=google.cloud.datastream.v1.Stream.update_time
-	UpdateTime *string `json:"updateTime,omitempty"`
-
-	// Output only. Errors on the Stream.
-	// +kcc:proto:field=google.cloud.datastream.v1.Stream.errors
-	Errors []Error `json:"errors,omitempty"`
-
-	// Output only. If the stream was recovered, the time of the last recovery.
-	//  Note: This field is currently experimental.
-	// +kcc:proto:field=google.cloud.datastream.v1.Stream.last_recovery_time
-	LastRecoveryTime *string `json:"lastRecoveryTime,omitempty"`
 }
