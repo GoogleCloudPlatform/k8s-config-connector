69757c9b221690b1d8b08bd02a12d85b8b27a32f
Gemma Hou <yuhou@google.com>
2025-04-08 23:43:32 +0000

    conductor: "DataplexTask: Support for testing with mockgcp"

--- config/tests/samples/create/harness.go
+++ config/tests/samples/create/harness.go
@@ -867,6 +867,7 @@ func MaybeSkip(t *testing.T, name string, resources []*unstructured.Unstructured
 			case schema.GroupKind{Group: "dataplex.cnrm.cloud.google.com", Kind: "DataplexEntryGroup"}:
 			case schema.GroupKind{Group: "dataplex.cnrm.cloud.google.com", Kind: "DataplexLake"}:
 			case schema.GroupKind{Group: "dataplex.cnrm.cloud.google.com", Kind: "DataplexZone"}:
+			case schema.GroupKind{Group: "dataplex.cnrm.cloud.google.com", Kind: "DataplexTask"}:
 
 			case schema.GroupKind{Group: "documentai.cnrm.cloud.google.com", Kind: "DocumentAIProcessorVersion"}:
 
