f8371602109096ad509c53e09dbddc8db951cd8f
Jingyi Hu <jingyih@google.com>
2025-04-08 02:35:02 +0000

    chore: make manifests

--- pkg/gvks/supportedgvks/gvks_generated.go
+++ pkg/gvks/supportedgvks/gvks_generated.go
@@ -4807,6 +4807,16 @@ var SupportedGVKs = map[schema.GroupVersionKind]GVKMetadata{
 	{
 		Group:   "speech.cnrm.cloud.google.com",
 		Version: "v1alpha1",
+		Kind:    "SpeechPhraseSet",
+	}: {
+		Labels: map[string]string{
+			"cnrm.cloud.google.com/managed-by-kcc": "true",
+			"cnrm.cloud.google.com/system":         "true",
+		},
+	},
+	{
+		Group:   "speech.cnrm.cloud.google.com",
+		Version: "v1alpha1",
 		Kind:    "SpeechRecognizer",
 	}: {
 		Labels: map[string]string{
