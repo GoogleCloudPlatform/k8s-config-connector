d14f2ed2057a2f28688e7a9ca968233548bebc4e
Jingyi Hu <jingyih@google.com>
2025-04-25 06:11:20 +0000

    CloudSpeech: write externalRef in Update

--- pkg/controller/direct/speech/customclass_controller.go
+++ pkg/controller/direct/speech/customclass_controller.go
@@ -202,6 +202,7 @@ func (a *customClassAdapter) Update(ctx context.Context, updateOp *directbase.Up
 	if mapCtx.Err() != nil {
 		return mapCtx.Err()
 	}
+	status.ExternalRef = direct.LazyPtr(a.id.String())
 	return updateOp.UpdateStatus(ctx, status, nil)
 }
 
