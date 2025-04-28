9e12943bc54f5e2a55b788eeb826253b781ae13f
Henry Z <zhenry@google.com>
2025-04-28 20:21:37 +0000

    Add WorkflowExecutions mock server registrations

--- mockgcp/mock_http_roundtrip.go
+++ mockgcp/mock_http_roundtrip.go
@@ -250,6 +250,7 @@ func NewMockRoundTripper(ctx context.Context, k8sClient client.Client, storage s
 	services = append(services, mockapigee.New(env, storage))
 	services = append(services, mockbigqueryreservation.New(env, storage))
 	services = append(services, mockworkflows.New(env, storage))
+	services = append(services, mockworkflowexecution.New(env, storage))
 	services = append(services, mockcomposer.New(env, storage))
 	services = append(services, mockdocumentai.New(env, storage))
 	services = append(services, mockapphub.New(env, storage))
