0cf57a7704d4ef334b406173e474e7a756ef0563
justinsb <justinsb@google.com>
2025-05-16 08:22:55 -0400

    ServiceIdentity: support cloudservices.gserviceaccount.com
    This is an unusual P4SA, that is the P4SA for Managed Instance Groups (MIGs)

--- pkg/controller/direct/serviceusage/serviceidentity_controller.go
+++ pkg/controller/direct/serviceusage/serviceidentity_controller.go
@@ -26,11 +26,13 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common/projects"
 	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/serviceusage/v1beta1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/config"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/directbase"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/registry"
+	kccpredicate "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/predicate"
 
 	gcp "google.golang.org/api/serviceusage/v1beta1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -39,8 +41,38 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+// How long to wait for an LRO to complete
+const operationPollTimeout = 5 * time.Minute
+
+// GoogleApisServiceAgent was historically used by multiple services, but for more granular permission control we moved to P4SAs instead.
+// MIGs were tricky to move though, and so instead of switching to a P4SA, they adopted the GoogleApisServiceAgent as their P4SA
+// We still want to support this though, so we recognize this "magic value"
+const GoogleApisServiceAgent = "cloudservices.gserviceaccount.com"
+
 func init() {
-	registry.RegisterModel(krm.ServiceIdentityGVK, NewServiceIdentityModel)
+	reconcileGate := &SecretReconcileGate{}
+	registry.RegisterModelWithReconcileGate(krm.ServiceIdentityGVK, NewServiceIdentityModel, reconcileGate)
+}
+
+type SecretReconcileGate struct {
+	optIn kccpredicate.OptInToDirectReconciliation
+}
+
+var _ kccpredicate.ReconcileGate = &SecretReconcileGate{}
+
+func (r *SecretReconcileGate) ShouldReconcile(o *unstructured.Unstructured) bool {
+	if r.optIn.ShouldReconcile(o) {
+		return true
+	}
+	obj := &krm.ServiceIdentity{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(o.Object, &obj); err != nil {
+		return false
+	}
+	serviceName := direct.ValueOf(obj.Spec.ResourceID)
+	if serviceName == "" {
+		serviceName = obj.GetName()
+	}
+	return serviceName == GoogleApisServiceAgent
 }
 
 func NewServiceIdentityModel(ctx context.Context, config *config.ControllerConfig) (directbase.Model, error) {
@@ -74,9 +106,10 @@ func (m *serviceIdentityModel) AdapterForObject(ctx context.Context, reader clie
 		return nil, err
 	}
 	return &serviceIdentityAdapter{
-		gcpBeta: gcpBeta,
-		id:      id.(*krm.ServiceIdentityIdentity),
-		desired: obj,
+		gcpBeta:       gcpBeta,
+		id:            id.(*krm.ServiceIdentityIdentity),
+		desired:       obj,
+		projectMapper: m.config.ProjectMapper,
 	}, nil
 }
 
@@ -86,7 +119,9 @@ func (m *serviceIdentityModel) AdapterForURL(ctx context.Context, url string) (d
 }
 
 type serviceIdentityAdapter struct {
-	gcpBeta *gcp.APIService
+	projectMapper *projects.ProjectMapper
+	gcpBeta       *gcp.APIService
+
 	id      *krm.ServiceIdentityIdentity
 	desired *krm.ServiceIdentity
 	actual  *gcp.ServiceIdentity // This will be populated from status or after generation
@@ -122,55 +157,68 @@ func (a *serviceIdentityAdapter) Create(ctx context.Context, createOp *directbas
 	fqn := a.id.String()
 	log.V(2).Info("generating service identity", "fqn", fqn)
 
-	//   - parent: Name of the consumer and service to generate an identity for. The
-	//     `GenerateServiceIdentity` methods currently support projects, folders,
-	//     organizations. Example parents would be:
-	//     `projects/123/services/example.googleapis.com`
-	//     `folders/123/services/example.googleapis.com`
-	//     `organizations/123/services/example.googleapis.com`.
-
-	op, err := a.gcpBeta.Services.GenerateServiceIdentity(fqn).Context(ctx).Do()
-	if err != nil {
-		return fmt.Errorf("generating service identity for %q: %w", fqn, err)
-	}
-
-	var serviceIdentity *gcp.GoogleApiServiceusageV1beta1ServiceIdentity
-	for {
-		if op.Done {
-			klog.Warningf("RESPONSE IS %v", string(op.Response))
-			klog.Warningf("FULL OPERATION IS %v", op)
-			identity := &gcp.GoogleApiServiceusageV1beta1ServiceIdentity{}
-			if err := json.Unmarshal(op.Response, identity); err != nil {
-				return fmt.Errorf("error parsing response: %w", err)
-			}
-			serviceIdentity = identity
-			break
-		}
-		time.Sleep(2 * time.Second)
-		if err := ctx.Err(); err != nil {
-			return err
-		}
-		op, err = a.gcpBeta.Operations.Get(op.Name).Context(ctx).Do()
-		if err != nil {
-			return fmt.Errorf("waiting for service identity generation for %q: %w", fqn, err)
-		}
-	}
-
-	log.V(2).Info("successfully generated service identity", "fqn", fqn, "identity.email", serviceIdentity.Email, "identity.uniqueId", serviceIdentity.UniqueId)
-
-	// It really doesn't seem worthwhile to use the mapper here
-
 	status := &krm.ServiceIdentityStatus{}
+	if a.id.Service == GoogleApisServiceAgent {
+		// Special case: the MIG "P4SA" (which is also the "Google APIs Service Agent")
+		// Format is always <projectNumber>@cloudservices.gserviceaccount.com
 
-	// observedState := krm.ServiceIdentityObservedState{
-	// 	Email:    direct.ValueOf(serviceIdentity.Email),
-	// 	UniqueID: direct.ValueOf(serviceIdentity.UniqueId),
-	// }
+		projectNumber, err := a.projectMapper.LookupProjectNumber(ctx, a.id.ParentID.ProjectID)
+		if err != nil {
+			return fmt.Errorf("looking up project number for %q: %w", a.id.ParentID.ProjectID, err)
+		}
+		email := fmt.Sprintf("%d@cloudservices.gserviceaccount.com", projectNumber)
+		status.Email = &email
+	} else {
+		//   - parent: Name of the consumer and service to generate an identity for. The
+		//     `GenerateServiceIdentity` methods currently support projects, folders,
+		//     organizations. Example parents would be:
+		//     `projects/123/services/example.googleapis.com`
+		//     `folders/123/services/example.googleapis.com`
+		//     `organizations/123/services/example.googleapis.com`.
 
-	// status.ObservedState = &observedState
+		op, err := a.gcpBeta.Services.GenerateServiceIdentity(fqn).Context(ctx).Do()
+		if err != nil {
+			return fmt.Errorf("generating service identity for %q: %w", fqn, err)
+		}
 
-	status.Email = direct.LazyPtr(serviceIdentity.Email)
-	// status.UniqueID = direct.LazyPtr(serviceIdentity.UniqueId)
+		var serviceIdentity *gcp.GoogleApiServiceusageV1beta1ServiceIdentity
+		start := time.Now()
+		for {
+			if op.Done {
+				identity := &gcp.GoogleApiServiceusageV1beta1ServiceIdentity{}
+				if err := json.Unmarshal(op.Response, identity); err != nil {
+					return fmt.Errorf("error parsing response: %w", err)
+				}
+				serviceIdentity = identity
+				break
+			}
+			if time.Since(start) > operationPollTimeout {
+				return fmt.Errorf("timed out waiting for service identity generation for %q", fqn)
+			}
+			time.Sleep(2 * time.Second)
+			if err := ctx.Err(); err != nil {
+				return err
+			}
+			op, err = a.gcpBeta.Operations.Get(op.Name).Context(ctx).Do()
+			if err != nil {
+				return fmt.Errorf("waiting for service identity generation for %q: %w", fqn, err)
+			}
+		}
+
+		log.V(2).Info("successfully generated service identity", "fqn", fqn, "identity.email", serviceIdentity.Email, "identity.uniqueId", serviceIdentity.UniqueId)
+
+		// It really doesn't seem worthwhile to use the mapper here
+
+		// observedState := krm.ServiceIdentityObservedState{
+		// 	Email:    direct.ValueOf(serviceIdentity.Email),
+		// 	UniqueID: direct.ValueOf(serviceIdentity.UniqueId),
+		// }
+
+		// status.ObservedState = &observedState
+
+		status.Email = direct.LazyPtr(serviceIdentity.Email)
+		// status.UniqueID = direct.LazyPtr(serviceIdentity.UniqueId)
+	}
 
 	// status.ExternalRef = direct.LazyPtr(parent)
 	return createOp.UpdateStatus(ctx, status, nil)
