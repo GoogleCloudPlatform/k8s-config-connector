1e6ee44171eae2a0d29785b0826859fabb34db4b
Margubur Rahman <margubur@google.com>
2025-05-19 12:37:30 +0000

    add anywherecache types

--- pkg/gvks/supportedgvks/gvks_generated.go
+++ pkg/gvks/supportedgvks/gvks_generated.go
@@ -4941,6 +4941,16 @@ var SupportedGVKs = map[schema.GroupVersionKind]GVKMetadata{
 	},
 	{
 		Group:   "storage.cnrm.cloud.google.com",
+		Version: "v1alpha1",
+		Kind:    "StorageAnywhereCache",
+	}: {
+		Labels: map[string]string{
+			"cnrm.cloud.google.com/managed-by-kcc": "true",
+			"cnrm.cloud.google.com/system":         "true",
+		},
+	},
+	{
+		Group:   "storage.cnrm.cloud.google.com",
 		Version: "v1beta1",
 		Kind:    "StorageBucketAccessControl",
 	}: {
