7a27181e5869bc808d46880e3869ad1049657274
Andy Lu <luyul@google.com>
2025-05-26 11:02:23 +0200

    chore: re-generate CRD for bigqueryreservation API v1beta1

--- apis/bigqueryreservation/v1beta1/assignment_types.go
+++ apis/bigqueryreservation/v1beta1/assignment_types.go
@@ -85,6 +85,7 @@ type BigQueryReservationAssignmentObservedState struct {
 // +kubebuilder:printcolumn:name="Ready",JSONPath=".status.conditions[?(@.type=='Ready')].status",type="string",description="When 'True', the most recent reconcile of the resource succeeded"
 // +kubebuilder:printcolumn:name="Status",JSONPath=".status.conditions[?(@.type=='Ready')].reason",type="string",description="The reason for the value in 'Ready'"
 // +kubebuilder:printcolumn:name="Status Age",JSONPath=".status.conditions[?(@.type=='Ready')].lastTransitionTime",type="date",description="The last transition time for the value in 'Status'"
+// +kubebuilder:storageversion
 
 // BigQueryReservationAssignment is the Schema for the BigQueryReservationAssignment API
 // +k8s:openapi-gen=true
