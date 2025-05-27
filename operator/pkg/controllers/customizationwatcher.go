d08a88b87d2f12cf06e8d25fe794a357c13dc950
Jingyi Hu <jingyih@google.com>
2025-05-27 20:47:06 +0000

    Reapply "feat: support partial CRD installation"
    This reverts commit 2ed0c880aff207a69b0b084980761fe53bf47e57.

--- operator/pkg/controllers/customizationwatcher.go
+++ operator/pkg/controllers/customizationwatcher.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"sync"
 
+	customizev1alpha1 "github.com/GoogleCloudPlatform/k8s-config-connector/operator/pkg/apis/core/customize/v1alpha1"
 	customizev1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/operator/pkg/apis/core/customize/v1beta1"
 	corekcck8s "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/k8s"
 
@@ -39,6 +40,7 @@ var (
 		corekcck8s.ToGVR(customizev1beta1.ValidatingWebhookConfigurationCustomizationGroupVersionKind),
 		corekcck8s.ToGVR(customizev1beta1.MutatingWebhookConfigurationCustomizationGroupVersionKind),
 		corekcck8s.ToGVR(customizev1beta1.ControllerReconcilerGroupVersionKind),
+		corekcck8s.ToGVR(customizev1alpha1.APIGroupInstallationGroupVersionKind),
 	}
 	// NamespacedCustomizationCRsToWatch contains all the namspaced customization CRs to watch
 	NamespacedCustomizationCRsToWatch = []schema.GroupVersionResource{
