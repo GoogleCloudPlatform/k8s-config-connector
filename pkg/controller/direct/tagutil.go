9d3fc1396046b995cf8f8f8e1d93cea2b3bb9da9
Henry Z <zhenry@google.com>
2025-06-03 22:05:53 +0000

    Use external format in tagutil and no Parent and Shortname

--- pkg/controller/direct/tagutil.go
+++ pkg/controller/direct/tagutil.go
@@ -35,19 +35,22 @@ func Tags_ToProto(mapCtx *MapContext, in []*refs.TagValueRef) map[string]string 
 	for k, v := range in {
 		tokens := strings.Split(v.External, "/")
 		if len(tokens) == WantedExternalTokenCount {
-			// For `tagKeys/[tag_key_id]` mapped to `tagValues/[tag_value_id]`, stored as `tagKeys/[tag_key_id]/[tag_value_id]`
 			key := strings.Join(tokens[:2], "/")
-			value := tokens[2]
-			out[key] = "tagValues/" + value
-		} else if v.Parent != "" && v.ShortName != "" {
-			// For `[org id, project id, or project number]/[tag_key_shortname]` mapped to `[value_shortname]`, stored as `Parent` and `ShortName` respectively
-			out[v.Parent] = v.ShortName
+			if tokens[0] == "tagKeys" {
+				// For `tagKeys/[tag_key_id]` mapped to `tagValues/[tag_value_id]`, stored as `tagKeys/[tag_key_id]/[tag_value_id]`
+				value := tokens[2]
+				out[key] = "tagValues/" + value
+			} else {
+				// For `[org id, project id, or project number]/[tag_key_shortname]` mapped to `[value_shortname]`
+				value := tokens[2]
+				out[key] = value
+			}
 		} else {
 			// Shouldn't reach here if tags were well formed
 			if v.Name != "" {
-				klog.Warningf("Skipping TagValueRef %v because we cannot handle identification by Name.", k)
+				klog.Warningf("Skipping TagValueRef %v because we cannot handle identification by Name alone.", k)
 			} else {
-				klog.Warningf("Skipping TagValueRef %v because it is not well formed. External token count is %v but wanted %v, or Parent or ShortName are empty.", k, len(tokens), WantedExternalTokenCount)
+				klog.Warningf("Skipping TagValueRef %v because it is not well formed. External token count is %v but wanted %v", k, len(tokens), WantedExternalTokenCount)
 			}
 		}
 	}
@@ -68,13 +71,10 @@ func Tags_FromProto(mapCtx *MapContext, in map[string]string) []*refs.TagValueRe
 		tagValueRef := &refs.TagValueRef{}
 		keyTokens := strings.Split(k, "/")
 		valueTokens := strings.Split(v, "/")
-		if len(keyTokens) == WantedKeyTokenCount && keyTokens[0] == "tagKeys" && len(valueTokens) == WantedValueTokenCount && valueTokens[0] == "tagValues" {
+		if len(keyTokens) == WantedKeyTokenCount {
 			// For `tagKeys/[tag_key_id]` mapped to `tagValues/[tag_value_id]`
-			tagValueRef.External = fmt.Sprintf("%s/%s", k, valueTokens[1])
-		} else if len(keyTokens) == WantedKeyTokenCount {
-			// For `[org id, project id, or project number]/[tag_key_shortname]` mapped to `[value_shortname]`
-			tagValueRef.Parent = k
-			tagValueRef.ShortName = v
+			// and `[org id, project id, or project number]/[tag_key_shortname]` mapped to `[value_shortname]`
+			tagValueRef.External = fmt.Sprintf("%s/%s", keyTokens[0], keyTokens[1], valueTokens[len(valueTokens) - 1])
 		} else {
 			// Shouldn't reach here if tags were well formed
 			klog.Warningf("Skipping Tag because it is not well formed. Key token count is %v but wanted %v, value token count is %v but wanted %v. Consult Resource Manager Tags Overview for more info", len(keyTokens), WantedKeyTokenCount, len(valueTokens), WantedValueTokenCount)
