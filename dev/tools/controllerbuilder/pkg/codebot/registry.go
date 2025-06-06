ce39807e5c854178979b822ca0b9d33e13bdb633
Yuwen Ma <yuwenma@google.com>
2025-06-06 17:44:25 +0000

    add comments

--- dev/tools/controllerbuilder/pkg/codebot/registry.go
+++ dev/tools/controllerbuilder/pkg/codebot/registry.go
@@ -36,6 +36,8 @@ func GetAllTools() []Tool {
 	return tools
 }
 
+// GetFilteredTools returns a list of tools that match the filter criteria.
+// It excludes tools that are marked for exclusion and includes those that are marked for inclusion.
 func GetFilteredTools(filter Filter) []Tool {
 	filtered := make([]Tool, 0, len(tools))
 	for _, tool := range tools {
