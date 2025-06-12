b49adf366bb11df2e83d0da5345c135e545c8840
justinsb <justinsb@google.com>
2025-06-12 11:49:42 -0400

    mockgcp: fix servicedirectory service update

--- mockgcp/mockservicedirectory/servicev1.go
+++ mockgcp/mockservicedirectory/servicev1.go
@@ -77,6 +77,19 @@ func (s *RegistrationServiceV1) UpdateService(ctx context.Context, req *pb.Updat
 		return nil, err
 	}
 
+	paths := req.GetUpdateMask().GetPaths()
+	if len(paths) == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "update_mask must be provided")
+	}
+	for _, path := range paths {
+		switch path {
+		case "metadata":
+			obj.Metadata = req.GetService().GetMetadata()
+		default:
+			return nil, status.Errorf(codes.InvalidArgument, "update_mask path %q not valid", path)
+		}
+	}
+
 	if err := s.storage.Update(ctx, fqn, obj); err != nil {
 		return nil, err
 	}
