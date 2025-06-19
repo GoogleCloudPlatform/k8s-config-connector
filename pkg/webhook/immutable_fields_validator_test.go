c6fec453108fa3d1cd80c8add2c75db2b1654d7f
Alex Pana <8968914+acpana@users.noreply.github.com>
2025-06-19 23:08:15 +0000

    refactor: reference new apis
    Signed-off-by: Alex Pana <8968914+acpana@users.noreply.github.com>

--- pkg/webhook/immutable_fields_validator_test.go
+++ pkg/webhook/immutable_fields_validator_test.go
@@ -21,8 +21,8 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/iam/v1beta1"
 	corekccv1alpha1 "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/apis/core/v1alpha1"
-	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/apis/iam/v1beta1"
 	dclmetadata "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/dcl/metadata"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/gvks/supportedgvks"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/k8s"
