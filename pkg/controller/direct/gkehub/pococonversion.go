b0cefcce916bd4508f55da8fc8f14599094e4950
justinsb <justinsb@google.com>
2025-06-30 10:07:19 -0400

    Add deploymentConfigs to mappers

--- pkg/controller/direct/gkehub/pococonversion.go
+++ pkg/controller/direct/gkehub/pococonversion.go
@@ -30,7 +30,11 @@ func convertKRMtoAPI_Policycontroller(r *krm.FeaturemembershipPolicycontroller) 
 	return apiObj
 }
 
-func convertKRMtoAPI_PolicycontrollerHubConfig(r krm.FeaturemembershipPolicyControllerHubConfig) *featureapi.PolicyControllerHubConfig {
+func convertKRMtoAPI_PolicycontrollerHubConfig(r *krm.HubConfig) *featureapi.PolicyControllerHubConfig {
+	if r == nil {
+		return nil
+	}
+
 	apiObj := &featureapi.PolicyControllerHubConfig{}
 	if r.AuditIntervalSeconds != nil {
 		apiObj.AuditIntervalSeconds = int64(direct.ValueOf(r.AuditIntervalSeconds))
@@ -59,6 +63,9 @@ func convertKRMtoAPI_PolicycontrollerHubConfig(r krm.FeaturemembershipPolicyCont
 	if r.ReferentialRulesEnabled != nil {
 		apiObj.ReferentialRulesEnabled = *r.ReferentialRulesEnabled
 	}
+	if r.DeploymentConfigs != nil {
+		apiObj.DeploymentConfigs = convertKRMtoAPI_DeploymentConfigs(r.DeploymentConfigs)
+	}
 	return apiObj
 }
 
@@ -84,4 +91,116 @@ func convertKRMtoAPI_TemplateLibrary(r *krm.FeaturemembershipTemplateLibrary) *f
 		apiObj.Installation = *r.Installation
 	}
 	return apiObj
+}
+
+func convertKRMtoAPI_DeploymentConfigs(r *krm.PolicyControllerDeploymentConfigs) map[string]featureapi.PolicyControllerPolicyControllerDeploymentConfig {
+	if r == nil {
+		return nil
+	}
+	out := make(map[string]featureapi.PolicyControllerPolicyControllerDeploymentConfig)
+	if r.Admission != nil {
+		out["admission"] = convertKRMtoAPI_PolicyControllerDeploymentConfig(r.Admission)
+	}
+	if r.Audit != nil {
+		out["audit"] = convertKRMtoAPI_PolicyControllerDeploymentConfig(r.Audit)
+	}
+	if r.Mutation != nil {
+		out["mutation"] = convertKRMtoAPI_PolicyControllerDeploymentConfig(r.Mutation)
+	}
+	return out
+}
+
+func convertKRMtoAPI_PolicyControllerDeploymentConfig(in *krm.PolicyControllerDeploymentConfig) featureapi.PolicyControllerPolicyControllerDeploymentConfig {
+	out := featureapi.PolicyControllerPolicyControllerDeploymentConfig{}
+	if in == nil {
+		return out
+	}
+	if in.ContainerResources != nil {
+		out.ContainerResources = &featureapi.PolicyControllerResourceRequirements{
+			Limits:   convertKRMToAPI_PolicyControllerResourceList(in.ContainerResources.Limits),
+			Requests: convertKRMToAPI_PolicyControllerResourceList(in.ContainerResources.Requests),
+		}
+	}
+
+	if in.PodAffinity != nil {
+		out.PodAffinity = direct.ValueOf(in.PodAffinity)
+	}
+
+	if in.ReplicaCount != nil {
+		out.ReplicaCount = direct.ValueOf(in.ReplicaCount)
+	}
+
+	for _, v := range in.PodTolerations {
+		out.PodTolerations = append(out.PodTolerations, &featureapi.PolicyControllerToleration{
+			Effect:   direct.ValueOf(v.Effect),
+			Key:      direct.ValueOf(v.Key),
+			Operator: direct.ValueOf(v.Operator),
+			Value:    direct.ValueOf(v.Value),
+		})
+	}
+
+	return out
+}
+
+func convertKRMToAPI_PolicyControllerResourceList(in *krm.ResourceList) *featureapi.PolicyControllerResourceList {
+	if in == nil {
+		return nil
+	}
+	out := &featureapi.PolicyControllerResourceList{}
+	out.Cpu = direct.ValueOf(in.CPU)
+	out.Memory = direct.ValueOf(in.Memory)
+	return out
+}
+
+func DeploymentConfigs_FromProto(mapCtx *direct.MapContext, in map[string]featureapi.PolicyControllerPolicyControllerDeploymentConfig) *krm.PolicyControllerDeploymentConfigs {
+	if in == nil {
+		return nil
+	}
+	out := &krm.PolicyControllerDeploymentConfigs{}
+	if val, ok := in["admission"]; ok {
+		out.Admission = PolicyControllerDeploymentConfig_FromProto(mapCtx, &val)
+	}
+	if val, ok := in["audit"]; ok {
+		out.Audit = PolicyControllerDeploymentConfig_FromProto(mapCtx, &val)
+	}
+	if val, ok := in["mutation"]; ok {
+		out.Mutation = PolicyControllerDeploymentConfig_FromProto(mapCtx, &val)
+	}
+	return out
+}
+
+func PolicyControllerDeploymentConfig_FromProto(mapCtx *direct.MapContext, in *featureapi.PolicyControllerPolicyControllerDeploymentConfig) *krm.PolicyControllerDeploymentConfig {
+	if in == nil {
+		return nil
+	}
+	out := &krm.PolicyControllerDeploymentConfig{}
+
+	if in.ContainerResources != nil {
+		out.ContainerResources = &krm.ResourceRequirements{
+			Limits:   ResourceList_FromProto(in.ContainerResources.Limits),
+			Requests: ResourceList_FromProto(in.ContainerResources.Requests),
+		}
+	}
+	out.PodAffinity = direct.LazyPtr(in.PodAffinity)
+	out.ReplicaCount = direct.LazyPtr(in.ReplicaCount)
+	for _, v := range in.PodTolerations {
+		out.PodTolerations = append(out.PodTolerations, krm.PolicyControllerDeploymentConfig_Toleration{
+			Effect:   direct.LazyPtr(v.Effect),
+			Key:      direct.LazyPtr(v.Key),
+			Operator: direct.LazyPtr(v.Operator),
+			Value:    direct.LazyPtr(v.Value),
+		})
+	}
+
+	return out
+}
+
+func ResourceList_FromProto(in *featureapi.PolicyControllerResourceList) *krm.ResourceList {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ResourceList{}
+	out.Memory = direct.LazyPtr(in.Memory)
+	out.CPU = direct.LazyPtr(in.Cpu)
+	return out
 }
