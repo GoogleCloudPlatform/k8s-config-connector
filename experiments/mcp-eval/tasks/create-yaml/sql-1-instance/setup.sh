0fdc5ac8ccd542fc71a269fd062da16ee78bde75
Yuwen Ma <yuwenma@google.com>
2025-07-10 22:52:17 +0000

    make fmt

--- experiments/mcp-eval/tasks/create-yaml/sql-1-instance/setup.sh
+++ experiments/mcp-eval/tasks/create-yaml/sql-1-instance/setup.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 mkdir -p ${REPO_ROOT}/.build/tasks/sql-1-instance
 rm -f ${REPO_ROOT}/.build/tasks/sql-1-instance/*
