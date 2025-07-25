e6268379bbbcd6ca2dad4c9e3f8eb1286d364211
Anh Le <anhdle@google.com>
2025-07-25 22:42:42 +0000

    add spec/metadata label logic to SecretManagerSecret direct controller

--- pkg/controller/direct/secretmanager/secret_controller.go
+++ pkg/controller/direct/secretmanager/secret_controller.go
@@ -30,6 +30,7 @@ import (
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/common"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/directbase"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/registry"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/label"
 	"google.golang.org/api/option"
 	"google.golang.org/protobuf/types/known/fieldmaskpb"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -90,16 +91,21 @@ func (m *secretModel) client(ctx context.Context) (*gcp.Client, error) {
 
 func (m *secretModel) AdapterForObject(ctx context.Context, reader client.Reader, u *unstructured.Unstructured) (directbase.Adapter, error) {
 	obj := &krm.SecretManagerSecret{}
-	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, &obj); err != nil {
+
+	copied := u.DeepCopy()
+	if err := label.ComputeLabels(copied); err != nil {
+		return nil, err
+	}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(copied.Object, &obj); err != nil {
 		return nil, fmt.Errorf("error converting to %T: %w", obj, err)
 	}
 
-	id, err := krm.NewSecretIdentity(ctx, reader, obj, u)
+	id, err := krm.NewSecretIdentity(ctx, reader, obj, copied)
 	if err != nil {
 		return nil, err
 	}
 
-	if err = normalizeExternal(ctx, reader, u, obj); err != nil {
+	if err = normalizeExternal(ctx, reader, copied, obj); err != nil {
 		return nil, err
 	}
 
