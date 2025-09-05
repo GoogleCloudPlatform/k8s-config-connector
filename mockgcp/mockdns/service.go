8d1c74d2d696a609d4de7fed1b31299adadf4dcd
justinsb <justinsb@google.com>
2025-09-05 12:56:35 +0000

    mockgcp: implement dns managedZones

---
+++ mockgcp/mockdns/service.go
@@ -0,0 +1,69 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mockdns
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/httpmux"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/projects"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/mockgcpregistry"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/pkg/storage"
+	"google.golang.org/grpc"
+
+	pb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/google/cloud/dns/v1"
+)
+
+func init() {
+	mockgcpregistry.Register(New)
+}
+
+// MockService represents a mocked dns service.
+type MockService struct {
+	storage storage.Storage
+
+	projects projects.ProjectStore
+}
+
+// New creates a dnsService.
+func New(env *common.MockEnvironment, storage storage.Storage) mockgcpregistry.MockService {
+	s := &MockService{
+		storage:  storage,
+		projects: env.Projects,
+	}
+	return s
+}
+
+func (s *MockService) ExpectedHosts() []string {
+	return []string{"dns.googleapis.com"}
+}
+
+func (s *MockService) Register(grpcServer *grpc.Server) {
+	pb.RegisterManagedZonesServerServer(grpcServer, &managedZonesService{MockService: s})
+}
+
+func (s *MockService) NewHTTPMux(ctx context.Context, conn *grpc.ClientConn) (http.Handler, error) {
+	mux, err := httpmux.NewServeMux(ctx, conn, httpmux.Options{},
+		pb.RegisterManagedZonesServerHandler,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return mux, nil
+}
