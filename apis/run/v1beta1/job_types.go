8bba37363448d7926f01f4ba93666518f54478a5
Yuwen Ma <yuwenma@google.com>
2025-09-06 05:58:17 +0000

    resolve comments

--- apis/run/v1beta1/job_types.go
+++ apis/run/v1beta1/job_types.go
@@ -16,7 +16,6 @@ package v1beta1
 
 import (
 	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
-
 	vpcaccessv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/vpcaccess/v1beta1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/apis/k8s/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -96,7 +95,7 @@ type RunJobObservedState struct {
 	Etag *string `json:"etag,omitempty"`
 
 	// Output only. For a deleted resource, the time after which it will be
-	// permamently deleted.
+	// permanently deleted.
 	ExpireTime *string `json:"expireTime,omitempty"`
 
 	// Output only. Email address of the last authenticated modifier.
@@ -277,87 +276,11 @@ type Container struct {
 	// BaseImageURI *string `json:"baseImageURI,omitempty"`
 }
 
-// +kcc:proto=google.cloud.run.v2.ContainerPort
-type ContainerPort struct {
-	// If specified, used to specify which protocol to use.
-	//  Allowed values are "http1" and "h2c".
-	// +kcc:proto:field=google.cloud.run.v2.ContainerPort.name
-	Name *string `json:"name,omitempty"`
-
-	// Port number the container listens on.
-	//  This must be a valid TCP port number, 0 < container_port < 65536.
-	// +kcc:proto:field=google.cloud.run.v2.ContainerPort.container_port
-	ContainerPort *int32 `json:"containerPort,omitempty"`
-}
-
-// +kcc:proto=google.cloud.run.v2.EmptyDirVolumeSource
-type EmptyDirVolumeSource struct {
-	// The medium on which the data is stored. Acceptable values today is only
-	//  MEMORY or none. When none, the default will currently be backed by memory
-	//  but could change over time. +optional
-	// +kcc:proto:field=google.cloud.run.v2.EmptyDirVolumeSource.medium
-	Medium *string `json:"medium,omitempty"`
-
-	// Limit on the storage usable by this EmptyDir volume.
-	//  The size limit is also applicable for memory medium.
-	//  The maximum usage on memory medium EmptyDir would be the minimum value
-	//  between the SizeLimit specified here and the sum of memory limits of all
-	//  containers. The default is nil which means that the limit is undefined.
-	//  More info:
-	//  https://cloud.google.com/run/docs/configuring/in-memory-volumes#configure-volume.
-	//  Info in Kubernetes:
-	//  https://kubernetes.io/docs/concepts/storage/volumes/#emptydir
-	// +kcc:proto:field=google.cloud.run.v2.EmptyDirVolumeSource.size_limit
-	SizeLimit *string `json:"sizeLimit,omitempty"`
-}
-
-// +kcc:proto=google.cloud.run.v2.EnvVar
-type EnvVar struct {
-	// Required. Name of the environment variable. Must not exceed 32768
-	//  characters.
-	// +kcc:proto:field=google.cloud.run.v2.EnvVar.name
-	Name *string `json:"name,omitempty"`
-
-	// Literal value of the environment variable.
-	//  Defaults to "", and the maximum length is 32768 bytes.
-	//  Variable references are not supported in Cloud Run.
-	// +kcc:proto:field=google.cloud.run.v2.EnvVar.value
-	Value *string `json:"value,omitempty"`
-
-	// Source for the environment variable's value.
-	// +kcc:proto:field=google.cloud.run.v2.EnvVar.value_source
-	ValueSource *EnvVarSource `json:"valueSource,omitempty"`
-}
-
 // +kcc:proto=google.cloud.run.v2.EnvVarSource
 type EnvVarSource struct {
 	// Selects a secret and a specific version from Cloud Secret Manager.
 	// +kcc:proto:field=google.cloud.run.v2.EnvVarSource.secret_key_ref
 	SecretKeyRef *SecretKeySelector `json:"secretKeyRef,omitempty"`
-}
-
-// +kcc:proto=google.cloud.run.v2.ExecutionReference
-type ExecutionReference struct {
-	// Name of the execution.
-	// +kcc:proto:field=google.cloud.run.v2.ExecutionReference.name
-	Name *string `json:"name,omitempty"`
-
-	// Creation timestamp of the execution.
-	// +kcc:proto:field=google.cloud.run.v2.ExecutionReference.create_time
-	CreateTime *string `json:"createTime,omitempty"`
-
-	// Creation timestamp of the execution.
-	// +kcc:proto:field=google.cloud.run.v2.ExecutionReference.completion_time
-	CompletionTime *string `json:"completionTime,omitempty"`
-
-	// The deletion time of the execution. It is only
-	//  populated as a response to a Delete request.
-	// +kcc:proto:field=google.cloud.run.v2.ExecutionReference.delete_time
-	DeleteTime *string `json:"deleteTime,omitempty"`
-
-	// Status for the execution completion.
-	// +kcc:proto:field=google.cloud.run.v2.ExecutionReference.completion_status
-	CompletionStatus *string `json:"completionStatus,omitempty"`
 }
 
 // +kcc:proto=google.cloud.run.v2.ExecutionTemplate
@@ -412,38 +335,6 @@ type ExecutionTemplate struct {
 	Template *TaskTemplate `json:"template,omitempty"`
 }
 
-// +kcc:proto=google.cloud.run.v2.GCSVolumeSource
-type GCSVolumeSource struct {
-	// Cloud Storage Bucket name.
-	// +kcc:proto:field=google.cloud.run.v2.GCSVolumeSource.bucket
-	Bucket *string `json:"bucket,omitempty"`
-
-	// If true, the volume will be mounted as read only for all mounts.
-	// +kcc:proto:field=google.cloud.run.v2.GCSVolumeSource.read_only
-	ReadOnly *bool `json:"readOnly,omitempty"`
-
-	// A list of additional flags to pass to the gcsfuse CLI.
-	//  Options should be specified without the leading "--".
-	// +kcc:proto:field=google.cloud.run.v2.GCSVolumeSource.mount_options
-	MountOptions []string `json:"mountOptions,omitempty"`
-}
-
-// +kcc:proto=google.cloud.run.v2.GRPCAction
-type GrpcAction struct {
-	// Optional. Port number of the gRPC service. Number must be in the range 1 to
-	//  65535. If not specified, defaults to the exposed port of the container,
-	//  which is the value of container.ports[0].containerPort.
-	// +kcc:proto:field=google.cloud.run.v2.GRPCAction.port
-	Port *int32 `json:"port,omitempty"`
-
-	// Optional. Service is the name of the service to place in the gRPC
-	//  HealthCheckRequest (see
-	//  https://github.com/grpc/grpc/blob/master/doc/health-checking.md ). If this
-	//  is not specified, the default behavior is defined by gRPC.
-	// +kcc:proto:field=google.cloud.run.v2.GRPCAction.service
-	Service *string `json:"service,omitempty"`
-}
-
 // +kcc:proto=google.cloud.run.v2.HTTPGetAction
 type HTTPGetAction struct {
 	// Optional. Path to access on the HTTP server. Defaults to '/'.
@@ -460,39 +351,6 @@ type HTTPGetAction struct {
 	//  which is the value of container.ports[0].containerPort.
 	// +kcc:proto:field=google.cloud.run.v2.HTTPGetAction.port
 	// Port *int32 `json:"port,omitempty"`
-}
-
-// +kcc:proto=google.cloud.run.v2.HTTPHeader
-type HTTPHeader struct {
-	// Required. The header field name
-	// +kcc:proto:field=google.cloud.run.v2.HTTPHeader.name
-	Name *string `json:"name,omitempty"`
-
-	// Optional. The header field value
-	// +kcc:proto:field=google.cloud.run.v2.HTTPHeader.value
-	Value *string `json:"value,omitempty"`
-}
-
-// +kcc:proto=google.cloud.run.v2.NFSVolumeSource
-type NfsVolumeSource struct {
-	// Hostname or IP address of the NFS server
-	// +kcc:proto:field=google.cloud.run.v2.NFSVolumeSource.server
-	Server *string `json:"server,omitempty"`
-
-	// Path that is exported by the NFS server.
-	// +kcc:proto:field=google.cloud.run.v2.NFSVolumeSource.path
-	Path *string `json:"path,omitempty"`
-
-	// If true, the volume will be mounted as read only for all mounts.
-	// +kcc:proto:field=google.cloud.run.v2.NFSVolumeSource.read_only
-	ReadOnly *bool `json:"readOnly,omitempty"`
-}
-
-// +kcc:proto=google.cloud.run.v2.NodeSelector
-type NodeSelector struct {
-	// Required. GPU accelerator type to attach to an instance.
-	// +kcc:proto:field=google.cloud.run.v2.NodeSelector.accelerator
-	Accelerator *string `json:"accelerator,omitempty"`
 }
 
 // +kcc:proto=google.cloud.run.v2.Probe
@@ -616,15 +474,6 @@ type SecretVolumeSource struct {
 	DefaultMode *int32 `json:"defaultMode,omitempty"`
 }
 
-// +kcc:proto=google.cloud.run.v2.TCPSocketAction
-type TCPSocketAction struct {
-	// Optional. Port number to access on the container. Must be in the range 1 to
-	//  65535. If not specified, defaults to the exposed port of the container,
-	//  which is the value of container.ports[0].containerPort.
-	// +kcc:proto:field=google.cloud.run.v2.TCPSocketAction.port
-	Port *int32 `json:"port,omitempty"`
-}
-
 // +kcc:proto=google.cloud.run.v2.TaskTemplate
 type TaskTemplate struct {
 	// Holds the single container that defines the unit of execution for this
@@ -737,21 +586,6 @@ type Volume struct {
 	// Persistent storage backed by a Google Cloud Storage bucket.
 	// +kcc:proto:field=google.cloud.run.v2.Volume.gcs
 	// GCS *GCSVolumeSource `json:"gcs,omitempty"`
-}
-
-// +kcc:proto=google.cloud.run.v2.VolumeMount
-type VolumeMount struct {
-	// Required. This must match the Name of a Volume.
-	// +kcc:proto:field=google.cloud.run.v2.VolumeMount.name
-	Name *string `json:"name,omitempty"`
-
-	// Required. Path within the container at which the volume should be mounted.
-	//  Must not contain ':'. For Cloud SQL volumes, it can be left empty, or must
-	//  otherwise be `/cloudsql`. All instances defined in the Volume will be
-	//  available as `/cloudsql/[instance]`. For more information on Cloud SQL
-	//  volumes, visit https://cloud.google.com/sql/docs/mysql/connect-run
-	// +kcc:proto:field=google.cloud.run.v2.VolumeMount.mount_path
-	MountPath *string `json:"mountPath,omitempty"`
 }
 
 // +kcc:proto=google.cloud.run.v2.VpcAccess
--- apis/run/v1beta1/types.generated.go
+++ apis/run/v1beta1/types.generated.go
@@ -51,6 +51,171 @@ type Condition struct {
 	Severity *string `json:"severity,omitempty"`
 }
 
+// +kcc:proto=google.cloud.run.v2.ContainerPort
+type ContainerPort struct {
+	// If specified, used to specify which protocol to use.
+	//  Allowed values are "http1" and "h2c".
+	// +kcc:proto:field=google.cloud.run.v2.ContainerPort.name
+	Name *string `json:"name,omitempty"`
+
+	// Port number the container listens on.
+	//  This must be a valid TCP port number, 0 < container_port < 65536.
+	// +kcc:proto:field=google.cloud.run.v2.ContainerPort.container_port
+	ContainerPort *int32 `json:"containerPort,omitempty"`
+}
+
+// +kcc:proto=google.cloud.run.v2.EmptyDirVolumeSource
+type EmptyDirVolumeSource struct {
+	// The medium on which the data is stored. Acceptable values today is only
+	//  MEMORY or none. When none, the default will currently be backed by memory
+	//  but could change over time. +optional
+	// +kcc:proto:field=google.cloud.run.v2.EmptyDirVolumeSource.medium
+	Medium *string `json:"medium,omitempty"`
+
+	// Limit on the storage usable by this EmptyDir volume.
+	//  The size limit is also applicable for memory medium.
+	//  The maximum usage on memory medium EmptyDir would be the minimum value
+	//  between the SizeLimit specified here and the sum of memory limits of all
+	//  containers. The default is nil which means that the limit is undefined.
+	//  More info:
+	//  https://cloud.google.com/run/docs/configuring/in-memory-volumes#configure-volume.
+	//  Info in Kubernetes:
+	//  https://kubernetes.io/docs/concepts/storage/volumes/#emptydir
+	// +kcc:proto:field=google.cloud.run.v2.EmptyDirVolumeSource.size_limit
+	SizeLimit *string `json:"sizeLimit,omitempty"`
+}
+
+// +kcc:proto=google.cloud.run.v2.EnvVar
+type EnvVar struct {
+	// Required. Name of the environment variable. Must not exceed 32768
+	//  characters.
+	// +kcc:proto:field=google.cloud.run.v2.EnvVar.name
+	Name *string `json:"name,omitempty"`
+
+	// Literal value of the environment variable.
+	//  Defaults to "", and the maximum length is 32768 bytes.
+	//  Variable references are not supported in Cloud Run.
+	// +kcc:proto:field=google.cloud.run.v2.EnvVar.value
+	Value *string `json:"value,omitempty"`
+
+	// Source for the environment variable's value.
+	// +kcc:proto:field=google.cloud.run.v2.EnvVar.value_source
+	ValueSource *EnvVarSource `json:"valueSource,omitempty"`
+}
+
+// +kcc:proto=google.cloud.run.v2.ExecutionReference
+type ExecutionReference struct {
+	// Name of the execution.
+	// +kcc:proto:field=google.cloud.run.v2.ExecutionReference.name
+	Name *string `json:"name,omitempty"`
+
+	// Creation timestamp of the execution.
+	// +kcc:proto:field=google.cloud.run.v2.ExecutionReference.create_time
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// Creation timestamp of the execution.
+	// +kcc:proto:field=google.cloud.run.v2.ExecutionReference.completion_time
+	CompletionTime *string `json:"completionTime,omitempty"`
+
+	// The deletion time of the execution. It is only
+	//  populated as a response to a Delete request.
+	// +kcc:proto:field=google.cloud.run.v2.ExecutionReference.delete_time
+	DeleteTime *string `json:"deleteTime,omitempty"`
+
+	// Status for the execution completion.
+	// +kcc:proto:field=google.cloud.run.v2.ExecutionReference.completion_status
+	CompletionStatus *string `json:"completionStatus,omitempty"`
+}
+
+// +kcc:proto=google.cloud.run.v2.GCSVolumeSource
+type GCSVolumeSource struct {
+	// Cloud Storage Bucket name.
+	// +kcc:proto:field=google.cloud.run.v2.GCSVolumeSource.bucket
+	Bucket *string `json:"bucket,omitempty"`
+
+	// If true, the volume will be mounted as read only for all mounts.
+	// +kcc:proto:field=google.cloud.run.v2.GCSVolumeSource.read_only
+	ReadOnly *bool `json:"readOnly,omitempty"`
+
+	// A list of additional flags to pass to the gcsfuse CLI.
+	//  Options should be specified without the leading "--".
+	// +kcc:proto:field=google.cloud.run.v2.GCSVolumeSource.mount_options
+	MountOptions []string `json:"mountOptions,omitempty"`
+}
+
+// +kcc:proto=google.cloud.run.v2.GRPCAction
+type GrpcAction struct {
+	// Optional. Port number of the gRPC service. Number must be in the range 1 to
+	//  65535. If not specified, defaults to the exposed port of the container,
+	//  which is the value of container.ports[0].containerPort.
+	// +kcc:proto:field=google.cloud.run.v2.GRPCAction.port
+	Port *int32 `json:"port,omitempty"`
+
+	// Optional. Service is the name of the service to place in the gRPC
+	//  HealthCheckRequest (see
+	//  https://github.com/grpc/grpc/blob/master/doc/health-checking.md ). If this
+	//  is not specified, the default behavior is defined by gRPC.
+	// +kcc:proto:field=google.cloud.run.v2.GRPCAction.service
+	Service *string `json:"service,omitempty"`
+}
+
+// +kcc:proto=google.cloud.run.v2.HTTPHeader
+type HTTPHeader struct {
+	// Required. The header field name
+	// +kcc:proto:field=google.cloud.run.v2.HTTPHeader.name
+	Name *string `json:"name,omitempty"`
+
+	// Optional. The header field value
+	// +kcc:proto:field=google.cloud.run.v2.HTTPHeader.value
+	Value *string `json:"value,omitempty"`
+}
+
+// +kcc:proto=google.cloud.run.v2.NFSVolumeSource
+type NfsVolumeSource struct {
+	// Hostname or IP address of the NFS server
+	// +kcc:proto:field=google.cloud.run.v2.NFSVolumeSource.server
+	Server *string `json:"server,omitempty"`
+
+	// Path that is exported by the NFS server.
+	// +kcc:proto:field=google.cloud.run.v2.NFSVolumeSource.path
+	Path *string `json:"path,omitempty"`
+
+	// If true, the volume will be mounted as read only for all mounts.
+	// +kcc:proto:field=google.cloud.run.v2.NFSVolumeSource.read_only
+	ReadOnly *bool `json:"readOnly,omitempty"`
+}
+
+// +kcc:proto=google.cloud.run.v2.NodeSelector
+type NodeSelector struct {
+	// Required. GPU accelerator type to attach to an instance.
+	// +kcc:proto:field=google.cloud.run.v2.NodeSelector.accelerator
+	Accelerator *string `json:"accelerator,omitempty"`
+}
+
+// +kcc:proto=google.cloud.run.v2.TCPSocketAction
+type TCPSocketAction struct {
+	// Optional. Port number to access on the container. Must be in the range 1 to
+	//  65535. If not specified, defaults to the exposed port of the container,
+	//  which is the value of container.ports[0].containerPort.
+	// +kcc:proto:field=google.cloud.run.v2.TCPSocketAction.port
+	Port *int32 `json:"port,omitempty"`
+}
+
+// +kcc:proto=google.cloud.run.v2.VolumeMount
+type VolumeMount struct {
+	// Required. This must match the Name of a Volume.
+	// +kcc:proto:field=google.cloud.run.v2.VolumeMount.name
+	Name *string `json:"name,omitempty"`
+
+	// Required. Path within the container at which the volume should be mounted.
+	//  Must not contain ':'. For Cloud SQL volumes, it can be left empty, or must
+	//  otherwise be `/cloudsql`. All instances defined in the Volume will be
+	//  available as `/cloudsql/[instance]`. For more information on Cloud SQL
+	//  volumes, visit https://cloud.google.com/sql/docs/mysql/connect-run
+	// +kcc:proto:field=google.cloud.run.v2.VolumeMount.mount_path
+	MountPath *string `json:"mountPath,omitempty"`
+}
+
 // +kcc:observedstate:proto=google.cloud.run.v2.BuildInfo
 type BuildInfoObservedState struct {
 	// Output only. Entry point of the function when the image is a Cloud Run
