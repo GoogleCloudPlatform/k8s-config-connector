27dbade83b55762e23151fee4e6db8a056a67e0c
Andy Lu <luyul@google.com>
2025-09-17 17:28:49 +0200

    WIP: add resource KRM mapper and controller

---
+++ apis/tags/v1alpha1/tagslocationtagbinding_identity.go
@@ -0,0 +1,102 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// TagBindingIdentity defines the resource reference to TagsLocationTagBinding, which "External" field
+// holds the GCP identifier for the KRM object.
+type TagsLocationTagBindingIdentity struct {
+	parent *TagsLocationTagBindingParent
+	id     string
+}
+
+func (i *TagsLocationTagBindingIdentity) String() string {
+	return i.id
+}
+
+func (i *TagsLocationTagBindingIdentity) ID() string {
+	return i.id
+}
+
+type TagsLocationTagBindingParent struct {
+	parentResource string
+}
+
+func (i *TagsLocationTagBindingIdentity) Parent() string {
+	return i.parent.parentResource
+}
+
+// New builds a TagsLocationTagBindingIdentity from the Config Connector TagsLocationTagBinding object.
+func NewTagsLocationTagBindingIdentity(ctx context.Context, reader client.Reader, obj *TagsLocationTagBinding) (*TagsLocationTagBindingIdentity, error) {
+
+	// Get the resource name of the resource that the tag binds to.
+	// Uses the "spec.ParentRef"
+	var parent string
+
+	if obj.Spec.ParentRef.External != "" {
+		parent = obj.Spec.ParentRef.External
+	} else {
+		return nil, fmt.Errorf("cannot resolve parentRef")
+	}
+
+	// Get desired ID
+	resourceID := common.ValueOf(obj.Spec.ResourceID)
+
+	// Use approved External
+	externalRef := common.ValueOf(obj.Status.ExternalRef)
+	if externalRef != "" {
+		// Validate desired with actual
+		actualParent, actualResourceID, err := ParseTagsLocationTagBindingExternal(externalRef)
+		if err != nil {
+			return nil, err
+		}
+		if actualParent.parentResource != parent {
+			return nil, fmt.Errorf("spec.parentRef changed, expect %s, got %s", actualParent.parentResource, parent)
+		}
+
+		if resourceID != "" && actualResourceID != resourceID {
+			return nil, fmt.Errorf("cannot reset `spec.resourceID` to %s, since it has already assigned to %s",
+				resourceID, actualResourceID)
+		}
+	}
+	return &TagsLocationTagBindingIdentity{
+		parent: &TagsLocationTagBindingParent{
+			parentResource: parent,
+		},
+		id: resourceID,
+	}, nil
+}
+
+func ParseTagsLocationTagBindingExternal(external string) (parent *TagsLocationTagBindingParent, resourceID string, err error) {
+	tokens := strings.Split(external, "/")
+	if len(tokens) != 4 || tokens[0] != "tagBindings" || tokens[3] != "tagValues" {
+		return nil, "", fmt.Errorf("format of TagsLocationTagsLocationTagBinding external=%q was not known (use tagBindings/{{ResourceName}}/tagValues/{{tagValue}}", external)
+	}
+
+	parent = &TagsLocationTagBindingParent{
+		parentResource: tokens[1],
+	}
+
+	resourceID = external
+	return parent, resourceID, nil
+}
---
+++ pkg/controller/direct/resourcemanager/tagbinding_controller.go
@@ -0,0 +1,276 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package resourcemanager
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+
+	gcp "cloud.google.com/go/resourcemanager/apiv3"
+	tagspb "cloud.google.com/go/resourcemanager/apiv3/resourcemanagerpb"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/tags/v1alpha1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/config"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/common"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/directbase"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/registry"
+
+	"google.golang.org/api/option"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func init() {
+	registry.RegisterModel(krm.TagsLocationTagBindingGVK, NewTagBindingModel)
+}
+
+func NewTagBindingModel(ctx context.Context, config *config.ControllerConfig) (directbase.Model, error) {
+	return &modelTagsLocationTagBinding{config: *config}, nil
+}
+
+var _ directbase.Model = &modelTagsLocationTagBinding{}
+
+type modelTagsLocationTagBinding struct {
+	config config.ControllerConfig
+}
+
+func (m *modelTagsLocationTagBinding) client(ctx context.Context) (*gcp.Client, error) {
+	var opts []option.ClientOption
+	opts, err := m.config.RESTClientOptions()
+	if err != nil {
+		return nil, err
+	}
+	gcpClient, err := gcp.NewRESTClient(ctx, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("building TagBinding client: %w", err)
+	}
+	return gcpClient, err
+}
+
+func (m *modelTagsLocationTagBinding) AdapterForObject(ctx context.Context, reader client.Reader, u *unstructured.Unstructured) (directbase.Adapter, error) {
+	obj := &krm.TagsLocationTagBinding{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, &obj); err != nil {
+		return nil, fmt.Errorf("error converting to %T: %w", obj, err)
+	}
+
+	id, err := krm.NewTagBindingIdentity(ctx, reader, obj)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get tags GCP client
+	gcpClient, err := m.client(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &TagBindingAdapter{
+		id:        id,
+		gcpClient: gcpClient,
+		desired:   obj,
+	}, nil
+}
+
+func (m *modelTagsLocationTagBinding) AdapterForURL(ctx context.Context, url string) (directbase.Adapter, error) {
+	// TODO: Support URLs
+	return nil, nil
+}
+
+type TagBindingAdapter struct {
+	id        *krm.TagBindingIdentity
+	gcpClient *gcp.Client
+	desired   *krm.TagsLocationTagBinding
+	actual    *tagspb.TagBinding
+}
+
+var _ directbase.Adapter = &TagBindingAdapter{}
+
+// Find retrieves the GCP resource.
+// Return true means the object is found. This triggers Adapter `Update` call.
+// Return false means the object is not found. This triggers Adapter `Create` call.
+// Return a non-nil error requeues the requests.
+func (a *TagBindingAdapter) Find(ctx context.Context) (bool, error) {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("getting TagBinding", "name", a.id)
+
+	req := &tagspb.GetTagBindingRequest{Name: a.id.String()}
+	tagbindingpb, err := a.gcpClient.GetTagBinding(ctx, req)
+	if err != nil {
+		if direct.IsNotFound(err) {
+			return false, nil
+		}
+		return false, fmt.Errorf("getting TagBinding %q: %w", a.id, err)
+	}
+
+	a.actual = tagbindingpb
+	return true, nil
+}
+
+// Create creates the resource in GCP based on `spec` and update the Config Connector object `status` based on the GCP response.
+func (a *TagBindingAdapter) Create(ctx context.Context, createOp *directbase.CreateOperation) error {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("creating TagBinding", "name", a.id)
+	mapCtx := &direct.MapContext{}
+
+	desired := a.desired.DeepCopy()
+	resource := TagsLocationTagBindingSpec_ToProto(mapCtx, &desired.Spec)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+
+	// TODO(contributor): Complete the gcp "CREATE" or "INSERT" request.
+	req := &tagspb.CreateTagBindingRequest{
+		Parent:     a.id.Parent().String(),
+		TagBinding: resource,
+	}
+	op, err := a.gcpClient.CreateTagBinding(ctx, req)
+	if err != nil {
+		return fmt.Errorf("creating TagBinding %s: %w", a.id, err)
+	}
+	created, err := op.Wait(ctx)
+	if err != nil {
+		return fmt.Errorf("TagBinding %s waiting creation: %w", a.id, err)
+	}
+	log.V(2).Info("successfully created TagBinding", "name", a.id)
+
+	status := &krm.TagsLocationTagBindingStatus{}
+	status.ObservedState = TagsLocationTagBindingObservedState_FromProto(mapCtx, created)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+	status.ExternalRef = direct.LazyPtr(a.id.String())
+	return createOp.UpdateStatus(ctx, status, nil)
+}
+
+// Update updates the resource in GCP based on `spec` and update the Config Connector object `status` based on the GCP response.
+func (a *TagBindingAdapter) Update(ctx context.Context, updateOp *directbase.UpdateOperation) error {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("updating TagBinding", "name", a.id)
+	mapCtx := &direct.MapContext{}
+
+	desiredPb := TagsLocationTagBindingSpec_ToProto(mapCtx, &a.desired.DeepCopy().Spec)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+
+	paths := make(sets.Set[string])
+	// Option 1: This option is good for proto that has `field_mask` for output-only, immutable, required/optional.
+	// TODO(contributor): If choosing this option, remove the "Option 2" code.
+	{
+		var err error
+		paths, err = common.CompareProtoMessage(desiredPb, a.actual, common.BasicDiff)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Option 2: manually add all mutable fields.
+	// TODO(contributor): If choosing this option, remove the "Option 1" code.
+	{
+		if !reflect.DeepEqual(a.desired.Spec.DisplayName, a.actual.DisplayName) {
+			paths = paths.Insert("display_name")
+		}
+	}
+
+	updated := a.actual
+	if len(paths) == 0 {
+		log.V(2).Info("no field needs update", "name", a.id)
+	} else {
+		log.V(2).Info("fields need update", "name", a.id, "paths", paths)
+		updateMask := &fieldmaskpb.FieldMask{
+			Paths: sets.List(paths),
+		}
+
+		// TODO(contributor): Complete the gcp "UPDATE" or "PATCH" request.
+		req := &tagspb.UpdateTagBindingRequest{
+			Name:       a.id.String(),
+			UpdateMask: updateMask,
+			TagBinding: desiredPb,
+		}
+		op, err := a.gcpClient.UpdateTagBinding(ctx, req)
+		if err != nil {
+			return fmt.Errorf("updating TagBinding %s: %w", a.id, err)
+		}
+		updated, err = op.Wait(ctx)
+		if err != nil {
+			return fmt.Errorf("TagBinding %s waiting update: %w", a.id, err)
+		}
+		log.V(2).Info("successfully updated TagBinding", "name", a.id)
+	}
+
+	status := &krm.TagsLocationTagBindingStatus{}
+	status.ObservedState = TagsLocationTagBindingObservedState_FromProto(mapCtx, updated)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+	return updateOp.UpdateStatus(ctx, status, nil)
+}
+
+// Export maps the GCP object to a Config Connector resource `spec`.
+func (a *TagBindingAdapter) Export(ctx context.Context) (*unstructured.Unstructured, error) {
+	if a.actual == nil {
+		return nil, fmt.Errorf("Find() not called")
+	}
+	u := &unstructured.Unstructured{}
+
+	obj := &krm.TagsLocationTagBinding{}
+	mapCtx := &direct.MapContext{}
+	obj.Spec = direct.ValueOf(TagsLocationTagBindingSpec_FromProto(mapCtx, a.actual))
+	if mapCtx.Err() != nil {
+		return nil, mapCtx.Err()
+	}
+	obj.Spec.ProjectRef = &refs.ProjectRef{External: a.id.Parent().ProjectID}
+	obj.Spec.Location = a.id.Parent().Location
+	uObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+	if err != nil {
+		return nil, err
+	}
+
+	u.SetName(a.id.ID())
+	u.SetGroupVersionKind(krm.TagsLocationTagBindingGVK)
+
+	u.Object = uObj
+	return u, nil
+}
+
+// Delete the resource from GCP service when the corresponding Config Connector resource is deleted.
+func (a *TagBindingAdapter) Delete(ctx context.Context, deleteOp *directbase.DeleteOperation) (bool, error) {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("deleting TagBinding", "name", a.id)
+
+	req := &tagspb.DeleteTagBindingRequest{Name: a.id.String()}
+	op, err := a.gcpClient.DeleteTagBinding(ctx, req)
+	if err != nil {
+		if direct.IsNotFound(err) {
+			// Return success if not found (assume it was already deleted).
+			log.V(2).Info("skipping delete for non-existent TagBinding, assuming it was already deleted", "name", a.id)
+			return true, nil
+		}
+		return false, fmt.Errorf("deleting TagBinding %s: %w", a.id, err)
+	}
+	log.V(2).Info("successfully deleted TagBinding", "name", a.id)
+
+	err = op.Wait(ctx)
+	if err != nil {
+		return false, fmt.Errorf("waiting delete TagBinding %s: %w", a.id, err)
+	}
+	return true, nil
+}
