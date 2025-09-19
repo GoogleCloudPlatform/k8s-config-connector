8c983dc3569805989dff5f82bb67db85d3e113c6
Shavon Zhang <shavonz@google.com>
2025-09-19 19:08:06 +0000

    Add MaterializedView to MockGCP

--- tests/e2e/normalize.go
+++ tests/e2e/normalize.go
@@ -111,6 +111,15 @@ func normalizeKRMObject(t *testing.T, u *unstructured.Unstructured, project test
 	// Specific to BigQuery
 	visitor.replacePaths[".spec.access[].userByEmail"] = "user@google.com"
 
+	// Specific to Bigtable
+	visitor.replacePaths[".materializedViews[].etag"] = "abcdef0123A="
+	// TODO: these can be removed once the following fields have been released
+	metadata_type, _, _ := unstructured.NestedString(u.Object, "metadata", "@type")
+	if metadata_type == "type.googleapis.com/google.bigtable.admin.v2.CreateMaterializedViewMetadata" || metadata_type == "type.googleapis.com/google.bigtable.admin.v2.UpdateMaterializedViewMetadata" {
+		visitor.RemovePath(".metadata.requestTime")
+		visitor.RemovePath(".metadata.finishTime")
+	}
+
 	// Specific to Dataflow
 	visitor.sortAndDeduplicateSlices.Insert(".spec.additionalExperiments")
 
