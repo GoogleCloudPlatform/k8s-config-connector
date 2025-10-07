2a9db21069953c2101f8a54e5e86e7b2bba651a0
Tyler Reid <tylerreid@google.com>
2025-10-07 16:33:57 +0000

    feat: add mock tests

---
+++ mockgcp/mocklogging/link.go
@@ -0,0 +1,168 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law_or_agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// +tool:mockgcp-support
+// proto.service: google.logging.v2.ConfigServiceV2
+// proto.message: google.logging.v2.Link
+
+package mocklogging
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	pb "cloud.google.com/go/logging/apiv2/loggingpb"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/projects"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/pkg/operations"
+)
+
+func (s *configServiceV2) GetLink(ctx context.Context, req *pb.GetLinkRequest) (*pb.Link, error) {
+	name, err := s.parseLinkName(req.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	fqn := name.String()
+
+	obj := &pb.Link{}
+	if err := s.storage.Get(ctx, fqn, obj); err != nil {
+		if status.Code(err) == codes.NotFound {
+			return nil, status.Errorf(codes.NotFound, "Link `%s` does not exist", name.LinkName)
+		}
+		return nil, err
+	}
+
+	return obj, nil
+}
+
+func (s *configServiceV2) CreateLink(ctx context.Context, req *pb.CreateLinkRequest) (*operations.Operation, error) {
+	reqName := req.Parent + "/links/" + req.GetLinkId()
+	name, err := s.parseLinkName(reqName)
+	if err != nil {
+		return nil, err
+	}
+
+	fqn := name.String()
+
+	now := time.Now()
+	obj := proto.Clone(req.GetLink()).(*pb.Link)
+	obj.Name = fqn
+	obj.CreateTime = timestamppb.New(now)
+
+	s.populateDefaultsForLink(obj)
+
+	if err := s.storage.Create(ctx, fqn, obj); err != nil {
+		return nil, err
+	}
+
+	return s.operations.NewLRO(ctx)
+}
+
+func (s *configServiceV2) populateDefaultsForLink(obj *pb.Link) {
+	if obj.LifecycleState == pb.LifecycleState_LIFECYCLE_STATE_UNSPECIFIED {
+		obj.LifecycleState = pb.LifecycleState_ACTIVE
+	}
+}
+
+func (s *configServiceV2) DeleteLink(ctx context.Context, req *pb.DeleteLinkRequest) (*operations.Operation, error) {
+	name, err := s.parseLinkName(req.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	fqn := name.String()
+
+	deletedObj := &pb.Link{}
+	if err := s.storage.Delete(ctx, fqn, deletedObj); err != nil {
+		return nil, err
+	}
+
+	return s.operations.NewLRO(ctx)
+}
+
+type linkName struct {
+	// Only one of project/folder/organization/billingAccount should be set
+	project        *projects.ProjectData
+	folder         string
+	organization   string
+	billingAccount string
+
+	location   string
+	bucketName string
+	LinkName   string
+}
+
+func (n *linkName) String() string {
+	if n.organization != "" {
+		return "organizations/" + n.organization + "/locations/" + n.location + "/buckets/" + n.bucketName + "/links/" + n.LinkName
+	}
+	if n.folder != "" {
+		return "folders/" + n.folder + "/locations/" + n.location + "/buckets/" + n.bucketName + "/links/" + n.LinkName
+	}
+	if n.billingAccount != "" {
+		return "billingAccounts/" + n.billingAccount + "/locations/" + n.location + "/buckets/" + n.bucketName + "/links/" + n.LinkName
+	}
+	return "projects/" + n.project.ID + "/locations/" + n.location + "/buckets/" + n.bucketName + "/links/" + n.LinkName
+}
+
+// parseLinkName parses a string into a linkName.
+// The expected form is `projects/*/locations/*/buckets/*/links/*`.
+func (s *MockService) parseLinkName(name string) (*linkName, error) {
+	tokens := strings.Split(name, "/")
+	if len(tokens) == 8 && tokens[0] == "projects" && tokens[2] == "locations" && tokens[4] == "buckets" && tokens[6] == "links" {
+		project, err := s.Projects.GetProjectByID(tokens[1])
+		if err != nil {
+			return nil, err
+		}
+		name := &linkName{
+			project:    project,
+			location:   tokens[3],
+			bucketName: tokens[5],
+			LinkName:   tokens[7],
+		}
+		return name, nil
+	} else if len(tokens) == 8 && tokens[0] == "folders" && tokens[2] == "locations" && tokens[4] == "buckets" && tokens[6] == "links" {
+		name := &linkName{
+			folder:     tokens[1],
+			location:   tokens[3],
+			bucketName: tokens[5],
+			LinkName:   tokens[7],
+		}
+		return name, nil
+	} else if len(tokens) == 8 && tokens[0] == "organizations" && tokens[2] == "locations" && tokens[4] == "buckets" && tokens[6] == "links" {
+		name := &linkName{
+			organization: tokens[1],
+			location:     tokens[3],
+			bucketName:   tokens[5],
+			LinkName:     tokens[7],
+		}
+		return name, nil
+	} else if len(tokens) == 8 && tokens[0] == "billingAccounts" && tokens[2] == "locations" && tokens[4] == "buckets" && tokens[6] == "links" {
+		name := &linkName{
+			billingAccount: tokens[1],
+			location:       tokens[3],
+			bucketName:     tokens[5],
+			LinkName:       tokens[7],
+		}
+		return name, nil
+	} else {
+		return nil, status.Errorf(codes.InvalidArgument, "name %q is not valid", name)
+	}
+}
---
+++ mockgcp/mocklogging/link_test.go
@@ -0,0 +1,74 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package mocklogging
+
+import (
+	"context"
+	"testing"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	pb "cloud.google.com/go/logging/apiv2/loggingpb"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/projects"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/pkg/storage"
+)
+
+func TestCreateLink(t *testing.T) {
+	s := &MockService{
+		storage: storage.NewEmptyStorage(),
+	}
+	s.Projects.StoreProject(&projects.ProjectData{
+		ID:     "my-project",
+		Number: 123456,
+	})
+
+	ctx := context.Background()
+	req := &pb.CreateLinkRequest{
+		Parent: "projects/my-project/locations/global/buckets/my-bucket",
+		LinkId: "my-link",
+		Link: &pb.Link{
+			Description: "my link",
+		},
+	}
+	op, err := s.CreateLink(ctx, req)
+	if err != nil {
+		t.Fatalf("CreateLink failed: %v", err)
+	}
+
+	if op.Done {
+		t.Errorf("expected LRO to not be done")
+	}
+
+	_, err = s.GetLink(ctx, &pb.GetLinkRequest{Name: "projects/my-project/locations/global/buckets/my-bucket/links/my-link"})
+	if err != nil {
+		t.Fatalf("GetLink failed: %v", err)
+	}
+}
+
+func TestGetLinkNotFound(t *testing.T) {
+	s := &MockService{
+		storage: storage.NewEmptyStorage(),
+	}
+	s.Projects.StoreProject(&projects.ProjectData{
+		ID:     "my-project",
+		Number: 123456,
+	})
+
+	ctx := context.Background()
+	_, err := s.GetLink(ctx, &pb.GetLinkRequest{Name: "projects/my-project/locations/global/buckets/my-bucket/links/my-link"})
+	if status.Code(err) != codes.NotFound {
+		t.Fatalf("GetLink request should have failed with NotFound, but got %v", err)
+	}
+}
