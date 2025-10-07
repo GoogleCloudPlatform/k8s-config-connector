20c56c9c7329be559f98f3ada0b73edd306e1b7a
justinsb <justinsb@google.com>
2025-10-07 01:32:04 +0000

    FirestoreDatabase: add support for app_engine_integration_mode
    ```prompt
    Please read @docs/ai/implement-fuzzer-field.md and implement support for app_engine_integration_mode in for service=firestore, kind=FirestoreDatabase
    ```

--- pkg/controller/direct/firestore/firestoredatabase_mappings.go
+++ pkg/controller/direct/firestore/firestoredatabase_mappings.go
@@ -58,6 +58,7 @@ func FirestoreDatabaseSpec_FromProto(mapCtx *direct.MapContext, in *pb.Database)
 	out.LocationID = direct.LazyPtr(in.GetLocationId())
 	out.ConcurrencyMode = direct.Enum_FromProto(mapCtx, in.GetConcurrencyMode())
 	out.PointInTimeRecoveryEnablement = direct.Enum_FromProto(mapCtx, in.GetPointInTimeRecoveryEnablement())
+	out.AppEngineIntegrationMode = direct.Enum_FromProto(mapCtx, in.GetAppEngineIntegrationMode())
 	return out
 }
 
@@ -69,5 +70,6 @@ func FirestoreDatabaseSpec_ToProto(mapCtx *direct.MapContext, in *krm.FirestoreD
 	out.LocationId = direct.ValueOf(in.LocationID)
 	out.ConcurrencyMode = direct.Enum_ToProto[pb.Database_ConcurrencyMode](mapCtx, in.ConcurrencyMode)
 	out.PointInTimeRecoveryEnablement = direct.Enum_ToProto[pb.Database_PointInTimeRecoveryEnablement](mapCtx, in.PointInTimeRecoveryEnablement)
+	out.AppEngineIntegrationMode = direct.Enum_ToProto[pb.Database_AppEngineIntegrationMode](mapCtx, in.AppEngineIntegrationMode)
 	return out
 }
