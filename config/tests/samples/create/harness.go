f846306c4fcc51cfb74a7fd817c8b3341f678595
justinsb <justinsb@google.com>
2025-10-14 14:48:34 +0000

    tests: enable mockgcp testing with FirestoreIndex

--- config/tests/samples/create/harness.go
+++ config/tests/samples/create/harness.go
@@ -1000,6 +1000,7 @@ func MaybeSkip(t *testing.T, name string, resources []*unstructured.Unstructured
 			case schema.GroupKind{Group: "eventarc.cnrm.cloud.google.com", Kind: "EventarcGoogleChannelConfig"}:
 
 			case schema.GroupKind{Group: "firestore.cnrm.cloud.google.com", Kind: "FirestoreDatabase"}:
+			case schema.GroupKind{Group: "firestore.cnrm.cloud.google.com", Kind: "FirestoreIndex"}:
 
 			case schema.GroupKind{Group: "kms.cnrm.cloud.google.com", Kind: "KMSKeyRing"}:
 			case schema.GroupKind{Group: "kms.cnrm.cloud.google.com", Kind: "KMSCryptoKey"}:
