f8aa7cb67534c021700ee9e03455c1b7dab7fbe4
justinsb <justinsb@google.com>
2025-10-17 01:52:58 +0000

    recorder: reduce memory footprint of recorder

--- cmd/recorder/main.go
+++ cmd/recorder/main.go
@@ -21,6 +21,7 @@ import (
 	"net/http"
 	_ "net/http/pprof" // Needed to allow pprof server to accept requests
 	"os"
+	goruntime "runtime"
 	"strings"
 	"time"
 
@@ -42,9 +43,6 @@ import (
 	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	crlog "sigs.k8s.io/controller-runtime/pkg/log"
-
-	// Ensure built-in types are registered.
-	_ "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/register"
 )
 
 var (
@@ -158,6 +156,8 @@ func (r *MetricsRecorder) Run(ctx context.Context) error {
 			break
 		}
 
+		goruntime.GC() // Reduce memory by cleaning up unused objects before sleeping
+
 		time.Sleep(r.MetricInterval)
 
 		// Reset all metrics before updating.
@@ -214,10 +214,13 @@ func (r *MetricsRecorder) Run(ctx context.Context) error {
 			}
 
 			// Record stats.
+			groupKind := crdInfo.GVK.GroupKind().String()
 			for ns, stats := range nsAggStats {
 				for condition, count := range stats.lastConditionCounts {
-					logger.V(2).Info("posting metrics", "namespace", ns, "gvk", crdInfo.GVK.String(), "status", condition, "count", count)
-					appliedResources.WithLabelValues(ns, crdInfo.GVK.GroupKind().String(), condition).Set(float64(count))
+					if logger.V(2).Enabled() {
+						logger.V(2).Info("posting metrics", "namespace", ns, "gvk", crdInfo.GVK.String(), "status", condition, "count", count)
+					}
+					appliedResources.WithLabelValues(ns, groupKind, condition).Set(float64(count))
 				}
 			}
 		}
