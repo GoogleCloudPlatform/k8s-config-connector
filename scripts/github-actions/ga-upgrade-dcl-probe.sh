ca5f02c3306397b848c4d323b47577b3b4d3a885
justinsb <justinsb@google.com>
2025-10-21 13:38:00 +0000

    chore: don't install kubebuilder
    We shouldn't need kubebuilder, and we would prefer to manage dependencies
    through go modules where possible.

--- scripts/github-actions/ga-upgrade-dcl-probe.sh
+++ scripts/github-actions/ga-upgrade-dcl-probe.sh
@@ -18,9 +18,6 @@ set -o pipefail
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 source ${REPO_ROOT}/scripts/shared-vars-public.sh
 cd ${REPO_ROOT}
-source ${REPO_ROOT}/scripts/fetch_ext_bins.sh && \
-	fetch_tools && \
-	setup_envs
 
 echo "Running upgrade-dcl probe..."
 make upgrade-dcl ready-pr
