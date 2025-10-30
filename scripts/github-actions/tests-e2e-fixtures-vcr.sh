c030127aec36d736412f3a204de98d98aca6160a
justinsb <justinsb@google.com>
2025-10-30 19:19:47 +0000

    tests: create shared task to setup envtest
    Also pin to a known-good version, as latest is having trouble right now.

--- scripts/github-actions/tests-e2e-fixtures-vcr.sh
+++ scripts/github-actions/tests-e2e-fixtures-vcr.sh
@@ -20,8 +20,7 @@ set -o pipefail
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 cd ${REPO_ROOT}
 
-echo "Downloading envtest assets..."
-export KUBEBUILDER_ASSETS=$(go run sigs.k8s.io/controller-runtime/tools/setup-envtest@latest use -p path)
+. ${REPO_ROOT}/dev/tasks/setup-envtest
 
 cd ${REPO_ROOT}/
 echo "Running e2e fixtures test with vcr replay mode..."
