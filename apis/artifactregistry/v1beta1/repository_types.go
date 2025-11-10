ac0b05a851193c0c4658a76a4769a65516bce6e4
Roland Otta <151041622+roland-otta@users.noreply.github.com>
2025-11-10 11:43:12 +0100

    feat: migrate ArtifactRegistry Repository from Terraform to Direct controller
    - Add Direct controller API types and implementation
    - Implement reference resolution for backward compatibility
    - Add comprehensive E2E tests for all repository types
    - Maintain full API compatibility during migration
    - Prepare foundation for remote repository functionality
    
    Follows official migration guide and established patterns.
    Uses native GCP ArtifactRegistry API for improved performance.

---
+++ apis/artifactregistry/v1beta1/repository_types.go
@@ -0,0 +1,332 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+import (
+	refv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/apis/k8s/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var ArtifactRegistryRepositoryGVK = GroupVersion.WithKind("ArtifactRegistryRepository")
+
+// ArtifactRegistryRepositoryRef represents a reference to an ArtifactRegistryRepository
+type ArtifactRegistryRepositoryRef struct {
+	/* A reference to an externally managed ArtifactRegistryRepository.
+	Should be in the format `projects/[project_id]/locations/[location]/repositories/[repository_id]`. */
+	// +optional
+	External *string `json:"external,omitempty"`
+
+	/* The `name` of an `ArtifactRegistryRepository` resource. */
+	// +optional
+	Name *string `json:"name,omitempty"`
+
+	/* The `namespace` of an `ArtifactRegistryRepository` resource. */
+	// +optional
+	Namespace *string `json:"namespace,omitempty"`
+}
+
+// DeepCopyInto copies all data from in to out
+func (in *ArtifactRegistryRepositoryRef) DeepCopyInto(out *ArtifactRegistryRepositoryRef) {
+	if in.External != nil {
+		out.External = new(string)
+		*out.External = *in.External
+	}
+	if in.Name != nil {
+		out.Name = new(string)
+		*out.Name = *in.Name
+	}
+	if in.Namespace != nil {
+		out.Namespace = new(string)
+		*out.Namespace = *in.Namespace
+	}
+}
+
+// CleanupPolicies defines cleanup policies for package versions
+// +kcc:proto=google.devtools.artifactregistry.v1.CleanupPolicy
+type CleanupPolicies struct {
+	/* Policy action. Possible values: ["DELETE", "KEEP"]. */
+	// +optional
+	Action *string `json:"action,omitempty"`
+
+	/* Policy condition for matching versions. */
+	// +optional
+	Condition *CleanupPolicyCondition `json:"condition,omitempty"`
+
+	Id string `json:"id"`
+
+	/* Policy condition for retaining a minimum number of versions. May only be
+	specified with a Keep action. */
+	// +optional
+	MostRecentVersions *CleanupPolicyMostRecentVersions `json:"mostRecentVersions,omitempty"`
+}
+
+// CleanupPolicyCondition defines conditions for cleanup policies
+// +kcc:proto=google.devtools.artifactregistry.v1.CleanupPolicyCondition
+type CleanupPolicyCondition struct {
+	/* Match versions newer than a duration. */
+	// +optional
+	NewerThan *string `json:"newerThan,omitempty"`
+
+	/* Match versions older than a duration. */
+	// +optional
+	OlderThan *string `json:"olderThan,omitempty"`
+
+	/* Match versions by package prefix. Applied on any prefix match. */
+	// +optional
+	PackageNamePrefixes []string `json:"packageNamePrefixes,omitempty"`
+
+	/* Match versions by tag prefix. Applied on any prefix match. */
+	// +optional
+	TagPrefixes []string `json:"tagPrefixes,omitempty"`
+
+	/* Match versions by tag status. Default value: "ANY" Possible values: ["TAGGED", "UNTAGGED", "ANY"]. */
+	// +optional
+	TagState *string `json:"tagState,omitempty"`
+
+	/* Match versions by version name prefix. Applied on any prefix match. */
+	// +optional
+	VersionNamePrefixes []string `json:"versionNamePrefixes,omitempty"`
+}
+
+// CleanupPolicyMostRecentVersions defines policy for retaining recent versions
+// +kcc:proto=google.devtools.artifactregistry.v1.CleanupPolicyMostRecentVersions
+type CleanupPolicyMostRecentVersions struct {
+	/* Minimum number of versions to keep. */
+	// +optional
+	KeepCount *int64 `json:"keepCount,omitempty"`
+
+	/* Match versions by package prefix. Applied on any prefix match. */
+	// +optional
+	PackageNamePrefixes []string `json:"packageNamePrefixes,omitempty"`
+}
+
+// DockerConfig defines Docker repository configuration
+// +kcc:proto=google.devtools.artifactregistry.v1.DockerRepositoryConfig
+type DockerConfig struct {
+	/* The repository which enabled this flag prevents all tags from being modified, moved or deleted. This does not prevent tags from being created. */
+	// +optional
+	ImmutableTags *bool `json:"immutableTags,omitempty"`
+}
+
+// MavenConfig defines Maven repository configuration
+// +kcc:proto=google.devtools.artifactregistry.v1.MavenRepositoryConfig
+type MavenConfig struct {
+	/* Immutable. The repository with this flag will allow publishing the same
+	snapshot versions. */
+	// +optional
+	AllowSnapshotOverwrites *bool `json:"allowSnapshotOverwrites,omitempty"`
+
+	/* Immutable. Version policy defines the versions that the registry will accept. Default value: "VERSION_POLICY_UNSPECIFIED" Possible values: ["VERSION_POLICY_UNSPECIFIED", "RELEASE", "SNAPSHOT"]. */
+	// +optional
+	VersionPolicy *string `json:"versionPolicy,omitempty"`
+}
+
+// DockerRepository defines Docker remote repository settings
+// +kcc:proto=google.devtools.artifactregistry.v1.RemoteRepositoryConfig.DockerRepository
+type DockerRepository struct {
+	/* Immutable. Address of the remote repository. Default value: "DOCKER_HUB" Possible values: ["DOCKER_HUB"]. */
+	// +optional
+	PublicRepository *string `json:"publicRepository,omitempty"`
+}
+
+// MavenRepository defines Maven remote repository settings
+// +kcc:proto=google.devtools.artifactregistry.v1.RemoteRepositoryConfig.MavenRepository
+type MavenRepository struct {
+	/* Immutable. Address of the remote repository. Default value: "MAVEN_CENTRAL" Possible values: ["MAVEN_CENTRAL"]. */
+	// +optional
+	PublicRepository *string `json:"publicRepository,omitempty"`
+}
+
+// NpmRepository defines NPM remote repository settings
+// +kcc:proto=google.devtools.artifactregistry.v1.RemoteRepositoryConfig.NpmRepository
+type NpmRepository struct {
+	/* Immutable. Address of the remote repository. Default value: "NPMJS" Possible values: ["NPMJS"]. */
+	// +optional
+	PublicRepository *string `json:"publicRepository,omitempty"`
+}
+
+// PythonRepository defines Python remote repository settings
+// +kcc:proto=google.devtools.artifactregistry.v1.RemoteRepositoryConfig.PythonRepository
+type PythonRepository struct {
+	/* Immutable. Address of the remote repository. Default value: "PYPI" Possible values: ["PYPI"]. */
+	// +optional
+	PublicRepository *string `json:"publicRepository,omitempty"`
+}
+
+// RemoteRepositoryConfig defines configuration for remote repositories
+// +kcc:proto=google.devtools.artifactregistry.v1.RemoteRepositoryConfig
+type RemoteRepositoryConfig struct {
+	/* Immutable. The description of the remote source. */
+	// +optional
+	Description *string `json:"description,omitempty"`
+
+	/* Immutable. Specific settings for a Docker remote repository. */
+	// +optional
+	DockerRepository *DockerRepository `json:"dockerRepository,omitempty"`
+
+	/* Immutable. Specific settings for a Maven remote repository. */
+	// +optional
+	MavenRepository *MavenRepository `json:"mavenRepository,omitempty"`
+
+	/* Immutable. Specific settings for an Npm remote repository. */
+	// +optional
+	NpmRepository *NpmRepository `json:"npmRepository,omitempty"`
+
+	/* Immutable. Specific settings for a Python remote repository. */
+	// +optional
+	PythonRepository *PythonRepository `json:"pythonRepository,omitempty"`
+}
+
+// UpstreamPolicy defines upstream policies for virtual repositories
+// +kcc:proto=google.devtools.artifactregistry.v1.UpstreamPolicy
+type UpstreamPolicy struct {
+	/* The user-provided ID of the upstream policy. */
+	// +optional
+	Id *string `json:"id,omitempty"`
+
+	/* Entries with a greater priority value take precedence in the pull order. */
+	// +optional
+	Priority *int64 `json:"priority,omitempty"`
+
+	/* A reference to the repository resource, for example:
+	"projects/p1/locations/us-central1/repositories/repo1". */
+	// +optional
+	RepositoryRef *ArtifactRegistryRepositoryRef `json:"repositoryRef,omitempty"`
+}
+
+// VirtualRepositoryConfig defines configuration for virtual repositories
+// +kcc:proto=google.devtools.artifactregistry.v1.VirtualRepositoryConfig
+type VirtualRepositoryConfig struct {
+	/* Policies that configure the upstream artifacts distributed by the Virtual
+	Repository. Upstream policies cannot be set on a standard repository. */
+	// +optional
+	UpstreamPolicies []UpstreamPolicy `json:"upstreamPolicies,omitempty"`
+}
+
+// ArtifactRegistryRepositorySpec defines the desired state of ArtifactRegistryRepository
+// +kcc:proto=google.devtools.artifactregistry.v1.Repository
+type ArtifactRegistryRepositorySpec struct {
+	/* Cleanup policies for this repository. Cleanup policies indicate when
+	certain package versions can be automatically deleted.
+	Map keys are policy IDs supplied by users during policy creation. They must
+	unique within a repository and be under 128 characters in length. */
+	// +optional
+	CleanupPolicies []CleanupPolicies `json:"cleanupPolicies,omitempty"`
+
+	/* If true, the cleanup pipeline is prevented from deleting versions in this
+	repository. */
+	// +optional
+	CleanupPolicyDryRun *bool `json:"cleanupPolicyDryRun,omitempty"`
+
+	/* The user-provided description of the repository. */
+	// +optional
+	Description *string `json:"description,omitempty"`
+
+	/* Docker repository config contains repository level configuration for the repositories of docker type. */
+	// +optional
+	DockerConfig *DockerConfig `json:"dockerConfig,omitempty"`
+
+	/* Immutable. The format of packages that are stored in the repository. Supported formats
+	can be found [here](https://cloud.google.com/artifact-registry/docs/supported-formats).
+	You can only create alpha formats if you are a member of the
+	[alpha user group](https://cloud.google.com/artifact-registry/docs/supported-formats#alpha-access). */
+	// +required
+	Format string `json:"format"`
+
+	/* The customer managed encryption key that's used to encrypt the
+	contents of the Repository. */
+	// +optional
+	KmsKeyRef *refv1beta1.KMSCryptoKeyRef `json:"kmsKeyRef,omitempty"`
+
+	/* Immutable. The name of the location this repository is located in. */
+	// +required
+	Location string `json:"location"`
+
+	/* MavenRepositoryConfig is maven related repository details.
+	Provides additional configuration details for repositories of the maven
+	format type. */
+	// +optional
+	MavenConfig *MavenConfig `json:"mavenConfig,omitempty"`
+
+	/* Immutable. The mode configures the repository to serve artifacts from different sources. Default value: "STANDARD_REPOSITORY" Possible values: ["STANDARD_REPOSITORY", "VIRTUAL_REPOSITORY", "REMOTE_REPOSITORY"]. */
+	// +optional
+	Mode *string `json:"mode,omitempty"`
+
+	/* Immutable. Configuration specific for a Remote Repository. */
+	// +optional
+	RemoteRepositoryConfig *RemoteRepositoryConfig `json:"remoteRepositoryConfig,omitempty"`
+
+	/* Immutable. Optional. The repositoryId of the resource. Used for creation and acquisition. When unset, the value of `metadata.name` is used as the default. */
+	// +optional
+	ResourceID *string `json:"resourceID,omitempty"`
+
+	/* Configuration specific for a Virtual Repository. */
+	// +optional
+	VirtualRepositoryConfig *VirtualRepositoryConfig `json:"virtualRepositoryConfig,omitempty"`
+}
+
+// ArtifactRegistryRepositoryStatus defines the config connector machine state of ArtifactRegistryRepository
+type ArtifactRegistryRepositoryStatus struct {
+	/* Conditions represent the latest available observations of the
+	   object's current state. */
+	Conditions []v1alpha1.Condition `json:"conditions,omitempty"`
+
+	// The time when the repository was created.
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// The name of the repository, for example: "repo1".
+	Name *string `json:"name,omitempty"`
+
+	// ObservedGeneration is the generation of the resource that was most recently observed by the Config Connector controller. If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource.
+	ObservedGeneration *int64 `json:"observedGeneration,omitempty"`
+
+	// The time when the repository was last updated.
+	UpdateTime *string `json:"updateTime,omitempty"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:resource:categories=gcp,shortName=gcpartifactregistryrepository;gcpartifactregistryrepositories
+// +kubebuilder:subresource:status
+// +kubebuilder:metadata:labels="cnrm.cloud.google.com/managed-by-kcc=true";"cnrm.cloud.google.com/system=true"
+// +kubebuilder:printcolumn:name="Age",JSONPath=".metadata.creationTimestamp",type="date"
+// +kubebuilder:printcolumn:name="Ready",JSONPath=".status.conditions[?(@.type=='Ready')].status",type="string",description="When 'True', the most recent reconcile of the resource succeeded"
+// +kubebuilder:printcolumn:name="Status",JSONPath=".status.conditions[?(@.type=='Ready')].reason",type="string",description="The reason for the value in 'Ready'"
+// +kubebuilder:printcolumn:name="Status Age",JSONPath=".status.conditions[?(@.type=='Ready')].lastTransitionTime",type="date",description="The last transition time for the value in 'Status'"
+
+// ArtifactRegistryRepository is the Schema for the artifactregistry API
+// +k8s:openapi-gen=true
+// +kubebuilder:storageversion
+type ArtifactRegistryRepository struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ArtifactRegistryRepositorySpec   `json:"spec"`
+	Status ArtifactRegistryRepositoryStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ArtifactRegistryRepositoryList contains a list of ArtifactRegistryRepository
+type ArtifactRegistryRepositoryList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ArtifactRegistryRepository `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ArtifactRegistryRepository{}, &ArtifactRegistryRepositoryList{})
+}
---
+++ pkg/controller/direct/artifactregistry/repository_controller.go
@@ -0,0 +1,299 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package artifactregistry
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/artifactregistry/v1beta1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/config"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/common"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/directbase"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/registry"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/label"
+
+	artifactregistry "cloud.google.com/go/artifactregistry/apiv1"
+	"cloud.google.com/go/artifactregistry/apiv1/artifactregistrypb"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	ctrlName = "artifactregistry-controller"
+)
+
+func init() {
+	registry.RegisterModel(krm.ArtifactRegistryRepositoryGVK, NewModel)
+}
+
+func NewModel(ctx context.Context, config *config.ControllerConfig) (directbase.Model, error) {
+	return &model{config: config}, nil
+}
+
+var _ directbase.Model = &model{}
+
+type model struct {
+	config *config.ControllerConfig
+}
+
+func (m *model) client(ctx context.Context) (*artifactregistry.Client, error) {
+	opts, err := m.config.RESTClientOptions()
+	if err != nil {
+		return nil, fmt.Errorf("error getting REST client options: %w", err)
+	}
+
+	gcpClient, err := artifactregistry.NewRESTClient(ctx, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("building artifact registry client: %w", err)
+	}
+
+	return gcpClient, nil
+}
+
+func (m *model) AdapterForObject(ctx context.Context, reader client.Reader, u *unstructured.Unstructured) (directbase.Adapter, error) {
+	obj := &krm.ArtifactRegistryRepository{}
+	copied := u.DeepCopy()
+	if err := label.ComputeLabels(copied); err != nil {
+		return nil, err
+	}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(copied.Object, &obj); err != nil {
+		return nil, fmt.Errorf("error converting to %T: %w", obj, err)
+	}
+
+	if err := ResolveArtifactRegistryRepositoryRefs(ctx, reader, obj); err != nil {
+		return nil, err
+	}
+
+	id, err := NewArtifactRegistryRepositoryIdentity(ctx, reader, obj, copied)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get GCP client
+	gcpClient, err := m.client(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &Adapter{
+		id:        id,
+		gcpClient: gcpClient,
+		desired:   obj,
+	}, nil
+}
+
+func (m *model) AdapterForURL(ctx context.Context, url string) (directbase.Adapter, error) {
+	// TODO: Support reading resources by URL
+	return nil, nil
+}
+
+type Adapter struct {
+	id        *ArtifactRegistryRepositoryIdentity
+	gcpClient *artifactregistry.Client
+	desired   *krm.ArtifactRegistryRepository
+	actual    *artifactregistrypb.Repository
+}
+
+var _ directbase.Adapter = &Adapter{}
+
+func (a *Adapter) Find(ctx context.Context) (bool, error) {
+	log := klog.FromContext(ctx).WithName(ctrlName)
+	log.V(2).Info("getting ArtifactRegistry repository", "name", a.id.FullyQualifiedName())
+
+	req := &artifactregistrypb.GetRepositoryRequest{Name: a.id.FullyQualifiedName()}
+
+	repositoryPB, err := a.gcpClient.GetRepository(ctx, req)
+	if err != nil {
+		if direct.IsNotFound(err) {
+			return false, nil
+		}
+		return false, fmt.Errorf("getting ArtifactRegistry repository %q: %w", a.id.FullyQualifiedName(), err)
+	}
+
+	a.actual = repositoryPB
+	return true, nil
+}
+
+func (a *Adapter) Create(ctx context.Context, createOp *directbase.CreateOperation) error {
+	log := klog.FromContext(ctx).WithName(ctrlName)
+	log.V(2).Info("creating ArtifactRegistry repository", "name", a.id.FullyQualifiedName())
+
+	mapCtx := &direct.MapContext{}
+
+	desired := a.desired.DeepCopy()
+	resource := ArtifactRegistryRepositorySpec_ToProto(mapCtx, &desired.Spec)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+
+	resource.Name = a.id.FullyQualifiedName()
+
+	req := &artifactregistrypb.CreateRepositoryRequest{
+		Parent:       a.id.Parent.String(),
+		RepositoryId: a.id.ResourceID,
+		Repository:   resource,
+	}
+
+	_, err := a.gcpClient.CreateRepository(ctx, req)
+	if err != nil {
+		return fmt.Errorf("creating ArtifactRegistry repository %q: %w", a.id.FullyQualifiedName(), err)
+	}
+
+	log.V(2).Info("successfully created ArtifactRegistry repository", "name", a.id.FullyQualifiedName())
+	return nil
+}
+
+func (a *Adapter) Update(ctx context.Context, updateOp *directbase.UpdateOperation) error {
+	log := klog.FromContext(ctx).WithName(ctrlName)
+	log.V(2).Info("updating ArtifactRegistry repository", "name", a.id.FullyQualifiedName())
+
+	if a.actual == nil {
+		return fmt.Errorf("Update called without a prior call to Find")
+	}
+
+	mapCtx := &direct.MapContext{}
+
+	desired := a.desired.DeepCopy()
+	resource := ArtifactRegistryRepositorySpec_ToProto(mapCtx, &desired.Spec)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+
+	resource.Name = a.id.FullyQualifiedName()
+
+	// Validate critical immutable fields explicitly
+	if a.desired.Spec.Format != "" && a.actual.Format.String() != "" && a.desired.Spec.Format != a.actual.Format.String() {
+		return fmt.Errorf("field 'spec.format' is immutable and cannot be updated from %q to %q", a.actual.Format.String(), a.desired.Spec.Format)
+	}
+
+	if a.desired.Spec.Location != "" && a.actual.GetName() != "" {
+		// Extract location from actual name path (projects/.../locations/LOCATION/repositories/...)
+		if !strings.Contains(a.actual.GetName(), "locations/"+a.desired.Spec.Location+"/") {
+			return fmt.Errorf("field 'spec.location' is immutable and cannot be updated")
+		}
+	}
+
+	if a.desired.Spec.Mode != nil && a.actual.Mode.String() != "" && *a.desired.Spec.Mode != a.actual.Mode.String() {
+		return fmt.Errorf("field 'spec.mode' is immutable and cannot be updated from %q to %q", a.actual.Mode.String(), *a.desired.Spec.Mode)
+	}
+
+	// Validate Maven config immutable fields
+	if a.desired.Spec.MavenConfig != nil && a.actual.GetMavenConfig() != nil {
+		if a.desired.Spec.MavenConfig.AllowSnapshotOverwrites != nil &&
+			a.actual.GetMavenConfig().GetAllowSnapshotOverwrites() != *a.desired.Spec.MavenConfig.AllowSnapshotOverwrites {
+			return fmt.Errorf("field 'spec.mavenConfig.allowSnapshotOverwrites' is immutable and cannot be updated from %v to %v",
+				a.actual.GetMavenConfig().GetAllowSnapshotOverwrites(), *a.desired.Spec.MavenConfig.AllowSnapshotOverwrites)
+		}
+		if a.desired.Spec.MavenConfig.VersionPolicy != nil &&
+			a.actual.GetMavenConfig().GetVersionPolicy().String() != *a.desired.Spec.MavenConfig.VersionPolicy {
+			return fmt.Errorf("field 'spec.mavenConfig.versionPolicy' is immutable and cannot be updated from %q to %q",
+				a.actual.GetMavenConfig().GetVersionPolicy().String(), *a.desired.Spec.MavenConfig.VersionPolicy)
+		}
+	}
+
+	// Validate RemoteRepositoryConfig is immutable (entire block)
+	if (a.desired.Spec.RemoteRepositoryConfig != nil) != (a.actual.GetRemoteRepositoryConfig() != nil) {
+		return fmt.Errorf("field 'spec.remoteRepositoryConfig' is immutable and cannot be changed after creation")
+	}
+
+	// Use common.CompareProtoMessage to automatically detect changes and validate immutable fields
+	paths, err := common.CompareProtoMessage(resource, a.actual, common.BasicDiff)
+	if err != nil {
+		return fmt.Errorf("comparing desired and actual state: %w", err)
+	}
+
+	if len(paths) == 0 {
+		log.V(2).Info("no field needs update", "name", a.id.FullyQualifiedName())
+		return nil
+	}
+
+	updateMask := &fieldmaskpb.FieldMask{}
+	updateMask.Paths = sets.List(paths)
+
+	req := &artifactregistrypb.UpdateRepositoryRequest{
+		Repository: resource,
+		UpdateMask: updateMask,
+	}
+
+	repository, err := a.gcpClient.UpdateRepository(ctx, req)
+	if err != nil {
+		return fmt.Errorf("updating ArtifactRegistry repository %q: %w", a.id.FullyQualifiedName(), err)
+	}
+
+	log.V(2).Info("successfully updated ArtifactRegistry repository", "name", a.id.FullyQualifiedName())
+
+	a.actual = repository
+	return nil
+}
+
+func (a *Adapter) Export(ctx context.Context) (*unstructured.Unstructured, error) {
+	if a.actual == nil {
+		return nil, fmt.Errorf("Find() not called")
+	}
+	u := &unstructured.Unstructured{}
+
+	obj := &krm.ArtifactRegistryRepository{}
+	mapCtx := &direct.MapContext{}
+	obj.Spec = direct.ValueOf(ArtifactRegistryRepositorySpec_FromProto(mapCtx, a.actual))
+	if mapCtx.Err() != nil {
+		return nil, mapCtx.Err()
+	}
+	// Set status fields directly (flat structure for backward compatibility)
+	if a.actual.CreateTime != nil {
+		obj.Status.CreateTime = direct.PtrTo(a.actual.CreateTime.AsTime().Format(time.RFC3339))
+	}
+	if a.actual.UpdateTime != nil {
+		obj.Status.UpdateTime = direct.PtrTo(a.actual.UpdateTime.AsTime().Format(time.RFC3339))
+	}
+	// Extract just the repository name from the full path
+	if a.actual.Name != "" {
+		parts := strings.Split(a.actual.Name, "/")
+		if len(parts) > 0 {
+			obj.Status.Name = direct.PtrTo(parts[len(parts)-1])
+		}
+	}
+	uObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+	if err != nil {
+		return nil, err
+	}
+	u.Object = uObj
+	return u, nil
+}
+
+func (a *Adapter) Delete(ctx context.Context, deleteOp *directbase.DeleteOperation) (bool, error) {
+	log := klog.FromContext(ctx).WithName(ctrlName)
+	log.V(2).Info("deleting ArtifactRegistry repository", "name", a.id.FullyQualifiedName())
+
+	req := &artifactregistrypb.DeleteRepositoryRequest{Name: a.id.FullyQualifiedName()}
+
+	_, err := a.gcpClient.DeleteRepository(ctx, req)
+	if err != nil {
+		if direct.IsNotFound(err) {
+			log.V(2).Info("ArtifactRegistry repository was not found, assuming already deleted", "name", a.id.FullyQualifiedName())
+			return true, nil
+		}
+		return false, fmt.Errorf("deleting ArtifactRegistry repository %q: %w", a.id.FullyQualifiedName(), err)
+	}
+
+	log.V(2).Info("successfully deleted ArtifactRegistry repository", "name", a.id.FullyQualifiedName())
+	return true, nil
+}
