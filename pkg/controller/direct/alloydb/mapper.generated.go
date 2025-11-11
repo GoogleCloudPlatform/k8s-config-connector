eefc2865df80f8cd75cdb5f73925fcd8bf49e0a9
Nancy Hong <nancyhong@google.com>
2025-11-11 23:42:46 +0000

    fix spacing

--- pkg/controller/direct/alloydb/mapper.generated.go
+++ pkg/controller/direct/alloydb/mapper.generated.go
@@ -590,22 +590,22 @@ func Instance_ClientConnectionConfig_ToProto(mapCtx *direct.MapContext, in *krmv
 	return out
 }
 func Instance_ConnectionPoolConfig_FromProto(mapCtx *direct.MapContext, in *pb.Instance_ConnectionPoolConfig) *krmv1beta1.Instance_ConnectionPoolConfig {
-        if in == nil {
-                return nil
-        }
-        out := &krmv1beta1.Instance_ConnectionPoolConfig{}
-        out.Enabled = direct.LazyPtr(in.GetEnabled())
-        out.Flags = in.Flags
-        return out
+	if in == nil {
+		return nil
+	}
+	out := &krmv1beta1.Instance_ConnectionPoolConfig{}
+	out.Enabled = direct.LazyPtr(in.GetEnabled())
+	out.Flags = in.Flags
+	return out
 }
 func Instance_ConnectionPoolConfig_ToProto(mapCtx *direct.MapContext, in *krmv1beta1.Instance_ConnectionPoolConfig) *pb.Instance_ConnectionPoolConfig {
-        if in == nil {
-                return nil
-        }
-        out := &pb.Instance_ConnectionPoolConfig{}
-        out.Enabled = direct.ValueOf(in.Enabled)
-        out.Flags = in.Flags
-        return out
+	if in == nil {
+		return nil
+	}
+	out := &pb.Instance_ConnectionPoolConfig{}
+	out.Enabled = direct.ValueOf(in.Enabled)
+	out.Flags = in.Flags
+	return out
 }
 func Instance_InstanceNetworkConfig_FromProto(mapCtx *direct.MapContext, in *pb.Instance_InstanceNetworkConfig) *krmv1beta1.Instance_InstanceNetworkConfig {
 	if in == nil {
