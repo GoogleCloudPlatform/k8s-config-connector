08182a661ead27318d2dc7a7b281f9f915413b6a
Yuwen Ma <yuwenma@google.com>
2025-11-21 21:30:40 +0000

    chore: apply ref and identity to dataplex, fix labels

--- apis/dataplex/v1alpha1/entrytype_reference.go
+++ apis/dataplex/v1alpha1/entrytype_reference.go
@@ -16,17 +16,14 @@ package v1alpha1
 
 import (
 	"context"
-	"fmt"
 
 	refsv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
-	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/k8s"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-var _ refsv1beta1.ExternalNormalizer = &EntryTypeRef{}
+var _ refsv1beta1.Ref = &EntryTypeRef{}
 
 // EntryTypeRef defines the resource reference to DataplexEntryType, which "External" field
 // holds the GCP identifier for the KRM object.
@@ -42,42 +39,33 @@ type EntryTypeRef struct {
 	Namespace string `json:"namespace,omitempty"`
 }
 
-// NormalizedExternal provision the "External" value for other resource that depends on DataplexEntryType.
-// If the "External" is given in the other resource's spec.DataplexEntryTypeRef, the given value will be used.
-// Otherwise, the "Name" and "Namespace" will be used to query the actual DataplexEntryType object from the cluster.
-func (r *EntryTypeRef) NormalizedExternal(ctx context.Context, reader client.Reader, otherNamespace string) (string, error) {
-	if r.External != "" && r.Name != "" {
-		return "", fmt.Errorf("cannot specify both name and external on %s reference", DataplexEntryTypeGVK.Kind)
-	}
-	// From given External
-	if r.External != "" {
-		if _, _, err := ParseEntryTypeExternal(r.External); err != nil {
-			return "", err
-		}
-		return r.External, nil
-	}
+func (r *EntryTypeRef) GetGVK() schema.GroupVersionKind {
+	return DataplexEntryTypeGVK
+}
 
-	// From the Config Connector object
-	if r.Namespace == "" {
-		r.Namespace = otherNamespace
+func (r *EntryTypeRef) GetNamespacedName() types.NamespacedName {
+	return types.NamespacedName{
+		Name:      r.Name,
+		Namespace: r.Namespace,
 	}
-	key := types.NamespacedName{Name: r.Name, Namespace: r.Namespace}
-	u := &unstructured.Unstructured{}
-	u.SetGroupVersionKind(DataplexEntryTypeGVK)
-	if err := reader.Get(ctx, key, u); err != nil {
-		if apierrors.IsNotFound(err) {
-			return "", k8s.NewReferenceNotFoundError(u.GroupVersionKind(), key)
-		}
-		return "", fmt.Errorf("reading referenced %s %s: %w", DataplexEntryTypeGVK, key, err)
+}
+
+func (r *EntryTypeRef) GetExternal() string {
+	return r.External
+}
+
+func (r *EntryTypeRef) SetExternal(ref string) {
+	r.External = ref
+}
+
+func (r *EntryTypeRef) ValidateExternal(ref string) error {
+	id := &EntryTypeIdentity{}
+	if err := id.FromExternal(ref); err != nil {
+		return err
 	}
-	// Get external from status.externalRef. This is the most trustworthy place.
-	actualExternalRef, _, err := unstructured.NestedString(u.Object, "status", "externalRef")
-	if err != nil {
-		return "", fmt.Errorf("reading status.externalRef: %w", err)
-	}
-	if actualExternalRef == "" {
-		return "", k8s.NewReferenceNotReadyError(u.GroupVersionKind(), key)
-	}
-	r.External = actualExternalRef
-	return r.External, nil
+	return nil
+}
+
+func (r *EntryTypeRef) Normalize(ctx context.Context, reader client.Reader, defaultNamespace string) error {
+	return refsv1beta1.Normalize(ctx, reader, r, defaultNamespace)
 }
