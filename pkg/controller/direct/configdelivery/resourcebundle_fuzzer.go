acbd0fff9ec0a486da76aa8425a19f462b13e300
Yuwen Ma <yuwenma@google.com>
2025-12-01 19:12:33 +0000

    add new fields from go.mod bump

--- pkg/controller/direct/configdelivery/resourcebundle_fuzzer.go
+++ pkg/controller/direct/configdelivery/resourcebundle_fuzzer.go
@@ -37,7 +37,7 @@ func resourceBundleFuzzer() fuzztesting.KRMFuzzer {
 	f.StatusField(".create_time")
 	f.StatusField(".update_time")
 
-	f.UnimplementedFields.Insert(".name")
+	f.Unimplemented_Identity(".name")
 
 	return f
 }
