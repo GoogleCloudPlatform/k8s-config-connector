c666c67781567d81d3cdf7a83f021b55feb89c47
Gemma Hou <yuhou@google.com>
2025-12-19 21:05:06 +0000

    test: missing normal fields

--- scripts/github-actions/validate-no-missingfields.sh
+++ scripts/github-actions/validate-no-missingfields.sh
@@ -26,7 +26,7 @@ MISSING_REFERENCE_STRING="neither 'external' nor 'name' are set"
 # Get the lines added to missingfields.txt in the current branch compared to master
 added_lines=$(git diff --unified=0 $BASE_COMMIT..$COMMIT_HEAD "${MISSING_FIELDS_FILE}" | grep '^+[^+]' | sed 's/^+//')
 # Filter for missing reference fields
-added_reference_lines=$(echo "${added_lines}" | grep "${MISSING_REFERENCE_STRING}")
+added_reference_lines=$(echo "${added_lines}" | grep "${MISSING_REFERENCE_STRING}" || true)
 
 if [[ -n "${added_reference_lines}" ]]; then
   echo "ERROR: Additions to ${MISSING_FIELDS_FILE} are not allowed."
