5b4e972d877d9885624372740b33facb30276f0e
codebot-robot <codebot-robot@google.com>
2026-01-14 15:43:24 +0000

    operator: restart manager pods on MCL config change
    This change ensures that changes to spec.experiments.multiClusterLease in the ConfigConnector CRD trigger a restart of the manager pods.
    
    It calculates a hash of the MultiClusterLease configuration and adds it as an annotation to the manager Pod template. This forces the StatefulSet controller to restart the pods when the configuration changes.
    
    Fixes #6020

--- operator/pkg/controllers/configconnector/experiments.go
+++ operator/pkg/controllers/configconnector/experiments.go
@@ -16,6 +16,9 @@ package configconnector
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"strings"
 
@@ -47,7 +50,7 @@ func (r *Reconciler) applyExperiments(ctx context.Context, cc *corev1beta1.Confi
 	}
 
 	if cc.Spec.Experiments.MultiClusterLease != nil {
-		if err := r.applyMultiClusterLeaderElection(ctx, m); err != nil {
+		if err := r.applyMultiClusterLeaderElection(ctx, cc.Spec.Experiments.MultiClusterLease, m); err != nil {
 			return err
 		}
 	}
@@ -59,7 +62,7 @@ func IsControllerManagerStatefulSet(item *manifest.Object) bool {
 	return item.Kind == "StatefulSet" && strings.HasPrefix(item.GetName(), "cnrm-controller-manager")
 }
 
-func (r *Reconciler) applyMultiClusterLeaderElection(ctx context.Context, obj *manifest.Objects) error {
+func (r *Reconciler) applyMultiClusterLeaderElection(ctx context.Context, lease *corev1beta1.MultiClusterLeaseSpec, obj *manifest.Objects) error {
 	log := log.FromContext(ctx)
 	for _, item := range obj.Items {
 		if !IsControllerManagerStatefulSet(item) {
@@ -83,6 +86,28 @@ func (r *Reconciler) applyMultiClusterLeaderElection(ctx context.Context, obj *m
 			return nil
 		}); err != nil {
 			return fmt.Errorf("failed to apply multi-cluster leader election to %s: %w", item.GetName(), err)
+		}
+
+		// Calculate hash of the lease config
+		leaseBytes, err := json.Marshal(lease)
+		if err != nil {
+			return fmt.Errorf("failed to marshal multi-cluster lease config: %w", err)
+		}
+		hash := sha256.Sum256(leaseBytes)
+		hashString := hex.EncodeToString(hash[:])
+
+		// Add hash as annotation to the Pod template
+		u := item.UnstructuredObject()
+		annotations, _, err := unstructured.NestedStringMap(u.Object, "spec", "template", "metadata", "annotations")
+		if err != nil {
+			return fmt.Errorf("failed to get pod template annotations: %w", err)
+		}
+		if annotations == nil {
+			annotations = make(map[string]string)
+		}
+		annotations["cnrm.cloud.google.com/multi-cluster-lease-hash"] = hashString
+		if err := unstructured.SetNestedStringMap(u.Object, annotations, "spec", "template", "metadata", "annotations"); err != nil {
+			return fmt.Errorf("failed to set pod template annotations: %w", err)
 		}
 	}
 
---
+++ operator/pkg/controllers/configconnector/experiments_test.go
@@ -0,0 +1,151 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package configconnector
+
+import (
+	"context"
+	"testing"
+
+	corev1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/operator/pkg/apis/core/v1beta1"
+	testcontroller "github.com/GoogleCloudPlatform/k8s-config-connector/operator/pkg/test/controller"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"sigs.k8s.io/kubebuilder-declarative-pattern/pkg/patterns/declarative/pkg/manifest"
+)
+
+func TestApplyMultiClusterLeaderElection(t *testing.T) {
+	ctx := context.Background()
+
+	// Helper to get manifest objects
+	getManifests := func(t *testing.T) *manifest.Objects {
+		return testcontroller.ParseObjects(ctx, t, testcontroller.ClusterModeComponents)
+	}
+
+	tests := []struct {
+		name          string
+		cc            *corev1beta1.ConfigConnector
+		expectEnabled bool
+		expectHash    bool
+	}{
+		{
+			name: "experiments nil",
+			cc: &corev1beta1.ConfigConnector{
+				Spec: corev1beta1.ConfigConnectorSpec{
+					Experiments: nil,
+				},
+			},
+			expectEnabled: false,
+			expectHash:    false,
+		},
+		{
+			name: "multicluster lease nil",
+			cc: &corev1beta1.ConfigConnector{
+				Spec: corev1beta1.ConfigConnectorSpec{
+					Experiments: &corev1beta1.CCExperiments{
+						MultiClusterLease: nil,
+					},
+				},
+			},
+			expectEnabled: false,
+			expectHash:    false,
+		},
+		{
+			name: "multicluster lease set",
+			cc: &corev1beta1.ConfigConnector{
+				Spec: corev1beta1.ConfigConnectorSpec{
+					Experiments: &corev1beta1.CCExperiments{
+						MultiClusterLease: &corev1beta1.MultiClusterLeaseSpec{
+							LeaseName:                "foo",
+							Namespace:                "bar",
+							ClusterCandidateIdentity: "baz",
+						},
+					},
+				},
+			},
+			expectEnabled: true,
+			expectHash:    true,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			m := getManifests(t)
+			r := &Reconciler{} // minimal reconciler, methods used don't access fields
+
+			if err := r.applyExperiments(ctx, tc.cc, m); err != nil {
+				t.Fatalf("applyExperiments failed: %v", err)
+			}
+
+			foundStatefulSet := false
+			for _, item := range m.Items {
+				if IsControllerManagerStatefulSet(item) {
+					foundStatefulSet = true
+					
+					// Check args
+					foundArg := false
+					err := item.MutateContainers(func(container map[string]interface{}) error {
+						name, _, _ := unstructured.NestedString(container, "name")
+						if name != "manager" {
+							return nil
+						}
+						args, _, _ := unstructured.NestedStringSlice(container, "args")
+						for _, arg := range args {
+							if arg == "--leader-election-type=multicluster" {
+								foundArg = true
+							}
+						}
+						return nil
+					})
+					if err != nil {
+						t.Fatalf("MutateContainers failed: %v", err)
+					}
+
+					if tc.expectEnabled && !foundArg {
+						t.Errorf("expected --leader-election-type=multicluster arg, but not found")
+					}
+					if !tc.expectEnabled && foundArg {
+						t.Errorf("did not expect --leader-election-type=multicluster arg, but found")
+					}
+
+					// Check annotation
+					annotations := item.UnstructuredObject().GetAnnotations()
+					// Check template annotations
+					templateAnnotations, _, _ := unstructured.NestedStringMap(item.UnstructuredObject().Object, "spec", "template", "metadata", "annotations")
+					
+					hashKey := "cnrm.cloud.google.com/multi-cluster-lease-hash"
+					hashVal, hasHash := templateAnnotations[hashKey]
+					
+					if tc.expectHash && !hasHash {
+						t.Errorf("expected pod template annotation %s, but not found", hashKey)
+					}
+					if !tc.expectHash && hasHash {
+						t.Errorf("did not expect pod template annotation %s, but found: %s", hashKey, hashVal)
+					}
+					
+					// If hash is expected, ensure it is not empty
+					if tc.expectHash && hashVal == "" {
+						t.Errorf("expected non-empty hash value")
+					}
+					
+					// Log annotations for debugging
+					t.Logf("StatefulSet Annotations: %v", annotations)
+					t.Logf("Pod Template Annotations: %v", templateAnnotations)
+				}
+			}
+			if !foundStatefulSet {
+				t.Fatalf("StatefulSet not found in manifest")
+			}
+		})
+	}
+}
