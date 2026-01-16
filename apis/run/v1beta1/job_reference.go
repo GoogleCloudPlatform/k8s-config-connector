56998bb43fc5d37af0fda61c3c1b75f08f8c2447
codebot-robot <codebot-robot@google.com>
2026-01-16 23:25:41 +0000

    fixup! Implement refs.Ref for CloudRunJob

--- apis/run/v1beta1/job_reference.go
+++ apis/run/v1beta1/job_reference.go
@@ -16,18 +16,13 @@ package v1beta1
 
 import (
 	"context"
-	"fmt"
 
 	refsv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
-	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/k8s"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-var _ refsv1beta1.ExternalNormalizer = &JobRef{}
 var _ refsv1beta1.Ref = &JobRef{}
 
 // JobRef defines the resource reference to RunJob, which "External" field
@@ -42,46 +37,6 @@ type JobRef struct {
 
 	// The namespace of a RunJob resource.
 	Namespace string `json:"namespace,omitempty"`
-}
-
-// NormalizedExternal provision the "External" value for other resource that depends on RunJob.
-// If the "External" is given in the other resource's spec.RunJobRef, the given value will be used.
-// Otherwise, the "Name" and "Namespace" will be used to query the actual RunJob object from the cluster.
-func (r *JobRef) NormalizedExternal(ctx context.Context, reader client.Reader, otherNamespace string) (string, error) {
-	if r.External != "" && r.Name != "" {
-		return "", fmt.Errorf("cannot specify both name and external on %s reference", RunJobGVK.Kind)
-	}
-	// From given External
-	if r.External != "" {
-		if _, _, err := ParseJobExternal(r.External); err != nil {
-			return "", err
-		}
-		return r.External, nil
-	}
-
-	// From the Config Connector object
-	if r.Namespace == "" {
-		r.Namespace = otherNamespace
-	}
-	key := types.NamespacedName{Name: r.Name, Namespace: r.Namespace}
-	u := &unstructured.Unstructured{}
-	u.SetGroupVersionKind(RunJobGVK)
-	if err := reader.Get(ctx, key, u); err != nil {
-		if apierrors.IsNotFound(err) {
-			return "", k8s.NewReferenceNotFoundError(u.GroupVersionKind(), key)
-		}
-		return "", fmt.Errorf("reading referenced %s %s: %w", RunJobGVK, key, err)
-	}
-	// Get external from status.externalRef. This is the most trustworthy place.
-	actualExternalRef, _, err := unstructured.NestedString(u.Object, "status", "externalRef")
-	if err != nil {
-		return "", fmt.Errorf("reading status.externalRef: %w", err)
-	}
-	if actualExternalRef == "" {
-		return "", k8s.NewReferenceNotReadyError(u.GroupVersionKind(), key)
-	}
-	r.External = actualExternalRef
-	return r.External, nil
 }
 
 func (r *JobRef) GetGVK() schema.GroupVersionKind {
@@ -104,7 +59,8 @@ func (r *JobRef) SetExternal(ref string) {
 }
 
 func (r *JobRef) ValidateExternal(ref string) error {
-	if _, _, err := ParseJobExternal(ref); err != nil {
+	id := &JobIdentity{}
+	if err := id.FromExternal(ref); err != nil {
 		return err
 	}
 	return nil
