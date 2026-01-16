cebea8da9c1473c14f8563e4fdf29173bba4bd00
codebot-robot <codebot-robot@google.com>
2026-01-16 13:35:07 +0000

    Add FromExternal to ArtifactRegistryRepositoryIdentity
    This PR adds the ArtifactRegistryRepositoryIdentity type and its FromExternal method to apis/artifactregistry/v1beta1.
    This allows parsing external references for Artifact Registry Repositories.
    
    Fixes #6080

---
+++ apis/artifactregistry/v1beta1/artifactregistryrepository_identity.go
@@ -0,0 +1,46 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+import (
+	"fmt"
+
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/gcpurls"
+)
+
+var ArtifactRegistryRepositoryIdentityFormat = gcpurls.Template[ArtifactRegistryRepositoryIdentity]("artifactregistry.googleapis.com", "projects/{project}/locations/{location}/repositories/{repository}")
+
+type ArtifactRegistryRepositoryIdentity struct {
+	Project    string
+	Location   string
+	Repository string
+}
+
+func (i *ArtifactRegistryRepositoryIdentity) String() string {
+	return ArtifactRegistryRepositoryIdentityFormat.ToString(*i)
+}
+
+func (i *ArtifactRegistryRepositoryIdentity) FromExternal(ref string) error {
+	parsed, match, err := ArtifactRegistryRepositoryIdentityFormat.Parse(ref)
+	if err != nil {
+		return fmt.Errorf("format of ArtifactRegistryRepository external=%q was not known (use %s): %w", ref, ArtifactRegistryRepositoryIdentityFormat.CanonicalForm(), err)
+	}
+	if !match {
+		return fmt.Errorf("format of ArtifactRegistryRepository external=%q was not known (use %s)", ref, ArtifactRegistryRepositoryIdentityFormat.CanonicalForm())
+	}
+
+	*i = *parsed
+	return nil
+}
---
+++ apis/artifactregistry/v1beta1/artifactregistryrepository_identity_test.go
@@ -0,0 +1,74 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+import (
+	"testing"
+)
+
+func TestArtifactRegistryRepositoryIdentity_FromExternal(t *testing.T) {
+	tests := []struct {
+		name    string
+		ref     string
+		wantErr bool
+		want    *ArtifactRegistryRepositoryIdentity
+	}{
+		{
+			name: "valid reference",
+			ref:  "projects/my-project/locations/us-central1/repositories/my-repo",
+			want: &ArtifactRegistryRepositoryIdentity{
+				Project:    "my-project",
+				Location:   "us-central1",
+				Repository: "my-repo",
+			},
+		},
+		{
+			name:    "invalid reference format",
+			ref:     "invalid/format",
+			wantErr: true,
+		},
+		{
+			name: "full url",
+			ref:  "https://artifactregistry.googleapis.com/projects/my-project/locations/us-central1/repositories/my-repo",
+			want: &ArtifactRegistryRepositoryIdentity{
+				Project:    "my-project",
+				Location:   "us-central1",
+				Repository: "my-repo",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			i := &ArtifactRegistryRepositoryIdentity{}
+			err := i.FromExternal(tt.ref)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FromExternal() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !tt.wantErr {
+				if i.Project != tt.want.Project {
+					t.Errorf("Project = %v, want %v", i.Project, tt.want.Project)
+				}
+				if i.Location != tt.want.Location {
+					t.Errorf("Location = %v, want %v", i.Location, tt.want.Location)
+				}
+				if i.Repository != tt.want.Repository {
+					t.Errorf("Repository = %v, want %v", i.Repository, tt.want.Repository)
+				}
+			}
+		})
+	}
+}
