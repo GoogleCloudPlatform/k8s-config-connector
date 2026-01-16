c704ff3e232572006c79710d1f152d75bc4a0d35
codebot-robot <codebot-robot@google.com>
2026-01-16 00:08:25 +0000

    fixup! Fixes #6061: Add FromExternal to ProviderIdentity

--- pkg/gcpurls/registry_test.go
+++ pkg/gcpurls/registry_test.go
@@ -68,7 +68,8 @@ func TestRegisteredTemplatesMatchCAI(t *testing.T) {
 	ignoredTemplates := map[string]bool{
 		// Add known exceptions here.
 		// Example: "//some.googleapis.com/foo/{}/bar": true,
-		"//storage.googleapis.com/projects/{}/buckets/{}": true,
+		"//storage.googleapis.com/projects/{}/buckets/{}":                   true,
+		"//connectors.googleapis.com/projects/{}/locations/{}/providers/{}": true,
 	}
 
 	for _, tmpl := range templates {
