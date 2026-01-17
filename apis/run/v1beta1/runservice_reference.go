578a699a35c080b0465934825087b49821146f82
codebot-robot <codebot-robot@google.com>
2026-01-17 00:12:03 +0000

    fixup! Implement refs.Ref for CloudRunService

--- apis/run/v1beta1/runservice_reference.go
+++ apis/run/v1beta1/runservice_reference.go
@@ -22,11 +22,12 @@ import (
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/k8s"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-var _ refsv1beta1.ExternalNormalizer = &RunServiceRef{}
+var _ refsv1beta1.Ref = &RunServiceRef{}
 
 // RunServiceRef defines the resource reference to RunService, which "External" field
 // holds the GCP identifier for the KRM object.
@@ -42,43 +43,60 @@ type RunServiceRef struct {
 	Namespace string `json:"namespace,omitempty"`
 }
 
-// NormalizedExternal provision the "External" value for other resource that depends on RunService.
-// If the "External" is given in the other resource's spec.RunServiceRef, the given value will be used.
-// Otherwise, the "Name" and "Namespace" will be used to query the actual RunService object from the cluster.
-func (r *RunServiceRef) NormalizedExternal(ctx context.Context, reader client.Reader, otherNamespace string) (string, error) {
-	if r.External != "" && r.Name != "" {
-		return "", fmt.Errorf("cannot specify both name and external on %s reference", RunServiceGVK.Kind)
+func (r *RunServiceRef) GetGVK() schema.GroupVersionKind {
+	return RunServiceGVK
+}
+
+func (r *RunServiceRef) GetNamespacedName() types.NamespacedName {
+	return types.NamespacedName{
+		Name:      r.Name,
+		Namespace: r.Namespace,
 	}
-	// From given External
+}
+
+func (r *RunServiceRef) GetExternal() string {
+	return r.External
+}
+
+func (r *RunServiceRef) SetExternal(ref string) {
+	r.External = ref
+}
+
+func (r *RunServiceRef) ValidateExternal(ref string) error {
+	id := &RunServiceIdentity{}
+	if err := id.FromExternal(ref); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *RunServiceRef) Normalize(ctx context.Context, reader client.Reader, defaultNamespace string) error {
 	if r.External != "" {
-		id := &RunServiceIdentity{}
-		if err := id.FromExternal(r.External); err != nil {
-			return "", err
-		}
-		return r.External, nil
+		return r.ValidateExternal(r.External)
 	}
 
-	// From the Config Connector object
-	if r.Namespace == "" {
-		r.Namespace = otherNamespace
+	key := r.GetNamespacedName()
+	if key.Namespace == "" {
+		key.Namespace = defaultNamespace
 	}
-	key := types.NamespacedName{Name: r.Name, Namespace: r.Namespace}
+
 	u := &unstructured.Unstructured{}
 	u.SetGroupVersionKind(RunServiceGVK)
 	if err := reader.Get(ctx, key, u); err != nil {
 		if apierrors.IsNotFound(err) {
-			return "", k8s.NewReferenceNotFoundError(u.GroupVersionKind(), key)
+			return k8s.NewReferenceNotFoundError(u.GroupVersionKind(), key)
 		}
-		return "", fmt.Errorf("reading referenced %s %s: %w", RunServiceGVK, key, err)
+		return fmt.Errorf("reading referenced %s %s: %w", RunServiceGVK, key, err)
 	}
+
 	// Get external from status.externalRef. This is the most trustworthy place.
-	actualExternalRef, _, err := unstructured.NestedString(u.Object, "status", "externalRef")
+	externalRef, _, err := unstructured.NestedString(u.Object, "status", "externalRef")
 	if err != nil {
-		return "", fmt.Errorf("reading status.externalRef: %w", err)
+		return fmt.Errorf("reading status.externalRef: %w", err)
 	}
-	if actualExternalRef != "" {
-		r.External = actualExternalRef
-		return r.External, nil
+	if externalRef != "" {
+		r.External = externalRef
+		return r.ValidateExternal(r.External)
 	}
 
 	// Fallback to constructing the external reference from the object state.
@@ -88,10 +106,10 @@ func (r *RunServiceRef) NormalizedExternal(ctx context.Context, reader client.Re
 	// 1. Resolve Project
 	projectRefMap, found, err := unstructured.NestedMap(u.Object, "spec", "projectRef")
 	if err != nil {
-		return "", fmt.Errorf("reading spec.projectRef: %w", err)
+		return fmt.Errorf("reading spec.projectRef: %w", err)
 	}
 	if !found {
-		return "", fmt.Errorf("spec.projectRef not found")
+		return fmt.Errorf("spec.projectRef not found")
 	}
 	projectRef := &refsv1beta1.ProjectRef{}
 	if val, ok := projectRefMap["external"].(string); ok {
@@ -104,24 +122,24 @@ func (r *RunServiceRef) NormalizedExternal(ctx context.Context, reader client.Re
 		projectRef.Namespace = val
 	}
 
-	project, err := refsv1beta1.ResolveProject(ctx, reader, r.Namespace, projectRef)
+	project, err := refsv1beta1.ResolveProject(ctx, reader, u.GetNamespace(), projectRef)
 	if err != nil {
-		return "", fmt.Errorf("resolving project: %w", err)
+		return fmt.Errorf("resolving project: %w", err)
 	}
 
 	// 2. Get Location
 	location, found, err := unstructured.NestedString(u.Object, "spec", "location")
 	if err != nil {
-		return "", fmt.Errorf("reading spec.location: %w", err)
+		return fmt.Errorf("reading spec.location: %w", err)
 	}
 	if !found || location == "" {
-		return "", fmt.Errorf("spec.location not found")
+		return fmt.Errorf("spec.location not found")
 	}
 
 	// 3. Get Service Name (spec.resourceID or metadata.name)
 	resourceID, _, err := unstructured.NestedString(u.Object, "spec", "resourceID")
 	if err != nil {
-		return "", fmt.Errorf("reading spec.resourceID: %w", err)
+		return fmt.Errorf("reading spec.resourceID: %w", err)
 	}
 	if resourceID == "" {
 		resourceID = u.GetName()
@@ -134,7 +152,7 @@ func (r *RunServiceRef) NormalizedExternal(ctx context.Context, reader client.Re
 		Service:  resourceID,
 	}
 	r.External = id.String()
-	return r.External, nil
+	return nil
 }
 
 var RunServiceGVK = GroupVersion.WithKind("RunService")
