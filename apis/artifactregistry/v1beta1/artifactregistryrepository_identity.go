2184d62fc20503e43ceb12cb9ef8eb5da712ced6
codebot-robot <codebot-robot@google.com>
2026-01-18 16:20:30 +0000

    Implement identity.Identity for ArtifactRegistryRepository
    Fixes #6137

--- apis/artifactregistry/v1beta1/artifactregistryrepository_identity.go
+++ apis/artifactregistry/v1beta1/artifactregistryrepository_identity.go
@@ -15,9 +15,18 @@
 package v1beta1
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common/identity"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/gcpurls"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var (
+	_ identity.Identity = &ArtifactRegistryRepositoryIdentity{}
+	_ identity.Resource = &ArtifactRegistryRepository{}
 )
 
 var ArtifactRegistryRepositoryIdentityFormat = gcpurls.Template[ArtifactRegistryRepositoryIdentity]("artifactregistry.googleapis.com", "projects/{project}/locations/{location}/repositories/{repository}")
@@ -44,4 +53,57 @@ func (i *ArtifactRegistryRepositoryIdentity) FromExternal(ref string) error {
 
 	*i = *parsed
 	return nil
+}
+
+func (obj *ArtifactRegistryRepository) GetIdentity(ctx context.Context, reader client.Reader) (identity.Identity, error) {
+	// Get desired ID
+	resourceID := common.ValueOf(obj.Spec.ResourceID)
+	if resourceID == "" {
+		resourceID = obj.GetName()
+	}
+	if resourceID == "" {
+		return nil, fmt.Errorf("cannot resolve resource ID")
+	}
+
+	// Get Location
+	location := obj.Spec.Location
+	if location == "" {
+		return nil, fmt.Errorf("cannot resolve location")
+	}
+
+	// Get Project
+	projectID := obj.GetAnnotations()["cnrm.cloud.google.com/project-id"]
+	if projectID == "" {
+		projectID = obj.GetNamespace()
+	}
+	if projectID == "" {
+		return nil, fmt.Errorf("cannot resolve project")
+	}
+
+	// Use approved External
+	// Status.Name matches the google.devtools.artifactregistry.v1.Repository.name field, which is the full resource name.
+	externalRef := common.ValueOf(obj.Status.Name)
+	if externalRef != "" {
+		// Validate desired with actual
+		actualIdentity := &ArtifactRegistryRepositoryIdentity{}
+		if err := actualIdentity.FromExternal(externalRef); err != nil {
+			return nil, err
+		}
+		if actualIdentity.Project != projectID {
+			return nil, fmt.Errorf("project changed, expect %s, got %s", actualIdentity.Project, projectID)
+		}
+		if actualIdentity.Location != location {
+			return nil, fmt.Errorf("location changed, expect %s, got %s", actualIdentity.Location, location)
+		}
+		if actualIdentity.Repository != resourceID {
+			return nil, fmt.Errorf("cannot reset `metadata.name` or `spec.resourceID` to %s, since it has already assigned to %s",
+				resourceID, actualIdentity.Repository)
+		}
+	}
+
+	return &ArtifactRegistryRepositoryIdentity{
+		Project:    projectID,
+		Location:   location,
+		Repository: resourceID,
+	}, nil
 }
