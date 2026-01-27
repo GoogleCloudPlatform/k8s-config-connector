c3689fef54b7d0ff741a3c0e038da3d785db5bd1
codebot-robot <codebot-robot@google.com>
2026-01-27 15:04:31 +0000

    Move TestOutputOnlyFieldsAreUnderObservedState to tests/apichecks
    Fixes #6286
    Normalize TestOutputOnlyFieldsAreUnderObservedState to use golden file testing.

---
+++ tests/apichecks/observed_state_test.go
@@ -0,0 +1,136 @@
+package lint
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+	"testing"
+
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	k8sschema "k8s.io/apimachinery/pkg/runtime/schema"
+
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/crd/crdloader"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/k8s"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/servicemapping/servicemappingloader"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/stateintospec"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/test"
+)
+
+func TestOutputOnlyFieldsAreUnderObservedState(t *testing.T) {
+	crds, err := crdloader.LoadCRDs()
+	if err != nil {
+		t.Fatalf("error loading crds: %v", err)
+	}
+	smLoader, err := servicemappingloader.New()
+	if err != nil {
+		t.Fatalf("error getting new service mapping loader: %v", err)
+	}
+
+	var errs []string
+
+	for _, crd := range crds {
+		for _, version := range crd.Spec.Versions {
+			if version.Name == k8s.KCCAPIVersionV1Alpha1 {
+				// we don't check for v1alpha1 resources.
+				continue
+			}
+			gvk := k8sschema.GroupVersionKind{
+				Group:   crd.Spec.Group,
+				Version: version.Name,
+				Kind:    crd.Spec.Names.Kind,
+			}
+
+			openAPISchema := version.Schema.OpenAPIV3Schema
+			prop := findOpenAPIProperty(openAPISchema, "status", "observedState")
+			hasObservedState := prop != nil
+
+			// 'status.observedState' should exist for CRDs that
+			// (1) have all the output-only fields under 'status.observedState'
+			//     instead of 'status', or
+			// (2) have observedFields configured in the resource config to
+			//     expose selected output 'spec' fields.
+			// 'status.observedState' does not exist for CRDs that
+			// (1) don't have any output-only fields, or
+			// (2) have all the output-only fields under 'status' but don't
+			//     have observedFields configured.
+			mayHaveObservedState := stateintospec.OutputOnlyFieldsAreUnderObservedState(gvk)
+
+			rcs, err := smLoader.GetResourceConfigs(gvk)
+			// Ignore not found error because there are handwritten and
+			// DCL-based resources.
+			if err != nil && !strings.Contains(err.Error(), "no mapping with name") {
+				t.Errorf("error getting resource config(s) for gvk %+v: %v", gvk, err)
+			}
+			shouldHaveObservedState := false
+			for _, rc := range rcs {
+				if rc.ObservedFields != nil && len(*rc.ObservedFields) > 0 {
+					shouldHaveObservedState = true
+				}
+			}
+
+			// If shouldHaveObservedState is true, we should expect there is
+			// 'status.observedState' in the CRD.
+			if shouldHaveObservedState && !hasObservedState {
+				errs = append(errs, fmt.Sprintf("crd=%s version=%v: 'status.observedState' doesn't exist, but it should", crd.Name, version.Name))
+			}
+
+			// If mayHaveObservedState is true, it means 'status' will only
+			// have subfields 'conditions' and 'observedGeneration', and
+			// optionally subfield 'observedState'.
+			if mayHaveObservedState {
+				statusProp := findOpenAPIProperty(openAPISchema, "status")
+				if statusProp == nil {
+					errs = append(errs, fmt.Sprintf("crd=%s version=%v: 'status' doesn't exist, but it should", crd.Name, version.Name))
+				} else {
+					requiredFieldsMap := map[string]bool{"observedGeneration": true, "conditions": true}
+					optionalFieldsMap := map[string]bool{"observedState": true}
+
+					// for direct resources, we will use the "externalRef" prop under the "status"
+					// to track the KCC full resource ID
+					optionalFieldsMap["externalRef"] = true
+
+					for k := range statusProp.Properties {
+						foundInMaps := false
+						if _, ok := requiredFieldsMap[k]; ok {
+							foundInMaps = true
+							delete(requiredFieldsMap, k)
+						}
+						if _, ok := optionalFieldsMap[k]; ok {
+							foundInMaps = true
+							delete(optionalFieldsMap, k)
+						}
+						if !foundInMaps {
+							errs = append(errs, fmt.Sprintf("crd=%s version=%v: CRD has non-boilerplate field '%v' under 'status'", crd.Name, version.Name, k))
+						}
+					}
+
+					if len(requiredFieldsMap) > 0 {
+						// Convert map keys to slice for consistent error message
+						missing := make([]string, 0, len(requiredFieldsMap))
+						for k := range requiredFieldsMap {
+							missing = append(missing, k)
+						}
+						sort.Strings(missing)
+						errs = append(errs, fmt.Sprintf("crd=%s version=%v: CRD doesn't have enough subfields under 'status' field: it should at least have fields %v", crd.Name, version.Name, missing))
+					}
+				}
+			}
+		}
+	}
+
+	sort.Strings(errs)
+	want := strings.Join(errs, "\n")
+	test.CompareGoldenFile(t, "testdata/exceptions/observed_state.txt", want)
+}
+
+func findOpenAPIProperty(schema *apiextensionsv1.JSONSchemaProps, path ...string) *apiextensionsv1.JSONSchemaProps {
+	pos := schema
+	for _, k := range path {
+		p, found := pos.Properties[k]
+		if !found {
+			return nil
+		}
+		pos = &p
+	}
+	return pos
+}
