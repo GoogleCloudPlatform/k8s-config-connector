6159e4d875bc9476419a5abe089b51b9baf404ff
Frank Currie <>
2026-02-15 22:49:07 +0000

    chore: fix CI failures for EdgeCacheService

--- apis/networkservices/v1alpha1/edgecacheservice_types.go
+++ apis/networkservices/v1alpha1/edgecacheservice_types.go
@@ -659,6 +659,7 @@ type NetworkServicesEdgeCacheServiceObservedState struct {
 // +kubebuilder:resource:categories=gcp,shortName=gcpnetworkservicesedgecacheservice;gcpnetworkservicesedgecacheservices
 // +kubebuilder:subresource:status
 // +kubebuilder:metadata:labels="cnrm.cloud.google.com/managed-by-kcc=true"
+// +kubebuilder:metadata:labels="cnrm.cloud.google.com/tf2crd=true"
 // +kubebuilder:metadata:labels="cnrm.cloud.google.com/stability-level=alpha"
 // +kubebuilder:metadata:labels="cnrm.cloud.google.com/system=true"
 // +kubebuilder:printcolumn:name="Age",JSONPath=".metadata.creationTimestamp",type="date"
