4bc50169ecdbcd741cb669894db8d9454f7a9c90
codebot-robot <codebot-robot@google.com>
2026-02-26 00:35:56 +0000

    fixup! Create direct CRD Go types for AccessContextManagerAccessLevelCondition

--- apis/accesscontextmanager/v1alpha1/accesscontextmanageraccesslevelcondition_types.go
+++ apis/accesscontextmanager/v1alpha1/accesscontextmanageraccesslevelcondition_types.go
@@ -98,7 +98,7 @@ type AccessContextManagerAccessLevelConditionStatus struct {
 // +kubebuilder:subresource:status
 // +kubebuilder:metadata:labels="cnrm.cloud.google.com/managed-by-kcc=true"
 // +kubebuilder:metadata:labels="cnrm.cloud.google.com/system=true"
-// +kubebuilder:metadata:labels="cnrm.cloud.google.com/stability-level=stable"
+// +kubebuilder:metadata:labels="cnrm.cloud.google.com/stability-level=alpha"
 // +kubebuilder:metadata:labels="cnrm.cloud.google.com/tf2crd=true"
 // +kubebuilder:printcolumn:name="Age",JSONPath=".metadata.creationTimestamp",type="date"
 // +kubebuilder:printcolumn:name="Ready",JSONPath=".status.conditions[?(@.type=='Ready')].status",type="string",description="When 'True', the most recent reconcile of the resource succeeded"
