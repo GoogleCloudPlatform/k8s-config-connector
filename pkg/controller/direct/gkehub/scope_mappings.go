914a8a9dc1fe9a14d39dc9772a75726a18f9ccbf
codebot-robot <codebot-robot@google.com>
2026-03-11 01:02:17 +0000

    feat(gkehub): support GKEHubScope and GKEHubNamespace
    This PR adds support for GKE Hub Scope and GKEHubNamespace resources using the Direct controller architecture.\n\nSummary of changes:\n- API types and CRDs for GKEHubScope and GKEHubNamespace\n- Direct controllers for both resources in pkg/controller/direct/gkehub\n- Mock implementation in mockgcp/mockgkehub\n- Registration of the mock service and controller\n- Test fixtures for validation in pkg/test/resourcefixture/testdata\n\nCloses #6051\nCloses #6052

---
+++ pkg/controller/direct/gkehub/scope_mappings.go
@@ -0,0 +1,54 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gkehub
+
+import (
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/gkehub/v1beta1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+	gkehubv1 "google.golang.org/api/gkehub/v1"
+)
+
+func GKEHubScopeSpec_ToAPI(mapCtx *direct.MapContext, in *krm.GKEHubScopeSpec) *gkehubv1.Scope {
+	if in == nil {
+		return nil
+	}
+	out := &gkehubv1.Scope{}
+	out.Labels = in.Labels
+	out.NamespaceLabels = in.NamespaceLabels
+	return out
+}
+
+func GKEHubScopeStatus_FromAPI(mapCtx *direct.MapContext, in *gkehubv1.Scope) *krm.GKEHubScopeStatus {
+	if in == nil {
+		return nil
+	}
+	out := &krm.GKEHubScopeStatus{}
+	out.ObservedState = &krm.GKEHubScopeObservedState{}
+	out.ObservedState.Uid = direct.LazyPtr(in.Uid)
+	out.ObservedState.CreateTime = direct.LazyPtr(in.CreateTime)
+	out.ObservedState.UpdateTime = direct.LazyPtr(in.UpdateTime)
+	out.ObservedState.DeleteTime = direct.LazyPtr(in.DeleteTime)
+	out.ObservedState.State = ScopeLifecycleStateStatus_FromAPI(mapCtx, in.State)
+	return out
+}
+
+func ScopeLifecycleStateStatus_FromAPI(mapCtx *direct.MapContext, in *gkehubv1.ScopeLifecycleState) *krm.ScopeLifecycleStateStatus {
+	if in == nil {
+		return nil
+	}
+	out := &krm.ScopeLifecycleStateStatus{}
+	out.Code = direct.LazyPtr(in.Code)
+	return out
+}
