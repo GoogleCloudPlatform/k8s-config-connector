57017e5b02ecc94cb53d5aade8b7a50573c5f7e8
Unknown User <>
2026-03-18 19:40:09 +0000

    chore: generate issues for generate.sh and types.go
    This commit was automatically generated as part of the 'Issue for Generate and Types' chore.

---
+++ parse_candidates.py
@@ -0,0 +1,40 @@
+import yaml
+import glob
+import os
+
+candidate_files = []
+# Just iterate over all files in config/crds/resources
+for root, _, files in os.walk('config/crds/resources'):
+    for file in files:
+        if file.endswith('.yaml'):
+            candidate_files.append(os.path.join(root, file))
+
+for path in candidate_files:
+    try:
+        with open(path, 'r') as f:
+            content = f.read()
+            if 'cnrm.cloud.google.com/dcl2crd: "true"' not in content:
+                continue
+            
+            # parse yaml
+            docs = list(yaml.safe_load_all(content))
+            for doc in docs:
+                if not doc:
+                    continue
+                metadata = doc.get('metadata', {})
+                labels = metadata.get('labels', {})
+                if labels.get('cnrm.cloud.google.com/dcl2crd') != "true":
+                    continue
+                
+                spec = doc.get('spec', {})
+                group = spec.get('group', '').split('.')[0]
+                kind = spec.get('names', {}).get('kind', '')
+                versions = spec.get('versions', [])
+                
+                has_beta = any('v1beta1' in v.get('name', '') for v in versions)
+                if has_beta:
+                    types_pattern = f"apis/{group}/v1beta1/*_types.go"
+                    if not glob.glob(types_pattern):
+                        print(f"Candidate: {group} {kind}")
+    except Exception as e:
+        pass
