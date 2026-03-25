9878b8e364bf61240e459222e6e82be743f4959f
Unknown User <>
2026-03-25 22:49:33 +0000

    Create Identity and Reference files for alloydb AlloyDBUser
    This PR adds the Go identity and reference for AlloyDBUser in apis/alloydb/v1beta1/.
    It implements IdentityV2 and Resource interfaces for AlloyDBUser.
    It also implements Normalize, ValidateExternal, and ParseExternalToIdentity for AlloyDBUserRef.
    
    Issue #7175

---
+++ apis/alloydb/v1beta1/user_identity_test.go
@@ -0,0 +1,77 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+import (
+	"testing"
+)
+
+func TestAlloyDBUserIdentity_FromExternal(t *testing.T) {
+	tests := []struct {
+		name       string
+		external   string
+		want       *AlloyDBUserIdentity
+		wantErr    bool
+	}{
+		{
+			name:     "valid external",
+			external: "projects/my-project/locations/us-central1/clusters/my-cluster/users/my-user",
+			want: &AlloyDBUserIdentity{
+				Project:  "my-project",
+				Location: "us-central1",
+				Cluster:  "my-cluster",
+				User:     "my-user",
+			},
+			wantErr: false,
+		},
+		{
+			name:     "invalid external - too few tokens",
+			external: "projects/my-project/locations/us-central1/clusters/my-cluster",
+			wantErr:  true,
+		},
+		{
+			name:     "invalid external - wrong prefix",
+			external: "organizations/my-org/locations/us-central1/clusters/my-cluster/users/my-user",
+			wantErr:  true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			i := &AlloyDBUserIdentity{}
+			if err := i.FromExternal(tt.external); (err != nil) != tt.wantErr {
+				t.Errorf("FromExternal() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !tt.wantErr {
+				if i.Project != tt.want.Project || i.Location != tt.want.Location || i.Cluster != tt.want.Cluster || i.User != tt.want.User {
+					t.Errorf("FromExternal() got = %v, want %v", i, tt.want)
+				}
+			}
+		})
+	}
+}
+
+func TestAlloyDBUserIdentity_String(t *testing.T) {
+	identity := &AlloyDBUserIdentity{
+		Project:  "my-project",
+		Location: "us-central1",
+		Cluster:  "my-cluster",
+		User:     "my-user",
+	}
+	want := "projects/my-project/locations/us-central1/clusters/my-cluster/users/my-user"
+	if got := identity.String(); got != want {
+		t.Errorf("String() = %v, want %v", got, want)
+	}
+}
---
+++ apis/alloydb/v1beta1/user_reference.go
@@ -0,0 +1,96 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+import (
+	"context"
+
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common/identity"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var _ refs.Ref = &AlloyDBUserRef{}
+
+// AlloyDBUserRef is a reference to a AlloyDBUser resource.
+type AlloyDBUserRef struct {
+	// A reference to an externally managed AlloyDBUser resource.
+	// Should be in the format "projects/{project}/locations/{location}/clusters/{cluster}/users/{user}".
+	External string `json:"external,omitempty"`
+
+	// The name of a AlloyDBUser resource.
+	Name string `json:"name,omitempty"`
+
+	// The namespace of a AlloyDBUser resource.
+	Namespace string `json:"namespace,omitempty"`
+}
+
+func init() {
+	refs.Register(&AlloyDBUserRef{})
+}
+
+func (r *AlloyDBUserRef) GetGVK() schema.GroupVersionKind {
+	return AlloyDBUserGVK
+}
+
+func (r *AlloyDBUserRef) GetNamespacedName() types.NamespacedName {
+	return types.NamespacedName{
+		Name:      r.Name,
+		Namespace: r.Namespace,
+	}
+}
+
+func (r *AlloyDBUserRef) GetExternal() string {
+	return r.External
+}
+
+func (r *AlloyDBUserRef) SetExternal(ref string) {
+	r.External = ref
+}
+
+func (r *AlloyDBUserRef) ValidateExternal(ref string) error {
+	id := &AlloyDBUserIdentity{}
+	if err := id.FromExternal(ref); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *AlloyDBUserRef) ParseExternalToIdentity() (identity.Identity, error) {
+	id := &AlloyDBUserIdentity{}
+	if err := id.FromExternal(r.External); err != nil {
+		return nil, err
+	}
+	return id, nil
+}
+
+func (r *AlloyDBUserRef) Normalize(ctx context.Context, reader client.Reader, defaultNamespace string) error {
+	fallback := func(u *unstructured.Unstructured) string {
+		obj := &AlloyDBUser{}
+		if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, obj); err != nil {
+			return ""
+		}
+		identity, err := obj.GetIdentity(ctx, reader)
+		if err != nil {
+			return ""
+		}
+		return identity.String()
+	}
+	return refs.NormalizeWithFallback(ctx, reader, r, defaultNamespace, fallback)
+}
