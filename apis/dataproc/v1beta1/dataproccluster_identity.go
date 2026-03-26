f38ad035ad548db24c5ca868d3d8fc3a2ff641e7
codebot-robot <codebot-robot@google.com>
2026-03-26 01:23:10 +0000

    Create Identity and Reference files for Dataproc DataprocCluster
    Fixes #7174
    
    This PR renames cluster_identity.go and cluster_reference.go to dataproccluster_identity.go and dataproccluster_reference.go to align with expected naming conventions, without prematurely moving the DataprocCluster Go types. Prematurely moving the types caused controller-gen to overwrite the tf2crd-generated CRD schema, resulting in validation failures and dropped fields.
    
    GetIdentity implementation is deferred until the resource is fully migrated via controllerbuilder.

---
+++ apis/dataproc/v1beta1/dataproccluster_identity.go
@@ -0,0 +1,87 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common/identity"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/gcpurls"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var (
+	_ identity.IdentityV2 = &DataprocClusterIdentity{}
+)
+
+var DataprocClusterIdentityFormat = gcpurls.Template[DataprocClusterIdentity](
+	"dataproc.googleapis.com",
+	"projects/{project}/regions/{region}/clusters/{cluster}",
+)
+
+// DataprocClusterIdentity represents the identity of a DataprocCluster.
+// +k8s:deepcopy-gen=false
+type DataprocClusterIdentity struct {
+	Project string
+	Region  string
+	Cluster string
+}
+
+func (i *DataprocClusterIdentity) String() string {
+	return DataprocClusterIdentityFormat.ToString(*i)
+}
+
+func (i *DataprocClusterIdentity) FromExternal(ref string) error {
+	parsed, match, err := DataprocClusterIdentityFormat.Parse(ref)
+	if err != nil {
+		return fmt.Errorf("format of DataprocCluster external=%q was not known (use %s): %w", ref, DataprocClusterIdentityFormat.CanonicalForm(), err)
+	}
+	if !match {
+		return fmt.Errorf("format of DataprocCluster external=%q was not known (use %s)", ref, DataprocClusterIdentityFormat.CanonicalForm())
+	}
+
+	*i = *parsed
+	return nil
+}
+
+func (i *DataprocClusterIdentity) Host() string {
+	return DataprocClusterIdentityFormat.Host()
+}
+
+func getIdentityFromDataprocClusterSpec(ctx context.Context, reader client.Reader, obj client.Object) (*DataprocClusterIdentity, error) {
+	resourceID, err := refs.GetResourceID(obj)
+	if err != nil {
+		return nil, fmt.Errorf("cannot resolve resource ID: %w", err)
+	}
+
+	region, err := refs.GetLocation(obj)
+	if err != nil {
+		return nil, fmt.Errorf("cannot resolve region: %w", err)
+	}
+
+	projectID, err := refs.ResolveProjectID(ctx, reader, obj)
+	if err != nil {
+		return nil, fmt.Errorf("cannot resolve project: %w", err)
+	}
+
+	identity := &DataprocClusterIdentity{
+		Project: projectID,
+		Region:  region,
+		Cluster: resourceID,
+	}
+	return identity, nil
+}
---
+++ apis/dataproc/v1beta1/dataproccluster_reference.go
@@ -0,0 +1,100 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+import (
+	"context"
+
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common/identity"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var _ refs.Ref = &DataprocClusterRef{}
+var DataprocClusterGVK = GroupVersion.WithKind("DataprocCluster")
+
+// DataprocClusterRef defines the resource reference to DataprocCluster, which "External" field
+// holds the GCP identifier for the KRM object.
+type DataprocClusterRef struct {
+	// A reference to an externally managed DataprocCluster resource.
+	// Should be in the format "projects/{project}/regions/{region}/clusters/{cluster}".
+	External string `json:"external,omitempty"`
+
+	// The name of a DataprocCluster resource.
+	Name string `json:"name,omitempty"`
+
+	// The namespace of a DataprocCluster resource.
+	Namespace string `json:"namespace,omitempty"`
+}
+
+func init() {
+	refs.Register(&DataprocClusterRef{})
+}
+
+func (r *DataprocClusterRef) GetGVK() schema.GroupVersionKind {
+	return DataprocClusterGVK
+}
+
+func (r *DataprocClusterRef) GetNamespacedName() types.NamespacedName {
+	return types.NamespacedName{
+		Name:      r.Name,
+		Namespace: r.Namespace,
+	}
+}
+
+func (r *DataprocClusterRef) GetExternal() string {
+	return r.External
+}
+
+func (r *DataprocClusterRef) SetExternal(ref string) {
+	r.External = ref
+}
+
+func (r *DataprocClusterRef) ValidateExternal(ref string) error {
+	id := &DataprocClusterIdentity{}
+	if err := id.FromExternal(ref); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *DataprocClusterRef) ParseExternalToIdentity() (identity.Identity, error) {
+	id := &DataprocClusterIdentity{}
+	if err := id.FromExternal(r.External); err != nil {
+		return nil, err
+	}
+	return id, nil
+}
+
+func (r *DataprocClusterRef) Normalize(ctx context.Context, reader client.Reader, defaultNamespace string) error {
+	fallback := func(u *unstructured.Unstructured) string {
+		identity, err := getIdentityFromDataprocClusterSpec(ctx, reader, u)
+		if err != nil {
+			return ""
+		}
+		return identity.String()
+	}
+	return refs.NormalizeWithFallback(ctx, reader, r, defaultNamespace, fallback)
+}
+
+func (r *DataprocClusterRef) NormalizedExternal(ctx context.Context, reader client.Reader, defaultNamespace string) (string, error) {
+	if err := r.Normalize(ctx, reader, defaultNamespace); err != nil {
+		return "", err
+	}
+	return r.GetExternal(), nil
+}
