cf7a23a05eb46c9a3dac4adf60c4f3f86f8694cf
codebot-robot <codebot-robot@google.com>
2026-03-29 22:07:33 +0000

    Agent iteration: Apply changes

--- test.go
+++
@@ -1,9 +0,0 @@
-package main
-import (
-	"fmt"
-	"strings"
-)
-func main() {
-	method := "/google.bigtable.admin.v2.BigtableTableAdmin/GetSchemaBundle"
-	fmt.Println(strings.Contains(method, "/Get"))
-}
