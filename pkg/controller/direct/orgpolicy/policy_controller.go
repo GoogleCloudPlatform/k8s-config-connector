bd3a70070613a52eb184ebd3e68decace80a96c3
Anh Le <anhdle@google.com>
2026-04-03 18:53:02 +0000

    Fix protobuf comparison in OrgPolicyPolicy structured reporting

--- pkg/controller/direct/orgpolicy/policy_controller.go
+++ pkg/controller/direct/orgpolicy/policy_controller.go
@@ -170,17 +170,19 @@ func (a *PolicyAdapter) Update(ctx context.Context, updateOp *directbase.UpdateO
 		return mapCtx.Err()
 	}
 
-	if proto.Equal(a.actual.GetSpec(), desiredPb.GetSpec()) && proto.Equal(a.actual.GetDryRunSpec(), desiredPb.GetDryRunSpec()) {
+	report := &structuredreporting.Diff{Object: updateOp.GetUnstructured()}
+	if !proto.Equal(a.actual.GetSpec(), desiredPb.GetSpec()) {
+		report.AddField("policy.spec", a.actual.GetSpec(), desiredPb.GetSpec())
+	}
+
+	if !proto.Equal(a.actual.GetDryRunSpec(), desiredPb.GetDryRunSpec()) {
+		report.AddField("policy.dry_run_spec", a.actual.GetDryRunSpec(), desiredPb.GetDryRunSpec())
+	}
+
+	if !report.HasDiff() {
 		log.V(2).Info("Policy is already up to date", "name", a.id)
 		// The resource is already up to date, but we still need to update the status
-		// to reflect the actual state from the backend.
-		status := &krm.OrgPolicyPolicyStatus{}
-		status.ObservedState = OrgPolicyPolicyObservedState_FromProto(mapCtx, a.actual)
-		if mapCtx.Err() != nil {
-			return mapCtx.Err()
-		}
-		status.ExternalRef = direct.LazyPtr(a.id.String())
-		return updateOp.UpdateStatus(ctx, status, nil)
+		return updateStatus(ctx, updateOp, a)
 	}
 
 	req := &orgpolicypb.UpdatePolicyRequest{
@@ -189,11 +191,7 @@ func (a *PolicyAdapter) Update(ctx context.Context, updateOp *directbase.UpdateO
 
 	// Let the backend handle validation
 	req.UpdateMask = &fieldmaskpb.FieldMask{
-		Paths: []string{"policy.spec", "policy.dry_run_spec"},
-	}
-	report := &structuredreporting.Diff{Object: updateOp.GetUnstructured()}
-	for _, path := range req.UpdateMask.Paths {
-		report.AddField(path, nil, nil)
+		Paths: report.FieldIDs(),
 	}
 	structuredreporting.ReportDiff(ctx, report)
 
@@ -202,9 +200,14 @@ func (a *PolicyAdapter) Update(ctx context.Context, updateOp *directbase.UpdateO
 		return fmt.Errorf("updating Policy %s: %w", a.id, err)
 	}
 	log.V(2).Info("successfully updated Policy", "name", a.id)
+	a.actual = updated
+	return updateStatus(ctx, updateOp, a)
+}
 
+func updateStatus(ctx context.Context, updateOp *directbase.UpdateOperation, a *PolicyAdapter) error {
+	mapCtx := &direct.MapContext{}
 	status := &krm.OrgPolicyPolicyStatus{}
-	status.ObservedState = OrgPolicyPolicyObservedState_FromProto(mapCtx, updated)
+	status.ObservedState = OrgPolicyPolicyObservedState_FromProto(mapCtx, a.actual)
 	if mapCtx.Err() != nil {
 		return mapCtx.Err()
 	}
