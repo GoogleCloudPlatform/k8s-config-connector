9fec59ff36f74134e22811549c4d49501f296061
codebot-robot <codebot-robot@google.com>
2026-04-04 14:24:53 +0000

    Support for Bigtable Authorized View
    This PR adds support for BigtableAuthorizedView resource in v1beta1 version.
    It includes:
    - Promotion of BigtableAuthorizedView from v1alpha1 to v1beta1.
    - Implementation of a direct controller for BigtableAuthorizedView.
    - Support for manual mapping of FamilySubsets map field.
    - Mock support and bug fix for UpdateAuthorizedView in mockgcp.
    - E2E test fixture for BigtableAuthorizedView.
    
    Fixes #6971

---
+++ apis/bigtable/v1beta1/authorizedview_identity.go
@@ -0,0 +1,111 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common/parent"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// AuthorizedViewIdentity defines the resource reference to BigtableAuthorizedView, which "External" field
+// holds the GCP identifier for the KRM object.
+type AuthorizedViewIdentity struct {
+	parent *TableIdentity
+	id     string
+}
+
+func (i *AuthorizedViewIdentity) String() string {
+	return i.parent.String() + "/authorizedViews/" + i.id
+}
+
+func (i *AuthorizedViewIdentity) ID() string {
+	return i.id
+}
+
+// New builds a AuthorizedViewIdentity from the Config Connector AuthorizedView object.
+func NewAuthorizedViewIdentity(ctx context.Context, reader client.Reader, obj *BigtableAuthorizedView) (*AuthorizedViewIdentity, error) {
+
+	// Get Parent
+	tableExternal, err := obj.Spec.TableRef.NormalizedExternal(ctx, reader, obj.GetNamespace())
+	if err != nil {
+		return nil, err
+	}
+	tableParent, tableID, err := ParseTableExternal(tableExternal)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get desired ID
+	resourceID := common.ValueOf(obj.Spec.ResourceID)
+	if resourceID == "" {
+		resourceID = obj.GetName()
+	}
+	if resourceID == "" {
+		return nil, fmt.Errorf("cannot resolve resource ID")
+	}
+
+	// Use approved External
+	externalRef := common.ValueOf(obj.Status.ExternalRef)
+	if externalRef != "" {
+		// Validate desired with actual
+		actualParent, actualResourceID, err := ParseAuthorizedViewExternal(externalRef)
+		if err != nil {
+			return nil, err
+		}
+		if actualParent.Parent.Parent.ProjectID != tableParent.Parent.ProjectID {
+			return nil, fmt.Errorf("spec.tableRef ProjectID changed, expect %s, got %s", actualParent.Parent.Parent.ProjectID, tableParent.Parent.ProjectID)
+		}
+		if actualParent.Parent.Id != tableParent.Id {
+			return nil, fmt.Errorf("spec.tableRef InstanceID changed, expect %s, got %s", actualParent.Parent.Id, tableParent.Id)
+		}
+		if actualParent.Id != tableID {
+			return nil, fmt.Errorf("spec.tableRef tableID changed, expect %s, got %s", actualParent.Id, tableID)
+		}
+		if actualResourceID != resourceID {
+			return nil, fmt.Errorf("cannot reset `metadata.name` or `spec.resourceID` to %s, since it has already assigned to %s",
+				resourceID, actualResourceID)
+		}
+	}
+	return &AuthorizedViewIdentity{
+		parent: &TableIdentity{
+			Parent: tableParent,
+			Id:     tableID,
+		},
+		id: resourceID,
+	}, nil
+}
+
+func ParseAuthorizedViewExternal(external string) (*TableIdentity, string, error) {
+	tokens := strings.Split(external, "/")
+	if len(tokens) != 8 || tokens[0] != "projects" || tokens[2] != "instances" || tokens[4] != "tables" || tokens[6] != "authorizedViews" {
+		return nil, "", fmt.Errorf("format of BigtableAuthorizedView external=%q was not known (use projects/{{projectID}}/instances/{{instanceID}}/tables/{{tableID}}/authorizedViews/{{authorizedViewID}})", external)
+	}
+	p := &TableIdentity{
+		Parent: &InstanceIdentity{
+			Parent: &parent.ProjectParent{
+				ProjectID: tokens[1],
+			},
+			Id: tokens[3],
+		},
+		Id: tokens[5],
+	}
+	resourceID := tokens[7]
+	return p, resourceID, nil
+}
---
+++ pkg/controller/direct/bigtable/authorizedview_controller.go
@@ -0,0 +1,240 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bigtable
+
+import (
+	"context"
+	"fmt"
+
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/bigtable/v1beta1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/config"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/directbase"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/registry"
+
+	adminpb "cloud.google.com/go/bigtable/admin/apiv2/adminpb"
+	"google.golang.org/api/option"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/klog/v2"
+)
+
+func init() {
+	registry.RegisterModel(krm.BigtableAuthorizedViewGVK, NewAuthorizedViewModel)
+}
+
+func NewAuthorizedViewModel(ctx context.Context, config *config.ControllerConfig) (directbase.Model, error) {
+	return &modelAuthorizedView{config: *config}, nil
+}
+
+var _ directbase.Model = &modelAuthorizedView{}
+
+type modelAuthorizedView struct {
+	config config.ControllerConfig
+}
+
+func (m *modelAuthorizedView) client(ctx context.Context) (adminpb.BigtableTableAdminClient, error) {
+	var opts []option.ClientOption
+	opts, err := m.config.GRPCClientOptions()
+	if err != nil {
+		return nil, fmt.Errorf("building BigtableAuthorizedView client options: %w", err)
+	}
+	conn, err := m.config.GRPCConnection(ctx, "bigtableadmin.googleapis.com", opts...)
+	if err != nil {
+		return nil, fmt.Errorf("building BigtableAuthorizedView connection: %w", err)
+	}
+	return adminpb.NewBigtableTableAdminClient(conn), nil
+}
+
+func (m *modelAuthorizedView) AdapterForObject(ctx context.Context, op *directbase.AdapterForObjectOperation) (directbase.Adapter, error) {
+	u := op.GetUnstructured()
+	reader := op.Reader
+	obj := &krm.BigtableAuthorizedView{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, &obj); err != nil {
+		return nil, fmt.Errorf("error converting to %T: %w", obj, err)
+	}
+
+	id, err := krm.NewAuthorizedViewIdentity(ctx, reader, obj)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := m.client(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("error creating bigtable table admin client: %w", err)
+	}
+	return &AuthorizedViewAdapter{
+		id:        id,
+		gcpClient: client,
+		desired:   obj,
+	}, nil
+}
+
+func (m *modelAuthorizedView) AdapterForURL(ctx context.Context, url string) (directbase.Adapter, error) {
+	// TODO: Support URLs
+	return nil, nil
+}
+
+type AuthorizedViewAdapter struct {
+	id        *krm.AuthorizedViewIdentity
+	gcpClient adminpb.BigtableTableAdminClient
+	desired   *krm.BigtableAuthorizedView
+	actual    *adminpb.AuthorizedView
+}
+
+var _ directbase.Adapter = &AuthorizedViewAdapter{}
+
+func (a *AuthorizedViewAdapter) Find(ctx context.Context) (bool, error) {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("getting BigtableAuthorizedView", "name", a.id)
+
+	req := &adminpb.GetAuthorizedViewRequest{
+		Name: a.id.String(),
+	}
+	authorizedView, err := a.gcpClient.GetAuthorizedView(ctx, req)
+	if err != nil {
+		if direct.IsNotFound(err) {
+			return false, nil
+		}
+		return false, fmt.Errorf("getting BigtableAuthorizedView %q: %w", a.id, err)
+	}
+
+	a.actual = authorizedView
+	return true, nil
+}
+
+func (a *AuthorizedViewAdapter) Create(ctx context.Context, createOp *directbase.CreateOperation) error {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("creating BigtableAuthorizedView", "name", a.id)
+	mapCtx := &direct.MapContext{}
+
+	proto := BigtableAuthorizedViewSpec_v1beta1_ToProto(mapCtx, &a.desired.Spec)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+
+	req := &adminpb.CreateAuthorizedViewRequest{
+		Parent:           a.id.Parent().String(),
+		AuthorizedViewId: a.id.ID(),
+		AuthorizedView:   proto,
+	}
+	created, err := a.gcpClient.CreateAuthorizedView(ctx, req)
+	if err != nil {
+		return fmt.Errorf("creating BigtableAuthorizedView %s: %w", a.id, err)
+	}
+	log.V(2).Info("successfully created BigtableAuthorizedView", "name", a.id)
+
+	status := &krm.BigtableAuthorizedViewStatus{}
+	status.ExternalRef = direct.LazyPtr(created.GetName())
+	status.ObservedState = BigtableAuthorizedViewObservedState_v1beta1_FromProto(mapCtx, created)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+	if err := createOp.UpdateStatus(ctx, status, nil); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *AuthorizedViewAdapter) Update(ctx context.Context, updateOp *directbase.UpdateOperation) error {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("updating BigtableAuthorizedView", "name", a.id)
+
+	mapCtx := &direct.MapContext{}
+	desired := BigtableAuthorizedViewSpec_v1beta1_ToProto(mapCtx, &a.desired.Spec)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+
+	updateMask := &fieldmaskpb.FieldMask{}
+
+	if !direct.IsNil(a.desired.Spec.SubsetView) {
+		updateMask.Paths = append(updateMask.Paths, "subset_view")
+	}
+	if !direct.IsNil(a.desired.Spec.DeletionProtection) {
+		updateMask.Paths = append(updateMask.Paths, "deletion_protection")
+	}
+
+	if len(updateMask.Paths) == 0 {
+		log.V(2).Info("no field needs update", "name", a.id)
+		return nil
+	}
+
+	desired.Name = a.id.String()
+	req := &adminpb.UpdateAuthorizedViewRequest{
+		AuthorizedView: desired,
+		UpdateMask:     updateMask,
+	}
+	updated, err := a.gcpClient.UpdateAuthorizedView(ctx, req)
+	if err != nil {
+		return fmt.Errorf("updating BigtableAuthorizedView %s: %w", a.id, err)
+	}
+	log.V(2).Info("successfully updated BigtableAuthorizedView", "name", a.id)
+
+	status := &krm.BigtableAuthorizedViewStatus{}
+	status.ExternalRef = direct.LazyPtr(updated.GetName())
+	status.ObservedState = BigtableAuthorizedViewObservedState_v1beta1_FromProto(mapCtx, updated)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+	return updateOp.UpdateStatus(ctx, status, nil)
+}
+
+func (a *AuthorizedViewAdapter) Export(ctx context.Context) (*unstructured.Unstructured, error) {
+	if a.actual == nil {
+		return nil, fmt.Errorf("Find() not called")
+	}
+	mapCtx := &direct.MapContext{}
+	spec := BigtableAuthorizedViewSpec_v1beta1_FromProto(mapCtx, a.actual)
+	if mapCtx.Err() != nil {
+		return nil, mapCtx.Err()
+	}
+
+	// Parent references
+	spec.InstanceRef = krm.InstanceRef{External: a.id.Parent().Parent().Id}
+	spec.TableRef = krm.TableRef{External: a.id.Parent().String()}
+
+	obj := &krm.BigtableAuthorizedView{}
+	obj.Spec = *spec
+	uObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+	if err != nil {
+		return nil, err
+	}
+
+	u := &unstructured.Unstructured{Object: uObj}
+	u.SetName(a.id.ID())
+	u.SetGroupVersionKind(krm.BigtableAuthorizedViewGVK)
+	return u, nil
+}
+
+func (a *AuthorizedViewAdapter) Delete(ctx context.Context, deleteOp *directbase.DeleteOperation) (bool, error) {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("deleting BigtableAuthorizedView", "name", a.id)
+
+	req := &adminpb.DeleteAuthorizedViewRequest{
+		Name: a.id.String(),
+	}
+	err := a.gcpClient.DeleteAuthorizedView(ctx, req)
+	if err != nil {
+		if direct.IsNotFound(err) {
+			return true, nil
+		}
+		return false, fmt.Errorf("deleting BigtableAuthorizedView %s: %w", a.id, err)
+	}
+	log.V(2).Info("successfully deleted BigtableAuthorizedView", "name", a.id)
+	return true, nil
+}
