0d5c6f1eee5e3f980f0e3ab40122c19576f19a24
codebot-robot <codebot-robot@google.com>
2026-04-04 20:23:20 +0000

    Regenerate KRM API code and docs
    This restores the deleted pkg/clients/generated/apis/resourcemanager/v1beta1/resourcemanagerpolicy_types.go file that is still expected to exist by the clientset generator, and updates its zz_generated.deepcopy.go accordingly.

--- pkg/clients/generated/apis/dataproc/v1beta1/zz_generated.deepcopy.go
+++ pkg/clients/generated/apis/dataproc/v1beta1/zz_generated.deepcopy.go
@@ -1679,6 +1679,11 @@ func (in *DataprocAutoscalingPolicyStatus) DeepCopyInto(out *DataprocAutoscaling
 		*out = make([]v1alpha1.Condition, len(*in))
 		copy(*out, *in)
 	}
+	if in.ExternalRef != nil {
+		in, out := &in.ExternalRef, &out.ExternalRef
+		*out = new(string)
+		**out = **in
+	}
 	if in.ObservedGeneration != nil {
 		in, out := &in.ObservedGeneration, &out.ObservedGeneration
 		*out = new(int64)
