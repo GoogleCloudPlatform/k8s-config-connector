f18b85d309c28ca32c16c18c9ba59471943907f6
codebot-robot <codebot-robot@google.com>
2026-04-07 17:19:18 +0000

    Fix ConnectivityTest fuzzer by marking missing gke_pod fields as untriaged

--- pkg/controller/direct/networkmanagement/connectivitytest_fuzzer.go
+++ pkg/controller/direct/networkmanagement/connectivitytest_fuzzer.go
@@ -121,6 +121,9 @@ func NetworkManagementConnectivityTestFuzzer() fuzztesting.KRMFuzzer {
 	f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].nat.cloud_nat_gateway_type")
 	f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].nat.cloud_nat_gateway_type")
 
+	f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].gke_pod")
+	f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].gke_pod")
+
 	f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].ip_masquerading_skipped")
 	f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].ip_masquerading_skipped")
 
