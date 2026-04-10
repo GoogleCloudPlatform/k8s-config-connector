8c3aebab3d4f87860be9a103440ad5c4de7eb9d4
codebot-robot <codebot-robot@google.com>
2026-04-10 17:46:24 +0000

    Add MockGCP implementation for ComputeFutureReservation
    Generated by Overseer.

--- mockgcp/mockcompute/service.go
+++ mockgcp/mockcompute/service.go
@@ -27,6 +27,7 @@ import (
 	"google.golang.org/protobuf/proto"
 
 	pb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/compute/v1"
+	pbv1beta "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/compute/v1beta"
 )
 
 func init() {
@@ -121,6 +122,8 @@ func (s *MockService) Register(grpcServer *grpc.Server) {
 	pb.RegisterInstanceTemplatesServer(grpcServer, &InstanceTemplatesV1{MockService: s})
 
 	pb.RegisterZonesServer(grpcServer, &ZonesV1{MockService: s})
+
+	pbv1beta.RegisterFutureReservationsServer(grpcServer, &FutureReservationsV1Beta1{MockService: s})
 }
 
 func (s *MockService) NewHTTPMux(ctx context.Context, conn *grpc.ClientConn) (http.Handler, error) {
@@ -280,6 +283,10 @@ func (s *MockService) NewHTTPMux(ctx context.Context, conn *grpc.ClientConn) (ht
 		return nil, err
 	}
 	if err := pb.RegisterZonesHandler(ctx, mux.ServeMux, conn); err != nil {
+		return nil, err
+	}
+
+	if err := pbv1beta.RegisterFutureReservationsHandler(ctx, mux.ServeMux, conn); err != nil {
 		return nil, err
 	}
 
