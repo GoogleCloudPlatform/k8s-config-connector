9a30d0bcfea8332754f3ae24b450bed415505fbc
codebot-robot <codebot-robot@google.com>
2026-04-11 21:46:43 +0000

    Fix formatting in mockgcp/mockdataproc/cluster.go

--- mockgcp/mockdataproc/cluster.go
+++ mockgcp/mockdataproc/cluster.go
@@ -347,7 +347,8 @@ func (s *clusterControllerServer) UpdateCluster(ctx context.Context, req *pb.Upd
 		case "config.worker_config.num_instances":
 			description = "Add 1 workers."
 			updated.Config.WorkerConfig.NumInstances = req.GetCluster().GetConfig().GetWorkerConfig().GetNumInstances()
-		case "config.security_config.identity_config.user_service_account_mapping", "config.securityConfig.identityConfig.userServiceAccountMapping":			if updated.Config.SecurityConfig == nil {
+		case "config.security_config.identity_config.user_service_account_mapping", "config.securityConfig.identityConfig.userServiceAccountMapping":
+			if updated.Config.SecurityConfig == nil {
 				updated.Config.SecurityConfig = &pb.SecurityConfig{}
 			}
 			if updated.Config.SecurityConfig.IdentityConfig == nil {
