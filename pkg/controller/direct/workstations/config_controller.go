a4a66b86eb334045a462409eda145f83cd9ccead
codebot-robot <codebot-robot@google.com>
2026-04-11 06:49:57 +0000

    Implement Direct IAM for WorkstationConfig
    Fixes #7510. This PR was generated by Overseer.

--- pkg/controller/direct/workstations/config_controller.go
+++ pkg/controller/direct/workstations/config_controller.go
@@ -28,6 +28,7 @@ import (
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/structuredreporting"
 
 	gcp "cloud.google.com/go/workstations/apiv1"
+	iampb "cloud.google.com/go/iam/apiv1/iampb"
 	pb "cloud.google.com/go/workstations/apiv1/workstationspb"
 	"google.golang.org/api/option"
 	"google.golang.org/protobuf/types/known/fieldmaskpb"
@@ -319,4 +320,29 @@ func (a *WorkstationConfigAdapter) Delete(ctx context.Context, deleteOp *directb
 	}
 
 	return true, nil
+}
+
+func (a *WorkstationConfigAdapter) GetIAMPolicy(ctx context.Context) (*iampb.Policy, error) {
+	req := &iampb.GetIamPolicyRequest{
+		Resource: a.id.String(),
+	}
+	policy, err := a.gcpClient.GetIamPolicy(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("getting iam policy for %q: %w", a.id.String(), err)
+	}
+
+	return policy, nil
+}
+
+func (a *WorkstationConfigAdapter) SetIAMPolicy(ctx context.Context, policy *iampb.Policy) (*iampb.Policy, error) {
+	req := &iampb.SetIamPolicyRequest{
+		Resource: a.id.String(),
+		Policy:   policy,
+	}
+	newPolicy, err := a.gcpClient.SetIamPolicy(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("setting iam policy for %q: %w", a.id.String(), err)
+	}
+
+	return newPolicy, nil
 }
