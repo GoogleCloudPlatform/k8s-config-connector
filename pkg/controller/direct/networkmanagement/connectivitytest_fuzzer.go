f4c7fed53f84043fa7a28a1bbace6d3230dc3a28
codebot-robot <codebot-robot@google.com>
2026-04-13 22:13:41 +0000

    Fix networkmanagement connectivitytest fuzzer

--- pkg/controller/direct/networkmanagement/connectivitytest_fuzzer.go
+++ pkg/controller/direct/networkmanagement/connectivitytest_fuzzer.go
@@ -99,6 +99,11 @@ func NetworkManagementConnectivityTestFuzzer() fuzztesting.KRMFuzzer {
 	f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].hybrid_subnet")
 	f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].hybrid_subnet")
 
+	f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].ngfw_packet_inspection")
+	f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].ngfw_packet_inspection")
+	f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].ngfw_packet_inspection.security_profile_group_uri")
+	f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].ngfw_packet_inspection.security_profile_group_uri")
+
 	f.Unimplemented_NotYetTriaged(".probing_details.edge_responses")
 	f.Unimplemented_NotYetTriaged(".destination.cloud_run_revision.service_uri")
 	f.Unimplemented_NotYetTriaged(".source.cloud_run_revision.service_uri")
