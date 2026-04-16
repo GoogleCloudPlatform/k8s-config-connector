7f046776c4452ebf3ebfa230562f583dd4e295a7
codebot-robot <codebot-robot@google.com>
2026-04-16 18:35:43 +0000

    feat: Add resource_manager_tags to ContainerCluster and ContainerNodePool
    Fixes #7604
    
    This PR adds support for resource manager tags in both ContainerCluster and ContainerNodePool resources.
    It updates the KCC vendored terraform-provider-google to add resource_manager_tags field to the node_config object, and regenerates the CRD schemas and Go types.

--- pkg/clients/generated/apis/container/v1beta1/containercluster_types.go
+++ pkg/clients/generated/apis/container/v1beta1/containercluster_types.go
@@ -756,6 +756,10 @@ type ClusterNodeConfig struct {
 	// +optional
 	ResourceLabels map[string]string `json:"resourceLabels,omitempty"`
 
+	/* A map of resource manager tags. Resource manager tag keys and values have the same definition as resource manager tags. Keys must be in the format tagKeys/{tag_key_id}, and values are in the format tagValues/456. The field is ignored (both PUT & PATCH) when empty. */
+	// +optional
+	ResourceManagerTags map[string]string `json:"resourceManagerTags,omitempty"`
+
 	/* Immutable. Sandbox configuration for this node. */
 	// +optional
 	SandboxConfig *ClusterSandboxConfig `json:"sandboxConfig,omitempty"`
