0feb62310635be504009575fd813aa82358831ce
codebot-robot <codebot-robot@google.com>
2026-04-22 22:31:14 +0000

    Fix Go client generation path and regenerate clients
    Corrected the trim-path-prefix and output-base in pkg/clients/generators.go
    to ensure deepcopy-gen outputs files in the correct relative path.
    Regenerated all Go clients and updated dependencies with make ensure.
    Removed the incorrect github.com/ directory created by misconfigured scripts.

--- pkg/clients/generators.go
+++ pkg/clients/generators.go
@@ -14,4 +14,4 @@
 
 package clients
 
-//go:generate go run k8s.io/code-generator/cmd/deepcopy-gen@v0.29.0 -O zz_generated.deepcopy -h ../../hack/boilerplate_client_alpha.go.txt -i github.com/GoogleCloudPlatform/k8s-config-connector/pkg/clients/... --output-base ../.. --trim-path-prefix github.com/GoogleCloudPlatform/k8s-config-connector/pkg/clients
+//go:generate go run k8s.io/code-generator/cmd/deepcopy-gen@v0.29.0 -O zz_generated.deepcopy -h ../../hack/boilerplate_client_alpha.go.txt -i github.com/GoogleCloudPlatform/k8s-config-connector/pkg/clients/... --output-base . --trim-path-prefix github.com/GoogleCloudPlatform/k8s-config-connector/pkg/clients
