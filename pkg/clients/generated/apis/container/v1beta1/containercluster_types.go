16f6a865355c60ba11381c5bac2dd39ad07756ce
codebot-robot <codebot-robot@google.com>
2026-04-23 22:28:06 +0000

    Update generated Container Go client to include missing fields

--- pkg/clients/generated/apis/container/v1beta1/containercluster_types.go
+++ pkg/clients/generated/apis/container/v1beta1/containercluster_types.go
@@ -38,6 +38,19 @@ import (
 
 var _ = apiextensionsv1.JSON{}
 
+type ClusterAdditionalIpRangesConfigs struct {
+	/* List of secondary ranges names within this subnetwork that can be used for pod IPs. */
+	// +optional
+	PodIpv4RangeNames []string `json:"podIpv4RangeNames,omitempty"`
+
+	/* Status of the subnetwork, If in draining status, subnet will not be selected for new node pools. */
+	// +optional
+	Status *string `json:"status,omitempty"`
+
+	/* The subnetwork path for the additional IP range. Format: projects/{project}/regions/{region}/subnetworks/{subnetwork}. */
+	SubnetworkRef k8sv1alpha1.ResourceRef `json:"subnetworkRef"`
+}
+
 type ClusterAdditionalPodRangesConfig struct {
 	/* Name for pod secondary ipv4 range which has the actual range defined ahead. */
 	PodRangeNames []string `json:"podRangeNames"`
@@ -426,6 +439,10 @@ type ClusterIdentityServiceConfig struct {
 }
 
 type ClusterIpAllocationPolicy struct {
+	/* AdditionalIpRangesConfigs is the configuration for additional pod secondary ranges supporting the ClusterUpdate message. Each AdditionalIPRangesConfig corresponds to a single subnetwork. */
+	// +optional
+	AdditionalIpRangesConfigs []ClusterAdditionalIpRangesConfigs `json:"additionalIpRangesConfigs,omitempty"`
+
 	/* AdditionalPodRangesConfig is the configuration for additional pod secondary ranges supporting the ClusterUpdate message. */
 	// +optional
 	AdditionalPodRangesConfig *ClusterAdditionalPodRangesConfig `json:"additionalPodRangesConfig,omitempty"`
