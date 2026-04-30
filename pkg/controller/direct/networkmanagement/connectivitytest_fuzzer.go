eeba2e53ef93eab18af96d2b4fc9db53dd23c6cf
codebot-robot <codebot-robot@google.com>
2026-04-30 16:25:01 +0000

    Fix ConnectivityTest fuzzer test failure by ignoring newly added upstream proto field

--- pkg/controller/direct/networkmanagement/connectivitytest_fuzzer.go
+++ pkg/controller/direct/networkmanagement/connectivitytest_fuzzer.go
@@ -176,9 +176,6 @@ func NetworkManagementConnectivityTestFuzzer() fuzztesting.KRMFuzzer {
 	f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].gke_network_policy")
 	f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].gke_network_policy")
 
-	f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].gke_network_policy_skipped")
-	f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].gke_network_policy_skipped")
-
 	f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].gke_network_policy_skipped.reason")
 	return f
 }
