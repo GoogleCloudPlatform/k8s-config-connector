c643c6b35c636177312ae74f47dac51938784e4e
codebot-robot <codebot-robot@google.com>
2026-05-01 03:11:16 +0000

    Fix NetworkManagementConnectivityTest fuzzer missing gke_network_policy_skipped

--- pkg/controller/direct/networkmanagement/connectivitytest_fuzzer.go
+++ pkg/controller/direct/networkmanagement/connectivitytest_fuzzer.go
@@ -178,5 +178,8 @@ func NetworkManagementConnectivityTestFuzzer() fuzztesting.KRMFuzzer {
 	f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].gke_network_policy")
 	f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].gke_network_policy")
 
+	f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].gke_network_policy_skipped")
+	f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].gke_network_policy_skipped")
+
 	return f
 }
