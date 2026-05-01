a5971e51a7992e82736bc698370b0534ffa5c7a7
codebot-robot <codebot-robot@google.com>
2026-05-01 12:23:16 +0000

    Implement Direct IAM for GKEBackupRestorePlan

--- pkg/controller/direct/registry/registry.go
+++ pkg/controller/direct/registry/registry.go
@@ -125,6 +125,8 @@ func IsIAMDirect(groupKind schema.GroupKind) bool {
 
 	// TODO: Move to registration somehow?
 	switch groupKind {
+	case schema.GroupKind{Group: "gkebackup.cnrm.cloud.google.com", Kind: "GKEBackupRestorePlan"}:
+		return true
 	case schema.GroupKind{Group: "privateca.cnrm.cloud.google.com", Kind: "PrivateCACAPool"}:
 		return true
 	}
