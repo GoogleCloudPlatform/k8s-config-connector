e9af6c876468c49443f1d0954fadd3de224c8b30
codebot-robot <codebot-robot@google.com>
2026-05-02 06:49:52 +0000

    Address PR review feedback
    - Revert unrelated fuzzer fix in connectivitytest_fuzzer.go
    - Use refs.Normalize() instead of refs.NormalizeWithFallback() for ComputeFutureReservationRef

--- pkg/controller/direct/networkmanagement/connectivitytest_fuzzer.go
+++ pkg/controller/direct/networkmanagement/connectivitytest_fuzzer.go
@@ -164,11 +164,11 @@ func NetworkManagementConnectivityTestFuzzer() fuzztesting.KRMFuzzer {
 	f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].gke_pod")
 	f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].gke_pod")
 
-	f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].ip_masquerading_skipped")
-	f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].ip_masquerading_skipped")
+        f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].ip_masquerading_skipped")
+        f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].ip_masquerading_skipped")
 
-	f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].ngfw_packet_inspection")
-	f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].ngfw_packet_inspection")
+        f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].ngfw_packet_inspection")
+        f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].ngfw_packet_inspection")
 
         f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].gke_network_policy_skipped")
         f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].gke_network_policy_skipped")
