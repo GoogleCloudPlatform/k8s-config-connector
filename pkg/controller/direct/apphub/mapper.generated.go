b254612025b91ff45638a328c70d54efc2d46500
codebot-robot <codebot-robot@google.com>
2026-05-02 16:53:05 +0000

    fixup! chore: apis/apphub should follow generate.sh pattern

--- pkg/controller/direct/apphub/mapper.generated.go
+++ pkg/controller/direct/apphub/mapper.generated.go
@@ -21,36 +21,6 @@
 package apphub
 
 /* found existing non-generated mapping function "AppHubApplicationObservedState_FromProto", skipping
-func AppHubApplicationObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Application) *krmapphubv1alpha1.AppHubApplicationObservedState {
-	if in == nil {
-		return nil
-	}
-	out := &krmapphubv1alpha1.AppHubApplicationObservedState{}
-	// MISSING: Name
-	out.CreateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetCreateTime())
-	out.UpdateTime = direct.StringTimestamp_FromProto(mapCtx, in.GetUpdateTime())
-	out.Uid = direct.LazyPtr(in.GetUid())
-	out.State = direct.Enum_FromProto(mapCtx, in.GetState())
-	return out
-}
-*/
-
-/* found existing non-generated mapping function "AppHubApplicationObservedState_ToProto", skipping
-func AppHubApplicationObservedState_ToProto(mapCtx *direct.MapContext, in *krmapphubv1alpha1.AppHubApplicationObservedState) *pb.Application {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Application{}
-	// MISSING: Name
-	out.CreateTime = direct.StringTimestamp_ToProto(mapCtx, in.CreateTime)
-	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
-	out.Uid = direct.ValueOf(in.Uid)
-	out.State = direct.Enum_ToProto[pb.Application_State](mapCtx, in.State)
-	return out
-}
-*/
-
-/* found existing non-generated mapping function "AppHubApplicationObservedState_FromProto", skipping
 func AppHubApplicationObservedState_FromProto(mapCtx *direct.MapContext, in *pb.Application) *krm.AppHubApplicationObservedState {
 	if in == nil {
 		return nil
@@ -76,36 +46,6 @@ func AppHubApplicationObservedState_ToProto(mapCtx *direct.MapContext, in *krm.A
 	out.UpdateTime = direct.StringTimestamp_ToProto(mapCtx, in.UpdateTime)
 	out.Uid = direct.ValueOf(in.Uid)
 	out.State = direct.Enum_ToProto[pb.Application_State](mapCtx, in.State)
-	return out
-}
-*/
-
-/* found existing non-generated mapping function "AppHubApplicationSpec_FromProto", skipping
-func AppHubApplicationSpec_FromProto(mapCtx *direct.MapContext, in *pb.Application) *krmapphubv1alpha1.AppHubApplicationSpec {
-	if in == nil {
-		return nil
-	}
-	out := &krmapphubv1alpha1.AppHubApplicationSpec{}
-	// MISSING: Name
-	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
-	out.Description = direct.LazyPtr(in.GetDescription())
-	out.Attributes = Attributes_FromProto(mapCtx, in.GetAttributes())
-	out.Scope = Scope_FromProto(mapCtx, in.GetScope())
-	return out
-}
-*/
-
-/* found existing non-generated mapping function "AppHubApplicationSpec_ToProto", skipping
-func AppHubApplicationSpec_ToProto(mapCtx *direct.MapContext, in *krmapphubv1alpha1.AppHubApplicationSpec) *pb.Application {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Application{}
-	// MISSING: Name
-	out.DisplayName = direct.ValueOf(in.DisplayName)
-	out.Description = direct.ValueOf(in.Description)
-	out.Attributes = Attributes_ToProto(mapCtx, in.Attributes)
-	out.Scope = Scope_ToProto(mapCtx, in.Scope)
 	return out
 }
 */
@@ -237,36 +177,6 @@ func AppHubDiscoveredWorkloadSpec_ToProto(mapCtx *direct.MapContext, in *krmapph
 */
 
 /* found existing non-generated mapping function "Attributes_FromProto", skipping
-func Attributes_FromProto(mapCtx *direct.MapContext, in *pb.Attributes) *krmapphubv1alpha1.Attributes {
-	if in == nil {
-		return nil
-	}
-	out := &krmapphubv1alpha1.Attributes{}
-	out.Criticality = Criticality_FromProto(mapCtx, in.GetCriticality())
-	out.Environment = Environment_FromProto(mapCtx, in.GetEnvironment())
-	out.DeveloperOwners = direct.Slice_FromProto(mapCtx, in.DeveloperOwners, ContactInfo_FromProto)
-	out.OperatorOwners = direct.Slice_FromProto(mapCtx, in.OperatorOwners, ContactInfo_FromProto)
-	out.BusinessOwners = direct.Slice_FromProto(mapCtx, in.BusinessOwners, ContactInfo_FromProto)
-	return out
-}
-*/
-
-/* found existing non-generated mapping function "Attributes_ToProto", skipping
-func Attributes_ToProto(mapCtx *direct.MapContext, in *krmapphubv1alpha1.Attributes) *pb.Attributes {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Attributes{}
-	out.Criticality = Criticality_ToProto(mapCtx, in.Criticality)
-	out.Environment = Environment_ToProto(mapCtx, in.Environment)
-	out.DeveloperOwners = direct.Slice_ToProto(mapCtx, in.DeveloperOwners, ContactInfo_ToProto)
-	out.OperatorOwners = direct.Slice_ToProto(mapCtx, in.OperatorOwners, ContactInfo_ToProto)
-	out.BusinessOwners = direct.Slice_ToProto(mapCtx, in.BusinessOwners, ContactInfo_ToProto)
-	return out
-}
-*/
-
-/* found existing non-generated mapping function "Attributes_FromProto", skipping
 func Attributes_FromProto(mapCtx *direct.MapContext, in *pb.Attributes) *krm.Attributes {
 	if in == nil {
 		return nil
@@ -297,30 +207,6 @@ func Attributes_ToProto(mapCtx *direct.MapContext, in *krm.Attributes) *pb.Attri
 */
 
 /* found existing non-generated mapping function "ContactInfo_FromProto", skipping
-func ContactInfo_FromProto(mapCtx *direct.MapContext, in *pb.ContactInfo) *krmapphubv1alpha1.ContactInfo {
-	if in == nil {
-		return nil
-	}
-	out := &krmapphubv1alpha1.ContactInfo{}
-	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
-	out.Email = direct.LazyPtr(in.GetEmail())
-	return out
-}
-*/
-
-/* found existing non-generated mapping function "ContactInfo_ToProto", skipping
-func ContactInfo_ToProto(mapCtx *direct.MapContext, in *krmapphubv1alpha1.ContactInfo) *pb.ContactInfo {
-	if in == nil {
-		return nil
-	}
-	out := &pb.ContactInfo{}
-	out.DisplayName = direct.ValueOf(in.DisplayName)
-	out.Email = direct.ValueOf(in.Email)
-	return out
-}
-*/
-
-/* found existing non-generated mapping function "ContactInfo_FromProto", skipping
 func ContactInfo_FromProto(mapCtx *direct.MapContext, in *pb.ContactInfo) *krm.ContactInfo {
 	if in == nil {
 		return nil
@@ -340,28 +226,6 @@ func ContactInfo_ToProto(mapCtx *direct.MapContext, in *krm.ContactInfo) *pb.Con
 	out := &pb.ContactInfo{}
 	out.DisplayName = direct.ValueOf(in.DisplayName)
 	out.Email = direct.ValueOf(in.Email)
-	return out
-}
-*/
-
-/* found existing non-generated mapping function "Criticality_FromProto", skipping
-func Criticality_FromProto(mapCtx *direct.MapContext, in *pb.Criticality) *krmapphubv1alpha1.Criticality {
-	if in == nil {
-		return nil
-	}
-	out := &krmapphubv1alpha1.Criticality{}
-	out.Type = direct.Enum_FromProto(mapCtx, in.GetType())
-	return out
-}
-*/
-
-/* found existing non-generated mapping function "Criticality_ToProto", skipping
-func Criticality_ToProto(mapCtx *direct.MapContext, in *krmapphubv1alpha1.Criticality) *pb.Criticality {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Criticality{}
-	out.Type = direct.Enum_ToProto[pb.Criticality_Type](mapCtx, in.Type)
 	return out
 }
 */
@@ -389,28 +253,6 @@ func Criticality_ToProto(mapCtx *direct.MapContext, in *krm.Criticality) *pb.Cri
 */
 
 /* found existing non-generated mapping function "Environment_FromProto", skipping
-func Environment_FromProto(mapCtx *direct.MapContext, in *pb.Environment) *krmapphubv1alpha1.Environment {
-	if in == nil {
-		return nil
-	}
-	out := &krmapphubv1alpha1.Environment{}
-	out.Type = direct.Enum_FromProto(mapCtx, in.GetType())
-	return out
-}
-*/
-
-/* found existing non-generated mapping function "Environment_ToProto", skipping
-func Environment_ToProto(mapCtx *direct.MapContext, in *krmapphubv1alpha1.Environment) *pb.Environment {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Environment{}
-	out.Type = direct.Enum_ToProto[pb.Environment_Type](mapCtx, in.Type)
-	return out
-}
-*/
-
-/* found existing non-generated mapping function "Environment_FromProto", skipping
 func Environment_FromProto(mapCtx *direct.MapContext, in *pb.Environment) *krm.Environment {
 	if in == nil {
 		return nil
@@ -428,28 +270,6 @@ func Environment_ToProto(mapCtx *direct.MapContext, in *krm.Environment) *pb.Env
 	}
 	out := &pb.Environment{}
 	out.Type = direct.Enum_ToProto[pb.Environment_Type](mapCtx, in.Type)
-	return out
-}
-*/
-
-/* found existing non-generated mapping function "Scope_FromProto", skipping
-func Scope_FromProto(mapCtx *direct.MapContext, in *pb.Scope) *krmapphubv1alpha1.Scope {
-	if in == nil {
-		return nil
-	}
-	out := &krmapphubv1alpha1.Scope{}
-	out.Type = direct.Enum_FromProto(mapCtx, in.GetType())
-	return out
-}
-*/
-
-/* found existing non-generated mapping function "Scope_ToProto", skipping
-func Scope_ToProto(mapCtx *direct.MapContext, in *krmapphubv1alpha1.Scope) *pb.Scope {
-	if in == nil {
-		return nil
-	}
-	out := &pb.Scope{}
-	out.Type = direct.Enum_ToProto[pb.Scope_Type](mapCtx, in.Type)
 	return out
 }
 */
