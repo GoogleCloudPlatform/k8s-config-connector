68b0bbf5d45c31241b46d315d4d2945de4b6080a
codebot-robot <codebot-robot@google.com>
2026-05-04 18:38:28 +0000

    Fix #7906: Move DataplexLake to identity and refs pattern
    This commit converts the DataplexLake resource to use the new identity.IdentityV2 and refs.Ref interfaces with gcpurls.Template. It also makes the necessary adjustments to DataplexTask and dependent direct controllers to accommodate the new identity pattern.

--- apis/dataplex/v1alpha1/task_identity.go
+++ apis/dataplex/v1alpha1/task_identity.go
@@ -1,4 +1,4 @@
-// Copyright 2025 Google LLC
+// Copyright 2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,93 +17,98 @@ package v1alpha1
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common/identity"
-	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common/parent"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/gcpurls"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-var _ identity.Identity = &TaskIdentity{}
-
-const (
-	TaskIDURL = LakeIDURL + "/tasks/{{taskID}}"
+var (
+	_ identity.IdentityV2 = &TaskIdentity{}
+	_ identity.Resource   = &DataplexTask{}
 )
 
-// TaskIdentity defines the resource reference to DataplexTask, which "External" field
-// holds the GCP identifier for the KRM object.
+var TaskIdentityFormat = gcpurls.Template[TaskIdentity]("dataplex.googleapis.com", "projects/{project}/locations/{location}/lakes/{lake}/tasks/{task}")
+
+// +k8s:deepcopy-gen=false
 type TaskIdentity struct {
-	parent *LakeIdentity
-	id     string
+	Project  string
+	Location string
+	Lake     string
+	Task     string
 }
 
 func (i *TaskIdentity) String() string {
-	// Uses the pattern: projects/{project}/locations/{location}/lakes/{lake}/tasks/{task}
-	return i.parent.String() + "/tasks/" + i.id
-}
-
-func (i *TaskIdentity) ID() string {
-	return i.id
-}
-
-func (i *TaskIdentity) Parent() *LakeIdentity {
-	return i.parent
+	return TaskIdentityFormat.ToString(*i)
 }
 
 func (i *TaskIdentity) FromExternal(ref string) error {
-	tokens := strings.Split(ref, "/tasks/")
-	if len(tokens) != 2 {
-		return fmt.Errorf("format of DataplexTask external=%q was not known (use %s)", ref, TaskIDURL)
+	parsed, match, err := TaskIdentityFormat.Parse(ref)
+	if err != nil {
+		return fmt.Errorf("format of DataplexTask external=%q was not known (use %s): %w", ref, TaskIdentityFormat.CanonicalForm(), err)
 	}
-	i.parent = &LakeIdentity{}
-	if err := i.parent.FromExternal(tokens[0]); err != nil {
-		return err
+	if !match {
+		return fmt.Errorf("format of DataplexTask external=%q was not known (use %s)", ref, TaskIdentityFormat.CanonicalForm())
 	}
-	i.id = tokens[1]
-	if i.id == "" {
-		return fmt.Errorf("taskID was empty in external=%q", ref)
-	}
+
+	*i = *parsed
 	return nil
 }
 
-var _ identity.Resource = &DataplexTask{}
+func (i *TaskIdentity) Host() string {
+	return TaskIdentityFormat.Host()
+}
+
+func getIdentityFromDataplexTaskSpec(ctx context.Context, reader client.Reader, obj *DataplexTask) (*TaskIdentity, error) {
+	resourceID, err := refs.GetResourceID(obj)
+	if err != nil {
+		return nil, fmt.Errorf("cannot resolve resource ID")
+	}
+
+	lakeRef := obj.Spec.LakeRef
+	if lakeRef == nil {
+		return nil, fmt.Errorf("LakeRef is required")
+	}
+
+	if err := lakeRef.Normalize(ctx, reader, obj.GetNamespace()); err != nil {
+		return nil, fmt.Errorf("cannot normalize LakeRef: %w", err)
+	}
+
+	lakeIdentity := &LakeIdentity{}
+	if err := lakeIdentity.FromExternal(lakeRef.External); err != nil {
+		return nil, fmt.Errorf("cannot parse LakeRef external: %w", err)
+	}
+
+	identity := &TaskIdentity{
+		Project:  lakeIdentity.Project,
+		Location: lakeIdentity.Location,
+		Lake:     lakeIdentity.Lake,
+		Task:     resourceID,
+	}
+	return identity, nil
+}
 
 func (obj *DataplexTask) GetIdentity(ctx context.Context, reader client.Reader) (identity.Identity, error) {
-	newIdentity := &TaskIdentity{
-		parent: &LakeIdentity{
-			parent: &parent.ProjectAndLocationParent{},
-		},
-	}
-
-	// Resolve Parent
-	if err := obj.Spec.LakeRef.Normalize(ctx, reader, obj.GetNamespace()); err != nil {
-		return nil, err
-	}
-	if err := newIdentity.parent.FromExternal(obj.Spec.LakeRef.External); err != nil {
+	specIdentity, err := getIdentityFromDataplexTaskSpec(ctx, reader, obj)
+	if err != nil {
 		return nil, err
 	}
 
-	// Get desired Resource ID
-	newIdentity.id = common.ValueOf(obj.Spec.ResourceID)
-	if newIdentity.id == "" {
-		newIdentity.id = obj.GetName()
-	}
-	if newIdentity.id == "" {
-		return nil, fmt.Errorf("cannot determine resource ID (metadata.name or spec.resourceID)")
-	}
-
-	// Validate against existing ExternalRef if present
+	// Cross-check the identity against the status value, if present.
 	externalRef := common.ValueOf(obj.Status.ExternalRef)
 	if externalRef != "" {
+		// Validate desired with actual
 		statusIdentity := &TaskIdentity{}
 		if err := statusIdentity.FromExternal(externalRef); err != nil {
-			return nil, fmt.Errorf("cannot parse existing externalRef=%q: %w", externalRef, err)
+			return nil, err
 		}
-		if statusIdentity.String() != newIdentity.String() {
-			return nil, fmt.Errorf("existing externalRef=%q does not match the identity resolved from spec: %q", externalRef, newIdentity.String())
+
+		if statusIdentity.String() != specIdentity.String() {
+			return nil, fmt.Errorf("cannot change DataplexTask identity (old=%q, new=%q)", statusIdentity.String(), specIdentity.String())
 		}
 	}
 
-	return newIdentity, nil
+	return specIdentity, nil
 }
