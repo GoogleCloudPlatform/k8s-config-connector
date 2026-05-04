c53b11b0b83150e463181b42f7118500db54734c
codebot-robot <codebot-robot@google.com>
2026-05-04 23:16:06 +0000

    fix(fuzzer): add Unimplemented_NotYetTriaged for newly added upstream API fields
    Adding missing fields to fuzzers to fix CI failure after dependency bump.

--- pkg/controller/direct/networkmanagement/connectivitytest_fuzzer.go
+++ pkg/controller/direct/networkmanagement/connectivitytest_fuzzer.go
@@ -169,6 +169,8 @@ func NetworkManagementConnectivityTestFuzzer() fuzztesting.KRMFuzzer {
 
 	f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].ngfw_packet_inspection")
 	f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].ngfw_packet_inspection")
+	f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].ngfw_packet_inspection.security_profile_group_uri")
+	f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].ngfw_packet_inspection.security_profile_group_uri")
 
 	f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].gke_network_policy")
 	f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].gke_network_policy")
