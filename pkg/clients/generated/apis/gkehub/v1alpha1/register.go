68340996a17eb8f2ae9c3f8f70106260cfc70b70
codebot-robot <codebot-robot@google.com>
2026-05-05 20:44:15 +0000

    chore: regenerate go clients for GKEHubMembershipBinding

--- pkg/clients/generated/apis/gkehub/v1alpha1/register.go
+++ pkg/clients/generated/apis/gkehub/v1alpha1/register.go
@@ -53,6 +53,12 @@ var (
 	// AddToScheme is a global function that registers this API group & version to a scheme
 	AddToScheme = SchemeBuilder.AddToScheme
 
+	GKEHubMembershipBindingGVK = schema.GroupVersionKind{
+		Group:   SchemeGroupVersion.Group,
+		Version: SchemeGroupVersion.Version,
+		Kind:    reflect.TypeOf(GKEHubMembershipBinding{}).Name(),
+	}
+
 	GKEHubNamespaceGVK = schema.GroupVersionKind{
 		Group:   SchemeGroupVersion.Group,
 		Version: SchemeGroupVersion.Version,
