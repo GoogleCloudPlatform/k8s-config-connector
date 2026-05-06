5694c7ca37e6753ed2ff793731b081bcb0feea2b
codebot-robot <codebot-robot@google.com>
2026-05-06 00:09:13 +0000

    mockgcp: stop generating grpc-gateway for memorystore
    Moves memorystore to use the official proto client library with httptogrpc reflection, rather than locally generated grpc-gateway bindings.
    
    Fixes #7929

--- mockgcp/mockmemorystore/normalize.go
+++ mockgcp/mockmemorystore/normalize.go
@@ -22,22 +22,64 @@ import (
 
 var _ mockgcpregistry.SupportsNormalization = &MockService{}
 
+const (
+	PlaceholderTimestamp = "2024-04-01T12:34:56.123456Z"
+	PlaceholderUID       = "11111111-1111-1111-1111-111111111111"
+)
+
 func (s *MockService) ConfigureVisitor(url string, replacements mockgcpregistry.NormalizingVisitor) {
-	if !strings.Contains(url, "memorystore.googleapis.com") {
+	if !strings.HasPrefix(url, "https://memorystore.googleapis.com") && !strings.HasPrefix(url, "https://memorystore.mtls.googleapis.com") {
 		return
 	}
-	replacements.ReplacePath(".backupFiles[].createTime", "2024-04-01T12:34:56.123456Z")
-	replacements.ReplacePath(".crossInstanceReplicationConfig.updateTime", "2024-04-01T12:34:56.123456Z")
-	replacements.ReplacePath(".expireTime", "2024-04-01T12:34:56.123456Z")
-	replacements.ReplacePath(".maintenancePolicy.createTime", "2024-04-01T12:34:56.123456Z")
-	replacements.ReplacePath(".maintenancePolicy.updateTime", "2024-04-01T12:34:56.123456Z")
-	replacements.ReplacePath(".response.backupFiles[].createTime", "2024-04-01T12:34:56.123456Z")
-	replacements.ReplacePath(".response.crossInstanceReplicationConfig.updateTime", "2024-04-01T12:34:56.123456Z")
-	replacements.ReplacePath(".response.expireTime", "2024-04-01T12:34:56.123456Z")
-	replacements.ReplacePath(".response.maintenancePolicy.createTime", "2024-04-01T12:34:56.123456Z")
-	replacements.ReplacePath(".response.maintenancePolicy.updateTime", "2024-04-01T12:34:56.123456Z")
+
+	replacements.ReplacePath(".crossInstanceReplicationConfig.updateTime", PlaceholderTimestamp)
+	replacements.ReplacePath(".expireTime", PlaceholderTimestamp)
+	replacements.ReplacePath(".maintenancePolicy.createTime", PlaceholderTimestamp)
+	replacements.ReplacePath(".maintenancePolicy.updateTime", PlaceholderTimestamp)
+	replacements.ReplacePath(".response.crossInstanceReplicationConfig.updateTime", PlaceholderTimestamp)
+	replacements.ReplacePath(".response.expireTime", PlaceholderTimestamp)
+	replacements.ReplacePath(".response.maintenancePolicy.createTime", PlaceholderTimestamp)
+	replacements.ReplacePath(".response.maintenancePolicy.updateTime", PlaceholderTimestamp)
+
+	replacements.ReplacePath(".crossInstanceReplicationConfig.membership.primaryInstance.uid", PlaceholderUID)
+	replacements.ReplacePath(".crossInstanceReplicationConfig.membership.secondaryInstances[].uid", PlaceholderUID)
+	replacements.ReplacePath(".response.crossInstanceReplicationConfig.membership.primaryInstance.uid", PlaceholderUID)
+	replacements.ReplacePath(".response.crossInstanceReplicationConfig.membership.secondaryInstances[].uid", PlaceholderUID)
+	// Backups
+	replacements.ReplacePath(".backupFiles[].createTime", PlaceholderTimestamp)
+	replacements.ReplacePath(".backupFiles[].fileName", "fakefilename.rdb")
+	replacements.ReplacePath(".response.backupFiles[].createTime", PlaceholderTimestamp)
+	replacements.ReplacePath(".instanceUid", PlaceholderUID)
+	replacements.ReplacePath(".encryptionInfo.lastUpdateTime", PlaceholderTimestamp)
+
 }
 
 func (s *MockService) Previsit(event mockgcpregistry.Event, replacements mockgcpregistry.NormalizingVisitor) {
-	// No-op for now
+	if strings.Contains(event.URL(), "https://memorystore.googleapis.com") || strings.HasPrefix(event.URL(), "https://memorystore.mtls.googleapis.com") {
+
+		event.VisitResponseStringValues(func(path string, value string) {
+			switch path {
+			case ".name":
+				tokens := strings.Split(value, "/")
+
+				if len(tokens) == 8 && tokens[0] == "projects" && tokens[2] == "locations" && tokens[4] == "backupCollections" && tokens[6] == "backups" {
+					replacements.ReplaceStringValue(tokens[5], "${backupCollectionID}")
+					replacements.ReplaceStringValue(tokens[7], "${backup}")
+				}
+
+			case ".pscAttachmentDetails[].serviceAttachment":
+				tokens := strings.Split(value, "/")
+
+				if len(tokens) == 6 && tokens[0] == "projects" && tokens[2] == "regions" && tokens[4] == "serviceAttachments" {
+					replacements.ReplaceStringValue(tokens[1], "${pscProjectNumber}")
+					//replacements.ReplaceStringValue(tokens[3], "${region}")
+					if strings.HasSuffix(tokens[5], "-2") {
+						replacements.ReplaceStringValue(tokens[5], "${pscServiceAttachment}-2")
+					} else {
+						replacements.ReplaceStringValue(tokens[5], "${pscServiceAttachment}")
+					}
+				}
+			}
+		})
+	}
 }
