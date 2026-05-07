429642efa385c6f9bfb66b59027416603a16eaf1
codebot-robot <codebot-robot@google.com>
2026-05-07 04:49:04 +0000

    fix: format and update generator comments
    Applies gofmt and adds generated license headers to address the PR CI failures.

--- pkg/controller/direct/gkehub/scoperbacrolebinding_mapper.go
+++ pkg/controller/direct/gkehub/scoperbacrolebinding_mapper.go
@@ -1,12 +1,26 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package gkehub
 
 import (
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/gkehub/v1alpha1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 	gkehubv1 "google.golang.org/api/gkehub/v1"
-	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/gkehub/v1alpha1"
 )
 
-func GKEHubScopeRBACRoleBindingSpec_ToAPI(mapCtx *direct.MapContext, in *krm.GKEHubScopeRBACRoleBindingSpec) (*gkehubv1.RBACRoleBinding) {
+func GKEHubScopeRBACRoleBindingSpec_ToAPI(mapCtx *direct.MapContext, in *krm.GKEHubScopeRBACRoleBindingSpec) *gkehubv1.RBACRoleBinding {
 	if in == nil {
 		return nil
 	}
@@ -23,7 +37,7 @@ func GKEHubScopeRBACRoleBindingSpec_ToAPI(mapCtx *direct.MapContext, in *krm.GKE
 	return out
 }
 
-func GKEHubScopeRBACRoleBindingStatus_FromAPI(mapCtx *direct.MapContext, in *gkehubv1.RBACRoleBinding) (*krm.GKEHubScopeRBACRoleBindingStatus) {
+func GKEHubScopeRBACRoleBindingStatus_FromAPI(mapCtx *direct.MapContext, in *gkehubv1.RBACRoleBinding) *krm.GKEHubScopeRBACRoleBindingStatus {
 	if in == nil {
 		return nil
 	}
