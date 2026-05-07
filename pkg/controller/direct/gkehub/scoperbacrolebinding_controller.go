76798eb7cdffbf9a704e99e656049fcf2ba37d2a
codebot-robot <codebot-robot@google.com>
2026-05-07 04:49:04 +0000

    fix: format and update generator comments
    Applies gofmt and adds generated license headers to address the PR CI failures.

--- pkg/controller/direct/gkehub/scoperbacrolebinding_controller.go
+++ pkg/controller/direct/gkehub/scoperbacrolebinding_controller.go
@@ -1,3 +1,17 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package gkehub
 
 import (
@@ -185,7 +199,7 @@ func (a *gkeHubScopeRBACRoleBindingAdapter) Update(ctx context.Context, updateOp
 		updateMask = append(updateMask, "labels")
 		actualCopy.Labels = desiredAPI.Labels
 	}
-	
+
 	if len(updateMask) == 0 {
 		log.V(2).Info("no updates required for GKEHubScopeRBACRoleBinding", "name", a.id.String())
 		return nil
