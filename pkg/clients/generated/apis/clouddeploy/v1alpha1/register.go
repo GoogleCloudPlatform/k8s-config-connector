a0b46bd6f2a404cb3df262885398634a6c35590c
Gemma Hou <yuhou@google.com>
2026-05-07 01:24:35 +0000

    Run make-ready-pr

--- pkg/clients/generated/apis/clouddeploy/v1alpha1/register.go
+++ pkg/clients/generated/apis/clouddeploy/v1alpha1/register.go
@@ -53,6 +53,12 @@ var (
 	// AddToScheme is a global function that registers this API group & version to a scheme
 	AddToScheme = SchemeBuilder.AddToScheme
 
+	CloudDeployAutomationGVK = schema.GroupVersionKind{
+		Group:   SchemeGroupVersion.Group,
+		Version: SchemeGroupVersion.Version,
+		Kind:    reflect.TypeOf(CloudDeployAutomation{}).Name(),
+	}
+
 	CloudDeployCustomTargetTypeGVK = schema.GroupVersionKind{
 		Group:   SchemeGroupVersion.Group,
 		Version: SchemeGroupVersion.Version,
