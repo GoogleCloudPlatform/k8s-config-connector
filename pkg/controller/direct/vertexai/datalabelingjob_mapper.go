8e91bb078495f176362d881f60810d17bf43da9f
codebot-robot <codebot-robot@google.com>
2026-05-09 17:39:13 +0000

    fixup! feat: Add direct types for VertexAIDataLabelingJob

--- pkg/controller/direct/vertexai/datalabelingjob_mapper.go
+++ pkg/controller/direct/vertexai/datalabelingjob_mapper.go
@@ -1,9 +1,23 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package vertexai
 
 import (
 	pb "cloud.google.com/go/aiplatform/apiv1/aiplatformpb"
-	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/vertexai/v1alpha1"
 	refsv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/vertexai/v1alpha1"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
 	"google.golang.org/genproto/googleapis/rpc/status"
 	"google.golang.org/genproto/googleapis/type/money"
@@ -48,7 +62,7 @@ func VertexAIDataLabelingJobSpec_FromProto(mapCtx *direct.MapContext, in *pb.Dat
 	}
 	out := &krm.VertexAIDataLabelingJobSpec{}
 	out.DisplayName = direct.LazyPtr(in.GetDisplayName())
-	out.Datasets = VertexAIDataLabelingJobSpec_Datasets_FromProto(mapCtx, in.GetDatasets())
+	out.DatasetRefs = VertexAIDataLabelingJobSpec_DatasetRefs_FromProto(mapCtx, in.GetDatasets())
 	out.AnnotationLabels = in.GetAnnotationLabels()
 	out.LabelerCount = direct.LazyPtr(in.GetLabelerCount())
 	out.InstructionURI = direct.LazyPtr(in.GetInstructionUri())
@@ -67,7 +81,7 @@ func VertexAIDataLabelingJobSpec_ToProto(mapCtx *direct.MapContext, in *krm.Vert
 	}
 	out := &pb.DataLabelingJob{}
 	out.DisplayName = direct.ValueOf(in.DisplayName)
-	out.Datasets = VertexAIDataLabelingJobSpec_Datasets_ToProto(mapCtx, in.Datasets)
+	out.Datasets = VertexAIDataLabelingJobSpec_DatasetRefs_ToProto(mapCtx, in.DatasetRefs)
 	out.AnnotationLabels = in.AnnotationLabels
 	out.LabelerCount = direct.ValueOf(in.LabelerCount)
 	out.InstructionUri = direct.ValueOf(in.InstructionURI)
@@ -80,7 +94,7 @@ func VertexAIDataLabelingJobSpec_ToProto(mapCtx *direct.MapContext, in *krm.Vert
 	return out
 }
 
-func VertexAIDataLabelingJobSpec_Datasets_FromProto(mapCtx *direct.MapContext, in []string) []refsv1beta1.VertexAIDatasetRef {
+func VertexAIDataLabelingJobSpec_DatasetRefs_FromProto(mapCtx *direct.MapContext, in []string) []refsv1beta1.VertexAIDatasetRef {
 	if in == nil {
 		return nil
 	}
@@ -91,7 +105,7 @@ func VertexAIDataLabelingJobSpec_Datasets_FromProto(mapCtx *direct.MapContext, i
 	return out
 }
 
-func VertexAIDataLabelingJobSpec_Datasets_ToProto(mapCtx *direct.MapContext, in []refsv1beta1.VertexAIDatasetRef) []string {
+func VertexAIDataLabelingJobSpec_DatasetRefs_ToProto(mapCtx *direct.MapContext, in []refsv1beta1.VertexAIDatasetRef) []string {
 	if in == nil {
 		return nil
 	}
