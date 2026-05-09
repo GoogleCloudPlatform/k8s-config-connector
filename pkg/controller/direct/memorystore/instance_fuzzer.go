d49905f9ad36b722e08708f13755ade942f1fe36
codebot-robot <codebot-robot@google.com>
2026-05-09 01:37:45 +0000

    Fix #7975: Add missing field psc_attachment_details to MemorystoreInstance
    Fixes #7975
    
    Added the `psc_attachment_details` field to the `MemorystoreInstance` resource's `observedState`. Updated the `instance_mapper.go` to use the generated `PscAttachmentDetailObservedState_FromProto` and `PscAttachmentDetailObservedState_ToProto` functions. Registered the field in the fuzzer's `StatusFields`. Regenerated the mock tests golden output.
    
    Also updated the `add-missing-field` skill documentation based on the findings.

--- pkg/controller/direct/memorystore/instance_fuzzer.go
+++ pkg/controller/direct/memorystore/instance_fuzzer.go
@@ -48,7 +48,7 @@ func memorystoreInstanceFuzzer() fuzztesting.KRMFuzzer {
 	f.UnimplementedFields.Insert(".gcs_source")
 
 	f.UnimplementedFields.Insert(".backup_collection")
-	f.UnimplementedFields.Insert(".psc_attachment_details")
+
 	f.UnimplementedFields.Insert(".maintenance_policy")
 	f.UnimplementedFields.Insert(".managed_backup_source")
 	f.UnimplementedFields.Insert(".async_instance_endpoints_deletion_enabled")
@@ -83,6 +83,7 @@ func memorystoreInstanceFuzzer() fuzztesting.KRMFuzzer {
 	f.StatusFields.Insert(".node_config") // NodeConfig is an output-only field in the proto, but KRM needs it for status.
 	f.StatusFields.Insert(".endpoints")
 	f.StatusFields.Insert(".effective_maintenance_version")
+	f.StatusField(".psc_attachment_details")
 
 	f.Unimplemented_NotYetTriaged(".available_maintenance_versions")
 	f.Unimplemented_NotYetTriaged(".maintenance_version")
