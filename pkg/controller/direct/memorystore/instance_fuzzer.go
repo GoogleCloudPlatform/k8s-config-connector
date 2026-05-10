0d3e7e7778751962ee8e23b312872e570d566e9a
codebot-robot <codebot-robot@google.com>
2026-05-10 15:19:14 +0000

    feat: add maintenance_version to MemorystoreInstance
    Fixes #8011.
    
    Added `maintenanceVersion` field to `MemorystoreInstanceSpec` in `apis/memorystore/v1beta1/instance_types.go`.
    Updated `pkg/controller/direct/memorystore/instance_fuzzer.go` and `instance_mapper.go` to support the new field.
    Regenerated API types, CRDs and mappers.
    Updated mock test `fullmemorystoreinstance` with the new field and regenerated golden files.

--- pkg/controller/direct/memorystore/instance_fuzzer.go
+++ pkg/controller/direct/memorystore/instance_fuzzer.go
@@ -74,7 +74,9 @@ func memorystoreInstanceFuzzer() fuzztesting.KRMFuzzer {
 	f.SpecFields.Insert(".deletion_protection_enabled")
 	f.SpecFields.Insert(".endpoints")
 	f.SpecFields.Insert(".mode")
+	f.SpecFields.Insert(".maintenance_version")
 
+	// Status Fields
 	f.StatusFields.Insert(".create_time")
 	f.StatusFields.Insert(".update_time")
 	f.StatusFields.Insert(".state")
@@ -86,7 +88,6 @@ func memorystoreInstanceFuzzer() fuzztesting.KRMFuzzer {
 	f.StatusField(".psc_attachment_details")
 
 	f.Unimplemented_NotYetTriaged(".available_maintenance_versions")
-	f.Unimplemented_NotYetTriaged(".maintenance_version")
 	f.Unimplemented_NotYetTriaged(".allow_fewer_zones_deployment")
 	f.Unimplemented_NotYetTriaged(".encryption_info")
 	f.Unimplemented_NotYetTriaged(".simulate_maintenance_event")
