3e9dc86bc4f91b312bbfa7c18603600967525126
codebot-robot <codebot-robot@google.com>
2026-05-12 01:17:21 +0000

    Regenerate client libraries

---
+++ fix_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+cat << 'EOF2' > tail_replacement.txt
+        f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].gke_network_policy_skipped")
+        f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].gke_network_policy_skipped")
+
+        f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].gke_network_policy")
+        f.Unimplemented_NotYetTriaged(".return_reachability_details.traces[].steps[].gke_network_policy")
+
+        f.Unimplemented_NotYetTriaged(".reachability_details.traces[].steps[].gke_network_policy_skipped.reason")
+        return f
+}
+EOF2
+
+head -n 172 pkg/controller/direct/networkmanagement/connectivitytest_fuzzer.go > temp.go
+cat tail_replacement.txt >> temp.go
+mv temp.go pkg/controller/direct/networkmanagement/connectivitytest_fuzzer.go
