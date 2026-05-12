b5edfa507af26ec171aeb12b24fd5018cbee829d
codebot-robot <codebot-robot@google.com>
2026-05-12 21:00:17 +0000

    Implement direct IAM for SecretManagerSecretVersion

--- pkg/controller/direct/registry/registry.go
+++ pkg/controller/direct/registry/registry.go
@@ -127,6 +127,8 @@ func IsIAMDirect(groupKind schema.GroupKind) bool {
 	switch groupKind {
 	case schema.GroupKind{Group: "privateca.cnrm.cloud.google.com", Kind: "PrivateCACAPool"}:
 		return true
+	case schema.GroupKind{Group: "secretmanager.cnrm.cloud.google.com", Kind: "SecretManagerSecretVersion"}:
+		return true
 	}
 	return false
 }
