9dc14a36c248bba0228b28d2b47737c8dacc3b53
codebot-robot <codebot-robot@google.com>
2026-05-12 00:41:43 +0000

    fixup! fixup! feat: Add direct types for VertexAIDataLabelingJob

--- pkg/clients/generated/apis/vertexai/v1alpha1/register.go
+++ pkg/clients/generated/apis/vertexai/v1alpha1/register.go
@@ -53,6 +53,12 @@ var (
 	// AddToScheme is a global function that registers this API group & version to a scheme
 	AddToScheme = SchemeBuilder.AddToScheme
 
+	VertexAIDataLabelingJobGVK = schema.GroupVersionKind{
+		Group:   SchemeGroupVersion.Group,
+		Version: SchemeGroupVersion.Version,
+		Kind:    reflect.TypeOf(VertexAIDataLabelingJob{}).Name(),
+	}
+
 	VertexAIFeaturestoreGVK = schema.GroupVersionKind{
 		Group:   SchemeGroupVersion.Group,
 		Version: SchemeGroupVersion.Version,
