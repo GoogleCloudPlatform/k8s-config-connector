ce24e87e0faaa60c56b098bd99ca870bc7c144bf
codebot-robot <codebot-robot@google.com>
2026-05-13 00:43:17 +0000

    mockgcp: fix import cycles and stale generated files for analytics and dataplex
    - Updated mockgcp/fixup-third-party.sh to rewrite go_package for analytics and dataplex.
    - Updated mockgcp services to import generated code from the correct mockgcp path.
    - Removed stale generated files in mockgcp/generated/google/ directory.
    - Regenerated protos to ensure they are up to date with the fixup script.

--- mockgcp/fixup-third-party.sh
+++ mockgcp/fixup-third-party.sh
@@ -96,7 +96,10 @@ find . -type f -print0 | xargs -0 sed -i \
   -e 's@option go_package = "cloud.google.com/go/gkehub/metering/apiv1beta/meteringpb;meteringpb"@option go_package = "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/gkehub/v1beta/metering;meteringpb"@g' \
   -e 's@option go_package = "cloud.google.com/go/gkehub/multiclusteringress/apiv1beta/multiclusteringresspb;multiclusteringresspb"@option go_package = "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/gkehub/v1beta/multiclusteringress;multiclusteringresspb"@g' \
   -e 's@option go_package = "cloud.google.com/go/gkehub/servicemesh/apiv1beta/servicemeshpb;servicemeshpb"@option go_package = "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/gkehub/servicemesh/v1beta;servicemeshpb"@g' \
-  -e 's@option go_package = "cloud.google.com/go/gkehub/policycontroller/apiv1beta/policycontrollerpb;policycontrollerpb"@option go_package = "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/gkehub/policycontroller/v1beta;policycontrollerpb"@g'
+  -e 's@option go_package = "cloud.google.com/go/gkehub/policycontroller/apiv1beta/policycontrollerpb;policycontrollerpb"@option go_package = "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/gkehub/policycontroller/v1beta;policycontrollerpb"@g' \
+  -e 's@option go_package = "cloud.google.com/go/analytics/admin/apiv1alpha/adminpb;adminpb"@option go_package = "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/analytics/admin/v1alpha;adminpb"@g' \
+  -e 's@option go_package = "cloud.google.com/go/analytics/admin/apiv1beta/adminpb;adminpb"@option go_package = "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/analytics/admin/v1beta;adminpb"@g' \
+  -e 's@option go_package = "cloud.google.com/go/dataplex/apiv1/dataplexpb;dataplexpb"@option go_package = "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/dataplex/v1;dataplexpb"@g'
 
 
 cd ${GOOGLEAPIS}/mockgrafeas
