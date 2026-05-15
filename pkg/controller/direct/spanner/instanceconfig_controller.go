9fa641767646bbe26b21c5ef79ac8740fc4404ab
codebot-robot <codebot-robot@google.com>
2026-05-15 20:56:47 +0000

    direct/spanner: implement IAMAdapter for SpannerInstanceConfig

--- pkg/controller/direct/spanner/instanceconfig_controller.go
+++ pkg/controller/direct/spanner/instanceconfig_controller.go
@@ -25,6 +25,7 @@ import (
 	"fmt"
 	"reflect"
 
+	iampb "cloud.google.com/go/iam/apiv1/iampb"
 	refv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
 
 	instance "cloud.google.com/go/spanner/admin/instance/apiv1"
@@ -227,6 +228,39 @@ func (a *instanceConfigAdapter) Delete(ctx context.Context, deleteOp *directbase
 	log.V(2).Info("successfully deleted spanner instance config", "name", a.id)
 
 	return true, nil
+}
+
+func (a *instanceConfigAdapter) GetIAMPolicy(ctx context.Context) (*iampb.Policy, error) {
+	if a.id == nil {
+		return nil, fmt.Errorf("cannot get iam policy for missing resource")
+	}
+
+	req := &iampb.GetIamPolicyRequest{
+		Resource: a.id.String(),
+	}
+	policy, err := a.gcpClient.GetIamPolicy(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("getting iam policy for %q: %w", a.id.String(), err)
+	}
+
+	return policy, nil
+}
+
+func (a *instanceConfigAdapter) SetIAMPolicy(ctx context.Context, policy *iampb.Policy) (*iampb.Policy, error) {
+	if a.id == nil {
+		return nil, fmt.Errorf("cannot set iam policy for missing resource")
+	}
+
+	req := &iampb.SetIamPolicyRequest{
+		Resource: a.id.String(),
+		Policy:   policy,
+	}
+	newPolicy, err := a.gcpClient.SetIamPolicy(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("setting iam policy for %q: %w", a.id.String(), err)
+	}
+
+	return newPolicy, nil
 }
 
 // Export maps the GCP object to a Config Connector resource `spec`.
