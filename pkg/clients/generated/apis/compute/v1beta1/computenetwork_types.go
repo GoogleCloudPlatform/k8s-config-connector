c5b785e6a29c14a9dd9e9a7c75f2228943e43898
codebot-robot <codebot-robot@google.com>
2026-05-15 03:30:25 +0000

    Regenerate Go clients for ComputeNetwork to sync with updated types

--- pkg/clients/generated/apis/compute/v1beta1/computenetwork_types.go
+++ pkg/clients/generated/apis/compute/v1beta1/computenetwork_types.go
@@ -48,60 +48,58 @@ type ComputeNetworkSpec struct {
 	// +optional
 	AutoCreateSubnetworks *bool `json:"autoCreateSubnetworks,omitempty"`
 
-	/* If set to 'true', default routes ('0.0.0.0/0') will be deleted
-	immediately after network creation. Defaults to 'false'. */
+	/* If set to 'true', default routes ('0.0.0.0/0') will be deleted immediately after network creation. Defaults to 'false'. */
 	// +optional
 	DeleteDefaultRoutesOnCreate *bool `json:"deleteDefaultRoutesOnCreate,omitempty"`
 
-	/* Immutable. An optional description of this resource. The resource must be
-	recreated to modify this field. */
+	/* Immutable. An optional description of this resource. The resource must be recreated to modify this field. */
 	// +optional
 	Description *string `json:"description,omitempty"`
 
-	/* Enable ULA internal ipv6 on this network. Enabling this feature will assign
-	a /48 from google defined ULA prefix fd20::/20. */
+	/* Enable ULA internal ipv6 on this network. Enabling this feature will assign a /48 from google defined ULA prefix fd20::/20. */
 	// +optional
-	EnableUlaInternalIpv6 *bool `json:"enableUlaInternalIpv6,omitempty"`
+	EnableUlaInternalIPV6 *bool `json:"enableUlaInternalIPV6,omitempty"`
 
-	/* Immutable. When enabling ula internal ipv6, caller optionally can specify the /48 range
-	they want from the google defined ULA prefix fd20::/20. The input must be a
-	valid /48 ULA IPv6 address and must be within the fd20::/20. Operation will
-	fail if the speficied /48 is already in used by another resource.
-	If the field is not speficied, then a /48 range will be randomly allocated from fd20::/20 and returned via this field. */
+	/* Immutable. When enabling ula internal ipv6, caller optionally can specify the /48 range they want from the google defined ULA prefix fd20::/20. The input must be a valid /48 ULA IPv6 address and must be within the fd20::/20. Operation will fail if the speficied /48 is already in used by another resource. If the field is not speficied, then a /48 range will be randomly allocated from fd20::/20 and returned via this field. */
 	// +optional
-	InternalIpv6Range *string `json:"internalIpv6Range,omitempty"`
+	InternalIPV6Range *string `json:"internalIPV6Range,omitempty"`
 
-	/* Immutable. Maximum Transmission Unit in bytes. The default value is 1460 bytes.
-	The minimum value for this field is 1300 and the maximum value is 8896 bytes (jumbo frames).
-	Note that packets larger than 1500 bytes (standard Ethernet) can be subject to TCP-MSS clamping or dropped
-	with an ICMP 'Fragmentation-Needed' message if the packets are routed to the Internet or other VPCs
-	with varying MTUs. */
+	/* Immutable. Maximum Transmission Unit in bytes. The default value is 1460 bytes. The minimum value for this field is 1300 and the maximum value is 8896 bytes (jumbo frames). Note that packets larger than 1500 bytes (standard Ethernet) can be subject to TCP-MSS clamping or dropped with an ICMP 'Fragmentation-Needed' message if the packets are routed to the Internet or other VPCs with varying MTUs. */
 	// +optional
-	Mtu *int64 `json:"mtu,omitempty"`
+	Mtu *int32 `json:"mtu,omitempty"`
 
 	/* Set the order that Firewall Rules and Firewall Policies are evaluated. Default value: "AFTER_CLASSIC_FIREWALL" Possible values: ["BEFORE_CLASSIC_FIREWALL", "AFTER_CLASSIC_FIREWALL"]. */
 	// +optional
 	NetworkFirewallPolicyEnforcementOrder *string `json:"networkFirewallPolicyEnforcementOrder,omitempty"`
 
-	/* Immutable. Optional. The name of the resource. Used for creation and acquisition. When unset, the value of `metadata.name` is used as the default. */
+	/* The ComputeNetwork name. If not given, the metadata.name will be used. */
 	// +optional
 	ResourceID *string `json:"resourceID,omitempty"`
 
-	/* The network-wide routing mode to use. If set to 'REGIONAL', this
-	network's cloud routers will only advertise routes with subnetworks
-	of this network in the same region as the router. If set to 'GLOBAL',
-	this network's cloud routers will advertise routes with all
-	subnetworks of this network, across regions. Possible values: ["REGIONAL", "GLOBAL"]. */
+	/* The network-wide routing mode to use. If set to 'REGIONAL', this network's cloud routers will only advertise routes with subnetworks of this network in the same region as the router. If set to 'GLOBAL', this network's cloud routers will advertise routes with all subnetworks of this network, across regions. Possible values: ["REGIONAL", "GLOBAL"]. */
 	// +optional
 	RoutingMode *string `json:"routingMode,omitempty"`
+}
+
+type NetworkObservedStateStatus struct {
+	/* [Output Only] The gateway address for default routing out of the network, selected by Google Cloud. */
+	// +optional
+	GatewayIPv4 *string `json:"gatewayIPv4,omitempty"`
+
+	/* [Output Only] Server-defined URL for the resource. */
+	// +optional
+	SelfLink *string `json:"selfLink,omitempty"`
 }
 
 type ComputeNetworkStatus struct {
 	/* Conditions represent the latest available observations of the
 	   ComputeNetwork's current state. */
 	Conditions []v1alpha1.Condition `json:"conditions,omitempty"`
-	/* The gateway address for default routing out of the network. This value
-	is selected by GCP. */
+	/* A unique specifier for the ComputeNetwork resource in GCP. */
+	// +optional
+	ExternalRef *string `json:"externalRef,omitempty"`
+
+	/* The gateway address for default routing out of the network. This value is selected by GCP. */
 	// +optional
 	GatewayIpv4 *string `json:"gatewayIpv4,omitempty"`
 
@@ -109,6 +107,11 @@ type ComputeNetworkStatus struct {
 	// +optional
 	ObservedGeneration *int64 `json:"observedGeneration,omitempty"`
 
+	/* ObservedState is the state of the resource as most recently observed in GCP. */
+	// +optional
+	ObservedState *NetworkObservedStateStatus `json:"observedState,omitempty"`
+
+	/* The self link of the ComputeNetwork. */
 	// +optional
 	SelfLink *string `json:"selfLink,omitempty"`
 }
