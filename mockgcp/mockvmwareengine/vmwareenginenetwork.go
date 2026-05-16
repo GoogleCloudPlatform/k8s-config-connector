5f5ab676b84f0b1407e8f53ac612401723890fd1
codebot-robot <codebot-robot@google.com>
2026-05-16 20:01:19 +0000

    Move mockgcp vmwareengine away from grpc-gateway Fixes #8208

--- mockgcp/mockvmwareengine/vmwareenginenetwork.go
+++ mockgcp/mockvmwareengine/vmwareenginenetwork.go
@@ -30,9 +30,9 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
+	pb "cloud.google.com/go/vmwareengine/apiv1/vmwareenginepb"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/fields"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/projects"
-	pb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/vmwareengine/v1"
 	"github.com/google/uuid"
 	longrunningpb "google.golang.org/genproto/googleapis/longrunning"
 )
