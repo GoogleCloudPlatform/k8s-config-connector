f8fca303e5730c97d786485b6a6b0a32ae4cf0bd
codebot-robot <codebot-robot@google.com>
2026-05-16 13:09:44 +0000

    Move ApigeeEndpointAttachment fuzzer to normal framework
    This change moves FuzzApigeeEndpointAttachmentSpec and FuzzApigeeEndpointAttachmentObservedState from special-case handling in fuzz-roundtrippers to the normal fuzzing framework by registering them with fuzztesting.RegisterKRMFuzzer_NoProto.
    
    Issue #8188
    
    This PR was generated by Overseer (powered by the gemini-3-flash-preview model).

---
+++ pkg/controller/direct/apigee/endpointattachment_fuzzer.go
@@ -0,0 +1,46 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package apigee
+
+import (
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/fuzztesting"
+	api "google.golang.org/api/apigee/v1"
+)
+
+func init() {
+	fuzztesting.RegisterKRMFuzzer_NoProto(ApigeeEndpointAttachmentFuzzer())
+}
+
+func ApigeeEndpointAttachmentFuzzer() fuzztesting.KRMFuzzer_NoProto {
+	f := fuzztesting.NewKRMTypedFuzzer_NoProto(&api.GoogleCloudApigeeV1EndpointAttachment{},
+		ApigeeEndpointAttachmentSpec_FromAPI, ApigeeEndpointAttachmentSpec_ToAPI,
+		ApigeeEndpointAttachmentObservedState_FromAPI, ApigeeEndpointAttachmentObservedState_ToAPI,
+	)
+
+	f.SpecField(".Location")
+	f.SpecField(".ServiceAttachment")
+
+	f.StatusField(".ConnectionState")
+	f.StatusField(".Host")
+	f.StatusField(".State")
+
+	// Fields that don't roundtrip
+	f.Unimplemented_NotYetTriaged(".Name")
+	f.Unimplemented_NotYetTriaged(".ForceSendFields")
+	f.Unimplemented_NotYetTriaged(".NullFields")
+	f.Unimplemented_NotYetTriaged(".ServerResponse")
+
+	return f
+}
