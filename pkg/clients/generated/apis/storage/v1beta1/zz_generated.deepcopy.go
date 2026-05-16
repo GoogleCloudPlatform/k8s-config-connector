339d7f95a5e11ce3da812b03ef0f1b0a7975b01d
codebot-robot <codebot-robot@google.com>
2026-05-16 01:31:44 +0000

    Fix type mismatches and out-of-sync generated files for StorageBucket
    - Update apis/storage/v1beta1/bucket_types.go to use int64 for fields to match proto and avoid generator diffs.
    - Update storagebucket_mapper.go to fix compilation errors with new types.
    - Regenerate CRD and deepcopy functions.
    - Update legacy types to match int64 changes, fixing validation failures.

--- pkg/clients/generated/apis/storage/v1beta1/zz_generated.deepcopy.go
+++ pkg/clients/generated/apis/storage/v1beta1/zz_generated.deepcopy.go
@@ -107,7 +107,7 @@ func (in *BucketCondition) DeepCopyInto(out *BucketCondition) {
 	*out = *in
 	if in.Age != nil {
 		in, out := &in.Age, &out.Age
-		*out = new(int32)
+		*out = new(int64)
 		**out = **in
 	}
 	if in.CreatedBefore != nil {
@@ -122,12 +122,12 @@ func (in *BucketCondition) DeepCopyInto(out *BucketCondition) {
 	}
 	if in.DaysSinceCustomTime != nil {
 		in, out := &in.DaysSinceCustomTime, &out.DaysSinceCustomTime
-		*out = new(int32)
+		*out = new(int64)
 		**out = **in
 	}
 	if in.DaysSinceNoncurrentTime != nil {
 		in, out := &in.DaysSinceNoncurrentTime, &out.DaysSinceNoncurrentTime
-		*out = new(int32)
+		*out = new(int64)
 		**out = **in
 	}
 	if in.MatchesPrefix != nil {
@@ -152,7 +152,7 @@ func (in *BucketCondition) DeepCopyInto(out *BucketCondition) {
 	}
 	if in.NumNewerVersions != nil {
 		in, out := &in.NumNewerVersions, &out.NumNewerVersions
-		*out = new(int32)
+		*out = new(int64)
 		**out = **in
 	}
 	if in.WithState != nil {
@@ -178,7 +178,7 @@ func (in *BucketCors) DeepCopyInto(out *BucketCors) {
 	*out = *in
 	if in.MaxAgeSeconds != nil {
 		in, out := &in.MaxAgeSeconds, &out.MaxAgeSeconds
-		*out = new(int32)
+		*out = new(int64)
 		**out = **in
 	}
 	if in.Method != nil {
@@ -333,7 +333,7 @@ func (in *BucketSoftDeletePolicy) DeepCopyInto(out *BucketSoftDeletePolicy) {
 	*out = *in
 	if in.RetentionDurationSeconds != nil {
 		in, out := &in.RetentionDurationSeconds, &out.RetentionDurationSeconds
-		*out = new(int32)
+		*out = new(int64)
 		**out = **in
 	}
 	return
@@ -359,7 +359,7 @@ func (in *BucketSoftDeletePolicyStatus) DeepCopyInto(out *BucketSoftDeletePolicy
 	}
 	if in.RetentionDurationSeconds != nil {
 		in, out := &in.RetentionDurationSeconds, &out.RetentionDurationSeconds
-		*out = new(int32)
+		*out = new(int64)
 		**out = **in
 	}
 	return
