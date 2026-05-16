7ecbd1cd479a9880234c657bc21e1bc2e95efcb5
codebot-robot <codebot-robot@google.com>
2026-05-16 19:49:41 +0000

    chore(mockgcp): migrate datastream to httptogrpc
    Fixes #8207

--- fix_makefile.sh
+++
@@ -1,3 +0,0 @@
-sed -i '/<<<<<<< HEAD/d' mockgcp/Makefile
-sed -i '/=======/d' mockgcp/Makefile
-sed -i '/>>>>>>> 23d510fb76 (Cleanup mockgcp netapp generated protobuf code)/d' mockgcp/Makefile
--- fix_mod.sh
+++
@@ -1,3 +0,0 @@
-sed -i '/<<<<<<< HEAD/d' go.mod
-sed -i '/=======/d' go.mod
-sed -i '/>>>>>>> 2d045b9396 (feat: implement BinaryAuthorizationPlatformPolicy using direct approach)/d' go.mod
