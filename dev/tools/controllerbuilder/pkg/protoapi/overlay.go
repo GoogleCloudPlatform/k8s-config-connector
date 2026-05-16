6549775e874e01e8cc1e229a52c33a443de02461
codebot-robot <codebot-robot@google.com>
2026-05-16 11:32:18 +0000

    Use a recursive descent parser for proto overlays
    Addresses review feedback by replacing the regex-based parsing
    with a recursive descent parser using text/scanner for a simple lexer.
    This makes the overlay parser more robust and maintainable.

--- dev/tools/controllerbuilder/pkg/protoapi/overlay.go
+++ dev/tools/controllerbuilder/pkg/protoapi/overlay.go
@@ -15,23 +15,15 @@
 package protoapi
 
 import (
-	"bufio"
 	"fmt"
 	"io"
 	"os"
-	"regexp"
+	"strings"
+	"text/scanner"
 
 	"google.golang.org/genproto/googleapis/api/annotations"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/descriptorpb"
-)
-
-var (
-	pkgRegex      = regexp.MustCompile(`^\s*package\s+([^;]+);`)
-	msgRegex      = regexp.MustCompile(`^\s*message\s+([^{\s]+)\s*\{`)
-	endMsgRegex   = regexp.MustCompile(`^\s*\}`)
-	fieldRegex    = regexp.MustCompile(`^\s*(?:repeated\s+)?(?:optional\s+)?(?:required\s+)?[^\s]+\s+([^\s=]+)\s*=\s*\d+\s*\[(.*)\];`)
-	behaviorRegex = regexp.MustCompile(`\(google\.api\.field_behavior\)\s*=\s*([A-Z_]+)`)
 )
 
 func ApplyOverlay(fds *descriptorpb.FileDescriptorSet, overlayPath string) error {
@@ -45,85 +37,258 @@ func ApplyOverlay(fds *descriptorpb.FileDescriptorSet, overlayPath string) error
 }
 
 func applyOverlayFromReader(fds *descriptorpb.FileDescriptorSet, r io.Reader) error {
-	scanner := bufio.NewScanner(r)
+	var s scanner.Scanner
+	s.Init(r)
+	s.Mode = scanner.ScanIdents | scanner.ScanInts | scanner.ScanStrings | scanner.SkipComments
+
+	l := &lexer{scanner: s}
+	l.next()
 
 	var currentPackage string
-	var messageStack []string
-
 	fieldBehaviors := make(map[string][]annotations.FieldBehavior)
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		
-		if pkgMatch := pkgRegex.FindStringSubmatch(line); pkgMatch != nil {
-			currentPackage = pkgMatch[1]
-			continue
-		}
-
-		if msgMatch := msgRegex.FindStringSubmatch(line); msgMatch != nil {
-			messageStack = append(messageStack, msgMatch[1])
-			continue
-		}
-
-		if endMsgRegex.MatchString(line) {
-			if len(messageStack) > 0 {
-				messageStack = messageStack[:len(messageStack)-1]
-			}
-			continue
-		}
-
-		if fieldMatch := fieldRegex.FindStringSubmatch(line); fieldMatch != nil {
-			fieldName := fieldMatch[1]
-			opts := fieldMatch[2]
-
-			if behaviorMatch := behaviorRegex.FindStringSubmatch(opts); behaviorMatch != nil {
-				behaviorStr := behaviorMatch[1]
-				var behavior annotations.FieldBehavior
-				switch behaviorStr {
-				case "OUTPUT_ONLY":
-					behavior = annotations.FieldBehavior_OUTPUT_ONLY
-				case "REQUIRED":
-					behavior = annotations.FieldBehavior_REQUIRED
-				case "OPTIONAL":
-					behavior = annotations.FieldBehavior_OPTIONAL
-				case "IMMUTABLE":
-					behavior = annotations.FieldBehavior_IMMUTABLE
-				case "INPUT_ONLY":
-					behavior = annotations.FieldBehavior_INPUT_ONLY
-				default:
-					return fmt.Errorf("unknown field behavior: %q", behaviorStr)
-				}
-				
-				fullName := currentPackage
-				for _, msg := range messageStack {
-					if fullName != "" {
-						fullName += "."
-					}
-					fullName += msg
-				}
-				if fullName != "" {
-					fullName += "."
-				}
-				fullName += fieldName
-
-				fieldBehaviors[fullName] = append(fieldBehaviors[fullName], behavior)
-			}
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		return err
+	if err := parseFile(l, &currentPackage, fieldBehaviors); err != nil {
+		return fmt.Errorf("parsing overlay: %w", err)
 	}
 
 	if len(fieldBehaviors) == 0 {
 		return nil
 	}
 
-	// Apply modifications to fds
 	for _, file := range fds.File {
 		applyToMessages(file.GetPackage(), file.MessageType, fieldBehaviors)
 	}
 
 	return nil
+}
+
+type lexer struct {
+	scanner scanner.Scanner
+	tok     rune
+}
+
+func (l *lexer) next() {
+	l.tok = l.scanner.Scan()
+}
+
+func (l *lexer) text() string {
+	return l.scanner.TokenText()
+}
+
+func parseFile(l *lexer, currentPackage *string, behaviors map[string][]annotations.FieldBehavior) error {
+	for l.tok != scanner.EOF {
+		if l.tok == scanner.Ident && l.text() == "package" {
+			l.next()
+			pkg, err := parseIdentPath(l)
+			if err != nil {
+				return err
+			}
+			*currentPackage = pkg
+			if l.tok != ';' {
+				return fmt.Errorf("expected ';' at %s", l.scanner.Position)
+			}
+			l.next()
+		} else if l.tok == scanner.Ident && l.text() == "message" {
+			l.next()
+			if l.tok != scanner.Ident {
+				return fmt.Errorf("expected message name at %s", l.scanner.Position)
+			}
+			msgName := l.text()
+			l.next()
+
+			if err := parseMessage(l, []string{msgName}, behaviors, currentPackage); err != nil {
+				return err
+			}
+		} else {
+			// skip other top-level elements (e.g. syntax, import, option)
+			if l.tok == scanner.Ident && (l.text() == "syntax" || l.text() == "import" || l.text() == "option") {
+				for l.tok != scanner.EOF && l.tok != ';' {
+					l.next()
+				}
+				if l.tok == ';' {
+					l.next()
+				}
+			} else {
+				l.next()
+			}
+		}
+	}
+	return nil
+}
+
+func parseIdentPath(l *lexer) (string, error) {
+	var sb strings.Builder
+	for {
+		if l.tok != scanner.Ident {
+			return "", fmt.Errorf("expected identifier at %s", l.scanner.Position)
+		}
+		sb.WriteString(l.text())
+		l.next()
+		if l.tok == '.' {
+			sb.WriteRune('.')
+			l.next()
+		} else {
+			break
+		}
+	}
+	return sb.String(), nil
+}
+
+func parseMessage(l *lexer, stack []string, behaviors map[string][]annotations.FieldBehavior, currentPackage *string) error {
+	if l.tok != '{' {
+		return fmt.Errorf("expected '{' at %s", l.scanner.Position)
+	}
+	l.next()
+
+	for l.tok != scanner.EOF && l.tok != '}' {
+		if l.tok == scanner.Ident && l.text() == "message" {
+			l.next()
+			if l.tok != scanner.Ident {
+				return fmt.Errorf("expected message name at %s", l.scanner.Position)
+			}
+			msgName := l.text()
+			l.next()
+
+			newStack := append([]string{}, stack...)
+			newStack = append(newStack, msgName)
+			if err := parseMessage(l, newStack, behaviors, currentPackage); err != nil {
+				return err
+			}
+		} else if l.tok == scanner.Ident && l.text() == "option" {
+			for l.tok != scanner.EOF && l.tok != ';' {
+				l.next()
+			}
+			if l.tok == ';' {
+				l.next()
+			}
+		} else if l.tok == scanner.Ident && (l.text() == "oneof" || l.text() == "enum") {
+			l.next()
+			if l.tok == scanner.Ident {
+				l.next()
+			}
+			if l.tok == '{' {
+				skipBlock(l)
+			}
+		} else if l.tok == scanner.Ident {
+			// A field declaration
+			var fieldName string
+			for l.tok != scanner.EOF && l.tok != '=' && l.tok != ';' && l.tok != '{' {
+				if l.tok == scanner.Ident {
+					fieldName = l.text()
+				}
+				l.next()
+			}
+
+			if l.tok == '{' {
+				// E.g. nested extension or unexpected block
+				skipBlock(l)
+			} else if l.tok == '=' {
+				l.next() // consume '='
+
+				for l.tok != scanner.EOF && l.tok != ';' && l.tok != '[' {
+					l.next()
+				}
+
+				if l.tok == '[' {
+					l.next() // consume '['
+					for l.tok != scanner.EOF && l.tok != ']' {
+						if l.tok == '(' {
+							l.next()
+							optName, _ := parseIdentPath(l)
+							if l.tok == ')' {
+								l.next()
+							}
+							if l.tok == '=' {
+								l.next()
+								if l.tok == scanner.Ident {
+									optVal := l.text()
+									l.next()
+									if optName == "google.api.field_behavior" {
+										behavior, err := parseFieldBehavior(optVal)
+										if err != nil {
+											return err
+										}
+
+										fullName := *currentPackage
+										for _, s := range stack {
+											if fullName != "" {
+												fullName += "."
+											}
+											fullName += s
+										}
+										if fullName != "" {
+											fullName += "."
+										}
+										fullName += fieldName
+
+										behaviors[fullName] = append(behaviors[fullName], behavior)
+									}
+								}
+							}
+						} else {
+							l.next()
+						}
+					}
+					if l.tok == ']' {
+						l.next()
+					}
+				}
+
+				if l.tok == ';' {
+					l.next()
+				} else {
+					return fmt.Errorf("expected ';' after field at %s", l.scanner.Position)
+				}
+			} else if l.tok == ';' {
+				l.next()
+			}
+		} else {
+			l.next() // ignore unexpected tokens inside message
+		}
+	}
+
+	if l.tok == '}' {
+		l.next()
+	} else {
+		return fmt.Errorf("expected '}' at %s", l.scanner.Position)
+	}
+
+	return nil
+}
+
+func skipBlock(l *lexer) {
+	if l.tok != '{' {
+		return
+	}
+	l.next()
+	depth := 1
+	for l.tok != scanner.EOF && depth > 0 {
+		if l.tok == '{' {
+			depth++
+		} else if l.tok == '}' {
+			depth--
+		}
+		if depth > 0 {
+			l.next()
+		}
+	}
+}
+
+func parseFieldBehavior(val string) (annotations.FieldBehavior, error) {
+	switch val {
+	case "OUTPUT_ONLY":
+		return annotations.FieldBehavior_OUTPUT_ONLY, nil
+	case "REQUIRED":
+		return annotations.FieldBehavior_REQUIRED, nil
+	case "OPTIONAL":
+		return annotations.FieldBehavior_OPTIONAL, nil
+	case "IMMUTABLE":
+		return annotations.FieldBehavior_IMMUTABLE, nil
+	case "INPUT_ONLY":
+		return annotations.FieldBehavior_INPUT_ONLY, nil
+	default:
+		return 0, fmt.Errorf("unknown field behavior: %q", val)
+	}
 }
 
 func applyToMessages(pkg string, msgs []*descriptorpb.DescriptorProto, fieldBehaviors map[string][]annotations.FieldBehavior) {
@@ -133,22 +298,17 @@ func applyToMessages(pkg string, msgs []*descriptorpb.DescriptorProto, fieldBeha
 		if pkg != "" {
 			fullMsgName = pkg + "." + msgName
 		}
-		
+
 		for _, field := range msg.Field {
 			fullName := fullMsgName + "." + field.GetName()
 			if behaviors, ok := fieldBehaviors[fullName]; ok {
 				if field.Options == nil {
 					field.Options = &descriptorpb.FieldOptions{}
 				}
-				// Append behaviors using proto extension.
-                // We overwrite for now, or append if exists?
-                // Usually it's better to append if already exists, or just set it.
-                // proto.GetExtension could be checked, but for simplicity we can just set.
-                // wait, if we just set it, it overrides any existing behaviors which is fine.
 				proto.SetExtension(field.Options, annotations.E_FieldBehavior, behaviors)
 			}
 		}
-		
+
 		if len(msg.NestedType) > 0 {
 			applyToMessages(fullMsgName, msg.NestedType, fieldBehaviors)
 		}
