330a1ff52aa802b07c37f6f550f53d00423ab682
codebot-robot <codebot-robot@google.com>
2026-05-16 13:28:18 +0000

    Fix mock drift for notebooks using go client library
    This commit addresses the review feedback by moving the notebook service
    away from grpc-gateway. We now use httptogrpc with the official client library
    protos.
    
    Changes:
    - Removed notebooks from Makefile proto generation and deleted the generated
      grpc-gateway files.
    - Updated mocknotebooks to use httptogrpc.
    - Implemented v1beta1 NotebookService and registered both v1 and v1beta1
      mux routes.
    - Modified httptogrpc/service.go to ignore bool and enum method options,
      resolving a panic when mapping the Notebooks API.
    - Re-ran tests and updated golden _http.log files.
    
    Fixes #7503

--- mockgcp/mocknotebooks/notebooksenvironment_v1beta1.go
+++ mockgcp/mocknotebooks/notebooksenvironment_v1beta1.go
@@ -1,106 +1,92 @@
-// Copyright 2026 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package mocknotebooks
 
 import (
-	"context"
-	"fmt"
-	"time"
+        "context"
+        "fmt"
+        "time"
 
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/proto"
-	"google.golang.org/protobuf/types/known/emptypb"
-	"google.golang.org/protobuf/types/known/timestamppb"
+        "google.golang.org/grpc/codes"
+        "google.golang.org/grpc/status"
+        "google.golang.org/protobuf/proto"
+        "google.golang.org/protobuf/types/known/emptypb"
+        "google.golang.org/protobuf/types/known/timestamppb"
 
-	pb_v1beta1 "cloud.google.com/go/notebooks/apiv1beta1/notebookspb"
-	longrunningpb "google.golang.org/genproto/googleapis/longrunning"
+        pb_v1beta1 "cloud.google.com/go/notebooks/apiv1beta1/notebookspb"
+        longrunningpb "google.golang.org/genproto/googleapis/longrunning"
 )
 
 func (s *NotebookServiceV1beta1) GetEnvironment(ctx context.Context, req *pb_v1beta1.GetEnvironmentRequest) (*pb_v1beta1.Environment, error) {
-	name, err := s.parseEnvironmentName(req.GetName())
-	if err != nil {
-		return nil, err
-	}
-	fqn := name.String()
+        name, err := s.parseEnvironmentName(req.GetName())
+        if err != nil {
+                return nil, err
+        }
+        fqn := name.String()
 
-	obj := &pb_v1beta1.Environment{}
-	if err := s.storage.Get(ctx, fqn, obj); err != nil {
-		if status.Code(err) == codes.NotFound {
-			return nil, status.Errorf(codes.NotFound, "environment %q not found", fqn)
-		}
-		return nil, err
-	}
+        obj := &pb_v1beta1.Environment{}
+        if err := s.storage.Get(ctx, fqn, obj); err != nil {
+                if status.Code(err) == codes.NotFound {
+                        return nil, status.Errorf(codes.NotFound, "environment %q not found", fqn)
+                }
+                return nil, err
+        }
 
-	return obj, nil
+        return obj, nil
 }
 
 func (s *NotebookServiceV1beta1) CreateEnvironment(ctx context.Context, req *pb_v1beta1.CreateEnvironmentRequest) (*longrunningpb.Operation, error) {
-	reqName := fmt.Sprintf("%s/environments/%s", req.GetParent(), req.GetEnvironmentId())
-	name, err := s.parseEnvironmentName(reqName)
-	if err != nil {
-		return nil, err
-	}
+        reqName := fmt.Sprintf("%s/environments/%s", req.GetParent(), req.GetEnvironmentId())
+        name, err := s.parseEnvironmentName(reqName)
+        if err != nil {
+                return nil, err
+        }
 
-	fqn := name.String()
-	obj := proto.CloneOf(req.GetEnvironment())
-	obj.Name = fqn
-	obj.CreateTime = timestamppb.New(time.Now())
+        fqn := name.String()
+        obj := proto.Clone(req.GetEnvironment()).(*pb_v1beta1.Environment)
+        obj.Name = fqn
+        obj.CreateTime = timestamppb.New(time.Now())
 
-	if err := s.storage.Create(ctx, fqn, obj); err != nil {
-		return nil, err
-	}
+        if err := s.storage.Create(ctx, fqn, obj); err != nil {
+                return nil, err
+        }
 
-	prefix := fmt.Sprintf("projects/%s/locations/%s", name.Project.ID, name.Location)
-	metadata := &pb_v1beta1.OperationMetadata{
-		ApiVersion:            "v1beta1",
-		CreateTime:            timestamppb.New(time.Now()),
-		RequestedCancellation: false,
-		Target:                name.String(),
-		Verb:                  "create",
-		Endpoint:              "CreateEnvironment",
-	}
-	return s.operations.StartLRO(ctx, prefix, metadata, func() (proto.Message, error) {
-		metadata.EndTime = timestamppb.New(time.Now())
-		return obj, nil
-	})
+        prefix := fmt.Sprintf("projects/%s/locations/%s", name.Project.ID, name.Location)
+        metadata := &pb_v1beta1.OperationMetadata{
+                ApiVersion:            "v1beta1",
+                CreateTime:            timestamppb.New(time.Now()),
+                RequestedCancellation: false,
+                Target:                name.String(),
+                Verb:                  "create",
+                Endpoint:              "CreateEnvironment",
+        }
+        return s.operations.StartLRO(ctx, prefix, metadata, func() (proto.Message, error) {
+                metadata.EndTime = timestamppb.New(time.Now())
+                return obj, nil
+        })
 }
 
 func (s *NotebookServiceV1beta1) DeleteEnvironment(ctx context.Context, req *pb_v1beta1.DeleteEnvironmentRequest) (*longrunningpb.Operation, error) {
-	name, err := s.parseEnvironmentName(req.GetName())
-	if err != nil {
-		return nil, err
-	}
-	fqn := name.String()
+        name, err := s.parseEnvironmentName(req.GetName())
+        if err != nil {
+                return nil, err
+        }
+        fqn := name.String()
 
-	deletedObj := &pb_v1beta1.Environment{}
-	if err := s.storage.Delete(ctx, fqn, deletedObj); err != nil {
-		return nil, err
-	}
+        deletedObj := &pb_v1beta1.Environment{}
+        if err := s.storage.Delete(ctx, fqn, deletedObj); err != nil {
+                return nil, err
+        }
 
-	prefix := fmt.Sprintf("projects/%s/locations/%s", name.Project.ID, name.Location)
-	metadata := &pb_v1beta1.OperationMetadata{
-		ApiVersion:            "v1beta1",
-		CreateTime:            timestamppb.Now(),
-		RequestedCancellation: false,
-		Target:                name.String(),
-		Verb:                  "delete",
-		Endpoint:              "DeleteEnvironment",
-	}
-	return s.operations.StartLRO(ctx, prefix, metadata, func() (proto.Message, error) {
-		metadata.EndTime = timestamppb.New(time.Now())
-		return &emptypb.Empty{}, nil
-	})
+        prefix := fmt.Sprintf("projects/%s/locations/%s", name.Project.ID, name.Location)
+        metadata := &pb_v1beta1.OperationMetadata{
+                ApiVersion:            "v1beta1",
+                CreateTime:            timestamppb.Now(),
+                RequestedCancellation: false,
+                Target:                name.String(),
+                Verb:                  "delete",
+                Endpoint:              "DeleteEnvironment",
+        }
+        return s.operations.StartLRO(ctx, prefix, metadata, func() (proto.Message, error) {
+                metadata.EndTime = timestamppb.New(time.Now())
+                return &emptypb.Empty{}, nil
+        })
 }
