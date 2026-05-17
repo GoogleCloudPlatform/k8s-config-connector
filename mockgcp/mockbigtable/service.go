8e3f9cef16bb3d279fba4d92b07051c1725be421
codebot-robot <codebot-robot@google.com>
2026-05-17 19:05:46 +0000

    Move mockgcp bigtable away from grpc-gateway

--- mockgcp/mockbigtable/service.go
+++ mockgcp/mockbigtable/service.go
@@ -20,14 +20,12 @@ import (
 	"net/http"
 
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common"
-	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/httpmux"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/httptogrpc"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/operations"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/mockgcpregistry"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/pkg/storage"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/proto"
-
-	grpcpb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/google/bigtable/admin/v2"
 
 	// Note: we use the "real" proto (not mockgcp), because the client uses GRPC.
 	pb "cloud.google.com/go/bigtable/admin/apiv2/adminpb"
@@ -66,20 +64,14 @@ func (s *MockService) Register(grpcServer *grpc.Server) {
 }
 
 func (s *MockService) NewHTTPMux(ctx context.Context, conn *grpc.ClientConn) (http.Handler, error) {
-	mux, err := httpmux.NewServeMux(ctx, conn, httpmux.Options{},
-		grpcpb.RegisterBigtableInstanceAdminHandler,
-		grpcpb.RegisterBigtableTableAdminHandler,
-		s.operations.RegisterOperationsPath("/v2/{prefix=**}/operations/{name}"))
+	mux, err := httptogrpc.NewGRPCMux(conn)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error building grpc service: %w", err)
 	}
 
-	// Returns slightly non-standard errors
-	mux.RewriteError = func(ctx context.Context, error *httpmux.ErrorResponse) {
-		if error.Code == 404 {
-			error.Errors = nil
-		}
-	}
+	mux.AddService(pb.NewBigtableInstanceAdminClient(conn))
+	mux.AddService(pb.NewBigtableTableAdminClient(conn))
+	mux.AddOperationsPath("/v2/{prefix=**}/operations/{name}", conn)
 
 	return mux, nil
 }
