97ddd77c6a39084bc6ce3e9d764a57d154239019
codebot-robot <codebot-robot@google.com>
2026-05-17 08:09:23 +0000

    Move mockgcp alloydb away from grpc-gateway
    Move the AlloyDB mock away from grpc-gateway to use the official Google Cloud Go client library and httptogrpc.
    
    Fixes #8241
    
    Generated by Overseer (powered by the gemini-3-flash-preview model).

--- mockgcp/mockalloydb/cluster.go
+++ mockgcp/mockalloydb/cluster.go
@@ -30,7 +30,7 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
-	pb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/alloydb/v1beta"
+	pb "cloud.google.com/go/alloydb/apiv1beta/alloydbpb"
 )
 
 type AlloyDBAdminV1 struct {
