e6b897380a31f990a4edc20c2fe4cfcc856ffef5
codebot-robot <codebot-robot@google.com>
2026-05-17 07:23:53 +0000

    Move mockgcp apphub away from grpc-gateway

--- mockgcp/mockapphub/application.go
+++ mockgcp/mockapphub/application.go
@@ -31,8 +31,8 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
+	pb "cloud.google.com/go/apphub/apiv1/apphubpb"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/projects"
-	pb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/apphub/v1"
 	longrunningpb "google.golang.org/genproto/googleapis/longrunning"
 )
 
