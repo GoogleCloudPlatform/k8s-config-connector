598625b40865893bc92a488e32f5189fd173dfb7
codebot-robot <codebot-robot@google.com>
2026-05-18 15:07:09 +0000

    Move mockgcp monitoring away from grpc-gateway
    This PR migrates the mockgcp monitoring service to use the official
    Google Cloud Go client libraries and httptogrpc for HTTP-to-gRPC mapping.
    
    Key changes:
    - Removed monitoring proto generation from mockgcp/Makefile.
    - Deleted generated monitoring protos in mockgcp/generated/.
    - Updated mockmonitoring to use cloud.google.com/go/monitoring imports.
    - Switched HTTP mux from httpmux to httptogrpc.
    - Fixed a bug in NotificationChannel update where 'description' was incorrectly mapped.
    - Updated mock update methods to handle missing updateMask (defaulting to full update).
    
    Fixes #8321
    
    This PR was generated by Overseer (powered by the gemini-3-flash-preview model).

--- mockgcp/mockmonitoring/alertpolicy.go
+++ mockgcp/mockmonitoring/alertpolicy.go
@@ -32,7 +32,7 @@ import (
 	"google.golang.org/protobuf/types/known/wrapperspb"
 
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/projects"
-	pb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/monitoring/v3"
+	pb "cloud.google.com/go/monitoring/apiv3/v2/monitoringpb"
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/pkg/storage"
 	"github.com/golang/protobuf/ptypes/empty"
 )
@@ -134,7 +134,12 @@ func (s *AlertPolicyService) UpdateAlertPolicy(ctx context.Context, req *pb.Upda
 	}
 
 	updated := proto.CloneOf(existing)
-	for _, path := range req.GetUpdateMask().GetPaths() {
+	paths := req.GetUpdateMask().GetPaths()
+	if len(paths) == 0 {
+		paths = []string{"display_name", "enabled", "conditions", "documentation", "combiner", "notification_channels", "severity"}
+	}
+
+	for _, path := range paths {
 		switch path {
 		case "displayName", "display_name":
 			updated.DisplayName = req.GetAlertPolicy().GetDisplayName()
@@ -146,7 +151,7 @@ func (s *AlertPolicyService) UpdateAlertPolicy(ctx context.Context, req *pb.Upda
 			updated.Documentation = req.GetAlertPolicy().GetDocumentation()
 		case "combiner":
 			updated.Combiner = req.GetAlertPolicy().GetCombiner()
-		case "notificationChannels":
+		case "notificationChannels", "notification_channels":
 			updated.NotificationChannels = req.GetAlertPolicy().GetNotificationChannels()
 		case "severity":
 			updated.Severity = req.GetAlertPolicy().GetSeverity()
--- mockgcp/mockmonitoring/group.go
+++ mockgcp/mockmonitoring/group.go
@@ -21,7 +21,7 @@ import (
 	"time"
 
 	"github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/common/projects"
-	pb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/monitoring/v3"
+	pb "cloud.google.com/go/monitoring/apiv3/v2/monitoringpb"
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
