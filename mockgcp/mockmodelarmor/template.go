6e63030ab4846c787ecc1c176fb2fd75bf9a6f3b
codebot-robot <codebot-robot@google.com>
2026-05-18 15:37:45 +0000

    mockgcp: move modelarmor away from grpc-gateway
    This PR moves the mockgcp modelarmor implementation away from
    grpc-gateway to use the official Google Cloud Go client library
    (cloud.google.com/go/modelarmor) and httptogrpc.
    
    Fixes #8320

--- mockgcp/mockmodelarmor/template.go
+++ mockgcp/mockmodelarmor/template.go
@@ -30,7 +30,7 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
-	pb "github.com/GoogleCloudPlatform/k8s-config-connector/mockgcp/generated/mockgcp/cloud/modelarmor/v1"
+	pb "cloud.google.com/go/modelarmor/apiv1/modelarmorpb"
 )
 
 type ModelArmorV1 struct {
