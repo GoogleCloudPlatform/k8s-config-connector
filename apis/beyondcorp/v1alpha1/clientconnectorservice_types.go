37081c57e2a6f04d2117e21116b14a0b19ca6c88
codebot-robot <codebot-robot@google.com>
2026-05-19 01:21:29 +0000

    ai:chore: Implement direct types for: BeyondCorpClientConnectorService
    Fixes #8403

---
+++ apis/beyondcorp/v1alpha1/clientconnectorservice_types.go
@@ -0,0 +1,115 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+import (
+	refsv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/apis/k8s/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var BeyondCorpClientConnectorServiceGVK = GroupVersion.WithKind("BeyondCorpClientConnectorService")
+
+// BeyondCorpClientConnectorServiceSpec defines the desired state of BeyondCorpClientConnectorService
+// +kcc:spec:proto=google.cloud.beyondcorp.clientconnectorservices.v1.ClientConnectorService
+type BeyondCorpClientConnectorServiceSpec struct {
+	// The project that this resource belongs to.
+	ProjectRef *refsv1beta1.ProjectRef `json:"projectRef"`
+
+	// The location of this resource.
+	Location string `json:"location"`
+
+	// The BeyondCorpClientConnectorService name. If not given, the metadata.name will be used.
+	ResourceID *string `json:"resourceID,omitempty"`
+
+	// Optional. User-provided name.
+	// The display name should follow certain format.
+	// * Must be 6 to 30 characters in length.
+	// * Can only contain lowercase letters, numbers, and hyphens.
+	// * Must start with a letter.
+	// +kubebuilder:validation:Optional
+	DisplayName *string `json:"displayName,omitempty"`
+
+	// Required. The details of the ingress settings.
+	// +kubebuilder:validation:Required
+	Ingress *ClientConnectorService_Ingress `json:"ingress"`
+
+	// Required. The details of the egress settings.
+	// +kubebuilder:validation:Required
+	Egress *ClientConnectorService_Egress `json:"egress"`
+}
+
+// BeyondCorpClientConnectorServiceStatus defines the config connector machine state of BeyondCorpClientConnectorService
+type BeyondCorpClientConnectorServiceStatus struct {
+	/* Conditions represent the latest available observations of the
+	   object's current state. */
+	Conditions []v1alpha1.Condition `json:"conditions,omitempty"`
+
+	// ObservedGeneration is the generation of the resource that was most recently observed by the Config Connector controller. If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource.
+	ObservedGeneration *int64 `json:"observedGeneration,omitempty"`
+
+	// A unique specifier for the BeyondCorpClientConnectorService resource in GCP.
+	ExternalRef *string `json:"externalRef,omitempty"`
+
+	// ObservedState is the state of the resource as most recently observed in GCP.
+	ObservedState *BeyondCorpClientConnectorServiceObservedState `json:"observedState,omitempty"`
+}
+
+// BeyondCorpClientConnectorServiceObservedState is the state of the BeyondCorpClientConnectorService resource as most recently observed in GCP.
+// +kcc:observedstate:proto=google.cloud.beyondcorp.clientconnectorservices.v1.ClientConnectorService
+type BeyondCorpClientConnectorServiceObservedState struct {
+	// Output only. [Output only] Create time stamp.
+	CreateTime *string `json:"createTime,omitempty"`
+
+	// Output only. [Output only] Update time stamp.
+	UpdateTime *string `json:"updateTime,omitempty"`
+
+	// Output only. The operational state of the ClientConnectorService.
+	State *string `json:"state,omitempty"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:resource:categories=gcp,shortName=gcpbeyondcorpclientconnectorservice;gcpbeyondcorpclientconnectorservices
+// +kubebuilder:subresource:status
+// +kubebuilder:metadata:labels="cnrm.cloud.google.com/managed-by-kcc=true"
+// +kubebuilder:metadata:labels="cnrm.cloud.google.com/system=true"
+// +kubebuilder:printcolumn:name="Age",JSONPath=".metadata.creationTimestamp",type="date"
+// +kubebuilder:printcolumn:name="Ready",JSONPath=".status.conditions[?(@.type=='Ready')].status",type="string",description="When 'True', the most recent reconcile of the resource succeeded"
+// +kubebuilder:printcolumn:name="Status",JSONPath=".status.conditions[?(@.type=='Ready')].reason",type="string",description="The reason for the value in 'Ready'"
+// +kubebuilder:printcolumn:name="Status Age",JSONPath=".status.conditions[?(@.type=='Ready')].lastTransitionTime",type="date",description="The last transition time for the value in 'Status'"
+
+// BeyondCorpClientConnectorService is the Schema for the BeyondCorpClientConnectorService API
+// +k8s:openapi-gen=true
+type BeyondCorpClientConnectorService struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// +required
+	Spec   BeyondCorpClientConnectorServiceSpec   `json:"spec,omitempty"`
+	Status BeyondCorpClientConnectorServiceStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// BeyondCorpClientConnectorServiceList contains a list of BeyondCorpClientConnectorService
+type BeyondCorpClientConnectorServiceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []BeyondCorpClientConnectorService `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&BeyondCorpClientConnectorService{}, &BeyondCorpClientConnectorServiceList{})
+}
