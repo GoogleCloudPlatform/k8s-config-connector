0d359260476be180b873938202bcc4e065b22886
codebot-robot <codebot-robot@google.com>
2026-05-20 03:50:10 +0000

    Address review feedback: follow standard generation patterns
    - Updated apis/serviceusage/v1beta1/generate.sh to include both ServiceIdentity and Service in a single generate-types call.
    - Removed the non-standard sed command that was manually adding the Service resource annotation.
    - Regenerated types.generated.go using the standard tool, which now includes all reachable (and pruned) types from the Service proto.
    - Verified that manual mappers in mapper.go continue to shadow complex proto fields.
    
    Issue #7499

--- apis/serviceusage/v1beta1/generate.sh
+++ apis/serviceusage/v1beta1/generate.sh
@@ -31,10 +31,8 @@ go run . generate-types \
   --service google.api.serviceusage.v1beta1 \
   --api-version serviceusage.cnrm.cloud.google.com/v1beta1  \
   --resource ServiceIdentity:ServiceIdentity \
+  --resource Service:Service \
   --include-skipped-output
-
-# Add back the Service resource annotation so it's registered
-sed -i 's/\/\/ resource: ServiceIdentity:ServiceIdentity/\/\/ resource: ServiceIdentity:ServiceIdentity\n\/\/ resource: Service:Service/' "${REPO_ROOT}/apis/serviceusage/v1beta1/types.generated.go"
 
 go run . generate-mapper \
   --service google.api.serviceusage.v1beta1 \
