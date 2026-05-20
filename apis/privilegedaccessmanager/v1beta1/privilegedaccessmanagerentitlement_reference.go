b65ab7c1c137eaf1cb27101df6936036bdb3f098
codebot-robot <codebot-robot@google.com>
2026-05-20 01:19:32 +0000

    Move PrivilegedAccessManagerEntitlement to identity and refs pattern
    Fixes #8456

---
+++ apis/privilegedaccessmanager/v1beta1/privilegedaccessmanagerentitlement_reference.go
@@ -0,0 +1,92 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+import (
+	"context"
+
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common/identity"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var _ refs.Ref = &PrivilegedAccessManagerEntitlementRef{}
+
+// PrivilegedAccessManagerEntitlementRef defines the resource reference to PrivilegedAccessManagerEntitlement, which "External" field
+// holds the GCP identifier for the KRM object.
+type PrivilegedAccessManagerEntitlementRef struct {
+	// A reference to an externally managed PrivilegedAccessManagerEntitlement resource.
+	// Should be in the format "projects/{{projectID}}/locations/{{location}}/entitlements/{{entitlementID}}", "folders/{{folderID}}/locations/{{location}}/entitlements/{{entitlementID}}", or "organizations/{{organizationID}}/locations/{{location}}/entitlements/{{entitlementID}}".
+	External string `json:"external,omitempty"`
+
+	// The name of a PrivilegedAccessManagerEntitlement resource.
+	Name string `json:"name,omitempty"`
+
+	// The namespace of a PrivilegedAccessManagerEntitlement resource.
+	Namespace string `json:"namespace,omitempty"`
+}
+
+func init() {
+	refs.Register(&PrivilegedAccessManagerEntitlementRef{})
+}
+
+func (r *PrivilegedAccessManagerEntitlementRef) GetGVK() schema.GroupVersionKind {
+	return PrivilegedAccessManagerEntitlementGVK
+}
+
+func (r *PrivilegedAccessManagerEntitlementRef) GetNamespacedName() types.NamespacedName {
+	return types.NamespacedName{
+		Name:      r.Name,
+		Namespace: r.Namespace,
+	}
+}
+
+func (r *PrivilegedAccessManagerEntitlementRef) GetExternal() string {
+	return r.External
+}
+
+func (r *PrivilegedAccessManagerEntitlementRef) SetExternal(ref string) {
+	r.External = ref
+}
+
+func (r *PrivilegedAccessManagerEntitlementRef) ValidateExternal(ref string) error {
+	id := &PrivilegedAccessManagerEntitlementIdentity{}
+	if err := id.FromExternal(ref); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *PrivilegedAccessManagerEntitlementRef) ParseExternalToIdentity() (identity.Identity, error) {
+	id := &PrivilegedAccessManagerEntitlementIdentity{}
+	if err := id.FromExternal(r.External); err != nil {
+		return nil, err
+	}
+	return id, nil
+}
+
+func (r *PrivilegedAccessManagerEntitlementRef) Normalize(ctx context.Context, reader client.Reader, defaultNamespace string) error {
+	fallback := func(u *unstructured.Unstructured) string {
+		identity, err := getIdentityFromPrivilegedAccessManagerEntitlementSpec(ctx, reader, u)
+		if err != nil {
+			return ""
+		}
+		return identity.String()
+	}
+	return refs.NormalizeWithFallback(ctx, reader, r, defaultNamespace, fallback)
+}
--- pkg/controller/direct/privilegedaccessmanager/entitlement_controller.go
+++ pkg/controller/direct/privilegedaccessmanager/entitlement_controller.go
@@ -80,50 +80,11 @@ func (m *entitlementModel) AdapterForObject(ctx context.Context, op *directbase.
 		return nil, fmt.Errorf("error converting to %T: %w", obj, err)
 	}
 
-	// Get ResourceID
-	resourceID := direct.ValueOf(obj.Spec.ResourceID)
-	if resourceID == "" {
-		resourceID = obj.GetName()
-	}
-	if resourceID == "" {
-		return nil, fmt.Errorf("cannot resolve resource ID")
-	}
-
-	container, err := oneOfContainer(ctx, reader, obj,
-		obj.Spec.ProjectRef,
-		obj.Spec.FolderRef,
-		obj.Spec.OrganizationRef)
+	i, err := obj.GetIdentity(ctx, reader)
 	if err != nil {
-		return nil, fmt.Errorf("error resolving 'obj.Spec.ProjectRef', "+
-			"'obj.Spec.FolderRef' and 'obj.Spec.OrganizationRef': %w", err)
+		return nil, err
 	}
-
-	// Get location
-	location := *obj.Spec.Location
-
-	var id *PrivilegedAccessManagerEntitlementIdentity
-	externalRef := direct.ValueOf(obj.Status.ExternalRef)
-	if externalRef == "" {
-		id = BuildID(container, location, resourceID)
-	} else {
-		id, err = asID(externalRef)
-		if err != nil {
-			return nil, err
-		}
-
-		if id.Parent.Container != container {
-			return nil, fmt.Errorf("PrivilegedAccessManagerEntitlement %s/%s has parent container changed, expected %s, got %s",
-				u.GetNamespace(), u.GetName(), id.Parent.Container, container)
-		}
-		if id.Parent.Location != location {
-			return nil, fmt.Errorf("PrivilegedAccessManagerEntitlement %s/%s has spec.location changed, expect %s, got %s",
-				u.GetNamespace(), u.GetName(), id.Parent.Location, location)
-		}
-		if id.Entitlement != resourceID {
-			return nil, fmt.Errorf("PrivilegedAccessManagerEntitlement %s/%s has metadata.name or spec.resourceID changed, expect %s, got %s",
-				u.GetNamespace(), u.GetName(), id.Entitlement, resourceID)
-		}
-	}
+	id := i.(*krm.PrivilegedAccessManagerEntitlementIdentity)
 
 	if obj.Spec.RequesterJustificationConfig.NotMandatory == nil && obj.Spec.RequesterJustificationConfig.Unstructured == nil {
 		return nil, fmt.Errorf("one and only one of 'spec.requesterJustificationConfig.notMandatory' " +
@@ -214,7 +175,7 @@ func (m *entitlementModel) AdapterForURL(ctx context.Context, url string) (direc
 }
 
 type Adapter struct {
-	id        *PrivilegedAccessManagerEntitlementIdentity
+	id        *krm.PrivilegedAccessManagerEntitlementIdentity
 	gcpClient *gcp.Client
 	desired   *krm.PrivilegedAccessManagerEntitlement
 	actual    *privilegedaccessmanagerpb.Entitlement
@@ -224,15 +185,15 @@ var _ directbase.Adapter = &Adapter{}
 
 func (a *Adapter) Find(ctx context.Context) (bool, error) {
 	log := klog.FromContext(ctx)
-	log.V(2).Info("getting PrivilegedAccessManagerEntitlement", "name", a.id.FullyQualifiedName())
+	log.V(2).Info("getting PrivilegedAccessManagerEntitlement", "name", a.id.String())
 
-	req := &privilegedaccessmanagerpb.GetEntitlementRequest{Name: a.id.FullyQualifiedName()}
+	req := &privilegedaccessmanagerpb.GetEntitlementRequest{Name: a.id.String()}
 	entitlementpb, err := a.gcpClient.GetEntitlement(ctx, req)
 	if err != nil {
 		if direct.IsNotFound(err) {
 			return false, nil
 		}
-		return false, fmt.Errorf("error getting PrivilegedAccessManagerEntitlement %q: %w", a.id.FullyQualifiedName(), err)
+		return false, fmt.Errorf("error getting PrivilegedAccessManagerEntitlement %q: %w", a.id.String(), err)
 	}
 
 	a.actual = entitlementpb
@@ -263,11 +224,11 @@ func (a *Adapter) Create(ctx context.Context, createOp *directbase.CreateOperati
 	u := createOp.GetUnstructured()
 
 	log := klog.FromContext(ctx)
-	log.V(2).Info("creating PrivilegedAccessManagerEntitlement", "name", a.id.FullyQualifiedName())
+	log.V(2).Info("creating PrivilegedAccessManagerEntitlement", "name", a.id.String())
 	mapCtx := &direct.MapContext{}
 
 	desired := a.desired.DeepCopy()
-	resourceType, resource, err := getResourceTypeAndResourceFromContainer(a.id.Parent.Container)
+	resourceType, resource, err := getResourceTypeAndResourceFromContainer(a.id.Parent())
 	if err != nil {
 		return fmt.Errorf("error getting resourceType and resource from container: %w", err)
 	}
@@ -278,19 +239,19 @@ func (a *Adapter) Create(ctx context.Context, createOp *directbase.CreateOperati
 	}
 
 	req := &privilegedaccessmanagerpb.CreateEntitlementRequest{
-		Parent:        a.id.Parent.String(),
+		Parent:        a.id.FullParent(),
 		EntitlementId: a.id.Entitlement,
 		Entitlement:   entitlement,
 	}
 	op, err := a.gcpClient.CreateEntitlement(ctx, req)
 	if err != nil {
-		return fmt.Errorf("error creating PrivilegedAccessManagerEntitlement %s: %w", a.id.FullyQualifiedName(), err)
+		return fmt.Errorf("error creating PrivilegedAccessManagerEntitlement %s: %w", a.id.String(), err)
 	}
 	created, err := op.Wait(ctx)
 	if err != nil {
-		return fmt.Errorf("error waiting for PrivilegedAccessManagerEntitlement %s to be created: %w", a.id.FullyQualifiedName(), err)
+		return fmt.Errorf("error waiting for PrivilegedAccessManagerEntitlement %s to be created: %w", a.id.String(), err)
 	}
-	log.V(2).Info("successfully created PrivilegedAccessManagerEntitlement", "name", a.id.FullyQualifiedName())
+	log.V(2).Info("successfully created PrivilegedAccessManagerEntitlement", "name", a.id.String())
 
 	status := &krm.PrivilegedAccessManagerEntitlementStatus{}
 	observedState := PrivilegedAccessManagerEntitlementObservedState_FromProto(mapCtx, created)
@@ -298,7 +259,7 @@ func (a *Adapter) Create(ctx context.Context, createOp *directbase.CreateOperati
 		return mapCtx.Err()
 	}
 	status.ObservedState = observedState
-	status.ExternalRef = a.id.AsExternalRef()
+	status.ExternalRef = direct.PtrTo("//" + a.id.Host() + "/" + a.id.String())
 	return setStatus(u, status)
 }
 
@@ -306,7 +267,7 @@ func (a *Adapter) Update(ctx context.Context, updateOp *directbase.UpdateOperati
 	u := updateOp.GetUnstructured()
 
 	log := klog.FromContext(ctx)
-	log.V(2).Info("updating PrivilegedAccessManagerEntitlement", "name", a.id.FullyQualifiedName())
+	log.V(2).Info("updating PrivilegedAccessManagerEntitlement", "name", a.id.String())
 	mapCtx := &direct.MapContext{}
 
 	updateMask := &fieldmaskpb.FieldMask{}
@@ -360,7 +321,7 @@ func (a *Adapter) Update(ctx context.Context, updateOp *directbase.UpdateOperati
 		updateMask.Paths = append(updateMask.Paths, "requester_justification_config")
 	}
 	if len(updateMask.Paths) == 0 {
-		log.V(2).Info("underlying PrivilegedAccessManagerEntitlement already up to date", "name", a.id.FullyQualifiedName())
+		log.V(2).Info("underlying PrivilegedAccessManagerEntitlement already up to date", "name", a.id.String())
 
 		status := &krm.PrivilegedAccessManagerEntitlementStatus{}
 		observedState := PrivilegedAccessManagerEntitlementObservedState_FromProto(mapCtx, a.actual)
@@ -368,14 +329,14 @@ func (a *Adapter) Update(ctx context.Context, updateOp *directbase.UpdateOperati
 			return mapCtx.Err()
 		}
 		status.ObservedState = observedState
-		status.ExternalRef = a.id.AsExternalRef()
+		status.ExternalRef = direct.PtrTo("//" + a.id.Host() + "/" + a.id.String())
 		return setStatus(u, status)
 	}
 
 	structuredreporting.ReportDiff(ctx, report)
 
 	desired := a.desired.DeepCopy()
-	resourceType, resource, err := getResourceTypeAndResourceFromContainer(a.id.Parent.Container)
+	resourceType, resource, err := getResourceTypeAndResourceFromContainer(a.id.Parent())
 	if err != nil {
 		return fmt.Errorf("error getting resourceType and resource from container: %w", err)
 	}
@@ -384,7 +345,7 @@ func (a *Adapter) Update(ctx context.Context, updateOp *directbase.UpdateOperati
 	if mapCtx.Err() != nil {
 		return mapCtx.Err()
 	}
-	entitlement.Name = a.id.FullyQualifiedName()
+	entitlement.Name = a.id.String()
 	entitlement.Etag = a.actual.Etag
 	req := &privilegedaccessmanagerpb.UpdateEntitlementRequest{
 		UpdateMask:  updateMask,
@@ -392,13 +353,13 @@ func (a *Adapter) Update(ctx context.Context, updateOp *directbase.UpdateOperati
 	}
 	op, err := a.gcpClient.UpdateEntitlement(ctx, req)
 	if err != nil {
-		return fmt.Errorf("error updating PrivilegedAccessManagerEntitlement %s: %w", a.id.FullyQualifiedName(), err)
+		return fmt.Errorf("error updating PrivilegedAccessManagerEntitlement %s: %w", a.id.String(), err)
 	}
 	updated, err := op.Wait(ctx)
 	if err != nil {
-		return fmt.Errorf("error waiting for PrivilegedAccessManagerEntitlement %s to be updated: %w", a.id.FullyQualifiedName(), err)
+		return fmt.Errorf("error waiting for PrivilegedAccessManagerEntitlement %s to be updated: %w", a.id.String(), err)
 	}
-	log.V(2).Info("successfully updated PrivilegedAccessManagerEntitlement", "name", a.id.FullyQualifiedName())
+	log.V(2).Info("successfully updated PrivilegedAccessManagerEntitlement", "name", a.id.String())
 
 	status := &krm.PrivilegedAccessManagerEntitlementStatus{}
 	observedState := PrivilegedAccessManagerEntitlementObservedState_FromProto(mapCtx, updated)
@@ -434,15 +395,15 @@ func (a *Adapter) Export(ctx context.Context) (*unstructured.Unstructured, error
 	if mapCtx.Err() != nil {
 		return nil, mapCtx.Err()
 	}
-	if strings.HasPrefix(a.id.Parent.Container, "projects") {
-		obj.Spec.ProjectRef = &refs.ProjectRef{External: a.id.Parent.Container}
-	} else if strings.HasPrefix(a.id.Parent.Container, "folders") {
-		obj.Spec.FolderRef = &refs.FolderRef{External: a.id.Parent.Container}
+	if strings.HasPrefix(a.id.Parent(), "projects") {
+		obj.Spec.ProjectRef = &refs.ProjectRef{External: a.id.Parent()}
+	} else if strings.HasPrefix(a.id.Parent(), "folders") {
+		obj.Spec.FolderRef = &refs.FolderRef{External: a.id.Parent()}
 	} else {
-		obj.Spec.OrganizationRef = &refs.OrganizationRef{External: a.id.Parent.Container}
+		obj.Spec.OrganizationRef = &refs.OrganizationRef{External: a.id.Parent()}
 	}
 
-	obj.Spec.Location = &a.id.Parent.Location
+	obj.Spec.Location = &a.id.Location
 	uObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
 	if err != nil {
 		return nil, err
@@ -454,23 +415,23 @@ func (a *Adapter) Export(ctx context.Context) (*unstructured.Unstructured, error
 // Delete implements the Adapter interface.
 func (a *Adapter) Delete(ctx context.Context, deleteOp *directbase.DeleteOperation) (bool, error) {
 	log := klog.FromContext(ctx)
-	log.V(2).Info("deleting PrivilegedAccessManagerEntitlement", "name", a.id.FullyQualifiedName())
+	log.V(2).Info("deleting PrivilegedAccessManagerEntitlement", "name", a.id.String())
 
-	req := &privilegedaccessmanagerpb.DeleteEntitlementRequest{Name: a.id.FullyQualifiedName()}
+	req := &privilegedaccessmanagerpb.DeleteEntitlementRequest{Name: a.id.String()}
 	op, err := a.gcpClient.DeleteEntitlement(ctx, req)
 	if err != nil {
 		if direct.IsNotFound(err) {
 			return false, nil
 		}
-		return false, fmt.Errorf("error deleting PrivilegedAccessManagerEntitlement %s: %w", a.id.FullyQualifiedName(), err)
+		return false, fmt.Errorf("error deleting PrivilegedAccessManagerEntitlement %s: %w", a.id.String(), err)
 	}
 
 	_, err = op.Wait(ctx)
 	if err != nil {
-		return false, fmt.Errorf("error waiting for PrivilegedAccessManagerEntitlement %s to be deleted: %w", a.id.FullyQualifiedName(), err)
+		return false, fmt.Errorf("error waiting for PrivilegedAccessManagerEntitlement %s to be deleted: %w", a.id.String(), err)
 	}
 
-	log.V(2).Info("successfully deleted PrivilegedAccessManagerEntitlement", "name", a.id.FullyQualifiedName())
+	log.V(2).Info("successfully deleted PrivilegedAccessManagerEntitlement", "name", a.id.String())
 	return true, nil
 }
 
