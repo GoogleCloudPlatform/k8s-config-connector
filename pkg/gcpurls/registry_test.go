98fd7f39cd32013a0df17efd743acc0b6bc18d8a
codebot-robot <codebot-robot@google.com>
2026-05-21 05:58:59 +0000

    Move AlloyDBUser to identity and refs pattern
    This PR migrates AlloyDBUser to the new identity and refs pattern, implementing IdentityV2 and refs.Ref interfaces. It also updates AlloyDBUserSpec to use the shared AlloyDBClusterRef for consistency.
    
    Fixes #8502
    
    This PR was generated by Overseer (powered by the gemini-3-flash-preview model).

--- pkg/gcpurls/registry_test.go
+++ pkg/gcpurls/registry_test.go
@@ -81,8 +81,9 @@ func TestRegisteredTemplatesMatchCAI(t *testing.T) {
 		"//aiplatform.googleapis.com/projects/{}/locations/{}/exampleStores/{}":                   true,
 		"//networkconnectivity.googleapis.com/projects/{}/locations/{}/regionalEndpoints/{}":      true,
 		"//aistreams.googleapis.com/projects/{}/locations/{}/clusters/{}":                         true,
-		"//batch.googleapis.com/projects/{}/locations/{}/resourceAllowances/{}":                   true}
-
+		"//batch.googleapis.com/projects/{}/locations/{}/resourceAllowances/{}":                   true,
+		"//alloydb.googleapis.com/projects/{}/locations/{}/clusters/{}/users/{}":                  true,
+	}
 	for _, tmpl := range templates {
 		fullURL := "//" + tmpl.Host() + "/" + tmpl.CanonicalForm()
 		normalized := normalizeTemplateFormat(fullURL)
