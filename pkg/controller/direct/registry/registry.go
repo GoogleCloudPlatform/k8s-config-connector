6f23a8bcf0dc30c94c92a75972b329b2457628fa
codebot-robot <codebot-robot@google.com>
2026-05-22 13:06:35 +0000

    Implement Direct IAM for DataCatalogEntry

--- pkg/controller/direct/registry/registry.go
+++ pkg/controller/direct/registry/registry.go
@@ -127,6 +127,8 @@ func IsIAMDirect(groupKind schema.GroupKind) bool {
 	switch groupKind {
 	case schema.GroupKind{Group: "privateca.cnrm.cloud.google.com", Kind: "PrivateCACAPool"}:
 		return true
+	case schema.GroupKind{Group: "datacatalog.cnrm.cloud.google.com", Kind: "DataCatalogEntry"}:
+		return true
 	}
 	return false
 }
