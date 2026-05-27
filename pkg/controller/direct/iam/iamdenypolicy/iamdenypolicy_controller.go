1c5269f1fda2b71f53c784832bc16d9058a3ff26
codebot-robot <codebot-robot@google.com>
2026-05-27 22:06:10 +0000

    feat: implement direct controller for IAMDenyPolicy

---
+++ pkg/controller/direct/iam/iamdenypolicy/iamdenypolicy_controller.go
@@ -0,0 +1,282 @@
+package iamdenypolicy
+
+import (
+	"context"
+	"fmt"
+
+	"google.golang.org/api/option"
+
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/iam/v1alpha1"
+	pb "cloud.google.com/go/iam/apiv2/iampb"
+	gcp_iam "cloud.google.com/go/iam/apiv2"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/config"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/directbase"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/registry"
+	mappers "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/iam"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/klog/v2"
+)
+
+func init() {
+	registry.RegisterModel(krm.IAMDenyPolicyGVK, NewModel)
+}
+
+func NewModel(ctx context.Context, config *config.ControllerConfig) (directbase.Model, error) {
+	return &model{config: *config}, nil
+}
+
+var _ directbase.Model = &model{}
+
+type model struct {
+	config config.ControllerConfig
+}
+
+func (m *model) client(ctx context.Context) (*gcp_iam.PoliciesClient, error) {
+	var opts []option.ClientOption
+	opts, err := m.config.RESTClientOptions()
+	if err != nil {
+		return nil, err
+	}
+	gcpClient, err := gcp_iam.NewPoliciesRESTClient(ctx, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("building policies client: %w", err)
+	}
+	return gcpClient, err
+}
+
+func (m *model) AdapterForObject(ctx context.Context, op *directbase.AdapterForObjectOperation) (directbase.Adapter, error) {
+	obj := &krm.IAMDenyPolicy{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(op.Object.Object, &obj); err != nil {
+		return nil, fmt.Errorf("error converting to %T: %w", obj, err)
+	}
+
+	id, err := obj.GetIdentity(ctx, op.Reader)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get gcp client
+	gcpClient, err := m.client(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &Adapter{
+		id:        id.(*krm.IAMDenyPolicyIdentity),
+		gcpClient: gcpClient,
+		desired:   obj,
+	}, nil
+}
+
+func (m *model) AdapterForURL(ctx context.Context, url string) (directbase.Adapter, error) {
+	return nil, nil
+}
+
+type Adapter struct {
+	id        *krm.IAMDenyPolicyIdentity
+	gcpClient *gcp_iam.PoliciesClient
+	desired   *krm.IAMDenyPolicy
+	actual    *pb.Policy
+}
+
+var _ directbase.Adapter = &Adapter{}
+
+func (a *Adapter) Find(ctx context.Context) (bool, error) {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("getting IAMDenyPolicy", "name", a.id.String())
+
+	req := &pb.GetPolicyRequest{
+		Name: a.id.String(),
+	}
+	policy, err := a.gcpClient.GetPolicy(ctx, req)
+	if err != nil {
+		if direct.IsNotFound(err) {
+			return false, nil
+		}
+		return false, fmt.Errorf("getting IAMDenyPolicy %q: %w", a.id.String(), err)
+	}
+
+	a.actual = policy
+
+	mapCtx := &direct.MapContext{}
+	observedState := mappers.IAMDenyPolicyObservedState_FromProto(mapCtx, policy)
+	if mapCtx.Err() != nil {
+		return false, mapCtx.Err()
+	}
+
+	a.desired.Status.ObservedState = observedState
+	a.desired.Status.ExternalRef = direct.LazyPtr(a.id.String())
+
+	return true, nil
+}
+
+func (a *Adapter) Create(ctx context.Context, createOp *directbase.CreateOperation) error {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("creating IAMDenyPolicy", "name", a.id.String())
+	mapCtx := &direct.MapContext{}
+
+	desiredProto := mappers.IAMDenyPolicySpec_ToProto(mapCtx, &a.desired.Spec)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+
+	parent := "policies/" + a.id.AttachmentPoint + "/denypolicies"
+
+	req := &pb.CreatePolicyRequest{
+		Parent:   parent,
+		Policy:   desiredProto,
+		PolicyId: a.id.DenyPolicy,
+	}
+	op, err := a.gcpClient.CreatePolicy(ctx, req)
+	if err != nil {
+		return fmt.Errorf("creating IAMDenyPolicy %q: %w", a.id.String(), err)
+	}
+
+	created, err := op.Wait(ctx)
+	if err != nil {
+		return fmt.Errorf("waiting for IAMDenyPolicy %q creation: %w", a.id.String(), err)
+	}
+
+	log.V(2).Info("successfully created IAMDenyPolicy", "name", a.id.String())
+
+	observedState := mappers.IAMDenyPolicyObservedState_FromProto(mapCtx, created)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+
+	a.desired.Status.ObservedState = observedState
+	a.desired.Status.ExternalRef = direct.LazyPtr(a.id.String())
+
+	return nil
+}
+
+func (a *Adapter) Update(ctx context.Context, updateOp *directbase.UpdateOperation) error {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("updating IAMDenyPolicy", "name", a.id.String())
+	mapCtx := &direct.MapContext{}
+
+	desiredProto := mappers.IAMDenyPolicySpec_ToProto(mapCtx, &a.desired.Spec)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+	desiredProto.Name = a.id.String()
+
+	// Compare desired with actual to prevent unnecessary updates
+	isChanged := false
+	if a.actual.GetDisplayName() != desiredProto.GetDisplayName() {
+		isChanged = true
+	}
+	// Note: proto.Equal is better, but deep equal might work if we serialized it back from actual via Export or mappers.
+	// We can use proto.Equal for protobufs, but desiredProto is generated by mappers and a.actual is raw API response.
+	// Actually, mappers might drop some output-only fields like etag, name, uid, etc.
+	// So we can convert a.actual back to Spec and then back to Proto to get a normalized actual,
+	// or we can just convert a.desired.Spec to Proto and compare rules.
+	// For simplicity, let's compare the string representation or just proto rules.
+	if !isChanged {
+		if len(desiredProto.Rules) != len(a.actual.Rules) {
+			isChanged = true
+		} else {
+			for i := range desiredProto.Rules {
+				if desiredProto.Rules[i].String() != a.actual.Rules[i].String() {
+					isChanged = true
+					break
+				}
+			}
+		}
+	}
+
+	if !isChanged {
+		log.V(2).Info("no changes detected for IAMDenyPolicy, skipping update", "name", a.id.String())
+		return nil
+	}
+
+	getReq := &pb.GetPolicyRequest{
+		Name: a.id.String(),
+	}
+	existing, err := a.gcpClient.GetPolicy(ctx, getReq)
+	if err != nil {
+		return fmt.Errorf("getting IAMDenyPolicy %q before update: %w", a.id.String(), err)
+	}
+	desiredProto.Etag = existing.Etag
+
+	req := &pb.UpdatePolicyRequest{
+		Policy: desiredProto,
+	}
+
+	op, err := a.gcpClient.UpdatePolicy(ctx, req)
+	if err != nil {
+		return fmt.Errorf("updating IAMDenyPolicy %q: %w", a.id.String(), err)
+	}
+
+	updated, err := op.Wait(ctx)
+	if err != nil {
+		return fmt.Errorf("waiting for IAMDenyPolicy %q update: %w", a.id.String(), err)
+	}
+
+	log.V(2).Info("successfully updated IAMDenyPolicy", "name", a.id.String())
+
+	observedState := mappers.IAMDenyPolicyObservedState_FromProto(mapCtx, updated)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+
+	a.desired.Status.ObservedState = observedState
+	a.desired.Status.ExternalRef = direct.LazyPtr(a.id.String())
+
+	return nil
+}
+
+func (a *Adapter) Delete(ctx context.Context, deleteOp *directbase.DeleteOperation) (bool, error) {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("deleting IAMDenyPolicy", "name", a.id.String())
+
+	req := &pb.DeletePolicyRequest{
+		Name: a.id.String(),
+	}
+
+	op, err := a.gcpClient.DeletePolicy(ctx, req)
+	if err != nil {
+		if direct.IsNotFound(err) {
+			return false, nil
+		}
+		return false, fmt.Errorf("deleting IAMDenyPolicy %q: %w", a.id.String(), err)
+	}
+
+	_, err = op.Wait(ctx)
+	if err != nil {
+		if direct.IsNotFound(err) {
+			return false, nil
+		}
+		return false, fmt.Errorf("waiting for IAMDenyPolicy %q deletion: %w", a.id.String(), err)
+	}
+
+	log.V(2).Info("successfully deleted IAMDenyPolicy", "name", a.id.String())
+	return true, nil
+}
+
+func (a *Adapter) Export(ctx context.Context) (*unstructured.Unstructured, error) {
+	if a.actual == nil {
+		return nil, fmt.Errorf("Find() not called")
+	}
+	u := &unstructured.Unstructured{}
+
+	obj := &krm.IAMDenyPolicy{}
+	mapCtx := &direct.MapContext{}
+	obj.Spec = direct.ValueOf(mappers.IAMDenyPolicySpec_FromProto(mapCtx, a.actual))
+	if mapCtx.Err() != nil {
+		return nil, mapCtx.Err()
+	}
+
+	// Restore references
+	obj.Spec.ProjectRef = a.desired.Spec.ProjectRef
+	obj.Spec.FolderRef = a.desired.Spec.FolderRef
+	obj.Spec.OrganizationRef = a.desired.Spec.OrganizationRef
+
+	uObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+	if err != nil {
+		return nil, err
+	}
+	u.Object = uObj
+	return u, nil
+}
