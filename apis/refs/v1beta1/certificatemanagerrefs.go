4cf3f4feacc76b21674858649d74a669d7d69e1d
codebot-robot <codebot-robot@google.com>
2026-05-28 12:59:19 +0000

    fix: address reviewer feedback for NetworkSecurityBackendAuthenticationConfig
    - Changed `Location` to a pointer.
    - Changed `ClientCertificate` and `TrustConfig` to use references (`CertificateManagerCertificateRef`, `CertificateManagerTrustConfigRef`).
    - Added missing reference structs in `apis/refs/v1beta1/certificatemanagerrefs.go`.
    - Fixed `dev/tools/controllerbuilder/generate-proto.sh` proto collision and deleted redundant `mockgcp` proto to resolve compilation issues.
    - Updated golden files in `tests/apichecks`.

---
+++ apis/refs/v1beta1/certificatemanagerrefs.go
@@ -0,0 +1,43 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+// CertificateManagerCertificateRef defines the resource reference to CertificateManagerCertificate, which "External" field
+// holds the GCP identifier for the KRM object.
+type CertificateManagerCertificateRef struct {
+	// A reference to an externally managed CertificateManagerCertificate resource.
+	// Should be in the format "projects/{{projectID}}/locations/{{location}}/certificates/{{certificate}}".
+	External string `json:"external,omitempty"`
+
+	// The name of a CertificateManagerCertificate resource.
+	Name string `json:"name,omitempty"`
+
+	// The namespace of a CertificateManagerCertificate resource.
+	Namespace string `json:"namespace,omitempty"`
+}
+
+// CertificateManagerTrustConfigRef defines the resource reference to CertificateManagerTrustConfig, which "External" field
+// holds the GCP identifier for the KRM object.
+type CertificateManagerTrustConfigRef struct {
+	// A reference to an externally managed CertificateManagerTrustConfig resource.
+	// Should be in the format "projects/{{projectID}}/locations/{{location}}/trustConfigs/{{trustConfig}}".
+	External string `json:"external,omitempty"`
+
+	// The name of a CertificateManagerTrustConfig resource.
+	Name string `json:"name,omitempty"`
+
+	// The namespace of a CertificateManagerTrustConfig resource.
+	Namespace string `json:"namespace,omitempty"`
+}
