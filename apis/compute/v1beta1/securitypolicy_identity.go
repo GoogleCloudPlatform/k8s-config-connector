899a01c0e1b6a1ef17a93ce3e7c99042965db36c
codebot-robot <codebot-robot@google.com>
2026-05-29 01:19:47 +0000

    Move ComputeSecurityPolicy to identity and refs pattern
    This PR implements the identity and refs pattern for the
    `ComputeSecurityPolicy` resource.
    
    It defines:
    - `ComputeSecurityPolicyIdentity` struct implementing `identity.IdentityV2` using `gcpurls.Template`.
    - `ComputeSecurityPolicyRef` struct implementing `refs.Ref`.
    
    This PR was generated by **Overseer** (powered by the gemini-3.5-flash model).
    
    Fixes #8814

---
+++ apis/compute/v1beta1/securitypolicy_identity.go
@@ -0,0 +1,100 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common/identity"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/gcpurls"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var (
+	_ identity.IdentityV2 = &ComputeSecurityPolicyIdentity{}
+	_ identity.Resource   = &ComputeSecurityPolicy{}
+)
+
+var ComputeSecurityPolicyIdentityFormat = gcpurls.Template[ComputeSecurityPolicyIdentity]("compute.googleapis.com", "projects/{project}/global/securityPolicies/{name}")
+
+// +k8s:deepcopy-gen=false
+type ComputeSecurityPolicyIdentity struct {
+	Project string
+	Name    string
+}
+
+func (i *ComputeSecurityPolicyIdentity) String() string {
+	return ComputeSecurityPolicyIdentityFormat.ToString(*i)
+}
+
+func (i *ComputeSecurityPolicyIdentity) FromExternal(ref string) error {
+	parsed, match, err := ComputeSecurityPolicyIdentityFormat.Parse(ref)
+	if err != nil {
+		return fmt.Errorf("format of ComputeSecurityPolicy external=%q was not known (use %s): %w", ref, ComputeSecurityPolicyIdentityFormat.CanonicalForm(), err)
+	}
+	if !match {
+		return fmt.Errorf("format of ComputeSecurityPolicy external=%q was not known (use %s)", ref, ComputeSecurityPolicyIdentityFormat.CanonicalForm())
+	}
+
+	*i = *parsed
+	return nil
+}
+
+func (i *ComputeSecurityPolicyIdentity) Host() string {
+	return ComputeSecurityPolicyIdentityFormat.Host()
+}
+
+func getIdentityFromComputeSecurityPolicySpec(ctx context.Context, reader client.Reader, obj client.Object) (*ComputeSecurityPolicyIdentity, error) {
+	resourceID, err := refs.GetResourceID(obj)
+	if err != nil {
+		return nil, fmt.Errorf("cannot resolve resource ID: %w", err)
+	}
+
+	projectID, err := refs.ResolveProjectID(ctx, reader, obj)
+	if err != nil {
+		return nil, fmt.Errorf("cannot resolve project: %w", err)
+	}
+
+	identity := &ComputeSecurityPolicyIdentity{
+		Project: projectID,
+		Name:    resourceID,
+	}
+	return identity, nil
+}
+
+func (obj *ComputeSecurityPolicy) GetIdentity(ctx context.Context, reader client.Reader) (identity.Identity, error) {
+	specIdentity, err := getIdentityFromComputeSecurityPolicySpec(ctx, reader, obj)
+	if err != nil {
+		return nil, err
+	}
+
+	// Cross-check the identity against the status value, if present.
+	externalRef := common.ValueOf(obj.Status.SelfLink)
+	if externalRef != "" {
+		statusIdentity := &ComputeSecurityPolicyIdentity{}
+		if err := statusIdentity.FromExternal(externalRef); err != nil {
+			return nil, err
+		}
+
+		if statusIdentity.String() != specIdentity.String() {
+			return nil, fmt.Errorf("cannot change ComputeSecurityPolicy identity (old=%q, new=%q)", statusIdentity.String(), specIdentity.String())
+		}
+	}
+
+	return specIdentity, nil
+}
