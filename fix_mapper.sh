c883dfd87b0833f299ce6f8ff393cbc5bf1ef99c
codebot-robot <codebot-robot@google.com>
2026-06-01 23:16:07 +0000

    Fix presubmit tests for NetworkSecurityUrlList
    This commit fixes the following issues:
    1. Regenerates missing Go Clients doc.go file by running `make generate-go-client ensure fmt`
    2. Adds missing fields and acronym exceptions to apichecks testdata using `WRITE_GOLDEN_OUTPUT=1 go test ./tests/apichecks/...`

--- fix_mapper.sh
+++
@@ -1,3 +0,0 @@
-sed -i '/<<<<<<< HEAD/d' pkg/controller/direct/compute/mapper.generated.go
-sed -i '/=======/d' pkg/controller/direct/compute/mapper.generated.go
-sed -i '/>>>>>>> 078173d901 (Fix ComputeHealthCheck reference resolution by moving SelfLink and Type back to status)/d' pkg/controller/direct/compute/mapper.generated.go
