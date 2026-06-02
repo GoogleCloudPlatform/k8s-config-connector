2bbab7175de4a04698a4dc1a864f877d34ef439b
codebot-robot <codebot-robot@google.com>
2026-06-02 04:23:54 +0000

    Remove temporary scripts

--- fix_gen2.py
+++
@@ -1,8 +0,0 @@
-import re
-with open("apis/networksecurity/v1alpha1/generate.sh", "r") as f:
-    content = f.read()
-
-content = re.sub(r'<<<<<<< HEAD\ncd \$\{REPO_ROOT\}\n=======\n(.*?)\n>>>>>>> [^\n]+\n', r'\1\n', content, flags=re.DOTALL)
-
-with open("apis/networksecurity/v1alpha1/generate.sh", "w") as f:
-    f.write(content)
