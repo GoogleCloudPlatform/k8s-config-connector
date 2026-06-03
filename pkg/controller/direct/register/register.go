988de31984ca6bd483f204757e00ff9da9aae511
codebot-robot <codebot-robot@google.com>
2026-06-03 00:59:56 +0000

    Address review feedback: remove Description field, implement direct controller, and drop unrelated changes
    * Remove the Description field from SecurityCenterManagementEventThreatDetectionCustomModuleSpec
    * Provide the direct reconciler implementation and mappers for the resource
    * Revert all unrelated changes to keep the PR focused

--- pkg/controller/direct/register/register.go
+++ pkg/controller/direct/register/register.go
@@ -89,6 +89,7 @@ import (
 	_ "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/secretmanager"
 	_ "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/securesourcemanager"
 	_ "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/securitycenter/bigqueryexport"
+	_ "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/securitycentermanagement"
 	_ "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/servicenetworking"
 	_ "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/serviceusage"
 	_ "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/spanner"
