0b4ac562c46c97bac3a48367632710eda15b1305
codebot-robot <codebot-robot@google.com>
2026-06-03 11:17:24 +0000

    feat: implement direct controller for LoggingLogView
    Implement Phase 2 for LoggingLogView: Controller and E2E Tests.
    
    - Scaffolds direct controller in `pkg/controller/direct/logging/logginglogview_controller.go`.
    - Configures default reconciler to `direct` using labels in `apis/logging/v1beta1/logview_types.go`.
    - Regenerates static config to map LoggingLogView to the new direct reconciler.
    - Updates golden files and records e2e test execution successfully against MockGCP.
    
    Fixes #9061

---
+++ pkg/controller/direct/logging/logginglogview_controller.go
@@ -0,0 +1,247 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package logging
+
+import (
+	"context"
+	"fmt"
+
+	krm "github.com/GoogleCloudPlatform/k8s-config-connector/apis/logging/v1beta1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/config"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/common"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/directbase"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/controller/direct/registry"
+
+	gcp "cloud.google.com/go/logging/apiv2"
+	loggingpb "cloud.google.com/go/logging/apiv2/loggingpb"
+	"google.golang.org/api/option"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/klog/v2"
+)
+
+func init() {
+	registry.RegisterModel(krm.LoggingLogViewGVK, NewLoggingLogViewModel)
+}
+
+func NewLoggingLogViewModel(ctx context.Context, config *config.ControllerConfig) (directbase.Model, error) {
+	return &modelLoggingLogView{config: *config}, nil
+}
+
+var _ directbase.Model = &modelLoggingLogView{}
+
+type modelLoggingLogView struct {
+	config config.ControllerConfig
+}
+
+func (m *modelLoggingLogView) client(ctx context.Context) (*gcp.ConfigClient, error) {
+	// Use the VCR-aware HTTP client
+	httpClient, err := m.config.NewAuthenticatedHTTPClient(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("building authenticated HTTP client: %w", err)
+	}
+
+	gcpClient, err := gcp.NewConfigRESTClient(ctx, option.WithHTTPClient(httpClient))
+	if err != nil {
+		return nil, fmt.Errorf("building Logging Config client: %w", err)
+	}
+	return gcpClient, err
+}
+
+func (m *modelLoggingLogView) AdapterForObject(ctx context.Context, op *directbase.AdapterForObjectOperation) (directbase.Adapter, error) {
+	u := op.GetUnstructured()
+	reader := op.Reader
+	obj := &krm.LoggingLogView{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, &obj); err != nil {
+		return nil, fmt.Errorf("error converting to %T: %w", obj, err)
+	}
+
+	id, err := obj.GetIdentity(ctx, reader)
+	if err != nil {
+		return nil, err
+	}
+
+	gcpClient, err := m.client(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &LoggingLogViewAdapter{
+		id:        id.(*krm.LoggingLogViewIdentity),
+		gcpClient: gcpClient,
+		desired:   obj,
+	}, nil
+}
+
+func (m *modelLoggingLogView) AdapterForURL(ctx context.Context, url string) (directbase.Adapter, error) {
+	// TODO: Support URLs
+	return nil, nil
+}
+
+type LoggingLogViewAdapter struct {
+	id        *krm.LoggingLogViewIdentity
+	gcpClient *gcp.ConfigClient
+	desired   *krm.LoggingLogView
+	actual    *loggingpb.LogView
+}
+
+var _ directbase.Adapter = &LoggingLogViewAdapter{}
+
+func (a *LoggingLogViewAdapter) Find(ctx context.Context) (bool, error) {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("getting LoggingLogView", "name", a.id)
+
+	req := &loggingpb.GetViewRequest{Name: a.id.String()}
+	viewpb, err := a.gcpClient.GetView(ctx, req)
+	if err != nil {
+		if direct.IsNotFound(err) {
+			return false, nil
+		}
+		return false, fmt.Errorf("getting LoggingLogView %q: %w", a.id, err)
+	}
+
+	a.actual = viewpb
+	return true, nil
+}
+
+func (a *LoggingLogViewAdapter) Create(ctx context.Context, createOp *directbase.CreateOperation) error {
+	u := createOp.GetUnstructured()
+	log := klog.FromContext(ctx)
+	log.V(2).Info("creating LoggingLogView", "u", u)
+
+	mapCtx := &direct.MapContext{}
+
+	desired := a.desired.DeepCopy()
+	resource := LoggingLogViewSpec_ToProto(mapCtx, &desired.Spec)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+
+	bucketIdentity := &krm.LogBucketIdentity{
+		Project:        a.id.Project,
+		Folder:         a.id.Folder,
+		Organization:   a.id.Organization,
+		BillingAccount: a.id.BillingAccount,
+		Location:       a.id.Location,
+		Bucket:         a.id.Bucket,
+	}
+	parent := bucketIdentity.String()
+
+	resourceID := direct.ValueOf(desired.Spec.ResourceID)
+	if resourceID == "" {
+		log.V(2).Info("ResourceID is not set, will use metadata.name")
+		resourceID = desired.Name
+	} else {
+		log.V(2).Info("ResourceID is set, will use")
+		resourceID = *desired.Spec.ResourceID
+	}
+
+	req := &loggingpb.CreateViewRequest{
+		Parent: parent,
+		View:   resource,
+		ViewId: resourceID,
+	}
+	created, err := a.gcpClient.CreateView(ctx, req)
+	if err != nil {
+		return fmt.Errorf("creating LogView %s: %w", a.id, err)
+	}
+	log.V(2).Info("successfully created LogView", "name", a.id)
+
+	status := LoggingLogViewStatus_FromProto(mapCtx, created)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+	return createOp.UpdateStatus(ctx, status, nil)
+}
+
+func (a *LoggingLogViewAdapter) Update(ctx context.Context, updateOp *directbase.UpdateOperation) error {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("updating LoggingLogView", "name", a.id.String())
+	mapCtx := &direct.MapContext{}
+
+	desiredPb := LoggingLogViewSpec_ToProto(mapCtx, &a.desired.DeepCopy().Spec)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+	desiredPb.Name = a.id.String()
+
+	paths, err := common.CompareProtoMessage(desiredPb, a.actual, common.BasicDiff)
+	if err != nil {
+		return err
+	}
+	if len(paths) == 0 {
+		log.V(2).Info("no field needs update", "name", a.id.String())
+		return nil
+	}
+
+	updateMask := &fieldmaskpb.FieldMask{}
+	for path := range paths {
+		updateMask.Paths = append(updateMask.Paths, path)
+	}
+
+	req := &loggingpb.UpdateViewRequest{
+		Name:       a.id.String(),
+		View:       desiredPb,
+		UpdateMask: updateMask,
+	}
+
+	updated, err := a.gcpClient.UpdateView(ctx, req)
+	if err != nil {
+		return fmt.Errorf("updating LoggingLogView %s: %w", a.id.String(), err)
+	}
+
+	status := LoggingLogViewStatus_FromProto(mapCtx, updated)
+	if mapCtx.Err() != nil {
+		return mapCtx.Err()
+	}
+	return updateOp.UpdateStatus(ctx, status, nil)
+}
+
+func (a *LoggingLogViewAdapter) Export(ctx context.Context) (*unstructured.Unstructured, error) {
+	if a.actual == nil {
+		return nil, fmt.Errorf("Find() not called")
+	}
+	u := &unstructured.Unstructured{}
+
+	obj := &krm.LoggingLogView{}
+	mapCtx := &direct.MapContext{}
+	obj.Spec = direct.ValueOf(LoggingLogViewSpec_FromProto(mapCtx, a.actual))
+	if mapCtx.Err() != nil {
+		return nil, mapCtx.Err()
+	}
+
+	u.SetName(a.actual.Name)
+	u.SetGroupVersionKind(krm.LoggingLogViewGVK)
+	return u, nil
+}
+
+func (a *LoggingLogViewAdapter) Delete(ctx context.Context, deleteOp *directbase.DeleteOperation) (bool, error) {
+	log := klog.FromContext(ctx)
+	log.V(2).Info("deleting LogView", "name", a.id)
+
+	req := &loggingpb.DeleteViewRequest{Name: a.id.String()}
+	err := a.gcpClient.DeleteView(ctx, req)
+	if err != nil {
+		if direct.IsNotFound(err) {
+			log.V(2).Info("skipping delete for non-existent LogView, assuming it was already deleted", "name", a.id.String())
+			return true, nil
+		}
+		return false, fmt.Errorf("deleting LogView %s: %w", a.id, err)
+	}
+	log.V(2).Info("successfully deleted LogView", "name", a.id)
+	return true, nil
+}
