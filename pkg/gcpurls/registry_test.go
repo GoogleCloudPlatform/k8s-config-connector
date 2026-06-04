e6bbd259199a36add211b4e974f9344eae36fb53
codebot-robot <codebot-robot@google.com>
2026-06-04 17:36:14 +0000

    feat: Implement direct controller and KRM types for CloudRunInstance

--- pkg/gcpurls/registry_test.go
+++ pkg/gcpurls/registry_test.go
@@ -121,7 +121,8 @@ func TestRegisteredTemplatesMatchCAI(t *testing.T) {
 		"//monitoring.googleapis.com/locations/global/metricsScopes/{}/projects/{}":                 true,
 		"//artifactregistry.googleapis.com/projects/{}/locations/{}/vpcscConfig":                    true,
 		"//iap.googleapis.com/projects/{}/brands/{}":                                                true,
-		"//serviceusage.googleapis.com/projects/{}/services/{}/identity":                            true,
+"//serviceusage.googleapis.com/projects/{}/services/{}/identity":                            true,
+"//run.googleapis.com/projects/{}/locations/{}/instances/{}":                                true,
 	}
 	for _, tmpl := range templates {
 		fullURL := "//" + tmpl.Host() + "/" + tmpl.CanonicalForm()
