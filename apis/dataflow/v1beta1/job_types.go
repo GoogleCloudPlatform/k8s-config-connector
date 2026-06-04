9117d1dd17896f4f0318118ee26541b55233b1cb
codebot-robot <codebot-robot@google.com>
2026-06-04 10:32:00 +0000

    feat: implement direct KRM types and generate.sh for DataflowJob
    This adds direct KRM types and configures generate.sh for DataflowJob, keeping them strictly compatible with the baseline CRD schema.

---
+++ apis/dataflow/v1beta1/job_types.go
@@ -0,0 +1,145 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+import (
+	computev1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/compute/v1beta1"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/apis/k8s/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtime "k8s.io/apimachinery/pkg/runtime"
+)
+
+var DataflowJobGVK = GroupVersion.WithKind("DataflowJob")
+
+// DataflowJobSpec defines the desired state of DataflowJob
+type DataflowJobSpec struct {
+	/* List of experiments that should be used by the job. An example value is ["enable_stackdriver_agent_metrics"]. */
+	// +optional
+	AdditionalExperiments []string `json:"additionalExperiments,omitempty"`
+
+	/* Indicates if the job should use the streaming engine feature. */
+	// +optional
+	EnableStreamingEngine *bool `json:"enableStreamingEngine,omitempty"`
+
+	/* The configuration for VM IPs. Options are "WORKER_IP_PUBLIC" or "WORKER_IP_PRIVATE". */
+	// +optional
+	IpConfiguration *string `json:"ipConfiguration,omitempty"`
+
+	/* The name for the Cloud KMS key for the job. */
+	// +optional
+	KmsKeyRef *refs.KMSCryptoKeyRef `json:"kmsKeyRef,omitempty"`
+
+	/* The machine type to use for the job. */
+	// +optional
+	MachineType *string `json:"machineType,omitempty"`
+
+	/* Immutable. The number of workers permitted to work on the job. More workers may improve processing speed at additional cost. */
+	// +optional
+	MaxWorkers *int64 `json:"maxWorkers,omitempty"`
+
+	// +optional
+	NetworkRef *computev1beta1.ComputeNetworkRef `json:"networkRef,omitempty"`
+
+	/* Key/Value pairs to be passed to the Dataflow job (as used in the template). */
+	// +optional
+	Parameters *runtime.RawExtension `json:"parameters,omitempty"`
+
+	/* Immutable. The region in which the created job should run. */
+	// +optional
+	Region *string `json:"region,omitempty"`
+
+	/* Immutable. Optional. The name of the resource. Used for creation and acquisition. When unset, the value of `metadata.name` is used as the default. */
+	// +optional
+	ResourceID *string `json:"resourceID,omitempty"`
+
+	// +optional
+	ServiceAccountRef *refs.IAMServiceAccountRef `json:"serviceAccountRef,omitempty"`
+
+	// +optional
+	SubnetworkRef *refs.ComputeSubnetworkRef `json:"subnetworkRef,omitempty"`
+
+	/* A writeable location on Google Cloud Storage for the Dataflow job to dump its temporary data. */
+	TempGcsLocation string `json:"tempGcsLocation"`
+
+	/* The Google Cloud Storage path to the Dataflow job template. */
+	TemplateGcsPath string `json:"templateGcsPath"`
+
+	/* Only applicable when updating a pipeline. Map of transform name prefixes of the job to be replaced with the corresponding name prefixes of the new job. */
+	// +optional
+	TransformNameMapping *runtime.RawExtension `json:"transformNameMapping,omitempty"`
+
+	/* Immutable. The zone in which the created job should run. If it is not provided, the provider zone is used. */
+	// +optional
+	Zone *string `json:"zone,omitempty"`
+}
+
+// DataflowJobStatus defines the config connector machine state of DataflowJob
+type DataflowJobStatus struct {
+	/* Conditions represent the latest available observations of the
+	   DataflowJob's current state. */
+	Conditions []v1alpha1.Condition `json:"conditions,omitempty"`
+
+	/* The unique ID of this job. */
+	// +optional
+	JobId *string `json:"jobId,omitempty"`
+
+	/* ObservedGeneration is the generation of the resource that was most recently observed by the Config Connector controller. If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource. */
+	// +optional
+	ObservedGeneration *int64 `json:"observedGeneration,omitempty"`
+
+	/* The current state of the resource, selected from the JobState enum. */
+	// +optional
+	State *string `json:"state,omitempty"`
+
+	/* The type of this job, selected from the JobType enum. */
+	// +optional
+	Type *string `json:"type,omitempty"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:resource:categories=gcp,shortName=gcpdataflowjob;gcpdataflowjobs
+// +kubebuilder:subresource:status
+// +kubebuilder:metadata:labels="cnrm.cloud.google.com/managed-by-kcc=true"
+// +kubebuilder:metadata:labels="cnrm.cloud.google.com/stability-level=stable"
+// +kubebuilder:metadata:labels="cnrm.cloud.google.com/system=true"
+// +kubebuilder:metadata:labels="cnrm.cloud.google.com/tf2crd=true"
+// +kubebuilder:printcolumn:name="Age",JSONPath=".metadata.creationTimestamp",type="date"
+// +kubebuilder:printcolumn:name="Ready",JSONPath=".status.conditions[?(@.type=='Ready')].status",type="string",description="When 'True', the most recent reconcile of the resource succeeded"
+// +kubebuilder:printcolumn:name="Status",JSONPath=".status.conditions[?(@.type=='Ready')].reason",type="string",description="The reason for the value in 'Ready'"
+// +kubebuilder:printcolumn:name="Status Age",JSONPath=".status.conditions[?(@.type=='Ready')].lastTransitionTime",type="date",description="The last transition time for the value in 'Status'"
+
+// DataflowJob is the Schema for the dataflow API
+// +k8s:openapi-gen=true
+type DataflowJob struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DataflowJobSpec   `json:"spec,omitempty"`
+	Status DataflowJobStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// DataflowJobList contains a list of DataflowJob
+type DataflowJobList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []DataflowJob `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&DataflowJob{}, &DataflowJobList{})
+}
