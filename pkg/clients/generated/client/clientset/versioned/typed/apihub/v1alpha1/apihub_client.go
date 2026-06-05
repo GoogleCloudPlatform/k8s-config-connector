bb85391548cbddb71cb22a02e8722271c815b24f
codebot-robot <codebot-robot@google.com>
2026-06-05 04:35:27 +0000

    ai: chore: Implement direct types for: APIHubPlugin
    This PR implements the initial KRM types, CRD, and IdentityV2 for the
    APIHubPlugin resource under apis/apihub/v1alpha1/.
    
    - Added apis/apihub/v1alpha1/plugin_types.go defining the APIHubPlugin KRM types.
    - Added apis/apihub/v1alpha1/apihubplugin_identity.go implementing identity.IdentityV2.
    - Added apis/apihub/v1alpha1/apihubplugin_identity_test.go with unit tests for identity.
    - Regenerated manifests and mappers.
    
    Fixes #9254

--- pkg/clients/generated/client/clientset/versioned/typed/apihub/v1alpha1/apihub_client.go
+++ pkg/clients/generated/client/clientset/versioned/typed/apihub/v1alpha1/apihub_client.go
@@ -33,6 +33,7 @@ type ApihubV1alpha1Interface interface {
 	RESTClient() rest.Interface
 	APIHubAPIsGetter
 	APIHubDeploymentsGetter
+	APIHubPluginsGetter
 	APIHubRuntimeProjectAttachmentsGetter
 }
 
@@ -47,6 +48,10 @@ func (c *ApihubV1alpha1Client) APIHubAPIs(namespace string) APIHubAPIInterface {
 
 func (c *ApihubV1alpha1Client) APIHubDeployments(namespace string) APIHubDeploymentInterface {
 	return newAPIHubDeployments(c, namespace)
+}
+
+func (c *ApihubV1alpha1Client) APIHubPlugins(namespace string) APIHubPluginInterface {
+	return newAPIHubPlugins(c, namespace)
 }
 
 func (c *ApihubV1alpha1Client) APIHubRuntimeProjectAttachments(namespace string) APIHubRuntimeProjectAttachmentInterface {
