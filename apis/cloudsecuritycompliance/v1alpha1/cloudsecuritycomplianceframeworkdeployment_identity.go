97c31aa96f1a9d9f5c715477d897dfeaf21a92fc
codebot-robot <codebot-robot@google.com>
2026-06-05 04:26:47 +0000

    ai:chore: Implement direct types for: CloudSecurityComplianceFrameworkDeployment
    Scaffolds the initial KRM types, CRD, and IdentityV2 for CloudSecurityComplianceFrameworkDeployment under cloudsecuritycompliance.cnrm.cloud.google.com/v1alpha1.
    
    Fixes #9257

---
+++ apis/cloudsecuritycompliance/v1alpha1/cloudsecuritycomplianceframeworkdeployment_identity.go
@@ -0,0 +1,124 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common/identity"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/gcpurls"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var (
+	_ identity.IdentityV2 = &CloudSecurityComplianceFrameworkDeploymentIdentity{}
+	_ identity.Resource   = &CloudSecurityComplianceFrameworkDeployment{}
+)
+
+var CloudSecurityComplianceFrameworkDeploymentIdentityFormat = gcpurls.Template[CloudSecurityComplianceFrameworkDeploymentIdentity]("cloudsecuritycompliance.googleapis.com", "organizations/{organization}/locations/{location}/frameworkDeployments/{framework_deployment}")
+
+// +k8s:deepcopy-gen=false
+type CloudSecurityComplianceFrameworkDeploymentIdentity struct {
+	Organization        string
+	Location            string
+	FrameworkDeployment string
+}
+
+func (i *CloudSecurityComplianceFrameworkDeploymentIdentity) String() string {
+	return CloudSecurityComplianceFrameworkDeploymentIdentityFormat.ToString(*i)
+}
+
+func (i *CloudSecurityComplianceFrameworkDeploymentIdentity) FromExternal(ref string) error {
+	parsed, match, err := CloudSecurityComplianceFrameworkDeploymentIdentityFormat.Parse(ref)
+	if err != nil {
+		return fmt.Errorf("format of CloudSecurityComplianceFrameworkDeployment external=%q was not known (use %s): %w", ref, CloudSecurityComplianceFrameworkDeploymentIdentityFormat.CanonicalForm(), err)
+	}
+	if !match {
+		return fmt.Errorf("format of CloudSecurityComplianceFrameworkDeployment external=%q was not known (use %s)", ref, CloudSecurityComplianceFrameworkDeploymentIdentityFormat.CanonicalForm())
+	}
+
+	*i = *parsed
+	return nil
+}
+
+func (i *CloudSecurityComplianceFrameworkDeploymentIdentity) Host() string {
+	return CloudSecurityComplianceFrameworkDeploymentIdentityFormat.Host()
+}
+
+func getIdentityFromCloudSecurityComplianceFrameworkDeploymentSpec(ctx context.Context, reader client.Reader, obj client.Object) (*CloudSecurityComplianceFrameworkDeploymentIdentity, error) {
+	resourceID, err := refs.GetResourceID(obj)
+	if err != nil {
+		return nil, fmt.Errorf("cannot resolve resource ID")
+	}
+
+	location, err := refs.GetLocation(obj)
+	if err != nil {
+		return nil, fmt.Errorf("cannot resolve location: %w", err)
+	}
+
+	organizationID, err := resolveFrameworkDeploymentOrganizationID(ctx, reader, obj)
+	if err != nil {
+		return nil, fmt.Errorf("cannot resolve organization: %w", err)
+	}
+
+	identity := &CloudSecurityComplianceFrameworkDeploymentIdentity{
+		Organization:        organizationID,
+		Location:            location,
+		FrameworkDeployment: resourceID,
+	}
+	return identity, nil
+}
+
+func resolveFrameworkDeploymentOrganizationID(ctx context.Context, reader client.Reader, obj client.Object) (string, error) {
+	if fd, ok := obj.(*CloudSecurityComplianceFrameworkDeployment); ok {
+		if fd.Spec.OrganizationRef != nil {
+			org, err := refs.ResolveOrganization(ctx, reader, obj, fd.Spec.OrganizationRef)
+			if err != nil {
+				return "", err
+			}
+			return org.OrganizationID, nil
+		}
+	} else if u, ok := obj.(*unstructured.Unstructured); ok {
+		return refs.ResolveOrganizationID(ctx, reader, u)
+	}
+	return "", fmt.Errorf("organizationRef is required")
+}
+
+func (obj *CloudSecurityComplianceFrameworkDeployment) GetIdentity(ctx context.Context, reader client.Reader) (identity.Identity, error) {
+	specIdentity, err := getIdentityFromCloudSecurityComplianceFrameworkDeploymentSpec(ctx, reader, obj)
+	if err != nil {
+		return nil, err
+	}
+
+	// Cross-check the identity against the status value, if present.
+	externalRef := common.ValueOf(obj.Status.ExternalRef)
+	if externalRef != "" {
+		// Validate desired with actual
+		statusIdentity := &CloudSecurityComplianceFrameworkDeploymentIdentity{}
+		if err := statusIdentity.FromExternal(externalRef); err != nil {
+			return nil, err
+		}
+
+		if statusIdentity.String() != specIdentity.String() {
+			return nil, fmt.Errorf("cannot change CloudSecurityComplianceFrameworkDeployment identity (old=%q, new=%q)", statusIdentity.String(), specIdentity.String())
+		}
+	}
+
+	return specIdentity, nil
+}
