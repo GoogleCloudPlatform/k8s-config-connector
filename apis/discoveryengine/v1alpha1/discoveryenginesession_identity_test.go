089307f71d85e9e583b49777ede4a6f3e02dc163
codebot-robot <codebot-robot@google.com>
2026-06-05 04:08:03 +0000

    feat(discoveryengine): implement IdentityV2 and reference for DiscoveryEngineSession

---
+++ apis/discoveryengine/v1alpha1/discoveryenginesession_identity_test.go
@@ -0,0 +1,79 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+import (
+	"testing"
+)
+
+func TestDiscoveryEngineSessionIdentity_FromExternal(t *testing.T) {
+	tests := []struct {
+		name    string
+		ref     string
+		wantErr bool
+		want    *DiscoveryEngineSessionIdentity
+	}{
+		{
+			name: "valid reference",
+			ref:  "projects/my-project/locations/global/dataStores/my-store/sessions/my-session",
+			want: &DiscoveryEngineSessionIdentity{
+				Project:   "my-project",
+				Location:  "global",
+				Datastore: "my-store",
+				Session:   "my-session",
+			},
+		},
+		{
+			name:    "invalid reference format",
+			ref:     "invalid/format",
+			wantErr: true,
+		},
+		{
+			name: "full url",
+			ref:  "https://discoveryengine.googleapis.com/projects/my-project/locations/global/dataStores/my-store/sessions/my-session",
+			want: &DiscoveryEngineSessionIdentity{
+				Project:   "my-project",
+				Location:  "global",
+				Datastore: "my-store",
+				Session:   "my-session",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			i := &DiscoveryEngineSessionIdentity{}
+			err := i.FromExternal(tt.ref)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FromExternal() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !tt.wantErr {
+				if i.Project != tt.want.Project {
+					t.Errorf("Project = %v, want %v", i.Project, tt.want.Project)
+				}
+				if i.Location != tt.want.Location {
+					t.Errorf("Location = %v, want %v", i.Location, tt.want.Location)
+				}
+				if i.Datastore != tt.want.Datastore {
+					t.Errorf("Datastore = %v, want %v", i.Datastore, tt.want.Datastore)
+				}
+				if i.Session != tt.want.Session {
+					t.Errorf("Session = %v, want %v", i.Session, tt.want.Session)
+				}
+			}
+		})
+	}
+}
