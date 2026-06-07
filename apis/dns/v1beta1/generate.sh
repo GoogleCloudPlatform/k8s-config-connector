a7dacff6c494027721e1de1baf5babc63894bd33
codebot-robot <codebot-robot@google.com>
2026-06-07 01:37:28 +0000

    feat: implement direct KRM types and generate.sh for DNSPolicy
    Fixes #9472

--- apis/dns/v1beta1/generate.sh
+++ apis/dns/v1beta1/generate.sh
@@ -24,6 +24,7 @@ go run "${REPO_ROOT}/dev/tools/openapi-to-krm/main.go" \
   --schema-file "${REPO_ROOT}/apis/dns/v1beta1/dns-api.json" \
   --api-version "dns.cnrm.cloud.google.com/v1beta1" \
   --resource "DNSManagedZone:ManagedZone" \
+  --resource "DNSPolicy:Policy" \
   --ignore-field "*:kind" \
   --require-field "ManagedZoneCloudLoggingConfig:enableLogging,ManagedZoneForwardingConfig:targetNameServers,ManagedZonePeeringConfig:targetNetwork,ManagedZoneServiceDirectoryConfig:namespace" \
   --output-file "${REPO_ROOT}/apis/dns/v1beta1/types.generated.go"
@@ -32,6 +33,8 @@ go run "${REPO_ROOT}/dev/tools/openapi-to-krm/main.go" \
 go run "${REPO_ROOT}/dev/tools/openapi-to-krm/cmd/generate-mapper/main.go" \
   --mapper "github.com/GoogleCloudPlatform/k8s-config-connector/apis/dns/v1beta1/DNSManagedZoneSpec::google.golang.org/api/dns/v1/ManagedZone" \
   --mapper "github.com/GoogleCloudPlatform/k8s-config-connector/apis/dns/v1beta1/DNSManagedZoneStatus::google.golang.org/api/dns/v1/ManagedZone" \
+  --mapper "github.com/GoogleCloudPlatform/k8s-config-connector/apis/dns/v1beta1/DNSPolicySpec::google.golang.org/api/dns/v1/Policy" \
+  --mapper "github.com/GoogleCloudPlatform/k8s-config-connector/apis/dns/v1beta1/DNSPolicyStatus::google.golang.org/api/dns/v1/Policy" \
   --output-file "${REPO_ROOT}/pkg/controller/direct/dns/zz_generated.mappers.go"
 
 cd ${REPO_ROOT}
