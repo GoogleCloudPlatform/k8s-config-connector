cb1ad4efe051a342793bd25296d8d1fa6bab7446
codebot-robot <codebot-robot@google.com>
2026-06-08 17:07:08 +0000

    Implement round-trip KRM fuzzer for StorageBucket
    This PR implements a round-trip KRM fuzzer for StorageBucket under pkg/controller/direct/storage/bucket_fuzzer.go and registers the package in pkg/controller/direct/register/register.go so it executes within the central fuzz test suite.
    
    Key Changes:
    * StorageBucket Fuzzer: Created a round-trip spec fuzzer targeting pb.Bucket with StorageBucketSpec_FromProto and StorageBucketSpec_ToProto.
    * Field Registration: Marked mapped spec fields and explicitly marked unmapped/unimplemented/identity fields as unimplemented or not yet triaged to satisfy round-trip validation.
    * Controller Registration: Registered pkg/controller/direct/storage in alphabetical order in pkg/controller/direct/register/register.go.
    
    Fixes #9588

---
+++ pkg/controller/direct/storage/bucket_fuzzer.go
@@ -0,0 +1,68 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +tool:fuzz-gen
+// proto.message: google.storage.v1.Bucket
+// api.group: storage.cnrm.cloud.google.com
+
+package storage
+
+import (
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/fuzztesting"
+	pb "google.golang.org/genproto/googleapis/storage/v1"
+)
+
+func init() {
+	fuzztesting.RegisterKRMSpecFuzzer(StorageBucketFuzzer())
+}
+
+func StorageBucketFuzzer() fuzztesting.KRMFuzzer {
+	f := fuzztesting.NewKRMTypedSpecFuzzer(&pb.Bucket{},
+		StorageBucketSpec_FromProto, StorageBucketSpec_ToProto,
+	)
+
+	f.SpecField(".cors")
+	f.SpecField(".location")
+	f.SpecField(".storage_class")
+	f.SpecField(".default_event_based_hold")
+	f.SpecField(".website")
+	f.SpecField(".versioning")
+	f.SpecField(".logging")
+	f.SpecField(".encryption")
+	f.SpecField(".retention_policy")
+	f.SpecField(".autoclass")
+
+	f.Unimplemented_Identity(".name")
+	f.Unimplemented_Identity(".id")
+	f.Unimplemented_Identity(".project_number")
+	f.Unimplemented_LabelsAnnotations(".labels")
+	f.Unimplemented_Etag()
+
+	f.Unimplemented_NotYetTriaged(".acl")
+	f.Unimplemented_NotYetTriaged(".default_object_acl")
+	f.Unimplemented_NotYetTriaged(".lifecycle")
+	f.Unimplemented_NotYetTriaged(".time_created")
+	f.Unimplemented_NotYetTriaged(".metageneration")
+	f.Unimplemented_NotYetTriaged(".updated")
+	f.Unimplemented_NotYetTriaged(".owner")
+	f.Unimplemented_NotYetTriaged(".billing")
+	f.Unimplemented_NotYetTriaged(".location_type")
+	f.Unimplemented_NotYetTriaged(".iam_configuration")
+	f.Unimplemented_NotYetTriaged(".zone_affinity")
+	f.Unimplemented_NotYetTriaged(".satisfies_pzs")
+	f.Unimplemented_NotYetTriaged(".autoclass.toggle_time")
+	f.Unimplemented_NotYetTriaged(".retention_policy.effective_time")
+
+	return f
+}
