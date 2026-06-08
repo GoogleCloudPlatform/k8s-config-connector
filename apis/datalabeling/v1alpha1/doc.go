66728f0a43726d33ce5298adc83736a4a41f4c3a
codebot-robot <codebot-robot@google.com>
2026-06-08 22:06:38 +0000

    ai:chore: Implement direct types for DataLabelingAnnotationSpecSet
    This PR implements the direct types, CRD, and IdentityV2 for DataLabelingAnnotationSpecSet.
    
    ### Changes:
    - Created apis/datalabeling/v1alpha1/annotationspecset_types.go and _identity.go.
    - Implemented unit tests under apis/datalabeling/v1alpha1/annotationspecset_identity_test.go.
    - Generated the CRD for DataLabelingAnnotationSpecSet and compiled clients.
    - Added learning journal entry for datalabeling.
    
    Fixes #9267

--- apis/datalabeling/v1alpha1/doc.go
+++ apis/datalabeling/v1alpha1/doc.go
@@ -12,5 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +kcc:proto=google.cloud.datalabeling.v1beta1
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen=package,register
+// +groupName=datalabeling.cnrm.cloud.google.com
+
 package v1alpha1
