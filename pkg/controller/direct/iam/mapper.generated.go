3e1d511b4623b2f95dcc49c2b1a3b51378930909
Anh Le <anhdle@google.com>
2026-06-11 22:10:35 +0000

    chore: Regenerate DNS and IAM types and mappers after template update

--- pkg/controller/direct/iam/mapper.generated.go
+++ pkg/controller/direct/iam/mapper.generated.go
@@ -18,7 +18,7 @@
 
 // +generated:mapper
 // krm.group: iam.cnrm.cloud.google.com
-// krm.version: v1alpha1
-// proto.service: google.iam.v2
+// krm.version: v1beta1
+// proto.service: google.iam.admin.v1
 
 package iam
