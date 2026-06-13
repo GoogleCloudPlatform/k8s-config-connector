b77f0901dcd1e199d84af1ad80875c0c4272e9bf
codebot-robot <codebot-robot@google.com>
2026-06-13 03:30:16 +0000

    feat(compute): implement direct KRM types and generate.sh for ComputeRouterInterface

---
+++ apis/compute/v1beta1/routerinterface_types.go
@@ -0,0 +1,276 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+import (
+	"context"
+
+	refsv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/apis/k8s/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var (
+	ComputeRouterInterfaceGVK        = GroupVersion.WithKind("ComputeRouterInterface")
+	ComputeRouterGVK                 = GroupVersion.WithKind("ComputeRouter")
+	ComputeVPNTunnelGVK              = GroupVersion.WithKind("ComputeVPNTunnel")
+	ComputeInterconnectAttachmentGVK = GroupVersion.WithKind("ComputeInterconnectAttachment")
+)
+
+var _ refsv1beta1.Ref = &ComputeInterconnectAttachmentRef{}
+
+type ComputeInterconnectAttachmentRef struct {
+	/* Allowed value: The `name` field of a `ComputeInterconnectAttachment` resource. */
+	External string `json:"external,omitempty"`
+	/* Name of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names */
+	Name string `json:"name,omitempty"`
+	/* Namespace of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/namespaces/ */
+	Namespace string `json:"namespace,omitempty"`
+}
+
+func (r *ComputeInterconnectAttachmentRef) GetGVK() schema.GroupVersionKind {
+	return ComputeInterconnectAttachmentGVK
+}
+
+func (r *ComputeInterconnectAttachmentRef) GetNamespacedName() types.NamespacedName {
+	return types.NamespacedName{
+		Name:      r.Name,
+		Namespace: r.Namespace,
+	}
+}
+
+func (r *ComputeInterconnectAttachmentRef) GetExternal() string {
+	return r.External
+}
+
+func (r *ComputeInterconnectAttachmentRef) SetExternal(ref string) {
+	r.External = ref
+	r.Name = ""
+	r.Namespace = ""
+}
+
+func (r *ComputeInterconnectAttachmentRef) ValidateExternal(ref string) error {
+	return nil
+}
+
+func (r *ComputeInterconnectAttachmentRef) Normalize(ctx context.Context, reader client.Reader, defaultNamespace string) error {
+	return refsv1beta1.Normalize(ctx, reader, r, defaultNamespace)
+}
+
+var _ refsv1beta1.Ref = &ComputeVPNTunnelRef{}
+
+type ComputeVPNTunnelRef struct {
+	/* Allowed value: The `name` field of a `ComputeVPNTunnel` resource. */
+	External string `json:"external,omitempty"`
+	/* Name of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names */
+	Name string `json:"name,omitempty"`
+	/* Namespace of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/namespaces/ */
+	Namespace string `json:"namespace,omitempty"`
+}
+
+func (r *ComputeVPNTunnelRef) GetGVK() schema.GroupVersionKind {
+	return ComputeVPNTunnelGVK
+}
+
+func (r *ComputeVPNTunnelRef) GetNamespacedName() types.NamespacedName {
+	return types.NamespacedName{
+		Name:      r.Name,
+		Namespace: r.Namespace,
+	}
+}
+
+func (r *ComputeVPNTunnelRef) GetExternal() string {
+	return r.External
+}
+
+func (r *ComputeVPNTunnelRef) SetExternal(ref string) {
+	r.External = ref
+	r.Name = ""
+	r.Namespace = ""
+}
+
+func (r *ComputeVPNTunnelRef) ValidateExternal(ref string) error {
+	return nil
+}
+
+func (r *ComputeVPNTunnelRef) Normalize(ctx context.Context, reader client.Reader, defaultNamespace string) error {
+	return refsv1beta1.Normalize(ctx, reader, r, defaultNamespace)
+}
+
+var _ refsv1beta1.Ref = &ComputeRouterRef{}
+
+type ComputeRouterRef struct {
+	/* Allowed value: The `name` field of a `ComputeRouter` resource. */
+	External string `json:"external,omitempty"`
+	/* Name of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names */
+	Name string `json:"name,omitempty"`
+	/* Namespace of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/namespaces/ */
+	Namespace string `json:"namespace,omitempty"`
+}
+
+func (r *ComputeRouterRef) GetGVK() schema.GroupVersionKind {
+	return ComputeRouterGVK
+}
+
+func (r *ComputeRouterRef) GetNamespacedName() types.NamespacedName {
+	return types.NamespacedName{
+		Name:      r.Name,
+		Namespace: r.Namespace,
+	}
+}
+
+func (r *ComputeRouterRef) GetExternal() string {
+	return r.External
+}
+
+func (r *ComputeRouterRef) SetExternal(ref string) {
+	r.External = ref
+	r.Name = ""
+	r.Namespace = ""
+}
+
+func (r *ComputeRouterRef) ValidateExternal(ref string) error {
+	return nil
+}
+
+func (r *ComputeRouterRef) Normalize(ctx context.Context, reader client.Reader, defaultNamespace string) error {
+	return refsv1beta1.Normalize(ctx, reader, r, defaultNamespace)
+}
+
+var _ refsv1beta1.Ref = &ComputeRouterInterfaceRef{}
+
+type ComputeRouterInterfaceRef struct {
+	/* Allowed value: The `name` field of a `ComputeRouterInterface` resource. */
+	External string `json:"external,omitempty"`
+	/* Name of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names */
+	Name string `json:"name,omitempty"`
+	/* Namespace of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/namespaces/ */
+	Namespace string `json:"namespace,omitempty"`
+}
+
+func (r *ComputeRouterInterfaceRef) GetGVK() schema.GroupVersionKind {
+	return ComputeRouterInterfaceGVK
+}
+
+func (r *ComputeRouterInterfaceRef) GetNamespacedName() types.NamespacedName {
+	return types.NamespacedName{
+		Name:      r.Name,
+		Namespace: r.Namespace,
+	}
+}
+
+func (r *ComputeRouterInterfaceRef) GetExternal() string {
+	return r.External
+}
+
+func (r *ComputeRouterInterfaceRef) SetExternal(ref string) {
+	r.External = ref
+	r.Name = ""
+	r.Namespace = ""
+}
+
+func (r *ComputeRouterInterfaceRef) ValidateExternal(ref string) error {
+	return nil
+}
+
+func (r *ComputeRouterInterfaceRef) Normalize(ctx context.Context, reader client.Reader, defaultNamespace string) error {
+	return refsv1beta1.Normalize(ctx, reader, r, defaultNamespace)
+}
+
+// ComputeRouterInterfaceSpec defines the desired state of ComputeRouterInterface
+// +kcc:spec:proto=google.cloud.compute.v1.RouterInterface
+type ComputeRouterInterfaceSpec struct {
+	// +optional
+	LinkedInterconnectAttachmentRef *ComputeInterconnectAttachmentRef `json:"interconnectAttachmentRef,omitempty"`
+
+	/* Immutable. The IP address and range of the interface. The IP range must be in the RFC3927 link-local IP space. Changing this forces a new interface to be created. */
+	// +optional
+	IPRange *string `json:"ipRange,omitempty"`
+
+	// +optional
+	PrivateIPAddressRef *refsv1beta1.ComputeAddressRef `json:"privateIpAddressRef,omitempty"`
+
+	/* The interface the BGP peer is associated with. */
+	// +optional
+	RedundantInterfaceRef *ComputeRouterInterfaceRef `json:"redundantInterfaceRef,omitempty"`
+
+	/* Immutable. The region this interface's router sits in. If not specified, the project region will be used. Changing this forces a new interface to be created. */
+	Region string `json:"region"`
+
+	/* Immutable. Optional. The name of the resource. Used for creation and acquisition. When unset, the value of `metadata.name` is used as the default. */
+	// +optional
+	Name *string `json:"resourceID,omitempty"`
+
+	RouterRef ComputeRouterRef `json:"routerRef"`
+
+	// +optional
+	SubnetworkRef *ComputeSubnetworkRef `json:"subnetworkRef,omitempty"`
+
+	// +optional
+	LinkedVPNTunnelRef *ComputeVPNTunnelRef `json:"vpnTunnelRef,omitempty"`
+}
+
+// ComputeRouterInterfaceStatus defines the config connector machine state of ComputeRouterInterface
+type ComputeRouterInterfaceStatus struct {
+	/* Conditions represent the latest available observations of the
+	   object's current state. */
+	Conditions []v1alpha1.Condition `json:"conditions,omitempty"`
+
+	// ObservedGeneration is the generation of the resource that was most recently observed by the Config Connector controller. If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource.
+	ObservedGeneration *int64 `json:"observedGeneration,omitempty"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:resource:categories=gcp,shortName=gcpcomputerouterinterface;gcpcomputerouterinterfaces
+// +kubebuilder:subresource:status
+// +kubebuilder:metadata:labels="cnrm.cloud.google.com/managed-by-kcc=true"
+// +kubebuilder:metadata:labels="cnrm.cloud.google.com/stability-level=stable"
+// +kubebuilder:metadata:labels="cnrm.cloud.google.com/system=true"
+// +kubebuilder:metadata:labels="cnrm.cloud.google.com/tf2crd=true"
+// +kubebuilder:printcolumn:name="Age",JSONPath=".metadata.creationTimestamp",type="date"
+// +kubebuilder:printcolumn:name="Ready",JSONPath=".status.conditions[?(@.type=='Ready')].status",type="string",description="When 'True', the most recent reconcile of the resource succeeded"
+// +kubebuilder:printcolumn:name="Status",JSONPath=".status.conditions[?(@.type=='Ready')].reason",type="string",description="The reason for the value in 'Ready'"
+// +kubebuilder:printcolumn:name="Status Age",JSONPath=".status.conditions[?(@.type=='Ready')].lastTransitionTime",type="date",description="The last transition time for the value in 'Status'"
+
+// ComputeRouterInterface is the Schema for the ComputeRouterInterface API
+// +k8s:openapi-gen=true
+type ComputeRouterInterface struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// +required
+	Spec   ComputeRouterInterfaceSpec   `json:"spec,omitempty"`
+	Status ComputeRouterInterfaceStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// ComputeRouterInterfaceList contains a list of ComputeRouterInterface
+type ComputeRouterInterfaceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ComputeRouterInterface `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ComputeRouterInterface{}, &ComputeRouterInterfaceList{})
+	refsv1beta1.Register(&ComputeInterconnectAttachmentRef{})
+	refsv1beta1.Register(&ComputeVPNTunnelRef{})
+	refsv1beta1.Register(&ComputeRouterRef{})
+	refsv1beta1.Register(&ComputeRouterInterfaceRef{})
+}
