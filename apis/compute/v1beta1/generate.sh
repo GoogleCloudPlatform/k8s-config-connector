18d03595a653a6ee4d6d603e1d0d15aaaa428c00
codebot-robot <codebot-robot@google.com>
2026-06-13 10:50:21 +0000

    feat: implement direct KRM types and fuzzer for ComputeVPNGateway
    This implements direct KRM Go types, custom mappings, and a fuzzer for ComputeVPNGateway v1beta1, ensuring strict schema-compatibility.
    
    Issue #10138

--- apis/compute/v1beta1/generate.sh
+++ apis/compute/v1beta1/generate.sh
@@ -60,6 +60,7 @@ go run . generate-types \
   --resource ComputeTargetSSLProxy:TargetSslProxy \
   --resource ComputeTargetTcpProxy:TargetTcpProxy \
   --resource ComputeURLMap:UrlMap \
+  --resource ComputeVPNGateway:VpnGateway \
   --resource ComputeInterconnectAttachment:InterconnectAttachment \
   --include-skipped-output
 
