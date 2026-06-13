546d728f7c010d39f075578d678fa4988dbcda46
codebot-robot <codebot-robot@google.com>
2026-06-13 03:24:03 +0000

    chore: generate code for ComputeTargetHTTPProxy
    Regenerate client libraries, CRDs, deepcopy functions, and resource reports.

--- apis/compute/v1beta1/types.generated.go
+++ apis/compute/v1beta1/types.generated.go
@@ -49,6 +49,7 @@
 // resource: ComputeSSLCertificate:SslCertificate
 // resource: ComputeSubnetwork:Subnetwork
 // resource: ComputeTargetHTTPSProxy:TargetHttpsProxy
+// resource: ComputeTargetHTTPProxy:TargetHttpProxy
 // resource: ComputeTargetPool:TargetPool
 // resource: ComputeTargetSSLProxy:TargetSslProxy
 // resource: ComputeTargetTcpProxy:TargetTcpProxy
@@ -6352,6 +6353,56 @@ type Tags struct {
 	// An array of tags. Each tag must be 1-63 characters long, and comply with RFC1035.
 	// +kcc:proto:field=google.cloud.compute.v1.Tags.items
 	Items []string `json:"items,omitempty"`
+}
+*/
+
+/* found existing non-generated go type with proto tag "google.cloud.compute.v1.TargetHttpProxy", skipping
+
+// +kcc:proto=google.cloud.compute.v1.TargetHttpProxy
+type TargetHTTPProxy struct {
+	// [Output Only] Creation timestamp in RFC3339 text format.
+	// +kcc:proto:field=google.cloud.compute.v1.TargetHttpProxy.creation_timestamp
+	CreationTimestamp *string `json:"creationTimestamp,omitempty"`
+
+	// An optional description of this resource. Provide this property when you create the resource.
+	// +kcc:proto:field=google.cloud.compute.v1.TargetHttpProxy.description
+	Description *string `json:"description,omitempty"`
+
+	// Fingerprint of this resource. A hash of the contents stored in this object. This field is used in optimistic locking. This field will be ignored when inserting a TargetHttpProxy. An up-to-date fingerprint must be provided in order to patch/update the TargetHttpProxy; otherwise, the request will fail with error 412 conditionNotMet. To see the latest fingerprint, make a get() request to retrieve the TargetHttpProxy.
+	// +kcc:proto:field=google.cloud.compute.v1.TargetHttpProxy.fingerprint
+	Fingerprint *string `json:"fingerprint,omitempty"`
+
+	// Specifies how long to keep a connection open, after completing a response, while there is no matching traffic (in seconds). If an HTTP keep-alive is not specified, a default value (610 seconds) will be used. For global external Application Load Balancers, the minimum allowed value is 5 seconds and the maximum allowed value is 1200 seconds. For classic Application Load Balancers, this option is not supported.
+	// +kcc:proto:field=google.cloud.compute.v1.TargetHttpProxy.http_keep_alive_timeout_sec
+	HTTPKeepAliveTimeoutSec *int32 `json:"httpKeepAliveTimeoutSec,omitempty"`
+
+	// [Output Only] The unique identifier for the resource. This identifier is defined by the server.
+	// +kcc:proto:field=google.cloud.compute.v1.TargetHttpProxy.id
+	ID *uint64 `json:"id,omitempty"`
+
+	// [Output Only] Type of resource. Always compute#targetHttpProxy for target HTTP proxies.
+	// +kcc:proto:field=google.cloud.compute.v1.TargetHttpProxy.kind
+	Kind *string `json:"kind,omitempty"`
+
+	// Name of the resource. Provided by the client when the resource is created. The name must be 1-63 characters long, and comply with RFC1035. Specifically, the name must be 1-63 characters long and match the regular expression `[a-z]([-a-z0-9]*[a-z0-9])?` which means the first character must be a lowercase letter, and all following characters must be a dash, lowercase letter, or digit, except the last character, which cannot be a dash.
+	// +kcc:proto:field=google.cloud.compute.v1.TargetHttpProxy.name
+	Name *string `json:"name,omitempty"`
+
+	// This field only applies when the forwarding rule that references this target proxy has a loadBalancingScheme set to INTERNAL_SELF_MANAGED. When this field is set to true, Envoy proxies set up inbound traffic interception and bind to the IP address and port specified in the forwarding rule. This is generally useful when using Traffic Director to configure Envoy as a gateway or middle proxy (in other words, not a sidecar proxy). The Envoy proxy listens for inbound requests and handles requests when it receives them. The default is false.
+	// +kcc:proto:field=google.cloud.compute.v1.TargetHttpProxy.proxy_bind
+	ProxyBind *bool `json:"proxyBind,omitempty"`
+
+	// [Output Only] URL of the region where the regional Target HTTP Proxy resides. This field is not applicable to global Target HTTP Proxies.
+	// +kcc:proto:field=google.cloud.compute.v1.TargetHttpProxy.region
+	Region *string `json:"region,omitempty"`
+
+	// [Output Only] Server-defined URL for the resource.
+	// +kcc:proto:field=google.cloud.compute.v1.TargetHttpProxy.self_link
+	SelfLink *string `json:"selfLink,omitempty"`
+
+	// URL to the UrlMap resource that defines the mapping from URL to the BackendService.
+	// +kcc:proto:field=google.cloud.compute.v1.TargetHttpProxy.url_map
+	URLMap *string `json:"urlMap,omitempty"`
 }
 */
 
