449ef1fccc043b59100111756bace0894d1b2dd0
codebot-robot <codebot-robot@google.com>
2026-06-15 21:29:28 +0000

    ai: chore: Implement direct types for FinancialServicesInstance
    Implement direct KRM types, generate.sh, and IdentityV2 for FinancialServicesInstance.
    
    In this PR, we have:
    1. Configured apis/financialservices/v1alpha1/generate.sh to register FinancialServicesInstance:Instance.
    2. Scaffolded direct KRM types in apis/financialservices/v1alpha1/instance_types.go.
    3. Implemented IdentityV2 and ExternalIdentifier in apis/financialservices/v1alpha1/financialservicesinstance_identity.go.
    4. Added identity unit test in apis/financialservices/v1alpha1/financialservicesinstance_identity_test.go.
    
    Fixes #10268

---
+++ apis/financialservices/v1alpha1/financialservicesinstance_identity.go
@@ -0,0 +1,116 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common/identity"
+	refs "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/gcpurls"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var (
+	_ identity.IdentityV2 = &FinancialServicesInstanceIdentity{}
+	_ identity.Resource   = &FinancialServicesInstance{}
+)
+
+var FinancialServicesInstanceIdentityFormat = gcpurls.Template[FinancialServicesInstanceIdentity]("financialservices.googleapis.com", "projects/{project}/locations/{location}/instances/{instance}")
+
+// +k8s:deepcopy-gen=false
+type FinancialServicesInstanceIdentity struct {
+	Project  string
+	Location string
+	Instance string
+}
+
+func (i *FinancialServicesInstanceIdentity) String() string {
+	return FinancialServicesInstanceIdentityFormat.ToString(*i)
+}
+
+func (i *FinancialServicesInstanceIdentity) FromExternal(ref string) error {
+	parsed, match, err := FinancialServicesInstanceIdentityFormat.Parse(ref)
+	if err != nil {
+		return fmt.Errorf("format of FinancialServicesInstance external=%q was not known (use %s): %w", ref, FinancialServicesInstanceIdentityFormat.CanonicalForm(), err)
+	}
+	if !match {
+		return fmt.Errorf("format of FinancialServicesInstance external=%q was not known (use %s)", ref, FinancialServicesInstanceIdentityFormat.CanonicalForm())
+	}
+
+	*i = *parsed
+	return nil
+}
+
+func (i *FinancialServicesInstanceIdentity) Host() string {
+	return FinancialServicesInstanceIdentityFormat.Host()
+}
+
+func (i *FinancialServicesInstanceIdentity) ParentString() string {
+	return fmt.Sprintf("projects/%s/locations/%s", i.Project, i.Location)
+}
+
+func getIdentityFromFinancialServicesInstanceSpec(ctx context.Context, reader client.Reader, obj client.Object) (*FinancialServicesInstanceIdentity, error) {
+	resourceID, err := refs.GetResourceID(obj)
+	if err != nil {
+		return nil, fmt.Errorf("cannot resolve resource ID")
+	}
+
+	location, err := refs.GetLocation(obj)
+	if err != nil {
+		return nil, fmt.Errorf("cannot resolve location")
+	}
+
+	projectID, err := refs.ResolveProjectID(ctx, reader, obj)
+	if err != nil {
+		return nil, fmt.Errorf("cannot resolve project")
+	}
+
+	identity := &FinancialServicesInstanceIdentity{
+		Project:  projectID,
+		Location: location,
+		Instance: resourceID,
+	}
+	return identity, nil
+}
+
+func (obj *FinancialServicesInstance) GetIdentity(ctx context.Context, reader client.Reader) (identity.Identity, error) {
+	specIdentity, err := getIdentityFromFinancialServicesInstanceSpec(ctx, reader, obj)
+	if err != nil {
+		return nil, err
+	}
+
+	// Cross-check the identity against the status value, if present.
+	externalRef := common.ValueOf(obj.Status.ExternalRef)
+	if externalRef != "" {
+		// Validate desired with actual
+		statusIdentity := &FinancialServicesInstanceIdentity{}
+		if err := statusIdentity.FromExternal(externalRef); err != nil {
+			return nil, err
+		}
+
+		if statusIdentity.String() != specIdentity.String() {
+			return nil, fmt.Errorf("cannot change FinancialServicesInstance identity (old=%q, new=%q)", statusIdentity.String(), specIdentity.String())
+		}
+	}
+
+	return specIdentity, nil
+}
+
+func (obj *FinancialServicesInstance) ExternalIdentifier() *string {
+	return obj.Status.ExternalRef
+}
