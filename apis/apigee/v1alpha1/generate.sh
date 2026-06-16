f1bfb96ffbc4f33aa7e736924f8e4b48f5acdce9
codebot-robot <codebot-robot@google.com>
2026-06-16 19:31:56 +0000

    fixup! feat: Implement direct types and controller for ApigeeApiProduct Fixes #8061

--- apis/apigee/v1alpha1/generate.sh
+++ apis/apigee/v1alpha1/generate.sh
@@ -20,15 +20,10 @@ set -o pipefail
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 cd ${REPO_ROOT}/dev/tools/controllerbuilder
 
-# Use the pinned SHA from apis/git.versions
-PROTO_SHA="731d7f2ab6e4e2ea15030c95039e2cb66174d4fb" 
-PROTO_OUT="${REPO_ROOT}/.build/googleapis-${PROTO_SHA}.pb"
-
-./generate-proto.sh ${PROTO_SHA} ${PROTO_OUT}
+./generate-proto.sh
 
 go run . generate-types \
     --service mockgcp.cloud.apigee.v1 \
     --api-version apigee.cnrm.cloud.google.com/v1alpha1 \
     --resource ApigeeAPIProduct:GoogleCloudApigeeV1ApiProduct \
-    --proto-source-path ${PROTO_OUT} \
     --skip-scaffold-files
