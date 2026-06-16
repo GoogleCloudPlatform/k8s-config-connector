1bba7f502cb47df174403957dd8edb4f3a369e33
codebot-robot <codebot-robot@google.com>
2026-06-16 21:34:38 +0000

    fixup! apis/refs: Support identity and refs pattern DataprocMetastoreService

--- apis/refs/dataprocmetastoreservice_reference_test.go
+++ apis/refs/dataprocmetastoreservice_reference_test.go
@@ -15,12 +15,16 @@
 package refs
 
 import (
+	"context"
 	"testing"
 
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
-func TestDataprocMetastoreServiceRef(t *testing.T) {
+func TestDataprocMetastoreServiceRef_Basic(t *testing.T) {
 	ref := &DataprocMetastoreServiceRef{
 		Name:      "my-service",
 		Namespace: "my-namespace",
@@ -44,14 +48,6 @@ func TestDataprocMetastoreServiceRef(t *testing.T) {
 		t.Errorf("GetExternal() = %v, want %v", ref.GetExternal(), externalRef)
 	}
 
-	if err := ref.ValidateExternal(externalRef); err != nil {
-		t.Errorf("ValidateExternal() unexpected error: %v", err)
-	}
-
-	if err := ref.ValidateExternal("invalid-ref"); err == nil {
-		t.Errorf("ValidateExternal() expected error on invalid ref, got nil")
-	}
-
 	parsedIdentity, err := ref.ParseExternalToIdentity()
 	if err != nil {
 		t.Errorf("ParseExternalToIdentity() unexpected error: %v", err)
@@ -72,5 +68,162 @@ func TestDataprocMetastoreServiceRef(t *testing.T) {
 		actualIdentity.Location != expectedIdentity.Location ||
 		actualIdentity.Service != expectedIdentity.Service {
 		t.Errorf("ParseExternalToIdentity() = %+v, want %+v", actualIdentity, expectedIdentity)
+	}
+}
+
+func TestDataprocMetastoreServiceRef_ValidateExternal(t *testing.T) {
+	tests := []struct {
+		name     string
+		external string
+		wantErr  bool
+	}{
+		{
+			name:     "valid external relative path",
+			external: "projects/my-project/locations/us-central1/services/my-service",
+			wantErr:  false,
+		},
+		{
+			name:     "valid external relative path with trailing slash",
+			external: "projects/my-project/locations/us-central1/services/my-service/",
+			wantErr:  false,
+		},
+		{
+			name:     "valid external host prefixed path",
+			external: "metastore.googleapis.com/projects/my-project/locations/us-central1/services/my-service",
+			wantErr:  false,
+		},
+		{
+			name:     "valid external scheme and host prefixed path",
+			external: "https://metastore.googleapis.com/projects/my-project/locations/us-central1/services/my-service",
+			wantErr:  false,
+		},
+		{
+			name:     "valid external double slash and host prefixed path",
+			external: "//metastore.googleapis.com/projects/my-project/locations/us-central1/services/my-service",
+			wantErr:  false,
+		},
+		{
+			name:     "invalid external - missing service name",
+			external: "projects/my-project/locations/us-central1/services/",
+			wantErr:  true,
+		},
+		{
+			name:     "invalid external - missing services segment",
+			external: "projects/my-project/locations/us-central1/",
+			wantErr:  true,
+		},
+		{
+			name:     "invalid external - empty location",
+			external: "projects/my-project/locations//services/my-service",
+			wantErr:  true,
+		},
+		{
+			name:     "invalid external - empty project",
+			external: "projects//locations/us-central1/services/my-service",
+			wantErr:  true,
+		},
+		{
+			name:     "invalid external - too many segments",
+			external: "projects/my-project/locations/us-central1/services/my-service/extra",
+			wantErr:  true,
+		},
+		{
+			name:     "invalid external - wrong prefix",
+			external: "other/my-project/locations/us-central1/services/my-service",
+			wantErr:  true,
+		},
+		{
+			name:     "invalid external - wrong host",
+			external: "https://other.googleapis.com/projects/my-project/locations/us-central1/services/my-service",
+			wantErr:  true,
+		},
+		{
+			name:     "invalid external - empty string",
+			external: "",
+			wantErr:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &DataprocMetastoreServiceRef{}
+			err := r.ValidateExternal(tt.external)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ValidateExternal() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestDataprocMetastoreServiceRef_Normalize(t *testing.T) {
+	s := runtime.NewScheme()
+
+	service := &unstructured.Unstructured{}
+	service.SetGroupVersionKind(DataprocMetastoreServiceGVK)
+	service.SetName("my-service")
+	service.SetNamespace("my-ns")
+	service.Object["spec"] = map[string]interface{}{
+		"resourceID": "my-service-id",
+		"location":   "us-central1",
+		"projectRef": map[string]interface{}{
+			"external": "my-project",
+		},
+	}
+
+	reader := fake.NewClientBuilder().WithScheme(s).WithObjects(service).Build()
+
+	tests := []struct {
+		name             string
+		ref              *DataprocMetastoreServiceRef
+		defaultNamespace string
+		want             string
+		wantErr          bool
+	}{
+		{
+			name: "external reference",
+			ref: &DataprocMetastoreServiceRef{
+				External: "projects/my-project/locations/us-central1/services/my-service",
+			},
+			want: "projects/my-project/locations/us-central1/services/my-service",
+		},
+		{
+			name: "internal reference with namespace",
+			ref: &DataprocMetastoreServiceRef{
+				Name:      "my-service",
+				Namespace: "my-ns",
+			},
+			want: "projects/my-project/locations/us-central1/services/my-service-id",
+		},
+		{
+			name: "internal reference with default namespace",
+			ref: &DataprocMetastoreServiceRef{
+				Name: "my-service",
+			},
+			defaultNamespace: "my-ns",
+			want:             "projects/my-project/locations/us-central1/services/my-service-id",
+		},
+		{
+			name: "internal reference not found",
+			ref: &DataprocMetastoreServiceRef{
+				Name:      "non-existent",
+				Namespace: "my-ns",
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.ref.Normalize(context.Background(), reader, tt.defaultNamespace)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Normalize() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !tt.wantErr {
+				if tt.ref.External != tt.want {
+					t.Errorf("Normalize() got = %v, want %v", tt.ref.External, tt.want)
+				}
+			}
+		})
 	}
 }
