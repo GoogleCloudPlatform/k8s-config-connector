1844199c0da6732e4060fb2c7d9ec4733c14216c
codebot-robot <codebot-robot@google.com>
2026-06-18 06:19:26 +0000

    test: Add TPUQueuedResource gcpurls template exception for CAI check

--- pkg/gcpurls/registry_test.go
+++ pkg/gcpurls/registry_test.go
@@ -121,6 +121,7 @@ func TestRegisteredTemplatesMatchCAI(t *testing.T) {
 		"//artifactregistry.googleapis.com/projects/{}/locations/{}/vpcscConfig":                    true,
 		"//iap.googleapis.com/projects/{}/brands/{}":                                                true,
 		"//serviceusage.googleapis.com/projects/{}/services/{}/identity":                            true,
+		"//tpu.googleapis.com/projects/{}/locations/{}/queuedResources/{}":                          true,
 	}
 	for _, tmpl := range templates {
 		fullURL := "//" + tmpl.Host() + "/" + tmpl.CanonicalForm()
