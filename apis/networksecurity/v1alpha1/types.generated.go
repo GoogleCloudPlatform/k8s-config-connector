75427390516fd2f078088699b915b4f728766418
codebot-robot <codebot-robot@google.com>
2026-06-18 07:23:18 +0000

    fixup! chore: regenerate legacy clients, types, and static config

--- apis/networksecurity/v1alpha1/types.generated.go
+++ apis/networksecurity/v1alpha1/types.generated.go
@@ -18,6 +18,7 @@
 // krm.version: v1alpha1
 // proto.service: google.cloud.networksecurity.v1alpha1
 // resource: NetworkSecurityPartnerSSERealm:PartnerSSERealm
+// resource: NetworkSecurityPartnerSSEGateway:PartnerSSEGateway
 
 package v1alpha1
 
