3df0e207303909159e330a15dd9e482a6d372531
codebot-robot <codebot-robot@google.com>
2026-06-18 19:59:01 +0000

    fixup! ai: chore: Implement direct types and IdentityV2 for ManagedKafkaConnectCluster

--- pkg/clients/generated/client/clientset/versioned/typed/managedkafka/v1alpha1/fake/fake_managedkafka_client.go
+++ pkg/clients/generated/client/clientset/versioned/typed/managedkafka/v1alpha1/fake/fake_managedkafka_client.go
@@ -31,6 +31,10 @@ type FakeManagedkafkaV1alpha1 struct {
 	*testing.Fake
 }
 
+func (c *FakeManagedkafkaV1alpha1) ManagedKafkaConnectClusters(namespace string) v1alpha1.ManagedKafkaConnectClusterInterface {
+	return newFakeManagedKafkaConnectClusters(c, namespace)
+}
+
 func (c *FakeManagedkafkaV1alpha1) ManagedKafkaConsumerGroups(namespace string) v1alpha1.ManagedKafkaConsumerGroupInterface {
 	return newFakeManagedKafkaConsumerGroups(c, namespace)
 }
