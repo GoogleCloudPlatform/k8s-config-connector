02fb32f736dba8ea54fc7bcd8b22c32b5636651d
codebot-robot <codebot-robot@google.com>
2026-07-07 03:48:04 +0000

    fixup! ai:chore: Follow-up fixes for CloudRunInstance (#9005)

---
+++ pkg/controller/direct/run/cloudruninstance_controller.go
@@ -0,0 +1,20 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package run
+
+// This is a placeholder file for CloudRunInstance to allow dev/tasks/generate_static_config.py
+// to automatically discover and register CloudRunInstance in pkg/controller/resourceconfig/static_config.go.
+//
+// RegisterModel(krm.CloudRunInstanceGVK
