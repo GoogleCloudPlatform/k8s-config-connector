c90101df61481202a1a640552bb35ef67d4451fd
codebot-robot <codebot-robot@google.com>
2026-07-08 00:42:29 +0000

    Greenfield: Implement direct KRM types, CRD, and identity for NetworkSecuritySecurityProfileGroup

--- apis/networksecurity/v1alpha1/generate.sh
+++ apis/networksecurity/v1alpha1/generate.sh
@@ -47,6 +47,7 @@ go run . generate-types \
   --resource NetworkSecurityMirroringEndpointGroup:MirroringEndpointGroup \
   --resource NetworkSecuritySACRealm:SACRealm \
   --resource NetworkSecuritySecurityProfile:SecurityProfile \
+  --resource NetworkSecuritySecurityProfileGroup:SecurityProfileGroup \
   --resource NetworkSecurityFirewallEndpointAssociation:FirewallEndpointAssociation \
   --resource NetworkSecurityGatewaySecurityPolicy:GatewaySecurityPolicy \
   --resource NetworkSecurityTLSInspectionPolicy:TlsInspectionPolicy \
