3bec9b831527028a0f37e376f403499a8154c470
Lucas Daniel Madariaga <lmadariaga@google.com>
2026-07-24 22:46:37 +0000

    chore: fix certificatemanager multiversion mapper generation race condition

--- apis/datamigration/generate.sh
+++ apis/datamigration/generate.sh
@@ -35,6 +35,10 @@ go run . generate-mapper \
     --service google.cloud.clouddms.v1 \
     --api-version datamigration.cnrm.cloud.google.com/v1alpha1
 
+# Fix acronym for IntermediateCas -> IntermediateCAs
+sed -i 's/out.IntermediateCas = direct.Slice_FromProto/out.IntermediateCAs = direct.Slice_FromProto/g' "${REPO_ROOT}/pkg/controller/direct/certificatemanager/mapper.generated.go"
+sed -i 's/Slice_ToProto(mapCtx, in.IntermediateCas,/Slice_ToProto(mapCtx, in.IntermediateCAs,/g' "${REPO_ROOT}/pkg/controller/direct/certificatemanager/mapper.generated.go"
+
 cd ${REPO_ROOT}
 dev/tasks/generate-crds
 
